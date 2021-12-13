3eb826d08dae7e82cfb900d17cab9418a9506dd1
hamilton <hamilton.ulmer@gmail.com>
2021-12-13 13:16:59 -0800

    swap RowIcon for ColumnIcon (close #9)

--- src/lib/components/ColumnIcon.svelte
+++ src/lib/components/ColumnIcon.svelte
@@ -1,8 +1,7 @@
 <script>
 export let size = 24;
 </script>
-
-<svg  shape-rendering=geometricPrecision width={size} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect x="4.5" y="4.5" width="5" height="15" stroke="currentColor"/>
-    <rect x="14.5" y="4.5" width="5" height="15" stroke="currentColor"/>
+<svg shape-rendering=geometricPrecision width={size} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect x="19.5" y="4.5" width="5" height="15" transform="rotate(90 19.5 4.5)" stroke="currentColor"/>
+    <rect x="19.5" y="14.5" width="5" height="15" transform="rotate(90 19.5 14.5)" stroke="currentColor"/>
 </svg>
