84e49244c07e487a9bf4d007bc9f79f50076861e
Hamilton Ulmer <hamilton.ulmer@gmail.com>
2022-01-09 07:34:47 -0800

    move index to model

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,6 +4,12 @@ import { setContext } from "svelte";
 import { createStore } from '$lib/app-store';
 import { browser } from "$app/env";
 
+import { fade } from "svelte/transition";
+
+import AddIcon from "$lib/components/icons/AddIcon.svelte";
+import RefreshIcon from "$lib/components/icons/RefreshIcon.svelte";
+import Spinner from "$lib/components/Spinner.svelte";
+import Logo from "$lib/components/Logo.svelte";
 import NotificationCenter from "$lib/components/notifications/NotificationCenter.svelte";
 import notification from "$lib/components/notifications/";
 
@@ -15,8 +21,99 @@ if (browser) {
   notification.listenToSocket(store.socket);
 }
 
+
+let dbRunState = 'disconnected';
+let runstateTimer;
+
+function debounceRunstate(state) {
+  if (runstateTimer) clearTimeout(runstateTimer);
+  setTimeout(() => {
+    dbRunState = state;
+  }, 500)
+}
+
+$: debounceRunstate($store?.status || 'disconnected');
+
 </script>
 
-<slot />
+
+
+<header class="header pr-3">
+  <h1><Logo /></h1>
+  <div class='grid grid-flow-col'>
+    <div id="controls" class="grid grid-flow-col">
+      <!-- FIXME: move this to slot in __layout -->
+      <button  on:click={() => store.action("addQuery", {})}><AddIcon size={18} /></button>
+      <button on:click={() => store.action('reset')}>
+          <RefreshIcon size={18} />
+      </button>
+    </div>
+  </div>
+    <div></div>
+    <div class="self-center">
+      {#if dbRunState === 'running'}
+      <div transition:fade={{ duration: 300 }}>
+        <Spinner />
+      </div>
+    {/if}
+    </div>
+
+
+</header>
+<div class='body'>
+  <slot />
+  </div>
 
 <NotificationCenter />
+
+
+<style>
+  .body {
+    width: calc(100vw);
+    display: grid;
+    grid-template-columns: max-content auto max-content;
+    align-content: stretch;
+    min-height: calc(100vh - var(--header-height));
+  }
+  
+  header {
+    box-sizing: border-box;
+    margin:0;
+    background: linear-gradient(to right, hsl(300, 30%, 14%), hsl(300, 60%, 18%));
+    color: white;
+    height: var(--header-height);
+    display: grid;
+    justify-items: left;
+    justify-content: stretch;
+    align-items: stretch;
+    align-content: stretch;
+    grid-template-columns: max-content max-content auto max-content;
+  }
+  
+  header h1 {
+    font-size:13px;
+    font-weight: normal;
+    margin:0;
+    padding:0;
+    display: grid;
+    place-items: center;
+    padding: 0px 12px;
+    padding-left: 2px;
+    margin-left: 1rem;
+  }
+  
+  header button {
+    color: white;
+    background-color: transparent;
+    display: grid;
+    place-items: center;
+    padding: 0px 12px;
+    border:none;
+    font-size: 1.5rem;
+  }
+  
+  header button:hover {
+    background-color: hsla(var(--hue), var(--sat), var(--lgt), .1);
+  }
+  
+  </style>
--- src/routes/model.svelte
+++ src/routes/model.svelte
@@ -1,56 +1,14 @@
 <script>
 import { getContext } from "svelte";
-import { fade } from "svelte/transition";
-
-import AddIcon from "$lib/components/icons/AddIcon.svelte";
-import RefreshIcon from "$lib/components/icons/RefreshIcon.svelte";
-import Logo from "$lib/components/Logo.svelte";
-import Spinner from "$lib/components/Spinner.svelte";
 
 import EditorPane from "./_panes/EditorPane.svelte";
 import InspectorPane from "./_panes/InspectorPane.svelte";
 import AssetsPane from "./_panes/AssetsPane.svelte";
 
-let resultset;
-let queryInfo;
-let query;
-let destinationInfo;
-
-// FIXME: this is out of control :(
-let destinationSize;
-
 const store = getContext("rill:app:store");
-
-let dbRunState = 'disconnected';
-let runstateTimer;
-
-function debounceRunstate(state) {
-  if (runstateTimer) clearTimeout(runstateTimer);
-  setTimeout(() => {
-    dbRunState = state;
-  }, 500)
-}
-
-$: debounceRunstate($store?.status || 'disconnected');
-
 </script>
 
-<header class="header pr-3">
-  <h1><Logo /></h1>
-  <button  on:click={() => store.action("addQuery", {})}><AddIcon size={18} /></button>
-  <button on:click={() => store.action('reset')}>
-      <RefreshIcon size={18} />
-  </button>
-  <div></div>
-  <div class="self-center">
-    {#if dbRunState === 'running'}
-      <div transition:fade={{ duration: 300 }}>
-        <Spinner />
-      </div>
-    {/if}
-  </div>
-</header>
-<div class='body'>
+
   <div class="pane assets">
     <AssetsPane />
   </div>
@@ -61,57 +19,9 @@ $: debounceRunstate($store?.status || 'disconnected');
   <div class='pane outputs'>
     <InspectorPane />
     </div>
-  </div>
+
 
 <style>
-.body {
-  width: calc(100vw);
-  display: grid;
-  grid-template-columns: max-content auto max-content;
-  align-content: stretch;
-  min-height: calc(100vh - var(--header-height));
-}
-
-header {
-  box-sizing: border-box;
-  margin:0;
-  background: linear-gradient(to right, hsl(300, 30%, 14%), hsl(300, 60%, 18%));
-  color: white;
-  height: var(--header-height);
-  display: grid;
-  justify-items: left;
-  justify-content: stretch;
-  align-items: stretch;
-  align-content: stretch;
-  grid-template-columns: max-content max-content max-content auto max-content;
-}
-
-header h1 {
-  font-size:13px;
-  font-weight: normal;
-  margin:0;
-  padding:0;
-  display: grid;
-  place-items: center;
-  padding: 0px 12px;
-  padding-left: 2px;
-  margin-left: 1rem;
-}
-
-header button {
-  color: white;
-  background-color: transparent;
-  display: grid;
-  place-items: center;
-  padding: 0px 12px;
-  border:none;
-  font-size: 1.5rem;
-}
-
-header button:hover {
-  background-color: hsla(var(--hue), var(--sat), var(--lgt), .1);
-}
-
 .inputs {
   --hue: 217;
   --sat: 20%;
@@ -126,10 +36,6 @@ header button:hover {
 
 .pane {
   box-sizing: border-box;
-}
-
-.outputs {
-  /* padding: 1rem; */
 }
 
 .pane:first-child {
