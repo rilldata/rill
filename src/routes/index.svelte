5e34f9fc663b9d2d968dfea73ed633046dc4c49a
Hamilton Ulmer <hamilton.ulmer@gmail.com>
2022-01-09 07:36:09 -0800

    adds index redirect to model

---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script context="module">
+    // Redirect to /model for now
+    export async function load() {
+        return {
+            status: 302,
+            redirect: "/model"
+        };
+    }
+</script>
