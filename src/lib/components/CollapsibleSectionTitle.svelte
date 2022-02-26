79e392af20d64da6aa10c1d94e9ed476f6fadd57
Hamilton Ulmer <hamilton.ulmer@gmail.com>
2022-02-25 18:23:55 -0800

    detail work on CollapsibleSectionTitle.svelte

--- src/lib/components/CollapsibleSectionTitle.svelte
+++ src/lib/components/CollapsibleSectionTitle.svelte
@@ -3,17 +3,39 @@
  * for expandable asset elements. By contrast, this element is used as a header element
  * for asset sections.
 */
-
+import { onMount } from "svelte";
+import { fly } from "svelte/transition";
 import CaretDownIcon from "$lib/components/icons/CaretDownIcon.svelte";
+
+import Tooltip from "$lib/components/tooltip/Tooltip.svelte";
+import TooltipContent from "$lib/components/tooltip/TooltipContent.svelte";
+
 import type { SvelteComponent } from "svelte";
 export let active = true;
 export let icon:SvelteComponent;
+let duration = 0;
+
+let tooltipActive;
+
+// here's where we handle setting the active tooltip
+// duration, which will help us animate the fly element.
+let timer;
+$: if (tooltipActive) {
+    timer = setTimeout(() => {
+        duration = 150;
+    }, 200);
+    
+} else {
+    clearTimeout(timer);
+    duration = 0;
+}
 </script>
 
 <div 
     class='collapsible-title align grid grid-cols-max justify-between'
     style="grid-template-columns: auto max-content;"
 >
+<Tooltip location="right" alignment="middle" distance={8} bind:active={tooltipActive}>
     <button 
         class="
             bg-transparent 
@@ -39,7 +61,16 @@ export let icon:SvelteComponent;
             <slot />
         </div>
     </button>
-
+    <TooltipContent slot="tooltip-content">
+        <div class="relative">
+            <span class="invisible">{active ? "hide" : "show"}</span>
+            {#key active}
+                <div class="absolute" style:left="0" style:top="0px" transition:fly={{duration, y: 7.5 * (!active ? 1 : -1)}}>{active ? "hide" : "show"}</div>
+            {/key}
+        </div>
+        
+    </TooltipContent>
+</Tooltip>
     <div class="contextual-information justify-self-stretch text-right">
         <slot name="contextual-information" />
     </div>
