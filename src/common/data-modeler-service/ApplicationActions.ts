3f18cb75c204addb2280ea48c12ffb7d293269c7
Hamilton Ulmer <hamilton.ulmer@gmail.com>
2022-03-17 19:03:50 -0700

    Update src/common/data-modeler-service/ApplicationActions.ts

--- src/common/data-modeler-service/ApplicationActions.ts
+++ src/common/data-modeler-service/ApplicationActions.ts
@@ -49,7 +49,6 @@ export class ApplicationActions extends  DataModelerActions {
     private getNextEntityId(entities: Array<EntityRecord>, entityId: string): string {
         if (entities.length === 1) return undefined;
         const idx = entities.findIndex(entity => entity.id === entityId);
-        // console.log(entityId, idx, entities.map(entity => entity.id));
         if (idx === 0) {
             return entities[idx + 1].id;
         } else {
