a2432f8daf39d06a41a29ca97b0b35be7a5929f4
Aditya Hegde <adityahegderocks@gmail.com>
2022-03-24 17:46:06 +0530

    Adding IP related info collection

--- src/cli/post-install.ts
+++ src/cli/post-install.ts
@@ -4,18 +4,34 @@ import { LocalConfig} from "$common/config/LocalConfig";
 import { guidGenerator } from "$lib/util/guid";
 import { ApplicationConfigFolder, LocalConfigFile } from "$common/config/ConfigFolders";
 
-if (!existsSync(ApplicationConfigFolder)) {
-    mkdirSync(ApplicationConfigFolder, {recursive: true});
-    console.log("creating folder");
-}
+/**
+ * Initializes the rill local config.
+ * 1. Creates a folder under ApplicationConfigFolder
+ * 2. Creates a config file LocalConfigFile
+ * 3. Generates installId and saves into the LocalConfigFile
+ *
+ * This is run as postinstall npm script.
+ */
 
-let configJson;
-if (existsSync(LocalConfigFile)) {
-    configJson = JSON.parse(readFileSync(LocalConfigFile).toString());
-} else {
-    configJson = {};
-}
-const configObject = new LocalConfig(configJson);
-configObject.installId = guidGenerator();
+(async () => {
+    if (!existsSync(ApplicationConfigFolder)) {
+        mkdirSync(ApplicationConfigFolder, {recursive: true});
+        console.log("creating folder");
+    }
 
-writeFileSync(LocalConfigFile, JSON.stringify(configObject));
+    let configJson;
+    if (existsSync(LocalConfigFile)) {
+        configJson = JSON.parse(readFileSync(LocalConfigFile).toString());
+    } else {
+        configJson = {};
+    }
+    const configObject = new LocalConfig(configJson);
+    configObject.installId = guidGenerator();
+
+    // We should instead move this to UI
+    // configObject.sendTelemetryData = await cliConfirmation(
+    //     "We collect usage info to improve the data modeler. " +
+    //     "Send anonymous tracking data? (y/N)");
+
+    writeFileSync(LocalConfigFile, JSON.stringify(configObject));
+})();
--- src/lib/metrics/collectCommonUserFields.ts
+++ src/lib/metrics/collectCommonUserFields.ts
@@ -4,10 +4,20 @@ import type { CommonUserFields } from "$common/metrics/MetricsTypes";
 export async function collectCommonUserFields(): Promise<CommonUserFields> {
     const parser = new UAParser();
     const result = parser.getResult();
+    let ipInfo;
+    try {
+        ipInfo = await (await fetch("https://ipapi.co/json/")).json();
+    } catch (err) {
+        ipInfo = {
+            city: "",
+            country_code: "",
+            languages: ",",
+        }
+    }
     return {
-        country_code: "",
-        city: "",
-        locale: "",
+        country_code: ipInfo.country_code,
+        city: ipInfo.city,
+        locale: ipInfo.languages.split(",")[0],
         browser: result.browser.name,
         os: result.os.name,
         device_model: result.os.model,
