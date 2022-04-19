7121519b75ee56fc16812f28ba6238ec7b521c08
Marissa Gorlick <magorlick@gmail.com>
2022-04-19 09:16:53 -0500

    Update example-project.sh

--- scripts/example-project.sh
+++ scripts/example-project.sh
@@ -3,9 +3,8 @@ npm run cli --silent -- init --project ../rill-developer-example
 mkdir ../rill-developer-example/data
 echo 'downloading datasets...'
 #download dataset and it's readme
-curl -s http://pkg.rilldata.com/rill-developer-example/data/aircraftDatabase-2022-02.csv --output ../rill-developer-example/data/flightlist_2022_02.csv.gz
-gunzip ../rill-developer-example/data/flightlist_2022_02.csv.gz
-curl -s http://pkg.rilldata.com/rill-developer-example/data/flightlist.zip --output ../rill-developer-example/data/flightlist_README.md
+curl -s http://pkg.rilldata.com/rill-developer-example/data/aircraftDatabase-2022-02.csv --output ../rill-developer-example/data/flightlist_2022_02.csv
+curl -s http://pkg.rilldata.com/rill-developer-example/data/flightlist.zip --output ../rill-developer-example/data/flightlist_README.zip
 
 # import tables
 npm run cli --silent -- import-table ../rill-developer-example/data/flightlist_2022_02.csv --project ../rill-developer-example
