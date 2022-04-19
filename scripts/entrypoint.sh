ddedf165ef808290e1a2ce8e7fe1c7cd0beabdb3
Himadri Singh <himadri.singh@gmail.com>
2022-04-19 19:42:48 +0530

    use aircraftDatabase-2022-02.csv

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -7,14 +7,12 @@ fi
 
 if [ "${PROJECT}" == "rill-developer-example" ] && [ ! -d ${PROJECT_BASE}/${PROJECT}/data ]; then
   echo "Downloading dataset for example project..."
-  mkdir ${PROJECT_BASE}/${PROJECT}/data
 
-  curl --progress-bar https://zenodo.org/record/6325961/files/flightlist_20220201_20220228.csv.gz --output ${PROJECT_BASE}/${PROJECT}/data/flightlist_2022_02.csv.gz
-  curl -s https://zenodo.org/record/6325961/files/readme.md --output ${PROJECT_BASE}/${PROJECT}/data/flightlist_README.md
-  gunzip ${PROJECT_BASE}/${PROJECT}/data/flightlist_2022_02.csv.gz
+  mkdir ${PROJECT_BASE}/${PROJECT}/data
+  curl --progress-bar http://pkg.rilldata.com/rill-developer-example/data/aircraftDatabase-2022-02.csv --output ${PROJECT_BASE}/${PROJECT}/data/aircraftDatabase-2022-02.csv
 
   echo "Importing example dataset into the project..."
-  rill-developer import-table ${PROJECT_BASE}/${PROJECT}/data/flightlist_2022_02.csv --project ${PROJECT_BASE}/${PROJECT}
+  rill-developer import-table ${PROJECT_BASE}/${PROJECT}/data/aircraftDatabase-2022-02.csv --project ${PROJECT_BASE}/${PROJECT}
 else
   echo "Please refer to README.md for importing datasets."
 fi
