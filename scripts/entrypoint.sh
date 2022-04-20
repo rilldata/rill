4ad47eb86e38ffa54921089b16f3aec156b5dda8
Himadri Singh <himadri.singh@gmail.com>
2022-04-20 20:25:17 +0530

    run a init script in docker

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,20 +1,14 @@
 #!/usr/bin/env bash
 
-if [ ! -d ${PROJECT_BASE}/${PROJECT} ]; then
+if [ ! -z "${PROJECT_BASE}" ] && [ ! -z "${PROJECT}" ] && [ ! -d ${PROJECT_BASE}/${PROJECT} ]; then
   echo "Initializing the project ${PROJECT}..."
   rill-developer init --project ${PROJECT_BASE}/${PROJECT}
 fi
 
-if [ "${PROJECT}" == "rill-developer-example" ] && [ ! -d ${PROJECT_BASE}/${PROJECT}/data ]; then
-  echo "Downloading dataset for example project..."
-
-  mkdir ${PROJECT_BASE}/${PROJECT}/data
-  curl --progress-bar http://pkg.rilldata.com/rill-developer-example/data/aircraftDatabase-2022-02.csv --output ${PROJECT_BASE}/${PROJECT}/data/aircraftDatabase-2022-02.csv
-
-  echo "Importing example dataset into the project..."
-  rill-developer import-table ${PROJECT_BASE}/${PROJECT}/data/aircraftDatabase-2022-02.csv --project ${PROJECT_BASE}/${PROJECT}
-else
-  echo "Please refer to README.md for importing datasets."
+if [ ! -z "${INIT_SCRIPT}" ] && [ -f ${INIT_SCRIPT} ]; then
+ echo "Found init script at ${INIT_SCRIPT}..."
+ source ${INIT_SCRIPT}
 fi
 
+echo "Starting Rill Developer at project ${PROJECT_BASE}/${PROJECT}..."
 rill-developer start --project ${PROJECT_BASE}/${PROJECT}
