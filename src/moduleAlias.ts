c98985cc89d6a20e60932f648e4ec08127ad419b
greenep12 <ericpgreen2@gmail.com>
2022-05-06 17:37:44 -0700

    add $server module alias

--- src/moduleAlias.ts
+++ src/moduleAlias.ts
@@ -2,5 +2,6 @@ import moduleAlias from "module-alias";
 moduleAlias.addAliases({
     "$lib": __dirname + "/lib",
     "$common": __dirname + "/common",
-    "$cli": __dirname + "/cli"
+    "$cli": __dirname + "/cli",
+    "$server": __dirname + "/server"
 });
