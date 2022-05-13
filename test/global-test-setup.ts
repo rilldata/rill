a4465b0ebf5a0a9093a7712ad7969e75f019140f
bcolloran <brendan@colloran.net>
2022-05-13 13:52:37 -0700

    remove console noise from test console (#228)
    * mock `console.error` during tests, remove some console.logs
    
    * kebab case

---
+++ test/global-test-setup.ts
@@ -0,0 +1,11 @@
+global.console = {
+  ...console,
+  // uncomment to ignore a specific log level
+  // log: jest.fn(),
+  // debug: jest.fn(),
+  // info: jest.fn(),
+  // warn: jest.fn(),
+  error: jest.fn(),
+};
+
+export {};
