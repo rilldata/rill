921fa9271eb0c85b06212f9a756c485e6fd5bcc6
bcolloran <brendan@colloran.net>
2022-05-16 10:45:03 -0700

    apply prettier (#241)

--- src/cli/InfoCommand.ts
+++ src/cli/InfoCommand.ts
@@ -1,49 +1,65 @@
 import { DataModelerCliCommand } from "$cli/DataModelerCliCommand";
 import { Command } from "commander";
 import type { DataModelerStateService } from "$common/data-modeler-state-service/DataModelerStateService";
-import { EntityType, StateType } from "$common/data-modeler-state-service/entity-state-service/EntityStateService";
+import {
+  EntityType,
+  StateType,
+} from "$common/data-modeler-state-service/entity-state-service/EntityStateService";
 
 export class InfoCommand extends DataModelerCliCommand {
-    public getCommand(): Command {
-        return this.applyCommonSettings(
-            new Command("info"),
-            "Display information about project.",
-        )
-            .action((opts, command: Command) => {
-                const {project} = command.optsWithGlobals();
-                return this.run({projectPath: project});
-            });
-    }
+  public getCommand(): Command {
+    return this.applyCommonSettings(
+      new Command("info"),
+      "Display information about project."
+    ).action((opts, command: Command) => {
+      const { project } = command.optsWithGlobals();
+      return this.run({ projectPath: project });
+    });
+  }
 
-    protected async sendActions(): Promise<void> {
-        InfoCommand.displayProjectInfo(this.projectPath, this.dataModelerStateService);
-    }
+  protected async sendActions(): Promise<void> {
+    InfoCommand.displayProjectInfo(
+      this.projectPath,
+      this.dataModelerStateService
+    );
+  }
 
-    public static displayProjectInfo(projectPath: string, dataModelerStateService: DataModelerStateService) {
-        console.log("*** Project Info ***");
-        console.log(`Project Path: ${projectPath}`);
+  public static displayProjectInfo(
+    projectPath: string,
+    dataModelerStateService: DataModelerStateService
+  ) {
+    console.log("*** Project Info ***");
+    console.log(`Project Path: ${projectPath}`);
 
-        this.displayTablesInfo(dataModelerStateService);
-        this.displayModelsInfo(dataModelerStateService);
-    }
+    this.displayTablesInfo(dataModelerStateService);
+    this.displayModelsInfo(dataModelerStateService);
+  }
 
-    private static displayTablesInfo(dataModelerStateService: DataModelerStateService) {
-        const tableState = dataModelerStateService
-            .getEntityStateService(EntityType.Table, StateType.Persistent)
-            .getCurrentState();
-        if (!tableState?.entities.length) return;
+  private static displayTablesInfo(
+    dataModelerStateService: DataModelerStateService
+  ) {
+    const tableState = dataModelerStateService
+      .getEntityStateService(EntityType.Table, StateType.Persistent)
+      .getCurrentState();
+    if (!tableState?.entities.length) return;
 
-        console.log("Imported Tables:");
-        tableState.entities.forEach(table => console.log(`${table.tableName} (${table.path})`));
-    }
+    console.log("Imported Tables:");
+    tableState.entities.forEach((table) =>
+      console.log(`${table.tableName} (${table.path})`)
+    );
+  }
 
-    private static displayModelsInfo(dataModelerStateService: DataModelerStateService) {
-        const modelState = dataModelerStateService
-            .getEntityStateService(EntityType.Model, StateType.Persistent)
-            .getCurrentState();
-        if (!modelState?.entities.length) return;
+  private static displayModelsInfo(
+    dataModelerStateService: DataModelerStateService
+  ) {
+    const modelState = dataModelerStateService
+      .getEntityStateService(EntityType.Model, StateType.Persistent)
+      .getCurrentState();
+    if (!modelState?.entities.length) return;
 
-        console.log("Models:");
-        modelState.entities.forEach(model => model.query && console.log(`${model.name}: ${model.query}`));
-    }
+    console.log("Models:");
+    modelState.entities.forEach(
+      (model) => model.query && console.log(`${model.name}: ${model.query}`)
+    );
+  }
 }
--- src/cli/bump-version.ts
+++ src/cli/bump-version.ts
@@ -1,23 +1,27 @@
-import {readFileSync, writeFileSync} from "fs";
+import { readFileSync, writeFileSync } from "fs";
 import semverInc from "semver/functions/inc";
-import {execSync} from "node:child_process";
+import { execSync } from "node:child_process";
 
 const BUMP_TYPES = {
-    "major": 1,
-    "minor": 1,
-    "patch": 1,
+  major: 1,
+  minor: 1,
+  patch: 1,
 };
 const PACKAGE_JSON_FILE = "./package.json";
 const PACKAGE_LOCK_JSON_FILE = "./package-lock.json";
-const execSyncToStdout = cmd => execSync(cmd, { stdio: "inherit" });
+const execSyncToStdout = (cmd) => execSync(cmd, { stdio: "inherit" });
 
 const packageJsonString = readFileSync(PACKAGE_JSON_FILE).toString();
 const currentVersion = JSON.parse(packageJsonString).version;
 const bumpType = process.argv[2];
 
 if (!(bumpType in BUMP_TYPES)) {
-    console.log(`Invalid version bump type: ${bumpType}. Can be one of ${Object.keys(BUMP_TYPES)}`);
-    process.exit(1);
+  console.log(
+    `Invalid version bump type: ${bumpType}. Can be one of ${Object.keys(
+      BUMP_TYPES
+    )}`
+  );
+  process.exit(1);
 }
 
 console.log("Pulling latest changes from main branch");
@@ -27,10 +31,20 @@ execSyncToStdout("git pull");
 const newVersion = semverInc(currentVersion, bumpType);
 
 console.log(`Bumping version from ${currentVersion} to ${newVersion}`);
-writeFileSync(PACKAGE_JSON_FILE, packageJsonString
-    .replace(`"version": "${currentVersion}",`, `"version": "${newVersion}",`));
-writeFileSync(PACKAGE_LOCK_JSON_FILE, packageJsonString
-    .replace(`"version": "${currentVersion}",`, `"version": "${newVersion}",`));
+writeFileSync(
+  PACKAGE_JSON_FILE,
+  packageJsonString.replace(
+    `"version": "${currentVersion}",`,
+    `"version": "${newVersion}",`
+  )
+);
+writeFileSync(
+  PACKAGE_LOCK_JSON_FILE,
+  packageJsonString.replace(
+    `"version": "${currentVersion}",`,
+    `"version": "${newVersion}",`
+  )
+);
 
 console.log("Creating new branch");
 const branchName = `release-candidate-${newVersion}`;
