33853500e0bc697b43e5a9fe35cea472c80ee06d
Eric P Green <ericpgreen2@gmail.com>
2022-06-02 08:46:56 -0700

    Quick wins to improve editor aesthetics (#327)
    * remove the outline when editor is focused
    
    * improve selection highlighting
    
    * minor code cleanups

--- src/lib/components/Editor.svelte
+++ src/lib/components/Editor.svelte
@@ -6,7 +6,6 @@
     Decoration,
     DecorationSet,
   } from "@codemirror/view";
-  import { RangeSet } from "@codemirror/rangeset";
   import { indentWithTab } from "@codemirror/commands";
   import { EditorState, StateField, StateEffect } from "@codemirror/state";
   import { basicSetup } from "@codemirror/basic-setup";
@@ -26,12 +25,6 @@
   let editor;
   let editorContainer;
   let editorContainerComponent;
-
-  export function refreshContent(newContent) {
-    editor.update({
-      changes: { from: 0, to: editor?.doc?.length || 0, insert: newContent },
-    });
-  }
 
   const addUnderline = StateEffect.define<{ from: number; to: number }>();
 
@@ -59,19 +52,27 @@
   const underlineMark = Decoration.mark({ class: "cm-underline" });
 
   const underlineTheme = EditorView.baseTheme({
-    ".cm-underline": { backgroundColor: "yellow", outline: "2px solid red" },
+    ".cm-underline": {
+      backgroundColor: "rgb(254 240 138)",
+    },
   });
 
-  const breakpointGutter = [
-    EditorView.baseTheme({
-      ".cm-breakpoint-gutter .cm-gutterElement": {
-        color: "red",
-        paddingLeft: "24px",
-        paddingRight: "24px",
-        cursor: "default",
+  const rillTheme = EditorView.theme({
+    "&.cm-editor": {
+      "&.cm-focused": {
+        outline: "none",
       },
-    }),
-  ];
+    },
+    "&.cm-focused .cm-selectionBackground, .cm-selectionBackground, .cm-content ::selection":
+      { backgroundColor: "rgb(65 99 255 / 25%)" },
+    ".cm-selectionMatch": { backgroundColor: "rgb(189 233 255)" },
+    ".cm-breakpoint-gutter .cm-gutterElement": {
+      color: "red",
+      paddingLeft: "24px",
+      paddingRight: "24px",
+      cursor: "default",
+    },
+  });
 
   function underlineSelection(view: EditorView, selections) {
     const effects = selections
@@ -100,7 +101,7 @@
           basicSetup,
           sql(),
           keymap.of([indentWithTab]),
-          breakpointGutter,
+          rillTheme,
           EditorView.updateListener.of((v) => {
             const candidateLocation = v.state.selection.ranges[0].head;
             if (candidateLocation !== cursorLocation) {
@@ -134,7 +135,7 @@
   });
 </script>
 
-<div bind:this={componentContainer} class=" h-full">
+<div bind:this={componentContainer} class="h-full">
   <div class="editor-container border h-full" bind:this={editorContainer}>
     <div bind:this={editorContainerComponent} />
   </div>
