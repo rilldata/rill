6ce4debbe1cf910274e814f2cd9dd0479372c6ef
brendan collran <brendan@colloran.net>
2022-06-13 15:43:04 -0700

    add placeholder to workspace `index.svelte` to show how other workspace components can be added

--- src/routes/_surfaces/workspace/index.svelte
+++ src/routes/_surfaces/workspace/index.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
+  import { getContext } from "svelte";
   import ModelView from "./Model.svelte";
   import ModelWorkspaceHeader from "./ModelWorkspaceHeader.svelte";
+
+  import type { ApplicationStore } from "$lib/application-state-stores/application-store";
+
+  import { EntityType } from "$common/data-modeler-state-service/entity-state-service/EntityStateService";
+  const rillAppStore = getContext("rill:app:store") as ApplicationStore;
+
+  $: useModelWorkspace = $rillAppStore?.activeEntity?.type === EntityType.Model;
 </script>
 
-<ModelWorkspaceHeader />
-<ModelView />
+{#if useModelWorkspace}
+  <ModelWorkspaceHeader />
+  <ModelView />
+{:else}
+  <!-- FIXME: this placeholder is here to show where you would plug in another kind of workspace component -->
+  <ModelWorkspaceHeader />
+  <ModelView />
+{/if}
