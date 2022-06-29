78340df7c73781c93d4f0e0be130f80a6b14ff63
Dhiraj Kumar <sumankumaribarnwal@gmail.com>
2022-06-29 15:54:24 +0530

    Show onboarding only on init and remove flickers

--- src/cli/InitCommand.ts
+++ src/cli/InitCommand.ts
@@ -1,6 +1,7 @@
 import { DataModelerCliCommand } from "$cli/DataModelerCliCommand";
 import { Command } from "commander";
 import { existsSync, mkdirSync, copyFileSync } from "fs";
+import { EntityType } from "$common/data-modeler-state-service/entity-state-service/EntityStateService";
 
 export class InitCommand extends DataModelerCliCommand {
   private alreadyInitialised: boolean;
@@ -49,6 +50,11 @@ export class InitCommand extends DataModelerCliCommand {
     if (!this.alreadyInitialised) {
       // add a single model.
       await this.dataModelerService.dispatch("addModel", [{}]);
+      // set dummy asset as active to show onboarding steps
+      await this.dataModelerService.dispatch("setActiveAsset", [
+        EntityType.Model,
+        undefined,
+      ]);
       console.log(
         "\nYou have successfully initialized a new project with Rill Developer."
       );
