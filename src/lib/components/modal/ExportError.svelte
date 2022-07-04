5f9d15282efbe9d113aca2f9fec8a360f65f7b24
Dhiraj Barnwal <sumankumaribarnwal@gmail.com>
2022-07-04 18:58:17 +0530

    Process export before opening tab (#444)
    * Process export before opening tab
    
    * Show modal on export error
    
    * Abstract modal and export logic

---
+++ src/lib/components/modal/ExportError.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Modal from "$lib/components/modal/Modal.svelte";
+  import ModalAction from "$lib/components/modal/ModalAction.svelte";
+  import ModalActions from "$lib/components/modal/ModalActions.svelte";
+  import ModalContent from "$lib/components/modal/ModalContent.svelte";
+  import ModalTitle from "$lib/components/modal/ModalTitle.svelte";
+
+  export let showExportErrorModal = false;
+  export let exportErrorMessage = "";
+</script>
+
+<Modal
+  open={showExportErrorModal}
+  onBackdropClick={() => (showExportErrorModal = false)}
+>
+  <ModalTitle>export error</ModalTitle>
+  <ModalContent>{exportErrorMessage}</ModalContent>
+  <ModalActions>
+    <ModalAction onClick={() => (showExportErrorModal = false)}>
+      close
+    </ModalAction>
+  </ModalActions>
+</Modal>
