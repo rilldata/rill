eb9963da0af74ff317dcc52f549c3abaf124578d
Hamilton Ulmer <hamilton.ulmer@gmail.com>
2022-07-08 14:44:51 -0700

    clean up the metrics explore leaderboard for release (#496)
    * makes the leaderboars a bit mroe functional; fixes layout issues
    
    * adds back in MeasureBigNumber.svelte
    
    * roughs in more layout components
    
    * adds back in filter for now; fixes issue with resize observer disconnection
    
    * adds in some scaffolding for leaderboard descriptions, more to come
    
    * changes broken piece of MeasureBigNumber

--- src/routes/_surfaces/workspace/explore/ExploreHeader.svelte
+++ src/routes/_surfaces/workspace/explore/ExploreHeader.svelte
@@ -6,14 +6,14 @@
   import Tooltip from "$lib/components/tooltip/Tooltip.svelte";
   import TooltipContent from "$lib/components/tooltip/TooltipContent.svelte";
   import { store } from "$lib/redux-store/store-root";
-  import { MetricsExploreEntity } from "$lib/redux-store/explore/explore-slice";
+  import type { MetricsExploreEntity } from "$lib/redux-store/explore/explore-slice";
   import { isAnythingSelected } from "$lib/util/isAnythingSelected";
   import { clearSelectedLeaderboardValuesAndUpdate } from "$lib/redux-store/explore/explore-apis";
   import type { Readable } from "svelte/store";
   import { getMetricsExploreById } from "$lib/redux-store/explore/explore-readables";
+  import { getMetricsDefReadableById } from "$lib/redux-store/metrics-definition/metrics-definition-readables";
 
   export let metricsDefId: string;
-  export let whichReferenceValue = "global";
 
   let metricsLeaderboard: Readable<MetricsExploreEntity>;
   $: metricsLeaderboard = getMetricsExploreById(metricsDefId);
@@ -22,53 +22,23 @@
   function clearAllFilters() {
     clearSelectedLeaderboardValuesAndUpdate(store.dispatch, metricsDefId);
   }
+  $: metricsDefinition = getMetricsDefReadableById(metricsDefId);
 </script>
 
 <header
   style:grid-template-columns="auto max-content"
-  class="pb-6 pt-6 grid w-full bg-white"
+  class="grid w-full bg-white self-stretch"
 >
   <div>
     <h1 style:line-height="1.1">
-      <div class="pl-2 text-gray-600 font-normal" style:font-size="1.5rem">
-        Total Records
+      <div class="pl-2 text-gray-600" style:font-size="24px">
+        {#if $metricsDefinition}
+          {$metricsDefinition?.metricDefLabel}
+        {/if}
       </div>
     </h1>
   </div>
   <div class="justify-self-end">
-    <div
-      style:font-size="24px"
-      class="grid justify-items-end justify-end grid-flow-col items-center"
-    >
-      <Tooltip distance={16}>
-        <button
-          class="m-0 p-1 transition-color"
-          class:bg-transparent={whichReferenceValue !== "filtered"}
-          class:bg-gray-200={whichReferenceValue === "filtered"}
-          class:font-bold={whichReferenceValue === "filtered"}
-          class:text-gray-400={whichReferenceValue !== "filtered"}
-          on:click={() => (whichReferenceValue = "filtered")}
-          ><CheckerHalf /></button
-        >
-        <TooltipContent slot="tooltip-content">
-          scale leaderboard bars by currently-filtered total
-        </TooltipContent>
-      </Tooltip>
-      <Tooltip distance={16}>
-        <button
-          class="m-0 p-1 transition-color"
-          class:bg-transparent={whichReferenceValue !== "global"}
-          class:bg-gray-200={whichReferenceValue === "global"}
-          class:font-bold={whichReferenceValue === "global"}
-          class:text-gray-400={whichReferenceValue !== "global"}
-          on:click={() => (whichReferenceValue = "global")}
-          ><CheckerFull /></button
-        >
-        <TooltipContent slot="tooltip-content">
-          scale leaderboard bars by total record count
-        </TooltipContent>
-      </Tooltip>
-    </div>
     <div class="pt-3">
       {#if anythingSelected}
         <button
@@ -88,5 +58,6 @@
         </button>
       {/if}
     </div>
+    <!-- NOTE: place share buttons here -->
   </div>
 </header>
