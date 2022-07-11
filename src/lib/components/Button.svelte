0a38fae4205ff647881e16ff412a7acfd95a76bc
greenep12 <ericpgreen2@gmail.com>
2022-07-11 14:59:16 -0700

    Add ability to override `Button` classes

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
   export let primary = false;
-
   export let element: HTMLElement = undefined;
+  export let override = "";
 </script>
 
 <button
   bind:this={element}
   class="px-4 py-2 rounded border border-gray-500 flex flex-row gap-x-2 {primary
     ? 'bg-blue-400/50 hover:bg-blue-300 hover:border-blue-300'
-    : 'hover:bg-gray-200 hover:border-gray-200 transition-tranform duration-100'}"
+    : 'hover:bg-gray-200 hover:border-gray-200 transition-tranform duration-100'} {override}"
   on:click
 >
   <slot />
