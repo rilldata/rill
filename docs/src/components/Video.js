6f4f8aecf4cedf4b925c034d4afab3e3d0e834c1
Mike Godwin <1851282+godzig@users.noreply.github.com>
2022-07-26 12:02:52 -0700

    autoplay videos in docs

--- docs/src/components/Video.js
+++ docs/src/components/Video.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function Video({vimeoId}) {
-  const url = 'https://player.vimeo.com/video/' + vimeoId + '?autoplay=1&loop=1&title=0&byline=0&portrait=0';
+  const url = 'https://player.vimeo.com/video/' + vimeoId + '?muted=1&autopause=0&autoplay=1&loop=1&title=0&byline=0&portrait=0&autopause=0';
   return (
     <div style={{
       padding: '56.23% 0 0 0', 
