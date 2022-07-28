9d6443984c58c0c8e852f5a27739c26cc63f8560
Hamilton Ulmer <hamilton.ulmer@gmail.com>
2022-07-28 08:58:56 -0700

    Integrate info icon into Callout (#674)
    * adds in InfoCircle icon into callout elemnt
    
    * updates icon size slightly
    
    Co-authored-by: Marissa Gorlick <marissagorlick@MacBook-Pro-2.lan>

--- src/lib/components/callout/Callout.svelte
+++ src/lib/components/callout/Callout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  import Alert from "../icons/Alert.svelte";
   import AlertCircle from "../icons/AlertCircle.svelte";
   import AlertTriangle from "../icons/AlertTriangle.svelte";
+  import InfoCircle from "../icons/InfoCircle.svelte";
 
   export let level: "info" | "warning" | "error" = "info";
 
@@ -17,10 +17,10 @@
   class="grid {sharedClasses} {classes[level]}"
   style:grid-template-columns="max-content auto"
 >
-  <div>
+  <div style:font-size=".875rem">
     <slot name="icon">
       {#if level === "info"}
-        <Alert />
+        <InfoCircle />
       {:else if level === "warning"}
         <AlertTriangle />
       {:else if level === "error"}
