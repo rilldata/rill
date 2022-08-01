30a06776d0ff13575c982553cc7965ec85f3a535
Dhiraj Kumar <sumankumaribarnwal@gmail.com>
2022-08-01 15:12:20 +0530

    show leaderboard on narrow screen

--- src/routes/_surfaces/workspace/explore/ExploreContainer.svelte
+++ src/routes/_surfaces/workspace/explore/ExploreContainer.svelte
@@ -15,7 +15,8 @@
   section {
     --header: 120px;
     grid-template-rows: var(--header) 1fr;
-    grid-template-columns: 540px auto;
+    grid-template-columns: 540px minmax(350px, auto);
+    overflow-x: scroll;
     grid-row-gap: 1rem;
     grid-template-areas:
       "header header"
