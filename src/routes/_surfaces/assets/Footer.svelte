48de380802e1379acc9e258267cbf62fffdf29c7
Hamilton Ulmer <hamilton.ulmer@gmail.com>
2022-08-03 12:57:51 -0700

    manually resize svg icons for discord and github

--- src/routes/_surfaces/assets/Footer.svelte
+++ src/routes/_surfaces/assets/Footer.svelte
@@ -17,33 +17,43 @@
       label: "Documentation",
       href: "https://docs.rilldata.com",
       className: "fill-gray-600",
+      shrinkIcon: false,
     },
     {
       icon: Discord,
       label: "Ask a question",
       href: "http://bit.ly/3jg4IsF",
       className: "fill-gray-500",
+      shrinkIcon: true,
     },
     {
       icon: Github,
       label: "Feature request",
       href: "https://github.com/rilldata/rill-developer/issues/new?assignees=&labels=request&template=feature_request.md&title=",
       className: "fill-gray-500",
+      shrinkIcon: true,
     },
   ];
 </script>
 
-<div class="flex flex-col bg-gray-50 pt-3 pb-3 gap-y-1 border-t">
+<div
+  class="flex flex-col bg-gray-50 pt-3 pb-3 gap-y-1 border-t sticky bottom-0"
+>
   {#each lineItems as lineItem}
     <a href={lineItem.href} target="_blank"
       ><div
         class="flex flex-row items-center px-4 py-1 gap-x-2 text-gray-700 font-semibold hover:bg-gray-200"
       >
-        <div>
+        <!-- workaround to resize the github and discord icons to match -->
+        <div
+          class="grid place-content-center"
+          style:width="16px"
+          style:height="16px"
+        >
           <svelte:component
             this={lineItem.icon}
             className={lineItem.className}
-            size="16px"
+            size={lineItem.shrinkIcon ? "14px" : "16px"}
           />
         </div>
         {lineItem.label}
@@ -51,7 +61,7 @@
     >
   {/each}
   <div
-    class="italic px-4 py-1 text-gray-600 flex flex-row items-center gap-x-2"
+    class="italic px-4 py-1 text-gray-600 flex flex-row  gap-x-2"
     style:font-size="10px"
   >
     <span class="text-gray-400">
