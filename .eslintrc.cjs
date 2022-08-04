275663ba61eb2b2d2a54b24fc830dddca655b624
Hamilton Ulmer <hamilton.ulmer@gmail.com>
2022-08-03 22:19:23 -0700

    fixes prettier errors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,7 +29,7 @@ module.exports = {
     "@typescript-eslint/no-empty-interface": "warn",
   },
   globals: {
-    "RILL_VERSION": true,
-    "RILL_COMMIT": true
-  }
+    RILL_VERSION: true,
+    RILL_COMMIT: true,
+  },
 };
--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,15 @@
 import adapter from "@sveltejs/adapter-static";
 import { execSync } from "child_process";
-import { readFileSync } from 'fs';
+import { readFileSync } from "fs";
 import { resolve } from "path";
 import preprocess from "svelte-preprocess";
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
 
-const commitHash = execSync('git rev-parse --short HEAD')
-  .toString().trim()
- 
-const file = fileURLToPath(new URL('package.json', import.meta.url));
-const json = readFileSync(file, 'utf8');
+const commitHash = execSync("git rev-parse --short HEAD").toString().trim();
+
+const file = fileURLToPath(new URL("package.json", import.meta.url));
+const json = readFileSync(file, "utf8");
 const pkg = JSON.parse(json);
-
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -34,8 +32,8 @@ const config = {
       },
       define: {
         RILL_VERSION: `"${pkg.version}"`,
-        RILL_COMMIT: `"${commitHash}"`
-      }
+        RILL_COMMIT: `"${commitHash}"`,
+      },
     },
   },
 };
