cba1ad6e4f5bf2b7318205ef8502ef0f4ee361e0
Dhiraj Kumar <sumankumaribarnwal@gmail.com>
2022-08-08 14:27:45 +0530

    Add priority levels

--- src/common/priority-action-queue/DatabaseActionQueuePriority.ts
+++ src/common/priority-action-queue/DatabaseActionQueuePriority.ts
@@ -6,3 +6,28 @@ export enum DatabaseActionQueuePriority {
   ModelExport,
   InactiveModelProfile,
 }
+
+export enum ProfilesPriority {
+  ActiveModelProfile = 2,
+  TableProfile = 3,
+  InactiveModelProfile = 5,
+}
+
+export enum ProfilesFieldPriority {
+  Focused = 0,
+  NonFocused = 0.1,
+}
+
+export enum ProfilesMetadataPriority {
+  SummaryProfile = 0,
+  EssentialProfileDetails = 0.01,
+  DeeperProfileDetails = 0.02,
+}
+
+export function getProfilePriority(
+  entity: ProfilesPriority,
+  field: ProfilesFieldPriority,
+  metadata: ProfilesMetadataPriority
+) {
+  return entity + field + metadata;
+}
