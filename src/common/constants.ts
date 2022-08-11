b9250bf57ac38e632cc3a0cc3296e172cd6c3844
Hamilton Ulmer <hamilton.ulmer@gmail.com>
2022-08-11 14:37:57 -0700

    fix prettier errors

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,7 +1,7 @@
 export const RUNNING_STATUS = "running";
 export const IDLE_STATUS = "idle";
 
-export const SOURCE_PREVIEW_COUNT = 500;
-export const MODEL_PREVIEW_COUNT = SOURCE_PREVIEW_COUNT;
+export const SOURCE_PREVIEW_COUNT = 150;
+export const MODEL_PREVIEW_COUNT = 25;
 
 export const DATABASE_POLLING_INTERVAL = 500;
