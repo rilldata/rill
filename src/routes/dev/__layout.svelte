df014514be4bb8366139a435c3a7bb471d70d368
Aditya Hegde <adityahegderocks@gmail.com>
2022-08-16 15:42:03 +0530

    Removing dev routes

--- src/routes/dev/__layout.svelte
+++ src/routes/dev/__layout.svelte
@@ -1,18 +1,3 @@
-<script lang="ts">
-  import { QueryClient, QueryClientProvider } from "@sveltestack/svelte-query";
-
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false,
-        placeholderData: [],
-      },
-    },
-  });
-</script>
-
-<QueryClientProvider client={queryClient}>
-  <div class="bg-white p-8" style:min-width="100vw" style:min-height="100vh">
-    <slot />
-  </div>
-</QueryClientProvider>
+<div class="bg-white p-8" style:min-width="100vw" style:min-height="100vh">
+  <slot />
+</div>
