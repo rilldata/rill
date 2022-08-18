c4a3cb4008a334fd05851f98f48f1ca30f750ec8
greenep12 <ericpgreen2@gmail.com>
2022-08-17 18:22:58 -0700

    Use vanilla fetch

--- src/lib/svelte-query/queries/metric-view.ts
+++ src/lib/svelte-query/queries/metric-view.ts
@@ -1,12 +1,24 @@
 import type { RuntimeMetricsMetaResponse } from "$common/rill-developer-service/MetricViewActions";
-import { fetchWrapper } from "$lib/util/fetchWrapper";
+import { config } from "$lib/application-state-stores/application-store";
 
 // GET /api/v1/metric-views/{view-name}/meta
 
-export const getMetricViewMetadata = (
+export const getMetricViewMetadata = async (
   metricViewId: string
 ): Promise<RuntimeMetricsMetaResponse> => {
-  return fetchWrapper(`v1/metric-views/${metricViewId}/meta`, "GET");
+  const resp = await fetch(
+    `${config.server.serverUrl}/api/v1/metric-views/${metricViewId}/meta`,
+    {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+  const json = await resp.json();
+  if (!resp.ok) {
+    const err = new Error(json);
+    return Promise.reject(err);
+  }
+  return json;
 };
 
 export const getMetricViewMetaQueryKey = (metricViewId: string) => {
