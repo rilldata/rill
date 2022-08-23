116c7bb5b3f25ec03e6e8ca1bf06470dfaed7e3f
greenep12 <ericpgreen2@gmail.com>
2022-08-23 11:14:11 -0700

    Merge `main` into `svelte-query`

--- src/routes/_surfaces/workspace/explore/ExploreHeader.svelte
+++ src/routes/_surfaces/workspace/explore/ExploreHeader.svelte
@@ -1,29 +1,20 @@
 <script lang="ts">
   import { EntityType } from "$common/data-modeler-state-service/entity-state-service/EntityStateService";
+  import type { MetricViewMetaResponse } from "$common/rill-developer-service/MetricViewActions";
   import { dataModelerService } from "$lib/application-state-stores/application-store";
+  import { metricsExplorerStore } from "$lib/application-state-stores/explorer-stores";
   import Button from "$lib/components/Button.svelte";
-  import Close from "$lib/components/icons/Close.svelte";
   import MetricsIcon from "$lib/components/icons/Metrics.svelte";
-  import type { MetricsExplorerEntity } from "$lib/redux-store/explore/explore-slice";
   import { getMetricsDefReadableById } from "$lib/redux-store/metrics-definition/metrics-definition-readables";
-  import { isFiltered } from "$lib/util/isFiltered";
-  import { fly } from "svelte/transition";
-  import TimeControls from "./time-controls/TimeControls.svelte";
   import {
     getMetricViewMetadata,
     getMetricViewMetaQueryKey,
   } from "$lib/svelte-query/queries/metric-view";
-  import { useQuery, useQueryClient } from "@sveltestack/svelte-query";
-  import { metricsExplorerStore } from "$lib/application-state-stores/explorer-stores";
-  import { MetricViewMetaResponse } from "$common/rill-developer-service/MetricViewActions";
-  import { invalidateMetricViewData } from "$lib/svelte-query/queries/metric-view";
+  import { useQuery } from "@sveltestack/svelte-query";
+  import Filters from "./filters/Filters.svelte";
+  import TimeControls from "./time-controls/TimeControls.svelte";
 
   export let metricsDefId: string;
-
-  const queryClient = useQueryClient();
-
-  let metricsExplorer: MetricsExplorerEntity;
-  $: metricsExplorer = $metricsExplorerStore.entities[metricsDefId];
 
   let queryKey = getMetricViewMetaQueryKey(metricsDefId);
   const queryResult = useQuery<MetricViewMetaResponse, Error>(queryKey, () =>
@@ -33,22 +24,16 @@
     queryKey = getMetricViewMetaQueryKey(metricsDefId);
     queryResult.setOptions(queryKey, () => getMetricViewMetadata(metricsDefId));
   }
+  // TODO: move this "sync" to a more relevant component
   $: metricsExplorerStore.sync(metricsDefId, $queryResult.data);
 
-  $: filtered = isFiltered(metricsExplorer?.filters);
-  function clearAllFilters() {
-    metricsExplorerStore.clearFilters(metricsDefId);
-    invalidateMetricViewData(queryClient, metricsDefId);
-  }
   $: metricsDefinition = getMetricsDefReadableById(metricsDefId);
 </script>
 
-<header
-  class="grid w-full bg-white self-stretch justify-between px-4 pt-3"
-  style:grid-template-columns="auto auto"
-  style:grid-template-rows="auto max-content"
->
-  <div class="grid gap-y-2 grid-flow-row">
+<section id="header" class="w-full flex flex-col gap-y-3">
+  <!-- top row -->
+  <div class="flex justify-between w-full pt-3 pl-4 pr-4">
+    <!-- title element -->
     <h1 style:line-height="1.1" class="pt-3">
       <div class="pl-4 text-gray-600" style:font-size="24px">
         {#if $metricsDefinition}
@@ -56,52 +41,26 @@
         {/if}
       </div>
     </h1>
-
-    <TimeControls {metricsDefId} />
-  </div>
-  <div
-    class="
-    justify-items-end
-    grid
-    grid-flow-row
-    h-max
-  "
-  >
-    <Button
-      type="secondary"
-      on:click={() => {
-        dataModelerService.dispatch("setActiveAsset", [
-          EntityType.MetricsDefinition,
-          metricsDefId,
-        ]);
-      }}
-    >
-      <div class="flex items-center gap-x-2">
-        Edit Metrics <MetricsIcon />
-      </div>
-    </Button>
-
-    <div class="justify-self-end self-start h-max">
-      <div class="pt-3">
-        {#if filtered}
-          <button
-            transition:fly={{ duration: 200, y: 5 }}
-            on:click={clearAllFilters}
-            class="
-                  grid gap-x-2 items-center font-bold
-                  bg-red-100
-                  text-red-900
-                  p-1
-                  pl-2 pr-2
-                  rounded
-              "
-            style:grid-template-columns="auto max-content"
-          >
-            clear all filters <Close />
-          </button>
-        {/if}
-      </div>
-      <!-- NOTE: place share buttons here -->
+    <!-- top right CTAs -->
+    <div>
+      <Button
+        type="secondary"
+        on:click={() => {
+          dataModelerService.dispatch("setActiveAsset", [
+            EntityType.MetricsDefinition,
+            metricsDefId,
+          ]);
+        }}
+      >
+        <div class="flex items-center gap-x-2">
+          Edit Metrics <MetricsIcon />
+        </div>
+      </Button>
     </div>
   </div>
-</header>
+  <!-- bottom row -->
+  <div class="px-4">
+    <TimeControls {metricsDefId} />
+    <Filters {metricsDefId} />
+  </div>
+</section>
---
+++ src/routes/_surfaces/workspace/explore/filters/Filters.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import { metricsExplorerStore } from "$lib/application-state-stores/explorer-stores";
+  import Close from "$lib/components/icons/Close.svelte";
+  import type { MetricsExplorerEntity } from "$lib/redux-store/explore/explore-slice";
+  import { invalidateMetricViewData } from "$lib/svelte-query/queries/metric-view";
+  import { isFiltered } from "$lib/util/isFiltered";
+  import { useQueryClient } from "@sveltestack/svelte-query";
+  import { fly } from "svelte/transition";
+  export let metricsDefId;
+
+  const queryClient = useQueryClient();
+
+  let metricsExplorer: MetricsExplorerEntity;
+  $: metricsExplorer = $metricsExplorerStore.entities[metricsDefId];
+
+  function clearAllFilters() {
+    metricsExplorerStore.clearFilters(metricsDefId);
+    invalidateMetricViewData(queryClient, metricsDefId);
+  }
+
+  $: hasFilters = isFiltered(metricsExplorer?.filters);
+</script>
+
+<div class="pt-3 pb-3" style:min-height="50px">
+  {#if hasFilters}
+    <button
+      transition:fly|local={{ duration: 200, y: 5 }}
+      on:click={clearAllFilters}
+      class="
+            grid gap-x-2 items-center font-bold
+            bg-red-100
+            text-red-900
+            p-1
+            pl-2 pr-2
+            rounded
+        "
+      style:grid-template-columns="auto max-content"
+    >
+      clear all filters <Close />
+    </button>
+  {/if}
+</div>
