81f451d14d1e681f336c725de0a5b9436e13cb37
greenep12 <ericpgreen2@gmail.com>
2022-08-29 21:54:02 -0400

    Fix key for self-managed query cache

--- src/lib/svelte-query/queries/QueriesRepository.ts
+++ src/lib/svelte-query/queries/QueriesRepository.ts
@@ -1,8 +1,8 @@
 import type {
-  QueryKey,
-  UseQueryStoreResult,
-  UseQueryOptions,
   QueryFunction,
+  QueryKey,
+  UseQueryOptions,
+  UseQueryStoreResult,
 } from "@sveltestack/svelte-query";
 import { useQuery } from "@sveltestack/svelte-query";
 
@@ -14,7 +14,7 @@ export class QueriesRepository {
     queryFn: QueryFunction<T>,
     queryOptions: UseQueryOptions<T, E>
   ): UseQueryStoreResult {
-    const key = this.convertQueryKeyToString(queryKey);
+    const key = convertQueryKeyToString(queryKey);
     let query: UseQueryStoreResult;
     if (!this.queriesMap.has(key)) {
       query = useQuery(queryKey, queryFn, queryOptions);
@@ -25,10 +25,51 @@ export class QueriesRepository {
     }
     return query;
   }
+}
 
-  private convertQueryKeyToString(queryKey: QueryKey): string {
-    return (queryKey as Array<string>).join("__");
+// Copied from: https://github.com/SvelteStack/svelte-query/blob/main/src/queryCore/core/utils.ts
+function convertQueryKeyToString(value: any): string {
+  return JSON.stringify(value, (_, val) =>
+    isPlainObject(val)
+      ? Object.keys(val)
+          .sort()
+          .reduce((result, key) => {
+            result[key] = val[key];
+            return result;
+          }, {} as any)
+      : val
+  );
+}
+
+// Copied from: https://github.com/jonschlinkert/is-plain-object
+function isPlainObject(o: any): o is Object {
+  if (!hasObjectPrototype(o)) {
+    return false;
   }
+
+  // If has modified constructor
+  const ctor = o.constructor;
+  if (typeof ctor === "undefined") {
+    return true;
+  }
+
+  // If has modified prototype
+  const prot = ctor.prototype;
+  if (!hasObjectPrototype(prot)) {
+    return false;
+  }
+
+  // If constructor does not have an Object-specific method
+  // if (!prot.hasOwnProperty('isPrototypeOf')) {
+  //   return false
+  // }
+
+  // Most likely a plain Object
+  return true;
+}
+
+function hasObjectPrototype(o: any): boolean {
+  return Object.prototype.toString.call(o) === "[object Object]";
 }
 
 export const queriesRepository = new QueriesRepository();
