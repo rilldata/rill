fc723026d84b6751282e86346ceec62a640708f7
Eric P Green <ericpgreen2@gmail.com>
2022-09-21 08:35:11 -0700

    Add a `PersistedNotfication`

---
+++ src/lib/components/notifications/PersistedNotification.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  import { scale } from "svelte/transition";
+  import { IconButton } from "../button";
+  import Close from "../icons/Close.svelte";
+
+  const dispatch = createEventDispatcher();
+</script>
+
+<div
+  transition:scale={{ duration: 200, start: 0.98, opacity: 0 }}
+  class="fixed bottom-10 inset-x-0 mx-auto w-1/2 py-5 text-sm whitespace-normal
+         text-slate-100 bg-slate-800 shadow-lg rounded-md flex flex-col gap-5"
+>
+  <div class="flex flex-row items-center justify-between pl-6 pr-4">
+    <h1><slot name="title" /></h1>
+    <div>
+      <IconButton on:click={() => dispatch("clear")} bgDark>
+        <Close size="16px" color="#fff" />
+      </IconButton>
+    </div>
+  </div>
+  <hr class="border-slate-200" />
+  <div class="font-normal px-7 py-3">
+    <slot name="body" />
+  </div>
+</div>
