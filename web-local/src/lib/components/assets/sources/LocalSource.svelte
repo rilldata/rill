96d18bd0fa7d156e483cbeb28d33e8137b246f60
Hamilton Ulmer <hamilton.ulmer@gmail.com>
2022-10-06 06:56:09 -0700

    adds a placeholder for LocalSource for now, without drag and drop.

--- web-local/src/lib/components/assets/sources/LocalSource.svelte
+++ web-local/src/lib/components/assets/sources/LocalSource.svelte
@@ -1,1 +1,12 @@
-<div>TODO...</div>
+<script>
+  import { Button } from "@rilldata/web-local/lib/components/button";
+</script>
+
+<div class="grid place-items-center h-full">
+  <Button
+    on:click={() => {
+      console.log("TODO: add standard upload file flow");
+    }}
+    type="primary">Upload a CSV or Parquet file</Button
+  >
+</div>
