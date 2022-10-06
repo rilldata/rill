746483acd74ce92e7ddd81fa933060b86ad14757
Eric P Green <ericpgreen2@gmail.com>
2022-10-05 23:56:19 -0700

    Remove extra component layer

--- web-local/src/lib/components/assets/sources/RemoteSource.svelte
+++ web-local/src/lib/components/assets/sources/RemoteSource.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { createForm } from "svelte-forms-lib";
   import {
     GCS,
     GCSYupSchema,
@@ -7,9 +8,9 @@
     S3,
     S3YupSchema,
   } from "../../../connectors/schemas";
+  import Input from "../../Input.svelte";
   import Tab from "../../tab/Tab.svelte";
   import TabGroup from "../../tab/TabGroup.svelte";
-  import RemoteSourceForm from "./RemoteSourceForm.svelte";
 
   let sourceName = "my_new_source";
 
@@ -17,7 +18,7 @@
   let connectorSpec = S3;
   let yupSchema = S3YupSchema;
 
-  function onChange(selectedConnector: string) {
+  function onConnectorChange(selectedConnector: string) {
     if (selectedConnector === "S3") {
       connectorSpec = S3;
       yupSchema = S3YupSchema;
@@ -30,7 +31,31 @@
     }
   }
 
-  $: onChange(selectedConnector);
+  $: onConnectorChange(selectedConnector);
+
+  function compileCreateSourceSql(values) {
+    const compiledKeyValues = Object.entries(values)
+      .map(([key, value]) => `${key}='${value}'`)
+      .join(", ");
+
+    return (
+      `CREATE SOURCE ${sourceName} WITH (connector = '${connectorSpec.name}', ` +
+      compiledKeyValues +
+      `)`
+    );
+  }
+
+  const { form, errors, handleSubmit } = createForm({
+    // TODO: initialValues should come from SQL asset and be reactive to asset modifications
+    initialValues: {},
+    validationSchema: yupSchema,
+    onSubmit: (values) => {
+      const sql = compileCreateSourceSql(values);
+      // TODO: dispatch sql to SQL editor
+      // TODO: submit sql to backend
+      alert(sql);
+    },
+  });
 </script>
 
 <TabGroup
@@ -46,6 +71,37 @@
 
 <div class="pt-8">
   {#key selectedConnector}
-    <RemoteSourceForm {sourceName} {connectorSpec} {yupSchema} />
+    <h1>{connectorSpec.title}</h1>
+    <div>{@html connectorSpec.description}</div>
+    <form on:submit={handleSubmit} id="{selectedConnector}-form">
+      <div class="py-4">
+        {#each Object.entries(connectorSpec.fields) as [name, attributes]}
+          {@const label =
+            attributes.label + (attributes.required ? "" : " (optional)")}
+          <div class="py-2">
+            {#if attributes.type === "text"}
+              <Input
+                id={name}
+                {label}
+                error={$errors[name]}
+                bind:value={$form[name]}
+                placeholder={attributes.placeholder}
+              />
+            {/if}
+            {#if attributes.type === "checkbox"}
+              <label for={name} class="flex items-center">
+                <input
+                  id={name}
+                  type="checkbox"
+                  bind:checked={$form[name]}
+                  class="h-5 w-5"
+                />
+                <span class="ml-2 text-sm">{label}</span>
+              </label>
+            {/if}
+          </div>
+        {/each}
+      </div>
+    </form>
   {/key}
 </div>
