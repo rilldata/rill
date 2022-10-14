bcca59d5f8d3accbc339bb8a3e09d6edb721619d
Aditya Hegde <adityahegderocks@gmail.com>
2022-10-14 11:02:56 +0530

    Fixing a couple of issues with connectors

--- web-local/src/server/adhoc-runtime.ts
+++ web-local/src/server/adhoc-runtime.ts
@@ -3,135 +3,36 @@ import { DatabaseConfig } from "@rilldata/web-local/common/config/DatabaseConfig
 import { RootConfig } from "@rilldata/web-local/common/config/RootConfig";
 import { DuckDBClient } from "@rilldata/web-local/common/database-service/DuckDBClient";
 
-// TODO: figure out adhoc runtime and add tests in go
+/**
+ * Use this file to test individual endpoints in runtime from outside.
+ * Good to test APIs without UI
+ *
+ * Run "npm run dev -w runtime" to start runtime in a new terminal
+ */
 (async () => {
   const duckdb = DuckDBClient.getInstance(
     new RootConfig({
       database: new DatabaseConfig({
         spawnRuntime: false,
-        spawnRuntimePort: 8080,
+        spawnRuntimePort: 8081,
       }),
     })
   );
   await duckdb.init();
   await duckdb.execute("SET enable_profiling=QUERY_TREE");
+
   console.log(
-    await duckdb.execute(
-      "CREATE OR REPLACE TABLE AdBids AS (SELECT * FROM 'web-local/test/data/AdBids.csv')"
-    )
-  );
-  console.log(
-    await duckdb.requestToInstance("metrics-views/AdBids/timeseries", {
-      metricsViewName: "AdBids",
-      measureNames: ["count(*) as count"],
-      timeStart: new Date("2022-01-01").getTime(),
-      timeEnd: new Date("2022-03-01").getTime(),
-      timeGranularity: "DAY",
+    await duckdb.requestToInstance("migrate/single", {
+      sql:
+        "create source s3test with connector = 's3', path = 's3://datasets-epg/yammerevents.parquet'," +
+        // WARNING: DO NOT CHECK IN KEY AND SECRET
+        "'aws.region' = 'us-east-1', 'aws.access.key' = '', 'aws.access.secret' = ''  ",
     })
   );
+
   console.log(
-    await duckdb.requestToInstance("metrics-views/AdBids/toplist/publisher", {
-      metricsViewName: "AdBids",
-      measureNames: ["count(*) as count"],
-      timeStart: new Date("2022-01-01").getTime(),
-      timeEnd: new Date("2022-03-01").getTime(),
-      limit: 25,
-      filter: {
-        include: [
-          {
-            name: "domain",
-            in: ["sports.yahoo.com"],
-          },
-        ],
-        exclude: [],
-      },
-    })
-  );
-  console.log(
-    await duckdb.requestToInstance("metrics-views/AdBids/toplist/publisher", {
-      metricsViewName: "AdBids",
-      measureNames: ["count(*) as count"],
-      timeStart: new Date("2022-01-01").getTime(),
-      timeEnd: new Date("2022-03-01").getTime(),
-      limit: 25,
-      filter: {
-        include: [],
-        exclude: [
-          {
-            name: "publisher",
-            in: ["sports.yahoo.com"],
-          },
-        ],
-      },
-    })
-  );
-  console.log(
-    await duckdb.requestToInstance("metrics-views/AdBids/toplist/publisher", {
-      metricsViewName: "AdBids",
-      measureNames: ["count(*) as count"],
-      timeStart: new Date("2022-01-01").getTime(),
-      timeEnd: new Date("2022-03-01").getTime(),
-      limit: 25,
-      filter: {
-        include: [],
-        exclude: [
-          {
-            name: "domain",
-            like: ["%oo%"],
-          },
-        ],
-      },
-    })
-  );
-  console.log(
-    await duckdb.requestToInstance("metrics-views/AdBids/totals", {
-      metricsViewName: "AdBids",
-      measureNames: ["count(*) as count"],
-      timeStart: new Date("2022-01-01").getTime(),
-      timeEnd: new Date("2022-03-01").getTime(),
-      filter: {
-        include: [
-          {
-            name: "domain",
-            in: ["sports.yahoo.com"],
-          },
-        ],
-        exclude: [],
-      },
-    })
-  );
-  console.log(
-    await duckdb.requestToInstance("metrics-views/AdBids/totals", {
-      metricsViewName: "AdBids",
-      measureNames: ["count(*) as count"],
-      timeStart: new Date("2022-01-01").getTime(),
-      timeEnd: new Date("2022-03-01").getTime(),
-      filter: {
-        include: [],
-        exclude: [
-          {
-            name: "publisher",
-            in: ["sports.yahoo.com"],
-          },
-        ],
-      },
-    })
-  );
-  console.log(
-    await duckdb.requestToInstance("metrics-views/AdBids/totals", {
-      metricsViewName: "AdBids",
-      measureNames: ["count(*) as count"],
-      timeStart: new Date("2022-01-01").getTime(),
-      timeEnd: new Date("2022-03-01").getTime(),
-      filter: {
-        include: [],
-        exclude: [
-          {
-            name: "domain",
-            like: ["%oo%"],
-          },
-        ],
-      },
+    await duckdb.requestToInstance("query/direct", {
+      sql: "select * from s3test limit 5",
     })
   );
 })();
