ac53aaf9d7924334a03b42f8f996f65d28fb8c9f
Benjamin Egelund-Müller <b@egelund-muller.com>
2022-10-21 13:36:25 +0200

    Run SQL library isolate in a single thread

--- runtime/sql/sql.go
+++ runtime/sql/sql.go
@@ -1,190 +1,89 @@
 package sql
 
-// #cgo darwin amd64 CFLAGS: -I${SRCDIR}/deps/darwin_amd64
-// #cgo darwin arm64 CFLAGS: -I${SRCDIR}/deps/darwin_arm64
-// #cgo linux amd64 CFLAGS: -I${SRCDIR}/deps/linux_amd64
-// #cgo windows amd64 CFLAGS: -I${SRCDIR}/deps/windows_amd64
-// #include <stdlib.h>
-// #include <librillsql.h>
-// void*(*my_malloc)(size_t) = malloc;
-import "C"
 import (
-	b64 "encoding/base64"
+	"encoding/json"
+	"errors"
 	"fmt"
-	"unsafe"
+	"sync"
 
-	"github.com/rilldata/rill/runtime/pkg/sharedlibrary"
+	"github.com/rilldata/rill/runtime/sql/ast"
 	"github.com/rilldata/rill/runtime/sql/rpc"
-	"google.golang.org/protobuf/proto"
 )
 
-type Isolate struct {
-	isolate *C.graal_isolate_t
-	library sharedlibrary.Library
+// Transpile transpiles a Rill SQL statement to a target dialect
+func Transpile(sql string, dialect rpc.Dialect, schema map[string]any) (string, error) {
+	res, err := getIsolate().Request(&rpc.Request{
+		Request: &rpc.Request_TranspileRequest{
+			TranspileRequest: &rpc.TranspileRequest{
+				Sql:     sql,
+				Dialect: dialect,
+				Schema:  marshalSchema(schema),
+			},
+		},
+	})
+	if err != nil {
+		// not a user error
+		panic(err)
+	}
+
+	if res.Error != nil {
+		return "", errors.New(res.Error.Message)
+	}
+
+	tr := res.GetTranspileResponse()
+	if tr == nil {
+		panic(fmt.Errorf("expected TranspileRequest to return TranspileResponse"))
+	}
+
+	return tr.Sql, nil
 }
 
-func NewIsolate() *Isolate {
-	lib, err := sharedlibrary.OpenEmbed(libraryFS, libraryPath)
+// Parse parses and validates a Rill SQL statement
+func Parse(sql string, schema map[string]any) (*ast.SqlNodeProto, error) {
+	res, err := getIsolate().Request(&rpc.Request{
+		Request: &rpc.Request_ParseRequest{
+			ParseRequest: &rpc.ParseRequest{
+				Sql:    sql,
+				Schema: marshalSchema(schema),
+			},
+		},
+	})
 	if err != nil {
+		// not a user error
 		panic(err)
 	}
 
-	graalCreateIsolate, err := lib.FindFunc("graal_create_isolate")
-	if err != nil {
-		panic(err)
+	if res.Error != nil {
+		return nil, errors.New(res.Error.Message)
 	}
 
-	var isolate *C.graal_isolate_t
-	var thread *C.graal_isolatethread_t
-	params := &C.graal_create_isolate_params_t{
-		reserved_address_space_size: 1024 * 1024 * 500,
+	pr := res.GetParseResponse()
+	if pr == nil {
+		panic(fmt.Errorf("expected ParseRequest to return ParseResponse"))
 	}
 
-	status, _, _ := graalCreateIsolate.Call(uintptr(unsafe.Pointer(params)), uintptr(unsafe.Pointer(&isolate)), uintptr(unsafe.Pointer(&thread)))
-	if status != 0 {
-		panic(fmt.Errorf("failed to create isolate"))
-	}
-
-	return &Isolate{
-		library: lib,
-		isolate: isolate,
-	}
+	return pr.Ast, nil
 }
 
-func (i *Isolate) Close() error {
-	graalDetachAllThreadsAndTearDownIsolate, err := i.library.FindFunc("graal_detach_all_threads_and_tear_down_isolate")
-	if err != nil {
-		panic(err)
-	}
+// See getIsolate
+var isolate *Isolate
+var isolateOnce sync.Once
 
-	thread := i.attachThread()
-
-	status, _, _ := graalDetachAllThreadsAndTearDownIsolate.Call(uintptr(unsafe.Pointer(thread)))
-	if status != 0 {
-		return fmt.Errorf("isolate teardown failed")
-	}
-
-	return i.library.Close()
+// getIsolate returns a lazily-loaded Isolate, which will never be closed.
+// If the performance of using a single thread-bound isolate suffers, we should
+// consider instead using a pool of isolates.
+func getIsolate() *Isolate {
+	isolateOnce.Do(func() {
+		isolate = OpenIsolate()
+	})
+	return isolate
 }
 
-func (i *Isolate) request(request *rpc.Request) *rpc.Response {
-	f, err := i.library.FindFunc("request")
+// marshalSchema serializes a runtime catalog to the schema format expected by the SQL library
+func marshalSchema(schema map[string]any) string {
+	data, err := json.Marshal(schema)
 	if err != nil {
 		panic(err)
 	}
-
-	thread := i.attachThread()
-
-	bytes, _ := proto.Marshal(request)
-	b64request := b64.StdEncoding.EncodeToString(bytes)
-
-	cBytes := C.CString(b64request)
-	defer C.free(unsafe.Pointer(cBytes))
-	res, _, _ := f.Call(
-		uintptr(unsafe.Pointer(thread)),
-		uintptr(unsafe.Pointer(C.my_malloc)),
-		uintptr(unsafe.Pointer(cBytes)),
-	)
-	if res == 0 {
-		panic(fmt.Errorf("call to request failed"))
-	}
-
-	b64response := C.GoString((*C.char)(unsafe.Pointer(res)))
-	C.free(unsafe.Pointer(res))
-
-	var response rpc.Response
-	decodedResponse, _ := b64.StdEncoding.DecodeString(b64response)
-	proto.Unmarshal(decodedResponse, &response)
-
-	return &response
-}
-
-func (i *Isolate) ConvertSQL(sql string, schema string, dialect string) string {
-	convertSql, err := i.library.FindFunc("convert_sql")
-	if err != nil {
-		panic(err)
-	}
-
-	thread := i.attachThread()
-
-	cSql := C.CString(sql)
-	defer C.free(unsafe.Pointer(cSql))
-
-	cSchema := C.CString(schema)
-	defer C.free(unsafe.Pointer(cSchema))
-
-	cDialect := C.CString(dialect)
-	defer C.free(unsafe.Pointer(cDialect))
-
-	res, _, _ := convertSql.Call(
-		uintptr(unsafe.Pointer(thread)),
-		uintptr(unsafe.Pointer(C.my_malloc)),
-		uintptr(unsafe.Pointer(cSql)),
-		uintptr(unsafe.Pointer(cSchema)),
-		uintptr(unsafe.Pointer(cDialect)),
-	)
-	if res == 0 {
-		panic(fmt.Errorf("call to convert_sql failed"))
-	}
-
-	goRes := C.GoString((*C.char)(unsafe.Pointer(res)))
-	C.free(unsafe.Pointer(res))
-
-	return goRes
-}
-
-func (i *Isolate) getAST(sql string, schema string) []byte {
-	getAST, err := i.library.FindFunc("get_ast")
-	if err != nil {
-		panic(err)
-	}
-
-	thread := i.attachThread()
-
-	cSql := C.CString(sql)
-	defer C.free(unsafe.Pointer(cSql))
-
-	cSchema := C.CString(schema)
-	defer C.free(unsafe.Pointer(cSchema))
-
-	res, _, _ := getAST.Call(
-		uintptr(unsafe.Pointer(thread)),
-		uintptr(unsafe.Pointer(C.my_malloc)),
-		uintptr(unsafe.Pointer(cSql)),
-		uintptr(unsafe.Pointer(cSchema)),
-	)
-	if res == 0 {
-		panic(fmt.Errorf("call to get_ast failed"))
-	}
-
-	goResString := C.GoString((*C.char)(unsafe.Pointer(res)))
-	goRes := []byte(goResString)
-	C.free(unsafe.Pointer(res))
-
-	return goRes
-}
-
-func (i *Isolate) attachThread() *C.graal_isolatethread_t {
-	graalGetCurrentThread, err := i.library.FindFunc("graal_get_current_thread")
-	if err != nil {
-		panic(err)
-	}
-
-	threadPtr, _, _ := graalGetCurrentThread.Call(uintptr(unsafe.Pointer(i.isolate)))
-	if threadPtr != 0 {
-		return (*C.graal_isolatethread_t)(unsafe.Pointer(threadPtr))
-	}
-
-	graalAttachThread, err := i.library.FindFunc("graal_attach_thread")
-	if err != nil {
-		panic(err)
-	}
-
-	var thread *C.graal_isolatethread_t
-	status, _, _ := graalAttachThread.Call(uintptr(unsafe.Pointer(i.isolate)), uintptr(unsafe.Pointer(&thread)))
-	if status != 0 {
-		panic(fmt.Errorf("failed to attach thread"))
-	}
-
-	return thread
+	return string(data)
 }
--- runtime/sql/sql_test.go
+++ runtime/sql/sql_test.go
@@ -1,59 +1,48 @@
 package sql
 
 import (
+	"fmt"
 	"testing"
 
-	"github.com/rilldata/rill/runtime/sql/rpc"
 	"github.com/stretchr/testify/require"
 )
 
-func TestSanity(t *testing.T) {
-	isolate := NewIsolate()
+// func TestTranspileSelect(t *testing.T) {
+// 	sql := fmt.Sprintf(`select %d as foo, 'hello' as bar, h1.id, h1."power", h2.name from heroes h1 join heroes h2 on h1.id = h2.id`, 10)
+// 	schema := map[string]any{
+// 		"tables": []map[string]any{
+// 			{
+// 				"name": "heroes",
+// 				"columns": []map[string]any{
+// 					{"name": "id", "type": "varchar"},
+// 					{"name": "power", "type": "varchar"},
+// 					{"name": "name", "type": "varchar"},
+// 				},
+// 			},
+// 		},
+// 	}
 
-	sql := "select 1 as foo, 'hello' as bar"
-	schema := `{ "tables": [] }`
-	dialect := "duckdb"
+// 	res, err := Transpile(sql, rpc.Dialect_DUCKDB, schema)
+// 	require.NoError(t, err)
+// 	require.Equal(t, res, res)
+// }
 
-	res := isolate.ConvertSQL(sql, schema, dialect)
-	require.Equal(t, `SELECT 1 AS "FOO", 'hello' AS "BAR"`, res)
-
-	err := isolate.Close()
-	require.NoError(t, err)
-}
-
-func TestTranspile(t *testing.T) {
-	isolate := NewIsolate()
-
-	sql := "select 1 as foo, 'hello' as bar"
-
-	r := rpc.Request{
-		Request: &rpc.Request_TranspileRequest{
-			TranspileRequest: &rpc.TranspileRequest{
-				Sql:     sql,
-				Dialect: rpc.Dialect_DUCKDB,
-				Schema:  `{ "tables": [] }`,
+func TestParseSelect(t *testing.T) {
+	sql := fmt.Sprintf(`select 10 as foo, 'hello' as bar, h1.id, h1."power", h2.name from heroes h1 join heroes h2 on h1.id = h2.id`)
+	schema := map[string]any{
+		"tables": []map[string]any{
+			{
+				"name": "heroes",
+				"columns": []map[string]any{
+					{"name": "id", "type": "varchar"},
+					{"name": "power", "type": "varchar"},
+					{"name": "name", "type": "varchar"},
+				},
 			},
 		},
 	}
 
-	res := isolate.request(&r)
-
-	require.Equal(t, `SELECT 1 AS "FOO", 'hello' AS "BAR"`, (*res).GetTranspileResponse().Sql)
-
-	err := isolate.Close()
+	res, err := Parse(sql, schema)
 	require.NoError(t, err)
-}
-
-func TestSanityGetAST(t *testing.T) {
-	isolate := NewIsolate()
-
-	sql := "select 1 as foo, 'hello' as bar"
-	schema := `{ "tables": [] }`
-
-	res := isolate.getAST(sql, schema)
-	println(res)
-	println(len(res))
-
-	err := isolate.Close()
-	require.NoError(t, err)
+	require.Equal(t, 5, len(res.GetSqlSelectProto().GetSelectList().List))
 }
