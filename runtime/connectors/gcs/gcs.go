3b969d73d4cadddc413202110c2f61372efbcf04
Eric P Green <ericpgreen2@gmail.com>
2022-10-21 09:12:45 -0700

    Clarify current lack of gzip & glob pattern support

--- runtime/connectors/gcs/gcs.go
+++ runtime/connectors/gcs/gcs.go
@@ -28,7 +28,7 @@ var spec = connectors.Spec{
 			Placeholder: "gs://bucket-name/path/to/file.csv",
 			Type:        connectors.StringPropertyType,
 			Required:    true,
-			Hint:        "Tip: use glob patterns to select multiple files",
+			Hint:        "Note that gzipped files & glob patterns aren't yet supported. They're coming in the next release!",
 		},
 		// {
 		// 	Key:         "gcp.region",
