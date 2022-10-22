6ea1303701b425e6cc8b0dcd7e9c67d3d2bfaef7
Benjamin Egelund-Müller <b@egelund-muller.com>
2022-10-22 11:10:54 +0200

    Test metrics views in runtime

--- runtime/sql/sql_test.go
+++ runtime/sql/sql_test.go
@@ -41,30 +41,68 @@ func TestTranspileSelect(t *testing.T) {
 }
 
 func TestParseSelect(t *testing.T) {
-	// sql := `select 10 as foo, 'hello' as bar, h1.id, h1."power", h2.name from heroes h1 join heroes h2 on h1.id = h2.id`
-	// catalog := map[string]any{
-	// 	"artifacts": []any{},
-	// 	"schemas": []map[string]any{
-	// 		{
-	// 			"name": "main",
-	// 			"tables": []map[string]any{
-	// 				{
-	// 					"name": "heroes",
-	// 					"columns": []map[string]any{
-	// 						{"name": "id", "type": "varchar"},
-	// 						{"name": "power", "type": "varchar"},
-	// 						{"name": "name", "type": "varchar"},
-	// 					},
-	// 				},
-	// 			},
-	// 		},
-	// 	},
-	// }
-
-	sql := "select 10"
-	catalog := map[string]any{}
+	sql := `select 10 as foo, 'hello' as bar, h1.id, h1."power", h2.name from main.heroes h1 join main.heroes h2 on h1.id = h2.id`
+	catalog := map[string]any{
+		"artifacts": []any{},
+		"schemas": []map[string]any{
+			{
+				"name": "main",
+				"tables": []map[string]any{
+					{
+						"name": "heroes",
+						"columns": []map[string]any{
+							{"name": "id", "type": "varchar"},
+							{"name": "power", "type": "varchar"},
+							{"name": "name", "type": "varchar"},
+						},
+					},
+				},
+			},
+		},
+	}
 
 	res, err := Parse(sql, catalog)
 	require.NoError(t, err)
-	require.Equal(t, 1, len(res.GetSqlSelectProto().GetSelectList().List))
+	require.Equal(t, 5, len(res.GetSqlSelectProto().GetSelectList().List))
+}
+
+func TestParseMetricsView(t *testing.T) {
+	sql := `
+		CREATE METRICS VIEW FOO_BAR
+		DIMENSIONS
+			ID,
+			BIRTHDAY,
+			"POWER",
+			NAME
+		MEASURES
+			COUNT(*) AS "COUNT",
+			COUNT(DISTINCT "POWER") AS POWERS,
+			COUNT(DISTINCT NAME) AS NAMES,
+			SUM(LEVEL) AS LEVES
+		FROM MAIN.HEROES
+	`
+	catalog := map[string]any{
+		"artifacts": []any{},
+		"schemas": []map[string]any{
+			{
+				"name": "main",
+				"tables": []map[string]any{
+					{
+						"name": "heroes",
+						"columns": []map[string]any{
+							{"name": "id", "type": "varchar"},
+							{"name": "birthday", "type": "timestamp"},
+							{"name": "power", "type": "varchar"},
+							{"name": "name", "type": "varchar"},
+							{"name": "level", "type": "integer"},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	res, err := Parse(sql, catalog)
+	require.NoError(t, err)
+	require.Equal(t, "SUM", res.GetSqlCreateMetricsViewProto().Measures.List[3].GetSqlBasicCallProto().OperandList[0].GetSqlBasicCallProto().Operator.Name)
 }
