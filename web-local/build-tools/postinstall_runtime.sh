a8b5231b92c36814799552f0605491d474e99778
Aditya Hegde <adityahegderocks@gmail.com>
2022-10-26 06:33:23 +0100

    Adding support for default aws region (#1092)
    * Adding support for default aws region
    
    * Updating runtime

--- web-local/build-tools/postinstall_runtime.sh
+++ web-local/build-tools/postinstall_runtime.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Hardcoded runtime version to install
-RUNTIME_VERSION="59fdab480557396d6e4be0bae2ba2d5b2ddbf438"
+RUNTIME_VERSION="6a4411eabff82037a1125d612a9c490f6e13ad1a"
 
 # Targets dist/runtime as the output directory
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
