8b11f5c60aa5c69bee66ca065659f399f183d79b
Benjamin Egelund-Müller <b@egelund-muller.com>
2022-11-09 18:45:01 +0100

    Syntax fixes

--- runtime/drivers/sqlite/migrations/0002.sql
+++ runtime/drivers/sqlite/migrations/0002.sql
@@ -1,12 +1,12 @@
 CREATE TABLE catalog (
     instance_id TEXT,
-	name TEXT,
-	type TEXT NOT NULL,
-	sql TEXT NOT NULL,
+    name TEXT,
+    type TEXT NOT NULL,
+    sql TEXT NOT NULL,
     schema BLOB,
     managed BOOLEAN NOT NULL,
-	created_on TIMESTAMP NOT NULL,
-	updated_on TIMESTAMP NOT NULL,
+    created_on TIMESTAMP NOT NULL,
+    updated_on TIMESTAMP NOT NULL,
     refreshed_on TIMESTAMP NOT NULL,
     PRIMARY KEY (instance_id, name)
 );
