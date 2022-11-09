19434496c766848a9b6fa936910d3cd7066a4026
egor-ryashin <egor.ryashin@rilldata.com>
2022-11-09 21:03:59 +0300

    platform: profiling api

--- runtime/server/table_api_test.go
+++ runtime/server/table_api_test.go
@@ -11,12 +11,11 @@ import (
 	"github.com/rilldata/rill/runtime/drivers"
 )
 
-func getSingleValue(rows *sqlx.Rows) int {
+func getSingleValue(t *testing.T, rows *sqlx.Rows) int {
 	var val int
 	for rows.Next() {
-		if err := rows.Scan(&val); err != nil {
-			panic(err)
-		}
+		err := rows.Scan(&val)
+		require.NoError(t, err)
 	}
 	rows.Close()
 	return val
@@ -24,27 +23,21 @@ func getSingleValue(rows *sqlx.Rows) int {
 
 func TestServer_Database(t *testing.T) {
 	server, instanceId, err := getTestServer()
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	rows := createTestTable(server, instanceId, t)
-	require.Equal(t, 1, getSingleValue(rows))
+	require.Equal(t, 1, getSingleValue(t, rows))
 	rows, err = server.query(context.Background(), instanceId, &drivers.Statement{
 		Query: "select count(*) from test",
 	})
-	if err != nil {
-		t.Fatal(err)
-	}
-	require.Equal(t, 1, getSingleValue(rows))
+	require.NoError(t, err)
+	require.Equal(t, 1, getSingleValue(t, rows))
 }
 
 func createTestTable(server *Server, instanceId string, t *testing.T) *sqlx.Rows {
 	rows, err := server.query(context.Background(), instanceId, &drivers.Statement{
 		Query: "create table test (a int)",
 	})
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	rows.Close()
 	rows, _ = server.query(context.Background(), instanceId, &drivers.Statement{
 		Query: "insert into test values (1)",
@@ -53,59 +46,45 @@ func createTestTable(server *Server, instanceId string, t *testing.T) *sqlx.Rows
 	rows, err = server.query(context.Background(), instanceId, &drivers.Statement{
 		Query: "select count(*) from test",
 	})
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	return rows
 }
 
 func TestServer_TableCardinality(t *testing.T) {
 	server, instanceId, err := getTestServer()
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	rows := createTestTable(server, instanceId, t)
 	rows.Close()
 	cr, err := server.TableCardinality(context.Background(), &api.CardinalityRequest{
 		InstanceId: instanceId,
 		TableName:  "test",
 	})
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	require.Equal(t, int64(1), cr.Cardinality)
 }
 
 func TestServer_ProfileColumns(t *testing.T) {
 	server, instanceId, err := getTestServer()
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	rows := createTestTable(server, instanceId, t)
 	rows.Close()
 	cr, err := server.ProfileColumns(context.Background(), &api.ProfileColumnsRequest{
 		InstanceId: instanceId,
 		TableName:  "test",
 	})
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	require.Equal(t, 0, len(cr.ProfileColumn))
 }
 
 func TestServer_TableRows(t *testing.T) {
 	server, instanceId, err := getTestServer()
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	rows := createTestTable(server, instanceId, t)
 	rows.Close()
 	cr, err := server.TableRows(context.Background(), &api.RowsRequest{
 		InstanceId: instanceId,
 		TableName:  "test",
 	})
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	require.Equal(t, 1, len(cr.Data))
 }
