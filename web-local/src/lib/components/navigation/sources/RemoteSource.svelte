4fee83eeb6a72929f875bb83082313620dabccf4
Eric P Green <ericpgreen2@gmail.com>
2022-11-10 20:57:47 -0800

    Infer source name from path (#1196)
    * Fix import warning
    
    * Refactor connector errors & yup schemas
    
    * Infer source name from path
    
    * Fix build
    
    * Add tests

--- web-local/src/lib/components/navigation/sources/RemoteSource.svelte
+++ web-local/src/lib/components/navigation/sources/RemoteSource.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
   import {
+    ConnectorProperty,
     ConnectorPropertyType,
     getRuntimeServiceListCatalogObjectsQueryKey,
-    RpcStatus,
     RuntimeServiceListCatalogObjectsType,
     useRuntimeServiceMigrateSingle,
     V1Connector,
@@ -16,17 +16,22 @@
   import { runtimeStore } from "../../../application-state-stores/application-store";
   import { overlay } from "../../../application-state-stores/overlay-store";
   import type { PersistentTableStore } from "../../../application-state-stores/table-stores";
-  import {
-    fromYupFriendlyKey,
-    getYupSchema,
-    toYupFriendlyKey,
-  } from "../../../connectors/schemas";
   import { Button } from "../../button";
   import InformationalField from "../../forms/InformationalField.svelte";
   import Input from "../../forms/Input.svelte";
   import SubmissionError from "../../forms/SubmissionError.svelte";
   import DialogFooter from "../../modal/dialog/DialogFooter.svelte";
-  import { compileCreateSourceSql, waitForSource } from "./sourceUtils";
+  import { humanReadableErrorMessage } from "./errors";
+  import {
+    compileCreateSourceSql,
+    inferSourceName,
+    waitForSource,
+  } from "./sourceUtils";
+  import {
+    fromYupFriendlyKey,
+    getYupSchema,
+    toYupFriendlyKey,
+  } from "./yupSchemas";
 
   export let connector: V1Connector;
 
@@ -39,11 +44,14 @@
 
   const dispatch = createEventDispatcher();
 
+  let connectorProperties: ConnectorProperty[];
   let yupSchema: yup.AnyObjectSchema;
 
   // state from svelte-forms-lib
   let form: Writable<any>;
+  let touched: Writable<Record<any, boolean>>;
   let errors: Writable<Record<never, string>>;
+  let handleChange: (event: Event) => any;
   let handleSubmit: (event: Event) => any;
 
   let waitingOnSourceImport = false;
@@ -51,7 +59,7 @@
   function onConnectorChange(connector: V1Connector) {
     yupSchema = getYupSchema(connector);
 
-    ({ form, errors, handleSubmit } = createForm({
+    ({ form, touched, errors, handleChange, handleSubmit } = createForm({
       // TODO: initialValues should come from SQL asset and be reactive to asset modifications
       initialValues: {
         sourceName: "", // avoids `values.sourceName` warning
@@ -97,79 +105,32 @@
         );
       },
     }));
+
+    // Place the "Source name" field directly under the "Path" field, which is the first property for each connector (s3, gcs, https).
+    connectorProperties = [
+      ...connector.properties.slice(0, 1),
+      {
+        key: "sourceName",
+        displayName: "Source name",
+        description: "The name of the source",
+        placeholder: "my_new_source",
+        type: ConnectorPropertyType.TYPE_STRING,
+        nullable: false,
+      },
+      ...connector.properties.slice(1),
+    ];
   }
 
   $: onConnectorChange(connector);
 
-  function humanReadableErrorMessage(connectorName: string, error: RpcStatus) {
-    // TODO: the error response type does not match the type defined in the API
-    switch (error.response.data.code) {
-      // gRPC error codes: https://pkg.go.dev/google.golang.org/grpc@v1.49.0/codes
-      // InvalidArgument
-      case 3: {
-        const serverError = error.response.data.message;
+  function onStringInputChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const { name, value } = target;
 
-        // Rill errors
-        if (
-          serverError.match(/an existing object with name '.*' already exists/)
-        ) {
-          return "A source with this name already exists. Please choose a different name.";
-        }
-
-        // AWS errors (ref: https://docs.aws.amazon.com/AmazonS3/latest/API/ErrorResponses.html)
-        if (connectorName === "s3") {
-          if (serverError.includes("MissingRegion")) {
-            return "Region not detected. Please enter a region.";
-          } else if (serverError.includes("NoCredentialProviders")) {
-            return "No credentials found. Please see the docs for how to configure AWS credentials.";
-          } else if (serverError.includes("InvalidAccessKey")) {
-            return "Invalid AWS access key. Please check your credentials.";
-          } else if (serverError.includes("SignatureDoesNotMatch")) {
-            return "Invalid AWS secret key. Please check your credentials.";
-          } else if (serverError.includes("BucketRegionError")) {
-            return "Bucket is not in the provided region. Please check your region.";
-          } else if (serverError.includes("AccessDenied")) {
-            return "Access denied. Please ensure you have the correct permissions.";
-          } else if (serverError.includes("NoSuchKey")) {
-            return "Invalid path. Please check your path.";
-          } else if (serverError.includes("NoSuchBucket")) {
-            return "Invalid bucket. Please check your bucket name.";
-          } else if (serverError.includes("AuthorizationHeaderMalformed")) {
-            return "Invalid authorization header. Please check your credentials.";
-          }
-        }
-
-        // GCP errors (ref: https://cloud.google.com/storage/docs/json_api/v1/status-codes)
-        if (connectorName === "gcs") {
-          if (serverError.includes("could not find default credentials")) {
-            return "No credentials found. Please see the docs for how to configure GCP credentials.";
-          } else if (serverError.includes("Unauthorized")) {
-            return "Unauthorized. Please check your credentials.";
-          } else if (serverError.includes("AccessDenied")) {
-            return "Access denied. Please ensure you have the correct permissions.";
-          }
-        }
-
-        if (connectorName === "https") {
-          if (serverError.includes("invalid file")) {
-            return "The provided URL does not appear to have a valid dataset. Please check your path and try again.";
-          } else if (serverError.includes("failed to fetch url")) {
-            return "We could not connect to the provided URL. Please check your path and try again.";
-          }
-        }
-
-        // DuckDB errors
-        if (serverError.match(/expected \d* values per row, but got \d*/)) {
-          return "Malformed CSV file: number of columns does not match header.";
-        } else if (
-          serverError.match(/Catalog Error: Table with name .* does not exist/)
-        ) {
-          return "We had trouble ingesting your data. Please see the docs for common issues. If you're still stuck, don't hesitate to reach out on Discord.";
-        }
-        return error.response.data.message;
-      }
-      default:
-        return "An unknown error occurred. If the error persists, please reach out for help on <a href=https://bit.ly/3unvA05 target=_blank>Discord</a>.";
+    if (name === "path") {
+      if ($touched.sourceName) return;
+      const sourceName = inferSourceName(connector, value);
+      $form.sourceName = sourceName ? sourceName : $form.sourceName;
     }
   }
 </script>
@@ -187,29 +148,28 @@
     </div>
     {#if $createSource.isError}
       <SubmissionError
-        message={humanReadableErrorMessage(connector.name, $createSource.error)}
+        message={humanReadableErrorMessage(
+          connector.name,
+          $createSource.error.response.data.code,
+          $createSource.error.response.data.message
+        )}
       />
     {/if}
-    <div class="py-2">
-      <Input
-        label="Source name"
-        bind:value={$form["sourceName"]}
-        error={$errors["sourceName"]}
-        placeholder="my_new_source"
-      />
-    </div>
-    {#each connector.properties as property}
+
+    {#each connectorProperties as property}
       {@const label =
         property.displayName + (property.nullable ? " (optional)" : "")}
       <div class="py-2">
         {#if property.type === ConnectorPropertyType.TYPE_STRING}
           <Input
-            id={property.key}
+            id={toYupFriendlyKey(property.key)}
             {label}
             placeholder={property.placeholder}
             hint={property.hint}
             error={$errors[toYupFriendlyKey(property.key)]}
             bind:value={$form[toYupFriendlyKey(property.key)]}
+            on:input={onStringInputChange}
+            on:change={handleChange}
           />
         {:else if property.type === ConnectorPropertyType.TYPE_BOOLEAN}
           <label for={property.key} class="flex items-center">
