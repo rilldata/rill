65c2981ef317d156d4e940a6b4cbee812c22858d
Parag Jain <pjain1@apache.org>
2022-11-14 16:25:59 +0530

    implementation of col level apis

--- runtime/server/column_api.go
+++ runtime/server/column_api.go
@@ -3,6 +3,7 @@ package server
 import (
 	"context"
 	"fmt"
+	"math"
 
 	"github.com/rilldata/rill/runtime/api"
 	"github.com/rilldata/rill/runtime/drivers"
@@ -202,10 +203,10 @@ func (s *Server) EstimateSmallestTimeGrain(ctx context.Context, request *api.Est
 	rows, err = s.query(ctx, request.InstanceId, &drivers.Statement{
 		Query: estimateSql,
 	})
-	defer rows.Close()
 	if err != nil {
 		return nil, status.Error(codes.Internal, err.Error())
 	}
+	defer rows.Close()
 
 	var timeGrainString string
 	for rows.Next() {
@@ -253,29 +254,240 @@ func (s *Server) EstimateSmallestTimeGrain(ctx context.Context, request *api.Est
 }
 
 func (s *Server) GetNumericHistogram(ctx context.Context, request *api.NumericHistogramRequest) (*api.NumericSummary, error) {
+	sanitizedColumnName := quoteName(request.ColumnName)
+	sql := fmt.Sprintf("SELECT approx_quantile(%s, 0.75)-approx_quantile(%s, 0.25) as IQR, approx_count_distinct(%s) as count, max(%s) - min(%s) as range FROM %s",
+		sanitizedColumnName, sanitizedColumnName, sanitizedColumnName, sanitizedColumnName, sanitizedColumnName, request.TableName)
+	rows, err := s.query(ctx, request.InstanceId, &drivers.Statement{
+		Query: sql,
+	})
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+	defer rows.Close()
+	var iqr, count, rangeVal float64
+	for rows.Next() {
+		err := rows.Scan(&iqr, &count, &rangeVal)
+		if err != nil {
+			return nil, status.Error(codes.Internal, err.Error())
+		}
+	}
+	var bucketSize float64
+	if count < 40 {
+		// Use cardinality if unique count less than 40
+		bucketSize = count
+	} else {
+		// Use Freedman–Diaconis rule for calculating number of bins
+		bucketWidth := (2 * iqr) / math.Cbrt(count)
+		FDEstimatorBucketSize := math.Ceil(rangeVal / bucketWidth)
+		bucketSize = math.Min(40, FDEstimatorBucketSize)
+	}
+	_, ok := TIMESTAMPS[request.ColumnType]
+	var selectColumn string
+	if ok {
+		selectColumn = fmt.Sprintf("epoch(%s)", sanitizedColumnName)
+	} else {
+		selectColumn = fmt.Sprintf("%s::DOUBLE", sanitizedColumnName)
+	}
+
+	histogramSql := fmt.Sprintf(`
+          WITH data_table AS (
+            SELECT %[1]s as %[2]s 
+            FROM %[3]s
+            WHERE %[2]s IS NOT NULL
+          ), S AS (
+            SELECT 
+              min(%[2]s) as minVal,
+              max(%[2]s) as maxVal,
+              (max(%[2]s) - min(%[2]s)) as range
+              FROM data_table
+          ), values AS (
+            SELECT %[2]s as value from data_table
+            WHERE %[2]s IS NOT NULL
+          ), buckets AS (
+            SELECT
+              range as bucket,
+              (range) * (select range FROM S) / %[4]v + (select minVal from S) as low,
+              (range + 1) * (select range FROM S) / %[4]v + (select minVal from S) as high
+            FROM range(0, %[4]v, 1)
+          ),
+          -- bin the values
+          binned_data AS (
+            SELECT 
+              FLOOR((value - (select minVal from S)) / (select range from S) * %[4]v) as bucket
+            from values
+          ),
+          -- join the bucket set with the binned values to generate the histogram
+          histogram_stage AS (
+          SELECT
+              buckets.bucket,
+              low,
+              high,
+              SUM(CASE WHEN binned_data.bucket = buckets.bucket THEN 1 ELSE 0 END) as count
+            FROM buckets
+            LEFT JOIN binned_data ON binned_data.bucket = buckets.bucket
+            GROUP BY buckets.bucket, low, high
+            ORDER BY buckets.bucket
+          ),
+          -- calculate the right edge, sine in histogram_stage we don't look at the values that
+          -- might be the largest.
+          right_edge AS (
+            SELECT count(*) as c from values WHERE value = (select maxVal from S)
+          )
+          SELECT 
+            bucket,
+            low,
+            high,
+            -- fill in the case where we've filtered out the highest value and need to recompute it, otherwise use count.
+            CASE WHEN high = (SELECT max(high) from histogram_stage) THEN count + (select c from right_edge) ELSE count END AS count
+            FROM histogram_stage
+	      `, selectColumn, sanitizedColumnName, request.TableName, bucketSize)
+	histogramRows, err := s.query(ctx, request.InstanceId, &drivers.Statement{
+		Query: histogramSql,
+	})
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+	defer histogramRows.Close()
+	histogramBins := make([]*api.NumericHistogramBins_Bin, 0)
+	for histogramRows.Next() {
+		bin := &api.NumericHistogramBins_Bin{}
+		rows.Scan(&bin.Bucket, &bin.Low, &bin.High, &bin.Count)
+		histogramBins = append(histogramBins, bin)
+	}
 	return &api.NumericSummary{
-		NumericHistogram: &api.NumericHistogram{
-			Buckets: []*api.NumericHistogram_Bucket{},
+		NumericHistogramBins: &api.NumericHistogramBins{
+			Bins: histogramBins,
 		},
 	}, nil
 }
 
 func (s *Server) GetRugHistogram(ctx context.Context, request *api.RugHistogramRequest) (*api.NumericSummary, error) {
+	sanitizedColumnName := quoteName(request.ColumnName)
+	outlierPseudoBucketSize := 500
+
+	_, ok := TIMESTAMPS[request.ColumnType]
+	var selectColumn string
+	if ok {
+		selectColumn = fmt.Sprintf("epoch(%s)", sanitizedColumnName)
+	} else {
+		selectColumn = fmt.Sprintf("%s::DOUBLE", sanitizedColumnName)
+	}
+
+	sql := fmt.Sprintf(`WITH data_table AS (
+            SELECT %[1]s as %[2]s
+            FROM %[3]s
+            WHERE %[2]s IS NOT NULL
+          ), S AS (
+            SELECT
+              min(%[2]s) as minVal,
+              max(%[2]s) as maxVal,
+              (max(%[2]s) - min(%[2]s)) as range
+              FROM data_table
+          ), values AS (
+            SELECT %[2]s as value from data_table
+            WHERE %[2]s IS NOT NULL
+          ), 
+          buckets AS (
+            SELECT
+              range as bucket,
+              (range) * (select range FROM S) / %[4]v + (select minVal from S) as low,
+              (range + 1) * (select range FROM S) / %[4]v + (select minVal from S) as high
+            FROM range(0, %[4]v, 1)
+          ),
+          -- bin the values
+          binned_data AS (
+            SELECT 
+              FLOOR((value - (select minVal from S)) / (select range from S) * %[4]v) as bucket
+            from values
+          ),
+          -- join the bucket set with the binned values to generate the histogram
+          histogram_stage AS (
+          SELECT
+              buckets.bucket,
+              low,
+              high,
+              SUM(CASE WHEN binned_data.bucket = buckets.bucket THEN 1 ELSE 0 END) as count
+            FROM buckets
+            LEFT JOIN binned_data ON binned_data.bucket = buckets.bucket
+            GROUP BY buckets.bucket, low, high
+            ORDER BY buckets.bucket
+          ),
+          -- calculate the right edge, sine in histogram_stage we don't look at the values that
+          -- might be the largest.
+          right_edge AS (
+            SELECT count(*) as c from values WHERE value = (select maxVal from S)
+          ), histrogram_with_edge AS (
+          SELECT
+            bucket,
+            low,
+            high,
+            -- fill in the case where we've filtered out the highest value and need to recompute it, otherwise use count.
+            CASE WHEN high = (SELECT max(high) from histogram_stage) THEN count + (select c from right_edge) ELSE count END AS count
+            FROM histogram_stage
+          )
+          SELECT
+            bucket,
+            low,
+            high,
+            CASE WHEN count>0 THEN true ELSE false END AS present
+          FROM histrogram_with_edge
+          WHERE present=true`, selectColumn, sanitizedColumnName, request.TableName, outlierPseudoBucketSize)
+
+	outlierResults, err := s.query(ctx, request.InstanceId, &drivers.Statement{
+		Query: sql,
+	})
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+	defer outlierResults.Close()
+
+	outlierBins := make([]*api.NumericOutliers_Outlier, 0)
+	for outlierResults.Next() {
+		outlier := &api.NumericOutliers_Outlier{}
+		outlierResults.Scan(&outlier.Bucket, &outlier.Low, &outlier.High, &outlier.Present)
+		outlierBins = append(outlierBins, outlier)
+	}
+
 	return &api.NumericSummary{
 		NumericOutliers: &api.NumericOutliers{
-			Outliers: []*api.NumericOutliers_Outlier{},
+			Outliers: outlierBins,
 		},
 	}, nil
 }
 
 func (s *Server) GetTimeRangeSummary(ctx context.Context, request *api.TimeRangeSummaryRequest) (*api.TimeRangeSummary, error) {
-	return &api.TimeRangeSummary{}, nil
+	sanitizedColumnName := quoteName(request.ColumnName)
+	rows, err := s.query(ctx, request.InstanceId, &drivers.Statement{
+		Query: fmt.Sprintf("SELECT min(%[1]s) as min, max(%[1]s) as max, max(%[1]s) - min(%[1]s) as interval FROM %[2]s",
+			sanitizedColumnName, request.TableName),
+	})
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+	defer rows.Close()
+	for rows.Next() {
+		summary := &api.TimeRangeSummary{}
+		rows.Scan(&summary.Min, &summary.Max, &summary.Interval)
+		return summary, nil
+	}
+	return nil, status.Error(codes.Internal, "no rows returned")
 }
 
 func (s *Server) GetCardinalityOfColumn(ctx context.Context, request *api.CardinalityOfColumnRequest) (*api.CategoricalSummary, error) {
-	return &api.CategoricalSummary{
-		Cardinality: new(int64),
-	}, nil
+	sanitizedColumnName := quoteName(request.ColumnName)
+	rows, err := s.query(ctx, request.InstanceId, &drivers.Statement{
+		Query: fmt.Sprintf("SELECT approx_count_distinct(%s) as count from %s", sanitizedColumnName, request.TableName),
+	})
+	defer rows.Close()
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+	for rows.Next() {
+		summary := &api.CategoricalSummary{}
+		rows.Scan(&summary.Cardinality)
+		return summary, nil
+	}
+	return nil, status.Error(codes.Internal, "no rows returned")
 }
 
 func quoteName(columnName string) string {
