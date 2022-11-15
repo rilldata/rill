6765cb66dc74ff3cabe8694ff9bdee98dc101f69
Benjamin Egelund-Müller <b@egelund-muller.com>
2022-11-15 14:04:39 +0100

    Use a new migration number for duckdb catalog

--- runtime/drivers/duckdb/migrations/0004.sql
+++ runtime/drivers/duckdb/migrations/0004.sql
@@ -2,6 +2,6 @@
 -- lower_name_unique_idx, add the columns, then create lower_name_unique_idx again.
 
 DROP INDEX IF EXISTS rill.lower_name_unique_idx;
-ALTER TABLE rill.catalog ADD COLUMN "schema" BLOB;
-ALTER TABLE rill.catalog ADD COLUMN managed BOOLEAN;
+ALTER TABLE rill.catalog ADD COLUMN definition VARBINARY;
+ALTER TABLE rill.catalog ADD COLUMN path TEXT;
 CREATE UNIQUE INDEX lower_name_unique_idx ON rill.catalog (lower(name));
