e180c8ab1a6058759e44b6a82774ac2cdc15380d
Eric P Green <ericpgreen2@gmail.com>
2022-11-16 20:43:32 -0800

    Use new Delete API

--- web-local/src/lib/components/navigation/models/ModelMenuItems.svelte
+++ web-local/src/lib/components/navigation/models/ModelMenuItems.svelte
@@ -1,37 +1,43 @@
 <script lang="ts">
+  import { goto } from "$app/navigation";
+  import {
+    getRuntimeServiceListCatalogObjectsQueryKey,
+    RuntimeServiceListCatalogObjectsType,
+    useRuntimeServiceDeleteFileAndMigrate,
+  } from "@rilldata/web-common/runtime-client";
   import type { DerivedModelEntity } from "@rilldata/web-local/common/data-modeler-state-service/entity-state-service/DerivedModelEntityService";
   import { EntityType } from "@rilldata/web-local/common/data-modeler-state-service/entity-state-service/EntityStateService";
-  import { getNextEntityId } from "@rilldata/web-local/common/utils/getNextEntityId";
-  import type { ApplicationStore } from "@rilldata/web-local/lib/application-state-stores/application-store";
-  import type {
-    DerivedModelStore,
-    PersistentModelStore,
-  } from "@rilldata/web-local/lib/application-state-stores/model-stores";
-  import { deleteModelApi } from "@rilldata/web-local/lib/redux-store/model/model-apis";
-  import { autoCreateMetricsDefinitionForModel } from "@rilldata/web-local/lib/redux-store/source/source-apis";
-  import {
-    derivedProfileEntityHasTimestampColumn,
-    selectTimestampColumnFromProfileEntity,
-  } from "@rilldata/web-local/lib/redux-store/source/source-selectors";
-  import { createEventDispatcher, getContext } from "svelte";
-  import Cancel from "../../icons/Cancel.svelte";
-  import EditIcon from "../../icons/EditIcon.svelte";
-  import Explore from "../../icons/Explore.svelte";
-
-  import { navigationEvent } from "../../../metrics/initMetrics";
-
-  import { goto } from "$app/navigation";
   import { BehaviourEventMedium } from "@rilldata/web-local/common/metrics-service/BehaviourEventTypes";
   import {
     EntityTypeToScreenMap,
     MetricsEventScreenName,
     MetricsEventSpace,
   } from "@rilldata/web-local/common/metrics-service/MetricsTypes";
+  import { getNextEntityId } from "@rilldata/web-local/common/utils/getNextEntityId";
+  import type { ApplicationStore } from "@rilldata/web-local/lib/application-state-stores/application-store";
+  import type {
+    DerivedModelStore,
+    PersistentModelStore,
+  } from "@rilldata/web-local/lib/application-state-stores/model-stores";
+  import { autoCreateMetricsDefinitionForModel } from "@rilldata/web-local/lib/redux-store/source/source-apis";
+  import {
+    derivedProfileEntityHasTimestampColumn,
+    selectTimestampColumnFromProfileEntity,
+  } from "@rilldata/web-local/lib/redux-store/source/source-selectors";
+  import { createEventDispatcher, getContext } from "svelte";
+  import { runtimeStore } from "../../../application-state-stores/application-store";
+  import { navigationEvent } from "../../../metrics/initMetrics";
+  import { queryClient } from "../../../svelte-query/globalQueryClient";
+  import Cancel from "../../icons/Cancel.svelte";
+  import EditIcon from "../../icons/EditIcon.svelte";
+  import Explore from "../../icons/Explore.svelte";
   import { Divider, MenuItem } from "../../menu";
 
   export let modelID;
 
   const dispatch = createEventDispatcher();
+
+  const deleteModel = useRuntimeServiceDeleteFileAndMigrate();
 
   const persistentModelStore = getContext(
     "rill:app:persistent-model-store"
@@ -68,22 +74,46 @@
     });
   };
 
-  /** delete model */
-  const deleteModel = (id: string) => {
-    if (
-      $applicationStore.activeEntity.type === EntityType.Model &&
-      $applicationStore.activeEntity.id === id
-    ) {
-      const nextModelId = getNextEntityId($persistentModelStore.entities, id);
+  const handleDeleteModel = (modelName: string) => {
+    $deleteModel.mutate(
+      {
+        data: {
+          repoId: $runtimeStore.repoId,
+          instanceId: $runtimeStore.instanceId,
+          path: `models/${modelName}`,
+        },
+      },
+      {
+        onSuccess: () => {
+          if (
+            $applicationStore.activeEntity.type === EntityType.Model &&
+            $applicationStore.activeEntity.id === persistentModel.id
+          ) {
+            const nextModelId = getNextEntityId(
+              $persistentModelStore.entities,
+              persistentModel.id
+            );
+            const nextModelName = $persistentModelStore.entities.find(
+              (source) => source.id === nextModelId
+            ).tableName;
+            if (nextModelName) {
+              goto(`/model/${nextModelName}`);
+            } else {
+              goto("/");
+            }
+          }
 
-      if (nextModelId) {
-        goto(`/model/${nextModelId}`);
-      } else {
-        goto("/");
+          queryClient.invalidateQueries(
+            getRuntimeServiceListCatalogObjectsQueryKey(
+              $runtimeStore.instanceId,
+              {
+                type: RuntimeServiceListCatalogObjectsType.TYPE_MODEL,
+              }
+            )
+          );
+        },
       }
-    }
-
-    deleteModelApi(id);
+    );
   };
 </script>
 
@@ -110,7 +140,7 @@
   <EditIcon slot="icon" />
   rename...
 </MenuItem>
-<MenuItem icon on:select={() => deleteModel(derivedModel.id)}>
+<MenuItem icon on:select={() => handleDeleteModel(persistentModel.name)}>
   <Cancel slot="icon" />
   delete
 </MenuItem>
