e3a8c4868429aa445e1519647f5ccb069b61f1d9
Benjamin Egelund-Müller <b@egelund-muller.com>
2022-11-17 17:47:45 +0100

    Update runtime version

--- web-local/build-tools/postinstall_runtime.sh
+++ web-local/build-tools/postinstall_runtime.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Hardcoded runtime version to install
-RUNTIME_VERSION="759f8fe293bb1bd9e3843aab0a9579be757d0f59"
+RUNTIME_VERSION="beae981332ef57db41eb82da8bb68f14958b7440"
 
 # Targets dist/runtime as the output directory
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
