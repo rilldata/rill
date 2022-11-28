d484bb9953ab320d20db4cc756570d4e26c0fb07
Dhiraj Kumar <sumankumaribarnwal@gmail.com>
2022-11-28 22:32:24 +0530

    Fix reactivity bug

--- web-local/src/routes/(application)/dashboard/[name]/edit/+page.svelte
+++ web-local/src/routes/(application)/dashboard/[name]/edit/+page.svelte
@@ -3,7 +3,9 @@
     runtimeServiceGetFile,
     useRuntimeServiceGetFile,
   } from "@rilldata/web-common/runtime-client";
+  import { EntityType } from "@rilldata/web-local/common/data-modeler-state-service/entity-state-service/EntityStateService";
   import { runtimeStore } from "@rilldata/web-local/lib/application-state-stores/application-store";
+  import { getFileFromName } from "@rilldata/web-local/lib/components/entity-mappers/mappers";
   import { MetricsDefinitionWorkspace } from "@rilldata/web-local/lib/components/workspace";
 
   export let data;
@@ -13,7 +15,7 @@
 
   $: dashboardYAML = useRuntimeServiceGetFile(
     instanceId,
-    `dashboards/${metricsDefName}.yaml`
+    getFileFromName(metricsDefName, EntityType.MetricsDefinition)
   );
 
   $: yaml = $dashboardYAML.data?.blob || "";
