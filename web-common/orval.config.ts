5d48be8a6a4ec288c8bc58a4daa514c6acaa65ba
Dhiraj Kumar <sumankumaribarnwal@gmail.com>
2022-11-29 15:12:08 +0530

    Totals API should be query

--- web-common/orval.config.ts
+++ web-common/orval.config.ts
@@ -43,6 +43,11 @@ export default defineConfig({
               useQuery: true,
             },
           },
+          RuntimeService_MetricsViewTotals: {
+            query: {
+              useQuery: true,
+            },
+          },
         },
       },
     },
