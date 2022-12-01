4d4b05f60cdf7ac10ea4332349af655cebbbfc26
rakeshsharma14317 <rakeshsharma14317@gmail.com>
2022-12-02 01:13:41 +0530

    Init and examples in CLI (#1312)
    * rill init-example with new cli
    
    * go fmt fix
    
    * fixing embed path
    
    * Updating make file to embed examples to binary
    
    * updated dir ModePerm
    
    * requested changes
    
    * added empty file to embed dir
    
    * travis failure changes
    
    * fix dit path for examples
    
    * adding list example arg
    
    * removing data file in example and PR changes
    
    * Updating cli readme.md
    
    * Many small cleanups
    
    * More refactors
    
    * More changes
    
    Co-authored-by: Benjamin Egelund-Müller <b@egelund-muller.com>

--- cli/cmd/docs/docs.go
+++ cli/cmd/docs/docs.go
@@ -1,11 +1,10 @@
 package docs
 
 import (
-	"github.com/mattn/go-colorable"
+	"fmt"
+
 	"github.com/rilldata/rill/cli/pkg/browser"
 	"github.com/spf13/cobra"
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 )
 
 var docsUrl = "https://docs.rilldata.com"
@@ -14,21 +13,11 @@ var docsUrl = "https://docs.rilldata.com"
 func DocsCmd() *cobra.Command {
 	var docsCmd = &cobra.Command{
 		Use:   "docs",
-		Short: "Show rill docs",
-		Long:  `A longer description`,
+		Short: "Open docs.rilldata.com",
 		Run: func(cmd *cobra.Command, args []string) {
-			// Create base logger
-			config := zap.NewDevelopmentEncoderConfig()
-			config.EncodeLevel = zapcore.CapitalColorLevelEncoder
-			logger := zap.New(zapcore.NewCore(
-				zapcore.NewConsoleEncoder(config),
-				zapcore.AddSync(colorable.NewColorableStdout()),
-				zapcore.DebugLevel,
-			))
-
 			err := browser.Open(docsUrl)
 			if err != nil {
-				logger.Sugar().Warnf("could not open browser error: %v, copy and paste this URL into your browser: %s", err, docsUrl)
+				fmt.Printf("Could not open browser. Copy this URL into your browser: %s\n", docsUrl)
 			}
 		},
 	}
--- cli/pkg/local/install_id.go
+++ cli/pkg/local/install_id.go
@@ -1,4 +1,4 @@
-package config
+package local
 
 import (
 	"encoding/json"
