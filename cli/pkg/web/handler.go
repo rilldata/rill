6c69f255d03415f216d0a1be4548117433e93bb6
Himadri Singh <himadri.singh@gmail.com>
2022-12-01 11:01:45 +0530

    try adding mime

--- cli/pkg/web/handler.go
+++ cli/pkg/web/handler.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"io/fs"
+	"mime"
 	"net/http"
 	"os"
 
@@ -16,6 +17,7 @@ var distFS embed.FS
 
 // Handler serves an web-local UI
 func StaticHandler() (http.Handler, error) {
+    mime.AddExtensionType(".js", "application/javascript")
 	uiAssetFS, err := newUIAssetFS()
 	if err != nil {
 		return nil, fmt.Errorf("UI assets error: %w", err)
