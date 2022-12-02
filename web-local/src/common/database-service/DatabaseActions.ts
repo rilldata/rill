f44668f7184f79e958028d0bbd5abf2fce9c0569
Aditya Hegde <adityahegderocks@gmail.com>
2022-12-02 20:09:22 +0530

    1st pass on nodejs code cleanup

--- web-local/src/common/database-service/DatabaseActions.ts
+++
@@ -1,9 +0,0 @@
-import type { DuckDBClient } from "./DuckDBClient";
-import type { DatabaseConfig } from "../config/DatabaseConfig";
-
-export class DatabaseActions {
-  public constructor(
-    protected readonly databaseConfig: DatabaseConfig,
-    protected readonly databaseClient: DuckDBClient
-  ) {}
-}
--- web-local/src/common/database-service/DatabaseMetadata.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Placeholder for query queuing and other db features.
- * Also helps to extract action methods by giving them a signature.
- */
-export class DatabaseMetadata {}
