adf98759a735737af76ff49ddcd3c2e9ce62de22
Eric P Green <ericpgreen2@gmail.com>
2022-12-06 10:01:00 -0800

    Fix dashboard routing

--- web-local/src/routes/(application)/dashboard/[name]/+page.ts
+++ web-local/src/routes/(application)/dashboard/[name]/+page.ts
@@ -1,6 +1,8 @@
-import { runtimeServiceGetCatalogEntry } from "@rilldata/web-common/runtime-client";
+import {
+  runtimeServiceGetCatalogEntry,
+  runtimeServiceGetFile,
+} from "@rilldata/web-common/runtime-client";
 import { runtimeServiceGetConfig } from "@rilldata/web-common/runtime-client/manual-clients";
-import { ExplorerMetricsDefinitionDoesntExist } from "@rilldata/web-local/common/errors/ErrorMessages";
 import { error, redirect } from "@sveltejs/kit";
 
 export const ssr = false;
@@ -10,28 +12,26 @@ export async function load({ params }) {
   const localConfig = await runtimeServiceGetConfig();
 
   try {
-    const dashboardMeta = await runtimeServiceGetCatalogEntry(
+    await runtimeServiceGetFile(
       localConfig.instance_id,
-      params.name
+      `dashboards/${params.name}.yaml`
     );
-
-    const dashboardYAML = dashboardMeta?.entry?.metricsView;
-
-    // check if metrics definition is defined
-    if (dashboardYAML) {
-      return {
-        metricViewName: params.name,
-      };
-    }
   } catch (err) {
-    if (
-      ExplorerMetricsDefinitionDoesntExist.includes(err.message) ||
-      err.message.includes(ExplorerMetricsDefinitionDoesntExist)
-    ) {
+    if (err.response?.data?.message.includes("entry not found")) {
       throw error(404, "Dashboard not found");
-    } else {
-      throw redirect(307, `/dashboard/${params.name}/edit`);
     }
+
+    throw error(err.response?.status || 500, err.message);
   }
-  throw redirect(307, `/dashboard/${params.name}/edit`);
+
+  try {
+    await runtimeServiceGetCatalogEntry(localConfig.instance_id, params.name);
+
+    return {
+      metricViewName: params.name,
+    };
+  } catch (err) {
+    // If the catalog entry doesn't exist, the dashboard config is invalid, so we redirect to the dashboard editor
+    throw redirect(307, `/dashboard/${params.name}/edit`);
+  }
 }
