dd37294db8c6804ab29e3b264128f0e7d9f24985
Aditya Hegde <adityahegderocks@gmail.com>
2022-12-08 10:42:25 +0530

    Adding parsing of csv delimiters

--- runtime/services/catalog/artifacts/yaml/objects.go
+++ runtime/services/catalog/artifacts/yaml/objects.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/jinzhu/copier"
+	"github.com/mitchellh/mapstructure"
 	runtimev1 "github.com/rilldata/rill/proto/gen/rill/runtime/v1"
 	"github.com/rilldata/rill/runtime/drivers"
 	"github.com/rilldata/rill/runtime/pkg/fileutil"
@@ -17,11 +18,12 @@ import (
 const Version = "0.0.1"
 
 type Source struct {
-	Version string
-	Type    string
-	URI     string `yaml:"uri,omitempty"`
-	Path    string `yaml:"path,omitempty"`
-	Region  string `yaml:"region,omitempty"`
+	Version      string
+	Type         string
+	Path         string `yaml:"path,omitempty"`
+	CsvDelimiter string `yaml:"csv.delimiter,omitempty" mapstructure:"csv.delimiter,omitempty"`
+	URI          string `yaml:"uri,omitempty"`
+	Region       string `yaml:"region,omitempty" mapstructure:"aws.region,omitempty"`
 }
 
 type MetricsView struct {
@@ -58,17 +60,15 @@ func toSourceArtifact(catalog *drivers.CatalogEntry) (*Source, error) {
 	}
 
 	props := catalog.GetSource().Properties.AsMap()
-	path, ok := props["path"].(string)
-	if ok {
-		if catalog.GetSource().Connector == "file" {
-			source.Path = path
-		} else {
-			source.URI = path
-		}
+
+	err := mapstructure.Decode(props, source)
+	if err != nil {
+		return nil, err
 	}
-	region, ok := props["aws.region"].(string)
-	if ok {
-		source.Region = region
+
+	if source.Path != "" && catalog.GetSource().Connector != "file" {
+		source.URI = source.Path
+		source.Path = ""
 	}
 
 	return source, nil
@@ -94,6 +94,9 @@ func fromSourceArtifact(source *Source, path string) (*drivers.CatalogEntry, err
 	}
 	if source.Region != "" {
 		props["aws.region"] = source.Region
+	}
+	if source.CsvDelimiter != "" {
+		props["csv.delimiter"] = source.CsvDelimiter
 	}
 	propsPB, err := structpb.NewStruct(props)
 	if err != nil {
