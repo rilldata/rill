ef1aa5fe8ab847425d45e7c4e88cfea09a851968
Parag Jain <pjain1@apache.org>
2022-12-09 18:23:29 +0530

    better concurrency test of priority worker

--- runtime/pkg/priorityworker/priorityworker_test.go
+++ runtime/pkg/priorityworker/priorityworker_test.go
@@ -11,13 +11,14 @@ import (
 
 func TestPriorityQueue(t *testing.T) {
 	n := 100
-	results := make(chan int, n)
+	results := make(chan int)
 	var g errgroup.Group
+	concurrency := 4
 
 	pw := New(func(ctx context.Context, i int) error {
 		results <- i
 		return nil
-	}, 4)
+	}, concurrency)
 	pw.Pause() // ensure predictable output
 
 	for i := n; i > 0; i-- {
@@ -27,20 +28,36 @@ func TestPriorityQueue(t *testing.T) {
 		})
 	}
 
-	// give the queue plenty of time to fill up, then unpause
+	// We need to check two things -
+	// 1. That the concurrency limit is respected and we are running as many jobs as concurrency
+	// 2. That the priority is respected
+	// Since we can't guarantee the order of the results, we are checking that sum of every n results is equal to sum
+	// of topN priorities, where n is the concurrency limit
+	// At this point all jobs are already present the priority worker
+	// After getting n results we are unPausing and letting next n jobs to run and pausing again unless we get n results
+	actual := 0
+	expected := 0
+	res := 0
+	for i := n; i > 0; i-- {
+		if i%concurrency == 0 {
+			require.Equal(t, expected, actual)
+			actual = 0
+			expected = 0
+			pw.Unpause()
+			// give time for topN jobs to start running,
+			time.Sleep(100 * time.Millisecond)
+			pw.Pause()
+			require.Equal(t, concurrency, len(pw.runningJobs))
+		}
+		res = <-results
+		actual += res
+		expected += i
+	}
 	time.Sleep(100 * time.Millisecond)
-	pw.Unpause()
+	require.Equal(t, 0, len(pw.runningJobs))
 
 	err := g.Wait()
 	require.NoError(t, err)
-
-	actual := 0
-	expected := 0
-	for i := n; i > 0; i-- {
-		actual += <-results
-		expected += i
-	}
-	require.Equal(t, expected, actual)
 }
 
 func TestCancel(t *testing.T) {
