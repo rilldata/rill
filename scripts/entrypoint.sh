c6f12ee66e9cdd3496bbf6893f7432534654f888
Himadri Singh <himadri.singh@gmail.com>
2022-12-13 18:24:14 +0530

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
-if [ ! -z "${PROJECT_BASE}" ] && [ ! -z "${PROJECT}" ] && [ ! -d ${PROJECT_BASE}/${PROJECT} ]; then
-  echo "Initializing the project ${PROJECT}..."
-  rill init --project ${PROJECT_BASE}/${PROJECT}
-fi
-
 if [ ! -z "${INIT_SCRIPT}" ] && [ -f ${INIT_SCRIPT} ]; then
  echo "Found init script at ${INIT_SCRIPT}..."
  source ${INIT_SCRIPT}
 fi
 
-echo "Starting Rill Developer at project ${PROJECT_BASE}/${PROJECT}..."
-rill start --project ${PROJECT_BASE}/${PROJECT}
+echo "Starting Rill Developer..."
+/app/rill start
