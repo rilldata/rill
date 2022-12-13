5241d939fb61b0550480e493b18b21147c2873d1
Benjamin Egelund-Müller <b@egelund-muller.com>
2022-12-13 17:21:15 +0100

    Sync example project from rill-developer-example

--- examples/default/models/iot_model.sql
+++
@@ -1,6 +0,0 @@
-SELECT 
-  to_timestamp(CAST(ts AS INT)) AS event_datetime,
-  CASE WHEN light THEN 'light' ELSE 'dark' END AS light,
-  CASE WHEN motion THEN 'activity' ELSE 'motionless' END AS motion,
-  * EXCLUDE (ts, light, motion)
-FROM iot_telemetry_data
