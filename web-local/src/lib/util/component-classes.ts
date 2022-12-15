e4d949cd0dcfbeb089100fa8cbb3fa197c5d3d33
Hamilton Ulmer <hamilton.ulmer@gmail.com>
2022-12-15 13:33:50 -0800

    tweaks a bit of spacing where too tight

--- web-local/src/lib/util/component-classes.ts
+++ web-local/src/lib/util/component-classes.ts
@@ -1,6 +1,6 @@
 /** classes for the native select element */
 export const NATIVE_SELECT =
-  "rounded border border-6 border-transparent bg-gray-100 hover:bg-gray-200";
+  "rounded border border-6 border-transparent hover:bg-gray-200";
 
 /** classes for elements that trigger the highlight in a model query */
 export const QUERY_REFERENCE_TRIGGER =
