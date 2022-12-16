a29316ffb8a0f8fae7a02aeffa1846d0a653eb26
Eric P Green <ericpgreen2@gmail.com>
2022-12-16 17:37:51 -0500

    Add SQL keywords

--- web-local/src/lib/components/Editor.svelte
+++ web-local/src/lib/components/Editor.svelte
@@ -165,7 +165,7 @@
 
   const DuckDBSQL: SQLDialect = SQLDialect.define({
     keywords:
-      "select from where group by all having order limit sample unnest with window qualify values filter exclude replace like ilike glob",
+      "select from where group by all having order limit sample unnest with window qualify values filter exclude replace like ilike glob as case when then end in cast left join on not desc asc sum union",
   });
 
   function makeAutocompleteConfig(schema: { [table: string]: string[] }) {
