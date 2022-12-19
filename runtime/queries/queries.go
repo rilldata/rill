3584a6ff361570495067e09852dcf2a728370688
Egor Riashin <egor-ryashin@users.noreply.github.com>
2022-12-19 14:54:51 +0300

    Query cache interface implementation for timeseries & metrics views (#1446)
    * caching: metricsview timeseries
    
    Co-authored-by: egor-ryashin <egor.ryashin@rilldata.com>

--- runtime/queries/queries.go
+++ runtime/queries/queries.go
@@ -1,7 +1,35 @@
 package queries
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/rilldata/rill/runtime/drivers"
+)
 
 func quoteName(name string) string {
 	return fmt.Sprintf("\"%s\"", name)
+}
+
+func EscapeSingleQuotes(value string) string {
+	return strings.ReplaceAll(value, "'", "''")
+}
+
+func EscapeDoubleQuotes(column string) string {
+	return strings.ReplaceAll(column, "\"", "\"\"")
+}
+
+func DropTempTable(olap drivers.OLAPStore, priority int, tableName string) {
+	rs, er := olap.Execute(context.Background(), &drivers.Statement{
+		Query:    `DROP TABLE "` + tableName + `"`,
+		Priority: priority,
+	})
+	if er == nil {
+		rs.Close()
+	}
+}
+
+func ReplaceHyphen(column string) string {
+	return strings.ReplaceAll(column, "-", "_")
 }
