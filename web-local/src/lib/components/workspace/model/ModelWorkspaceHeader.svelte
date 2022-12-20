cd52c1d131d8142084b0b2744632ee5c239f493f
Hamilton Ulmer <hamilton.ulmer@gmail.com>
2022-12-19 16:17:33 -0800

    adds in ModelWorkspaceheader.svelte

---
+++ web-local/src/lib/components/workspace/model/ModelWorkspaceHeader.svelte
@@ -0,0 +1,162 @@
+<script lang="ts">
+  import { useRuntimeServiceRenameFileAndReconcile } from "@rilldata/web-common/runtime-client";
+  import { runtimeStore } from "@rilldata/web-local/lib/application-state-stores/application-store";
+  import { fileArtifactsStore } from "@rilldata/web-local/lib/application-state-stores/file-artifacts-store";
+  import { RuntimeUrl } from "@rilldata/web-local/lib/application-state-stores/initialize-node-store-contexts";
+  import { Button } from "@rilldata/web-local/lib/components/button";
+  import WithTogglableFloatingElement from "@rilldata/web-local/lib/components/floating-element/WithTogglableFloatingElement.svelte";
+  import Export from "@rilldata/web-local/lib/components/icons/Export.svelte";
+  import { Menu, MenuItem } from "@rilldata/web-local/lib/components/menu";
+  import PanelCTA from "@rilldata/web-local/lib/components/panel/PanelCTA.svelte";
+  import Tooltip from "@rilldata/web-local/lib/components/tooltip/Tooltip.svelte";
+  import TooltipContent from "@rilldata/web-local/lib/components/tooltip/TooltipContent.svelte";
+  import {
+    isDuplicateName,
+    renameFileArtifact,
+    useAllNames,
+  } from "@rilldata/web-local/lib/svelte-query/actions";
+  import { EntityType } from "@rilldata/web-local/lib/temp/entity";
+  import { getFilePathFromNameAndType } from "@rilldata/web-local/lib/util/entity-mappers";
+  import { useQueryClient } from "@sveltestack/svelte-query";
+  import { getContext } from "svelte";
+  import { WorkspaceHeader } from "..";
+  import { IconButton } from "../../button";
+  import HideBottomPane from "../../icons/HideBottomPane.svelte";
+  import { notifications } from "../../notifications";
+  import CreateDashboardButton from "./inspector/header/CreateDashboardButton.svelte";
+
+  export let modelName: string;
+
+  $: runtimeInstanceId = $runtimeStore.instanceId;
+
+  $: allNamesQuery = useAllNames(runtimeInstanceId);
+  const queryClient = useQueryClient();
+  const renameModel = useRuntimeServiceRenameFileAndReconcile();
+
+  const outputLayout = getContext("rill:app:output-layout");
+  // const outputVisibilityTween = getContext("rill:app:output-visibility-tween");
+  $: modelPath = getFilePathFromNameAndType(modelName, EntityType.Model);
+  $: modelError = $fileArtifactsStore.entities[modelPath]?.errors[0]?.message;
+  $: modelHasError = !!modelError;
+
+  let contextMenuOpen = false;
+
+  const onExport = async (exportExtension: "csv" | "parquet") => {
+    // TODO: how do we handle errors ?
+    window.open(
+      `${RuntimeUrl}/v1/instances/${$runtimeStore.instanceId}/table/${modelName}/export/${exportExtension}`
+    );
+  };
+
+  function formatModelName(str) {
+    return str?.trim().replaceAll(" ", "_").replace(/\.sql/, "");
+  }
+
+  const onChangeCallback = async (e) => {
+    if (!e.target.value.match(/^[a-zA-Z_][a-zA-Z0-9_]*$/)) {
+      notifications.send({
+        message:
+          "Model name must start with a letter or underscore and contain only letters, numbers, and underscores",
+      });
+      e.target.value = modelName; // resets the input
+      return;
+    }
+    if (isDuplicateName(e.target.value, $allNamesQuery.data)) {
+      notifications.send({
+        message: `Name ${e.target.value} is already in use`,
+      });
+      e.target.value = modelName; // resets the input
+      return;
+    }
+
+    try {
+      await renameFileArtifact(
+        queryClient,
+        runtimeInstanceId,
+        modelName,
+        e.target.value,
+        EntityType.Model,
+        $renameModel
+      );
+    } catch (err) {
+      console.error(err.response.data.message);
+    }
+  };
+
+  $: titleInput = modelName;
+</script>
+
+<WorkspaceHeader
+  {...{ titleInput: formatModelName(titleInput), onChangeCallback }}
+  showStatus={false}
+>
+  <svelte:fragment slot="workspace-controls">
+    <IconButton
+      on:click={() => {
+        outputLayout.update((state) => {
+          state.visible = !state.visible;
+          return state;
+        });
+      }}
+      ><HideBottomPane size="18px" />
+    </IconButton>
+  </svelte:fragment>
+  <svelte:fragment slot="cta">
+    <PanelCTA let:width side="right">
+      <Tooltip
+        alignment="middle"
+        distance={16}
+        location="left"
+        suppress={contextMenuOpen}
+      >
+        <!-- attach floating element right here-->
+        <WithTogglableFloatingElement
+          alignment="start"
+          bind:active={contextMenuOpen}
+          distance={16}
+          let:toggleFloatingElement
+          location="left"
+        >
+          <Button
+            disabled={modelHasError}
+            on:click={toggleFloatingElement}
+            type="secondary"
+          >
+            Export Results
+            <Export size="14px" />
+          </Button>
+          <Menu
+            dark
+            on:click-outside={toggleFloatingElement}
+            on:escape={toggleFloatingElement}
+            slot="floating-element"
+          >
+            <MenuItem
+              on:select={() => {
+                toggleFloatingElement();
+                onExport("parquet");
+              }}
+            >
+              Export as Parquet
+            </MenuItem>
+            <MenuItem
+              on:select={() => {
+                toggleFloatingElement();
+                onExport("csv");
+              }}
+            >
+              Export as CSV
+            </MenuItem>
+          </Menu>
+        </WithTogglableFloatingElement>
+        <TooltipContent slot="tooltip-content">
+          {#if modelHasError}Fix the errors in your model to export
+          {:else}
+            Export this model as a dataset
+          {/if}
+        </TooltipContent>
+      </Tooltip>
+      <CreateDashboardButton hasError={modelHasError} {modelName} {width} />
+    </PanelCTA>
+  </svelte:fragment>
+</WorkspaceHeader>
