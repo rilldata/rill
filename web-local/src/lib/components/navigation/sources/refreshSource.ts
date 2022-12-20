7c3a4505d5055c8d1865621b9369a966c368a0da
Aditya Hegde <adityahegderocks@gmail.com>
2022-12-20 16:32:27 +0530

    Show error on refresh failure

--- web-local/src/lib/components/navigation/sources/refreshSource.ts
+++ web-local/src/lib/components/navigation/sources/refreshSource.ts
@@ -1,10 +1,13 @@
 import type {
   V1PutFileAndReconcileResponse,
+  V1ReconcileResponse,
   V1RefreshAndReconcileResponse,
 } from "@rilldata/web-common/runtime-client";
 import { fileArtifactsStore } from "@rilldata/web-local/lib/application-state-stores/file-artifacts-store";
 import { overlay } from "@rilldata/web-local/lib/application-state-stores/overlay-store";
+import { humanReadableErrorMessage } from "@rilldata/web-local/lib/components/navigation/sources/errors";
 import { compileCreateSourceYAML } from "@rilldata/web-local/lib/components/navigation/sources/sourceUtils";
+import { notifications } from "@rilldata/web-local/lib/components/notifications";
 import { EntityType } from "@rilldata/web-local/lib/temp/entity";
 import {
   openFileUploadDialog,
@@ -22,16 +25,19 @@ export async function refreshSource(
   createSource: UseMutationResult<V1PutFileAndReconcileResponse>,
   queryClient: QueryClient
 ) {
+  const artifactPath = getFilePathFromNameAndType(sourceName, EntityType.Table);
+
   if (connector !== "local_file") {
     overlay.set({ title: `Importing ${sourceName}` });
     const resp = await refreshSource.mutateAsync({
       data: {
         instanceId,
-        path: getFilePathFromNameAndType(sourceName, EntityType.Table),
+        path: artifactPath,
       },
     });
     invalidateAfterReconcile(queryClient, instanceId, resp);
     fileArtifactsStore.setErrors(resp.affectedPaths, resp.errors);
+    showNotificationForSourceRefresh(resp, connector, sourceName, artifactPath);
     return resp;
   }
 
@@ -55,7 +61,7 @@ export async function refreshSource(
   const resp = await createSource.mutateAsync({
     data: {
       instanceId,
-      path: getFilePathFromNameAndType(sourceName, EntityType.Table),
+      path: artifactPath,
       blob: yaml,
       create: true,
       strict: true,
@@ -63,5 +69,33 @@ export async function refreshSource(
   });
   invalidateAfterReconcile(queryClient, instanceId, resp);
   fileArtifactsStore.setErrors(resp.affectedPaths, resp.errors);
+  showNotificationForSourceRefresh(
+    resp,
+    "local_file",
+    sourceName,
+    artifactPath
+  );
   return resp;
+}
+
+function showNotificationForSourceRefresh(
+  resp: V1ReconcileResponse,
+  connector: string,
+  name: string,
+  artifactPath: string
+) {
+  const error = resp.errors.find((err) => err.filePath === artifactPath);
+  if (error) {
+    notifications.send({
+      message: `Failed to refresh source ${name}`,
+      detail: humanReadableErrorMessage(connector, 3, error.message),
+      options: {
+        persisted: true,
+      },
+    });
+  } else {
+    notifications.send({
+      message: `Refreshed source ${name}.`,
+    });
+  }
 }
