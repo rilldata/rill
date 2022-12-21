449ca15a23ef5cc42e71d7a2bd437a983b8193e5
Eric P Green <ericpgreen2@gmail.com>
2022-12-21 15:46:31 -0500

    Remove unused util files

--- web-local/src/lib/util/queryUtils.ts
+++
@@ -1,5 +0,0 @@
-export function sanitizeColumn(columnName: string) {
-  // // special-case for how duckdb works
-  // if (columnName === 'count(*)') { return '"count_star()"'};
-  return `"${columnName}"`;
-}
