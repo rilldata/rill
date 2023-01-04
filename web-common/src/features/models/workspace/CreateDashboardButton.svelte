753ac5ded12e948b743f385adc3161bb20c6cd16
Dhiraj Kumar <sumankumaribarnwal@gmail.com>
2023-01-04 20:40:32 +0530

    Remove redundant import

--- web-common/src/features/models/workspace/CreateDashboardButton.svelte
+++ web-common/src/features/models/workspace/CreateDashboardButton.svelte
@@ -27,7 +27,6 @@
   import { selectTimestampColumnFromSchema } from "@rilldata/web-local/lib/svelte-query/column-selectors";
   import { useDashboardNames } from "@rilldata/web-local/lib/svelte-query/dashboards";
   import { invalidateAfterReconcile } from "@rilldata/web-local/lib/svelte-query/invalidation";
-  import { EntityType } from "@rilldata/web-local/lib/temp/entity";
   import { getFilePathFromNameAndType } from "@rilldata/web-local/lib/util/entity-mappers";
   import { getName } from "@rilldata/web-local/lib/util/incrementName";
   import { useQueryClient } from "@sveltestack/svelte-query";
