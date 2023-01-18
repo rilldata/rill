69bd64250f54bae24d3eb0c75bdbd27cd5e4dd03
Rakesh Sharma <rakeshsharma14317@gmail.com>
2023-01-18 17:09:15 +0530

    changed default uuid

--- cli/pkg/local/app.go
+++ cli/pkg/local/app.go
@@ -38,7 +38,7 @@ import (
 
 // Default instance config on local.
 const (
-	DefaultInstanceID = "default"
+	DefaultInstanceID = "123e4567-e89b-12d3-a456-426614174000"
 	DefaultOLAPDriver = "duckdb"
 	DefaultOLAPDSN    = "stage.db"
 )
