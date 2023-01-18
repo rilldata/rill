3c39f6da5d9ca355c644e958c4b097e2f7d4f9e0
Hamilton Ulmer <hamilton.ulmer@gmail.com>
2023-01-17 21:59:16 -0800

    fixes source workspace issues

--- web-common/src/features/sources/workspace/SourceWorkspace.svelte
+++ web-common/src/features/sources/workspace/SourceWorkspace.svelte
@@ -24,6 +24,8 @@
   import SourceInspector from "./SourceInspector.svelte";
   import SourceWorkspaceHeader from "./SourceWorkspaceHeader.svelte";
   import Inspector from "@rilldata/web-local/lib/components/workspace/core/Inspector.svelte";
+
+  import WorkspaceBody from "@rilldata/web-local/lib/components/workspace/core/WorkspaceBody.svelte";
   export let sourceName: string;
   export let embedded = false;
   export let path: string = undefined;
@@ -104,129 +106,129 @@
     <div slot="header">
       <SourceWorkspaceHeader {sourceName} {path} {embedded} />
     </div>
-    <div
-      slot="body"
-      class="grid pb-3"
-      style:grid-template-rows="max-content auto"
-      style:height="100vh"
-    >
-      {#if entryExists}
-        <div
-          style:overflow="auto"
-          style:height="calc(100vh - var(--header-height) - 2rem)"
-          class="m-4 border border-gray-300 rounded"
-        >
-          {#key sourceName}
-            <ConnectedPreviewTable objectName={sourceName} />
-          {/key}
-        </div>
-      {:else}
-        <!-- error states -->
-        <div
-          class="errors flex flex-col items-center pt-8 gap-y-4 m-auto mt-0 text-gray-500"
-          style:width="500px"
-        >
-          {#if !allConnectors.includes(source?.type)}
-            <div>
-              {#if source?.type}
-                Rill does not support a connector for <span class="font-bold"
-                  >{source?.type}</span
+    <WorkspaceBody bgClass="bg-gray-100">
+      <div
+        class="grid pb-3"
+        style:grid-template-rows="max-content auto"
+        style:height="100vh"
+      >
+        {#if entryExists}
+          <div
+            style:overflow="auto"
+            style:height="calc(100vh - var(--header-height) - 2rem)"
+            class="m-4 border border-gray-300 rounded"
+          >
+            {#key sourceName}
+              <ConnectedPreviewTable objectName={sourceName} />
+            {/key}
+          </div>
+        {:else}
+          <!-- error states -->
+          <div
+            class="errors flex flex-col items-center pt-8 gap-y-4 m-auto mt-0 text-gray-500"
+            style:width="500px"
+          >
+            {#if !allConnectors.includes(source?.type)}
+              <div>
+                {#if source?.type}
+                  Rill does not support a connector for <span class="font-bold"
+                    >{source?.type}</span
+                  >.
+                {:else}
+                  Connector not defined.
+                {/if}
+                Edit <b>{`sources/${sourceName}.yaml`}</b> to add a valid "type:
+                {"<filetype>"}" to get started.
+              </div>
+              <div>
+                For more information,
+                <a href="https://docs.rilldata.com/using-rill/import-data"
+                  >view the documentation</a
                 >.
-              {:else}
-                Connector not defined.
-              {/if}
-              Edit <b>{`sources/${sourceName}.yaml`}</b> to add a valid "type:
-              {"<filetype>"}" to get started.
-            </div>
-            <div>
-              For more information,
-              <a href="https://docs.rilldata.com/using-rill/import-data"
-                >view the documentation</a
-              >.
-            </div>
-          {:else if source?.type === "local_file"}
-            <div class="text-center">
-              The data file for <span class="font-bold">{sourceName}</span> has not
-              been imported as a source.
-            </div>
-            <Button
-              type="primary"
-              on:click={async () => {
-                uploadErrors = undefined;
-                await onRefreshClick(sourceName);
-              }}
-              >Import a CSV or Parquet file
-            </Button>
-          {:else if !Object.keys(source || {})?.length}
-            <!-- source is empty -->
-            <div>
-              The source <span class="font-bold">{sourceName}</span>
-              is empty. Edit <b>{`sources/${sourceName}.yaml`}</b> to add a source
-              definition.
-            </div>
-            <div>
-              For more information,
-              <a href="https://docs.rilldata.com/using-rill/import-data"
-                >view the documentation</a
-              >.
-            </div>
-          {:else if !source?.type}
-            <div>
-              The source <span class="font-bold">{sourceName}</span> does not
-              have a defined type. Edit <b>{`sources/${sourceName}.yaml`}</b> to
-              add "type:
-              {"<filetype>"}"
-            </div>
-            <div>
-              For more information,
-              <a href="https://docs.rilldata.com/using-rill/import-data"
-                >view the documentation</a
-              >.
-            </div>
-          {:else if remoteConnectorNames.includes(currentConnector?.name) && !source?.uri}
-            <div>
-              The source URI has not been defined. Edit <b
-                >{`sources/${sourceName}.yaml`}</b
-              >
-              to add "uri:
-              {"<uri>"}"
-            </div>
-            <div>
-              For more information,
-              <a href="https://docs.rilldata.com/using-rill/import-data"
-                >view the documentation</a
-              >.
-            </div>
-          {:else}
-            <div class="text-center">
-              The source <span class="font-bold">{sourceName}</span> has not been
-              imported.
-            </div>
-            <Button
-              type="primary"
-              on:click={async () => {
-                uploadErrors = undefined;
-                await onRefreshClick(sourceName);
-              }}
-              >Import data
-            </Button>
-          {/if}
-          <!-- show any remaining errors -->
-          {#if uploadErrors?.length}
-            <Callout level="error">
-              {#each uploadErrors as error}
-                {humanReadableErrorMessage(
-                  currentConnector?.name,
-                  3,
-                  error.message
-                )}
-              {/each}
-            </Callout>
-          {/if}
-        </div>
-      {/if}
-    </div>
-
+              </div>
+            {:else if source?.type === "local_file"}
+              <div class="text-center">
+                The data file for <span class="font-bold">{sourceName}</span> has
+                not been imported as a source.
+              </div>
+              <Button
+                type="primary"
+                on:click={async () => {
+                  uploadErrors = undefined;
+                  await onRefreshClick(sourceName);
+                }}
+                >Import a CSV or Parquet file
+              </Button>
+            {:else if !Object.keys(source || {})?.length}
+              <!-- source is empty -->
+              <div>
+                The source <span class="font-bold">{sourceName}</span>
+                is empty. Edit <b>{`sources/${sourceName}.yaml`}</b> to add a source
+                definition.
+              </div>
+              <div>
+                For more information,
+                <a href="https://docs.rilldata.com/using-rill/import-data"
+                  >view the documentation</a
+                >.
+              </div>
+            {:else if !source?.type}
+              <div>
+                The source <span class="font-bold">{sourceName}</span> does not
+                have a defined type. Edit <b>{`sources/${sourceName}.yaml`}</b>
+                to add "type:
+                {"<filetype>"}"
+              </div>
+              <div>
+                For more information,
+                <a href="https://docs.rilldata.com/using-rill/import-data"
+                  >view the documentation</a
+                >.
+              </div>
+            {:else if remoteConnectorNames.includes(currentConnector?.name) && !source?.uri}
+              <div>
+                The source URI has not been defined. Edit <b
+                  >{`sources/${sourceName}.yaml`}</b
+                >
+                to add "uri:
+                {"<uri>"}"
+              </div>
+              <div>
+                For more information,
+                <a href="https://docs.rilldata.com/using-rill/import-data"
+                  >view the documentation</a
+                >.
+              </div>
+            {:else}
+              <div class="text-center">
+                The source <span class="font-bold">{sourceName}</span> has not been
+                imported.
+              </div>
+              <Button
+                type="primary"
+                on:click={async () => {
+                  uploadErrors = undefined;
+                  await onRefreshClick(sourceName);
+                }}
+                >Import data
+              </Button>
+            {/if}
+            <!-- show any remaining errors -->
+            {#if uploadErrors?.length}
+              <Callout level="error">
+                {#each uploadErrors as error}
+                  {humanReadableErrorMessage(
+                    currentConnector?.name,
+                    3,
+                    error.message
+                  )}
+                {/each}
+              </Callout>
+            {/if}
+          </div>
+        {/if}
+      </div>
+    </WorkspaceBody>
     <Inspector>
       <SourceInspector {sourceName} />
     </Inspector>
