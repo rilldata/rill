a2331ad379b8146a98ae9024d0252eeab04abeef
Eric P Green <ericpgreen2@gmail.com>
2023-01-20 15:52:23 -0800

    Remove unused function

--- web-common/src/features/entity-management/getNextEntityId.ts
+++ web-common/src/features/entity-management/getNextEntityId.ts
@@ -1,18 +1,3 @@
-import type { EntityRecord } from "@rilldata/web-common/features/entity-management/types";
-
-export function getNextEntityId(
-  entities: Array<EntityRecord>,
-  entityId: string
-): string {
-  if (entities.length === 1) return undefined;
-  const idx = entities.findIndex((entity) => entity.id === entityId);
-  if (idx === 0) {
-    return entities[idx + 1].id;
-  } else {
-    return entities[idx - 1].id;
-  }
-}
-
 export function getNextEntityName(
   entityNames: Array<string>,
   entityName: string
