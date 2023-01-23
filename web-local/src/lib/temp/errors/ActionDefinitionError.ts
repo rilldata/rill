3361ca63243594d2903b47e92b260fa1b4167f0b
Eric P Green <ericpgreen2@gmail.com>
2023-01-23 13:31:52 -0800

    Remove old error code

--- web-local/src/lib/temp/errors/ActionDefinitionError.ts
+++
@@ -1,6 +0,0 @@
-import { ActionErrorType } from "../data-modeler-service/response/ActionResponseMessage";
-import type { TypedError } from "./TypedError";
-
-export class ActionDefinitionError extends Error implements TypedError {
-  public readonly errorType = ActionErrorType.ActionDefinition;
-}
