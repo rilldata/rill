58d38075fe0df6f9bebae1b180a15a16ffb46353
anshul khandelwal <anshulkhandelwal94@gmail.com>
2023-02-03 18:09:10 +0530

    json support in sources

--- runtime/drivers/duckdb/connectors.go
+++ runtime/drivers/duckdb/connectors.go
@@ -144,6 +144,8 @@ func sourceReader(paths []string, csvDelimiter, format string, hivePartition int
 		return sourceReaderWithDelimiter(paths, csvDelimiter), nil
 	} else if strings.Contains(format, ".parquet") {
 		return fmt.Sprintf("read_parquet(['%s'], HIVE_PARTITIONING=%v)", strings.Join(paths, "','"), hivePartition), nil
+	} else if strings.Contains(format, "json") {
+		return fmt.Sprintf("read_json_objects(['%s'])", strings.Join(paths, ",")), nil
 	} else {
 		return "", fmt.Errorf("file type not supported : %s", format)
 	}
