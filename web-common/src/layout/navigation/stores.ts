d69793ff426602a80cc6991cd7f99eeddf0e678b
Hamilton Ulmer <hamilton.ulmer@gmail.com>
2023-02-04 07:11:45 -0800

    handle all other goto cases by reactively updating currentHref if the path doesn't quite match.

--- web-common/src/layout/navigation/stores.ts
+++ web-common/src/layout/navigation/stores.ts
@@ -1,7 +1,21 @@
-import { writable } from "svelte/store";
+import { page } from "$app/stores";
+import { get, writable } from "svelte/store";
+
 /** maintains the global state across all navigation entries.
  * We use this store to track immediate clicks, rather than use the page store
  * from sveltekit. This is because the page store is updated after the navigation
  * while we need to capture immediate clicks.
  */
 export const currentHref = writable<string>(undefined);
+
+page.subscribe((pageState) => {
+  const href = get(currentHref);
+  const pathname = pageState?.url?.pathname;
+  // This is a hack to prevent the currentHref from being set to undefined.
+  if (!pathname) return;
+  // look at only the first two segments of the path. We map all file type views to these two for now.
+  // later, we will have better decision rules for reactively setting this variable.
+  const segments = pathname.split("/").slice(0, 3);
+  const path = segments.join("/");
+  if (href !== path) currentHref.set(path);
+});
