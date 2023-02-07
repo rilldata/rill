5c0c629d7c34d882bba71b408d4ce2d46f1901a4
Eric P Green <ericpgreen2@gmail.com>
2023-02-07 13:32:47 -0800

    Press `enter` to submit custom time range

--- web-common/src/features/dashboards/time-controls/CustomTimeRangeInput.svelte
+++ web-common/src/features/dashboards/time-controls/CustomTimeRangeInput.svelte
@@ -6,23 +6,39 @@
 
   let start: string;
   let end: string;
+
+  function applyCustomTimeRange() {
+    dispatch("apply", { startDate: start, endDate: end });
+  }
+
+  function handleKeydown(event: KeyboardEvent) {
+    if (event.key === "Enter") {
+      applyCustomTimeRange();
+    }
+  }
 </script>
 
 <div class="flex flex-col gap-y-3 my-3 px-3">
   <div class="flex flex-col gap-y-1">
     <label for="start-date" style="font-size: 10px;">Start date</label>
-    <input bind:value={start} type="datetime-local" name="start-date" />
+    <input
+      bind:value={start}
+      type="datetime-local"
+      name="start-date"
+      on:keydown={handleKeydown}
+    />
   </div>
   <div class="flex flex-col gap-y-1">
     <label for="end-date" style="font-size: 10px;">End date</label>
-    <input bind:value={end} type="datetime-local" name="end-date" />
+    <input
+      bind:value={end}
+      type="datetime-local"
+      name="end-date"
+      on:keydown={handleKeydown}
+    />
   </div>
   <div class="flex">
     <div class="flex-grow" />
-    <Button
-      type="primary"
-      on:click={() => dispatch("apply", { startDate: start, endDate: end })}
-      >Apply</Button
-    >
+    <Button type="primary" on:click={applyCustomTimeRange}>Apply</Button>
   </div>
 </div>
