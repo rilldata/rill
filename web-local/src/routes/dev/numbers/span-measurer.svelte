86481b717676446118640b35a98e089a59c35f61
brendan colloran <brendan@colloran.net>
2023-02-10 06:15:17 -0800

    rename SpanMeasurer and add examples

---
+++ web-local/src/routes/dev/numbers/span-measurer.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  let container;
+  let pxWidth: number;
+
+  onMount(() => {
+    let rect = container.getBoundingClientRect();
+    // pxWidth = Math.ceil(rect.right - rect.left);
+    pxWidth = rect.right - rect.left;
+  });
+</script>
+
+<div>
+  <span bind:this={container} style="outline: solid 1px #ccc;"><slot /></span>
+  <span> -- {pxWidth}px</span>
+</div>
