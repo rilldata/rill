f83b6cd47f046d5f8d9fa06f5fa85a2c07cd03ef
Aditya Hegde <adityahegderocks@gmail.com>
2023-02-10 21:05:21 +0530

    Model page improvements

--- web-local/src/routes/(application)/model/[name]/+page.ts
+++ web-local/src/routes/(application)/model/[name]/+page.ts
@@ -1,6 +1,3 @@
-import { getFilePathFromNameAndType } from "@rilldata/web-common/features/entity-management/entity-mappers";
-import { EntityType } from "@rilldata/web-common/features/entity-management/types";
-import { runtimeServiceGetFile } from "@rilldata/web-common/runtime-client";
 import { runtimeServiceGetConfig } from "@rilldata/web-common/runtime-client/manual-clients";
 import { error } from "@sveltejs/kit";
 
@@ -10,27 +7,13 @@ export const ssr = false;
 export async function load({ params, url }) {
   /** If ?focus, tell the page to focus the editor as soon as available */
   const focusEditor = url.searchParams.get("focus") === "";
-  try {
-    const config = await runtimeServiceGetConfig();
-    if (config.readonly) {
-      throw error(404, "Page not found");
-    }
-
-    await runtimeServiceGetFile(
-      config.instance_id,
-      getFilePathFromNameAndType(params.name, EntityType.Model)
-    );
-
-    return {
-      modelName: params.name,
-      focusEditor,
-    };
-  } catch (e) {
-    if (e.response?.status && e.response?.data?.message) {
-      throw error(e.response.status, e.response.data.message);
-    } else {
-      console.error(e);
-      throw error(500, e.message);
-    }
+  const config = await runtimeServiceGetConfig();
+  if (config.readonly) {
+    throw error(404, "Page not found");
   }
+
+  return {
+    modelName: params.name,
+    focusEditor,
+  };
 }
