3e75e67211f43d0e8e8d9951522c1af83284cd47
Benjamin Egelund-Müller <b@egelund-muller.com>
2023-02-16 21:37:29 +0100

    Add error test cases for duration

--- runtime/pkg/duration/duration_test.go
+++ runtime/pkg/duration/duration_test.go
@@ -14,9 +14,10 @@ func TestParseISO8601(t *testing.T) {
 	}{
 		{from: "P2W", expected: Duration{Week: 2}},
 		{from: "P1Y2WT5M", expected: Duration{Year: 1, Week: 2, Minute: 5}},
+		{from: "P1X", err: true},
 		{from: "inf", expected: Duration{Inf: true}},
 		{from: "Inf", expected: Duration{Inf: true}},
-		{from: "infinity", expected: Duration{Inf: false}},
+		{from: "infinity", err: true},
 	}
 	for _, tt := range tests {
 		got, err := ParseISO8601(tt.from)
