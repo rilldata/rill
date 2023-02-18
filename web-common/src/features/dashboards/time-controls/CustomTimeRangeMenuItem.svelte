bfe1d5b5ae9695d3ecd93dc08bcf613234338bb8
Hamilton Ulmer <hamilton.ulmer@gmail.com>
2023-02-18 15:37:07 -0800

    clean up CustomTimeRangeMenuItem.svelte

--- web-common/src/features/dashboards/time-controls/CustomTimeRangeMenuItem.svelte
+++ web-common/src/features/dashboards/time-controls/CustomTimeRangeMenuItem.svelte
@@ -7,12 +7,10 @@
 </script>
 
 <MenuItem animateSelect={false} on:select>
-  <div
-    class="w-full flex flex-row flex-grow items-center transition-tranform duration-100"
-  >
+  <div class="w-full flex flex-row flex-grow items-center">
     <div>{TimeRangeName.Custom}</div>
     <div class="flex-grow" />
-    <div class="transition-transform" class:-rotate-180={open}>
+    <div class="transition-transform duration-100" class:-rotate-180={open}>
       <CaretDownIcon size="16px" />
     </div>
   </div>
