3514635bc2c770cb5470ef7344074e4055641ee9
Hamilton Ulmer <hamilton.ulmer@gmail.com>
2023-02-23 12:33:09 -0800

    adds FormattedSelectorText.svelte

---
+++ web-common/src/features/metrics-views/workspace/config-parameters/FormattedSelectorText.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { SELECTOR_BUTTON_TEXT_CLASSES } from "../styles";
+
+  export let value: string;
+  export let selected = true;
+</script>
+
+<span
+  style:max-width="14em"
+  class={selected
+    ? SELECTOR_BUTTON_TEXT_CLASSES.selected
+    : SELECTOR_BUTTON_TEXT_CLASSES.unselected}
+>
+  {value}
+</span>
