a5c1b4d407fa60ca2dd28af3de8935a6a1dd3639
Eric P Green <ericpgreen2@gmail.com>
2023-02-27 00:01:00 -0800

    Add runtime request interceptor to set host

--- web-common/src/runtime-client/http-client.ts
+++ web-common/src/runtime-client/http-client.ts
@@ -1,19 +1,36 @@
 import type { FetchWrapperOptions } from "@rilldata/web-local/lib/util/fetchWrapper";
+import { get } from "svelte/store";
 import { HttpRequestQueue } from "./http-request-queue/HttpRequestQueue";
+import { runtime } from "./runtime-store";
 
-let RuntimeUrl = "";
-try {
-  RuntimeUrl = RILL_RUNTIME_URL;
-} catch (e) {
-  // no-op
-}
+/**
+ * Runtime base URL
+ *  Local
+ *    In dev & prod: http://localhost:9009
+ *  Cloud
+ *    In dev: http://localhost:9009
+ *    In prod: https://{region}.runtime.rilldata.com
+ */
 
-export const httpRequestQueue = new HttpRequestQueue(RuntimeUrl);
+const httpRequestQueues = new Map<string, HttpRequestQueue>();
 
 export const httpClient = async <T>(
   config: FetchWrapperOptions
 ): Promise<T> => {
-  return (await httpRequestQueue.add(config)) as Promise<T>;
+  // naive request interceptor
+  const host = get(runtime).host;
+  const interceptedConfig = { ...config, baseUrl: host };
+
+  const httpRequestQueue = getHttpRequestQueueForHost(host);
+  return (await httpRequestQueue.add(interceptedConfig)) as Promise<T>;
 };
+
+export function getHttpRequestQueueForHost(host: string): HttpRequestQueue {
+  if (!httpRequestQueues.has(host)) {
+    httpRequestQueues.set(host, new HttpRequestQueue(host));
+  }
+
+  return httpRequestQueues.get(host);
+}
 
 export default httpClient;
