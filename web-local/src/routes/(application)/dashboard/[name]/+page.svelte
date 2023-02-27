7fd18bdfedeabf323fa1e21bb91080e25d32047c
Eric P Green <ericpgreen2@gmail.com>
2023-02-27 00:02:57 -0800

    Move routing logic out of `+page.ts` given no SSR

--- web-local/src/routes/(application)/dashboard/[name]/+page.svelte
+++ web-local/src/routes/(application)/dashboard/[name]/+page.svelte
@@ -1,13 +1,57 @@
 <script lang="ts">
+  import { page } from "$app/stores";
   import { DashboardWorkspace } from "@rilldata/web-common/features/dashboards";
+  import { getFilePathFromNameAndType } from "@rilldata/web-common/features/entity-management/entity-mappers";
+  import { EntityType } from "@rilldata/web-common/features/entity-management/types";
+  import {
+    useRuntimeServiceGetCatalogEntry,
+    useRuntimeServiceGetFile,
+  } from "@rilldata/web-common/runtime-client";
+  import { runtime } from "@rilldata/web-common/runtime-client/runtime-store";
+  import { error, redirect } from "@sveltejs/kit";
+  import { featureFlags } from "../../../../lib/application-state-stores/application-store";
+  import { CATALOG_ENTRY_NOT_FOUND } from "../../../../lib/errors/messages";
 
-  export let data;
+  const metricViewName: string = $page.params.name;
 
-  $: metricViewName = data.metricViewName;
+  const fileQuery = useRuntimeServiceGetFile(
+    $runtime.instanceId,
+    getFilePathFromNameAndType(metricViewName, EntityType.MetricsDefinition),
+    {
+      query: {
+        onError: (err) => {
+          if (err.response?.data?.message.includes(CATALOG_ENTRY_NOT_FOUND)) {
+            throw error(404, "Dashboard not found");
+          }
+
+          throw error(err.response?.status || 500, err.message);
+        },
+      },
+    }
+  );
+
+  const catalogQuery = useRuntimeServiceGetCatalogEntry(
+    $runtime.instanceId,
+    metricViewName,
+    {
+      query: {
+        onError: () => {
+          // When the catalog entry doesn't exist, the dashboard config is invalid
+          if ($featureFlags.readOnly) {
+            throw error(400, "Invalid dashboard");
+          }
+
+          throw redirect(307, `/dashboard/${metricViewName}/edit`);
+        },
+      },
+    }
+  );
 </script>
 
 <svelte:head>
   <title>Rill Developer | {metricViewName}</title>
 </svelte:head>
 
-<DashboardWorkspace {metricViewName} />
+{#if $fileQuery.data && $catalogQuery.data}
+  <DashboardWorkspace {metricViewName} />
+{/if}
--- web-local/src/routes/(application)/model/[name]/+page.svelte
+++ web-local/src/routes/(application)/model/[name]/+page.svelte
@@ -1,14 +1,47 @@
 <script lang="ts">
+  import { page } from "$app/stores";
+  import { getFilePathFromNameAndType } from "@rilldata/web-common/features/entity-management/entity-mappers";
+  import { EntityType } from "@rilldata/web-common/features/entity-management/types";
   import { ModelWorkspace } from "@rilldata/web-common/features/models";
+  import { useRuntimeServiceGetFile } from "@rilldata/web-common/runtime-client";
+  import { runtime } from "@rilldata/web-common/runtime-client/runtime-store";
+  import { error } from "@sveltejs/kit";
+  import { onMount } from "svelte";
+  import { featureFlags } from "../../../../lib/application-state-stores/application-store";
 
-  export let data;
+  const modelName: string = $page.params.name;
+
+  /** If ?focus, tell the page to focus the editor as soon as available */
+  const focusEditor = $page.url.searchParams.get("focus") === "";
+
+  onMount(() => {
+    if ($featureFlags.readOnly) {
+      throw error(404, "Page not found");
+    }
+  });
+
+  const fileQuery = useRuntimeServiceGetFile(
+    $runtime.instanceId,
+    getFilePathFromNameAndType(modelName, EntityType.Model),
+    {
+      query: {
+        onError: (err) => {
+          if (err.response?.status && err.response?.data?.message) {
+            throw error(err.response.status, err.response.data.message);
+          } else {
+            console.error(err);
+            throw error(500, err.message);
+          }
+        },
+      },
+    }
+  );
 </script>
 
 <svelte:head>
-  <title>Rill Developer | {data.modelName}</title>
+  <title>Rill Developer | {modelName}</title>
 </svelte:head>
 
-<ModelWorkspace
-  modelName={data.modelName}
-  focusEditorOnMount={data.focusEditor}
-/>
+{#if $fileQuery.data}
+  <ModelWorkspace {modelName} focusEditorOnMount={focusEditor} />
+{/if}
