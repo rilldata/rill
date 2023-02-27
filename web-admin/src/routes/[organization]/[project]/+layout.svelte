2fe0c26df61c62a630a06de38d758fb661c4bc22
Eric P Green <ericpgreen2@gmail.com>
2023-02-27 00:17:57 -0800

    Use `RuntimeProvider` in Rill Cloud

---
+++ web-admin/src/routes/[organization]/[project]/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import RuntimeProvider from "@rilldata/web-common/runtime-client/RuntimeProvider.svelte";
+
+  const org = $page.params.organization;
+  const project = $page.params.project;
+
+  // TODO: call the admin server
+  $: runtimeHost = undefined;
+  $: runtimeInstanceId = undefined;
+</script>
+
+<RuntimeProvider host={runtimeHost} instanceId={runtimeInstanceId}>
+  <slot />
+</RuntimeProvider>
