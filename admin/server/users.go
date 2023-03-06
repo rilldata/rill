a7691e518a25f3f74d98bbdf22fb8f0987eab670
Benjamin Egelund-Müller <b@egelund-muller.com>
2023-03-06 11:14:47 +0100

    Return null for current user if anon

--- admin/server/users.go
+++ admin/server/users.go
@@ -12,10 +12,19 @@ import (
 
 func (s *Server) GetCurrentUser(ctx context.Context, req *adminv1.GetCurrentUserRequest) (*adminv1.GetCurrentUserResponse, error) {
 	claims := auth.GetClaims(ctx)
-	if ent, ok := claims.OwnerEntity(); !ok || ent != database.EntityUser {
-		return nil, fmt.Errorf("not an authenticated user")
+
+	// Return an empty result if not authenticated.
+	ent, ok := claims.OwnerEntity()
+	if !ok {
+		return &adminv1.GetCurrentUserResponse{}, nil
 	}
 
+	// Error if authenticated as anything other than a user
+	if ent != database.EntityUser {
+		return nil, fmt.Errorf("not authenticated as a user")
+	}
+
+	// Owner is a user
 	u, err := s.admin.DB.FindUser(ctx, claims.OwnerID())
 	if err != nil {
 		return nil, err
