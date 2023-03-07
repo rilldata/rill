c7acc0e0b45474ad444381c70cf76ddee1f0554b
Eric P Green <ericpgreen2@gmail.com>
2023-03-07 17:12:57 +0100

    Allow credentials in requests to admin server (#1880)

--- admin/server/server.go
+++ admin/server/server.go
@@ -195,6 +195,7 @@ func cors(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if origin := r.Header.Get("Origin"); origin != "" {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
 			if r.Method == "OPTIONS" && r.Header.Get("Access-Control-Request-Method") != "" {
 				w.Header().Set("Access-Control-Allow-Headers", "*")
 				w.Header().Set("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, PATCH, DELETE")
