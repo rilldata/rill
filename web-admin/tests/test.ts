cb5dd7511621d3731afd2ad8db8789810e5ef46e
Himadri Singh <himadri.singh@gmail.com>
2023-03-09 15:22:09 +0530

    conditions

--- web-admin/tests/test.ts
+++ web-admin/tests/test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from "@playwright/test";
 
 test("empty test", async () => {
-  // #TODO: Run admin behind the scenes to properly test this module
+  // TODO: Run admin behind the scenes to properly test this module
   expect("hello world").toBe("hello world");
 });
