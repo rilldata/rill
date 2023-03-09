b192a99fad27196fa391947407acdf698b1357b5
Himadri Singh <himadri.singh@gmail.com>
2023-03-09 14:40:25 +0530

    fix lint
    ```
    error  'project' is assigned a value but never used  @typescript-eslint/no-unused-vars
    error  'org' is assigned a value but never used      @typescript-eslint/no-unused-vars
    ```

--- web-admin/src/routes/[organization]/[project]/+layout.svelte
+++ web-admin/src/routes/[organization]/[project]/+layout.svelte
@@ -2,9 +2,6 @@
   import { page } from "$app/stores";
   import RuntimeProvider from "@rilldata/web-common/runtime-client/RuntimeProvider.svelte";
 
-  const org = $page.params.organization;
-  const project = $page.params.project;
-
   // TODO: call the admin server
   $: runtimeHost = undefined;
   $: runtimeInstanceId = undefined;
