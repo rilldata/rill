51e68fa0fc8bacd33659c2840a03b43190e5da2a
Rakesh Sharma <rakeshsharma14317@gmail.com>
2023-03-15 21:14:40 +0530

    rill org switch with and without args

--- cli/cmd/org/switch.go
+++ cli/cmd/org/switch.go
@@ -16,6 +16,7 @@ func SwitchCmd(cfg *config.Config) *cobra.Command {
 	switchCmd := &cobra.Command{
 		Use:   "switch",
 		Short: "Switch",
+		Args:  cobra.MaximumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			client, err := client.New(cfg.AdminURL, cfg.AdminToken())
 			if err != nil {
@@ -23,22 +24,33 @@ func SwitchCmd(cfg *config.Config) *cobra.Command {
 			}
 			defer client.Close()
 
-			res, err := client.ListOrganizations(context.Background(), &adminv1.ListOrganizationsRequest{})
-			if err != nil {
-				return err
-			}
+			var defaultOrg string
+			if len(args) == 0 {
+				res, err := client.ListOrganizations(context.Background(), &adminv1.ListOrganizationsRequest{})
+				if err != nil {
+					return err
+				}
 
-			if len(res.Organizations) < 1 {
-				fmt.Println("No organizations found, run `rill org create` first.")
-				return nil
-			}
+				if len(res.Organizations) < 1 {
+					fmt.Println("No organizations found, run `rill org create` first.")
+					return nil
+				}
 
-			var orgNames []string
-			for _, org := range res.Organizations {
-				orgNames = append(orgNames, org.Name)
-			}
+				var orgNames []string
+				for _, org := range res.Organizations {
+					orgNames = append(orgNames, org.Name)
+				}
 
-			defaultOrg := cmdutil.PromptGetSelect(orgNames, "Select default org.")
+				defaultOrg = cmdutil.PromptGetSelect(orgNames, "Select default org.")
+			} else {
+				_, err = client.GetOrganization(context.Background(), &adminv1.GetOrganizationRequest{
+					Name: args[0],
+				})
+				if err != nil {
+					return err
+				}
+				defaultOrg = args[0]
+			}
 
 			err = dotrill.SetDefaultOrg(defaultOrg)
 			if err != nil {
