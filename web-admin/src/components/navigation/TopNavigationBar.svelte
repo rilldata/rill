f33b8a4ad071a24b61e08651fafc792ac294a7dc
Eric P Green <ericpgreen2@gmail.com>
2023-03-15 16:23:15 +0100

    Create `[project]/deployment` page for status and logs (#1939)
    * Fix dashboard breadcrumb
    
    * Allow content overflow
    
    * Add deployment status to top bar
    
    * Add `[project]/deployment` page
    
    * Adjust layout

--- web-admin/src/components/navigation/TopNavigationBar.svelte
+++ web-admin/src/components/navigation/TopNavigationBar.svelte
@@ -1,9 +1,13 @@
 <script>
+  import { page } from "$app/stores";
   import RillLogo from "@rilldata/web-common/components/icons/RillLogo.svelte";
   import { useAdminServiceGetCurrentUser } from "../../client";
   import SignIn from "../authentication/SignIn.svelte";
   import UserButton from "../authentication/UserButton.svelte";
+  import DeploymentStatusChip from "../deployments/DeploymentStatusChip.svelte";
   import Breadcrumbs from "./Breadcrumbs.svelte";
+
+  $: project = $page.params.project;
 
   const userQuery = useAdminServiceGetCurrentUser();
   $: signedIn = !!$userQuery.data?.user;
@@ -14,6 +18,11 @@
     <RillLogo iconOnly size={"2.25em"} />
   </a>
   <Breadcrumbs />
+  {#if project}
+    <div class="ml-3">
+      <DeploymentStatusChip />
+    </div>
+  {/if}
   <div class="flex-grow" />
   {#if signedIn}
     <UserButton />
