389cb7df15c91369e46c2c9fe2f59611b73f5635
Hamilton Ulmer <hamilton.ulmer@gmail.com>
2023-03-16 12:36:13 -0700

    add global def

--- web-local/src/routes/(application)/+layout.svelte
+++ web-local/src/routes/(application)/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  /*global  RILL_RUNTIME_URL */
   import { RillDeveloperLayout } from "@rilldata/web-common/layout";
   import RuntimeProvider from "@rilldata/web-common/runtime-client/RuntimeProvider.svelte";
   import { QueryClientProvider } from "@sveltestack/svelte-query";
@@ -9,6 +10,8 @@
   // Note: this right hand side is set by Vite at build time.
   const rillRuntimeURL = RILL_RUNTIME_URL;
 </script>
+
+/*global RILL_RUNTIME_URL */
 
 <QueryClientProvider client={queryClient}>
   <RuntimeProvider host={rillRuntimeURL} instanceId="default">
