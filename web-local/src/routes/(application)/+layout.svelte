9ba7ad225dd35619261c0dc55ad24b95c78743a0
Hamilton Ulmer <hamilton.ulmer@gmail.com>
2023-03-16 12:39:17 -0700

    moves to templated ts runtime url

--- web-local/src/routes/(application)/+layout.svelte
+++ web-local/src/routes/(application)/+layout.svelte
@@ -1,20 +1,17 @@
 <script lang="ts">
-  /*global  RILL_RUNTIME_URL */
   import { RillDeveloperLayout } from "@rilldata/web-common/layout";
   import RuntimeProvider from "@rilldata/web-common/runtime-client/RuntimeProvider.svelte";
+  import { RuntimeUrl } from "@rilldata/web-local/lib/application-state-stores/initialize-node-store-contexts";
   import { QueryClientProvider } from "@sveltestack/svelte-query";
   import { createQueryClient } from "../../lib/svelte-query/globalQueryClient";
 
   const queryClient = createQueryClient();
 
   // Note: this right hand side is set by Vite at build time.
-  const rillRuntimeURL = RILL_RUNTIME_URL;
 </script>
 
-/*global RILL_RUNTIME_URL */
-
 <QueryClientProvider client={queryClient}>
-  <RuntimeProvider host={rillRuntimeURL} instanceId="default">
+  <RuntimeProvider host={RuntimeUrl} instanceId="default">
     <RillDeveloperLayout>
       <slot />
     </RillDeveloperLayout>
