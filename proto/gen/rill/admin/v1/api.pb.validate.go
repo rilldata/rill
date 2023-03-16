2656af6d404a42794ce8035e1f6552cc770accaa
rakeshsharma14317 <rakeshsharma14317@gmail.com>
2023-03-16 22:16:16 +0530

    Added drop down list for login and switch (#1938)
    * added drop down list for login and switch
    
    * PR chanegs
    
    * linter issue
    
    * gen/ui issue
    
    * pull request changes
    
    * rill org switch with and without
    args

--- proto/gen/rill/admin/v1/api.pb.validate.go
+++ proto/gen/rill/admin/v1/api.pb.validate.go
@@ -392,7 +392,7 @@ func (m *ListOrganizationsResponse) validate(all bool) error {
 
 	var errors []error
 
-	for idx, item := range m.GetOrganization() {
+	for idx, item := range m.GetOrganizations() {
 		_, _ = idx, item
 
 		if all {
@@ -400,7 +400,7 @@ func (m *ListOrganizationsResponse) validate(all bool) error {
 			case interface{ ValidateAll() error }:
 				if err := v.ValidateAll(); err != nil {
 					errors = append(errors, ListOrganizationsResponseValidationError{
-						field:  fmt.Sprintf("Organization[%v]", idx),
+						field:  fmt.Sprintf("Organizations[%v]", idx),
 						reason: "embedded message failed validation",
 						cause:  err,
 					})
@@ -408,7 +408,7 @@ func (m *ListOrganizationsResponse) validate(all bool) error {
 			case interface{ Validate() error }:
 				if err := v.Validate(); err != nil {
 					errors = append(errors, ListOrganizationsResponseValidationError{
-						field:  fmt.Sprintf("Organization[%v]", idx),
+						field:  fmt.Sprintf("Organizations[%v]", idx),
 						reason: "embedded message failed validation",
 						cause:  err,
 					})
@@ -417,7 +417,7 @@ func (m *ListOrganizationsResponse) validate(all bool) error {
 		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
 			if err := v.Validate(); err != nil {
 				return ListOrganizationsResponseValidationError{
-					field:  fmt.Sprintf("Organization[%v]", idx),
+					field:  fmt.Sprintf("Organizations[%v]", idx),
 					reason: "embedded message failed validation",
 					cause:  err,
 				}
