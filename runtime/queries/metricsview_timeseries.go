76a6d716f1fb313991c04a228222068e18d4c74c
Benjamin Egelund-Müller <b@egelund-muller.com>
2023-03-23 15:09:15 +0530

    Timeseries query for Druid

--- runtime/queries/metricsview_timeseries.go
+++ runtime/queries/metricsview_timeseries.go
@@ -4,10 +4,13 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"strings"
+	"time"
 
 	runtimev1 "github.com/rilldata/rill/proto/gen/rill/runtime/v1"
 	"github.com/rilldata/rill/runtime"
 	"github.com/rilldata/rill/runtime/drivers"
+	"github.com/rilldata/rill/runtime/pkg/pbutil"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
@@ -53,15 +56,6 @@ func (q *MetricsViewTimeSeries) UnmarshalResult(v any) error {
 }
 
 func (q *MetricsViewTimeSeries) Resolve(ctx context.Context, rt *runtime.Runtime, instanceID string, priority int) error {
-	olap, err := rt.OLAP(ctx, instanceID)
-	if err != nil {
-		return err
-	}
-
-	if olap.Dialect() != drivers.DialectDuckDB {
-		return fmt.Errorf("not available for dialect '%s'", olap.Dialect())
-	}
-
 	mv, err := lookupMetricsView(ctx, rt, instanceID, q.MetricsViewName)
 	if err != nil {
 		return err
@@ -71,7 +65,23 @@ func (q *MetricsViewTimeSeries) Resolve(ctx context.Context, rt *runtime.Runtime
 		return fmt.Errorf("metrics view '%s' does not have a time dimension", q.MetricsViewName)
 	}
 
-	measures, err := toMeasures(mv.Measures, q.MeasureNames)
+	olap, err := rt.OLAP(ctx, instanceID)
+	if err != nil {
+		return err
+	}
+
+	switch olap.Dialect() {
+	case drivers.DialectDuckDB:
+		return q.resolveDuckDB(ctx, rt, instanceID, mv, priority)
+	case drivers.DialectDruid:
+		return q.resolveDruid(ctx, olap, mv, priority)
+	default:
+		return fmt.Errorf("not available for dialect '%s'", olap.Dialect())
+	}
+}
+
+func (q *MetricsViewTimeSeries) resolveDuckDB(ctx context.Context, rt *runtime.Runtime, instanceID string, mv *runtimev1.MetricsView, priority int) error {
+	measures, err := toColumnTimeseriesMeasures(mv.Measures, q.MeasureNames)
 	if err != nil {
 		return err
 	}
@@ -102,7 +112,7 @@ func (q *MetricsViewTimeSeries) Resolve(ctx context.Context, rt *runtime.Runtime
 	return nil
 }
 
-func toMeasures(measures []*runtimev1.MetricsView_Measure, measureNames []string) ([]*runtimev1.ColumnTimeSeriesRequest_BasicMeasure, error) {
+func toColumnTimeseriesMeasures(measures []*runtimev1.MetricsView_Measure, measureNames []string) ([]*runtimev1.ColumnTimeSeriesRequest_BasicMeasure, error) {
 	var res []*runtimev1.ColumnTimeSeriesRequest_BasicMeasure
 	for _, n := range measureNames {
 		found := false
@@ -120,4 +130,111 @@ func toMeasures(measures []*runtimev1.MetricsView_Measure, measureNames []string
 		}
 	}
 	return res, nil
+}
+
+func (q *MetricsViewTimeSeries) resolveDruid(ctx context.Context, olap drivers.OLAPStore, mv *runtimev1.MetricsView, priority int) error {
+	sql, tsAlias, args, err := q.buildDruidMetricsTimeseriesSQL(mv)
+	if err != nil {
+		return fmt.Errorf("error building query: %w", err)
+	}
+
+	rows, err := olap.Execute(ctx, &drivers.Statement{
+		Query:    sql,
+		Args:     args,
+		Priority: priority,
+	})
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	var data []*runtimev1.TimeSeriesValue
+	for rows.Next() {
+		rowMap := make(map[string]any)
+		err := rows.MapScan(rowMap)
+		if err != nil {
+			return err
+		}
+
+		var t time.Time
+		switch v := rowMap[tsAlias].(type) {
+		case time.Time:
+			t = v
+		default:
+			panic(fmt.Sprintf("unexpected type for timestamp column: %T", v))
+		}
+
+		delete(rowMap, tsAlias)
+		records, err := pbutil.ToStruct(rowMap)
+		if err != nil {
+			return err
+		}
+
+		data = append(data, &runtimev1.TimeSeriesValue{
+			Ts:      timestamppb.New(t),
+			Records: records,
+		})
+	}
+
+	meta := structTypeToMetricsViewColumn(rows.Schema)
+
+	q.Result = &runtimev1.MetricsViewTimeSeriesResponse{
+		Meta: meta,
+		Data: data,
+	}
+
+	return nil
+}
+
+func (q *MetricsViewTimeSeries) buildDruidMetricsTimeseriesSQL(mv *runtimev1.MetricsView) (string, string, []any, error) {
+	selectCols := []string{}
+	for _, n := range q.MeasureNames {
+		found := false
+		for _, m := range mv.Measures {
+			if m.Name == n {
+				expr := fmt.Sprintf(`%s as "%s"`, m.Expression, m.Name)
+				selectCols = append(selectCols, expr)
+				found = true
+				break
+			}
+		}
+		if !found {
+			return "", "", nil, fmt.Errorf("measure does not exist: '%s'", n)
+		}
+	}
+
+	whereClause := "1=1"
+	args := []any{}
+	if q.TimeStart != nil {
+		whereClause += fmt.Sprintf(" AND %s >= ?", safeName(mv.TimeDimension))
+		args = append(args, q.TimeStart.AsTime())
+	}
+	if q.TimeEnd != nil {
+		whereClause += fmt.Sprintf(" AND %s < ?", safeName(mv.TimeDimension))
+		args = append(args, q.TimeEnd.AsTime())
+	}
+
+	if q.Filter != nil {
+		clause, clauseArgs, err := buildFilterClauseForMetricsViewFilter(q.Filter)
+		if err != nil {
+			return "", "", nil, err
+		}
+		whereClause += " " + clause
+		args = append(args, clauseArgs...)
+	}
+
+	tsAlias := tempName("_ts_")
+	tsSpecifier := convertToDateTruncSpecifier(q.TimeGranularity)
+
+	sql := fmt.Sprintf(
+		`SELECT date_trunc('%s', %s) AS %s, %s FROM %q WHERE %s GROUP BY 1 ORDER BY 1`,
+		tsSpecifier,
+		safeName(mv.TimeDimension),
+		tsAlias,
+		strings.Join(selectCols, ", "),
+		mv.Model,
+		whereClause,
+	)
+
+	return sql, tsAlias, args, nil
 }
