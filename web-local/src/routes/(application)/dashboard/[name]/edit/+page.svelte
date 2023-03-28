3a72198e076d34dc7bfec10dea55b6225d42473b
Hamilton Ulmer <hamilton.ulmer@gmail.com>
2023-03-28 07:09:22 -0700

    if yaml file is empty still show editor

--- web-local/src/routes/(application)/dashboard/[name]/edit/+page.svelte
+++ web-local/src/routes/(application)/dashboard/[name]/edit/+page.svelte
@@ -3,10 +3,7 @@
   import { getFilePathFromNameAndType } from "@rilldata/web-common/features/entity-management/entity-mappers";
   import { EntityType } from "@rilldata/web-common/features/entity-management/types";
   import { MetricsWorkspace } from "@rilldata/web-common/features/metrics-views";
-  import {
-    useRuntimeServiceGetCatalogEntry,
-    useRuntimeServiceGetFile,
-  } from "@rilldata/web-common/runtime-client";
+  import { useRuntimeServiceGetFile } from "@rilldata/web-common/runtime-client";
   import { runtime } from "@rilldata/web-common/runtime-client/runtime-store";
   import { error } from "@sveltejs/kit";
   import { onMount } from "svelte";
@@ -40,31 +37,12 @@
   $: yaml = $fileQuery.data?.blob || "";
 
   let nonStandardError: string | undefined;
-  $: catalogQuery = useRuntimeServiceGetCatalogEntry(
-    $runtime.instanceId,
-    metricViewName,
-    {
-      query: {
-        onError: async (err) => {
-          // If the catalog entry doesn't exist, the dashboard config is invalid
-          // The component should render the specific error
-          if (err.response?.data?.message.includes(CATALOG_ENTRY_NOT_FOUND)) {
-            nonStandardError = err.message;
-          }
-
-          // Throw all other errors
-          console.log("big error!!!!");
-          //throw error(err.response?.status || 500, err.message);
-        },
-      },
-    }
-  );
 </script>
 
 <svelte:head>
   <title>Rill Developer | {metricViewName}</title>
 </svelte:head>
 
-{#if $fileQuery.data && yaml}
+{#if $fileQuery.data && yaml !== undefined}
   <MetricsWorkspace metricsDefName={metricViewName} {nonStandardError} {yaml} />
 {/if}
