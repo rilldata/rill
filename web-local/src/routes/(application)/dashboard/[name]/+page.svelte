fb701fb5f54a41c943aa16d268bd307faf55bba7
Aditya Hegde <adityahegderocks@gmail.com>
2023-03-28 21:34:53 +0530

    Fixing build

--- web-local/src/routes/(application)/dashboard/[name]/+page.svelte
+++ web-local/src/routes/(application)/dashboard/[name]/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { goto } from "$app/navigation";
   import { page } from "$app/stores";
   import { Dashboard } from "@rilldata/web-common/features/dashboards";
   import { metricsExplorerStore } from "@rilldata/web-common/features/dashboards/dashboard-stores";
