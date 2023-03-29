7d7bde6f6e6abf43066871cf18c81b3f5a58b76e
Aditya Hegde <adityahegderocks@gmail.com>
2023-03-29 19:41:38 +0530

    enable basic e2e tests for rill developer (#1991)
    * Fix existing tests
    
    * Standardising locators
    
    * Standardise a few more selectors
    
    * Fixing tests
    
    * Add E2E to CI-CD
    
    * Add a note in docs for web-local testing

--- web-local/test/ui/utils/dataSpecifcHelpers.ts
+++ web-local/test/ui/utils/dataSpecifcHelpers.ts
@@ -1,4 +1,8 @@
+import path from "node:path";
 import type { Page } from "playwright";
+import { assertLeaderboards } from "./dashboardHelpers";
+import { waitForProfiling, wrapRetryAssertion } from "./helpers";
+import { createModel, updateModelSql } from "./modelHelpers";
 import { waitForSource } from "./sourceHelpers";
 
 export async function waitForAdBids(page: Page, name: string) {
@@ -7,4 +11,43 @@ export async function waitForAdBids(page: Page, name: string) {
 
 export async function waitForAdImpressions(page: Page, name: string) {
   return waitForSource(page, name, ["city", "country"]);
+}
+
+export async function createAdBidsModel(page: Page) {
+  await createModel(page, "AdBids_model");
+  await Promise.all([
+    waitForProfiling(page, "AdBids_model", [
+      "publisher",
+      "domain",
+      "timestamp",
+    ]),
+    updateModelSql(
+      page,
+      `from "${path.join(__dirname, "../../data", "AdBids.csv")}"`
+    ),
+  ]);
+}
+
+export async function assertAdBidsDashboard(page: Page) {
+  await wrapRetryAssertion(() =>
+    assertLeaderboards(page, [
+      {
+        label: "Publisher",
+        values: ["null", "Facebook", "Google", "Yahoo", "Microsoft"],
+      },
+      {
+        label: "Domain",
+        values: [
+          "facebook.com",
+          "msn.com",
+          "google.com",
+          "news.yahoo.com",
+          "instagram.com",
+          "sports.yahoo.com",
+          "news.google.com",
+        ],
+      },
+    ])
+  );
+  // TODO: how do we assert timeseries?
 }
