41921ff4747e5ac07693de565952e6f3e3a944ee
Dhiraj Kumar <sumankumaribarnwal@gmail.com>
2023-03-31 15:58:38 +0530

    Use preventVerticalOverlap

--- web-common/src/components/data-graphic/marks/MultiMetricMouseoverLabel.svelte
+++ web-common/src/components/data-graphic/marks/MultiMetricMouseoverLabel.svelte
@@ -8,6 +8,7 @@ It is probably not the most up to date code; but it works very well in practice.
 
   import { WithTween } from "../functional-components";
   import type { ScaleStore, SimpleConfigurationStore } from "../state/types";
+  import { preventVerticalOverlap } from "./prevent-vertical-overlap";
 
   interface Point {
     x: number;
@@ -18,6 +19,8 @@ It is probably not the most up to date code; but it works very well in practice.
     labelColorClass?: string;
     labelStyleClass?: string;
     pointColorClass?: string;
+    yOverride?: boolean;
+    yOverrideLabel?: string;
   }
 
   export let point: Point[] = [];
@@ -26,7 +29,8 @@ It is probably not the most up to date code; but it works very well in practice.
   export let xOffset = 0;
   export let fontSize = 11;
   export let xBuffer = 8;
-  export let yBuffer = 3;
+  export let elementHeight = 12;
+  export let yBuffer = 4;
   export let showPoints = true;
   export let showLabels = true;
 
@@ -46,108 +50,32 @@ It is probably not the most up to date code; but it works very well in practice.
   export let direction = "right";
   export let flipAtEdge: "body" | "graphic" | false = "graphic"; // "body", "graphic", or undefined
 
-  // FIXME – we should replace this with preventVerticalOverlap!
-  function toLocations(pt, xs, ys, left, right, top, bottom, elementHeight) {
-    // this is where the boundary condition lives.
-    let locations = [
-      ...pt.map((p) => ({
-        ...p,
-        xRange: Math.max(left, xs(p.x)) || 0,
-        yRange: ys(p.y),
-      })),
-    ];
-    // sort order makes all the difference here
-    locations.sort((a, b) => {
-      if (a.y < b.y) return 1;
-      if (a.y > b.y) return -1;
-      return 0;
-    });
-    if (locations.length === 1) {
-      locations[0].yRange = Math.min(
-        bottom,
-        Math.max(top, locations[0].yRange)
-      );
-      return locations;
-    }
-    if (!locations.length) return locations;
-
-    const middle = ~~(locations.length / 2); // eslint-disable-line
-
-    // STEP 1: inside up to top label.
-    let i = middle;
-    while (i >= 0) {
-      if (i !== middle) {
-        const diff = locations[i + 1].yRange - locations[i].yRange;
-        if (diff <= elementHeight + yBuffer) {
-          locations[i].yRange -= elementHeight + yBuffer - diff;
-        }
-      }
-      i -= 1;
-    }
-
-    // STEP 2: top label shuffle down to reasonable place, shift to middle.
-    if (locations[0].yRange < top + yBuffer) {
-      locations[0].yRange = top + yBuffer;
-      i = 0;
-      while (i < middle) {
-        const diff = locations[i + 1].yRange - locations[i].yRange;
-        if (diff <= elementHeight + yBuffer) {
-          locations[i + 1].yRange += elementHeight + yBuffer - diff;
-        }
-        i += 1;
-      }
-    }
-
-    // STEP 3: inside down to bottom label;
-    i = middle;
-    while (i < locations.length) {
-      if (i !== middle) {
-        const diff = locations[i].yRange - locations[i - 1].yRange;
-        if (diff < elementHeight + yBuffer) {
-          locations[i].yRange += elementHeight + yBuffer - diff;
-        }
-      }
-      i += 1;
-    }
-    if (locations[locations.length - 1].yRange > bottom - yBuffer) {
-      locations[locations.length - 1].yRange = bottom - yBuffer;
-      i = locations.length - 1;
-      while (i > 0) {
-        const diff = locations[i].yRange - locations[i - 1].yRange;
-        if (diff <= fontSize + yBuffer) {
-          locations[i - 1].yRange -= elementHeight + yBuffer - diff;
-        }
-        i -= 1;
-      }
-    }
-    return locations;
-  }
-
-  let locations = toLocations(
-    point,
-    $xScale,
-    $yScale,
-    plotLeft,
-    plotRight,
-    plotTop,
-    plotBottom,
-    fontSize
-  );
   let container;
   let containerWidths = [];
   // let labelWidth = 0;
 
   // update locations.
-  $: locations = toLocations(
-    point,
-    $xScale,
-    $yScale,
-    plotLeft,
-    plotRight,
+  $: nonOverlappingLocations = preventVerticalOverlap(
+    point.map((p, i) => ({
+      key: `${p.x}-${p.y}-${i}`,
+      value: $yScale(p.y),
+    })),
     plotTop,
     plotBottom,
-    fontSize
+    elementHeight,
+    yBuffer
   );
+
+  $: locations = point.map((p, i) => {
+    const key = `${p.x}-${p.y}-${i}`;
+    return {
+      ...p,
+      key,
+      xRange: $xScale(p.x),
+      yRange: nonOverlappingLocations.find((l) => l.key === key).value,
+    };
+  });
+
   // update containerWidths. We keep track of the last 6 points.
   $: if (container && locations) {
     containerWidths = [
@@ -216,7 +144,7 @@ It is probably not the most up to date code; but it works very well in practice.
       );
 
       textWidths = Array.from(container.querySelectorAll(".text-elements")).map(
-        (q) => q.getBoundingClientRect().width
+        (q: SVGElement) => q.getBoundingClientRect().width
       );
       if (!Number.isFinite(labelWidth)) {
         labelWidth = 0;
@@ -228,7 +156,7 @@ It is probably not the most up to date code; but it works very well in practice.
 <g bind:this={container}>
   {#if showLabels}
     {#each locations as location, i (location.key || location.label)}
-      {#if (location.y || location.rangeY) && (location.x || location.rangeX)}
+      {#if (location.y || location.yRange) && (location.x || location.xRange)}
         <WithTween
           value={{
             y: location.yRange || 0,
