df509e2974ab9ed7e46dc0d270be4100b5e59248
anshul khandelwal <anshulkhandelwal94@gmail.com>
2023-04-04 10:49:41 +0530

    fix

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -8,7 +8,7 @@ import (
 	"github.com/rilldata/rill/cli/cmd/admin"
 	"github.com/rilldata/rill/cli/cmd/auth"
 	"github.com/rilldata/rill/cli/cmd/build"
-	"github.com/rilldata/rill/cli/cmd/connect"
+	"github.com/rilldata/rill/cli/cmd/deploy"
 	"github.com/rilldata/rill/cli/cmd/docs"
 	"github.com/rilldata/rill/cli/cmd/initialize"
 	"github.com/rilldata/rill/cli/cmd/org"
@@ -87,7 +87,7 @@ func runCmd(ctx context.Context, ver config.Version) error {
 	adminCmds := []*cobra.Command{
 		org.OrgCmd(cfg),
 		project.ProjectCmd(cfg),
-		connect.ConnectCmd(cfg),
+		deploy.DeployCmd(cfg),
 	}
 	for _, cmd := range adminCmds {
 		cmd.PersistentFlags().StringVar(&cfg.AdminURL, "api-url", "https://admin.rilldata.io", "Base URL for the admin API")
