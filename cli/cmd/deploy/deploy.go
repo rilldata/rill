4660c00d0c1c158473b2765ff1447859128a12f7
anshul khandelwal <anshulkhandelwal94@gmail.com>
2023-04-06 18:51:02 +0530

    removing connect page

--- cli/cmd/deploy/deploy.go
+++ cli/cmd/deploy/deploy.go
@@ -317,7 +317,7 @@ func verifyAccess(ctx context.Context, c *client.Client, githubURL string) (*adm
 	// If the user has not already granted access, open browser and poll for access
 	if !ghRes.HasAccess {
 		// Print instructions to grant access
-		fmt.Printf("Rill projects deploy continuously when you push changes to Github.\n\n")
+		fmt.Printf("Rill projects deploy continuously when you push changes to Github.\n")
 		fmt.Printf("You need to install rill github app to grant read only access to your project.\n\n")
 		time.Sleep(3 * time.Second)
 		fmt.Printf("Open this URL in your browser to grant Rill access to your Github repository:\n\n")
