6dd6f1116534885e532ba9ff20d0a02b2825165c
Eric P Green <ericpgreen2@gmail.com>
2023-04-07 17:58:41 -0700

    Migrate `web-local` code to v4

--- web-local/src/routes/(application)/+layout.svelte
+++ web-local/src/routes/(application)/+layout.svelte
@@ -2,7 +2,7 @@
   import { RillDeveloperLayout } from "@rilldata/web-common/layout";
   import RuntimeProvider from "@rilldata/web-common/runtime-client/RuntimeProvider.svelte";
   import { RuntimeUrl } from "@rilldata/web-local/lib/application-state-stores/initialize-node-store-contexts";
-  import { QueryClientProvider } from "@sveltestack/svelte-query";
+  import { QueryClientProvider } from "@tanstack/svelte-query";
   import { createQueryClient } from "../../lib/svelte-query/globalQueryClient";
 
   const queryClient = createQueryClient();
