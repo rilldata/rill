b28eb37c207bcd7f8d96b835d01e0bbecc5dcf0c
Rakesh Sharma <rakeshsharma14317@gmail.com>
2023-04-10 15:54:53 +0530

    go fmt ./...

--- proto/gen/rill/admin/v1/api.pb.go
+++ proto/gen/rill/admin/v1/api.pb.go
@@ -1659,7 +1659,7 @@ func (x *TriggerReconcileRequest) GetName() string {
 	return ""
 }
 
-//Should need to add any response here (i.e similar to ReconcileResponse in runtime.proto)
+// Should need to add any response here (i.e similar to ReconcileResponse in runtime.proto)
 type TriggerReconcileResponse struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
