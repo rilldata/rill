e8d39fcc1c556dabf2ac7a85690a0c1a69c22f8e
Eric P Green <ericpgreen2@gmail.com>
2023-04-10 15:24:33 -0700

    Fix redirect when unauthenticated

--- web-admin/src/routes/+page.svelte
+++ web-admin/src/routes/+page.svelte
@@ -1,21 +1,28 @@
 <script lang="ts">
+  import { goto } from "$app/navigation";
   import { useAdminServiceGetCurrentUser } from "../client";
+  import { ADMIN_URL } from "../client/http-client";
   import OrganizationList from "../components/home/OrganizationList.svelte";
 
-  const user = useAdminServiceGetCurrentUser();
-  if ($user.data && !$user.data.user) {
-    // throw redirect(307, `${ADMIN_URL}/auth/login?redirect=${window.origin}`);
-  }
+  const user = useAdminServiceGetCurrentUser({
+    query: {
+      onSuccess: (data) => {
+        if (!data.user) {
+          goto(`${ADMIN_URL}/auth/login?redirect=${window.origin}`);
+        }
+      },
+    },
+  });
 </script>
 
-<section class="flex flex-col justify-center w-4/5 mx-auto h-2/5">
-  <h1 class="text-4xl leading-10 font-light mb-2">
-    {#if $user.data && $user.data.user}
+{#if $user.data && $user.data.user}
+  <section class="flex flex-col justify-center w-4/5 mx-auto h-2/5">
+    <h1 class="text-4xl leading-10 font-light mb-2">
       Hi {$user.data.user.displayName}!
-    {/if}
-  </h1>
-  <h3 class="text-base leading-6 font-normal text-gray-500 mb-2">
-    Check out your dashboards below.
-  </h3>
-  <OrganizationList />
-</section>
+    </h1>
+    <h3 class="text-base leading-6 font-normal text-gray-500 mb-2">
+      Check out your dashboards below.
+    </h3>
+    <OrganizationList />
+  </section>
+{/if}
