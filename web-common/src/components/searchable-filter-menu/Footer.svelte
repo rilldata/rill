32fbd86cb19e3e3b35737f81da2cd4be1a4a15ca
brendan colloran <brendan@colloran.net>
2023-04-11 11:32:36 -0700

    review fix

--- web-common/src/components/searchable-filter-menu/Footer.svelte
+++ web-common/src/components/searchable-filter-menu/Footer.svelte
@@ -1,5 +1,5 @@
 <div
-  class="flex flex-row mt-auto items-center justify-between gap-x-2 px-2 py-1 sticky bottom-0 border-t bg-gray-50 dark:bg-gray-600 border-gray-200 dark:border-gray-500"
+  class="flex flex-row mt-auto items-center justify-between gap-x-2 pl-2 pr-4 py-1 sticky bottom-0 border-t bg-gray-50 dark:bg-gray-600 border-gray-200 dark:border-gray-500"
 >
   <slot />
 </div>
