b2a323aeb045c6193386baacddf6964ca0d7eb1e
Eric P Green <ericpgreen2@gmail.com>
2023-04-13 15:40:34 -0700

    Poll for project status

--- web-admin/src/routes/[organization]/[project]/[dashboard]/+page.svelte
+++ web-admin/src/routes/[organization]/[project]/[dashboard]/+page.svelte
@@ -3,24 +3,55 @@
   import { Dashboard } from "@rilldata/web-common/features/dashboards";
   import { useDashboardStore } from "@rilldata/web-common/features/dashboards/dashboard-stores";
   import { StateSyncManager } from "@rilldata/web-common/features/dashboards/proto-state/StateSyncManager";
+  import {
+    createAdminServiceGetProject,
+    V1DeploymentStatus,
+  } from "../../../../client";
+  import Building from "../../../../components/deployments/Building.svelte";
+  import Errored from "../../../../components/deployments/Errored.svelte";
 
-  $: metricViewName = $page.params.dashboard;
-  const stateSyncManager = new StateSyncManager(metricViewName);
+  // TODO: add 404 logic as in `web-local`'s `dashboard/[name]/+page.svelte`
 
-  $: metricsExplorer = useDashboardStore(metricViewName);
+  $: org = $page.params.organization;
+  $: proj = $page.params.project;
+  $: dash = $page.params.dashboard;
 
+  // Poll for project status
+  $: project = createAdminServiceGetProject(org, proj, {
+    query: {
+      refetchInterval: 1000,
+    },
+  });
+  $: isProjectBuilding =
+    $project.data?.productionDeployment?.status ===
+      V1DeploymentStatus.DEPLOYMENT_STATUS_PENDING ||
+    $project.data?.productionDeployment?.status ===
+      V1DeploymentStatus.DEPLOYMENT_STATUS_RECONCILING;
+  $: isProjectErrored =
+    $project.data?.productionDeployment?.status ===
+    V1DeploymentStatus.DEPLOYMENT_STATUS_ERROR;
+  $: isProjectOK =
+    $project.data?.productionDeployment?.status ===
+    V1DeploymentStatus.DEPLOYMENT_STATUS_OK;
+
+  $: metricsExplorer = useDashboardStore(dash);
+  const stateSyncManager = new StateSyncManager(dash);
   $: if ($metricsExplorer) {
     stateSyncManager.handleStateChange($metricsExplorer);
   }
   $: if ($page) {
     stateSyncManager.handleUrlChange();
   }
-
-  // TODO: add 404 logic as in `web-local`'s `dashboard/[name]/+page.svelte`
 </script>
 
 <svelte:head>
-  <title>Rill | {metricViewName}</title>
+  <title>Rill | {dash}</title>
 </svelte:head>
 
-<Dashboard leftMargin={"44px"} hasTitle={false} {metricViewName} />
+{#if isProjectBuilding}
+  <Building organization={org} project={proj} />
+{:else if isProjectErrored}
+  <Errored organization={org} project={proj} />
+{:else if isProjectOK}
+  <Dashboard leftMargin={"44px"} hasTitle={false} metricViewName={dash} />
+{/if}
