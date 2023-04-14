79bfb68caa1e86f90c317f3487c06e6ac431aacd
Hamilton Ulmer <hamilton.ulmer@gmail.com>
2023-04-14 16:34:16 -0700

    minor restyle of org icon

--- web-admin/src/components/navigation/OrganizationAvatar.svelte
+++ web-admin/src/components/navigation/OrganizationAvatar.svelte
@@ -3,7 +3,8 @@
 </script>
 
 <div
-  class="inline-flex items-center justify-center rounded text-white text-xs font-bold bg-gray-500"
+  style:font-size="9px"
+  class="inline-flex items-center justify-center rounded text-white  bg-gray-500"
   style="height: 15px; width: 15px;"
 >
   {organization.charAt(0).toUpperCase()}
