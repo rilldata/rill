fcf43c4f56d77bb5bd5df5dac086642961456cf4
Hamilton Ulmer <hamilton.ulmer@gmail.com>
2023-04-14 16:35:44 -0700

    reduces translation of breadcrumb caret

--- web-admin/src/components/navigation/BreadcrumbItem.svelte
+++ web-admin/src/components/navigation/BreadcrumbItem.svelte
@@ -41,7 +41,7 @@
         on:mouseleave={() => setHovered(false)}
       >
         <span>{label}</span>
-        <div class="transition-transform" class:translate-y-1={hovered}>
+        <div class="transition-transform {hovered ? 'translate-y-[2px]' : ''}">
           <CaretDownIcon size="14px" />
         </div>
       </button>
