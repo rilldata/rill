b9768bd40534c03aee9f2d72c718d145cd4b24cd
Eric P Green <ericpgreen2@gmail.com>
2023-04-18 22:53:21 -0700

    Create error boundary

---
+++ web-admin/src/components/errors/error-store.ts
@@ -0,0 +1,32 @@
+import { derived, writable, Writable } from "svelte/store";
+
+export interface ErrorStoreState {
+  statusCode: number | null;
+  header: string;
+  body: string;
+}
+
+export interface ErrorStore extends Writable<ErrorStoreState> {
+  reset: () => void;
+}
+
+const createErrorStore = (): ErrorStore => {
+  const { subscribe, set, update } = writable({
+    statusCode: null,
+    header: "",
+    body: "",
+  });
+
+  const reset = (): void => {
+    set({ statusCode: null, header: "", body: "" });
+  };
+
+  return { subscribe, set, update, reset };
+};
+
+export const errorStore = createErrorStore();
+
+export const isErrorStoreEmpty = derived(errorStore, ($errorStore) => {
+  const { statusCode, header, body } = $errorStore;
+  return statusCode === null && header === "" && body === "";
+});
