49ceff1146c639bfca93c183f45a77452a6b2fe2
Dhiraj Kumar <sumankumaribarnwal@gmail.com>
2023-04-20 16:57:02 +0530

    Fix 0 bug, better null handling

--- web-common/src/features/dashboards/leaderboard/DimensionLeaderboardEntrySet.svelte
+++ web-common/src/features/dashboards/leaderboard/DimensionLeaderboardEntrySet.svelte
@@ -27,7 +27,7 @@ see more button
     humanizeDataType,
   } from "../humanize-numbers";
   import DimensionLeaderboardEntry from "./DimensionLeaderboardEntry.svelte";
-  import { FormattedDataType, Number } from "../../../components/data-types";
+  import { FormattedDataType } from "../../../components/data-types";
 
   export let values;
   export let comparisonValues;
@@ -116,7 +116,7 @@ see more button
       {excluded}
     >
       <svelte:fragment slot="label">
-        <FormattedDataType isNull={label === null} value={label} />
+        <FormattedDataType value={label} />
       </svelte:fragment>
       <div slot="right" class="flex items-baseline gap-x-1">
         {#if showComparisonForThisValue && comparisonValue !== undefined && comparisonValue !== null}
@@ -128,11 +128,7 @@ see more button
             →
           </span>
         {/if}
-        <Number
-          type="INTEGER"
-          isNull={!(formattedValue || value)}
-          value={formattedValue || value}
-        />
+        <FormattedDataType type="INTEGER" value={formattedValue || value} />
       </div>
       <span slot="context">
         <PercentageChange
