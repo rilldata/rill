05560a643d71a87ed1eaec2b6c0f46ee06fd37a3
Eric P Green <ericpgreen2@gmail.com>
2023-04-20 14:04:59 -0700

    Add max-width and truncate

--- web-admin/src/components/projects/ProjectGithubConnection.svelte
+++ web-admin/src/components/projects/ProjectGithubConnection.svelte
@@ -15,7 +15,7 @@
 </script>
 
 {#if $proj.data}
-  <div class="flex flex-col gap-y-1">
+  <div class="flex flex-col gap-y-1 max-w-[340px]">
     <span class="uppercase text-gray-500 font-semibold text-[10px] leading-4"
       >Github</span
     >
@@ -26,12 +26,12 @@
           <span>Connected to </span>
           <a
             href={$proj.data?.project?.githubUrl}
-            class="flex items-center gap-x-1 text-gray-800"
+            class="flex items-center gap-x-1 text-gray-800 flex-1 truncate"
             target="_blank"
             rel="noreferrer"
           >
             <Github className="inline-block w-4 h-4" />
-            <span class="font-semibold text-[12px] leading-5 font-mono"
+            <span class="font-semibold text-[12px] leading-5 font-mono truncate"
               >{prettyGithubRepo}</span
             ></a
           >
