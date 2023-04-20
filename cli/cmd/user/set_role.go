4a12c3a48198305aa1dd854f52f9e35a54bf9a3a
Aditya Hegde <adityahegderocks@gmail.com>
2023-04-20 18:14:43 +0530

    Adding 'user set-role' cmd

---
+++ cli/cmd/user/set_role.go
@@ -0,0 +1,60 @@
+package user
+
+import (
+	"errors"
+
+	"github.com/rilldata/rill/cli/cmd/cmdutil"
+	"github.com/rilldata/rill/cli/pkg/config"
+	adminv1 "github.com/rilldata/rill/proto/gen/rill/admin/v1"
+	"github.com/spf13/cobra"
+)
+
+func SetRoleCmd(cfg *config.Config) *cobra.Command {
+	var orgName string
+	var projectName string
+	var email string
+	var role string
+
+	setRoleCmd := &cobra.Command{
+		Use:   "set-role",
+		Short: "Set Role",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if orgName == "" && projectName == "" {
+				return errors.New("either organization or project has to be specified")
+			}
+			if orgName != "" && projectName != "" {
+				return errors.New("only one of organization or project has to be specified")
+			}
+
+			if orgName == "" {
+				orgName = cfg.Org
+			}
+
+			client, err := cmdutil.Client(cfg)
+			if err != nil {
+				return err
+			}
+			defer client.Close()
+
+			_, err = client.SetProjectMemberRole(cmd.Context(), &adminv1.SetProjectMemberRoleRequest{
+				Organization: orgName,
+				Project:      projectName,
+				Email:        email,
+				Role:         role,
+			})
+			if err != nil {
+				return err
+			}
+
+			cmdutil.SuccessPrinter("Updated")
+			return nil
+		},
+	}
+
+	setRoleCmd.Flags().StringVar(&orgName, "org", "", "Organization")
+	setRoleCmd.Flags().StringVar(&projectName, "project", "", "Project")
+	setRoleCmd.Flags().StringVar(&email, "email", "", "Email of the user")
+	setRoleCmd.Flags().StringVar(&role, "role", "", "Role of the user, should be admin/viewer")
+
+	return setRoleCmd
+}
