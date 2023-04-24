40ca47e385d7c949be1d2ca89131d4b92ded06db
Rakesh Sharma <rakeshsharma14317@gmail.com>
2023-04-24 21:27:20 +0530

    remove project members command

--- cli/cmd/project/project.go
+++ cli/cmd/project/project.go
@@ -25,7 +25,6 @@ func ProjectCmd(cfg *config.Config) *cobra.Command {
 	projectCmd.AddCommand(EditCmd(cfg))
 	projectCmd.AddCommand(DeleteCmd(cfg))
 	projectCmd.AddCommand(ListCmd(cfg))
-	projectCmd.AddCommand(MembersCmd(cfg))
 	projectCmd.AddCommand(RenameCmd(cfg))
 	return projectCmd
 }
