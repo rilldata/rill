a9411fb1700d2e72f8dc57b0b4e28bc10a8594c7
Eric P Green <ericpgreen2@gmail.com>
2023-04-25 12:25:15 -0700

    Add more reusable "CTA" components

---
+++ web-admin/src/components/calls-to-action/CTAContentContainer.svelte
@@ -0,0 +1,8 @@
+<div class="flex flex-col justify-center items-center gap-y-6 w-[400px]">
+  <slot />
+  <!-- <slot name="image" />
+  <slot name="title" />
+  <slot name="message" />
+  <slot name="button" />
+  <slot name="footer" /> -->
+</div>
