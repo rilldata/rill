8f17e71438a9c816ac899278a2840345304ca7f5
Eric P Green <ericpgreen2@gmail.com>
2023-04-25 11:49:56 -0700

    Style `/-/github/connect/success`

--- web-admin/src/routes/-/github/connect/success/+page.svelte
+++ web-admin/src/routes/-/github/connect/success/+page.svelte
@@ -1,13 +1,27 @@
+<script>
+  import Github from "@rilldata/web-common/components/icons/Github.svelte";
+  import CtaButton from "../../../../../components/CTAButton.svelte";
+
+  // https://stackoverflow.com/questions/8057802/html-button-close-window
+  function closePage() {
+    window.open("", "_self", "");
+    window.close();
+  }
+</script>
+
 <svelte:head>
   <title>Github connected successfully</title>
 </svelte:head>
 
 <div class="flex flex-col justify-center items-center h-3/5">
-  <h1 class="text-3xl font-medium text-gray-800 mb-4">
-    Github connected successfully
-  </h1>
-  <p class="text-md text-green-700 font-bold mb-6">
-    Return to the CLI to continue setting up your project. The page can be
-    closed now.
-  </p>
+  <div class="flex flex-col justify-center items-center gap-y-6 w-[400px]">
+    <Github className="w-[56px] h-[42px] text-gray-900" />
+    <h1 class="text-xl font-medium text-gray-800">
+      Github connected successfully
+    </h1>
+    <p class="text-base text-gray-500">
+      Close this page and continue setup in the Rill CLI.
+    </p>
+    <CtaButton variant="secondary" on:click={closePage}>Close page</CtaButton>
+  </div>
 </div>
