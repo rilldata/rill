fe57ba882356b19c2b62c017b42c9a1c530293f9
Eric P Green <ericpgreen2@gmail.com>
2023-04-25 06:33:57 -0700

    Fix places where dashboard name went blank (#2214)
    * Bugfix
    
    * Fix other places where dashboard name went blank
    
    * Make concise

--- web-admin/src/components/home/DashboardList.svelte
+++ web-admin/src/components/home/DashboardList.svelte
@@ -27,7 +27,7 @@
         <a
           href="/{organization}/{project}/{dashboard.name}"
           class="text-gray-700 hover:underline text-xs font-medium leading-4"
-          >{dashboard.metricsView?.label ?? dashboard.name}</a
+          >{dashboard.metricsView?.label || dashboard.name}</a
         >
       </li>
     {/each}
