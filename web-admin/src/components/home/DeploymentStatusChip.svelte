6c0c09795eab623bca304d3da36192fc8d9bca44
Eric P Green <ericpgreen2@gmail.com>
2023-04-25 07:38:51 -0700

    Address feedback

--- web-admin/src/components/home/DeploymentStatusChip.svelte
+++ web-admin/src/components/home/DeploymentStatusChip.svelte
@@ -6,7 +6,6 @@
   import CancelCircle from "@rilldata/web-common/components/icons/CancelCircle.svelte";
   import CheckCircle from "@rilldata/web-common/components/icons/CheckCircle.svelte";
   import Spacer from "@rilldata/web-common/components/icons/Spacer.svelte";
-  import Timer from "@rilldata/web-common/components/icons/Timer.svelte";
   import Spinner from "@rilldata/web-common/features/entity-management/Spinner.svelte";
   import { EntityStatus } from "@rilldata/web-common/features/entity-management/types";
   import { useQueryClient } from "@tanstack/svelte-query";
@@ -18,7 +17,8 @@
 
   $: proj = useProject(organization, project);
   let deploymentStatus: V1DeploymentStatus;
-  $: currentStatus = !!deploymentStatus && statusMap.get(deploymentStatus);
+  $: currentStatusDisplay =
+    !!deploymentStatus && statusDisplays[deploymentStatus];
 
   const queryClient = useQueryClient();
 
@@ -52,74 +52,65 @@
     wrapperClass?: string;
   };
 
-  const statusMap = new Map<V1DeploymentStatus, StatusDisplay>([
-    [
-      V1DeploymentStatus.DEPLOYMENT_STATUS_PENDING,
-      {
-        icon: Timer,
-        iconProps: {
-          className: "text-amber-600 hover:text-amber-500",
-        },
-        text: "deploying",
-        textClass: "text-amber-600",
-        wrapperClass: "bg-amber-50 border-amber-300",
+  const statusDisplays: Record<V1DeploymentStatus, StatusDisplay> = {
+    [V1DeploymentStatus.DEPLOYMENT_STATUS_OK]: {
+      icon: CheckCircle,
+      iconProps: { className: "text-blue-600 hover:text-blue-500" },
+      text: "ready",
+      textClass: "text-blue-600",
+      wrapperClass: "bg-blue-50 border-blue-300",
+    },
+    [V1DeploymentStatus.DEPLOYMENT_STATUS_PENDING]: {
+      icon: Spinner,
+      iconProps: {
+        className: "text-purple-600 hover:text-purple-500",
+        status: EntityStatus.Running,
       },
-    ],
-    [
-      V1DeploymentStatus.DEPLOYMENT_STATUS_RECONCILING,
-      {
-        icon: Spinner,
-        iconProps: {
-          className: "text-purple-600 hover:text-purple-500",
-          status: EntityStatus.Running,
-        },
-        text: "syncing",
-        textClass: "text-purple-600",
-        wrapperClass: "bg-purple-50 border-purple-300",
+      text: "syncing",
+      textClass: "text-purple-600",
+      wrapperClass: "bg-purple-50 border-purple-300",
+    },
+    [V1DeploymentStatus.DEPLOYMENT_STATUS_RECONCILING]: {
+      icon: Spinner,
+      iconProps: {
+        className: "text-purple-600 hover:text-purple-500",
+        status: EntityStatus.Running,
       },
-    ],
-    [
-      V1DeploymentStatus.DEPLOYMENT_STATUS_OK,
-      {
-        icon: CheckCircle,
-        iconProps: { className: "text-blue-600 hover:text-blue-500" },
-        text: "ready",
-        textClass: "text-blue-600",
-        wrapperClass: "bg-blue-50 border-blue-300",
-      },
-    ],
-    [
-      V1DeploymentStatus.DEPLOYMENT_STATUS_ERROR,
-      {
-        icon: CancelCircle,
-        iconProps: { className: "text-red-600 hover:text-red-500" },
-        text: "error",
-        textClass: "text-red-600",
-        wrapperClass: "bg-red-50 border-red-300",
-      },
-    ],
-    [
-      V1DeploymentStatus.DEPLOYMENT_STATUS_UNSPECIFIED,
-      {
-        icon: Spacer,
-      },
-    ],
-  ]);
+      text: "syncing",
+      textClass: "text-purple-600",
+      wrapperClass: "bg-purple-50 border-purple-300",
+    },
+    [V1DeploymentStatus.DEPLOYMENT_STATUS_ERROR]: {
+      icon: CancelCircle,
+      iconProps: { className: "text-red-600 hover:text-red-500" },
+      text: "error",
+      textClass: "text-red-600",
+      wrapperClass: "bg-red-50 border-red-300",
+    },
+    [V1DeploymentStatus.DEPLOYMENT_STATUS_UNSPECIFIED]: {
+      icon: Spacer,
+    },
+  };
 </script>
 
 {#if deploymentStatus && deploymentStatus !== V1DeploymentStatus.DEPLOYMENT_STATUS_UNSPECIFIED}
   {#if iconOnly}
-    <svelte:component this={currentStatus.icon} {...currentStatus.iconProps} />
+    <svelte:component
+      this={currentStatusDisplay.icon}
+      {...currentStatusDisplay.iconProps}
+    />
   {:else}
     <div
-      class="flex space-x-1 items-center px-2 border rounded rounded-[20px] w-fit {currentStatus.wrapperClass} {iconOnly &&
+      class="flex space-x-1 items-center px-2 border rounded rounded-[20px] w-fit {currentStatusDisplay.wrapperClass} {iconOnly &&
         'hidden'}"
     >
       <svelte:component
-        this={currentStatus.icon}
-        {...currentStatus.iconProps}
+        this={currentStatusDisplay.icon}
+        {...currentStatusDisplay.iconProps}
       />
-      <span class={currentStatus.textClass}>{currentStatus.text}</span>
+      <span class={currentStatusDisplay.textClass}
+        >{currentStatusDisplay.text}</span
+      >
     </div>
   {/if}
 {:else}
