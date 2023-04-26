73c566f860b27f37eb68e16a4358f34a3da172d2
brendan colloran <brendan@colloran.net>
2023-04-26 10:49:24 -0700

    use observed element sizes to set more dashboard container dimensions

--- web-common/src/features/dashboards/workspace/DashboardContainer.svelte
+++ web-common/src/features/dashboards/workspace/DashboardContainer.svelte
@@ -12,33 +12,70 @@
     "rill:app:navigation-visibility-tween"
   ) as Tweened<number>;
 
-  const { observedNode, listenToNodeResize } =
-    createResizeListenerActionFactory();
+  const {
+    observedNode: dashboardContainerNode,
+    listenToNodeResize: dashboardContainerNodeWatcher,
+  } = createResizeListenerActionFactory();
 
-  $: exploreContainerWidth = $observedNode?.offsetWidth || 0;
+  const {
+    observedNode: measuresContainerNode,
+    listenToNodeResize: measuresContainerNodeWatcher,
+  } = createResizeListenerActionFactory();
 
-  $: width = $observedNode?.getBoundingClientRect()?.width;
+  const {
+    observedNode: headerContainerNode,
+    listenToNodeResize: headerContainerNodeWatcher,
+  } = createResizeListenerActionFactory();
+
+  $: exploreContainerWidth = $dashboardContainerNode?.offsetWidth || 0;
+  // $: console.log("exploreContainerWidth", exploreContainerWidth);
+
+  $: measureContainerWidth = $measuresContainerNode?.offsetWidth || 0;
+  // $: console.log("measureContainerWidth", measureContainerWidth);
+
+  $: width = $dashboardContainerNode?.getBoundingClientRect()?.width;
+  // $: console.log("width", width);
+
+  $: targetLeaderboardContainerWidth =
+    exploreContainerWidth - measureContainerWidth || 0;
+
+  $: targetLeaderboardContainerHeight =
+    $dashboardContainerNode?.getBoundingClientRect()?.height -
+      $headerContainerNode?.getBoundingClientRect()?.height || 0;
+
+  $: console.log(
+    "targetLeaderboardContainerHeight",
+    targetLeaderboardContainerHeight
+  );
 
   $: leftSide = leftMargin
     ? leftMargin
     : `calc(${$navigationVisibilityTween * 24}px + 1.25rem)`;
 </script>
 
-<section use:listenToNodeResize class="grid items-stretch surface">
+<section use:dashboardContainerNodeWatcher class="grid items-stretch surface">
   <div
+    use:headerContainerNodeWatcher
     class="explore-header border-b mb-3"
     style:padding-left={leftSide}
     style:width={width + "px"}
   >
     <slot name="header" />
   </div>
-  <div class="explore-metrics mb-8" style:padding-left={leftSide}>
+  <div
+    use:measuresContainerNodeWatcher
+    class="explore-metrics mb-8"
+    style:padding-left={leftSide}
+  >
     <slot
       name="metrics"
-      width={$observedNode?.getBoundingClientRect()?.width}
+      width={$dashboardContainerNode?.getBoundingClientRect()?.width}
     />
   </div>
-  <div class="explore-leaderboards pr-4 pb-8">
+  <div
+    class="explore-leaderboards px-4"
+    style={`height:${targetLeaderboardContainerHeight}px; width:${targetLeaderboardContainerWidth}px`}
+  >
     <slot name="leaderboards" />
   </div>
 </section>
@@ -47,7 +84,6 @@
   section {
     grid-template-rows: auto auto 1fr;
     grid-template-columns: min-content 1fr;
-    column-gap: 16px;
     height: 100vh;
     overflow-x: auto;
     overflow-y: hidden;
