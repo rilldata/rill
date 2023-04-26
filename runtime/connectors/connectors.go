71bd90581bfe4ad915d66460de83255d5d6ddd89
e.sevastyanov <eugene.sevastianov@rilldata.com>
2023-04-26 20:37:54 +0400

    Simplified a comparison of properties

--- runtime/connectors/connectors.go
+++ runtime/connectors/connectors.go
@@ -164,16 +164,5 @@ func ConsumeAsIterator(ctx context.Context, env *Env, source *Source) (FileItera
 }
 
 func (s *Source) PropertiesEquals(o *Source) bool {
-	if len(s.Properties) != len(o.Properties) {
-		return false
-	}
-
-	for k1, v1 := range s.Properties {
-		v2, ok := o.Properties[k1]
-		if !ok || !reflect.DeepEqual(v1, v2) {
-			return false
-		}
-	}
-
-	return true
+	return reflect.DeepEqual(s.Properties, o.Properties)
 }
