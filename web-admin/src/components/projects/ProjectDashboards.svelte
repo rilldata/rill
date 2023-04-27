b639084a4e853dbd6950924845ac21bba4806d27
Dhiraj Kumar <sumankumaribarnwal@gmail.com>
2023-04-27 17:02:25 +0530

    Project layout changes

---
+++ web-admin/src/components/projects/ProjectDashboards.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import DashboardList from "../home/DashboardList.svelte";
+
+  export let organization: string;
+  export let project: string;
+</script>
+
+<div class="flex flex-col gap-y-1">
+  <span class="uppercase text-gray-500 font-semibold text-[10px] leading-4"
+    >Dashboards</span
+  >
+  <div>
+    <DashboardList {organization} {project} />
+  </div>
+</div>
