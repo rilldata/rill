bf256b5cc47e0499f7f9dec3f4eba26fd3f39f65
Dhiraj Kumar <sumankumaribarnwal@gmail.com>
2023-04-28 17:20:08 +0530

    Share button style changes

--- web-common/src/components/commands/CLICommandDisplay.svelte
+++ web-common/src/components/commands/CLICommandDisplay.svelte
@@ -15,11 +15,14 @@
 </script>
 
 <div class="flex flex-row text-gray-800 my-1">
-  <div class="p-0.5 border border-gray-200 rounded-sm bg-gray-50 command-text">
+  <div
+    class="p-0.5 border border-gray-200 rounded-bl-sm rounded-tl-sm bg-gray-50 command-text"
+  >
     {command}
   </div>
   <div
-    class="p-1 border border-gray-200 rounded-sm bg-gray-50"
+    class="p-1 border border-gray-200 rounded-br-sm rounded-tr-sm bg-gray-50
+    {copied ? '' : 'cursor-pointer'}"
     on:click={copyCommand}
     on:keydown={copyCommand}
   >
@@ -33,6 +36,7 @@
 
 <style>
   .command-text {
+    font-family: "Source Code Variable", monospace;
     padding-left: 7px;
     padding-right: 7px;
     border-right: 0;
