e32c93c1115d1a4bf9b20720aa2dc506afaf8ceb
egor-ryashin <egor.ryashin@rilldata.com>
2023-04-28 18:09:35 +0300

    cli docs tree structured - refactoring

--- cli/cmd/docs/docs.go
+++ cli/cmd/docs/docs.go
@@ -1,17 +1,24 @@
 package docs
 
 import (
+	"fmt"
+
+	"github.com/rilldata/rill/cli/pkg/browser"
 	"github.com/rilldata/rill/cli/pkg/config"
 	"github.com/spf13/cobra"
 )
 
 func DocsCmd(cfg *config.Config, rootCmd *cobra.Command) *cobra.Command {
 	docsCmd := &cobra.Command{
-		Use:    "docs",
-		Hidden: !cfg.IsDev(),
-		Short:  "Manage documentation",
+		Use:   "docs",
+		Short: "Open docs.rilldata.com",
+		Run: func(cmd *cobra.Command, args []string) {
+			err := browser.Open(docsURL)
+			if err != nil {
+				fmt.Printf("Could not open browser. Copy this URL into your browser: %s\n", docsURL)
+			}
+		},
 	}
-	docsCmd.AddCommand(OpenCmd())
 	docsCmd.AddCommand(GenerateCmd(rootCmd))
 
 	return docsCmd
