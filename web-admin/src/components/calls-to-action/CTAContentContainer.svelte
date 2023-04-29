c7b65a47ab69a277ee42720057c4db7ffb5387f6
Eric P Green <ericpgreen2@gmail.com>
2023-04-28 20:38:13 -0700

    Use 570px `max-width` for all CTA pages

--- web-admin/src/components/calls-to-action/CTAContentContainer.svelte
+++ web-admin/src/components/calls-to-action/CTAContentContainer.svelte
@@ -1,9 +1,5 @@
-<script lang="ts">
-  export let maxWidth = "600px";
-</script>
-
 <div
-  class="flex flex-col justify-center items-center gap-y-6 w-full max-w-[{maxWidth}] text-center"
+  class="flex flex-col justify-center items-center gap-y-6 w-full max-w-[570px] text-center"
 >
   <slot />
 </div>
