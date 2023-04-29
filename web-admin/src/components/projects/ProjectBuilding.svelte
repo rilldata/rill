e39b19dd577c3edd97171fca980c4dbee2012787
Eric P Green <ericpgreen2@gmail.com>
2023-04-28 21:02:13 -0700

    Use updated "CTA" components

--- web-admin/src/components/projects/ProjectBuilding.svelte
+++ web-admin/src/components/projects/ProjectBuilding.svelte
@@ -1,22 +1,22 @@
 <script lang="ts">
   import Spinner from "@rilldata/web-common/features/entity-management/Spinner.svelte";
   import { EntityStatus } from "@rilldata/web-common/features/entity-management/types";
-  import CtaContentContainer from "../calls-to-action/CTAContentContainer.svelte";
-  import CtaLayoutContainer from "../calls-to-action/CTALayoutContainer.svelte";
-  import CtaHeader from "../calls-to-action/CTAHeader.svelte";
   import CtaButton from "../calls-to-action/CTAButton.svelte";
-  import CtaMessage from "../calls-to-action/CTAMessage.svelte";
+  import CtaContentContainer from "../calls-to-action/CTAContentContainer.svelte";
+  import CtaHeader from "../calls-to-action/CTAHeader.svelte";
+  import CtaLayoutContainer from "../calls-to-action/CTALayoutContainer.svelte";
+  import CtaNeedHelp from "../calls-to-action/CTANeedHelp.svelte";
 
   export let organization: string;
   export let project: string;
 </script>
 
 <CtaLayoutContainer>
-  <CtaContentContainer maxWidth="400px">
+  <CtaContentContainer>
     <div class="h-36">
       <Spinner status={EntityStatus.Running} size="7rem" duration={725} />
     </div>
-    <CtaHeader fontSizeClass="text-lg" fontWeightClass="font-semibold"
+    <CtaHeader variant="bold"
       >Hang tight! We're building your dashboard...</CtaHeader
     >
     <CtaButton
@@ -26,8 +26,6 @@
       }}
       >View project status
     </CtaButton>
-    <CtaMessage fontSizeClass="text-sm">
-      Need help? Reach out to us on <a href="http://bit.ly/3jg4IsF">Discord</a>
-    </CtaMessage>
+    <CtaNeedHelp />
   </CtaContentContainer>
 </CtaLayoutContainer>
