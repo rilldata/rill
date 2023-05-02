ee98a37a5d5080ad577d533ceb26a03850c545e8
Eric P Green <ericpgreen2@gmail.com>
2023-05-01 21:37:48 -0700

    Remove `placeholderData: {}` from Query Clients

--- web-admin/src/routes/+layout.svelte
+++ web-admin/src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import NotificationCenter from "@rilldata/web-common/components/notifications/NotificationCenter.svelte";
   import RillTheme from "@rilldata/web-common/layout/RillTheme.svelte";
   import { featureFlags } from "@rilldata/web-local/lib/application-state-stores/application-store";
   import {
@@ -9,7 +10,6 @@
   import { globalErrorCallback } from "../components/errors/error-utils";
   import ErrorBoundary from "../components/errors/ErrorBoundary.svelte";
   import TopNavigationBar from "../components/navigation/TopNavigationBar.svelte";
-  import NotificationCenter from "@rilldata/web-common/components/notifications/NotificationCenter.svelte";
 
   const queryClient = new QueryClient({
     queryCache: new QueryCache({
@@ -24,7 +24,6 @@
         refetchOnReconnect: false,
         refetchOnWindowFocus: false,
         retry: false,
-        placeholderData: {}, // there's an issue somewhere in the Leaderboard components that depends on this setting
       },
     },
   });
