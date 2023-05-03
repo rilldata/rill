8fe65303ebf6ea4832f63743484d5d8e6c9973e9
Eric P Green <ericpgreen2@gmail.com>
2023-05-03 11:21:02 -0700

    Remove unused callback

--- web-admin/src/components/navigation/Breadcrumbs.svelte
+++ web-admin/src/components/navigation/Breadcrumbs.svelte
@@ -87,7 +87,6 @@
           $organizations.data.organizations.map((org) => ({
             key: org.name,
             main: org.name,
-            callback: () => goto(`/${org.name}`),
           }))}
         menuKey={orgName}
         onSelectMenuOption={(organization) => goto(`/${organization}`)}
@@ -104,7 +103,6 @@
           $projects.data.projects.map((proj) => ({
             key: proj.name,
             main: proj.name,
-            callback: () => goto(`/${orgName}/${proj.name}`),
           }))}
         menuKey={projectName}
         onSelectMenuOption={(project) => goto(`/${orgName}/${project}`)}
@@ -120,8 +118,6 @@
             return {
               key: listing.name,
               main: listing?.title || listing.name,
-              callback: () =>
-                goto(`/${orgName}/${projectName}/${listing.name}`),
             };
           })}
         menuKey={currentDashboard.name}
