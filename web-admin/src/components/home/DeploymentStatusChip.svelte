c1bda700825b6f629d4253d8027f53d880e26168
Dhiraj Kumar <sumankumaribarnwal@gmail.com>
2023-05-09 17:13:12 +0530

    Change spinner color for dashboard

--- web-admin/src/components/home/DeploymentStatusChip.svelte
+++ web-admin/src/components/home/DeploymentStatusChip.svelte
@@ -80,6 +80,7 @@
     [V1DeploymentStatus.DEPLOYMENT_STATUS_PENDING]: {
       icon: Spinner,
       iconProps: {
+        bg: "linear-gradient(90deg, #22D3EE -0.5%, #6366F1 98.5%)",
         className: "text-purple-600 hover:text-purple-500",
         status: EntityStatus.Running,
       },
@@ -90,6 +91,7 @@
     [V1DeploymentStatus.DEPLOYMENT_STATUS_RECONCILING]: {
       icon: Spinner,
       iconProps: {
+        bg: "linear-gradient(90deg, #22D3EE -0.5%, #6366F1 98.5%)",
         className: "text-purple-600 hover:text-purple-500",
         status: EntityStatus.Running,
       },
