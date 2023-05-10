53276c1d94c489f57f283efea6a3cc18c6732ce9
Eric P Green <ericpgreen2@gmail.com>
2023-05-10 08:35:00 -0700

    Use `font-medium` in calls to action (#2383)
    * Remove log
    
    * Add `font-medium` to CTA Button
    
    * Use CTA components for the error page

--- web-admin/src/components/errors/ErrorPage.svelte
+++ web-admin/src/components/errors/ErrorPage.svelte
@@ -1,30 +1,30 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
+  import CtaButton from "@rilldata/web-common/components/calls-to-action/CTAButton.svelte";
+  import CtaContentContainer from "@rilldata/web-common/components/calls-to-action/CTAContentContainer.svelte";
+  import CtaLayoutContainer from "@rilldata/web-common/components/calls-to-action/CTALayoutContainer.svelte";
+  import CtaMessage from "@rilldata/web-common/components/calls-to-action/CTAMessage.svelte";
+  import CtaNeedHelp from "@rilldata/web-common/components/calls-to-action/CTANeedHelp.svelte";
 
   export let statusCode: number;
   export let header: string;
   export let body: string;
 </script>
 
-<div class="flex flex-col justify-center items-center h-3/5 space-y-6 m-auto">
-  <h1
-    class="text-8xl font-extrabold bg-gradient-to-b from-[#CBD5E1] to-[#E2E8F0] text-transparent bg-clip-text"
-  >
-    {statusCode}
-  </h1>
-  <div class="flex flex-col items-center space-y-2">
+<CtaLayoutContainer>
+  <CtaContentContainer>
+    <h1
+      class="text-8xl font-extrabold bg-gradient-to-b from-[#CBD5E1] to-[#E2E8F0] text-transparent bg-clip-text"
+    >
+      {statusCode}
+    </h1>
     <h2 class="text-lg font-semibold">{header}</h2>
-    <p class="text-base text-gray-500 text-center" style="max-width: 530px">
+    <CtaMessage>
       {body}
-    </p>
-  </div>
-  <button
-    class="border border-blue-300 text-blue-600 w-full h-10 hover:bg-slate-100 hover:border-gray-100"
-    style="max-width: 400px"
-    on:click={() => goto("/")}
-    >Back to home
-  </button>
-  <p class="text-gray-500">
-    Need help? Reach out to us on <a href="http://bit.ly/3jg4IsF">Discord</a>
-  </p>
-</div>
+    </CtaMessage>
+    <CtaButton variant="primary-outline" on:click={() => goto("/")}>
+      Back to home
+    </CtaButton>
+    <CtaNeedHelp />
+  </CtaContentContainer>
+</CtaLayoutContainer>
