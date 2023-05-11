ea8af3eaac6bebac8aa105758440163e67576f67
Eric P Green <ericpgreen2@gmail.com>
2023-05-11 12:21:09 -0700

    Update docs link

--- web-admin/src/routes/[organization]/+page.svelte
+++ web-admin/src/routes/[organization]/+page.svelte
@@ -31,7 +31,7 @@
         {#if $projs.data.projects?.length === 0}
           <span
             >This organization has no projects yet. <a
-              href="https://docs.rilldata.com/quickstart/local"
+              href="https://docs.rilldata.com/quickstart/end-to-end"
               target="_blank"
               rel="noreferrer">See docs</a
             ></span
