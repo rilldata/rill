55c0f3dbeb3eacf26709ab538798922e8904eb91
Dhiraj Kumar <sumankumaribarnwal@gmail.com>
2023-05-12 13:33:40 +0530

    Add icons for Okta ping

--- web-auth/src/config.js
+++ web-auth/src/config.js
@@ -1,5 +1,7 @@
 import Google from "@rilldata/web-common/components/icons/Google.svelte";
 import Microsoft from "@rilldata/web-common/components//icons/Microsoft.svelte";
+import Okta from "@rilldata/web-common/components//icons/Okta.svelte";
+import Pingfed from "@rilldata/web-common/components//icons/Pingfed.svelte";
 
 export const LOGIN_OPTIONS = [
   {
@@ -18,11 +20,13 @@ export const LOGIN_OPTIONS = [
   },
   {
     name: "Okta",
+    icon: Okta,
     label: "Continue with Okta",
     style: "secondary",
   },
   {
     name: "Pingfed",
+    icon: Pingfed,
     label: "Continue with Ping Fed",
     style: "secondary",
   },
