99c819df22ad991f3cee8b68a05e03ce8985d754
Speros Kokenes <skokenes@users.noreply.github.com>
2023-05-17 12:06:42 -0400

    test: re-enable e2e tests (#2444)
    * test: re-enable e2e tests
    
    * fix: echo syntax for bash

--- web-local/test/setup.sh
+++ web-local/test/setup.sh
@@ -2,10 +2,10 @@
 skipUIBuild="${SKIP_UI_BUILD:-false}"
 
 if ! $skipUIBuild ; then
-    ECHO "Building UI"
+    echo "Building UI"
     make cli.prepare -C ../
 else
-    ECHO "Skipping UI build"
+    echo "Skipping UI build"
 fi
-ECHO Building e2e test Rill
+echo Building e2e test Rill
 go build -o ./test/rill-e2e-test ../cli/main.go
