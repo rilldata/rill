e1ac2a2a330942a42474de169f2d5389d6697c4d
Eric P Green <ericpgreen2@gmail.com>
2023-05-17 16:20:49 -0700

    Create project tiles

---
+++ web-local/src/components/welcome/EmptyProject.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import AddCircleOutline from "@rilldata/web-common/components/icons/AddCircleOutline.svelte";
+  import Card from "../card/Card.svelte";
+  import CardHeader from "../card/CardHeader.svelte";
+</script>
+
+<Card>
+  <AddCircleOutline size="2em" className="text-slate-800" />
+  <CardHeader>Start with an empty project</CardHeader>
+</Card>
