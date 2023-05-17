987560c2ad9023ec78319ee2120b5cc92b7c27f1
Eric P Green <ericpgreen2@gmail.com>
2023-05-17 16:21:07 -0700

    Add Welcome page

---
+++ web-local/src/components/welcome/BodyContainer.svelte
@@ -0,0 +1,3 @@
+<div class="px-20 py-16 flex flex-col gap-y-8 items-center">
+  <slot />
+</div>
---
+++ web-local/src/components/welcome/Welcome.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import BodyContainer from "./BodyContainer.svelte";
+  import ProjectTiles from "./ProjectTiles.svelte";
+  import TitleContent from "./TitleContent.svelte";
+</script>
+
+<BodyContainer>
+  <TitleContent />
+  <ProjectTiles />
+</BodyContainer>
