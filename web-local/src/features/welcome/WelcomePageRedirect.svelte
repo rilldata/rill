0d05dc1431098a6586089119d1857cdbb0594f2e
Eric P Green <ericpgreen2@gmail.com>
2023-05-22 14:12:36 -0700

    Add redirect to `/welcome`

---
+++ web-local/src/features/welcome/WelcomePageRedirect.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import { createRuntimeServiceListFiles } from "@rilldata/web-common/runtime-client";
+  import { runtime } from "@rilldata/web-common/runtime-client/runtime-store";
+
+  // Check the directory for a rill.yaml file
+  $: includesProjectFile = createRuntimeServiceListFiles(
+    $runtime.instanceId,
+    {
+      glob: "rill.{yaml,yml}",
+    },
+    {
+      query: {
+        refetchOnWindowFocus: true,
+      },
+    }
+  );
+
+  // If the project file does not exist, redirect to the Welcome page
+  $: if (
+    $includesProjectFile.isSuccess &&
+    $includesProjectFile.data.paths.length === 0
+  ) {
+    goto("/welcome");
+  }
+</script>
+
+{#if $includesProjectFile.isSuccess}
+  <slot />
+{/if}
