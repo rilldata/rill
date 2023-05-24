603f4ac6cad72296b26fe77f4ef83c913767eddb
Eric P Green <ericpgreen2@gmail.com>
2023-05-24 15:33:54 -0700

    Add drop shadow, hover state, and transition

--- web-common/src/components/card/Card.svelte
+++ web-common/src/components/card/Card.svelte
@@ -1,5 +1,26 @@
+<script lang="ts">
+  export let bgClasses = "";
+</script>
+
 <div
-  class="w-[240px] h-[240px] p-4 border border-gray-300 rounded-md flex flex-col items-center justify-center gap-y-2"
+  class="w-[240px] h-[240px] rounded-md
+  flex flex-col items-center justify-center gap-y-2
+  relative {bgClasses} 
+  transition duration-300 ease-out custom-shadow custom-shadow-hover cursor-pointer"
+  on:click
 >
   <slot />
 </div>
+
+<style>
+  .custom-shadow {
+    box-shadow: 0px 2px 3px rgba(15, 23, 42, 0.06),
+      0px 1px 3px rgba(15, 23, 42, 0.08), 0px 0px 0px 1px rgba(15, 23, 42, 0.12);
+  }
+
+  .custom-shadow-hover:hover {
+    box-shadow: 0px 2px 3px rgba(99, 102, 241, 0.2),
+      0px 1px 3px rgba(15, 23, 42, 0.08), 0px 0px 0px 1px rgba(15, 23, 42, 0.12),
+      0px 4px 6px rgba(15, 23, 42, 0.12);
+  }
+</style>
