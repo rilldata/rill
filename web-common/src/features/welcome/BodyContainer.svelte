260ba3ffe92027424dc549f5f9653620c2944186
Eric P Green <ericpgreen2@gmail.com>
2023-05-25 10:47:17 -0700

    Remove `BodyContainer` abstraction

--- web-common/src/features/welcome/BodyContainer.svelte
+++
@@ -1,3 +0,0 @@
-<div class="flex flex-col gap-y-8 py-16 2xl:py-32 px-10 2xl:px-20">
-  <slot />
-</div>
--- web-common/src/features/welcome/Welcome.svelte
+++ web-common/src/features/welcome/Welcome.svelte
@@ -1,16 +1,15 @@
 <script lang="ts">
   import { fly } from "svelte/transition";
   import BackgroundImage from "./BackgroundImage.svelte";
-  import BodyContainer from "./BodyContainer.svelte";
   import ProjectCards from "./ProjectCards.svelte";
   import TitleContent from "./TitleContent.svelte";
 </script>
 
 <BackgroundImage>
   <div in:fly={{ duration: 1600, delay: 400, y: 8 }}>
-    <BodyContainer>
+    <div class="flex flex-col gap-y-8 py-16 2xl:py-32 px-10 2xl:px-20">
       <TitleContent />
       <ProjectCards />
-    </BodyContainer>
+    </div>
   </div>
 </BackgroundImage>
