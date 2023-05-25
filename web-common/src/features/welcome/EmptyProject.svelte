702037de755a2140a5cc514c32d7169040ad79ca
Eric P Green <ericpgreen2@gmail.com>
2023-05-25 14:00:12 -0700

    Remove unnecessary invalidation

--- web-common/src/features/welcome/EmptyProject.svelte
+++ web-common/src/features/welcome/EmptyProject.svelte
@@ -2,16 +2,10 @@
   import { goto } from "$app/navigation";
   import AddCircleOutline from "@rilldata/web-common/components/icons/AddCircleOutline.svelte";
   import { runtime } from "@rilldata/web-common/runtime-client/runtime-store";
-  import { useQueryClient } from "@tanstack/svelte-query";
   import Card from "../../components/card/Card.svelte";
   import CardTitle from "../../components/card/CardTitle.svelte";
-  import {
-    createRuntimeServiceUnpackEmpty,
-    getRuntimeServiceGetFileQueryKey,
-  } from "../../runtime-client";
+  import { createRuntimeServiceUnpackEmpty } from "../../runtime-client";
   import { EMPTY_PROJECT_TITLE } from "./constants";
-
-  const queryClient = useQueryClient();
 
   const unpackEmptyProject = createRuntimeServiceUnpackEmpty();
   async function startWithEmptyProject() {
@@ -25,13 +19,6 @@
       },
       {
         onSuccess: () => {
-          // Invalidate `rill.yaml` GetFile
-          queryClient.invalidateQueries({
-            queryKey: getRuntimeServiceGetFileQueryKey(
-              $runtime.instanceId,
-              "rill.yaml"
-            ),
-          });
           goto("/");
         },
       }
