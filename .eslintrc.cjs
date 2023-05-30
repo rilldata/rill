2104129f84eb13a9998921dfbb36ddcacf83aaee
brendan colloran <brendan@colloran.net>
2023-05-30 16:22:41 -0700

    initial commit, not working

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,29 +1,30 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier", "plugin:storybook/recommended"],
   plugins: ["svelte3", "@typescript-eslint"],
   ignorePatterns: ["*.cjs"],
-  overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
+  overrides: [{
+    files: ["*.svelte"],
+    processor: "svelte3/svelte3"
+  }],
   settings: {
-    "svelte3/typescript": () => require("typescript"),
+    "svelte3/typescript": () => require("typescript")
   },
   parserOptions: {
     sourceType: "module",
-    ecmaVersion: 2019,
+    ecmaVersion: 2019
   },
   env: {
     browser: true,
     es2017: true,
-    node: true,
+    node: true
   },
   rules: {
     "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
-    "@typescript-eslint/ban-ts-comment": "warn",
-  },
+    "@typescript-eslint/no-unused-vars": ["error", {
+      argsIgnorePattern: "^_"
+    }],
+    "@typescript-eslint/ban-ts-comment": "warn"
+  }
 };
