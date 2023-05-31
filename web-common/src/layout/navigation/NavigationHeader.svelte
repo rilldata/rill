44ac2fd78550034e5e8be234f01b77a174a74eb3
Eric P Green <ericpgreen2@gmail.com>
2023-05-31 15:46:50 -0700

    Remove "Add [asset]" context button from headers

--- web-common/src/layout/navigation/NavigationHeader.svelte
+++ web-common/src/layout/navigation/NavigationHeader.svelte
@@ -1,19 +1,10 @@
 <script lang="ts">
-  import ContextButton from "@rilldata/web-common/components/column-profile/ContextButton.svelte";
-  import AddIcon from "@rilldata/web-common/components/icons/Add.svelte";
-  import { createEventDispatcher } from "svelte";
   import { slide } from "svelte/transition";
   import CollapsibleSectionTitle from "../CollapsibleSectionTitle.svelte";
   import { LIST_SLIDE_DURATION } from "../config";
 
   export let show = true;
-  export let tooltipText: string;
-  export let toggleText = "models";
-  /** The CSS ID used for tests for the context button */
-  export let contextButtonID: string = undefined;
-  export let canAddAsset = true;
-
-  const dispatch = createEventDispatcher();
+  export let toggleText: string;
 </script>
 
 <div
@@ -26,18 +17,4 @@
       <slot />
     </div>
   </CollapsibleSectionTitle>
-  {#if canAddAsset}
-    <ContextButton
-      id={contextButtonID}
-      {tooltipText}
-      on:click={() => {
-        dispatch("add");
-      }}
-      width={24}
-      height={24}
-      rounded
-    >
-      <AddIcon />
-    </ContextButton>
-  {/if}
 </div>
