d347dee39f825f6ea37ab548910568234c1364fe
Eric P Green <ericpgreen2@gmail.com>
2023-06-02 16:06:50 -0700

    Add 'Add data' button

--- web-common/src/features/onboarding/OnboardingWorkspace.svelte
+++ web-common/src/features/onboarding/OnboardingWorkspace.svelte
@@ -1,33 +1,45 @@
 <script lang="ts">
+  import AddSourceModal from "@rilldata/web-common/features/sources/add-source/AddSourceModal.svelte";
+  import Button from "../../components/button/Button.svelte";
   import { WorkspaceContainer } from "../../layout/workspace";
 
   interface OnboardingStep {
+    id: string;
     heading: string;
     description: string;
   }
 
   const steps: OnboardingStep[] = [
     {
+      id: "source",
       heading: "Import your data source",
       description:
         "Click 'Add data' or drag a file (Parquet, NDJSON, or CSV) into this window.",
     },
     {
+      id: "model",
       heading: "Model your sources into one big table",
       description:
         "Build intuition about your sources and use SQL to model them into an analytics-ready resource.",
     },
     {
+      id: "metrics",
       heading: "Define your metrics and dimensions",
       description:
         "Define aggregate metrics and break out dimensions for your modeled data.",
     },
     {
+      id: "dashboard",
       heading: "Explore your metrics dashboard",
       description:
         "Interactively explore line charts and leaderboards to uncover insights.",
     },
   ];
+
+  let showAddSourceModal = false;
+  function openAddSourceModal() {
+    showAddSourceModal = true;
+  }
 </script>
 
 <WorkspaceContainer top="0px" assetID="onboarding" inspector={false}>
@@ -42,12 +54,26 @@
       {#each steps as step, i (step.heading)}
         <li class="flex gap-x-0.5">
           <span class="font-bold">{i + 1}.</span>
-          <div class="flex flex-col items-start gap-y-0.5">
-            <h5 class="font-bold">{step.heading}</h5>
-            <p>{step.description}</p>
+          <div class="flex flex-col items-start gap-y-2">
+            <div class="flex flex-col items-start gap-y-0.5">
+              <h5 class="font-bold">{step.heading}</h5>
+              <p>{step.description}</p>
+            </div>
+            {#if step.id === "source"}
+              <Button type="secondary" on:click={openAddSourceModal}
+                >Add data</Button
+              >
+            {/if}
           </div>
         </li>
       {/each}
     </ol>
+    {#if showAddSourceModal}
+      <AddSourceModal
+        on:close={() => {
+          showAddSourceModal = false;
+        }}
+      />
+    {/if}
   </div>
 </WorkspaceContainer>
