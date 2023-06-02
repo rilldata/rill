43111ffbccabd16acba991e58ff730a9ebc6c976
Eric P Green <ericpgreen2@gmail.com>
2023-06-02 15:55:39 -0700

    Clean up onboarding screen

--- web-common/src/features/onboarding/OnboardingWorkspace.svelte
+++ web-common/src/features/onboarding/OnboardingWorkspace.svelte
@@ -1,67 +1,53 @@
 <script lang="ts">
-  import Explore from "@rilldata/web-common/components/icons/Explore.svelte";
-  import Metrics from "@rilldata/web-common/components/icons/Metrics.svelte";
-  import Model from "@rilldata/web-common/components/icons/Model.svelte";
-  import Source from "@rilldata/web-common/components/icons/Source.svelte";
-  import { featureFlags } from "@rilldata/web-common/features/feature-flags";
   import { WorkspaceContainer } from "../../layout/workspace";
 
-  const dataModelerSteps = [
+  interface OnboardingStep {
+    heading: string;
+    description: string;
+  }
+
+  const steps: OnboardingStep[] = [
     {
       heading: "Import your data source",
       description:
-        "Add to your sources by clicking on the + icon, or by dragging a csv or parquet file to this window.",
-      icon: Source,
+        "Click 'Add data' or drag a file (Parquet, NDJSON, or CSV) into this window.",
     },
     {
       heading: "Model your sources into one big table",
       description:
         "Build intuition about your sources and use SQL to model them into an analytics-ready resource.",
-      icon: Model,
     },
     {
       heading: "Define your metrics and dimensions",
       description:
         "Define aggregate metrics and break out dimensions for your modeled data.",
-      icon: Metrics,
     },
-  ];
-
-  const dashboardSteps = [
     {
       heading: "Explore your metrics dashboard",
       description:
         "Interactively explore line charts and leaderboards to uncover insights.",
-      icon: Explore,
     },
   ];
-
-  $: isModelerEnabled = $featureFlags.readOnly === false;
-  $: steps = isModelerEnabled
-    ? [...dataModelerSteps, ...dashboardSteps]
-    : dashboardSteps;
 </script>
 
 <WorkspaceContainer top="0px" assetID="onboarding" inspector={false}>
-  <div class="mt-10 p-2 place-content-center h-screen" slot="body">
+  <div class="pt-20 px-8 flex flex-col gap-y-6 items-center" slot="body">
     <div class="text-center">
       <div class="font-bold">Getting started</div>
       <p>Building data intuition at every step of analysis</p>
     </div>
-    <div class="p-5 pt-2">
-      {#each steps as step (step.heading)}
-        <div
-          class="flex items-center p-6 mt-3 bg-gray-50 rounded-lg border border-gray-200"
-        >
-          <div>
-            <svelte:component this={step.icon} color="grey" size="3em" />
-          </div>
-          <div class="ml-5">
+    <ol
+      class="w-full lg:w-2/3 xl:w-1/2 2xl:w-2/5 flex flex-col gap-y-4 px-9 pt-9 pb-[60px] bg-gray-50 rounded-lg border border-gray-200"
+    >
+      {#each steps as step, i (step.heading)}
+        <li class="flex gap-x-0.5">
+          <span class="font-bold">{i + 1}.</span>
+          <div class="flex flex-col items-start gap-y-0.5">
             <h5 class="font-bold">{step.heading}</h5>
             <p>{step.description}</p>
           </div>
-        </div>
+        </li>
       {/each}
-    </div>
+    </ol>
   </div>
 </WorkspaceContainer>
