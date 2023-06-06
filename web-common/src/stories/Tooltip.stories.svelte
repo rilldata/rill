0626e33b60946df45d57527b3f57ad7b67c28c11
brendan colloran <brendan@colloran.net>
2023-06-06 07:37:51 -0700

    expand tooltip storied

--- web-common/src/stories/Tooltip.stories.svelte
+++ web-common/src/stories/Tooltip.stories.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
 
-  // import { FormattedDataType } from "@rilldata/web-common/components/data-types";
   import Shortcut from "@rilldata/web-common/components/tooltip/Shortcut.svelte";
   import StackingWord from "@rilldata/web-common/components/tooltip/StackingWord.svelte";
   import TooltipContent from "@rilldata/web-common/components/tooltip/TooltipContent.svelte";
@@ -14,8 +13,8 @@
 <Template let:args>
   <TooltipContent maxWidth="360px">
     <TooltipTitle>
-      <div slot="name">Name</div>
-      <div slot="description">description</div>
+      <div slot="name">{args.name}</div>
+      <div slot="description">{args.description}</div>
     </TooltipTitle>
     <TooltipShortcutContainer>
       <div>
@@ -28,8 +27,9 @@
   </TooltipContent>
 </Template>
 
-<Story name="Rounded" args={{ rounded: true }} />
+<Story name="base case" args={{ name: "name", description:"description" }} />
 
-<Story name="Square" source args={{ rounded: false }} />
+<Story name="long name" args={{ name: "A_very_long_string_in_the_name_field_that_has_no_whitespace_like_a_terrible_database_column_name" ,description:"description"}} />
 
-<!-- Dynamic snippet should be disabled for this story -->
+<Story name="long description" source args={{ name:"name",description: "A long string in the description field that does have whitespace like a longish string in a database field" }} />
+
