8aecb3967c88bdd3690e6ab2dbed59a9f11612f2
brendan colloran <brendan@colloran.net>
2023-06-06 06:41:19 -0700

    Minimal setup to get storybook running in web-common

---
+++ web-common/.storybook/preview.ts
@@ -0,0 +1,20 @@
+// import "tailwindcss/tailwind.css";
+import "../src/app.css";
+// import "../static/app.css";
+import "../static/fonts/fonts.css";
+
+import type { Preview } from "@storybook/svelte";
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
---
+++ web-common/src/stories/Tooltip.stories.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
+
+  // import { FormattedDataType } from "@rilldata/web-common/components/data-types";
+  import Shortcut from "@rilldata/web-common/components/tooltip/Shortcut.svelte";
+  import StackingWord from "@rilldata/web-common/components/tooltip/StackingWord.svelte";
+  import TooltipContent from "@rilldata/web-common/components/tooltip/TooltipContent.svelte";
+  import TooltipShortcutContainer from "@rilldata/web-common/components/tooltip/TooltipShortcutContainer.svelte";
+  import TooltipTitle from "@rilldata/web-common/components/tooltip/TooltipTitle.svelte";
+</script>
+
+<Meta title="tooltip template" />
+
+<Template let:args>
+  <TooltipContent maxWidth="360px">
+    <TooltipTitle>
+      <div slot="name">Name</div>
+      <div slot="description">description</div>
+    </TooltipTitle>
+    <TooltipShortcutContainer>
+      <div>
+        <StackingWord key="shift">Copy</StackingWord> this value to clipboard
+      </div>
+      <Shortcut>
+        <span style="font-family: var(--system);">⇧</span> + Click
+      </Shortcut>
+    </TooltipShortcutContainer>
+  </TooltipContent>
+</Template>
+
+<Story name="Rounded" args={{ rounded: true }} />
+
+<Story name="Square" source args={{ rounded: false }} />
+
+<!-- Dynamic snippet should be disabled for this story -->
