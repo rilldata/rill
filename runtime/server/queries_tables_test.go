f9e087399a4c6bf683c4a2a4168fbecef7288c40
Benjamin Egelund-Müller <b@egelund-muller.com>
2023-06-07 12:20:53 +0200

    Retry initial JWKS fetch (#2577)
    * Retry initial JWKS fetch
    
    * Fix tests

--- runtime/server/queries_tables_test.go
+++ runtime/server/queries_tables_test.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"context"
 	"testing"
 
 	"github.com/jmoiron/sqlx"
@@ -112,7 +113,7 @@ func getTableTestServer(t *testing.T) (*Server, string) {
 		SELECT 1::int AS a, 10::int AS "b""b"
 	`)
 
-	server, err := NewServer(&Options{}, rt, nil)
+	server, err := NewServer(context.Background(), &Options{}, rt, nil)
 	require.NoError(t, err)
 
 	return server, instanceID
@@ -121,7 +122,7 @@ func getTableTestServer(t *testing.T) (*Server, string) {
 func getTableTestServerWithSql(t *testing.T, sql string) (*Server, string) {
 	rt, instanceID := testruntime.NewInstanceWithModel(t, "test", sql)
 
-	server, err := NewServer(&Options{}, rt, nil)
+	server, err := NewServer(context.Background(), &Options{}, rt, nil)
 	require.NoError(t, err)
 
 	return server, instanceID
@@ -142,7 +143,7 @@ func getTableTestServerWithEmptyModel(t *testing.T) (*Server, string) {
 		SELECT 1::int AS a, 10::int AS "b""b" where 1<>1
 	`)
 
-	server, err := NewServer(&Options{}, rt, nil)
+	server, err := NewServer(context.Background(), &Options{}, rt, nil)
 	require.NoError(t, err)
 
 	return server, instanceID
