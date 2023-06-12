dd2c6f90da548c61ab2fd3c5496ba6ff315b6530
Benjamin Egelund-Müller <b@egelund-muller.com>
2023-06-12 19:35:46 +0200

    Set export limit to 10k by default

--- runtime/server/downloads.go
+++ runtime/server/downloads.go
@@ -19,6 +19,10 @@ func (s *Server) Export(ctx context.Context, req *runtimev1.ExportRequest) (*run
 		return nil, ErrForbidden
 	}
 
+	if req.Limit <= 0 {
+		req.Limit = 10000
+	}
+
 	r, err := proto.Marshal(req)
 	if err != nil {
 		return nil, err
@@ -42,6 +46,11 @@ func (s *Server) downloadHandler(w http.ResponseWriter, req *http.Request) {
 	err = proto.Unmarshal(marshalled, request)
 	if err != nil {
 		http.Error(w, fmt.Sprintf("failed to parse request: %s", err), http.StatusBadRequest)
+		return
+	}
+
+	if request.Limit > 10000 {
+		http.Error(w, "limit must be less than or equal to 10000", http.StatusBadRequest)
 		return
 	}
 
