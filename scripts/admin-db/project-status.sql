5911d54784e48ea82dd64abf9ac09b75d060cfd4
Kasper Sjørslev <kaspersjo@gmail.com>
2023-06-12 16:57:22 +0200

    Add slots to project status query

--- scripts/admin-db/project-status.sql
+++ scripts/admin-db/project-status.sql
@@ -1,9 +1,10 @@
 SELECT
     o.name AS org_name,
     p.name AS project_name,
+    p.prod_slots,
     d.status,
     d.updated_on,
     d.logs
 FROM deployments d
 JOIN projects p ON d.project_id=p.id
-JOIN orgs o on p.org_id=o.id;
+JOIN orgs o ON p.org_id=o.id;
