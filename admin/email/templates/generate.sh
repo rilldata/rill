5c6d8ecb24caeb91e52072734889fea278aac10c
Benjamin Egelund-Müller <b@egelund-muller.com>
2023-06-15 13:26:33 +0200

    Fix image loading

--- admin/email/templates/generate.sh
+++ admin/email/templates/generate.sh
@@ -2,4 +2,4 @@
 npm install -g mjml
 rm -rf admin/email/templates/gen
 mkdir -p admin/email/templates/gen
-mjml admin/email/templates/*.mjml --config.minify -o admin/email/templates/gen/
+mjml admin/email/templates/*.mjml -o admin/email/templates/gen/
