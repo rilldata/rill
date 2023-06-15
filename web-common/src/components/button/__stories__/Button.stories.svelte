ed51d51d5a5aea29812087d99ae4af31d75011e6
bcolloran <brendan@colloran.net>
2023-06-15 12:35:39 -0700

    adding stories for buttons, and adding mock svelteKit $app/environment in storybook (#2611)
    * adding stories for buttons, and adding mock svelteKit $app/environment in storybook
    
    * cleanup
    
    * fix package-lock.json
    
    * rebuilding package-lock.json again
    
    * try package-lock again
    
    * try updating vite.config.ts to fix build
    
    * revert package-lock to match main
    
    * remove fixme comment
    
    * fix vite config

---
+++ web-common/src/components/button/__stories__/Button.stories.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+  import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
+
+  import Button from "@rilldata/web-common/components/button/Button.svelte";
+
+  type ButtonProps = {
+    type: "primary" | "secondary" | "text";
+    status: "info" | "error";
+    disabled: boolean;
+    compact: boolean;
+    label: string;
+  };
+
+  const buttonProps: ButtonProps[] = [];
+
+  for (const type of ["primary", "secondary", "text"]) {
+    for (const status of ["info", "error"]) {
+      for (const disabled of [true, false]) {
+        for (const compact of [true, false]) {
+          buttonProps.push({
+            type,
+            status,
+            disabled,
+            compact,
+            label: `${type} ${status} ${disabled} ${compact}`,
+          });
+        }
+      }
+    }
+  }
+</script>
+
+<Meta title="Button stories" />
+
+<Template let:args>
+  <table>
+    <tr>
+      <td />
+      <td>type</td>
+      <td>status</td>
+      <td>disabled</td>
+      <td>compact</td>
+    </tr>
+    {#each buttonProps as props}
+      <tr>
+        <td><Button {...props}>Button</Button></td>
+        <td>{props.type}</td>
+        <td>{props.status}</td>
+        <td>{props.disabled}</td>
+        <td>{props.compact}</td>
+      </tr>
+    {/each}
+  </table>
+</Template>
+
+<Story name="all button variations" />
+
+<style>
+  td {
+    padding: 5px;
+  }
+  td:first-child {
+    padding-right: 40px;
+  }
+</style>
