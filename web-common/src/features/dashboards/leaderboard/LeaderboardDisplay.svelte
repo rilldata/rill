c777a3846d0852c12305a9157cf27b7552bd8106
Eric P Green <ericpgreen2@gmail.com>
2023-06-18 23:29:01 -0700

    Fix length is `undefined` error (#2641)

--- web-common/src/features/dashboards/leaderboard/LeaderboardDisplay.svelte
+++ web-common/src/features/dashboards/leaderboard/LeaderboardDisplay.svelte
@@ -57,7 +57,7 @@
     {
       query: {
         enabled:
-          selectedMeasureNames.length > 0 &&
+          selectedMeasureNames?.length > 0 &&
           (hasTimeSeries ? !!timeStart && !!timeEnd : true) &&
           !!$dashboardStore?.filters,
       },
