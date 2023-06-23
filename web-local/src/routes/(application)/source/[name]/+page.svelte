135e0fa8ea9f20627b9296b62cbb38c4111aa107
Eric P Green <ericpgreen2@gmail.com>
2023-06-22 18:35:17 -0700

    Show invalid sources

--- web-local/src/routes/(application)/source/[name]/+page.svelte
+++ web-local/src/routes/(application)/source/[name]/+page.svelte
@@ -4,10 +4,7 @@
   import { EntityType } from "@rilldata/web-common/features/entity-management/types";
   import { featureFlags } from "@rilldata/web-common/features/feature-flags";
   import { SourceWorkspace } from "@rilldata/web-common/features/sources";
-  import {
-    createRuntimeServiceGetCatalogEntry,
-    createRuntimeServiceGetFile,
-  } from "@rilldata/web-common/runtime-client";
+  import { createRuntimeServiceGetFile } from "@rilldata/web-common/runtime-client";
   import { runtime } from "@rilldata/web-common/runtime-client/runtime-store";
   import { error } from "@sveltejs/kit";
   import { onMount } from "svelte";
@@ -19,21 +16,6 @@
       throw error(404, "Page not found");
     }
   });
-
-  // try to get the catalog entry.
-  $: catalogQuery = createRuntimeServiceGetCatalogEntry(
-    $runtime.instanceId,
-    sourceName,
-    {
-      query: {
-        onError: () => {
-          // no-op. we'll try to get the file below.
-        },
-      },
-    }
-  );
-  $: embedded = $catalogQuery.data?.entry?.embedded;
-  $: path = $catalogQuery.data?.entry?.source.properties.path;
 
   $: fileQuery = createRuntimeServiceGetFile(
     $runtime.instanceId,
@@ -54,9 +36,9 @@
 </script>
 
 <svelte:head>
-  <title>Rill Developer | {embedded ? path : sourceName}</title>
+  <title>Rill Developer | {sourceName}</title>
 </svelte:head>
 
-{#if $fileQuery.data && $catalogQuery.data}
-  <SourceWorkspace {sourceName} {embedded} {path} />
+{#if $fileQuery.data}
+  <SourceWorkspace {sourceName} embedded={false} />
 {/if}
