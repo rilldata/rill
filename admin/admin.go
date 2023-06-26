6abba424eebdc1fd365bbf7d184ad12f4194908d
Benjamin Egelund-Müller <b@egelund-muller.com>
2023-06-26 20:12:17 +0200

    Try again

--- admin/admin.go
+++ admin/admin.go
@@ -3,12 +3,11 @@ package admin
 import (
 	"context"
 	"github.com/rilldata/rill/admin/database"
-	"sync"
-	"github.com/rilldata/rill/admin/database"
 	"github.com/rilldata/rill/admin/email"
 	"github.com/rilldata/rill/admin/provisioner"
 	"github.com/rilldata/rill/runtime/server/auth"
 	"go.uber.org/zap"
+	"sync"
 )
 
 type Options struct {
