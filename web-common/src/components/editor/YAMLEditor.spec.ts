d84161a0c3c563bd62a4f49b6cf08828a8c065f3
Hamilton Ulmer <hamilton.ulmer@gmail.com>
2023-07-05 06:41:35 -0700

    fix YAMLEditor.spec.ts

--- web-common/src/components/editor/YAMLEditor.spec.ts
+++ web-common/src/components/editor/YAMLEditor.spec.ts
@@ -3,51 +3,56 @@ import { tick } from "svelte";
 import { beforeAll, describe, expect, it, vi } from "vitest";
 import YAMLEditor from "./YAMLEditor.svelte";
 
+function getLines(container: HTMLElement) {
+  return Array.from(container.querySelectorAll(".cm-line")).map(
+    (l) => l.textContent
+  );
+}
+
 describe("YAMLEditor.svelte", () => {
   beforeAll(() => {
-    HTMLElement.prototype.getClientRects = vi.fn<DOMRect[]>(() => {
-      return [
-        {
-          width: 120,
-          height: 120,
-          top: 0,
-          left: 0,
-          bottom: 0,
-          right: 0,
-          item: null,
-        },
-      ];
-    });
+    document.createRange = () => {
+      const range = new Range();
+
+      range.getBoundingClientRect = vi.fn();
+
+      range.getClientRects = () => {
+        return {
+          item: () => null,
+          length: 0,
+          [Symbol.iterator]: vi.fn(),
+        };
+      };
+
+      return range;
+    };
   });
 
   it("starts with an editor with one line & updates to content prop creates more lines", async () => {
     const { component, unmount, container } = render(YAMLEditor, {
       content: "",
     });
+    const propMap = component?.$$?.props;
+    const viewInstance = component.$$.ctx[propMap["view"]];
 
-    // cm-content should have a single <div> element, reflecting that there's no content.
-    let elems = container.querySelector(".cm-content").children;
-    expect(elems?.length).toBe(1);
-    component.$set({
-      content: "name: test\nfield: whatever\nanother_field: another",
-    });
-    elems = container.querySelector(".cm-content").children;
-    await tick();
-    expect(elems?.length).toBe(3);
-    unmount();
-  });
+    expect(getLines(container)).toHaveLength(1);
 
-  it("successfully fires the state update functions when the content changes", async () => {
-    const update = vi.fn();
-    const { component, unmount } = render(YAMLEditor, {
-      content: "",
-      stateFieldUpdaters: [update],
-    });
-    component.$set({
-      content: "name: test\nfield: whatever\nanother_field: another",
+    const onUpdate = vi.fn();
+    component?.$on("update", onUpdate);
+
+    const content = "foo: 10\nbar: 20\nfoo: 10\nbar: 20";
+
+    viewInstance.dispatch({
+      changes: {
+        from: 0,
+        to: 0,
+        insert: content,
+      },
     });
     await tick();
-    expect(update).toHaveBeenCalled();
+
+    expect(getLines(container)).toHaveLength(4);
+    expect(onUpdate).toHaveBeenCalledOnce();
     unmount();
   });
 });
