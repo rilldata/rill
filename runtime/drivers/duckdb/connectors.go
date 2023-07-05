e5099de0c26b077f13cc952ab705f4dd88a1d756
Rakesh Sharma <rakeshsharma14317@gmail.com>
2023-07-05 16:11:19 +0530

    added EstimateSize in OLAPStore

--- runtime/drivers/duckdb/connectors.go
+++ runtime/drivers/duckdb/connectors.go
@@ -43,6 +43,20 @@ func (c *connection) Ingest(ctx context.Context, env *connectors.Env, source *co
 	return summary, err
 }
 
+func (c *connection) EstimateSize() (int64, bool) {
+	var paths []string
+	path := c.config.DSN
+	if c.config.DSN == "" {
+		return 0, false
+	}
+
+	// Add .wal file path (e.g final size will be sum of *.db and *.db.wal)
+	dbWalPath := fmt.Sprintf("%s.wal", path)
+	paths = append(paths, path, dbWalPath)
+	c.logger.Info("duckdb file paths", zap.Strings("paths", paths), zap.Int64("size", fileSize(paths)))
+	return fileSize(paths), true
+}
+
 func (c *connection) ingest(ctx context.Context, env *connectors.Env, source *connectors.Source) (*drivers.IngestionSummary, error) {
 	// Driver-specific overrides
 	if source.Connector == "local_file" {
