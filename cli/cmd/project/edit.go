31688e22e6be871e586632c595b247418312722b
Rakesh Sharma <rakeshsharma14317@gmail.com>
2023-07-07 13:29:56 +0530

    Implement deployment hibernation (#2363)
    * initial commit
    
    * added func to check for expired deployments and de-provision them
    
    * fmt
    
    * go mod tidy
    
    * review changes
    
    * hibernation deployment job
    
    * small changes
    
    * TriggerReprovision added
    
    * fix unused opts in admin.go
    
    * Add status chip for `HIBERNATED` state
    
    * review changes
    
    * merging issue
    
    * hibernation job time
    
    * nit
    
    * review changes
    
    * reverted ui changes for hibernation
    
    * nit
    
    * review changes
    
    * added sync.WaitGroup for flusher
    
    * Review
    
    * Run prettier
    
    * Review part 2
    
    * Move logic to a custom hook
    
    * Add redirect for all routes under `/org/project`
    
    ---------
    
    Co-authored-by: Eric P Green <ericpgreen2@gmail.com>
    Co-authored-by: Benjamin Egelund-Müller <b@egelund-muller.com>

--- cli/cmd/project/edit.go
+++ cli/cmd/project/edit.go
@@ -13,6 +13,7 @@ func EditCmd(cfg *config.Config) *cobra.Command {
 	var name, description, prodBranch, path, region string
 	var public bool
 	var slots int
+	var prodTTL int64
 
 	editCmd := &cobra.Command{
 		Use:   "edit [<project-name>]",
@@ -71,6 +72,11 @@ func EditCmd(cfg *config.Config) *cobra.Command {
 				req.Public = &public
 			}
 
+			if cmd.Flags().Changed("prod-ttl-seconds") {
+				promptFlagValues = false
+				req.ProdTtlSeconds = &prodTTL
+			}
+
 			if promptFlagValues {
 				resp, err := client.GetProject(ctx, &adminv1.GetProjectRequest{OrganizationName: cfg.Org, Name: name})
 				if err != nil {
@@ -114,6 +120,7 @@ func EditCmd(cfg *config.Config) *cobra.Command {
 	editCmd.Flags().BoolVar(&public, "public", false, "Make dashboards publicly accessible")
 	editCmd.Flags().StringVar(&path, "path", ".", "Project directory")
 	editCmd.Flags().IntVar(&slots, "prod-slots", 0, "Slots to allocate for production deployments (default: current slots)")
+	editCmd.Flags().Int64Var(&prodTTL, "prod-ttl-seconds", 0, "Prod deployment TTL in seconds")
 	editCmd.Flags().StringVar(&region, "region", "", "Deployment region (default: current region)")
 	if !cfg.IsDev() {
 		if err := editCmd.Flags().MarkHidden("prod-slots"); err != nil {
