1bee9e070c45b80aefc95aa5ce70dede356774c7
Speros Kokenes <speroskokenes@gmail.com>
2023-07-14 11:25:53 -0400

    proto wip

--- web-local/src/routes/(application)/dashboard/[name]/table/ProtoTable.svelte
+++ web-local/src/routes/(application)/dashboard/[name]/table/ProtoTable.svelte
@@ -1,11 +1,91 @@
 <script lang="ts">
   import { getStateManagers } from "@rilldata/web-common/features/dashboards/state-managers/state-managers";
-  import { createSvelteTable } from "@tanstack/svelte-table";
-
-  // const table = createSvelteTable({});
+  import {
+    createSvelteTable,
+    ColumnDef,
+    TableOptions,
+    getCoreRowModel,
+    flexRender,
+  } from "@tanstack/svelte-table";
+  import { getTableData } from "./selectors";
+  import { writable } from "svelte/store";
 
   const managers = getStateManagers();
-  // console.log(createSvelteTable);
+  const tableData = getTableData(managers);
+
+  const options = writable<TableOptions<any>>({
+    data: [],
+    columns: [],
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  $: {
+    const columns = $tableData.meta.map((field) => {
+      return {
+        // header: either string, function, or component for rendering header
+        // cell: either string, function, or component for rendering cell
+
+        // Method for getting underlying value. Can also use accessorFn or accessorKey
+        accessorKey: field.name,
+      };
+    });
+    $options.columns = columns;
+    $options.data = $tableData.data;
+  }
+
+  const table = createSvelteTable(options);
 </script>
 
-hello world
+<div>
+  <table>
+    <thead>
+      {#each $table.getHeaderGroups() as headerGroup}
+        <tr>
+          {#each headerGroup.headers as header}
+            <th>
+              {#if !header.isPlaceholder}
+                <svelte:component
+                  this={flexRender(
+                    header.column.columnDef.header,
+                    header.getContext()
+                  )}
+                />
+              {/if}
+            </th>
+          {/each}
+        </tr>
+      {/each}
+    </thead>
+    <tbody>
+      {#each $table.getRowModel().rows as row}
+        <tr>
+          {#each row.getVisibleCells() as cell}
+            <td>
+              <svelte:component
+                this={flexRender(cell.column.columnDef.cell, cell.getContext())}
+              />
+            </td>
+          {/each}
+        </tr>
+      {/each}
+    </tbody>
+    <tfoot>
+      {#each $table.getFooterGroups() as footerGroup}
+        <tr>
+          {#each footerGroup.headers as header}
+            <th>
+              {#if !header.isPlaceholder}
+                <svelte:component
+                  this={flexRender(
+                    header.column.columnDef.footer,
+                    header.getContext()
+                  )}
+                />
+              {/if}
+            </th>
+          {/each}
+        </tr>
+      {/each}
+    </tfoot>
+  </table>
+</div>
--- web-local/src/routes/(application)/dashboard/[name]/table/selectors.ts
+++ web-local/src/routes/(application)/dashboard/[name]/table/selectors.ts
@@ -1,3 +1,40 @@
 import type { StateManagers } from "@rilldata/web-common/features/dashboards/state-managers/state-managers";
+import { createQueryServiceMetricsViewRows } from "@rilldata/web-common/runtime-client";
+import { derived } from "svelte/store";
 
-export function getColumnDefs(managers: StateManagers) {}
+export function getTableData(managers: StateManagers) {
+  // const metricViewName = get(managers.metricsViewName);
+  // const runtime =
+  return derived(
+    [managers.dashboardStore, managers.metricsViewName, managers.runtime],
+    ([dashboardStore, metricsViewName, runtime], set) => {
+      const query = createQueryServiceMetricsViewRows(
+        runtime.instanceId,
+        metricsViewName,
+        {
+          limit: 100,
+          //   filter: dashboardStore.filters,
+          //   timeStart: hasTimeSeries ? timeStart : undefined,
+          //   timeEnd: hasTimeSeries ? timeEnd : undefined,
+        },
+        {
+          query: {
+            enabled: true,
+            queryClient: managers.queryClient,
+            //   (hasTimeSeries ? !!timeStart && !!timeEnd : true) &&
+            //   !!$dashboardStore?.filters,
+          },
+        }
+      );
+
+      return query.subscribe((q) => {
+        if (q.data) set(q.data);
+        else
+          set({
+            data: [],
+            meta: [],
+          });
+      });
+    }
+  );
+}
