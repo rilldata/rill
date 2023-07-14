c15ae47fd6083075218929613b97f0b102c0fa01
Eric P Green <ericpgreen2@gmail.com>
2023-07-14 14:00:06 -0400

    Make not-saved indicator responsive to new server state

--- web-common/src/features/sources/editor/SourceNotSavedIndicator.svelte
+++ web-common/src/features/sources/editor/SourceNotSavedIndicator.svelte
@@ -1,14 +1,23 @@
 <script lang="ts">
+  import { createRuntimeServiceGetFile } from "../../../runtime-client";
   import { runtime } from "../../../runtime-client/runtime-store";
+  import { getFilePathFromNameAndType } from "../../entity-management/entity-mappers";
+  import { EntityType } from "../../entity-management/types";
   import { useIsSourceNotSaved } from "../selectors";
   import { useSourceStore } from "../sources-store";
 
   export let sourceName: string;
 
-  $: sourceStore = useSourceStore(sourceName);
+  const sourceStore = useSourceStore();
 
-  // Include `clientYAML` in the reactive statement to recompute the value whenever `clientYAML` changes
+  // Include `$file.dataUpdatedAt` and `clientYAML` in the reactive statement to recompute
+  // the `isSourceNotSaved` value whenever they change
+  const file = createRuntimeServiceGetFile(
+    $runtime.instanceId,
+    getFilePathFromNameAndType(sourceName, EntityType.Table)
+  );
   $: isSourceNotSaved =
+    $file.dataUpdatedAt &&
     $sourceStore.clientYAML &&
     useIsSourceNotSaved($runtime.instanceId, sourceName);
 </script>
