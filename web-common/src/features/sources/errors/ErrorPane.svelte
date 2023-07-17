fbba872c15792d3a3ebc10f9c0bec4fd6713275a
Eric P Green <ericpgreen2@gmail.com>
2023-07-17 16:26:08 -0400

    Fix width

--- web-common/src/features/sources/errors/ErrorPane.svelte
+++ web-common/src/features/sources/errors/ErrorPane.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div
-  class="w-full h-full m-4 bg-white rounded border border-gray-200 flex-col justify-center inline-flex"
+  class="h-full m-4 bg-white rounded border border-gray-200 flex-col justify-center inline-flex"
 >
   <div class="flex-col justify-start items-center gap-1 flex text-red-500">
     <CancelCircle size="24px" />
