dcfb7261d6d5c9ad1cfa4b196f1e2710364c841f
brendan colloran <brendan@colloran.net>
2023-07-18 15:30:44 -0700

    top k list item should never have a  long bar (values always in [0,1] since it's a percent of rows count)

--- web-common/src/components/column-profile/column-types/details/TopKListItem.svelte
+++ web-common/src/components/column-profile/column-types/details/TopKListItem.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import BarAndLabel from "@rilldata/web-common/components/BarAndLabel.svelte";
   import { createEventDispatcher } from "svelte";
-  import { fly, slide } from "svelte/transition";
+  import { slide } from "svelte/transition";
 
   export let value: number; // should be between 0 and 1.
   export let color = "bg-blue-200 dark:bg-blue-600";
@@ -21,14 +21,6 @@
     hovered = false;
     dispatch("blur");
   };
-  /** used for overly-large bar values */
-  let zigZag =
-    "M" +
-    Array.from({ length: 7 })
-      .map((_, i) => {
-        return `${15 - 4 * (i % 2)} ${1.7 * (i * 2)}`;
-      })
-      .join(" L");
 </script>
 
 <button
@@ -75,25 +67,6 @@
     </div>
   </BarAndLabel>
 </button>
-<!-- if the value is greater than 100%, we should add this little serration -->
-{#if value > 1.001}
-  <div
-    style="position: relative"
-    transition:fly|local={{ duration: 200, x: 20 }}
-  >
-    <svg
-      style="
-      position:absolute;
-      right: 0px;
-      transform: translateY(-18px);
-    "
-      width="15"
-      height="22"
-    >
-      <path d={zigZag} fill="white" />
-    </svg>
-  </div>
-{/if}
 
 <style>
   .leaderboard-entry {
