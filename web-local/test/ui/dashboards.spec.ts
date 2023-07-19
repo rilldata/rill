9691d62084fe70094da2c98ef765f24fcbc7e76b
brendan colloran <brendan@colloran.net>
2023-07-18 21:20:51 -0700

    trying an even higher value

--- web-local/test/ui/dashboards.spec.ts
+++ web-local/test/ui/dashboards.spec.ts
@@ -436,7 +436,7 @@ describe("dashboards", () => {
     await updateMetricsInput(page, docWithCompleteMeasure);
     await playwrightExpect(
       page.getByRole("button", { name: "Go to dashboard" })
-    ).toBeEnabled({ timeout: 20000 });
+    ).toBeEnabled({ timeout: 60000 });
 
     // Go to dashboard
     await page.getByRole("button", { name: "Go to dashboard" }).click();
