f9ff95a549f6d9be3d6621e84caf4d7f8bb34559
Eric P Green <ericpgreen2@gmail.com>
2023-07-20 14:51:53 -0400

    Remove unused code

--- web-common/src/features/models/workspace/inspector/ModelInspectorModelProfile.svelte
+++ web-common/src/features/models/workspace/inspector/ModelInspectorModelProfile.svelte
@@ -1,29 +1,13 @@
 <script lang="ts">
   import ColumnProfile from "@rilldata/web-common/components/column-profile/ColumnProfile.svelte";
-  import { getFilePathFromNameAndType } from "@rilldata/web-common/features/entity-management/entity-mappers";
-  import { EntityType } from "@rilldata/web-common/features/entity-management/types";
-  // import type { QueryHighlightState } from "@rilldata/web-common/features/models/query-highlight-store";
   import CollapsibleSectionTitle from "@rilldata/web-common/layout/CollapsibleSectionTitle.svelte";
   import { LIST_SLIDE_DURATION } from "@rilldata/web-common/layout/config";
-  import {
-    createQueryServiceTableCardinality,
-    createRuntimeServiceGetCatalogEntry,
-    createRuntimeServiceGetFile,
-    createRuntimeServiceListCatalogEntries,
-  } from "@rilldata/web-common/runtime-client";
-  //import { getContext } from "svelte";
-  import { derived, writable } from "svelte/store";
+  import { createRuntimeServiceGetCatalogEntry } from "@rilldata/web-common/runtime-client";
   import { slide } from "svelte/transition";
   import { runtime } from "../../../../runtime-client/runtime-store";
-  import { getTableReferences } from "../../utils/get-table-references";
   import References from "./References.svelte";
-  import { combineEntryWithReference } from "./utils";
 
   export let modelName: string;
-
-  //  const queryHighlight: Writable<QueryHighlightState> = getContext(
-  //    "rill:app:query-highlight"
-  //  );
 
   $: getModel = createRuntimeServiceGetCatalogEntry(
     $runtime?.instanceId,
@@ -33,88 +17,7 @@
   // refresh entry value only if the data has changed
   $: entry = $getModel?.data?.entry || entry;
 
-  $: getModelFile = createRuntimeServiceGetFile(
-    $runtime?.instanceId,
-    getFilePathFromNameAndType(modelName, EntityType.Model)
-  );
-
-  $: references = getTableReferences($getModelFile?.data.blob ?? "");
-  $: getAllSources = createRuntimeServiceListCatalogEntries(
-    $runtime?.instanceId,
-    {
-      type: "OBJECT_TYPE_SOURCE",
-    }
-  );
-
-  $: getAllModels = createRuntimeServiceListCatalogEntries(
-    $runtime?.instanceId,
-    {
-      type: "OBJECT_TYPE_MODEL",
-    }
-  );
-
-  // for each reference, match to an existing model or source,
-  $: referencedThings = derived(
-    [getAllSources, getAllModels],
-    ([$sources, $models]) => {
-      return [
-        ...($sources?.data?.entries || []),
-        ...($models?.data?.entries || []),
-      ]
-        ?.filter(combineEntryWithReference(modelName, references))
-        ?.map((entry) => {
-          // get the reference that matches this entry
-          return [
-            entry,
-            references.find(
-              (ref) =>
-                ref.reference === entry.name ||
-                (entry?.embedded &&
-                  entry?.children?.includes(modelName.toLowerCase()))
-            ),
-          ];
-        });
-    }
-  );
-
-  // associate with the cardinality
-  $: referencedWithMetadata = derived(
-    $referencedThings.map(([$thing, ref]) => {
-      return derived(
-        [
-          writable($thing),
-          writable(ref),
-          createQueryServiceTableCardinality($runtime?.instanceId, $thing.name),
-        ],
-        ([$thing, ref, $cardinality]) => ({
-          entry: $thing,
-          reference: ref,
-          totalRows: +$cardinality?.data?.cardinality,
-        })
-      );
-    }),
-    ($referencedThings) => $referencedThings
-  );
-
   let showColumns = true;
-
-  // toggle state for inspector sections
-  // let showSourceTables = true;
-
-  //  function focus(reference) {
-  //    return () => {
-  //      // FIXME
-  //      if (references.length) {
-  //        queryHighlight.set([reference]);
-  //      }
-  //    };
-  //  }
-  //  function blur() {
-  //    queryHighlight.set(undefined);
-  //  }
-
-  // FIXME
-  // let modelHasError = false;
 </script>
 
 <div class="model-profile">
@@ -133,13 +36,11 @@
 
       {#if showColumns}
         <div transition:slide|local={{ duration: LIST_SLIDE_DURATION }}>
-          <!-- {#key entry?.model?.sql} -->
           <ColumnProfile
             key={entry?.model?.sql}
             objectName={entry?.model?.name}
             indentLevel={0}
           />
-          <!-- {/key} -->
         </div>
       {/if}
     </div>
