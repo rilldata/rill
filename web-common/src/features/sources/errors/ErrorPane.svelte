cc914598fb42d2d99f7a4dbcbb45a144e0cf9737
Eric P Green <ericpgreen2@gmail.com>
2023-07-24 17:37:45 -0400

    Add padding to error pane

--- web-common/src/features/sources/errors/ErrorPane.svelte
+++ web-common/src/features/sources/errors/ErrorPane.svelte
@@ -4,7 +4,7 @@
   export let error: any;
 </script>
 
-<div class="w-full h-full bg-white flex-col justify-center inline-flex">
+<div class="w-full h-full bg-white flex-col justify-center inline-flex p-3">
   <div class="flex-col justify-start items-center gap-1 flex text-red-500">
     <CancelCircle size="24px" />
     <div class="text-center text-sm font-medium">
