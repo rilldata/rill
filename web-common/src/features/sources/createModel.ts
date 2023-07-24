3d59925025a41a4698c47553f2200636fbf6777d
Eric P Green <ericpgreen2@gmail.com>
2023-07-24 10:10:39 -0400

    Create better `createModelFromSource` action

--- web-common/src/features/sources/createModel.ts
+++ web-common/src/features/sources/createModel.ts
@@ -1,11 +1,22 @@
+import { goto } from "$app/navigation";
 import { getName } from "@rilldata/web-common/features/entity-management/name-utils";
 import { createModel } from "@rilldata/web-common/features/models/createModel";
 import type {
   CreateBaseMutationResult,
   QueryClient,
 } from "@tanstack/svelte-query";
+import { get } from "svelte/store";
 import { notifications } from "../../components/notifications";
-import type { V1PutFileAndReconcileResponse } from "../../runtime-client";
+import {
+  runtimeServicePutFileAndReconcile,
+  type V1PutFileAndReconcileResponse,
+} from "../../runtime-client";
+import { invalidateAfterReconcile } from "../../runtime-client/invalidation";
+import { runtime } from "../../runtime-client/runtime-store";
+import { getFilePathFromNameAndType } from "../entity-management/entity-mappers";
+import { fileArtifactsStore } from "../entity-management/file-artifacts-store";
+import { EntityType } from "../entity-management/types";
+import { getModelNames } from "../models/selectors";
 
 export async function createModelFromSource(
   queryClient: QueryClient,
@@ -29,4 +40,41 @@ export async function createModelFromSource(
     message: `Queried ${sourceNameInQuery} in workspace`,
   });
   return newModelName;
+}
+
+export async function createModelFromSourceV2(
+  queryClient: QueryClient,
+  sourceName: string
+): Promise<string> {
+  const instanceId = get(runtime).instanceId;
+
+  // Get new model name
+  const modelNames = await getModelNames(queryClient, instanceId);
+  const newModelName = getName(`${sourceName}_model`, modelNames);
+
+  // Create model
+  const resp = await runtimeServicePutFileAndReconcile({
+    instanceId,
+    path: getFilePathFromNameAndType(newModelName, EntityType.Model),
+    blob: `select * from ${sourceName}`,
+    createOnly: true,
+    strict: true,
+  });
+
+  // Handle errors
+  fileArtifactsStore.setErrors(resp.affectedPaths, resp.errors);
+
+  // Navigate to new model
+  goto(`/model/${newModelName}?focus`);
+
+  // Invalidate relevant queries
+  invalidateAfterReconcile(queryClient, instanceId, resp);
+
+  // Show toast message
+  notifications.send({
+    message: `Data source imported. Start modeling it here.`,
+  });
+
+  // Done
+  return;
 }
