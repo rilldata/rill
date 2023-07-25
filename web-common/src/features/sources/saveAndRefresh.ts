9445b089c589619bb10d81a7ec70308a77201079
Eric P Green <ericpgreen2@gmail.com>
2023-07-25 10:49:12 -0400

    Handle newline addition/removal bug

--- web-common/src/features/sources/saveAndRefresh.ts
+++ web-common/src/features/sources/saveAndRefresh.ts
@@ -1,6 +1,9 @@
 import type { QueryClient } from "@tanstack/svelte-query";
 import { get } from "svelte/store";
-import { runtimeServicePutFileAndReconcile } from "../../runtime-client";
+import {
+  getRuntimeServiceGetFileQueryKey,
+  runtimeServicePutFileAndReconcile,
+} from "../../runtime-client";
 import { invalidateAfterReconcile } from "../../runtime-client/invalidation";
 import { runtime } from "../../runtime-client/runtime-store";
 import { getFilePathFromNameAndType } from "../entity-management/entity-mappers";
@@ -13,9 +16,11 @@ export async function saveAndRefresh(
   yaml: string
 ) {
   const instanceId = get(runtime).instanceId;
+  const filePath = getFilePathFromNameAndType(tableName, EntityType.Table);
+
   const resp = await runtimeServicePutFileAndReconcile({
     instanceId,
-    path: getFilePathFromNameAndType(tableName, EntityType.Table),
+    path: filePath,
     blob: yaml,
     create: false,
     createOnly: false,
@@ -24,6 +29,14 @@ export async function saveAndRefresh(
   });
 
   invalidateAfterReconcile(queryClient, instanceId, resp);
+
+  // Sometimes, reconcile doesn't detect any changes, but we still need to invalidate the GetFile query
+  // One such case is the addition/removal of newlines in the file
+  if (resp.affectedPaths.length === 0) {
+    queryClient.invalidateQueries(
+      getRuntimeServiceGetFileQueryKey(instanceId, filePath)
+    );
+  }
 
   // handle errors
   fileArtifactsStore.setErrors(resp.affectedPaths, resp.errors);
