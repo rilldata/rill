26d5fa8a037dfd65ad874160d8abf5de3f9786f9
Speros Kokenes <speroskokenes@gmail.com>
2023-07-26 09:47:35 -0400

    virtualization testing

--- web-local/src/routes/(application)/dashboard/[name]/table/ProtoTable.svelte
+++ web-local/src/routes/(application)/dashboard/[name]/table/ProtoTable.svelte
@@ -9,15 +9,51 @@
   } from "@tanstack/svelte-table";
   import { getTableData } from "./selectors";
   import { writable } from "svelte/store";
+  import { createVirtualizer } from "@tanstack/svelte-virtual";
 
   const managers = getStateManagers();
   const tableData = getTableData(managers);
+  let container;
 
   const options = writable<TableOptions<any>>({
     data: [],
     columns: [],
     getCoreRowModel: getCoreRowModel(),
   });
+
+  // const rowVirtualizer = useVirtualizer({
+  //   count: 10000,
+  //   getScrollElement: () => parentRef.current,
+  //   estimateSize: () => 35,
+  //   overscan: 5,
+  // })
+
+  $: rowVirtualizer = createVirtualizer({
+    count: 10000,
+    getScrollElement: () => container,
+    estimateSize: () => 35,
+    overscan: 5,
+  });
+
+  // const { virtualItems: virtualRows, totalSize } = rowVirtualizer
+
+  $: virtualRows = $rowVirtualizer.getVirtualItems();
+  $: totalSize = $rowVirtualizer.getTotalSize();
+
+  $: paddingTop = virtualRows?.length > 0 ? virtualRows?.[0]?.start || 0 : 0;
+  $: paddingBottom =
+    virtualRows?.length > 0
+      ? totalSize - (virtualRows?.[virtualRows.length - 1]?.end || 0)
+      : 0;
+
+  $: {
+    console.log({
+      virtualRows,
+      totalSize,
+      paddingTop,
+      paddingBottom,
+    });
+  }
 
   $: {
     const columns = $tableData.meta.map((field) => {
@@ -36,13 +72,13 @@
   const table = createSvelteTable(options);
 </script>
 
-<div>
+<div class="p-16">
   <table>
     <thead>
       {#each $table.getHeaderGroups() as headerGroup}
         <tr>
           {#each headerGroup.headers as header}
-            <th>
+            <th class="border p-2">
               {#if !header.isPlaceholder}
                 <svelte:component
                   this={flexRender(
@@ -60,7 +96,7 @@
       {#each $table.getRowModel().rows as row}
         <tr>
           {#each row.getVisibleCells() as cell}
-            <td>
+            <td class="border p-2">
               <svelte:component
                 this={flexRender(cell.column.columnDef.cell, cell.getContext())}
               />
@@ -88,4 +124,34 @@
       {/each}
     </tfoot>
   </table>
+  <div
+    bind:this={container}
+    style="height: 200px; overflow: auto;"
+    class="border"
+  >
+    <table
+      class="w-full border-collapse"
+      style={`height: ${totalSize}px; max-height: ${totalSize}px; overflow: none;`}
+    >
+      <tbody>
+        {#if paddingTop > 0}
+          <tr>
+            <td style={`height: ${paddingTop}px`} />
+          </tr>
+        {/if}
+        {#each virtualRows.slice(0, 6) as virtualRow}
+          <tr>
+            <td style="height: 35px" class="border w-full"
+              >{virtualRow.index}</td
+            >
+          </tr>
+        {/each}
+        {#if paddingBottom > 0}
+          <tr>
+            <td style={`height: ${paddingBottom}px`} />
+          </tr>
+        {/if}
+      </tbody>
+    </table>
+  </div>
 </div>
