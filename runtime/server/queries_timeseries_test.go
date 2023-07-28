01eb9098287cc7250c0262419952f68622b3d5d1
Anshul Khandelwal <anshulkhandelwal94@gmail.com>
2023-07-28 19:49:58 +0530

    compile fix

--- runtime/server/queries_timeseries_test.go
+++ runtime/server/queries_timeseries_test.go
@@ -3,9 +3,12 @@ package server
 import (
 	"context"
 	"fmt"
-	"github.com/rilldata/rill/runtime/pkg/ratelimit"
 	"testing"
 	"time"
+
+	"github.com/rilldata/rill/runtime/pkg/ratelimit"
+
+	"strings"
 
 	"github.com/marcboeker/go-duckdb"
 	runtimev1 "github.com/rilldata/rill/proto/gen/rill/runtime/v1"
@@ -14,7 +17,6 @@ import (
 	"github.com/rilldata/rill/runtime/testruntime"
 	"github.com/stretchr/testify/require"
 	structpb "google.golang.org/protobuf/types/known/structpb"
-	"strings"
 )
 
 func TestServer_Timeseries_EmptyModel(t *testing.T) {
@@ -573,7 +575,7 @@ func getTimeseriesTestServerWithWeekGrain(t *testing.T) (*Server, string) {
 	sql := strings.Join(selects, " UNION ALL ")
 	rt, instanceID := testruntime.NewInstanceWithModel(t, "timeseries", sql)
 
-	server, err := NewServer(context.Background(), &Options{}, rt, nil, ratelimit.NewNoop())
+	server, err := NewServer(context.Background(), &Options{}, rt, nil, ratelimit.NewNoop(), activity.NewNoopClient())
 	require.NoError(t, err)
 
 	return server, instanceID
