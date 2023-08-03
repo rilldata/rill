fd46a0e267ecca9ee4a1554ce71842a4cdd6bbd1
Rakesh Sharma <rakeshsharma14317@gmail.com>
2023-08-03 19:58:09 +0530

    check if local port is blocked

--- runtime/pkg/graceful/grpc.go
+++ runtime/pkg/graceful/grpc.go
@@ -11,7 +11,7 @@ import (
 
 // ServeGRPC serves a GRPC server and performs a graceful shutdown if/when ctx is cancelled.
 func ServeGRPC(ctx context.Context, server *grpc.Server, port int) error {
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	lis, err := net.Listen("tcp", fmt.Sprintf("127.0.0.1:%d", port))
 	if err != nil {
 		if strings.Contains(err.Error(), "address already in use") {
 			return fmt.Errorf("grpc port %d is in use by another process. Either kill that process or pass `--port-grpc PORT` to run Rill on another port", port)
