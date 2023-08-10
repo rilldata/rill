174f581da35efb47116b18b7b26021b8756bd7de
Benjamin Egelund-Müller <b@egelund-muller.com>
2023-08-10 13:14:51 +0200

    Clarify comment

--- runtime/pkg/graceful/http.go
+++ runtime/pkg/graceful/http.go
@@ -11,7 +11,7 @@ import (
 // ServeHTTP serves a HTTP server and performs a graceful shutdown if/when ctx is cancelled.
 func ServeHTTP(ctx context.Context, server *http.Server, port int) error {
 	// Calling net.Listen("tcp", ...) will succeed if the port is blocked on IPv4 but not on IPv6.
-	// This workaround ensures we get the port on IPv4.
+	// This workaround ensures we get the port on IPv4 (and most likely also on IPv6).
 	lis, err := net.Listen("tcp4", fmt.Sprintf(":%d", port))
 	if err == nil {
 		lis.Close()
