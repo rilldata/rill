a6f3f5d82fe89f41b243959a6e2ebe2f9b904bce
Benjamin Egelund-Müller <b@egelund-muller.com>
2023-08-10 13:08:01 +0200

    Ensure port errors are returned if the port is blocked

--- runtime/pkg/graceful/http.go
+++ runtime/pkg/graceful/http.go
@@ -10,7 +10,13 @@ import (
 
 // ServeHTTP serves a HTTP server and performs a graceful shutdown if/when ctx is cancelled.
 func ServeHTTP(ctx context.Context, server *http.Server, port int) error {
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	// Calling net.Listen("tcp", ...) will succeed if the port is blocked on IPv4 but not on IPv6.
+	// This workaround ensures we get the port on IPv4.
+	lis, err := net.Listen("tcp4", fmt.Sprintf(":%d", port))
+	if err == nil {
+		lis.Close()
+		lis, err = net.Listen("tcp", fmt.Sprintf(":%d", port))
+	}
 	if err != nil {
 		if strings.Contains(err.Error(), "address already in use") {
 			return fmt.Errorf("http port %d is in use by another process. Either kill that process or pass `--port PORT` to run Rill on another port", port)
