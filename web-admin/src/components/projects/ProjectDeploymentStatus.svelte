ba380658efe1489365fb0d1a9ab5e02cf1c1d8e0
Eric P Green <ericpgreen2@gmail.com>
2023-08-16 14:18:00 -0400

    Cloud UI: Project page v2 (#2929)
    * Move project logs to a dedicated page
    
    * Format "Logs" badge
    
    * Make dashboard error page permission-specific
    
    * Make dashboard building page permission-specific
    
    * Create and use `Tag` component
    
    * Add scroll
    
    * Create `DashboardList` component v2
    
    * Use new `DashboardList` component on Project page
    
    * Add some spacing to the Project Status block
    
    * Remove space
    
    * Improve responsiveness
    
    * Remove unused classes

--- web-admin/src/components/projects/ProjectDeploymentStatus.svelte
+++ web-admin/src/components/projects/ProjectDeploymentStatus.svelte
@@ -14,26 +14,28 @@
   }
 </script>
 
-<div class="flex flex-col gap-y-1">
-  <span class="uppercase text-gray-500 font-semibold text-[10px] leading-4"
-    >Project status</span
-  >
+<div class="flex flex-col gap-y-2">
   <div>
-    <DeploymentStatusChip {organization} {project} />
+    <span class="uppercase text-gray-500 font-semibold text-[10px] leading-4"
+      >Project status</span
+    >
+    <div>
+      <DeploymentStatusChip {organization} {project} />
+    </div>
+    {#if $proj && $proj.data && $proj.data.prodDeployment}
+      <span class="text-gray-500 text-[11px] leading-4">
+        Synced {new Date($proj.data.prodDeployment.updatedOn).toLocaleString(
+          undefined,
+          {
+            month: "short",
+            day: "numeric",
+            hour: "numeric",
+            minute: "numeric",
+          }
+        )}
+      </span>
+    {/if}
   </div>
-  {#if $proj && $proj.data && $proj.data.prodDeployment}
-    <span class="text-gray-500 text-[11px] leading-4">
-      Synced {new Date($proj.data.prodDeployment.updatedOn).toLocaleString(
-        undefined,
-        {
-          month: "short",
-          day: "numeric",
-          hour: "numeric",
-          minute: "numeric",
-        }
-      )}
-    </span>
-  {/if}
   <div>
     <Button type="secondary" on:click={handleViewLogs}>View logs</Button>
   </div>
---
+++ web-admin/src/components/projects/ProjectHero.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import AccessControls from "../access-controls/AccessControls.svelte";
+  import ProjectDeploymentStatus from "./ProjectDeploymentStatus.svelte";
+  import ProjectGithubConnection from "./ProjectGithubConnection.svelte";
+  import ShareProjectCta from "./ShareProjectCTA.svelte";
+
+  export let organization: string;
+  export let project: string;
+</script>
+
+<div class="flex flex-col gap-y-1">
+  <h1 class="text-sky-950 text-4xl font-light leading-10">
+    {project}
+  </h1>
+  <AccessControls {organization} {project}>
+    <svelte:fragment slot="manage-project">
+      <div
+        class="flex flex-wrap gap-x-16 gap-y-8 items-start mt-8 mb-12 rounded w-fit"
+      >
+        <ProjectDeploymentStatus {organization} {project} />
+        <ProjectGithubConnection {organization} {project} />
+        <ShareProjectCta {organization} {project} />
+      </div>
+    </svelte:fragment>
+  </AccessControls>
+  <span class="text-gray-500 text-base font-normal leading-normal"
+    >Check out your dashboards below.</span
+  >
+</div>
