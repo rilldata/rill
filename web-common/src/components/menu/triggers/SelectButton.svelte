5c32f1f18ab330eec678b76cdef26708a176b2d0
brendan colloran <brendan@colloran.net>
2023-08-16 23:08:30 -0700

    slightly tighten spacing on SelectButton

--- web-common/src/components/menu/triggers/SelectButton.svelte
+++ web-common/src/components/menu/triggers/SelectButton.svelte
@@ -45,7 +45,7 @@
   aria-label={label}
   class="
 {block ? 'flex w-full h-full px-2' : 'inline-flex w-max rounded px-1'} 
-  items-center gap-x-2 justify-between
+  items-center gap-x-1 justify-between
   {classes[level]}
   {tailwindClasses}
   {active && !disabled ? activeTailwindClasses : ''}
