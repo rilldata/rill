9e3a8a451c5230647c2ea9df3954eed2670bde41
Speros Kokenes <skokenes@users.noreply.github.com>
2023-08-18 10:21:11 -0400

    feat: TDD rendering 2 (#2940)
    * feat: determine TDT fetching block
    
    * feat: async load from cell
    
    * fix: eslint
    
    * fix: typings
    
    * feat: tdt render cell types
    
    * feat: tdt formatted num cell
    
    * cell rendering
    
    * feat: performant rendering
    
    * feat: rendering headers, cells, visible dim vals
    
    * chore: prettier cleanup
    
    * feat: aria roles in pivot
    
    * feat: remove border, reuse format
    
    * feat: filter checks in TDD
    
    * feat: change TDT to TDD for consistency
    
    * chore: remove unused imports
    
    * fix: merge issue
    
    * chore: cleanup

--- web-common/src/features/dashboards/time-dimension-details/TimeDimensionDetailsTable.svelte
+++ web-common/src/features/dashboards/time-dimension-details/TimeDimensionDetailsTable.svelte
@@ -4,12 +4,17 @@
   import TimeDimensionDetailsTableHeaderCell from "./TimeDimensionDetailsTableHeaderCell.svelte";
   import { createTimeDimensionDetailsStore } from "./time-dimension-details-store";
   import { data } from "./mock-data";
-  import { createTDTContext } from "./context";
+  import { createTDDContext } from "./context";
+  import {
+    FILTER_OVERFLOW_WIDTH,
+    HEADER_HEIGHT,
+    ROW_HEIGHT,
+  } from "./constants";
 
   // Store of state to share between line chart and table
   let store = createTimeDimensionDetailsStore();
 
-  createTDTContext({
+  createTDDContext({
     store,
     headers: data.headers,
   });
@@ -19,13 +24,17 @@
 
   // Mock state for now
   let state = {
-    getRowSize: () => 24,
+    getRowSize: () => ROW_HEIGHT,
     getColumnWidth: (idx: number) => columnWidths[idx] ?? 60,
   };
+
+  // Leave padding on table header and body to make space for the filter check/X overflow
+  const headerStyle = `padding-left: ${FILTER_OVERFLOW_WIDTH}px;`;
+  const bodyStyle = `padding-left: ${FILTER_OVERFLOW_WIDTH}px;`;
 </script>
 
 <h1>Store</h1>
-<pre>
+<pre style="max-height: 200px; overflow: auto; width: fit-content;">
   {JSON.stringify($store, null, 2)}
 </pre>
 <h1>Table</h1>
@@ -38,5 +47,7 @@
   getRowSize={state.getRowSize}
   cellComponent={TimeDimensionDetailsTableCell}
   headerComponent={TimeDimensionDetailsTableHeaderCell}
-  headerHeight={36}
+  headerHeight={HEADER_HEIGHT}
+  {headerStyle}
+  {bodyStyle}
 />
--- web-common/src/features/dashboards/time-dimension-details/TimeDimensionDetailsTableHeaderCell.svelte
+++ web-common/src/features/dashboards/time-dimension-details/TimeDimensionDetailsTableHeaderCell.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
-  import { useTDTContext } from "./context";
+  import { FILTER_OVERFLOW_WIDTH } from "./constants";
+  import { useTDDContext } from "./context";
 
   export const rowIdx = -1;
   export let colIdx: number;
   export let fixed = false;
   export let lastFixed = false;
 
-  const { store, headers } = useTDTContext();
+  const { store, headers } = useTDDContext();
 
   let _class = "";
   $: {
     _class =
       "h-full w-full bg-white border-b px-2 flex items-center overflow-hidden";
-    if (fixed) _class += ` z-2`;
+    if (fixed) _class += ` z-10`;
     if (lastFixed) _class += ` right-shadow`;
     if (colIdx > 0) _class += ` justify-end font-medium`;
     else _class += ` justify-start font-bold`;
@@ -37,6 +38,13 @@
     <svelte:component this={header.component} />
   {:else}
     {header.title}
+  {/if}
+  {#if colIdx === 0}
+    <!-- Placeholder for table padding to fit the overflowing checkboxes from dimension cells. This will hide column labels that horizontally scroll to the left -->
+    <div
+      style={`left: -${FILTER_OVERFLOW_WIDTH}px; width: ${FILTER_OVERFLOW_WIDTH}px;`}
+      class="absolute top-0 h-full bg-white z-10 flex items-center justify-center bg-white border-b"
+    />
   {/if}
 </div>
 
