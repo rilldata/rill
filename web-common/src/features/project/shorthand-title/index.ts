477ab78b2f331aee8273c34efd83c66cfa798190
Eric P Green <ericpgreen2@gmail.com>
2023-08-21 14:10:57 -0400

    Fix unit test

--- web-common/src/features/project/shorthand-title/index.ts
+++ web-common/src/features/project/shorthand-title/index.ts
@@ -8,7 +8,8 @@ const replacePunctuationWithSpace = (str: string) => {
 };
 
 export function shorthandTitle(str: string) {
-  if (str.length < 2) return str;
+  if (!str) return;
+  if (str.length === 1) return str.toUpperCase();
   const out = replacePunctuationWithSpace(str)
     .toUpperCase()
     .split(" ")
