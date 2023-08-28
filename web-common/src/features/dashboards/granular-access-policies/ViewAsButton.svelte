366ca3cdad0d024a93cfcfd7bf377f8f5e416589
Eric P Green <ericpgreen2@gmail.com>
2023-08-28 14:32:53 -0400

    Add message "No mock users"

--- web-common/src/features/dashboards/granular-access-policies/ViewAsButton.svelte
+++ web-common/src/features/dashboards/granular-access-policies/ViewAsButton.svelte
@@ -76,7 +76,9 @@
     on:escape={toggleFloatingElement}
     slot="floating-element"
   >
-    {#if $mockUsers.data?.length > 0}
+    {#if $mockUsers.data?.length === 0}
+      <MenuItem disabled>No mock users</MenuItem>
+    {:else if $mockUsers.data?.length > 0}
       {#each $mockUsers.data as user}
         <MenuItem
           icon
