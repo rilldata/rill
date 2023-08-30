30b5c00b42fb9ffaa661f567d4e4a7526d005946
Eric P Green <ericpgreen2@gmail.com>
2023-08-30 11:33:07 -0400

    Better comment

--- web-common/src/features/dashboards/granular-access-policies/resetSelectedMockUserAfterNavigate.ts
+++ web-common/src/features/dashboards/granular-access-policies/resetSelectedMockUserAfterNavigate.ts
@@ -2,11 +2,12 @@ import { afterNavigate } from "$app/navigation";
 import { selectedMockUserStore } from "./stores";
 
 /**
- * Remove the selected mock user (if any), unless you're navigating from the edit page to the view page
+ * Remove the selected mock user (if any) when navigating to a dashboard
+ * (This doesn't apply when navigating from a dashboard's edit page to its view page)
  *
- * Note: It'd be better if we didn't do this. It's a hack to avoid the following bug:
- * Navigating to a dashboard where the selected mock user does not have access shows a blank page –
- * because under this scenario, the catalog entry returns a 401, and it's required to enter the top-level
+ * Note: It'd be better if we didn't do this. It's a hack to avoid the following bug: Navigating to
+ * a dashboard where the selected mock user does not have access shows a blank page – because
+ * under this scenario, the catalog entry returns a 401, and it's required to enter the top-level
  * `Dashboard.svelte` component.
  */
 export function resetSelectedMockUserAfterNavigate() {
