164d25b7f757fbd3ca528765d27e32f868d7541e
Eric P Green <ericpgreen2@gmail.com>
2023-09-01 09:00:22 -0400

    Edit copy in `rill.yaml` (#3013)
    * Edit copy in `rill.yaml`
    
    * Edit comment

--- web-common/src/runtime-client/RuntimeProvider.svelte
+++ web-common/src/runtime-client/RuntimeProvider.svelte
@@ -16,7 +16,7 @@
   const queryClient = useQueryClient();
 
   // Whenever the runtime's (dev) JWT changes (even to `null`), invalidate all metrics views.
-  // Metrics views may have access policies, for which a JWT grants permission.
+  // Metrics views may have a security policy, for which a JWT grants permission.
   $: ($runtime?.jwt || $runtime?.jwt === null) &&
     invalidateAllMetricsViews(queryClient, instanceId);
 </script>
