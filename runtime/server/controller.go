a34132180adbcde91de1e96ff31067a0ee312625
Aditya Hegde <adityahegderocks@gmail.com>
2023-09-04 12:39:55 +0530

    Remove temporary code

--- runtime/server/controller.go
+++ runtime/server/controller.go
@@ -2,12 +2,8 @@ package server
 
 import (
 	"context"
-	"fmt"
-	"time"
 
 	runtimev1 "github.com/rilldata/rill/proto/gen/rill/runtime/v1"
-	"github.com/rilldata/rill/runtime/drivers"
-	"golang.org/x/sync/errgroup"
 )
 
 // GetLogs implements runtimev1.RuntimeServiceServer
@@ -27,58 +23,7 @@ func (s *Server) ListResources(ctx context.Context, req *runtimev1.ListResources
 
 // WatchResources implements runtimev1.RuntimeServiceServer
 func (s *Server) WatchResources(req *runtimev1.WatchResourcesRequest, srv runtimev1.RuntimeService_WatchResourcesServer) error {
-	// Temporary code for testing
-	ctx := srv.Context()
-
-	catalog, catalogRelease, err := s.runtime.Catalog(ctx, req.InstanceId)
-	if err != nil {
-		return err
-	}
-	defer catalogRelease()
-
-	g, ctx := errgroup.WithContext(ctx)
-
-	g.Go(func() error {
-		lastRun := time.Now()
-
-		for {
-			entries, err := catalog.FindEntries(ctx, drivers.ObjectTypeUnspecified)
-			if err != nil {
-				return err
-			}
-
-			for _, entry := range entries {
-				if entry.UpdatedOn.Before(lastRun) {
-					continue
-				}
-
-				r := &runtimev1.Resource{
-					Meta: &runtimev1.ResourceMeta{
-						Name: &runtimev1.ResourceName{
-							Kind: "catalog",
-							Name: entry.Name,
-						},
-						FilePaths: []string{entry.Path},
-					},
-				}
-				err = srv.Send(&runtimev1.WatchResourcesResponse{
-					Resource: r,
-				})
-				if err != nil {
-					return err
-				}
-			}
-
-			if ctx.Err() != nil {
-				fmt.Println(ctx.Err())
-				return ctx.Err()
-			}
-			lastRun = time.Now()
-			time.Sleep(time.Second)
-		}
-	})
-
-	return g.Wait()
+	panic("not implemented")
 }
 
 // GetResource implements runtimev1.RuntimeServiceServer
