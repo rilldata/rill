746d3461d2721020deb661a889c565599057007f
Aditya Hegde <adityahegderocks@gmail.com>
2023-09-05 15:01:58 +0530

    Fixing periodic page refresh on errored dashboard (#3020)
    * Fixing periodic page refresh on errored dashboard
    
    * Going with the correct solution
    
    * Cleanup

--- web-local/src/routes/(application)/+layout.svelte
+++ web-local/src/routes/(application)/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import { beforeNavigate } from "$app/navigation";
-  import DevJWTProvider from "@rilldata/web-common/features/dashboards/granular-access-policies/DevJWTProvider.svelte";
   import { retainFeaturesFlags } from "@rilldata/web-common/features/feature-flags";
   import RillDeveloperLayout from "@rilldata/web-common/layout/RillDeveloperLayout.svelte";
   import RuntimeProvider from "@rilldata/web-common/runtime-client/RuntimeProvider.svelte";
@@ -24,11 +23,9 @@
 </script>
 
 <QueryClientProvider client={queryClient}>
-  <DevJWTProvider let:jwt>
-    <RuntimeProvider host={RuntimeUrl} instanceId="default" {jwt}>
-      <RillDeveloperLayout>
-        <slot />
-      </RillDeveloperLayout>
-    </RuntimeProvider>
-  </DevJWTProvider>
+  <RuntimeProvider host={RuntimeUrl} instanceId="default">
+    <RillDeveloperLayout>
+      <slot />
+    </RillDeveloperLayout>
+  </RuntimeProvider>
 </QueryClientProvider>
