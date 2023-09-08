e136526ad87371a339137f5a03fe9b23336c50e0
Eric P Green <ericpgreen2@gmail.com>
2023-09-08 12:01:54 -0400

    Acquire and set JWT

--- web-admin/src/routes/[organization]/[project]/+layout.svelte
+++ web-admin/src/routes/[organization]/[project]/+layout.svelte
@@ -2,6 +2,8 @@
   import { goto } from "$app/navigation";
   import { page } from "$app/stores";
   import RuntimeProvider from "@rilldata/web-common/runtime-client/RuntimeProvider.svelte";
+  import { createAdminServiceGetDeploymentCredentials } from "../../../client";
+  import { viewAsUserStore } from "../../../components/authentication/viewAsUserStore";
   import { useProjectRuntime } from "../../../components/projects/selectors";
 
   $: projRuntime = useProjectRuntime(
@@ -15,13 +17,27 @@
     // Redirect any nested routes (notably dashboards) to the project page
     goto(`/${$page.params.organization}/${$page.params.project}`);
   }
+
+  // if viewAs is set (which only admins can configure), we need to update the runtime with the new jwt
+  $: deploymentCredsQuery = createAdminServiceGetDeploymentCredentials(
+    $page.params.organization,
+    $page.params.project,
+    {
+      userId: $viewAsUserStore?.id,
+    },
+    {
+      query: {
+        enabled: $viewAsUserStore?.id !== undefined,
+      },
+    }
+  );
 </script>
 
 {#if $projRuntime.data}
   <RuntimeProvider
     host={$projRuntime.data.host}
     instanceId={$projRuntime.data.instanceId}
-    jwt={$projRuntime.data?.jwt}
+    jwt={$deploymentCredsQuery.data?.jwt || $projRuntime.data?.jwt}
   >
     <slot />
   </RuntimeProvider>
