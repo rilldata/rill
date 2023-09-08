77c59573ebfebd4a6b08e226c9145b7b8ef12a2f
Speros Kokenes <speroskokenes@gmail.com>
2023-09-08 15:50:09 -0400

    feat: prototype reg-table perf

--- web-common/src/features/dashboards/time-dimension-details-rt/TimeDimensionDetailsTableRT.svelte
+++ web-common/src/features/dashboards/time-dimension-details-rt/TimeDimensionDetailsTableRT.svelte
@@ -1,63 +1,175 @@
 <script lang="ts">
   import "regular-table";
   import "regular-table/dist/css/material.css";
+  import { getBlock } from "../time-dimension-details/util";
+  import {
+    CreateQueryResult,
+    createQuery,
+    useQueryClient,
+  } from "@tanstack/svelte-query";
+  import { fetchData } from "../time-dimension-details/mock-data";
+  import { get, writable } from "svelte/store";
 
   let table;
-
-  const DATA = [
-    [0, 1, 2, 3, 4, 5],
-    ["A", "B", "C", "D", "E", "F"],
-    [true, false, true, false, true, false],
-  ];
-
+  const __state = writable({ cachedBlock: null });
   const NUM_ROWS = 1000;
-  const NUM_COLUMNS = 106;
+  const NUM_COLUMNS = 105;
+  const STICKY_COLUMNS = 5;
   function range(x0, x1, f) {
     return Array.from(Array(x1 - x0).keys()).map((x) => f(x + x0));
   }
-  function dataListener(num_rows, num_columns) {
-    return (x0, y0, x1, y1) => ({
-      num_rows,
-      num_columns,
-      // row_headers: range(y0, y1, (d) => d),
-      // column_headers: range(x0, x1, (d) => d),
-      data: range(x0, x1, (x) => range(y0, y1, (y) => `${x},${y}`)),
-    });
-  }
 
-  // function getDataSlice(x0, y0, x1, y1) {
-  //   console.log("getDataSlice", x0, y0, x1, y1);
-  //   return {
-  //     num_rows: DATA[0].length,
-  //     num_columns: DATA.length,
-  //     data: DATA.slice(x0, x1).map((col) => col.slice(y0, y1)),
-  //   };
-  // }
+  let pos = { x0: 0, x1: 0, y0: 0, y1: 0 };
+
+  let debouncedPos = { x0: 0, x1: 0, y0: 0, y1: 0 };
+  const debounce = (cb, t) => {
+    let timeout = null;
+    return (...args) => {
+      const next = () => cb(...args);
+      clearTimeout(timeout);
+      timeout = setTimeout(next, t);
+    };
+  };
+
+  const updateDebouncedPos = debounce((pos) => {
+    debouncedPos = pos;
+  }, 0);
 
   $: {
-    console.log(table);
+    const posIsEmpty = pos.x1 - pos.x0 === 0 || pos.y1 - pos.y0 === 0;
+    if (!posIsEmpty) {
+      updateDebouncedPos(pos);
+    }
+  }
+
+  const queryClient = useQueryClient();
+  const cache = queryClient.getQueryCache();
+
+  let block = getBlock(100, debouncedPos.y0, debouncedPos.y1);
+  $: {
+    const nextBlock = getBlock(100, debouncedPos.y0, debouncedPos.y1);
+    if (block[0] !== nextBlock[0] || block[1] !== nextBlock[1]) {
+      block = nextBlock;
+    }
+  }
+
+  let cellQuery = createQuery({
+    queryKey: ["time-dimension-details", block[0], block[1]],
+    queryFn: fetchData(block, 1000),
+  }) as CreateQueryResult<{
+    block: number[];
+    data: { text?: string; value?: number; sparkline?: number[] }[][];
+  }>;
+
+  $: cellQuery = createQuery({
+    queryKey: ["time-dimension-details", block[0], block[1]],
+    queryFn: fetchData(block, 1000),
+  }) as CreateQueryResult<{
+    block: number[];
+    data: { text?: string; value?: number; sparkline?: number[] }[][];
+  }>;
+
+  const columnRenderers = [
+    (d) => `<div class="">${d.text}</div>`,
+    (d) =>
+      `<div class="flex items-center justify-end">${d.value.toFixed(
+        0
+      )} <svg height="24px" width="60px" viewBox="0 0 9 15" preserveAspectRatio="none"> <path  d="M 0 14 L 1 15 L 2 10 L 3 11 L 4 7 L 5 5 L 6 0 L 7 5 L 8 10 L 9 11"  stroke-width="1"  stroke="black"  fill="none"  vector-effect="non-scaling-stroke" /></svg></div>`,
+    (d) => (d.value * 10).toFixed(2) + "%",
+    (d) =>
+      `<span class="${d.value < 0 ? "text-red-500" : ""}">${d.value.toFixed(
+        2
+      )}</span>`,
+    (d) =>
+      `<span class="${d.value < 0 ? "text-red-500" : ""}">${
+        (d.value * 10).toFixed(2) + "%"
+      }</span>`,
+  ];
+  const defaultRender = (d) => d.value.toFixed(2);
+
+  function renderCell(cell, data) {
+    // read __row and __col attribute of td
+    const x = Number(cell.getAttribute("__col"));
+    const y = Number(cell.getAttribute("__row"));
+    if (data) {
+      // if (data && data.block[0] <= y && data.block[1] > y) {
+      const yOffset = y - data.block[0];
+      const v = data.data[yOffset][x];
+      cell.innerHTML = columnRenderers[x]?.(v) ?? defaultRender(v);
+    } else
+      cell.innerHTML = `<div class="w-full h-4 bg-gray-100 rounded loading-cell"/>`; // "...";
+  }
+
+  $: {
+    const data = $cellQuery.data;
+    // If data and don't have a cachedBlock that was already rendered on scroll, render the cells
+    if (data && table && !get(__state).cachedBlock) {
+      for (const cell of table.querySelectorAll("tbody td, tbody th")) {
+        renderCell(cell, data);
+      }
+    }
+  }
+
+  $: {
     if (table) {
       function style_td(td) {
         const meta = table.getMeta(td);
-        td.setAttribute("__col", String(meta.x));
-        td.setAttribute("__row", String(meta.y));
-        td.classList.toggle("bg-slate-50", meta.x > 0 && meta.x < 6);
-        if (meta.x === 1) {
-          td.innerHTML = `<div class="flex">#,### <svg height="24px" width="60px" viewBox="0 0 9 15" preserveAspectRatio="none">
-  <path
-    d="M 0 14 L 1 15 L 2 10 L 3 11 L 4 7 L 5 5 L 6 0 L 7 5 L 8 10 L 9 11"
-    stroke-width="1"
-    stroke="black"
-    fill="none"
-    vector-effect="non-scaling-stroke"
-  />
-</svg></div>`;
-        }
+        const x = meta.x + STICKY_COLUMNS;
+        const y = meta.y;
+        td.setAttribute("__col", String(x));
+        td.setAttribute("__row", String(y));
+        const data = get(__state).cachedBlock;
+        renderCell(td, data);
       }
-      table.setDataListener(dataListener(NUM_ROWS, NUM_COLUMNS));
+
+      function style_th(th) {
+        const meta = table.getMeta(th);
+        const x = meta.row_header_x;
+        const y = meta.y;
+        th.setAttribute("__col", String(x));
+        th.setAttribute("__row", String(y));
+        th.classList.toggle("bg-slate-50", x > 0 && x < 6);
+        const data = get(__state).cachedBlock;
+        renderCell(th, data);
+      }
+
+      function dataListener(num_rows, num_columns) {
+        return (x0, y0, x1, y1) => {
+          pos = { x0, x1, y0, y1 };
+          const block = getBlock(100, y0, y1);
+          const cachedBlock = cache.find([
+            "time-dimension-details",
+            block[0],
+            block[1],
+          ])?.state?.data;
+          __state.set({ cachedBlock });
+          return {
+            num_rows,
+            num_columns,
+            data: range(x0, x1, (x) =>
+              range(y0, y1, (y) => `${x + STICKY_COLUMNS},${y}`)
+            ),
+            row_height: 24,
+            row_headers: range(y0, y1, (y) =>
+              new Array(STICKY_COLUMNS).fill(0).map((n, i) => [`${i},${y}`])
+            ),
+            column_headers: new Array(NUM_COLUMNS - STICKY_COLUMNS)
+              .fill(0)
+              .map((n, i) => [`${i + x0}`]),
+          };
+        };
+      }
+
+      table.setDataListener(
+        dataListener(NUM_ROWS, NUM_COLUMNS - STICKY_COLUMNS)
+      );
       table.addStyleListener(() => {
         for (const td of table.querySelectorAll("tbody td")) {
           style_td(td);
+        }
+
+        for (const th of table.querySelectorAll("tbody th")) {
+          style_th(th);
         }
       });
       table.draw();
@@ -65,42 +177,73 @@
   }
 </script>
 
-<regular-table
-  class="border m-8"
-  style="height: 400px; width: 2000px;"
-  bind:this={table}
-/>
+<div class="border m-8 relative" style="height: 400px; width: 100%">
+  <regular-table class="w-full h-full tdd-table" bind:this={table} />
+</div>
 
 <style>
-  :global(regular-table td) {
+  :global(regular-table tr td) {
+    height: 24px;
+    width: 60px;
+    min-width: 60px;
+    max-width: 60px;
+    padding-block: 0px;
+    text-align: right;
+    background-color: white;
+  }
+
+  :global(.tdd-table tbody tr td) {
+    background-color: white;
+    color: black;
+    padding-inline: 8px;
+  }
+
+  :global(.tdd-table tbody tr th) {
+    color: black;
+    font-weight: normal;
+    padding-inline: 8px;
+  }
+
+  :global(.tdd-table tbody tr th:nth-child(1)) {
+    text-align: left;
+  }
+
+  :global(regular-table th) {
     height: 24px;
     width: 60px;
     min-width: 60px;
     max-width: 60px;
     padding: 0px;
+    text-align: right;
+  }
+
+  :global(regular-table tbody th) {
+    height: 24px;
+    text-align: right;
   }
 
   /* const columnWidths = [120, 126, 64, 64, 64]; */
 
-  :global(regular-table td[__col="0"]) {
+  :global(regular-table th[__col="0"]) {
     width: 120px;
     min-width: 120px;
     max-width: 120px;
+    left: 0px;
   }
 
-  :global(regular-table td[__col="1"]) {
+  :global(regular-table th[__col="1"]) {
     width: 126px;
     min-width: 126px;
     max-width: 126px;
   }
 
-  :global(regular-table td[__col="2"]) {
+  :global(regular-table th[__col="2"]) {
     width: 64px;
     min-width: 64px;
     max-width: 64px;
   }
 
-  :global(regular-table td[__col="3"]) {
+  :global(regular-table th[__col="3"]) {
     width: 64px;
     min-width: 64px;
     max-width: 64px;
