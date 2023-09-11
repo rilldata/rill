d37bd4fff1692e1ee19b195e50f3a841127184b9
Eric P Green <ericpgreen2@gmail.com>
2023-09-11 09:04:20 -0400

    Add comment

--- web-common/src/runtime-client/error.ts
+++ web-common/src/runtime-client/error.ts
@@ -1,3 +1,4 @@
+// The Orval-generated type for query errors is `RpcStatus`, but the following is the observed type that is actually returned.
 export interface QueryError {
   response: {
     status: number;
