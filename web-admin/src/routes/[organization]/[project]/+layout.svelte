a4e72ae07d69938d3c989075877f933021e7f0ff
Eric P Green <ericpgreen2@gmail.com>
2023-09-13 12:08:07 -0400

    Remove unneccessary query observer

--- web-admin/src/routes/[organization]/[project]/+layout.svelte
+++ web-admin/src/routes/[organization]/[project]/+layout.svelte
@@ -2,9 +2,7 @@
   import { goto } from "$app/navigation";
   import { page } from "$app/stores";
   import RuntimeProvider from "@rilldata/web-common/runtime-client/RuntimeProvider.svelte";
-  import { createAdminServiceGetDeploymentCredentials } from "../../../client";
   import { useProjectRuntime } from "../../../components/projects/selectors";
-  import { viewAsUserStore } from "../../../features/view-as-user/viewAsUserStore";
 
   $: projRuntime = useProjectRuntime(
     $page.params.organization,
@@ -17,27 +15,13 @@
     // Redirect any nested routes (notably dashboards) to the project page
     goto(`/${$page.params.organization}/${$page.params.project}`);
   }
-
-  // if viewAs is set (which only admins can configure), we need to update the runtime with the new jwt
-  $: deploymentCredsQuery = createAdminServiceGetDeploymentCredentials(
-    $page.params.organization,
-    $page.params.project,
-    {
-      userId: $viewAsUserStore?.id,
-    },
-    {
-      query: {
-        enabled: $viewAsUserStore?.id !== undefined,
-      },
-    }
-  );
 </script>
 
 {#if $projRuntime.data}
   <RuntimeProvider
     host={$projRuntime.data.host}
     instanceId={$projRuntime.data.instanceId}
-    jwt={$deploymentCredsQuery.data?.jwt || $projRuntime.data?.jwt}
+    jwt={$projRuntime.data?.jwt}
   >
     <slot />
   </RuntimeProvider>
