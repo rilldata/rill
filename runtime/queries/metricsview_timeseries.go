539b1fac1a28bb187a8979a469546896f60146c8
Egor Ryashin <egor.ryashin@rilldata.com>
2023-09-14 17:13:08 +0300

    decoupling mv

--- runtime/queries/metricsview_timeseries.go
+++ runtime/queries/metricsview_timeseries.go
@@ -110,7 +110,10 @@ func (q *MetricsViewTimeSeries) Resolve(ctx context.Context, rt *runtime.Runtime
 		}
 	}
 
+	var start time.Time
+	var zeroTime time.Time
 	var data []*runtimev1.TimeSeriesValue
+	var nullRecords *structpb.Struct
 	for rows.Next() {
 		rowMap := make(map[string]any)
 		err := rows.MapScan(rowMap)
@@ -132,10 +135,35 @@ func (q *MetricsViewTimeSeries) Resolve(ctx context.Context, rt *runtime.Runtime
 			return err
 		}
 
+		tz := time.UTC
+		if q.TimeZone != "" {
+			tz, err = time.LoadLocation(q.TimeZone)
+			if err != nil {
+				return err
+			}
+		}
+		if nullRecords == nil {
+			nullRecords = generateNullRecords(records)
+		}
+		if start.Before(t) {
+			if zeroTime.Equal(start) {
+				if q.TimeStart != nil {
+					start = truncateTime(q.TimeStart.AsTime(), q.TimeGranularity, tz, 1, 1)
+					data = addNulls(data, nullRecords, start, t, q.TimeGranularity)
+				}
+			} else {
+				data = addNulls(data, nullRecords, start, t, q.TimeGranularity)
+			}
+		}
+
 		data = append(data, &runtimev1.TimeSeriesValue{
 			Ts:      timestamppb.New(t),
 			Records: records,
 		})
+		start = addTo(t, q.TimeGranularity)
+	}
+	if q.TimeEnd != nil && nullRecords != nil {
+		data = addNulls(data, nullRecords, start, q.TimeEnd.AsTime(), q.TimeGranularity)
 	}
 
 	meta := structTypeToMetricsViewColumn(rows.Schema)
@@ -146,6 +174,111 @@ func (q *MetricsViewTimeSeries) Resolve(ctx context.Context, rt *runtime.Runtime
 	}
 
 	return nil
+}
+
+func truncateTime(start time.Time, tg runtimev1.TimeGrain, tz *time.Location, firstDay, firstMonth int) time.Time {
+	switch tg {
+	case runtimev1.TimeGrain_TIME_GRAIN_MILLISECOND:
+		return start.Truncate(time.Millisecond)
+	case runtimev1.TimeGrain_TIME_GRAIN_SECOND:
+		return start.Truncate(time.Second)
+	case runtimev1.TimeGrain_TIME_GRAIN_MINUTE:
+		return start.Truncate(time.Minute)
+	case runtimev1.TimeGrain_TIME_GRAIN_HOUR:
+		start = start.In(tz)
+		start = time.Date(start.Year(), start.Month(), start.Day(), start.Hour(), 0, 0, 0, tz)
+		return start.In(time.UTC)
+	case runtimev1.TimeGrain_TIME_GRAIN_DAY:
+		start = start.In(tz)
+		start = time.Date(start.Year(), start.Month(), start.Day(), 0, 0, 0, 0, tz)
+		return start.In(time.UTC)
+	case runtimev1.TimeGrain_TIME_GRAIN_WEEK:
+		start = start.In(tz)
+		weekday := int(start.Weekday())
+		if weekday == 0 {
+			weekday = 7
+		}
+		if firstDay < 1 {
+			firstDay = 1
+		}
+		if firstDay > 7 {
+			firstDay = 7
+		}
+
+		daysToSubtract := -(weekday - firstDay)
+		if weekday < firstDay {
+			daysToSubtract = -7 + daysToSubtract
+		}
+		start = time.Date(start.Year(), start.Month(), start.Day(), 0, 0, 0, 0, tz)
+		start = start.AddDate(0, 0, daysToSubtract)
+		return start.In(time.UTC)
+	case runtimev1.TimeGrain_TIME_GRAIN_MONTH:
+		start = start.In(tz)
+		start = time.Date(start.Year(), start.Month(), 1, 0, 0, 0, 0, tz)
+		start = start.In(time.UTC)
+		return start
+	case runtimev1.TimeGrain_TIME_GRAIN_QUARTER:
+		monthsToSubtract := 1 - int(start.Month())%3 // todo first month of year
+		start = start.In(tz)
+		start = time.Date(start.Year(), start.Month(), 1, 0, 0, 0, 0, tz)
+		start = start.AddDate(0, monthsToSubtract, 0)
+		return start.In(time.UTC)
+	case runtimev1.TimeGrain_TIME_GRAIN_YEAR:
+		start = start.In(tz)
+		year := start.Year()
+		if int(start.Month()) < firstMonth {
+			year = start.Year() - 1
+		}
+
+		start = time.Date(year, time.Month(firstMonth), 1, 0, 0, 0, 0, tz)
+		return start.In(time.UTC)
+	}
+
+	return start
+}
+
+func generateNullRecords(s *structpb.Struct) *structpb.Struct {
+	nullStruct := structpb.Struct{Fields: make(map[string]*structpb.Value, len(s.Fields))}
+	for k := range s.Fields {
+		nullStruct.Fields[k] = structpb.NewNullValue()
+	}
+	return &nullStruct
+}
+
+func addNulls(data []*runtimev1.TimeSeriesValue, nullRecords *structpb.Struct, start, end time.Time, tg runtimev1.TimeGrain) []*runtimev1.TimeSeriesValue {
+	for start.Before(end) {
+		data = append(data, &runtimev1.TimeSeriesValue{
+			Ts:      timestamppb.New(start),
+			Records: nullRecords,
+		})
+		start = addTo(start, tg)
+	}
+	return data
+}
+
+func addTo(start time.Time, tg runtimev1.TimeGrain) time.Time {
+	switch tg {
+	case runtimev1.TimeGrain_TIME_GRAIN_MILLISECOND:
+		return start.Add(time.Millisecond)
+	case runtimev1.TimeGrain_TIME_GRAIN_SECOND:
+		return start.Add(time.Second)
+	case runtimev1.TimeGrain_TIME_GRAIN_MINUTE:
+		return start.Add(time.Minute)
+	case runtimev1.TimeGrain_TIME_GRAIN_HOUR:
+		return start.Add(time.Hour)
+	case runtimev1.TimeGrain_TIME_GRAIN_DAY:
+		return start.AddDate(0, 0, 1)
+	case runtimev1.TimeGrain_TIME_GRAIN_WEEK:
+		return start.AddDate(0, 0, 7)
+	case runtimev1.TimeGrain_TIME_GRAIN_MONTH:
+		return start.AddDate(0, 1, 0)
+	case runtimev1.TimeGrain_TIME_GRAIN_QUARTER:
+		return start.AddDate(0, 3, 0)
+	case runtimev1.TimeGrain_TIME_GRAIN_YEAR:
+		return start.AddDate(1, 0, 0)
+	}
+
+	return start
 }
 
 func (q *MetricsViewTimeSeries) Export(ctx context.Context, rt *runtime.Runtime, instanceID string, w io.Writer, opts *runtime.ExportOptions) error {
--- runtime/server/queries_metrics_timeseries_test.go
+++ runtime/server/queries_metrics_timeseries_test.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"bytes"
+	"fmt"
 	"strings"
 	"testing"
 
@@ -11,6 +12,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"github.com/xuri/excelize/v2"
 	"google.golang.org/protobuf/types/known/structpb"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func TestServer_MetricsViewTimeSeries(t *testing.T) {
@@ -335,6 +337,185 @@ func TestServer_Timeseries_numeric_dim_and_null(t *testing.T) {
 	results := response.Data
 	require.Equal(t, 1, len(results))
 	require.Equal(t, 2.0, results[0].Records.Fields["count"].GetNumberValue())
+}
+
+func TestServer_Timeseries_TimeRange_Day(t *testing.T) {
+	t.Parallel()
+	server, instanceID := getMetricsTestServer(t, "timeseries")
+
+	response, err := server.MetricsViewTimeSeries(testCtx(), &runtimev1.MetricsViewTimeSeriesRequest{
+		InstanceId:      instanceID,
+		MetricsViewName: "timeseries",
+		MeasureNames:    []string{"max_clicks"},
+		TimeGranularity: runtimev1.TimeGrain_TIME_GRAIN_DAY,
+	})
+
+	require.NoError(t, err)
+	results := response.Data
+	for i, v := range response.Data {
+		fmt.Printf("i: %d, ts: %v\n", i, v.Ts.AsTime())
+	}
+	require.Equal(t, 2, len(results))
+	require.Equal(t, 1.0, results[0].Records.Fields["max_clicks"].GetNumberValue())
+	require.Equal(t, parseTime(t, "2019-01-01T00:00:00Z"), results[0].Ts.AsTime())
+	require.Equal(t, parseTime(t, "2019-01-02T00:00:00Z"), results[1].Ts.AsTime())
+}
+
+func TestServer_Timeseries_TimeRange_Start(t *testing.T) {
+	t.Parallel()
+	server, instanceID := getMetricsTestServer(t, "timeseries")
+
+	response, err := server.MetricsViewTimeSeries(testCtx(), &runtimev1.MetricsViewTimeSeriesRequest{
+		InstanceId:      instanceID,
+		MetricsViewName: "timeseries",
+		MeasureNames:    []string{"max_clicks"},
+		TimeStart:       timestamppb.New(parseTime(t, "2018-12-31T00:00:00Z")),
+		TimeGranularity: runtimev1.TimeGrain_TIME_GRAIN_DAY,
+	})
+
+	require.NoError(t, err)
+	results := response.Data
+	for i, v := range response.Data {
+		fmt.Printf("i: %d, ts: %v\n", i, v.Ts.AsTime())
+	}
+	require.Equal(t, 3, len(results))
+	require.Equal(t, parseTime(t, "2018-12-31T00:00:00Z"), results[0].Ts.AsTime())
+	require.Equal(t, structpb.NullValue_NULL_VALUE, results[0].Records.Fields["max_clicks"].GetNullValue())
+	require.Equal(t, parseTime(t, "2019-01-01T00:00:00Z"), results[1].Ts.AsTime())
+	require.Equal(t, 1.0, results[1].Records.Fields["max_clicks"].GetNumberValue())
+	require.Equal(t, parseTime(t, "2019-01-02T00:00:00Z"), results[2].Ts.AsTime())
+	require.Equal(t, 1.0, results[2].Records.Fields["max_clicks"].GetNumberValue())
+}
+
+func TestServer_Timeseries_TimeRange_Start_2day_before(t *testing.T) {
+	t.Parallel()
+	server, instanceID := getMetricsTestServer(t, "timeseries")
+
+	response, err := server.MetricsViewTimeSeries(testCtx(), &runtimev1.MetricsViewTimeSeriesRequest{
+		InstanceId:      instanceID,
+		MetricsViewName: "timeseries",
+		MeasureNames:    []string{"max_clicks"},
+		TimeStart:       timestamppb.New(parseTime(t, "2018-12-30T00:00:00Z")),
+		TimeGranularity: runtimev1.TimeGrain_TIME_GRAIN_DAY,
+	})
+
+	require.NoError(t, err)
+	results := response.Data
+	for i, v := range response.Data {
+		fmt.Printf("i: %d, ts: %v\n", i, v.Ts.AsTime())
+	}
+	require.Equal(t, 4, len(results))
+	i := 0
+	require.Equal(t, parseTime(t, "2018-12-30T00:00:00Z"), results[i].Ts.AsTime())
+	require.Equal(t, structpb.NullValue_NULL_VALUE, results[i].Records.Fields["max_clicks"].GetNullValue())
+	i += 1
+	require.Equal(t, parseTime(t, "2018-12-31T00:00:00Z"), results[i].Ts.AsTime())
+	require.Equal(t, structpb.NullValue_NULL_VALUE, results[i].Records.Fields["max_clicks"].GetNullValue())
+	i += 1
+	require.Equal(t, parseTime(t, "2019-01-01T00:00:00Z"), results[i].Ts.AsTime())
+	require.Equal(t, 1.0, results[i].Records.Fields["max_clicks"].GetNumberValue())
+	i += 1
+	require.Equal(t, parseTime(t, "2019-01-02T00:00:00Z"), results[i].Ts.AsTime())
+	require.Equal(t, 1.0, results[i].Records.Fields["max_clicks"].GetNumberValue())
+}
+
+func TestServer_Timeseries_TimeRange_End(t *testing.T) {
+	t.Parallel()
+	server, instanceID := getMetricsTestServer(t, "timeseries")
+
+	response, err := server.MetricsViewTimeSeries(testCtx(), &runtimev1.MetricsViewTimeSeriesRequest{
+		InstanceId:      instanceID,
+		MetricsViewName: "timeseries",
+		MeasureNames:    []string{"max_clicks"},
+		TimeEnd:         timestamppb.New(parseTime(t, "2019-01-04T00:00:00Z")),
+		TimeGranularity: runtimev1.TimeGrain_TIME_GRAIN_DAY,
+	})
+
+	require.NoError(t, err)
+	results := response.Data
+	for i, v := range response.Data {
+		fmt.Printf("i: %d, ts: %v\n", i, v.Ts.AsTime())
+	}
+	require.Equal(t, 3, len(results))
+	i := 0
+	require.Equal(t, parseTime(t, "2019-01-01T00:00:00Z"), results[i].Ts.AsTime())
+	require.Equal(t, 1.0, results[i].Records.Fields["max_clicks"].GetNumberValue())
+	i += 1
+	require.Equal(t, parseTime(t, "2019-01-02T00:00:00Z"), results[i].Ts.AsTime())
+	require.Equal(t, 1.0, results[i].Records.Fields["max_clicks"].GetNumberValue())
+	i += 1
+	require.Equal(t, parseTime(t, "2019-01-03T00:00:00Z"), results[i].Ts.AsTime())
+	require.Equal(t, structpb.NullValue_NULL_VALUE, results[i].Records.Fields["max_clicks"].GetNullValue())
+}
+
+func TestServer_Timeseries_TimeRange_End_2day_after(t *testing.T) {
+	t.Parallel()
+	server, instanceID := getMetricsTestServer(t, "timeseries")
+
+	response, err := server.MetricsViewTimeSeries(testCtx(), &runtimev1.MetricsViewTimeSeriesRequest{
+		InstanceId:      instanceID,
+		MetricsViewName: "timeseries",
+		MeasureNames:    []string{"max_clicks"},
+		TimeEnd:         timestamppb.New(parseTime(t, "2019-01-05T00:00:00Z")),
+		TimeGranularity: runtimev1.TimeGrain_TIME_GRAIN_DAY,
+	})
+
+	require.NoError(t, err)
+	results := response.Data
+	for i, v := range response.Data {
+		fmt.Printf("i: %d, ts: %v\n", i, v.Ts.AsTime())
+	}
+	require.Equal(t, 4, len(results))
+	i := 0
+	require.Equal(t, parseTime(t, "2019-01-01T00:00:00Z"), results[i].Ts.AsTime())
+	require.Equal(t, 1.0, results[i].Records.Fields["max_clicks"].GetNumberValue())
+	i += 1
+	require.Equal(t, parseTime(t, "2019-01-02T00:00:00Z"), results[i].Ts.AsTime())
+	require.Equal(t, 1.0, results[i].Records.Fields["max_clicks"].GetNumberValue())
+	i += 1
+	require.Equal(t, parseTime(t, "2019-01-03T00:00:00Z"), results[i].Ts.AsTime())
+	require.Equal(t, structpb.NullValue_NULL_VALUE, results[i].Records.Fields["max_clicks"].GetNullValue())
+	i += 1
+	require.Equal(t, parseTime(t, "2019-01-04T00:00:00Z"), results[i].Ts.AsTime())
+	require.Equal(t, structpb.NullValue_NULL_VALUE, results[i].Records.Fields["max_clicks"].GetNullValue())
+
+}
+
+func TestServer_Timeseries_TimeRange_middle_nulls(t *testing.T) {
+	t.Parallel()
+	server, instanceID := getMetricsTestServer(t, "timeseries")
+
+	response, err := server.MetricsViewTimeSeries(testCtx(), &runtimev1.MetricsViewTimeSeriesRequest{
+		InstanceId:      instanceID,
+		MetricsViewName: "timeseries_gaps",
+		MeasureNames:    []string{"max_clicks"},
+		TimeGranularity: runtimev1.TimeGrain_TIME_GRAIN_DAY,
+	})
+
+	require.NoError(t, err)
+	results := response.Data
+	for i, v := range response.Data {
+		fmt.Printf("i: %d, ts: %v\n", i, v.Ts.AsTime())
+	}
+	require.Equal(t, 6, len(results))
+	i := 0
+	require.Equal(t, parseTime(t, "2019-01-01T00:00:00Z"), results[i].Ts.AsTime())
+	require.Equal(t, 1.0, results[i].Records.Fields["max_clicks"].GetNumberValue())
+	i += 1
+	require.Equal(t, parseTime(t, "2019-01-02T00:00:00Z"), results[i].Ts.AsTime())
+	require.Equal(t, structpb.NullValue_NULL_VALUE, results[i].Records.Fields["max_clicks"].GetNullValue())
+	i += 1
+	require.Equal(t, parseTime(t, "2019-01-03T00:00:00Z"), results[i].Ts.AsTime())
+	require.Equal(t, 1.0, results[i].Records.Fields["max_clicks"].GetNumberValue())
+	i += 1
+	require.Equal(t, parseTime(t, "2019-01-04T00:00:00Z"), results[i].Ts.AsTime())
+	require.Equal(t, structpb.NullValue_NULL_VALUE, results[i].Records.Fields["max_clicks"].GetNullValue())
+	i += 1
+	require.Equal(t, parseTime(t, "2019-01-05T00:00:00Z"), results[i].Ts.AsTime())
+	require.Equal(t, structpb.NullValue_NULL_VALUE, results[i].Records.Fields["max_clicks"].GetNullValue())
+	i += 1
+	require.Equal(t, parseTime(t, "2019-01-06T00:00:00Z"), results[i].Ts.AsTime())
+	require.Equal(t, 1.0, results[i].Records.Fields["max_clicks"].GetNumberValue())
 }
 
 func TestServer_Timeseries_2measures(t *testing.T) {
