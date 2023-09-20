798d7832394028eb36e78a104d1f12d22ea6dcf8
brendan colloran <brendan@colloran.net>
2023-09-20 12:37:51 -0700

    switching dimension table to use sorted query API

--- web-common/src/features/dashboards/dimension-table/DimensionDisplay.svelte
+++ web-common/src/features/dashboards/dimension-table/DimensionDisplay.svelte
@@ -16,26 +16,32 @@
   import { getStateManagers } from "@rilldata/web-common/features/dashboards/state-managers/state-managers";
   import { useTimeControlStore } from "@rilldata/web-common/features/dashboards/time-controls/time-control-store";
   import {
+    createQueryServiceMetricsViewComparisonToplist,
     createQueryServiceMetricsViewToplist,
     createQueryServiceMetricsViewTotals,
     MetricsViewDimension,
     MetricsViewMeasure,
-    V1MetricsViewToplistResponseDataItem,
   } from "@rilldata/web-common/runtime-client";
   import { useQueryClient } from "@tanstack/svelte-query";
   import { runtime } from "../../../runtime-client/runtime-store";
   import { SortDirection } from "../proto-state/derived-types";
   import { metricsExplorerStore, useDashboardStore } from "../dashboard-stores";
-  import { humanizeGroupByColumns, FormatPreset } from "../humanize-numbers";
   import {
-    computeComparisonValues,
-    computePercentOfTotal,
-    getComparisonProperties,
     getFilterForComparisonTable,
+    prepareDimensionTableRows,
+    prepareVirtualizedTableColumns,
     updateFilterOnSearch,
   } from "./dimension-table-utils";
   import DimensionHeader from "./DimensionHeader.svelte";
   import DimensionTable from "./DimensionTable.svelte";
+  import {
+    getDimensionColumn,
+    isSummableMeasure,
+    prepareSortedQueryBody,
+  } from "../dashboard-utils";
+
+  import type { VirtualizedTableColumns } from "@rilldata/web-local/lib/types";
+  // import { getQuerySortType } from "../leaderboard/leaderboard-utils";
 
   export let metricViewName: string;
   export let dimensionName: string;
@@ -55,7 +61,7 @@
   );
   let dimension: MetricsViewDimension;
   $: dimension = $dimensionQuery?.data;
-  $: dimensionColumn = dimension?.column || dimension?.name;
+  $: dimensionColumn = getDimensionColumn(dimension);
 
   $: dashboardStore = useDashboardStore(metricViewName);
 
@@ -87,6 +93,8 @@
           ?.in
       : $dashboardStore?.filters.include.find((d) => d.name === dimension?.name)
           ?.in) ?? [];
+
+  $: console.log("filters", $dashboardStore?.filters);
 
   $: allMeasures = $metaQuery.data?.measures.filter((m) =>
     $dashboardStore?.visibleMeasureKeys.has(m.name)
@@ -129,7 +137,7 @@
   );
 
   // Compose the comparison /toplist query
-  $: displayComparison = $timeControlsStore.showComparison;
+  $: timeComparison = $timeControlsStore.showComparison;
   $: comparisonFilterSet = getFilterForComparisonTable(
     filterForDimension,
     dimensionName,
@@ -178,121 +186,82 @@
     }
   );
 
-  let referenceValues = {};
+  let referenceValues: { string: number } = {};
   $: if ($totalsQuery?.data?.data) {
     allMeasures.map((m) => {
-      const isSummableMeasure =
-        m?.expression.toLowerCase()?.includes("count(") ||
-        m?.expression?.toLowerCase()?.includes("sum(");
-      if (isSummableMeasure) {
+      if (isSummableMeasure(m)) {
         referenceValues[m.name] = $totalsQuery.data.data?.[m.name];
       }
     });
   }
 
-  let values: V1MetricsViewToplistResponseDataItem[] = [];
-  let columns = [];
-  let measureNames = [];
+  let columns: VirtualizedTableColumns[] = [];
 
   $: if (!$topListQuery?.isFetching && dimension) {
-    let columnsMeta = $topListQuery?.data?.meta || [];
-    values = $topListQuery?.data?.data ?? [];
-
-    let columnNames: Array<string> = columnsMeta
-      .map((c) => c.name)
-      .filter(
-        (name) =>
-          name !== dimensionColumn &&
-          $dashboardStore.visibleMeasureKeys.has(name)
-      );
-
-    const selectedMeasure = allMeasures.find(
-      (m) => m.name === leaderboardMeasureName
+    columns = prepareVirtualizedTableColumns(
+      allMeasures,
+      leaderboardMeasureName,
+      referenceValues,
+      dimension,
+      $topListQuery?.data?.meta || [],
+      timeComparison,
+      validPercentOfTotal,
+      $dashboardStore.visibleMeasureKeys
     );
-    const sortByColumnIndex = columnNames.indexOf(leaderboardMeasureName);
-    // Add comparison columns if available
-    let percentOfTotalSpliceIndex = 1;
-    if (displayComparison) {
-      percentOfTotalSpliceIndex = 2;
-      columnNames.splice(
-        sortByColumnIndex + 1,
-        0,
-        `${leaderboardMeasureName}_delta`
-      );
-
-      // Only push percentage delta column if selected measure is not a percentage
-      if (selectedMeasure?.format != FormatPreset.PERCENTAGE) {
-        percentOfTotalSpliceIndex = 3;
-        columnNames.splice(
-          sortByColumnIndex + 2,
-          0,
-          `${leaderboardMeasureName}_delta_perc`
-        );
-      }
-    }
-    if (validPercentOfTotal) {
-      columnNames.splice(
-        sortByColumnIndex + percentOfTotalSpliceIndex,
-        0,
-        `${leaderboardMeasureName}_percent_of_total`
-      );
-    }
-
-    // Make dimension the first column
-    columnNames.unshift(dimensionColumn);
-    measureNames = allMeasures.map((m) => m.name);
-
-    columns = columnNames
-      .map((columnName) => {
-        if (measureNames.includes(columnName)) {
-          // Handle all regular measures
-          const measure = allMeasures.find((m) => m.name === columnName);
-          return {
-            name: columnName,
-            type: "INT",
-            label: measure?.label || measure?.expression,
-            description: measure?.description,
-            total: referenceValues[measure.name] || 0,
-            enableResize: false,
-            format: measure?.format,
-          };
-        } else if (columnName === dimensionColumn) {
-          // Handle dimension column
-          return {
-            name: columnName,
-            type: "VARCHAR",
-            label: dimension?.label,
-            enableResize: true,
-          };
-        } else if (selectedMeasure) {
-          // Handle delta and delta_perc
-          const comparison = getComparisonProperties(
-            columnName,
-            selectedMeasure
-          );
-          return {
-            name: columnName,
-            type: comparison.type,
-            label: comparison.label,
-            description: comparison.description,
-            enableResize: false,
-            format: comparison.format,
-          };
-        }
-        return undefined;
-      })
-      .filter((column) => !!column);
   }
 
+  $: validPercentOfTotal = (
+    $leaderboardMeasureQuery?.data as MetricsViewMeasure
+  )?.validPercentOfTotal;
+
+  //////////////////////////// SORTED QUERY
+
+  $: sortedQueryBody = prepareSortedQueryBody(
+    dimensionName,
+    selectedMeasureNames,
+    $timeControlsStore,
+    leaderboardMeasureName,
+    $dashboardStore.dashboardSortType,
+    sortAscending,
+    filterForDimension
+  );
+
+  $: sortedQuery = createQueryServiceMetricsViewComparisonToplist(
+    $runtime.instanceId,
+    metricViewName,
+    sortedQueryBody,
+    {
+      query: {
+        enabled: $timeControlsStore.ready && !!filterForDimension,
+      },
+    }
+  );
+
+  $: unfilteredTotal = $totalsQuery.data?.data?.[leaderboardMeasureName];
+
+  $: newRows = $sortedQuery?.isFetching
+    ? []
+    : prepareDimensionTableRows(
+        $sortedQuery?.data?.rows,
+        allMeasures,
+        leaderboardMeasureName,
+        dimensionColumn,
+        timeComparison,
+        validPercentOfTotal,
+        unfilteredTotal
+      );
+
+  ////////////////////////////
+
   function onSelectItem(event) {
-    const label = values[event.detail][dimensionColumn];
+    const label = newRows[event.detail][dimensionColumn];
     cancelDashboardQueries(queryClient, metricViewName);
     metricsExplorerStore.toggleFilter(metricViewName, dimension?.name, label);
   }
 
   function onSortByColumn(event) {
     const columnName = event.detail;
-    if (!measureNames.includes(columnName)) return;
+    if (!allMeasures.map((m) => m.name).includes(columnName)) return;
 
     if (columnName === leaderboardMeasureName) {
       metricsExplorerStore.toggleSort(metricViewName);
@@ -311,41 +280,6 @@
       isBeingCompared ? undefined : dimensionName
     );
   }
-
-  $: if ($comparisonTopListQuery?.data && values.length && displayComparison) {
-    values = computeComparisonValues(
-      $comparisonTopListQuery?.data,
-      values,
-      dimensionName,
-      dimensionColumn,
-      leaderboardMeasureName
-    );
-  }
-
-  $: validPercentOfTotal = (
-    $leaderboardMeasureQuery?.data as MetricsViewMeasure
-  )?.validPercentOfTotal;
-
-  $: if (validPercentOfTotal && values.length && leaderboardMeasureName) {
-    const referenceValue = $totalsQuery.data?.data?.[leaderboardMeasureName];
-    values = computePercentOfTotal(
-      values,
-      referenceValue,
-      leaderboardMeasureName
-    );
-  }
-
-  $: if (values) {
-    const measureFormatSpec = columns?.map((column) => {
-      return {
-        columnName: column.name,
-        formatPreset: column.format as FormatPreset,
-      };
-    });
-    if (measureFormatSpec) {
-      values = humanizeGroupByColumns(values, measureFormatSpec);
-    }
-  }
 </script>
 
 {#if topListQuery}
@@ -362,7 +296,7 @@
       />
     </div>
 
-    {#if values && columns.length}
+    {#if newRows && columns.length}
       <div class="grow" style="overflow-y: hidden;">
         <DimensionTable
           on:select-item={(event) => onSelectItem(event)}
@@ -374,7 +308,7 @@
           {isBeingCompared}
           {columns}
           {selectedValues}
-          rows={values}
+          rows={newRows}
           sortByColumn={leaderboardMeasureName}
           {excludeMode}
         />
---
+++ web-common/src/features/dashboards/dimension-table/dimension-table-types.ts
@@ -0,0 +1,37 @@
+// import type { VirtualizedTableColumns } from "@rilldata/web-local/lib/types";
+// import type { SvelteComponent } from "svelte";
+
+import type { PERC_DIFF } from "@rilldata/web-common/components/data-types/type-utils";
+import type { NumberParts } from "@rilldata/web-common/lib/number-formatting/humanizer-types";
+
+/**
+ * A DimensionTableRow object represents a single row in a
+ * dimension table.
+ *
+ * Each row will have the following key/value pairs:
+ *
+ * 1) dimension:
+ *    - key: dimension name //FIXME: could cause key collisions
+ *    - value: dimension value for this row
+ *
+ * 2) measures:
+ *    - key: the `name` field from the YAML e.g. `measure_1`, `total`, ect
+ *    - value: the raw measure value
+ *
+ * 3) context columns for delta, delta percent, pct of total for the
+ * active measure:
+ *    - key: the YAML `name` of the active measure with  `_delta`,
+ *      `_delta_perc`, and `_percent_of_total` appended.
+ *    - value: the raw value of the context column,
+ *      or a NumberParts or PERC_DIFF for percent of total and delta
+ *
+ * 4) formatted strings:
+ *    - key: the YAML `name` of each measure with `__formatted_` prepended.
+ *      Context columns are included here as well.
+ *    - value: the formatted string for the measure or context column.
+ *
+ */
+export type DimensionTableRow = Record<
+  string,
+  string | number | NumberParts | PERC_DIFF
+>;
