e81fcd725a0e59c3c2549d165a8f9cc6610f8176
Eric P Green <ericpgreen2@gmail.com>
2023-09-25 14:06:20 -0700

    Matching styling in other export button

--- web-common/src/features/dashboards/rows-viewer/ExportModelDataButton.svelte
+++ web-common/src/features/dashboards/rows-viewer/ExportModelDataButton.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
-  import { IconButton } from "@rilldata/web-common/components/button";
   import { WithTogglableFloatingElement } from "@rilldata/web-common/components/floating-element";
   import { Menu, MenuItem } from "@rilldata/web-common/components/menu";
-  import Export from "@rilldata/web-common/components/icons/Export.svelte";
   import { getStateManagers } from "@rilldata/web-common/features/dashboards/state-managers/state-managers";
   import { useTimeControlStore } from "@rilldata/web-common/features/dashboards/time-controls/time-control-store";
   import {
-    V1ExportFormat,
     createQueryServiceExport,
+    V1ExportFormat,
   } from "@rilldata/web-common/runtime-client";
+  import CaretDownIcon from "../../../components/icons/CaretDownIcon.svelte";
   import exportMetrics from "./export-metrics";
 
   export let metricViewName;
@@ -35,18 +34,20 @@
   on:close={() => (exportMenuOpen = false)}
   on:open={() => (exportMenuOpen = true)}
 >
-  <IconButton
-    ariaLabel="Export model data"
-    disableTooltip={exportMenuOpen}
+  <button
+    aria-label="Export model data"
     on:click={(evt) => {
       evt.stopPropagation();
       toggleFloatingElement();
     }}
-    ><span class="text-gray-800">
-      <Export size="18px" />
-    </span>
-    <svelte:fragment slot="tooltip-content">Export model data</svelte:fragment>
-  </IconButton>
+    class="h-6 px-1.5 py-px flex items-center gap-[3px] rounded-sm hover:bg-gray-200 text-gray-800"
+  >
+    Export
+    <CaretDownIcon
+      size="10px"
+      className="transition-transform {exportMenuOpen && '-rotate-180'}"
+    />
+  </button>
   <Menu
     minWidth=""
     on:click-outside={toggleFloatingElement}
