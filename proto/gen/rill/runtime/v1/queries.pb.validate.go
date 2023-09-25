5e53c60e7ed4137488b5273009da4ff0521dde62
Parag Jain <pjain1@apache.org>
2023-09-25 16:35:56 +0530

    Export support for metrics view comparison toplist (#3119)
    * downloads support for metricsview comparison toplist
    
    * review comments
    
    * fix headers duckdb export

--- proto/gen/rill/runtime/v1/queries.pb.validate.go
+++ proto/gen/rill/runtime/v1/queries.pb.validate.go
@@ -552,6 +552,47 @@ func (m *ExportRequest) validate(all bool) error {
 			}
 		}
 
+	case *ExportRequest_MetricsViewComparisonToplistRequest:
+		if v == nil {
+			err := ExportRequestValidationError{
+				field:  "Request",
+				reason: "oneof value cannot be a typed-nil",
+			}
+			if !all {
+				return err
+			}
+			errors = append(errors, err)
+		}
+
+		if all {
+			switch v := interface{}(m.GetMetricsViewComparisonToplistRequest()).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, ExportRequestValidationError{
+						field:  "MetricsViewComparisonToplistRequest",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, ExportRequestValidationError{
+						field:  "MetricsViewComparisonToplistRequest",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(m.GetMetricsViewComparisonToplistRequest()).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return ExportRequestValidationError{
+					field:  "MetricsViewComparisonToplistRequest",
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
 	default:
 		_ = v // ensures v is used
 	}
