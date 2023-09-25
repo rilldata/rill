125039247e9b53477ca29e7fe3f64a04b625d422
Anshul Khandelwal <12948312+k-anshul@users.noreply.github.com>
2023-09-25 16:13:15 +0530

    Postgres connector (#3065)
    * initial pg connector commit
    
    * linting
    
    * open connection in query
    
    * remove pg prefix
    
    * array types
    
    * revert upgrade in admin
    
    * database_url is optional
    
    * interim commit
    
    * interim commit
    
    * numeric type fix
    
    * ui changes
    
    * close on errors
    
    * Update runtime/drivers/postgres/sql_store.go
    
    * Fix linter
    
    ---------
    
    Co-authored-by: Benjamin Egelund-Müller <b@egelund-muller.com>

--- runtime/services/catalog/artifacts/yaml/objects.go
+++ runtime/services/catalog/artifacts/yaml/objects.go
@@ -45,6 +45,7 @@ type Source struct {
 	SQL                         string         `yaml:"sql,omitempty" mapstructure:"sql,omitempty"`
 	DB                          string         `yaml:"db,omitempty" mapstructure:"db,omitempty"`
 	ProjectID                   string         `yaml:"project_id,omitempty" mapstructure:"project_id,omitempty"`
+	PostgresDatabaseURL         string         `yaml:"database_url,omitempty" mapstructure:"database_url,omitempty"`
 }
 
 type MetricsView struct {
@@ -205,6 +206,10 @@ func fromSourceArtifact(source *Source, path string) (*drivers.CatalogEntry, err
 
 	if source.ProjectID != "" {
 		props["project_id"] = source.ProjectID
+	}
+
+	if source.PostgresDatabaseURL != "" {
+		props["database_url"] = source.PostgresDatabaseURL
 	}
 
 	propsPB, err := structpb.NewStruct(props)
