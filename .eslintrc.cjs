34b681930a47432fe6e3a740a5906857d33af634
brendan colloran <brendan@colloran.net>
2023-09-26 15:02:15 -0700

    update eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,7 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:svelte/recommended",
     "prettier",
   ],
   plugins: [ "@typescript-eslint"],
@@ -11,7 +12,17 @@ module.exports = {
   parserOptions: {
     sourceType: "module",
     ecmaVersion: 2019,
+    extraFileExtensions: ['.svelte'] // This is a required setting in `@typescript-eslint/parser` v4.24.0.
   },
+  overrides: [
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      // Parse the `<script>` in `.svelte` as TypeScript by adding the following configuration.
+      parserOptions: {
+        parser: '@typescript-eslint/parser'
+      }
+    }],
   env: {
     browser: true,
     es2017: true,
