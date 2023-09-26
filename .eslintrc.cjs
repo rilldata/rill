a1b78ab5997f04823299f5640dbfb3934195c797
brendan colloran <brendan@colloran.net>
2023-09-26 14:56:58 -0700

    update eslint-plugin-svelte

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,12 +6,8 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "prettier",
   ],
-  plugins: ["svelte3", "@typescript-eslint"],
+  plugins: [ "@typescript-eslint"],
   ignorePatterns: ["*.cjs"],
-  overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
-  settings: {
-    "svelte3/typescript": () => require("typescript"),
-  },
   parserOptions: {
     sourceType: "module",
     ecmaVersion: 2019,
