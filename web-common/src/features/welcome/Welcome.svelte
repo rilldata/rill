241cee3fd2278134d8d80bee46d2aac8280e5baa
Eric P Green <ericpgreen2@gmail.com>
2023-09-28 09:56:08 -0700

    Add CTA to sign up for a user test (#3109)
    * Add CTA to sign up for a user test
    
    * Adjust spacing
    
    * Add missing word
    
    * Adjust spacing
    
    * Fix width

--- web-common/src/features/welcome/Welcome.svelte
+++ web-common/src/features/welcome/Welcome.svelte
@@ -3,14 +3,19 @@
   import BackgroundImage from "./BackgroundImage.svelte";
   import ProjectCards from "./ProjectCards.svelte";
   import TitleContent from "./TitleContent.svelte";
+  import UserTestCta from "./UserTestCTA.svelte";
 </script>
 
 <BackgroundImage>
   <div in:fly={{ duration: 1600, delay: 400, y: 8 }}>
-    <div class="flex flex-col gap-y-8 py-16 2xl:py-32 px-10 2xl:px-20">
+    <div class="flex flex-col items-center gap-y-6 py-10 px-10 2xl:py-16">
       <TitleContent />
-      <div in:fly={{ duration: 1600, delay: 1200, y: 4 }}>
+      <div
+        class="flex flex-col items-center gap-y-6"
+        in:fly={{ duration: 1600, delay: 1200, y: 4 }}
+      >
         <ProjectCards />
+        <UserTestCta />
       </div>
     </div>
   </div></BackgroundImage
