cb0f4edf81775ee570779600546da9f5f4d8b667
Egor Ryashin <egor.ryashin@rilldata.com>
2023-10-04 20:56:27 +0300

    first-day-of-week (benchmarks)

--- runtime/queries/metricsview_api_benchmark_test.go
+++ runtime/queries/metricsview_api_benchmark_test.go
@@ -5,18 +5,21 @@ import (
 	"testing"
 
 	runtimev1 "github.com/rilldata/rill/proto/gen/rill/runtime/v1"
+	"github.com/rilldata/rill/runtime"
 	_ "github.com/rilldata/rill/runtime/drivers/duckdb"
 	"github.com/rilldata/rill/runtime/testruntime"
 	"github.com/stretchr/testify/require"
 )
 
 func BenchmarkMetricsViewsTotals(b *testing.B) {
-	rt, instanceID := testruntime.NewInstanceForProject(b, "ad_bids")
+	rt, instanceID, mv := prepareEnvironment(b)
+
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		q := &MetricsViewTotals{
 			MetricsViewName: "ad_bids_metrics",
-			MeasureNames:    []string{"measure_0"},
+			MeasureNames:    []string{"avg_price"},
+			MetricsView:     mv,
 		}
 		err := q.Resolve(context.Background(), rt, instanceID, 0)
 		require.NoError(b, err)
@@ -25,18 +28,20 @@ func BenchmarkMetricsViewsTotals(b *testing.B) {
 }
 
 func BenchmarkMetricsViewsToplist(b *testing.B) {
-	rt, instanceID := testruntime.NewInstanceForProject(b, "ad_bids")
+	rt, instanceID, mv := prepareEnvironment(b)
+
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		q := &MetricsViewToplist{
 			MetricsViewName: "ad_bids_metrics",
 			DimensionName:   "dom",
-			MeasureNames:    []string{"measure_1"},
+			MeasureNames:    []string{"avg_price"},
 			Sort: []*runtimev1.MetricsViewSort{
 				{
-					Name: "measure_1",
+					Name: "avg_price",
 				},
 			},
+			MetricsView: mv,
 		}
 		err := q.Resolve(context.Background(), rt, instanceID, 0)
 		require.NoError(b, err)
@@ -45,13 +50,15 @@ func BenchmarkMetricsViewsToplist(b *testing.B) {
 }
 
 func BenchmarkMetricsViewsTimeSeries(b *testing.B) {
-	rt, instanceID := testruntime.NewInstanceForProject(b, "ad_bids")
+	rt, instanceID, mv := prepareEnvironment(b)
+
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		q := &MetricsViewTimeSeries{
 			MetricsViewName: "ad_bids_metrics",
 			TimeGranularity: runtimev1.TimeGrain_TIME_GRAIN_DAY,
-			MeasureNames:    []string{"measure_0"},
+			MeasureNames:    []string{"avg_price"},
+			MetricsView:     mv,
 		}
 		err := q.Resolve(context.Background(), rt, instanceID, 0)
 		require.NoError(b, err)
@@ -60,14 +67,16 @@ func BenchmarkMetricsViewsTimeSeries(b *testing.B) {
 }
 
 func BenchmarkMetricsViewsTimeSeries_TimeZone(b *testing.B) {
-	rt, instanceID := testruntime.NewInstanceForProject(b, "ad_bids")
+	rt, instanceID, mv := prepareEnvironment(b)
+
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		q := &MetricsViewTimeSeries{
 			MetricsViewName: "ad_bids_metrics",
 			TimeGranularity: runtimev1.TimeGrain_TIME_GRAIN_DAY,
-			MeasureNames:    []string{"measure_0"},
+			MeasureNames:    []string{"avg_price"},
 			TimeZone:        "Asia/Kathmandu",
+			MetricsView:     mv,
 		}
 		err := q.Resolve(context.Background(), rt, instanceID, 0)
 		require.NoError(b, err)
@@ -76,17 +85,27 @@ func BenchmarkMetricsViewsTimeSeries_TimeZone(b *testing.B) {
 }
 
 func BenchmarkMetricsViewsTimeSeries_TimeZone_Hour(b *testing.B) {
-	rt, instanceID := testruntime.NewInstanceForProject(b, "ad_bids")
+	rt, instanceID, mv := prepareEnvironment(b)
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		q := &MetricsViewTimeSeries{
 			MetricsViewName: "ad_bids_metrics",
 			TimeGranularity: runtimev1.TimeGrain_TIME_GRAIN_HOUR,
-			MeasureNames:    []string{"measure_0"},
+			MeasureNames:    []string{"avg_price"},
 			TimeZone:        "Asia/Kathmandu",
+			MetricsView:     mv,
 		}
 		err := q.Resolve(context.Background(), rt, instanceID, 0)
 		require.NoError(b, err)
 		require.NotEmpty(b, q.Result)
 	}
+}
+
+func prepareEnvironment(b *testing.B) (*runtime.Runtime, string, *runtimev1.MetricsView) {
+	rt, instanceID := testruntime.NewInstanceForProject(b, "ad_bids")
+	obj, err := rt.GetCatalogEntry(context.Background(), instanceID, "ad_bids_metrics")
+	require.NoError(b, err)
+
+	mv := obj.GetMetricsView()
+	return rt, instanceID, mv
 }
