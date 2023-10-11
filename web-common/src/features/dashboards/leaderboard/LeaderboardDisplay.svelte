3c945bde36eb7c54ec1b557fd33d16aa11c3b2f0
Benjamin Egelund-Müller <b@egelund-muller.com>
2023-10-11 09:55:45 +0200

    Fix frontend build

--- web-common/src/features/dashboards/leaderboard/LeaderboardDisplay.svelte
+++ web-common/src/features/dashboards/leaderboard/LeaderboardDisplay.svelte
@@ -63,7 +63,7 @@
   );
 
   $: formatPreset =
-    (activeMeasure?.format as FormatPreset) ?? FormatPreset.HUMANIZE;
+    (activeMeasure?.formatPreset as FormatPreset) ?? FormatPreset.HUMANIZE;
 
   let referenceValue: number;
   $: if (activeMeasure?.name && $totalsQuery?.data?.data) {
