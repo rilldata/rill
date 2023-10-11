3fdad3bc9100c79954c2e9a2fb89f6792d7dafcf
Rakesh Sharma <rakeshsharma14317@gmail.com>
2023-10-11 20:14:47 +0530

    small changes

--- cli/cmd/org/org_test.go
+++ cli/cmd/org/org_test.go
@@ -3,30 +3,21 @@ package org
 import (
 	"bytes"
 	"context"
-	"encoding/hex"
 	"encoding/json"
 	"testing"
 	"time"
 
 	qt "github.com/frankban/quicktest"
 	"github.com/google/go-github/v50/github"
-	"github.com/rilldata/rill/admin"
 	"github.com/rilldata/rill/admin/database"
-	"github.com/rilldata/rill/admin/email"
 	"github.com/rilldata/rill/admin/pkg/pgtestcontainer"
-	"github.com/rilldata/rill/admin/server"
-	admincli "github.com/rilldata/rill/cli/cmd/admin"
 	"github.com/rilldata/rill/cli/pkg/cmdutil"
 	"github.com/rilldata/rill/cli/pkg/config"
+	"github.com/rilldata/rill/cli/pkg/mock"
 	"github.com/rilldata/rill/cli/pkg/printer"
 	"github.com/rilldata/rill/runtime/pkg/graceful"
-	"github.com/rilldata/rill/runtime/pkg/observability"
-	"github.com/rilldata/rill/runtime/pkg/ratelimit"
-	runtimeauth "github.com/rilldata/rill/runtime/server/auth"
-	"github.com/stretchr/testify/require"
-	"golang.org/x/sync/errgroup"
-
 	"go.uber.org/zap"
+	"golang.org/x/sync/errgroup"
 )
 
 func TestOrgCmd(t *testing.T) {
@@ -37,92 +28,35 @@ func TestOrgCmd(t *testing.T) {
 	ctx := context.Background()
 	logger, _ := zap.NewDevelopment()
 
-	sender, err := email.NewConsoleSender(logger, "rill-test@rilldata.io", "")
-	require.NoError(t, err)
-	emailClient := email.New(sender, "", "")
-
-	gh := &mockGithub{}
-	issuer, err := runtimeauth.NewEphemeralIssuer("")
-	require.NoError(t, err)
-
-	provisionerSpec := "{\"runtimes\":[{\"host\":\"http://localhost:9091\",\"slots\":50,\"data_dir\":\"\",\"audience_url\":\"http://localhost:8081\"}]}"
-
-	// Init admin service
-	admOpts := &admin.Options{
-		DatabaseDriver:  "postgres",
-		DatabaseDSN:     pg.DatabaseURL,
-		ProvisionerSpec: provisionerSpec,
-	}
-
-	adm, err := admin.New(ctx, admOpts, logger, issuer, emailClient, gh)
-	if err != nil {
-		logger.Fatal("error creating service", zap.Error(err))
-	}
+	// Get Admin service
+	adm, err := mock.AdminService(ctx, logger, pg.DatabaseURL)
 	defer adm.Close()
-
+	c.Assert(err, qt.IsNil)
 	db := adm.DB
-	// create admin user
+
+	// create mock admin user
 	adminUser, err := db.InsertUser(ctx, &database.InsertUserOptions{
 		Email:               "admin@test.io",
 		DisplayName:         "admin",
 		QuotaSingleuserOrgs: 3,
 	})
-	require.NoError(t, err)
+	c.Assert(err, qt.IsNil)
 
 	// issue admin and viewer tokens
 	adminAuthToken, err := adm.IssueUserAuthToken(ctx, adminUser.ID, database.AuthClientIDRillWeb, "test", nil, nil)
-	require.NoError(t, err)
-	require.NotNil(t, adminAuthToken)
+	c.Assert(err, qt.IsNil)
+	c.Assert(adminAuthToken, qt.Not(qt.IsNil))
 
-	// Creating a dummy config
-	seesionKeyPairs := []string{"7938b8c95ac90b3731c353076daeae8a", "90c22a5a6c6b442afdb46855f95eb7d6"}
-	conf := &admincli.Config{
-		DatabaseURL:     pg.DatabaseURL,
-		HTTPPort:        8080,
-		GRPCPort:        9090,
-		ExternalURL:     "http://localhost:8080",
-		FrontendURL:     "http://localhost:3000",
-		SessionKeyPairs: seesionKeyPairs,
-		AuthDomain:      "gorillio-stage.auth0.com",
-	}
-
-	keyPairs := make([][]byte, len(conf.SessionKeyPairs))
-	for idx, keyHex := range conf.SessionKeyPairs {
-		key, err := hex.DecodeString(keyHex)
-		if err != nil {
-			logger.Fatal("failed to parse session key from hex string to bytes")
-		}
-		keyPairs[idx] = key
-	}
+	// Create mock admin server
+	srv, err := mock.AdminServer(ctx, logger, adm)
+	c.Assert(err, qt.IsNil)
 
 	// Make errgroup for running the processes
 	ctx = graceful.WithCancelOnTerminate(ctx)
 	group, cctx := errgroup.WithContext(ctx)
 
-	limiter := ratelimit.NewNoop()
-	srv, err := server.New(logger, adm, issuer, limiter, &server.Options{
-		HTTPPort:               conf.HTTPPort,
-		GRPCPort:               conf.GRPCPort,
-		ExternalURL:            conf.ExternalURL,
-		FrontendURL:            conf.FrontendURL,
-		SessionKeyPairs:        keyPairs,
-		AllowedOrigins:         conf.AllowedOrigins,
-		ServePrometheus:        conf.MetricsExporter == observability.PrometheusExporter,
-		AuthDomain:             conf.AuthDomain,
-		AuthClientID:           conf.AuthClientID,
-		AuthClientSecret:       conf.AuthClientSecret,
-		GithubAppName:          conf.GithubAppName,
-		GithubAppWebhookSecret: conf.GithubAppWebhookSecret,
-		GithubClientID:         conf.GithubClientID,
-		GithubClientSecret:     conf.GithubClientSecret,
-	})
-	if err != nil {
-		logger.Fatal("error creating server", zap.Error(err))
-	}
-
 	group.Go(func() error { return srv.ServeGRPC(cctx) })
 	group.Go(func() error { return srv.ServeHTTP(cctx) })
-
 	time.Sleep(05 * time.Second)
 
 	// Create organization with name
