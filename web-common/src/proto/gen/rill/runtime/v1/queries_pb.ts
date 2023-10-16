45d4089e1d17de7e563bc6e6e663e70fbcc01e3c
Egor Riashin <egor-ryashin@users.noreply.github.com>
2023-10-16 19:11:30 +0300

    Approximate toplist comparison (#3161)
    * approximate-join
    
    * approximate-join
    
    * approximate-join
    
    * approximate-join
    
    * approximate-join
    
    * approximate-join
    
    * approximate-join
    
    * approximate-join
    
    * approximate-join
    
    * approximate-join
    
    * approximate-join
    
    * approximate-join
    
    * Merge remote-tracking branch 'origin/main' into approximate-comparison
    
    * Merge remote-tracking branch 'origin/main' into approximate-comparison
    
    * Merge remote-tracking branch 'origin/main' into approximate-comparison
    
    ---------
    
    Co-authored-by: Egor Ryashin <egor.ryashin@rilldata.com>

--- web-common/src/proto/gen/rill/runtime/v1/queries_pb.ts
+++ web-common/src/proto/gen/rill/runtime/v1/queries_pb.ts
@@ -854,6 +854,11 @@ export class MetricsViewComparisonToplistRequest extends Message<MetricsViewComp
    */
   priority = 0;
 
+  /**
+   * @generated from field: bool exact = 13;
+   */
+  exact = false;
+
   constructor(data?: PartialMessage<MetricsViewComparisonToplistRequest>) {
     super();
     proto3.util.initPartial(data, this);
@@ -874,6 +879,7 @@ export class MetricsViewComparisonToplistRequest extends Message<MetricsViewComp
     { no: 10, name: "limit", kind: "scalar", T: 3 /* ScalarType.INT64 */ },
     { no: 11, name: "offset", kind: "scalar", T: 3 /* ScalarType.INT64 */ },
     { no: 12, name: "priority", kind: "scalar", T: 5 /* ScalarType.INT32 */ },
+    { no: 13, name: "exact", kind: "scalar", T: 8 /* ScalarType.BOOL */ },
   ]);
 
   static fromBinary(bytes: Uint8Array, options?: Partial<BinaryReadOptions>): MetricsViewComparisonToplistRequest {
