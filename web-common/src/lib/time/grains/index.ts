f650624213193361fd65e309446dfa64e16d9b86
Nishant Bangarwa <nishant.monu51@gmail.com>
2023-10-24 12:22:53 -0700

    Relax constraints to allow minute granularity for a day

--- web-common/src/lib/time/grains/index.ts
+++ web-common/src/lib/time/grains/index.ts
@@ -30,12 +30,12 @@ export function getDefaultTimeGrain(start: Date, end: Date): TimeGrain {
     return TIME_GRAIN.TIME_GRAIN_MINUTE;
   } else if (
     timeRangeDurationMs <
-    7 * durationToMillis(TIME_GRAIN.TIME_GRAIN_DAY.duration)
+    durationToMillis(TIME_GRAIN.TIME_GRAIN_WEEK.duration)
   ) {
     return TIME_GRAIN.TIME_GRAIN_HOUR;
   } else if (
     timeRangeDurationMs <
-    3 * durationToMillis(TIME_GRAIN.TIME_GRAIN_DAY.duration) * 30
+    durationToMillis(TIME_GRAIN.TIME_GRAIN_QUARTER.duration)
   ) {
     return TIME_GRAIN.TIME_GRAIN_DAY;
   } else if (
@@ -57,21 +57,22 @@ export function getAllowedTimeGrains(start: Date, end: Date): TimeGrain[] {
     return [TIME_GRAIN.TIME_GRAIN_MINUTE];
   } else if (
     timeRangeDurationMs <
-    6 * durationToMillis(TIME_GRAIN.TIME_GRAIN_HOUR.duration)
+    durationToMillis(TIME_GRAIN.TIME_GRAIN_DAY.duration)
   ) {
     return [TIME_GRAIN.TIME_GRAIN_MINUTE, TIME_GRAIN.TIME_GRAIN_HOUR];
   } else if (
-    timeRangeDurationMs < durationToMillis(TIME_GRAIN.TIME_GRAIN_DAY.duration)
+      timeRangeDurationMs ==
+      durationToMillis(TIME_GRAIN.TIME_GRAIN_DAY.duration)
   ) {
-    return [TIME_GRAIN.TIME_GRAIN_HOUR];
+    return [TIME_GRAIN.TIME_GRAIN_MINUTE, TIME_GRAIN.TIME_GRAIN_HOUR, TIME_GRAIN.TIME_GRAIN_DAY];
   } else if (
     timeRangeDurationMs <
-    7 * durationToMillis(TIME_GRAIN.TIME_GRAIN_DAY.duration)
+    durationToMillis(TIME_GRAIN.TIME_GRAIN_WEEK.duration)
   ) {
     return [TIME_GRAIN.TIME_GRAIN_HOUR, TIME_GRAIN.TIME_GRAIN_DAY];
   } else if (
     timeRangeDurationMs <
-    30 * durationToMillis(TIME_GRAIN.TIME_GRAIN_DAY.duration)
+    durationToMillis(TIME_GRAIN.TIME_GRAIN_MONTH.duration)
   ) {
     return [
       TIME_GRAIN.TIME_GRAIN_HOUR,
@@ -80,7 +81,7 @@ export function getAllowedTimeGrains(start: Date, end: Date): TimeGrain[] {
     ];
   } else if (
     timeRangeDurationMs <
-    3 * 30 * durationToMillis(TIME_GRAIN.TIME_GRAIN_DAY.duration)
+    durationToMillis(TIME_GRAIN.TIME_GRAIN_QUARTER.duration)
   ) {
     return [
       TIME_GRAIN.TIME_GRAIN_DAY,
