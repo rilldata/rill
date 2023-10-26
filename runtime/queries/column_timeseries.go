88a34ffc0f7bb424ec001fe2a6f6099348a42fa1
Egor Riashin <egor-ryashin@users.noreply.github.com>
2023-10-26 17:49:48 +0300

    MetricsView and column timeseries decoupling (#2925)
    * mv-decouple
    
    * decoupling metrics view and column timeseries
    
    * decoupling metrics view and column timeseries
    
    * review
    
    * null filling for UI
    
    * merge fix
    
    * null filling for UI
    
    * prettier
    
    * code style
    
    * tests
    
    * tests
    
    * null filling for UI
    
    * ui test fix
    
    * Merge remote-tracking branch 'origin/main' into decouple-ts-and-mv
    
    * null filling for UI
    
    * null filling for UI
    
    * null filling for UI
    
    * null filling for UI
    
    * decoupling mv
    
    * Revert "null filling for UI"
    
    This reverts commit 04be700ea2bf7066fe49d6c8a68fb3b93987114f.
    
    * decoupling mv timeseries
    
    * decoupling mv timeseries
    
    * decoupling mv timeseries
    
    * merge fix
    
    * decoupling mv timeseries
    
    * decoupling mv timeseries
    
    * merge fix
    
    * decoupling mv timeseries
    
    ---------
    
    Co-authored-by: Egor Ryashin <egor.ryashin@rilldata.com>

--- runtime/queries/column_timeseries.go
+++ runtime/queries/column_timeseries.go
@@ -221,8 +221,8 @@ func (q *ColumnTimeseries) Resolve(ctx context.Context, rt *runtime.Runtime, ins
 		}
 
 		var data []*runtimev1.TimeSeriesValue
+		rowMap := make(map[string]any)
 		for rows.Next() {
-			rowMap := make(map[string]any)
 			err := rows.MapScan(rowMap)
 			if err != nil {
 				rows.Close()
