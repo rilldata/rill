b2856e34ebaf594e949fd0b204e93b31f2813de1
Benjamin Egelund-Müller <b@egelund-muller.com>
2023-10-25 19:12:37 -0700

    Fix migration

--- admin/database/postgres/migrations/0018.sql
+++ admin/database/postgres/migrations/0018.sql
@@ -1,5 +1,5 @@
-ALTER TABLE project_roles ADD COLUMN create_reports BOOLEAN NOT NULL DEFAULT false;
-UPDATE TABLE project_roles SET create_reports = read_prod;
+ALTER TABLE project_roles ADD create_reports BOOLEAN NOT NULL DEFAULT false;
+UPDATE project_roles SET create_reports = read_prod;
 
-ALTER TABLE project_roles ADD COLUMN manage_reports BOOLEAN NOT NULL DEFAULT false;
-UPDATE TABLE project_roles SET manage_reports = manage_prod;
+ALTER TABLE project_roles ADD manage_reports BOOLEAN NOT NULL DEFAULT false;
+UPDATE project_roles SET manage_reports = manage_prod;
