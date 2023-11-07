0d7f071dd41f84e62b107d3f6a14c486cc76516e
Eric P Green <ericpgreen2@gmail.com>
2023-11-06 17:47:40 -0800

    Better logic for report's dashboard name; add link

--- web-admin/src/features/scheduled-reports/selectors.ts
+++ web-admin/src/features/scheduled-reports/selectors.ts
@@ -16,3 +16,28 @@ export function useReport(instanceId: string, name: string) {
     "name.kind": ResourceKind.Report,
   });
 }
+
+export function useReportDashboardName(instanceId: string, name: string) {
+  return createRuntimeServiceGetResource(
+    instanceId,
+    {
+      "name.name": name,
+      "name.kind": ResourceKind.Report,
+    },
+    {
+      query: {
+        select: (data) => {
+          const queryArgsJson = JSON.parse(
+            data.resource.report.spec.queryArgsJson
+          );
+
+          return (
+            queryArgsJson?.metrics_view_name ??
+            queryArgsJson?.metricsViewName ??
+            null
+          );
+        },
+      },
+    }
+  );
+}
