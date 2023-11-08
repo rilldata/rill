fcf914cdb675b56281b1dd91a33546ab4f9c78f6
brendan colloran <brendan@colloran.net>
2023-11-08 12:36:06 -0800

    initial experiments

---
+++ web-local/test/ui/dashboard-flows/dashboard-flow-template.ts
@@ -0,0 +1,26 @@
+import { createDashboardFromModel } from "../utils/dashboardHelpers";
+import { createAdBidsModel } from "../utils/dataSpecifcHelpers";
+import { test, expect } from "@playwright/test";
+import { startRuntimeForEachTest } from "../utils/startRuntimeForEachTest";
+
+test.describe("dashboard", () => {
+  startRuntimeForEachTest();
+
+  test("Dashboard runthrough", async ({ page }) => {
+    test.setTimeout(60000);
+    await page.goto("/");
+    // disable animations
+    await page.addStyleTag({
+      content: `
+        *, *::before, *::after {
+          animation-duration: 0s !important;
+          transition-duration: 0s !important;
+        }
+      `,
+    });
+    await createAdBidsModel(page);
+    await createDashboardFromModel(page, "AdBids_model");
+
+    // ADD BREAKPOINT HERE
+  });
+});
