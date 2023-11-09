e7222c1b82c678a21c61db870495230271cca97b
brendan colloran <brendan@colloran.net>
2023-11-09 12:21:47 -0800

    limit strictNullChecks=false to just two folders within web-common/src/features

--- web-local/src/routes/(application)/+error.svelte
+++ web-local/src/routes/(application)/+error.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <div class="grid h-screen place-content-center bg-white text-xl">
-  {#if $page.error.message}
-    <p class="error">{$page.status}: {$page.error.message}</p>
+  {#if $page.error?.message}
+    <p class="error">{$page.status}: {$page.error?.message}</p>
   {:else}
     <p class="error">Encountered a {$page.status} error</p>
   {/if}
