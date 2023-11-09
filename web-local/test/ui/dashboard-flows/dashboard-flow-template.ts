08fa99ee0ef85cf872dd7c1ce05b5b11080fdb69
brendan colloran <brendan@colloran.net>
2023-11-09 09:42:28 -0800

    add "rename" strings in template

--- web-local/test/ui/dashboard-flows/dashboard-flow-template.ts
+++ web-local/test/ui/dashboard-flows/dashboard-flow-template.ts
@@ -3,10 +3,12 @@ import { createAdBidsModel } from "../utils/dataSpecifcHelpers";
 import { test, expect } from "@playwright/test";
 import { startRuntimeForEachTest } from "../utils/startRuntimeForEachTest";
 
-test.describe("dashboard", () => {
+test.describe("~~~~~~~~~~~~~~~~~~~~FIXME RENAME THIS~~~~~~~~~~~~~~~~~~~~~~~", () => {
   startRuntimeForEachTest();
 
-  test("Dashboard runthrough", async ({ page }) => {
+  test("~~~~~~~~~~~~~~~~~~~~FIXME RENAME THIS~~~~~~~~~~~~~~~~~~~~~~~", async ({
+    page,
+  }) => {
     test.setTimeout(60000);
     await page.goto("/");
     // disable animations
