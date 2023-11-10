baa5db8fe62ea01504e3dd92050b631860f36411
Dhiraj Kumar <sumankumaribarnwal@gmail.com>
2023-11-11 02:24:44 +0530

    Big number entry point as card

--- web-common/src/features/dashboards/big-number/MeasureBigNumber.svelte
+++ web-common/src/features/dashboards/big-number/MeasureBigNumber.svelte
@@ -4,10 +4,7 @@
   import PercentageChange from "@rilldata/web-common/components/data-types/PercentageChange.svelte";
   import CrossIcon from "@rilldata/web-common/components/icons/CrossIcon.svelte";
   import Tooltip from "@rilldata/web-common/components/tooltip/Tooltip.svelte";
-  import TooltipContent from "@rilldata/web-common/components/tooltip/TooltipContent.svelte";
-  import TooltipDescription from "@rilldata/web-common/components/tooltip/TooltipDescription.svelte";
   import { EntityStatus } from "@rilldata/web-common/features/entity-management/types";
-  import { TIME_COMPARISON } from "@rilldata/web-common/lib/time/config";
   import type { TimeComparisonOption } from "@rilldata/web-common/lib/time/types";
   import {
     CrossfadeParams,
@@ -16,11 +13,14 @@
     fly,
   } from "svelte/transition";
   import Spinner from "../../entity-management/Spinner.svelte";
-
   import type { MetricsViewSpecMeasureV2 } from "@rilldata/web-common/runtime-client";
   import { createMeasureValueFormatter } from "@rilldata/web-common/lib/number-formatting/format-measure-value";
   import { FormatPreset } from "@rilldata/web-common/lib/number-formatting/humanizer-types";
   import { formatMeasurePercentageDifference } from "@rilldata/web-common/lib/number-formatting/percentage-formatter";
+  import BigNumberTooltipContent from "@rilldata/web-common/features/dashboards/big-number/BigNumberTooltipContent.svelte";
+  import { notifications } from "@rilldata/web-common/components/notifications";
+  import { numberPartsToString } from "@rilldata/web-common/lib/number-formatting/utils/number-parts-utils";
+  import { createShiftClickAction } from "@rilldata/web-common/lib/actions/shift-click-action";
 
   export let measure: MetricsViewSpecMeasureV2;
   export let value: number;
@@ -31,14 +31,11 @@
   export let status: EntityStatus;
   export let withTimeseries = true;
   export let isMeasureExpanded = false;
-  export let hoveredMeasure: string | undefined = undefined;
 
   const dispatch = createEventDispatcher();
 
-  $: description =
-    measure?.description || measure?.label || measure?.expression;
-
   $: measureValueFormatter = createMeasureValueFormatter(measure);
+
   $: measureValueFormatterUnabridged = createMeasureValueFormatter(
     measure,
     true
@@ -66,120 +63,128 @@
 
   /** when the measure is a percentage, we don't show a percentage change. */
   $: measureIsPercentage = measure?.formatPreset === FormatPreset.PERCENTAGE;
+
+  $: hoveredValue = measureValueFormatterUnabridged(value);
+
+  const { shiftClickAction } = createShiftClickAction();
+  async function shiftClickHandler(number: string) {
+    await navigator.clipboard.writeText(number);
+    notifications.send({
+      message: `copied dimension value "${number}" to clipboard`,
+    });
+  }
 </script>
 
-<button
-  on:mouseenter={() => (hoveredMeasure = measure?.name)}
-  on:mouseleave={() => (hoveredMeasure = undefined)}
-  on:click={() => dispatch("expand-measure")}
-  class="big-number flex flex-col px-2 text-left rounded
-  {isMeasureExpanded ? 'pointer-events-none' : 'hover:bg-gray-100'}
-  {withTimeseries ? 'py-3' : 'py-1 justify-between'}"
->
-  <Tooltip distance={16} location="top" alignment="start">
-    <h2
-      style:overflow-wrap="anywhere"
-      class="line-clamp-2"
-      style:font-size={withTimeseries ? "" : "0.8rem"}
-    >
-      {name}
-    </h2>
-    <TooltipContent slot="tooltip-content" maxWidth="280px">
-      <TooltipDescription>
-        {description}
-      </TooltipDescription>
-    </TooltipContent>
-  </Tooltip>
+<Tooltip distance={8} location="right" alignment="start">
+  <BigNumberTooltipContent
+    slot="tooltip-content"
+    {measure}
+    value={hoveredValue}
+  />
+
   <div
-    class="ui-copy-muted relative"
-    style:font-size={withTimeseries ? "1.6rem" : "1.8rem"}
-    style:font-weight="light"
+    use:shiftClickAction
+    on:shift-click={() => shiftClickHandler(hoveredValue)}
+    class="big-number m-0.5 rounded"
   >
-    <div>
-      {#if valueIsPresent && status === EntityStatus.Idle}
-        <Tooltip distance={8} location="bottom" alignment="start">
-          <div class="w-max">
-            <WithTween {value} tweenProps={{ duration: 500 }} let:output>
-              {measureValueFormatter(output)}
-            </WithTween>
-          </div>
-          <TooltipContent slot="tooltip-content">
-            {measureValueFormatterUnabridged(value)}
-            <TooltipDescription>
-              the aggregate value over the current time period
-            </TooltipDescription>
-          </TooltipContent>
-        </Tooltip>
-        {#if showComparison && comparisonOption && comparisonValue}
-          <Tooltip distance={8} location="bottom" alignment="start">
-            <div class="flex items-baseline gap-x-3">
-              {#if comparisonValue != null}
-                <div
-                  class="w-max text-sm ui-copy-inactive"
-                  class:font-semibold={isComparisonPositive}
-                >
-                  {#if !noChange}
-                    {formattedDiff}
-                  {:else}
-                    <span
-                      class="ui-copy-disabled-faint italic"
-                      style:font-size=".9em">no change</span
-                    >
-                  {/if}
-                </div>
-              {/if}
-              {#if comparisonPercChange != null && !noChange && !measureIsPercentage}
-                <div
-                  class="w-max text-sm
-              {isComparisonPositive ? 'ui-copy-inactive' : 'text-red-500'}"
-                >
-                  <WithTween
-                    value={comparisonPercChange}
-                    tweenProps={{ duration: 500 }}
-                    let:output
-                  >
-                    <PercentageChange
-                      tabularNumber={false}
-                      value={formatMeasurePercentageDifference(output)}
-                    />
-                  </WithTween>
-                </div>
-              {/if}
+    <button
+      on:click={(e) => {
+        if (e.shiftKey) return;
+        dispatch("expand-measure");
+      }}
+      class="flex flex-col px-2 text-left
+    {withTimeseries ? 'py-3' : 'py-1 justify-between'}
+    {isMeasureExpanded ? 'cursor-default' : ''}
+    "
+    >
+      <h2
+        style:overflow-wrap="anywhere"
+        class="line-clamp-2"
+        style:font-size={withTimeseries ? "" : "0.8rem"}
+      >
+        {name}
+      </h2>
+      <div
+        class="ui-copy-muted relative"
+        style:font-size={withTimeseries ? "1.6rem" : "1.8rem"}
+        style:font-weight="light"
+      >
+        <div>
+          {#if valueIsPresent && status === EntityStatus.Idle}
+            <div class="w-max">
+              <WithTween {value} tweenProps={{ duration: 500 }} let:output>
+                {measureValueFormatter(output)}
+              </WithTween>
             </div>
-            <TooltipContent slot="tooltip-content" maxWidth="300px">
-              {#if noChange}
-                no change over {TIME_COMPARISON[comparisonOption].shorthand}
-              {:else}
-                {TIME_COMPARISON[comparisonOption].shorthand}
-                <span class="font-semibold">
-                  {measureValueFormatter(comparisonValue)}
-                </span>
-                {#if !measureIsPercentage && comparisonPercChange}
-                  <span class="text-gray-300">,</span>
-                  <span
-                    >{formatMeasurePercentageDifference(comparisonPercChange)
-                      .int}% {isComparisonPositive
-                      ? "increase"
-                      : "decrease"}</span
+            {#if showComparison && comparisonOption && comparisonValue}
+              <div class="flex items-baseline gap-x-3">
+                {#if comparisonValue != null}
+                  <div
+                    on:mouseenter={() =>
+                      (hoveredValue = measureValueFormatterUnabridged(diff))}
+                    on:mouseleave={() =>
+                      (hoveredValue = measureValueFormatterUnabridged(value))}
+                    class="w-max text-sm ui-copy-inactive"
+                    class:font-semibold={isComparisonPositive}
                   >
+                    {#if !noChange}
+                      {formattedDiff}
+                    {:else}
+                      <span
+                        class="ui-copy-disabled-faint italic"
+                        style:font-size=".9em">no change</span
+                      >
+                    {/if}
+                  </div>
                 {/if}
-              {/if}
-            </TooltipContent>
-          </Tooltip>
-        {/if}
-      {:else if status === EntityStatus.Error}
-        <CrossIcon />
-      {:else if status === EntityStatus.Running}
-        <div
-          class="{withTimeseries ? '' : 'bottom-0'} absolute p-2"
-          in:receive|local={{ key: "spinner" }}
-          out:send|local={{ key: "spinner" }}
-        >
-          <Spinner status={EntityStatus.Running} />
+                {#if comparisonPercChange != null && !noChange && !measureIsPercentage}
+                  <div
+                    on:mouseenter={() =>
+                      (hoveredValue = numberPartsToString(
+                        formatMeasurePercentageDifference(
+                          comparisonPercChange ?? 0
+                        )
+                      ))}
+                    on:mouseleave={() =>
+                      (hoveredValue = measureValueFormatterUnabridged(value))}
+                    class="w-max text-sm
+              {isComparisonPositive ? 'ui-copy-inactive' : 'text-red-500'}"
+                  >
+                    <WithTween
+                      value={comparisonPercChange}
+                      tweenProps={{ duration: 500 }}
+                      let:output
+                    >
+                      <PercentageChange
+                        tabularNumber={false}
+                        value={formatMeasurePercentageDifference(output)}
+                      />
+                    </WithTween>
+                  </div>
+                {/if}
+              </div>
+            {/if}
+          {:else if status === EntityStatus.Error}
+            <CrossIcon />
+          {:else if status === EntityStatus.Running}
+            <div
+              class="{withTimeseries ? '' : 'bottom-0'} absolute p-2"
+              in:receive|local={{ key: "spinner" }}
+              out:send|local={{ key: "spinner" }}
+            >
+              <Spinner status={EntityStatus.Running} />
+            </div>
+          {:else if !valueIsPresent}
+            <span class="ui-copy-disabled-faint italic text-sm">no data</span>
+          {/if}
         </div>
-      {:else if !valueIsPresent}
-        <span class="ui-copy-disabled-faint italic text-sm">no data</span>
-      {/if}
-    </div>
+      </div>
+    </button>
   </div>
-</button>
+</Tooltip>
+
+<style lang="postcss">
+  .big-number:hover {
+    @apply ui-card;
+  }
+</style>
