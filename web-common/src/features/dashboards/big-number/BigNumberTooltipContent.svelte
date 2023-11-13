b2b9f7a6d81a065c367ea7d3676321701914bfa2
Dhiraj Barnwal <sumankumaribarnwal@gmail.com>
2023-11-13 23:17:37 +0530

    Update tooltips for Big number (#3460)
    * Big number entry point as card
    
    * Declare custom event as namespace
    
    * shorten import
    
    * Full card is clickable
    
    * Fix tests
    
    * cursor-fix

---
+++ web-common/src/features/dashboards/big-number/BigNumberTooltipContent.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import Shortcut from "@rilldata/web-common/components/tooltip/Shortcut.svelte";
+  import StackingWord from "@rilldata/web-common/components/tooltip/StackingWord.svelte";
+  import TooltipContent from "@rilldata/web-common/components/tooltip/TooltipContent.svelte";
+  import TooltipShortcutContainer from "@rilldata/web-common/components/tooltip/TooltipShortcutContainer.svelte";
+  import TooltipTitle from "@rilldata/web-common/components/tooltip/TooltipTitle.svelte";
+  import TooltipDescription from "@rilldata/web-common/components/tooltip/TooltipDescription.svelte";
+  import type { MetricsViewSpecMeasureV2 } from "@rilldata/web-common/runtime-client";
+
+  export let measure: MetricsViewSpecMeasureV2;
+  export let value: any;
+
+  $: description =
+    measure?.description || measure?.label || measure?.expression;
+  $: name = measure?.label || measure?.expression;
+</script>
+
+<TooltipContent maxWidth="280px">
+  <TooltipTitle>
+    <svelte:fragment slot="name">
+      {name}
+    </svelte:fragment>
+    <svelte:fragment slot="description">
+      {value}
+    </svelte:fragment>
+  </TooltipTitle>
+  <TooltipDescription>
+    {description}
+  </TooltipDescription>
+
+  <TooltipShortcutContainer>
+    <div>Expand measure</div>
+    <Shortcut>Click</Shortcut>
+  </TooltipShortcutContainer>
+  <TooltipShortcutContainer>
+    <div>
+      <StackingWord key="shift">Copy</StackingWord>
+      number
+    </div>
+    <Shortcut>
+      <span style="font-family: var(--system);">⇧</span> + Click
+    </Shortcut>
+  </TooltipShortcutContainer>
+</TooltipContent>
