0d91de2ce4895befdee954fd8e34271f0daf05a0
Aditya Hegde <adityahegderocks@gmail.com>
2023-11-14 16:54:44 +0530

    Adding telemetry endpoint to admin service

--- web-common/src/proto/gen/rill/admin/v1/api_pb.ts
+++ web-common/src/proto/gen/rill/admin/v1/api_pb.ts
@@ -5735,6 +5735,74 @@ export class GenerateReportYAMLResponse extends Message<GenerateReportYAMLRespon
 }
 
 /**
+ * @generated from message rill.admin.v1.TelemetryRequest
+ */
+export class TelemetryRequest extends Message<TelemetryRequest> {
+  /**
+   * @generated from field: map<string, string> event = 1;
+   */
+  event: { [key: string]: string } = {};
+
+  constructor(data?: PartialMessage<TelemetryRequest>) {
+    super();
+    proto3.util.initPartial(data, this);
+  }
+
+  static readonly runtime: typeof proto3 = proto3;
+  static readonly typeName = "rill.admin.v1.TelemetryRequest";
+  static readonly fields: FieldList = proto3.util.newFieldList(() => [
+    { no: 1, name: "event", kind: "map", K: 9 /* ScalarType.STRING */, V: {kind: "scalar", T: 9 /* ScalarType.STRING */} },
+  ]);
+
+  static fromBinary(bytes: Uint8Array, options?: Partial<BinaryReadOptions>): TelemetryRequest {
+    return new TelemetryRequest().fromBinary(bytes, options);
+  }
+
+  static fromJson(jsonValue: JsonValue, options?: Partial<JsonReadOptions>): TelemetryRequest {
+    return new TelemetryRequest().fromJson(jsonValue, options);
+  }
+
+  static fromJsonString(jsonString: string, options?: Partial<JsonReadOptions>): TelemetryRequest {
+    return new TelemetryRequest().fromJsonString(jsonString, options);
+  }
+
+  static equals(a: TelemetryRequest | PlainMessage<TelemetryRequest> | undefined, b: TelemetryRequest | PlainMessage<TelemetryRequest> | undefined): boolean {
+    return proto3.util.equals(TelemetryRequest, a, b);
+  }
+}
+
+/**
+ * @generated from message rill.admin.v1.TelemetryResponse
+ */
+export class TelemetryResponse extends Message<TelemetryResponse> {
+  constructor(data?: PartialMessage<TelemetryResponse>) {
+    super();
+    proto3.util.initPartial(data, this);
+  }
+
+  static readonly runtime: typeof proto3 = proto3;
+  static readonly typeName = "rill.admin.v1.TelemetryResponse";
+  static readonly fields: FieldList = proto3.util.newFieldList(() => [
+  ]);
+
+  static fromBinary(bytes: Uint8Array, options?: Partial<BinaryReadOptions>): TelemetryResponse {
+    return new TelemetryResponse().fromBinary(bytes, options);
+  }
+
+  static fromJson(jsonValue: JsonValue, options?: Partial<JsonReadOptions>): TelemetryResponse {
+    return new TelemetryResponse().fromJson(jsonValue, options);
+  }
+
+  static fromJsonString(jsonString: string, options?: Partial<JsonReadOptions>): TelemetryResponse {
+    return new TelemetryResponse().fromJsonString(jsonString, options);
+  }
+
+  static equals(a: TelemetryResponse | PlainMessage<TelemetryResponse> | undefined, b: TelemetryResponse | PlainMessage<TelemetryResponse> | undefined): boolean {
+    return proto3.util.equals(TelemetryResponse, a, b);
+  }
+}
+
+/**
  * @generated from message rill.admin.v1.User
  */
 export class User extends Message<User> {
