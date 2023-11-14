7a0fbee3ce772a0735cf2b6ac4c8bdb132811379
brendan colloran <brendan@colloran.net>
2023-11-14 15:53:27 -0800

    updates for formattingSelectors

--- web-common/src/features/dashboards/state-managers/selectors/data-formatting.ts
+++ web-common/src/features/dashboards/state-managers/selectors/data-formatting.ts
@@ -5,7 +5,9 @@ import { FormatPreset } from "@rilldata/web-common/lib/number-formatting/humaniz
 
 export const formattingSelectors = {
   /**
-   * the currently active measure's format preset.
+   * the currently active measure's format preset. If no measure is active,
+   * or the active measure has no format preset, this will return the
+   * default format preset, FormatPreset.HUMANIZE.
    */
   activeMeasureFormatPreset: (args: DashboardDataSources): FormatPreset =>
     (activeMeasure(args)?.formatPreset as FormatPreset) ??
@@ -15,7 +17,19 @@ export const formattingSelectors = {
    * A readable containing a function that formats values
    * according to the active measure's format specification,
    * whether it's a d3 format string or a format preset.
+   *
+   * Note that this formatter is ONLY valid when an active measure
+   * is present. If no measure is active, the formatter
+   * will return an empty string.
    */
-  activeMeasureFormatter: (args: DashboardDataSources) =>
-    createMeasureValueFormatter(activeMeasure(args)),
+  activeMeasureFormatter: (args: DashboardDataSources) => {
+    const measure = activeMeasure(args);
+    if (measure) {
+      createMeasureValueFormatter(measure);
+    }
+
+    // allowed to make make function signatures match in both cases
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    return (value: number) => "";
+  },
 };
