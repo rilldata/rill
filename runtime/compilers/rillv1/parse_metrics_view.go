7ceb71c002afbb6201fda1f5c4381da8f15c0e66
Aditya Hegde <adityahegderocks@gmail.com>
2023-11-29 17:35:59 +0530

    Fix lint

--- runtime/compilers/rillv1/parse_metrics_view.go
+++ runtime/compilers/rillv1/parse_metrics_view.go
@@ -6,29 +6,30 @@ import (
 	"fmt"
 	"strings"
 	"time"
-	// Load IANA time zone data
-	_ "time/tzdata"
 
 	runtimev1 "github.com/rilldata/rill/proto/gen/rill/runtime/v1"
 	"github.com/rilldata/rill/runtime/pkg/duration"
 	"gopkg.in/yaml.v3"
+
+	// Load IANA time zone data
+	_ "time/tzdata"
 )
 
 // MetricsViewYAML is the raw structure of a MetricsView resource defined in YAML
 type MetricsViewYAML struct {
 	commonYAML         `yaml:",inline"` // Not accessed here, only setting it so we can use KnownFields for YAML parsing
-	Title              string   `yaml:"title"`
-	DisplayName        string   `yaml:"display_name"` // Backwards compatibility
-	Description        string   `yaml:"description"`
-	Model              string   `yaml:"model"`
-	Table              string   `yaml:"table"`
-	TimeDimension      string   `yaml:"timeseries"`
-	SmallestTimeGrain  string   `yaml:"smallest_time_grain"`
-	DefaultTimeRange   string   `yaml:"default_time_range"`
-	AvailableTimeZones []string `yaml:"available_time_zones"`
-	FirstDayOfWeek     uint32   `yaml:"first_day_of_week"`
-	FirstMonthOfYear   uint32   `yaml:"first_month_of_year"`
-	DefaultTheme       string   `yaml:"default_theme"`
+	Title              string           `yaml:"title"`
+	DisplayName        string           `yaml:"display_name"` // Backwards compatibility
+	Description        string           `yaml:"description"`
+	Model              string           `yaml:"model"`
+	Table              string           `yaml:"table"`
+	TimeDimension      string           `yaml:"timeseries"`
+	SmallestTimeGrain  string           `yaml:"smallest_time_grain"`
+	DefaultTimeRange   string           `yaml:"default_time_range"`
+	AvailableTimeZones []string         `yaml:"available_time_zones"`
+	FirstDayOfWeek     uint32           `yaml:"first_day_of_week"`
+	FirstMonthOfYear   uint32           `yaml:"first_month_of_year"`
+	DefaultTheme       string           `yaml:"default_theme"`
 	Dimensions         []*struct {
 		Name        string
 		Label       string
