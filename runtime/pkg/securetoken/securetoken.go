d4379972d919e29b58db51a5f55ffa1ec3074a4e
Benjamin Egelund-Müller <b@egelund-muller.com>
2023-11-30 13:08:05 +0100

    Add compression and use gob encoding for download tokens (#3567)
    * Add compression and use gob encoding for download tokens
    
    * Add compression for baked queries
    
    * Backwards compatibility for baked queries
    
    * Add missing close

--- runtime/pkg/securetoken/securetoken.go
+++ runtime/pkg/securetoken/securetoken.go
@@ -2,9 +2,12 @@ package securetoken
 
 import "github.com/gorilla/securecookie"
 
-// Codec provides a means of encoding and decoding values to URL-friendly opaque tokens that clients cannot decode or tamper with.
-// It encodes values using JSON before encrypting them, and uses base64 URL encoding for serializing the encrypted bytes to the token string.
+// Codec provides a means of encoding and decoding values to URL-friendly opaque tokens that clients cannot tamper with.
+// It encodes values using encoding/gob before encrypting them, and uses base64 URL encoding for serializing the encrypted bytes to the token string.
 // It's implemented as a thin wrapper around gorilla/securecookie, which already provides the necessary functionality.
+//
+// TODO: securecookie redundantly applies two iterations of base64 encoding, increasing the output size (see https://github.com/gorilla/securecookie/issues/36).
+// Since there's not that much code, we should consider modifying/implementing the encoding directly in this package.
 type Codec struct {
 	codecs []securecookie.Codec
 }
@@ -14,13 +17,13 @@ func NewCodec(keyPairs [][]byte) *Codec {
 	for _, c := range codecs {
 		sc := c.(*securecookie.SecureCookie)
 		sc.MaxLength(0)
-		sc.SetSerializer(securecookie.JSONEncoder{})
+		sc.SetSerializer(securecookie.GobEncoder{})
 	}
 	return &Codec{codecs: codecs}
 }
 
 func NewRandom() *Codec {
-	keyPairs := [][]byte{securecookie.GenerateRandomKey(32)}
+	keyPairs := [][]byte{securecookie.GenerateRandomKey(32), securecookie.GenerateRandomKey(32)}
 	return NewCodec(keyPairs)
 }
 
