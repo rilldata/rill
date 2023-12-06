08b09afaa65936c289eee00d1d1c76bfafca4394
Eric P Green <ericpgreen2@gmail.com>
2023-12-06 14:43:39 -0800

    Add tooltip

--- web-admin/src/features/dashboards/listing/LastRefreshedDate.svelte
+++ web-admin/src/features/dashboards/listing/LastRefreshedDate.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import { page } from "$app/stores";
+  import Tooltip from "@rilldata/web-common/components/tooltip/Tooltip.svelte";
+  import TooltipContent from "@rilldata/web-common/components/tooltip/TooltipContent.svelte";
   import { runtime } from "@rilldata/web-common/runtime-client/runtime-store";
   import { useDashboardV2 } from "./selectors";
   import { timeAgo } from "./utils";
@@ -12,7 +14,12 @@
 </script>
 
 {#if lastRefreshedDate}
-  <div class="text-[11px] text-gray-600">
-    Last refreshed {timeAgo(lastRefreshedDate)}
-  </div>
+  <Tooltip distance={8}>
+    <div class="text-[11px] text-gray-600">
+      Last refreshed {timeAgo(lastRefreshedDate)}
+    </div>
+    <TooltipContent slot="tooltip-content">
+      {lastRefreshedDate.toLocaleString()}
+    </TooltipContent>
+  </Tooltip>
 {/if}
