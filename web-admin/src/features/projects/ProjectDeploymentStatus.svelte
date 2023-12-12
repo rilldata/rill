9442aff0c085f5168ca983bb973a5005fb01dad8
Eric P Green <ericpgreen2@gmail.com>
2023-12-12 12:43:39 -0800

    Add "data last refreshed" copy to status page

--- web-admin/src/features/projects/ProjectDeploymentStatus.svelte
+++ web-admin/src/features/projects/ProjectDeploymentStatus.svelte
@@ -1,19 +1,15 @@
 <script lang="ts">
-  import { useDashboardsLastUpdated } from "@rilldata/web-admin/features/dashboards/listing/selectors";
   import { runtime } from "@rilldata/web-common/runtime-client/runtime-store";
   import { createAdminServiceGetProject } from "../../client";
   import ProjectDeploymentStatusChip from "./ProjectDeploymentStatusChip.svelte";
+  import { useProjectDataLastRefreshed } from "./selectors";
 
   export let organization: string;
   export let project: string;
 
   $: proj = createAdminServiceGetProject(organization, project);
   $: isProjectDeployed = $proj?.data && $proj.data.prodDeployment;
-  $: lastUpdated = useDashboardsLastUpdated(
-    $runtime.instanceId,
-    organization,
-    project
-  );
+  $: dataLastRefreshed = useProjectDataLastRefreshed($runtime?.instanceId);
 </script>
 
 <div class="flex flex-col gap-y-2">
@@ -24,10 +20,9 @@
     <div>
       <ProjectDeploymentStatusChip {organization} {project} />
     </div>
-    {#if $lastUpdated}
+    {#if $dataLastRefreshed?.data}
       <span class="text-gray-500 text-[11px] leading-4">
-        Synced {$lastUpdated.toLocaleString(undefined, {
-          month: "short",
+        Data refreshed {$dataLastRefreshed.data.toLocaleString(undefined, {month: "short",
           day: "numeric",
           hour: "numeric",
           minute: "numeric",
