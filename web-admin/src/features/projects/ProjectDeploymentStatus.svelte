901011159eeb7774fcdf3b817f3f67d127b21dbe
Eric P Green <ericpgreen2@gmail.com>
2023-12-12 22:50:37 -0800

    Cloud UI: Add "data [last] refreshed" timestamp to project status page (#3687)
    * Increase spacing
    
    * Add "data last refreshed" copy to status page
    
    * Move the Github last synced date into the Github status block
    
    * Tweak spacing
    
    * Remove hibernation message that'll never get hit
    
    * Fix prettier

--- web-admin/src/features/projects/ProjectDeploymentStatus.svelte
+++ web-admin/src/features/projects/ProjectDeploymentStatus.svelte
@@ -1,41 +1,29 @@
 <script lang="ts">
-  import { useDashboardsLastUpdated } from "@rilldata/web-admin/features/dashboards/listing/selectors";
   import { runtime } from "@rilldata/web-common/runtime-client/runtime-store";
-  import { createAdminServiceGetProject } from "../../client";
   import ProjectDeploymentStatusChip from "./ProjectDeploymentStatusChip.svelte";
+  import { useProjectDataLastRefreshed } from "./selectors";
 
   export let organization: string;
   export let project: string;
 
-  $: proj = createAdminServiceGetProject(organization, project);
-  $: isProjectDeployed = $proj?.data && $proj.data.prodDeployment;
-  $: lastUpdated = useDashboardsLastUpdated(
-    $runtime.instanceId,
-    organization,
-    project
-  );
+  $: dataLastRefreshed = useProjectDataLastRefreshed($runtime?.instanceId);
 </script>
 
-<div class="flex flex-col gap-y-2">
+<div class="flex flex-col gap-y-1">
+  <span class="uppercase text-gray-500 font-semibold text-[10px] leading-none"
+    >Project status</span
+  >
   <div>
-    <span class="uppercase text-gray-500 font-semibold text-[10px] leading-none"
-      >Project status</span
-    >
-    <div>
-      <ProjectDeploymentStatusChip {organization} {project} />
-    </div>
-    {#if $lastUpdated}
-      <span class="text-gray-500 text-[11px] leading-4">
-        Synced {$lastUpdated.toLocaleString(undefined, {
-          month: "short",
-          day: "numeric",
-          hour: "numeric",
-          minute: "numeric",
-        })}
-      </span>
-    {/if}
+    <ProjectDeploymentStatusChip {organization} {project} />
   </div>
-  {#if !isProjectDeployed}
-    <div>This project is not deployed.</div>
+  {#if $dataLastRefreshed?.data}
+    <span class="text-gray-500 text-[11px] leading-4">
+      Data refreshed {$dataLastRefreshed.data.toLocaleString(undefined, {
+        month: "short",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+      })}
+    </span>
   {/if}
 </div>
