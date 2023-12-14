d1f910565fbcaaf42ffa54ba8e8faf29f6ac2b87
Anshul Khandelwal <anshulkhandelwal94@gmail.com>
2023-12-14 18:55:28 +0530

    lint fix

--- web-common/src/features/sources/sourceUtils.ts
+++ web-common/src/features/sources/sourceUtils.ts
@@ -39,13 +39,14 @@ export function compileCreateSourceYAML(
       delete values.db;
       delete values.table;
       break;
-    case "duckdb":
+    case "duckdb": {
       const db = values.db as string;
       if (db.startsWith("md:")) {
         connectorName = "motherduck";
         values.db = db.replace("md:", "");
       }
       break;
+    }
   }
 
   const compiledKeyValues = Object.entries(values)
