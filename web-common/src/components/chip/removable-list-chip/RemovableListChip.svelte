94e0355fe4a7e61b1818e13564395de9ef10fcc0
Brian Holmes <briangregoryholmes@gmail.com>
2023-12-14 13:56:37 -0500

    resolve name collision

--- web-common/src/components/chip/removable-list-chip/RemovableListChip.svelte
+++ web-common/src/components/chip/removable-list-chip/RemovableListChip.svelte
@@ -39,7 +39,7 @@ are details left to the consumer of the component; this component should remain 
   export let label: string | undefined = undefined;
   export let dimensionName: string;
 
-  const active = !selectedValues.length;
+  const initiallyActive = !selectedValues.length;
 
   const dispatch = createEventDispatcher();
 
@@ -55,7 +55,7 @@ are details left to the consumer of the component; this component should remain 
   let:active
   distance={8}
   alignment="start"
-  {active}
+  active={initiallyActive}
 >
   <Tooltip
     location="bottom"
