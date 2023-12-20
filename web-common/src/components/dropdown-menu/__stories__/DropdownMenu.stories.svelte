7be5208192c58d9305330923951dc5bcec844efd
Eric P Green <ericpgreen2@gmail.com>
2023-12-20 13:12:46 -0800

    Add dropdown menu Storybook stories

---
+++ web-common/src/components/dropdown-menu/__stories__/DropdownMenu.stories.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  import { Meta, Story } from "@storybook/addon-svelte-csf";
+  import { DropdownMenu, DropdownMenuSub, DropdownMenuTrigger } from "..";
+  import { Button } from "../../button";
+  import CopyIcon from "../../icons/CopyIcon.svelte";
+  import DashboardIcon from "../../icons/DashboardIcon.svelte";
+  import DropdownMenuContent from "../dropdown-menu-content.svelte";
+  import DropdownMenuItem from "../dropdown-menu-item.svelte";
+  import DropdownMenuSubContent from "../dropdown-menu-sub-content.svelte";
+  import DropdownMenuSubTrigger from "../dropdown-menu-sub-trigger.svelte";
+</script>
+
+<Meta title="Components/Dropdown Menu" />
+
+<Story name="Simple">
+  <DropdownMenu>
+    <DropdownMenuTrigger>
+      <Button type="secondary">Open menu</Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align="start">
+      <DropdownMenuItem>Option 1</DropdownMenuItem>
+      <DropdownMenuItem>Option 2</DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+</Story>
+
+<Story name="With icons">
+  <DropdownMenu>
+    <DropdownMenuTrigger>
+      <Button type="secondary">Open menu</Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align="start">
+      <DropdownMenuItem>
+        <CopyIcon className="w-4 h-4 mr-2" />
+        Option 1
+      </DropdownMenuItem>
+      <DropdownMenuItem>
+        <DashboardIcon className="w-4 h-4 mr-2" />
+        Option 2
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+</Story>
+
+<Story name="With submenu">
+  <DropdownMenu>
+    <DropdownMenuTrigger>
+      <Button type="secondary">Open menu</Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align="start">
+      <DropdownMenuSub>
+        <DropdownMenuSubTrigger>Option 1</DropdownMenuSubTrigger>
+        <DropdownMenuSubContent class="whitespace-nowrap">
+          <DropdownMenuItem>Submenu option 1</DropdownMenuItem>
+          <DropdownMenuItem>Submenu option 2</DropdownMenuItem>
+        </DropdownMenuSubContent>
+      </DropdownMenuSub>
+      <DropdownMenuItem>Option 2</DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+</Story>
