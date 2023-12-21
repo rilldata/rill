7ccbefa3d69556b8be2c150e5126aa49ab2e8878
Eric P Green <ericpgreen2@gmail.com>
2023-12-21 15:48:32 -0800

    Use ShadCN's `DropdownMenu` in the Cloud UI (#3737)
    * Init ShadCN
    
    * Add ShadCN's dropdown menu
    
    * Add dropdown menu Storybook stories
    
    * Use new dropdown on Scheduled Report page
    
    * Use new dropdown in breadcrumbs
    
    * Use new dropdown for Share Dashboard button
    
    * Use new dropdown for Avatar Button
    
    * Change file names from kebab to caps case
    
    * Remove extra `button` layer
    
    * Fix overflow
    
    * Open submenu on click, not on hover
    
    * Tighter imports
    
    * Factor out classes from `Button` component
    
    * Remove extra `button` layer
    
    * Use `DropdownMenuGroup`
    
    * Update stories

--- web-common/src/components/icons/Spacer.svelte
+++ web-common/src/components/icons/Spacer.svelte
@@ -1,5 +1,6 @@
 <script>
   export let size = "1em";
+  export let className = "";
 </script>
 
-<div style:width={size} style:height={size} />
+<div style:width={size} style:height={size} class={className} />
