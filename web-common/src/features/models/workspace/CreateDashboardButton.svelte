f17eb3a8c0223eef1f5ad81086841c18334b3daa
Alexander Thor <alec.karlsson@gmail.com>
2023-12-22 10:44:18 +0100

    chore: lint for too long line

--- web-common/src/features/models/workspace/CreateDashboardButton.svelte
+++ web-common/src/features/models/workspace/CreateDashboardButton.svelte
@@ -44,7 +44,9 @@
     <IconSpaceFixer pullLeft pullRight={collapse}>
       <Add />
     </IconSpaceFixer>
-    <ResponsiveButtonText {collapse}>Autogenerate Dashboard</ResponsiveButtonText>
+    <ResponsiveButtonText {collapse}>
+      Autogenerate Dashboard
+    </ResponsiveButtonText>
   </Button>
   <TooltipContent slot="tooltip-content">
     {#if hasError}
