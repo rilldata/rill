38ac47c456ecd6851a91058ba399f38afa314c51
brendan colloran <brendan@colloran.net>
2023-12-29 09:51:42 -0800

    ReportMetadata

--- web-admin/src/features/scheduled-reports/metadata/ReportMetadata.svelte
+++ web-admin/src/features/scheduled-reports/metadata/ReportMetadata.svelte
@@ -6,11 +6,17 @@
   import Tag from "@rilldata/web-common/components/tag/Tag.svelte";
   import { useDashboard } from "@rilldata/web-common/features/dashboards/selectors";
   import EditScheduledReportDialog from "@rilldata/web-common/features/scheduled-reports/EditScheduledReportDialog.svelte";
-  import { getRuntimeServiceListResourcesQueryKey } from "@rilldata/web-common/runtime-client";
+  import {
+    V1Report,
+    getRuntimeServiceListResourcesQueryKey,
+  } from "@rilldata/web-common/runtime-client";
   import { runtime } from "@rilldata/web-common/runtime-client/runtime-store";
   import { useQueryClient } from "@tanstack/svelte-query";
   import cronstrue from "cronstrue";
-  import { createAdminServiceDeleteReport } from "../../../client";
+  import {
+    V1ExportFormat,
+    createAdminServiceDeleteReport,
+  } from "../../../client";
   import ProjectAccessControls from "../../projects/ProjectAccessControls.svelte";
   import {
     useIsReportCreatedByCode,
@@ -36,17 +42,15 @@
   // Get dashboard
   $: dashboardName = useReportDashboardName($runtime.instanceId, report);
   $: dashboard = useDashboard($runtime.instanceId, $dashboardName.data);
-  $: dashboardTitle = $dashboard.data?.metricsView.spec.title;
+  $: dashboardTitle = $dashboard.data?.metricsView?.spec?.title;
 
+  $: cron = $reportQuery.data?.resource?.report?.spec?.refreshSchedule?.cron;
   // Get human-readable frequency
   $: humanReadableFrequency =
-    $reportQuery.data &&
-    cronstrue.toString(
-      $reportQuery.data.resource.report.spec.refreshSchedule.cron,
-      {
-        verbose: true,
-      }
-    );
+    cron &&
+    cronstrue.toString(cron, {
+      verbose: true,
+    });
 
   // Actions
   const queryClient = useQueryClient();
@@ -58,19 +62,48 @@
   }
 
   async function handleDeleteReport() {
+    const name = $reportQuery?.data?.resource?.meta?.name?.name;
+    if (!name) {
+      // if name is not ready, then we can't delete the report
+      return;
+    }
     await $deleteReport.mutateAsync({
       organization,
       project,
-      name: $reportQuery.data.resource.meta.name.name,
+      name,
     });
     queryClient.invalidateQueries(
       getRuntimeServiceListResourcesQueryKey($runtime.instanceId)
     );
     goto(`/${organization}/${project}/-/reports`);
   }
+
+  /**
+   * Casts V1Report data. This assumes that if a V1Report is present,
+   * then all the fields are present. This cast fn can be removed once
+   * API types correctly reflect which fields are optional.
+   * @param data
+   */
+  function reportCast(report: V1Report) {
+    return {
+      admin_owner_user_id: report.spec?.annotations?.[
+        "admin_owner_user_id"
+      ] as string,
+      exportFormat: report?.spec?.exportFormat as V1ExportFormat,
+      exportLimit: report?.spec?.exportLimit as string,
+      title: report?.spec?.title as string,
+      emailRecipients: report?.spec?.emailRecipients as string[],
+      nextRunOn: report?.state?.nextRunOn as string,
+      timeZone: report?.spec?.refreshSchedule?.timeZone as string,
+    };
+  }
+
+  $: v1report = $reportQuery.data?.resource?.report
+    ? reportCast($reportQuery.data.resource.report)
+    : undefined;
 </script>
 
-{#if $reportQuery.data}
+{#if v1report}
   <div class="flex flex-col gap-y-9 w-full max-w-full 2xl:max-w-[1200px]">
     <div class="flex flex-col gap-y-2">
       <!-- Header row 1 -->
@@ -78,33 +111,27 @@
         <!-- Author -->
         <ProjectAccessControls {organization} {project}>
           <svelte:fragment slot="manage-project">
-            {#if $reportQuery.data}
-              <ReportOwnerBlock
-                {organization}
-                {project}
-                ownerId={$reportQuery.data.resource.report.spec.annotations[
-                  "admin_owner_user_id"
-                ]}
-              />
-            {/if}
+            <ReportOwnerBlock
+              {organization}
+              {project}
+              ownerId={v1report.admin_owner_user_id}
+            />
           </svelte:fragment>
         </ProjectAccessControls>
         <!-- Format -->
         <span>
-          {exportFormatToPrettyString(
-            $reportQuery.data.resource.report.spec.exportFormat
-          )} •
+          {exportFormatToPrettyString(v1report.exportFormat)} •
         </span>
         <!-- Limit -->
         <span>
-          {$reportQuery.data?.resource.report.spec.exportLimit === "0"
+          {v1report.exportLimit === "0"
             ? "No row limit"
-            : `${$reportQuery.data?.resource.report.spec.exportLimit} row limit`}
+            : `${v1report.exportLimit} row limit`}
         </span>
       </div>
       <div class="flex gap-x-2 items-center">
         <h1 class="text-gray-700 text-lg font-bold">
-          {$reportQuery.data.resource.report.spec.title}
+          {v1report.title}
         </h1>
         <div class="grow" />
         <RunNowButton {organization} {project} {report} />
@@ -152,10 +179,7 @@
       <div class="flex flex-col gap-y-3">
         <MetadataLabel>Next run</MetadataLabel>
         <MetadataValue>
-          {formatNextRunOn(
-            $reportQuery.data.resource.report.state.nextRunOn,
-            $reportQuery.data.resource.report.spec.refreshSchedule.timeZone
-          )}
+          {formatNextRunOn(v1report.nextRunOn, v1report.timeZone)}
         </MetadataValue>
       </div>
     </div>
@@ -163,11 +187,10 @@
     <!-- Recipients -->
     <div class="flex flex-col gap-y-3">
       <MetadataLabel
-        >Recipients ({$reportQuery.data.resource.report.spec.emailRecipients
-          .length})</MetadataLabel
+        >Recipients ({v1report.emailRecipients.length})</MetadataLabel
       >
       <div class="flex flex-wrap gap-2">
-        {#each $reportQuery.data.resource.report.spec.emailRecipients as recipient}
+        {#each v1report.emailRecipients as recipient}
           <Tag>
             {recipient}
           </Tag>
@@ -177,10 +200,10 @@
   </div>
 {/if}
 
-{#if $reportQuery.data}
+{#if $reportQuery?.data?.resource?.report?.spec}
   <EditScheduledReportDialog
     open={showEditReportDialog}
-    reportSpec={$reportQuery.data.resource.report.spec}
+    reportSpec={$reportQuery?.data?.resource?.report?.spec}
     on:close={() => (showEditReportDialog = false)}
   />
 {/if}
