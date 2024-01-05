da45b858654783326f89134713cfcfa60d6fab7c
Eric P Green <ericpgreen2@gmail.com>
2024-01-04 22:18:58 -0500

    Create dedicated section for parse errors (not resource errors)

--- web-admin/src/features/projects/ProjectParseErrors.svelte
+++ web-admin/src/features/projects/ProjectParseErrors.svelte
@@ -1,46 +1,37 @@
 <script lang="ts">
-  import { getProjectErrors } from "@rilldata/web-admin/features/projects/getProjectErrors";
-  import CheckCircle from "@rilldata/web-common/components/icons/CheckCircle.svelte";
-  import type { V1ParseError } from "@rilldata/web-common/runtime-client";
-  import { useQueryClient } from "@tanstack/svelte-query";
-  import type { Readable } from "svelte/store";
-  import { createAdminServiceGetProject } from "../../client";
+  import {
+    ResourceKind,
+    SingletonProjectParserName,
+  } from "@rilldata/web-common/features/entity-management/resource-selectors";
+  import { createRuntimeServiceGetResource } from "@rilldata/web-common/runtime-client";
+  import { runtime } from "@rilldata/web-common/runtime-client/runtime-store";
 
-  export let organization: string;
-  export let project: string;
-
-  const queryClient = useQueryClient();
-
-  $: proj = createAdminServiceGetProject(organization, project);
-  $: hasReadAccess = $proj.data?.projectPermissions?.readProdStatus;
-
-  let errors: Readable<Array<V1ParseError>>;
-  $: if ($proj.data?.prodDeployment?.runtimeInstanceId && hasReadAccess)
-    errors = getProjectErrors(
-      queryClient,
-      $proj.data?.prodDeployment?.runtimeInstanceId
-    );
+  $: parserErrors = createRuntimeServiceGetResource(
+    $runtime.instanceId,
+    {
+      "name.kind": ResourceKind.ProjectParser,
+      "name.name": SingletonProjectParserName,
+    },
+    {
+      query: {
+        select: (data) => data.resource.projectParser.state.parseErrors,
+      },
+    }
+  );
 </script>
 
-<div>
+<section class="flex flex-col gap-y-4">
   <!-- Title -->
-  <div
-    class="px-12 py-2 font-semibold text-gray-500 uppercase text-[10px] leading-none border-b border-gray-200"
-  >
-    Errors
-  </div>
+  <h2 class="text-lg font-medium">Parse errors</h2>
   <!-- Parse errors -->
-  {#if $proj.isSuccess}
-    {#if !$errors || $errors.length === 0}
-      <div class="flex flex-col items-center gap-y-4 mt-40">
-        <CheckCircle size="48px" className="text-slate-300" />
-        <div class="font-semibold text-gray-600">No errors</div>
-      </div>
+  {#if $parserErrors.isSuccess}
+    {#if !$parserErrors.data || $parserErrors.data.length === 0}
+      <div class="text-gray-600">None!</div>
     {:else}
-      <ul>
-        {#each $errors as error}
+      <ul class="border rounded-sm">
+        {#each $parserErrors.data as error}
           <li
-            class="flex gap-x-5 justify-between py-1 px-9 border-b border-gray-200 bg-red-50 font-mono"
+            class="flex gap-x-5 justify-between py-1 px-9 border-b border-gray-200 bg-red-50 font-mono last:border-b-0"
           >
             <span class="text-red-600 break-all">
               {error.message}
@@ -55,4 +46,4 @@
       </ul>
     {/if}
   {/if}
-</div>
+</section>
