41618d73c18b710b9ba97d8e0a4edf3be9a2fd31
Eric P Green <ericpgreen2@gmail.com>
2024-01-05 16:55:01 -0500

    Move components into `features/projects/status` directory

---
+++ web-admin/src/features/projects/status/ProjectParseErrors.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import {
+    ResourceKind,
+    SingletonProjectParserName,
+  } from "@rilldata/web-common/features/entity-management/resource-selectors";
+  import { createRuntimeServiceGetResource } from "@rilldata/web-common/runtime-client";
+  import { runtime } from "@rilldata/web-common/runtime-client/runtime-store";
+
+  $: parserErrors = createRuntimeServiceGetResource(
+    $runtime.instanceId,
+    {
+      "name.kind": ResourceKind.ProjectParser,
+      "name.name": SingletonProjectParserName,
+    },
+    {
+      query: {
+        select: (data) => data.resource.projectParser.state.parseErrors,
+      },
+    },
+  );
+</script>
+
+<section class="flex flex-col gap-y-4">
+  <!-- Title -->
+  <h2 class="text-lg font-medium">Parse errors</h2>
+  <!-- Parse errors -->
+  {#if $parserErrors.isSuccess}
+    {#if !$parserErrors.data || $parserErrors.data.length === 0}
+      <div class="text-gray-600">None!</div>
+    {:else}
+      <ul class="border rounded-sm">
+        {#each $parserErrors.data as error}
+          <li
+            class="flex gap-x-5 justify-between py-1 px-9 border-b border-gray-200 bg-red-50 font-mono last:border-b-0"
+          >
+            <span class="text-red-600 break-all">
+              {error.message}
+            </span>
+            {#if error.filePath}
+              <span class="text-stone-500 font-semibold shrink-0">
+                {error.filePath}
+              </span>
+            {/if}
+          </li>
+        {/each}
+      </ul>
+    {/if}
+  {/if}
+</section>
--- web-admin/src/routes/[organization]/[project]/-/status/+page.svelte
+++ web-admin/src/routes/[organization]/[project]/-/status/+page.svelte
@@ -2,9 +2,9 @@
   import { page } from "$app/stores";
   import ContentContainer from "@rilldata/web-admin/components/layout/ContentContainer.svelte";
   import ProjectDeploymentStatus from "@rilldata/web-admin/features/projects/ProjectDeploymentStatus.svelte";
-  import ProjectGithubConnection from "@rilldata/web-admin/features/projects/ProjectGithubConnection.svelte";
-  import ProjectParseErrors from "@rilldata/web-admin/features/projects/ProjectParseErrors.svelte";
-  import ProjectResources from "@rilldata/web-admin/features/projects/resources/ProjectResources.svelte";
+  import ProjectGithubConnection from "@rilldata/web-admin/features/projects/status/ProjectGithubConnection.svelte";
+  import ProjectParseErrors from "@rilldata/web-admin/features/projects/status/ProjectParseErrors.svelte";
+  import ProjectResources from "@rilldata/web-admin/features/projects/status/ProjectResources.svelte";
   import VerticalScrollContainer from "@rilldata/web-common/layout/VerticalScrollContainer.svelte";
   import { createRuntimeServiceListResources } from "@rilldata/web-common/runtime-client";
   import { runtime } from "@rilldata/web-common/runtime-client/runtime-store";
@@ -23,11 +23,11 @@
           // filter out the "ProjectParser" resource
           return data.resources.filter(
             (resource) =>
-              resource.meta.name.kind !== "rill.runtime.v1.ProjectParser"
+              resource.meta.name.kind !== "rill.runtime.v1.ProjectParser",
           );
         },
       },
-    }
+    },
   );
 </script>
 
