e455202d9db14eebd9378a9f4641a28322e84d68
Eric P Green <ericpgreen2@gmail.com>
2024-01-04 22:17:17 -0500

    Add ShadCN's Table component set

---
+++ web-admin/src/components/table-shadcn/table-caption.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { cn } from "@rilldata/web-common/lib/shadcn";
+  import type { HTMLAttributes } from "svelte/elements";
+
+  type $$Props = HTMLAttributes<HTMLTableCaptionElement>;
+
+  let className: $$Props["class"] = undefined;
+  export { className as class };
+</script>
+
+<caption
+  class={cn("mt-4 text-sm text-muted-foreground", className)}
+  {...$$restProps}
+>
+  <slot />
+</caption>
