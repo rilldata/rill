005937acd58a1476edc700516151ee2387bff7b2
Brian Holmes <briangregoryholmes@gmail.com>
2024-01-11 00:03:47 -0500

    remove unused import

--- web-common/src/features/dashboards/workspace/DashboardCTAs.svelte
+++ web-common/src/features/dashboards/workspace/DashboardCTAs.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
   import MetricsIcon from "@rilldata/web-common/components/icons/Metrics.svelte";
-  import PanelCTA from "@rilldata/web-common/components/panel/PanelCTA.svelte";
   import { useDashboard } from "@rilldata/web-common/features/dashboards/selectors";
   import { V1ReconcileStatus } from "@rilldata/web-common/runtime-client";
   import { Button } from "../../../components/button";
