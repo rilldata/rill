73a3f43807740adf9f2cea37a893ce19e1ef200f
Eric P Green <ericpgreen2@gmail.com>
2024-01-12 14:46:36 -0800

    Remove the `VerticalScrollContainer`

--- web-admin/src/routes/[organization]/[project]/-/reports/[report]/+page.svelte
+++ web-admin/src/routes/[organization]/[project]/-/reports/[report]/+page.svelte
@@ -3,20 +3,17 @@
   import ContentContainer from "@rilldata/web-admin/components/layout/ContentContainer.svelte";
   import ReportHistoryTable from "@rilldata/web-admin/features/scheduled-reports/history/ReportHistoryTable.svelte";
   import ReportMetadata from "@rilldata/web-admin/features/scheduled-reports/metadata/ReportMetadata.svelte";
-  import VerticalScrollContainer from "@rilldata/web-common/layout/VerticalScrollContainer.svelte";
 
   $: organization = $page.params.organization;
   $: project = $page.params.project;
   $: report = $page.params.report;
 </script>
 
-<VerticalScrollContainer>
-  <ContentContainer>
-    <div class="flex justify-center">
-      <div class="w-[960px] flex flex-col items-start gap-y-9">
-        <ReportMetadata {organization} {project} {report} />
-        <ReportHistoryTable {report} />
-      </div>
+<ContentContainer>
+  <div class="flex justify-center">
+    <div class="w-[960px] flex flex-col items-start gap-y-9">
+      <ReportMetadata {organization} {project} {report} />
+      <ReportHistoryTable {report} />
     </div>
-  </ContentContainer>
-</VerticalScrollContainer>
+  </div>
+</ContentContainer>
--- web-admin/src/routes/[organization]/[project]/-/status/+page.svelte
+++ web-admin/src/routes/[organization]/[project]/-/status/+page.svelte
@@ -5,21 +5,18 @@
   import ProjectGithubConnection from "@rilldata/web-admin/features/projects/status/ProjectGithubConnection.svelte";
   import ProjectParseErrors from "@rilldata/web-admin/features/projects/status/ProjectParseErrors.svelte";
   import ProjectResources from "@rilldata/web-admin/features/projects/status/ProjectResources.svelte";
-  import VerticalScrollContainer from "@rilldata/web-common/layout/VerticalScrollContainer.svelte";
 
   $: organization = $page.params.organization;
   $: project = $page.params.project;
 </script>
 
-<VerticalScrollContainer>
-  <ContentContainer>
-    <div class="flex flex-col gap-y-12">
-      <div class="flex gap-x-20 items-start">
-        <ProjectGithubConnection {organization} {project} />
-        <ProjectDeploymentStatus {organization} {project} />
-      </div>
-      <ProjectResources />
-      <ProjectParseErrors />
+<ContentContainer>
+  <div class="flex flex-col gap-y-12">
+    <div class="flex gap-x-20 items-start">
+      <ProjectGithubConnection {organization} {project} />
+      <ProjectDeploymentStatus {organization} {project} />
     </div>
-  </ContentContainer>
-</VerticalScrollContainer>
+    <ProjectResources />
+    <ProjectParseErrors />
+  </div>
+</ContentContainer>
