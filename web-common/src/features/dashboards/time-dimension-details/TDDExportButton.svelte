8e9a4e5b2fefb6a46fe68792af9bf1099a1d6bac
Eric P Green <ericpgreen2@gmail.com>
2024-01-12 13:41:20 -0800

    Add TDD export button

---
+++ web-common/src/features/dashboards/time-dimension-details/TDDExportButton.svelte
@@ -0,0 +1,130 @@
+<script lang="ts">
+  import { WithTogglableFloatingElement } from "@rilldata/web-common/components/floating-element";
+  import { Menu, MenuItem } from "@rilldata/web-common/components/menu";
+  import { getStateManagers } from "@rilldata/web-common/features/dashboards/state-managers/state-managers";
+  import { useTimeControlStore } from "@rilldata/web-common/features/dashboards/time-controls/time-control-store";
+  import {
+    V1ExportFormat,
+    createQueryServiceExport,
+  } from "@rilldata/web-common/runtime-client";
+  import { onMount } from "svelte";
+  import { get } from "svelte/store";
+  import CaretDownIcon from "../../../components/icons/CaretDownIcon.svelte";
+  import { useDashboard } from "../selectors";
+  import exportTDD from "./export-tdd";
+
+  export let includeScheduledReport: boolean;
+  export let metricViewName: string;
+
+  let exportMenuOpen = false;
+  let showScheduledReportDialog = false;
+
+  const ctx = getStateManagers();
+  const timeControlStore = useTimeControlStore(ctx);
+  const metricsView = useDashboard(get(ctx.runtime).instanceId, metricViewName);
+
+  const exportDash = createQueryServiceExport();
+  const handleExportTDD = async (format: V1ExportFormat) => {
+    await exportTDD({
+      metricViewName,
+      query: exportDash,
+      format,
+      timeControlStore,
+      timeDimension: $metricsView.data?.metricsView?.spec
+        ?.timeDimension as string,
+    });
+  };
+
+  // Only import the Scheduled Report dialog if in the Cloud context.
+  // This ensures Rill Developer doesn't try and fail to import the admin-client.
+  let CreateScheduledReportDialog;
+  onMount(async () => {
+    if (includeScheduledReport) {
+      CreateScheduledReportDialog = (
+        await import(
+          "../../scheduled-reports/CreateScheduledReportDialog.svelte"
+        )
+      ).default;
+    }
+  });
+
+  // future work
+  // $: scheduledReportsQueryArgs = getDimensionTableExportArgs(ctx);
+</script>
+
+<WithTogglableFloatingElement
+  alignment="end"
+  distance={8}
+  let:toggleFloatingElement
+  location="bottom"
+  on:close={() => (exportMenuOpen = false)}
+  on:open={() => (exportMenuOpen = true)}
+>
+  <button
+    class="h-6 px-1.5 py-px flex items-center gap-[3px] rounded-sm hover:bg-gray-200 text-gray-700"
+    on:click={(evt) => {
+      evt.stopPropagation();
+      toggleFloatingElement();
+    }}
+  >
+    Export
+    <CaretDownIcon
+      className="transition-transform {exportMenuOpen && '-rotate-180'}"
+      size="10px"
+    />
+  </button>
+  <Menu
+    minWidth=""
+    on:click-outside={toggleFloatingElement}
+    on:escape={toggleFloatingElement}
+    slot="floating-element"
+    let:toggleFloatingElement
+  >
+    <MenuItem
+      on:select={async () => {
+        toggleFloatingElement();
+        await handleExportTDD("EXPORT_FORMAT_CSV");
+      }}
+    >
+      Export as CSV
+    </MenuItem>
+    <MenuItem
+      on:select={async () => {
+        toggleFloatingElement();
+        await handleExportTDD("EXPORT_FORMAT_PARQUET");
+      }}
+    >
+      Export as Parquet
+    </MenuItem>
+    <MenuItem
+      on:select={async () => {
+        toggleFloatingElement();
+        await handleExportTDD("EXPORT_FORMAT_XLSX");
+      }}
+    >
+      Export as XLSX
+    </MenuItem>
+    <!-- {#if includeScheduledReport}
+      <MenuItem
+        on:select={() => {
+          toggleFloatingElement();
+          showScheduledReportDialog = true;
+        }}
+      >
+        Create scheduled report...
+      </MenuItem>
+    {/if} -->
+  </Menu>
+</WithTogglableFloatingElement>
+
+<!-- Including `showScheduledReportDialog` in the conditional ensures we tear 
+  down the form state when the dialog closes -->
+{#if includeScheduledReport && CreateScheduledReportDialog && showScheduledReportDialog}
+  <!-- <svelte:component
+    this={CreateScheduledReportDialog}
+    queryName="MetricsViewComparison"
+    queryArgs={$scheduledReportsQueryArgs}
+    open={showScheduledReportDialog}
+    on:close={() => (showScheduledReportDialog = false)}
+  /> -->
+{/if}
---
+++ web-common/src/features/dashboards/time-dimension-details/export-tdd.ts
@@ -0,0 +1,70 @@
+import type { TimeControlStore } from "@rilldata/web-common/features/dashboards/time-controls/time-control-store";
+import type {
+  V1ExportFormat,
+  createQueryServiceExport,
+} from "@rilldata/web-common/runtime-client";
+import { get } from "svelte/store";
+import { useDashboardStore } from "web-common/src/features/dashboards/stores/dashboard-stores";
+import { runtime } from "../../../runtime-client/runtime-store";
+
+export default async function exportTDD({
+  query,
+  metricViewName,
+  format,
+  timeControlStore,
+  timeDimension,
+}: {
+  query: ReturnType<typeof createQueryServiceExport>;
+  metricViewName: string;
+  format: V1ExportFormat;
+  // we need this from argument since getContext is called to get the state managers
+  // which cannot run outside of component initialisation
+  timeControlStore: TimeControlStore;
+  timeDimension: string;
+}) {
+  const dashboardStore = useDashboardStore(metricViewName);
+  const timeControlState = get(timeControlStore);
+
+  const dashboard = get(dashboardStore);
+
+  const result = await get(query).mutateAsync({
+    instanceId: get(runtime).instanceId,
+    data: {
+      format,
+      query: {
+        metricsViewAggregationRequest: {
+          dimensions: [
+            { name: dashboard.selectedComparisonDimension },
+            {
+              name: timeDimension,
+              timeGrain: dashboard.selectedTimeRange?.interval,
+              // TODO: providing the timeZone triggers a 500 error
+              // timeZone: dashboard.selectedTimezone,
+            },
+          ],
+          filter: dashboard.filters,
+          having: undefined,
+          instanceId: get(runtime).instanceId,
+          limit: undefined, // the backend handles export limits
+          measures: [{ name: dashboard.expandedMeasureName }],
+          metricsView: metricViewName,
+          offset: "0",
+          pivotOn: [timeDimension], // spreads the time dimension across columns
+          priority: undefined,
+          sort: undefined, // future work
+          timeEnd: undefined,
+          timeRange: {
+            start: timeControlState.timeStart,
+            end: timeControlState.timeEnd,
+          },
+          timeStart: undefined,
+          where: undefined,
+        },
+      },
+    },
+  });
+
+  const downloadUrl = `${get(runtime).host}${result.downloadUrlPath}`;
+
+  window.open(downloadUrl, "_self");
+}
