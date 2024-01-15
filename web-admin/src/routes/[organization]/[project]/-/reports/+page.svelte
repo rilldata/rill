f16ee514b058ba3f27cbb0d600636fa0650e0e04
Eric P Green <ericpgreen2@gmail.com>
2024-01-15 13:35:13 -0800

    Cloud UI: Fix scroll issues (#3843)
    * Only apply `h-screen` to `<main>`when on the dashboard page
    
    * Remove the `VerticalScrollContainer`

--- web-admin/src/routes/[organization]/[project]/-/reports/+page.svelte
+++ web-admin/src/routes/[organization]/[project]/-/reports/+page.svelte
@@ -2,16 +2,13 @@
   import { page } from "$app/stores";
   import ContentContainer from "@rilldata/web-admin/components/layout/ContentContainer.svelte";
   import ReportsTable from "@rilldata/web-admin/features/scheduled-reports/listing/ReportsTable.svelte";
-  import VerticalScrollContainer from "@rilldata/web-common/layout/VerticalScrollContainer.svelte";
 
   $: organization = $page.params.organization;
   $: project = $page.params.project;
 </script>
 
-<VerticalScrollContainer>
-  <ContentContainer>
-    <div class="flex flex-col items-center">
-      <ReportsTable {organization} {project} />
-    </div>
-  </ContentContainer>
-</VerticalScrollContainer>
+<ContentContainer>
+  <div class="flex flex-col items-center">
+    <ReportsTable {organization} {project} />
+  </div>
+</ContentContainer>
