a06dfc96b44870f3dc3719c0d1fbdf9988362083
brendan colloran <brendan@colloran.net>
2024-01-17 12:26:10 -0800

    remove unused chip color defs

--- web-common/src/components/chip/chip-types.ts
+++ web-common/src/components/chip/chip-types.ts
@@ -28,32 +28,3 @@ export const excludeChipColors = {
   outlineClass: "outline outline-1 outline-gray-200",
   outlineActiveClass: "!outline-gray-400",
 };
-
-export const subrangeChipColors = {
-  bgBaseClass: "bg-slate-50 dark:bg-slate-700",
-  bgHoverClass: "hover:bg-slate-100 hover:dark:bg-slate-600",
-  textClass: "text-slate-600",
-  bgActiveClass: "bg-slate-100 dark:bg-slate-600",
-  outlineClass: "",
-  outlineActiveClass: "",
-};
-
-export const measureChipColors = {
-  bgBaseClass: "bg-indigo-50 dark:bg-indigo-600",
-  bgHoverClass: "hover:bg-indigo-100 hover:dark:bg-indigo-800",
-  textClass: "text-indigo-800",
-  bgActiveClass: "bg-indigo-100 dark:bg-indigo-600",
-  outlineClass:
-    "outline outline-1 outline-indigo-100 dark:outline-indigo-500 hover:outline-indigo-200",
-  outlineActiveClass: "!outline-indigo-500 dark:outline-indigo-500",
-};
-
-export const specialChipColors = {
-  bgBaseClass: "bg-purple-50 dark:bg-purple-600",
-  bgHoverClass: "hover:bg-purple-100 hover:dark:bg-purple-800",
-  textClass: "text-purple-800",
-  bgActiveClass: "bg-purple-100 dark:bg-purple-600",
-  outlineClass:
-    "outline outline-1 outline-purple-100 dark:outline-purple-500 hover:outline-purple-200",
-  outlineActiveClass: "!outline-purple-500 dark:outline-purple-500",
-};
