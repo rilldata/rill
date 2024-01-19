0c540cd03114f1fee307ce9432749126bc9ef94e
Eric P Green <ericpgreen2@gmail.com>
2024-01-19 15:51:46 -0800

    Add sections to the Data tab

--- web-common/src/features/alerts/AlertDialogDataTab.svelte
+++ web-common/src/features/alerts/AlertDialogDataTab.svelte
@@ -1,5 +1,56 @@
 <script lang="ts">
-  // props
+  import FormSection from "../../components/forms/FormSection.svelte";
+  import InputV2 from "../../components/forms/InputV2.svelte";
+  import Select from "../../components/forms/Select.svelte";
+  import DataPreview from "./DataPreview.svelte";
+
+  let title: string;
+  let titleError: string;
+  let splitByDimension: string;
+  let forEvery: string;
 </script>
 
-<div class="h-64 bg-blue-50"></div>
+<div class="flex flex-col gap-y-5">
+  <FormSection title="Alert name">
+    <InputV2
+      bind:value={title}
+      error={titleError}
+      id="title"
+      placeholder="My alert"
+    />
+  </FormSection>
+  <FormSection
+    title="Filters"
+    description="These are inherited from the underlying dashboard view."
+  ></FormSection>
+  <FormSection
+    title="Alert data"
+    description="Select the measures you want to monitor."
+  >
+    <InputV2
+      bind:value={title}
+      error={titleError}
+      id="measures"
+      placeholder="Add measures"
+    />
+    <Select
+      bind:value={splitByDimension}
+      id="dimensionSplit"
+      label="Split by dimension"
+      options={["Dim1", "Dim2", "Dim3"].map((dimension) => ({
+        value: dimension,
+      }))}
+    />
+    <Select
+      bind:value={forEvery}
+      id="forEvery"
+      label="For every"
+      options={["Interval1", "Interval2", "Interval3"].map((timeInterval) => ({
+        value: timeInterval,
+      }))}
+    />
+  </FormSection>
+  <FormSection title="Data preview">
+    <DataPreview />
+  </FormSection>
+</div>
---
+++ web-common/src/features/alerts/DataPreview.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import TableIcon from "../../components/icons/TableIcon.svelte";
+
+  // props
+
+  let data = undefined;
+</script>
+
+{#if !data}
+  <div class="pt-5 pb-10 flex flex-col justify-center items-center gap-1">
+    <TableIcon size="32px" className="text-slate-300" />
+    <div class="flex flex-col justify-center items-center text-sm">
+      <div class="text-gray-600 font-semibold">No data to preview</div>
+      <div class="text-gray-500 font-normal">
+        To see preview, select measures above.
+      </div>
+    </div>
+  </div>
+{/if}
