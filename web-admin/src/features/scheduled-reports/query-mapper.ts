1605308c970d66b947dffab036583084d77cb240
Aditya Hegde <adityahegderocks@gmail.com>
2024-01-23 21:53:24 +0530

    Query mapper

---
+++ web-admin/src/features/scheduled-reports/query-mapper.ts
@@ -0,0 +1,205 @@
+import { SortDirection } from "@rilldata/web-common/features/dashboards/proto-state/derived-types";
+import { getProtoFromDashboardState } from "@rilldata/web-common/features/dashboards/proto-state/toProto";
+import { useMetaQuery } from "@rilldata/web-common/features/dashboards/selectors";
+import { getDefaultMetricsExplorerEntity } from "@rilldata/web-common/features/dashboards/stores/dashboard-store-defaults";
+import type { MetricsExplorerEntity } from "@rilldata/web-common/features/dashboards/stores/metrics-explorer-entity";
+import { isoDurationToFullTimeRange } from "@rilldata/web-common/lib/time/ranges/iso-ranges";
+import { TimeRangePreset } from "@rilldata/web-common/lib/time/types";
+import {
+  createQueryServiceMetricsViewTimeRange,
+  type V1MetricsViewAggregationRequest,
+  type V1MetricsViewComparisonRequest,
+  type V1MetricsViewRowsRequest,
+  type V1MetricsViewSpec,
+  type V1MetricsViewTimeSeriesRequest,
+  type V1MetricsViewToplistRequest,
+  type V1Resource,
+  type V1TimeRangeSummary,
+} from "@rilldata/web-common/runtime-client";
+import { runtime } from "@rilldata/web-common/runtime-client/runtime-store";
+import { derived, get, type Readable, readable } from "svelte/store";
+
+type ReportQueryRequest =
+  | V1MetricsViewAggregationRequest
+  | V1MetricsViewToplistRequest
+  | V1MetricsViewRowsRequest
+  | V1MetricsViewTimeSeriesRequest
+  | V1MetricsViewComparisonRequest;
+
+type QueryMapperArgs<R extends ReportQueryRequest> = {
+  dashboard: MetricsExplorerEntity;
+  req: R;
+  metricsView: V1MetricsViewSpec;
+  timeRangeSummary: V1TimeRangeSummary;
+  executionTime: string;
+};
+
+export function parseReport(
+  reportResource: V1Resource,
+  executionTime: string,
+): Readable<{
+  ready: boolean;
+  state?: string;
+  metricsView?: string;
+}> {
+  if (!reportResource?.report?.spec?.queryName)
+    return readable({
+      ready: false,
+    });
+
+  let metricsViewName: string;
+  const req: ReportQueryRequest = JSON.parse(
+    reportResource.report.spec.queryArgsJson,
+  );
+  let queryMapper: (args: QueryMapperArgs<ReportQueryRequest>) => void;
+
+  switch (reportResource.report.spec.queryName) {
+    case "MetricsViewAggregation":
+      metricsViewName = (req as V1MetricsViewAggregationRequest).metricsView;
+      queryMapper = mapMetricsViewAggregationRequest;
+      break;
+    case "MetricsViewToplist":
+      metricsViewName = (req as V1MetricsViewToplistRequest).metricsViewName;
+      queryMapper = mapMetricsViewToplistRequest;
+      break;
+    case "MetricsViewRows":
+      metricsViewName = (req as V1MetricsViewRowsRequest).metricsViewName;
+      queryMapper = mapMetricsViewRowsRequest;
+      break;
+    case "MetricsViewTimeSeries":
+      metricsViewName = (req as V1MetricsViewTimeSeriesRequest).metricsViewName;
+      queryMapper = mapMetricsViewTimeSeriesRequest;
+      break;
+    case "MetricsViewComparison":
+      metricsViewName = (req as V1MetricsViewComparisonRequest).metricsViewName;
+      queryMapper = mapMetricsViewComparisonRequest;
+      break;
+  }
+
+  return derived(
+    [
+      useMetaQuery(get(runtime).instanceId, metricsViewName),
+      // TODO: handle non-timestamp dashboards
+      createQueryServiceMetricsViewTimeRange(
+        get(runtime).instanceId,
+        metricsViewName,
+        {},
+      ),
+    ],
+    ([metricsViewResource, timeRangeSummary]) => {
+      if (!metricsViewResource.data || !timeRangeSummary.data)
+        return {
+          ready: false,
+        };
+
+      const dashboard = getDefaultMetricsExplorerEntity(
+        metricsViewName,
+        metricsViewResource.data,
+        timeRangeSummary.data,
+      );
+      queryMapper({
+        dashboard,
+        req,
+        metricsView: metricsViewResource.data,
+        timeRangeSummary: timeRangeSummary.data.timeRangeSummary,
+        executionTime,
+      });
+      return {
+        ready: true,
+        state: getProtoFromDashboardState(dashboard),
+        metricsView: metricsViewName,
+      };
+    },
+  );
+}
+
+function mapMetricsViewAggregationRequest({
+  req,
+  dashboard,
+}: QueryMapperArgs<V1MetricsViewAggregationRequest>) {
+  if (req.where) dashboard.whereFilter = req.where;
+}
+
+function mapMetricsViewToplistRequest({
+  req,
+  dashboard,
+}: QueryMapperArgs<V1MetricsViewToplistRequest>) {
+  if (req.where) dashboard.whereFilter = req.where;
+}
+
+function mapMetricsViewRowsRequest({
+  req,
+  dashboard,
+}: QueryMapperArgs<V1MetricsViewRowsRequest>) {
+  if (req.where) dashboard.whereFilter = req.where;
+}
+
+function mapMetricsViewTimeSeriesRequest({
+  req,
+  dashboard,
+}: QueryMapperArgs<V1MetricsViewTimeSeriesRequest>) {
+  if (req.where) dashboard.whereFilter = req.where;
+}
+
+function mapMetricsViewComparisonRequest({
+  req,
+  dashboard,
+  metricsView,
+  timeRangeSummary,
+  executionTime,
+}: QueryMapperArgs<V1MetricsViewComparisonRequest>) {
+  if (req.where) dashboard.whereFilter = req.where;
+
+  if (req.timeRange) {
+    if (req.timeRange.start && req.timeRange.end) {
+      dashboard.selectedTimeRange = {
+        name: TimeRangePreset.CUSTOM,
+        start: new Date(req.timeRange.start),
+        end: new Date(req.timeRange.end),
+      };
+    } else {
+      dashboard.selectedTimeRange = isoDurationToFullTimeRange(
+        req.timeRange.isoDuration,
+        new Date(timeRangeSummary.min),
+        new Date(executionTime),
+      );
+    }
+
+    // TODO: get grain from report frequency
+    dashboard.selectedTimeRange.interval = req.timeRange.roundToGrain;
+  }
+
+  if (req.comparisonTimeRange) {
+    if (req.comparisonTimeRange.start && req.comparisonTimeRange.end) {
+      dashboard.selectedComparisonTimeRange = {
+        name: TimeRangePreset.CUSTOM,
+        start: new Date(req.comparisonTimeRange.start),
+        end: new Date(req.comparisonTimeRange.end),
+      };
+    } else {
+      dashboard.selectedComparisonTimeRange = isoDurationToFullTimeRange(
+        req.comparisonTimeRange.isoDuration,
+        new Date(timeRangeSummary.min),
+        new Date(executionTime),
+      );
+    }
+
+    dashboard.selectedComparisonTimeRange.interval =
+      dashboard.selectedTimeRange?.interval;
+  }
+
+  dashboard.selectedComparisonDimension = req.dimension.name;
+  dashboard.visibleMeasureKeys = new Set(req.measures.map((m) => m.name));
+
+  // if the selected sort is a measure set it to leaderboardMeasureName
+  if (
+    req.sort.length > 0 &&
+    metricsView.measures.findIndex((m) => m.name === req.sort[0].name) >= 0
+  ) {
+    dashboard.leaderboardMeasureName = req.sort[0].name;
+    dashboard.sortDirection = req.sort[0].desc
+      ? SortDirection.DESCENDING
+      : SortDirection.ASCENDING;
+    // TODO: sort type
+  }
+}
---
+++ web-admin/src/routes/[organization]/[project]/-/reports/[report]/open/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import { page } from "$app/stores";
+  import { parseReport } from "@rilldata/web-admin/features/scheduled-reports/query-mapper";
+  import { useReport } from "@rilldata/web-admin/features/scheduled-reports/selectors";
+  import { runtime } from "@rilldata/web-common/runtime-client/runtime-store";
+
+  $: organization = $page.params.organization;
+  $: project = $page.params.project;
+  $: reportId = $page.params.report;
+  $: executionTime = $page.url.searchParams.get("execution_time");
+
+  $: report = useReport($runtime.instanceId, reportId);
+
+  $: parsedReport = parseReport($report.data?.resource, executionTime);
+
+  $: if ($parsedReport.ready) {
+    goto(
+      `/${organization}/${project}/${$parsedReport.metricsView}?state=${$parsedReport.state}`,
+    );
+  }
+</script>
