e1d67e07750cbc8df1ec1473f57582163a64de70
Aditya Hegde <adityahegderocks@gmail.com>
2024-01-23 16:33:35 +0530

    Fix some edge cases

--- web-common/src/features/dashboards/stores/dashboard-store-defaults.ts
+++ web-common/src/features/dashboards/stores/dashboard-store-defaults.ts
@@ -60,8 +60,6 @@ function setDefaultComparison(
         metricsView.dimensions?.[0]?.name;
       break;
 
-    // if default_comparison is not specified it defaults to time comparison
-    case MetricsViewSpecComparisonMode.COMPARISON_MODE_UNSPECIFIED:
     case MetricsViewSpecComparisonMode.COMPARISON_MODE_TIME:
       setDefaultComparisonTimeRange(
         metricsView,
@@ -69,6 +67,9 @@ function setDefaultComparison(
         fullTimeRange,
       );
       break;
+
+    // if default_comparison is not specified it defaults to no comparison
+    case MetricsViewSpecComparisonMode.COMPARISON_MODE_UNSPECIFIED:
   }
 }
 
@@ -78,6 +79,7 @@ function setDefaultComparisonTimeRange(
   fullTimeRange: V1MetricsViewTimeRangeResponse | undefined,
 ) {
   if (!fullTimeRange) return;
+  metricsExplorer.showTimeComparison = true;
 
   const preset = ISODurationToTimePreset(metricsView.defaultTimeRange, true);
   const comparisonOption = DEFAULT_TIME_RANGES[preset]
@@ -100,7 +102,6 @@ function setDefaultComparisonTimeRange(
     start: comparisonRange.start,
     end: comparisonRange.end,
   };
-  metricsExplorer.showTimeComparison = true;
   metricsExplorer.leaderboardContextColumn =
     LeaderboardContextColumn.DELTA_PERCENT;
 }
