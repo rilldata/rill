fd10b444d114b86881f6a9dca7d521baf9d2c832
Brian Holmes <briangregoryholmes@gmail.com>
2024-01-26 10:11:21 -0800

    add tooltip and disabled state to pivot tab

--- web-common/src/features/dashboards/tab-bar/Tab.svelte
+++ web-common/src/features/dashboards/tab-bar/Tab.svelte
@@ -2,9 +2,11 @@
   import { Tab } from "@rgossiaux/svelte-headlessui";
 
   const baseClasses = "font-semibold py-2 px-1 items-center";
+  export let disabled = false;
 </script>
 
 <Tab
+  {disabled}
   class={({ selected }) => {
     return `${baseClasses} ${
       selected ? "text-blue-600 border-b-2 border-blue-500" : "text-gray-500"
