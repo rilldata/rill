0b247d5789a91b40521a330309be1fc71327c559
brendan colloran <brendan@colloran.net>
2024-02-01 12:02:25 -0800

    storybook set up to match figma

--- web-common/src/components/button/__stories__/Button.stories.svelte
+++ web-common/src/components/button/__stories__/Button.stories.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
   import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
 
-  import Button from "@rilldata/web-common/components/button/Button.svelte";
+  import Button, {
+    ButtonShape,
+    ButtonSize,
+    ButtonType,
+  } from "@rilldata/web-common/components/button/Button.svelte";
+
+  // export const meta = {
+  //   title: "Button stories",
+  // };
 
   type ButtonTypes = "primary" | "secondary" | "text";
   type ButtonStatuses = "info" | "error";
@@ -30,6 +38,48 @@
       }
     }
   }
+
+  // outermost table layer
+
+  type FigmaRow = {
+    shape: ButtonShape;
+    size: ButtonSize;
+    danger: boolean;
+    state: string;
+  };
+
+  const shapes: ButtonShape[] = ["normal", "square", "circle"];
+  const sizes: ButtonSize[] = ["large", "medium", "small"];
+  const states = ["normal", "hovered/pressed", "disabled"];
+
+  const figmaRows: FigmaRow[] = [];
+
+  for (const shape of shapes) {
+    const dangers = shape === "normal" ? [false, true] : [false];
+    for (const danger of dangers) {
+      for (const size of sizes) {
+        for (const state of states) {
+          figmaRows.push({
+            shape,
+            size,
+            danger,
+            state,
+          });
+        }
+      }
+    }
+  }
+
+  // used as columns
+  const buttonTypes: ButtonType[] = [
+    "brand",
+    "primary",
+    "secondary",
+    "noStroke",
+    "dashed",
+    "link",
+    "text",
+  ];
 </script>
 
 <Meta title="Button stories" />
@@ -57,11 +107,86 @@
 
 <Story name="all button variations" />
 
+<Story name="Button table (like Figma)">
+  <p>
+    This story lays out the available button types in a table that is intended
+    to match the
+    <a
+      href="https://www.figma.com/file/nqqazRo1ckU9ooC9ym9weI/Rill-Design-System?type=design&node-id=1155%3A311096&mode=design&t=Ux60sppUPewJKbIW-1"
+    >
+      mocks in our design system in Figma</a
+    > (as of Feb 2024).
+  </p>
+  <br />
+  <!-- {#each figmaRows as row}
+    <h1>{shape}</h1>
+  {/each} -->
+
+  <table>
+    <thead>
+      <tr class="red">
+        <th></th>
+        {#each buttonTypes as buttonType}
+          <th><h2>{buttonType}</h2></th>
+        {/each}
+      </tr>
+    </thead>
+
+    {#each figmaRows as row}
+      <tr>
+        <td>
+          {#if row.size === "large" && row.state === "normal"}
+            <h1>{row.danger ? "dangerous" : row.shape}</h1>
+            <!-- {:else} -->
+          {/if}
+          {#if row.state === "normal"}
+            <h2>{row.size}</h2>
+            <!-- {:else} -->
+          {/if}
+          {row.state}
+          <!-- <br />{JSON.stringify(row)} -->
+        </td>
+        {#each buttonTypes as buttonType}
+          <td>
+            <Button
+              shape={row.shape}
+              size={row.size}
+              status={row.danger ? "error" : "info"}
+              type={buttonType}
+              disabled={row.state === "disabled"}
+              >{row.shape === "normal" ? "Button Title" : "A"}</Button
+            ></td
+          >
+        {/each}
+      </tr>
+    {/each}
+  </table>
+</Story>
+
 <style>
   td {
     padding: 5px;
+    vertical-align: bottom;
+    text-align: center;
   }
   td:first-child {
     padding-right: 40px;
+  }
+
+  th {
+    background: white;
+    position: sticky;
+    top: 0; /* Don't forget this, required for the stickiness */
+    box-shadow: 0 2px 2px -1px rgba(0, 0, 0, 0.4);
+  }
+
+  h1 {
+    font-size: 24px;
+    padding-top: 20px;
+  }
+  h2 {
+    font-size: 18px;
+    color: #999;
+    padding-top: 20px;
   }
 </style>
