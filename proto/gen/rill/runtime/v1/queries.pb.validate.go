9b6417a72b8729762c490387ee4e1ef4f99de2e9
Egor Riashin <egor-ryashin@users.noreply.github.com>
2024-02-07 16:15:45 +0300

    MetricsView aggregation filters (#3975)
    * metricsview aggregation filters
    
    * metricsview aggregation filters
    
    * metricsview aggregation filters
    
    * metricsview aggregation filters
    
    ---------
    
    Co-authored-by: Egor Ryashin <egor.ryashin@rilldata.com>

--- proto/gen/rill/runtime/v1/queries.pb.validate.go
+++ proto/gen/rill/runtime/v1/queries.pb.validate.go
@@ -3389,6 +3389,35 @@ func (m *MetricsViewAggregationMeasure) validate(all bool) error {
 
 	}
 
+	if all {
+		switch v := interface{}(m.GetFilter()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, MetricsViewAggregationMeasureValidationError{
+					field:  "Filter",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, MetricsViewAggregationMeasureValidationError{
+					field:  "Filter",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetFilter()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return MetricsViewAggregationMeasureValidationError{
+				field:  "Filter",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if len(errors) > 0 {
 		return MetricsViewAggregationMeasureMultiError(errors)
 	}
