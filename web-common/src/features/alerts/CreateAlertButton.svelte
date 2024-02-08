448ee2aee1e7cb266e6756f9d665bba1b74dde96
Eric P Green <ericpgreen2@gmail.com>
2024-02-08 11:39:22 -0800

    Disable "Create alert" button when time range is custom

--- web-common/src/features/alerts/CreateAlertButton.svelte
+++ web-common/src/features/alerts/CreateAlertButton.svelte
@@ -1,18 +1,32 @@
 <script lang="ts">
+  import Tooltip from "../../components/tooltip/Tooltip.svelte";
+  import TooltipContent from "../../components/tooltip/TooltipContent.svelte";
+  import { getStateManagers } from "../dashboards/state-managers/state-managers";
   import CreateAlertDialog from "./CreateAlertDialog.svelte";
+
+  const {
+    selectors: {
+      timeRangeSelectors: { isCustomTimeRange },
+    },
+  } = getStateManagers();
 
   let showAlertDialog = false;
 </script>
 
-<!-- TODO: match Figma mocks -->
-<button
-  class="h-6 px-1.5 py-px flex items-center gap-[3px] rounded-sm hover:bg-gray-200 text-gray-700"
-  on:click={() => {
-    showAlertDialog = true;
-  }}
->
-  Create alert
-</button>
+<Tooltip location="top" distance={8} suppress={!$isCustomTimeRange}>
+  <button
+    disabled={$isCustomTimeRange}
+    class="h-6 px-1.5 py-px flex items-center gap-[3px] rounded-sm hover:bg-gray-200 text-gray-700 disabled:cursor-not-allowed disabled:text-gray-400 disabled:hover:bg-gray-100"
+    on:click={() => {
+      showAlertDialog = true;
+    }}
+  >
+    Create alert
+  </button>
+  <TooltipContent slot="tooltip-content">
+    To create an alert, set a non-custom time range.
+  </TooltipContent>
+</Tooltip>
 
 <!-- Including `showAlertDialog` in the conditional ensures we tear 
   down the form state when the dialog closes -->
