aead2dd63b162e191cf56de38273f43e2bf79597
Anshul Khandelwal <anshulkhandelwal94@gmail.com>
2024-02-09 11:12:33 +0530

    remove slog

--- runtime/reconcilers/util.go
+++ runtime/reconcilers/util.go
@@ -12,7 +12,7 @@ import (
 	compilerv1 "github.com/rilldata/rill/runtime/compilers/rillv1"
 	"github.com/rilldata/rill/runtime/drivers"
 	"github.com/robfig/cron/v3"
-	"golang.org/x/exp/slog"
+	"go.uber.org/zap"
 )
 
 // checkRefs checks that all refs exist, are idle, and have no errors.
@@ -160,14 +160,14 @@ func safeSQLName(name string) string {
 func logTableNameAndType(ctx context.Context, c *runtime.Controller, connector, name string) {
 	olap, release, err := c.AcquireOLAP(ctx, connector)
 	if err != nil {
-		c.Logger.Error("LogTableNameAndType: failed to acquire OLAP", slog.Any("err", err))
+		c.Logger.Error("LogTableNameAndType: failed to acquire OLAP", zap.Error(err))
 		return
 	}
 	defer release()
 
 	res, err := olap.Execute(context.Background(), &drivers.Statement{Query: "SELECT column_name, data_type FROM information_schema.columns WHERE table_name=? ORDER BY column_name ASC", Args: []any{name}})
 	if err != nil {
-		c.Logger.Error("LogTableNameAndType: failed information_schema.columns", slog.Any("err", err))
+		c.Logger.Error("LogTableNameAndType: failed information_schema.columns", zap.Error(err))
 		return
 	}
 	defer res.Close()
@@ -177,13 +177,13 @@ func logTableNameAndType(ctx context.Context, c *runtime.Controller, connector, 
 	for res.Next() {
 		err = res.Scan(&col, &typ)
 		if err != nil {
-			c.Logger.Error("LogTableNameAndType: failed scan", slog.Any("err", err))
+			c.Logger.Error("LogTableNameAndType: failed scan", zap.Error(err))
 			return
 		}
 		colTyp = append(colTyp, fmt.Sprintf("%s:%s", col, typ))
 	}
 
-	c.Logger.Info("LogTableNameAndType: ", slog.String("name", name), slog.String("schema", strings.Join(colTyp, ", ")))
+	c.Logger.Info("LogTableNameAndType: ", zap.String("name", name), zap.String("schema", strings.Join(colTyp, ", ")))
 }
 
 func resolveTemplatedProps(ctx context.Context, c *runtime.Controller, self compilerv1.TemplateResource, props map[string]any) (map[string]any, error) {
