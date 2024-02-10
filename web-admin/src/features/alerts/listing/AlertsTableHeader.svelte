21a1d1ccbffb0ef81305841ca65079fc9123371e
Eric P Green <ericpgreen2@gmail.com>
2024-02-09 17:53:59 -0800

    Add alerts table

---
+++ web-admin/src/features/alerts/listing/AlertsTableHeader.svelte
@@ -0,0 +1,100 @@
+<script lang="ts">
+  import { Search } from "@rilldata/web-common/components/search";
+  import { getContext } from "svelte";
+
+  const table = getContext("table");
+
+  // Search
+  let filter = "";
+
+  $: filterTable(filter);
+
+  function filterTable(filter: string) {
+    $table.setGlobalFilter(filter);
+  }
+
+  // Number of alerts
+  $: numAlerts = $table.getRowModel().rows.length;
+
+  // Sort
+  // function sortAlphabetically() {
+  //   $table.setSorting([{ id: "monocolumn", desc: false }]);
+  // }
+
+  // function sortByMostRecentlyRun() {
+  //   $table.setSorting([{ id: "lastRun", desc: true }]);
+  // }
+
+  // function sortByNextToRun() {
+  //   $table.setSorting([{ id: "monocolumn", desc: false }]);
+  // }
+
+  // let openSortMenu = false;
+  // function closeSortMenu() {
+  //   openSortMenu = false;
+  // }
+</script>
+
+<thead>
+  <tr>
+    <td
+      class="pl-2 pr-4 py-2 max-w-[800px] flex items-center gap-x-2 bg-slate-100"
+    >
+      <!-- Search bar -->
+      <div class="px-2 grow">
+        <Search placeholder="Search" autofocus={false} bind:value={filter} />
+      </div>
+
+      <!-- Spacer -->
+      <div class="grow" />
+
+      <!-- filter menu button (future work) -->
+      <!-- <Button on:click={() => console.log("open filter menu")} type="secondary">
+    <span>Filter</span>
+    <CaretDownIcon />
+  </Button> -->
+
+      <!-- Number of alerts -->
+      <span class="shrink-0">{numAlerts} alert{numAlerts !== 1 ? "s" : ""}</span
+      >
+
+      <!-- Sort button -->
+      <!-- <WithTogglableFloatingElement active={openSortMenu}>
+        <Button on:click={() => (openSortMenu = true)} type="secondary">
+          <span>Sort</span>
+          <CaretDownIcon />
+        </Button>
+        <Menu
+          slot="floating-element"
+          minWidth="0px"
+          on:item-select={closeSortMenu}
+          on:click-outside={closeSortMenu}
+          on:escape={closeSortMenu}
+        >
+          <MenuItem on:select={sortAlphabetically}>Alphabetical</MenuItem>
+          <MenuItem on:select={sortByMostRecentlyRun}
+            >Most recently run</MenuItem
+          >
+          <MenuItem on:select={sortByNextToRun} disabled>Next to run</MenuItem>
+        </Menu>
+      </WithTogglableFloatingElement> -->
+    </td>
+  </tr>
+</thead>
+
+<!-- 
+Rounded table corners are tricky:
+- `border-radius` does not apply to table elements when `border-collapse` is `collapse`.
+- You can only apply `border-radius` to <td>, not <tr> or <table>.
+-->
+<style lang="postcss">
+  thead tr td {
+    @apply border-y;
+  }
+  thead tr td:first-child {
+    @apply border-l rounded-tl-sm;
+  }
+  thead tr td:last-child {
+    @apply border-r rounded-tr-sm;
+  }
+</style>
---
+++ web-admin/src/features/alerts/selectors.ts
@@ -0,0 +1,81 @@
+import { createAdminServiceSearchProjectUsers } from "@rilldata/web-admin/client";
+import { ResourceKind } from "@rilldata/web-common/features/entity-management/resource-selectors";
+import {
+  createRuntimeServiceGetResource,
+  createRuntimeServiceListResources,
+} from "@rilldata/web-common/runtime-client";
+
+export function useAlerts(instanceId: string) {
+  return createRuntimeServiceListResources(instanceId, {
+    kind: ResourceKind.Alert,
+  });
+}
+
+export function useAlert(instanceId: string, name: string) {
+  return createRuntimeServiceGetResource(instanceId, {
+    "name.name": name,
+    "name.kind": ResourceKind.Alert,
+  });
+}
+
+export function useAlertDashboardName(instanceId: string, name: string) {
+  return createRuntimeServiceGetResource(
+    instanceId,
+    {
+      "name.name": name,
+      "name.kind": ResourceKind.Alert,
+    },
+    {
+      query: {
+        select: (data) => {
+          const queryArgsJson = JSON.parse(
+            data.resource.report.spec.queryArgsJson,
+          );
+
+          return (
+            queryArgsJson?.metrics_view_name ??
+            queryArgsJson?.metricsViewName ??
+            null
+          );
+        },
+      },
+    },
+  );
+}
+
+export function useAlertOwnerName(
+  organization: string,
+  project: string,
+  ownerId: string,
+) {
+  return createAdminServiceSearchProjectUsers(
+    organization,
+    project,
+    {
+      emailQuery: "%",
+      pageSize: 1000,
+      pageToken: undefined,
+    },
+    {
+      query: {
+        select: (data) => data.users.find((u) => u.id === ownerId)?.displayName,
+      },
+    },
+  );
+}
+
+export function useIsAlertCreatedByCode(instanceId: string, name: string) {
+  return createRuntimeServiceGetResource(
+    instanceId,
+    {
+      "name.name": name,
+      "name.kind": ResourceKind.Alert,
+    },
+    {
+      query: {
+        select: (data) =>
+          !data.resource.report.spec.annotations["admin_owner_user_id"],
+      },
+    },
+  );
+}
