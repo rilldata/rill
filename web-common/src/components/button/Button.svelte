c399410fdf32f023ca637a5784ff01a875697486
Brian Holmes <briangregoryholmes@gmail.com>
2024-02-12 11:30:35 -0500

    remove padding from add button styles

--- web-common/src/components/button/Button.svelte
+++ web-common/src/components/button/Button.svelte
@@ -232,6 +232,6 @@
     @apply w-[34px] h-[26px] rounded-2xl;
     @apply flex items-center justify-center;
     @apply border border-dashed border-slate-300;
-    @apply bg-white;
+    @apply bg-white px-0;
   }
 </style>
