5ac1c799950e1ece278de6ad443491c30785bdeb
Eric P Green <ericpgreen2@gmail.com>
2024-02-12 22:14:39 -0800

    MVP "Create alert" dialog (#3881)
    * Add feature flag for Alerts
    
    * Fix lint
    
    * Add basic dialog
    
    * Add basic tabs
    
    * Add placeholder components for each tab
    
    * Add sections to the Data tab
    
    * Fix spacing when no label on an `Input` element
    
    * Add framework for multi-tab form
    
    * Implement the Data Preview (mvp)
    
    * Basic criteria tab
    
    * Update to use a single top level form
    
    * Add delivery tab
    
    * Adding readonly filters
    
    * Add criteria group operation
    
    * Fix feature flag merge
    
    * Move code to `alerts/data-tab`
    
    * Add placeholder text
    
    * Refactor dimension filters
    
    * Create `readOnly` chips
    
    * Create `readOnly` dimension filter chips
    
    * Create `readOnly` measure filter chips
    
    * Use `label` if available
    
    * Use measure/dimension `label` (not `name`) for Data Preview
    
    * Apply time range filter to the Data Preview
    
    * Match mock for "No filters selected"
    
    * Add ShadCN tabs
    
    * Switch to ShadCN tabs, which are controllable
    
    * Add a back button
    
    * Add `dialog-tabs` component set
    
    * Use `dialog-tabs` in Create Alert dialog
    
    * Fix logic of Data tab's "Next" button
    
    * Fix "Next" button pt 2
    
    * Conditionally enable buttons
    
    * Cleanups
    
    * Implement aesthetics custom to this dialog
    
    * Fix top margin
    
    * Fix redundant "Recipients" label
    
    * Clicking outside the dialog shouldn't close it
    
    * Clean up header of dimension table
    
    * Disable "Create alert" button when time range is custom
    
    * Add `read-only` chip variation
    
    * Split `outlineClass` into `outlineBaseClass` and  `outlineHoverClass`
    
    * Refactor `Filters.svelte` so we can extract `read-only` chips in "Create alert" dialog
    
    * Remove extra `ChipContainer.svelte` wrapper
    
    * Create `read-only` dimension filter chip
    
    * Create `read-only` measure filter chip
    
    * Use `label` if available
    
    * Bugfix
    
    * Cleanup
    
    * Remove extra layer
    
    * Remove unused code
    
    * Rename
    
    * Fix lint
    
    * Drop the `v2` from `components/dialog-v2`
    
    * Add dialog tabs component set
    
    * Upgrade storybook (to fix startup bug)
    
    * Add story for a multi-step dialog
    
    * Spacing
    
    * Fix prettier
    
    * Fix import
    
    * Use `esnext` (as does SvelteKit's `tsconfig.json`)
    
    * Use dialog tabs from `components/dialog/tabs` directory
    
    * Fix prettier
    
    * Hit the "Create Alert" API
    
    * Fix lint
    
    * Fix spacing
    
    * Add dynamic import to avoid loading `web-admin` in Rill Developer
    
    * Fix 400 error initiated from a different tab
    
    * Fix story
    
    ---------
    
    Co-authored-by: Aditya Hegde <adityahegderocks@gmail.com>

---
+++ web-common/src/features/alerts/delivery-tab/snooze.ts
@@ -0,0 +1,23 @@
+// TODO: replace value with the enum from backend
+export const SnoozeOptions = [
+  {
+    value: "OFF",
+    label: "Off",
+  },
+  {
+    value: "REST_OF_HOUR",
+    label: "Rest of the hour",
+  },
+  {
+    value: "REST_OF_DAY",
+    label: "Rest of the day",
+  },
+  {
+    value: "REST_OF_WEEK",
+    label: "Rest of the week",
+  },
+  {
+    value: "REST_OF_MONTH",
+    label: "Rest of the month",
+  },
+];
