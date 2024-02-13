c8798a56bdc0f384d337a2c08176975c35c4a4ab
Aditya Hegde <adityahegderocks@gmail.com>
2024-02-13 14:29:08 +0530

    Add basics of alerts resource

---
+++ web-admin/src/features/alerts/selectors.ts
@@ -0,0 +1,43 @@
+import { ResourceKind } from "@rilldata/web-common/features/entity-management/resource-selectors";
+import {
+  createRuntimeServiceGetResource,
+  createRuntimeServiceListResources,
+} from "@rilldata/web-common/runtime-client";
+
+export function useAlerts(instanceId: string) {
+  return createRuntimeServiceListResources(instanceId, {
+    kind: ResourceKind.Alert,
+  });
+}
+
+export function useAlert(instanceId: string, name: string) {
+  return createRuntimeServiceGetResource(instanceId, {
+    "name.name": name,
+    "name.kind": ResourceKind.Alert,
+  });
+}
+
+export function useAlertDashboardName(instanceId: string, name: string) {
+  return createRuntimeServiceGetResource(
+    instanceId,
+    {
+      "name.name": name,
+      "name.kind": ResourceKind.Alert,
+    },
+    {
+      query: {
+        select: (data) => {
+          const queryArgsJson = JSON.parse(
+            data.resource.alert.spec.queryArgsJson,
+          );
+
+          return (
+            queryArgsJson?.metrics_view_name ??
+            queryArgsJson?.metricsViewName ??
+            null
+          );
+        },
+      },
+    },
+  );
+}
--- web-common/src/features/entity-management/resource-selectors.ts
+++ web-common/src/features/entity-management/resource-selectors.ts
@@ -15,6 +15,7 @@ export enum ResourceKind {
   Model = "rill.runtime.v1.Model",
   MetricsView = "rill.runtime.v1.MetricsView",
   Report = "rill.runtime.v1.Report",
+  Alert = "rill.runtime.v1.Alert",
   Theme = "rill.runtime.v1.Theme",
 }
 export const SingletonProjectParserName = "parser";
