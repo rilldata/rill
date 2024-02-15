c9fdfabcd2e40739421c881dbf45405b194085f8
Benjamin Egelund-Müller <b@egelund-muller.com>
2024-02-15 15:32:41 +0100

    Prettier

--- web-admin/src/features/projects/status/selectors.ts
+++ web-admin/src/features/projects/status/selectors.ts
@@ -8,31 +8,36 @@ export const PollTimeWhenProjectDeploymentError = 5000;
 export const PollTimeWhenProjectDeployed = 60 * 1000;
 
 export function useProjectDeploymentStatus(orgName: string, projName: string) {
-  return createAdminServiceGetProject<V1DeploymentStatus>(orgName, projName, undefined, {
-    query: {
-      select: (data) => {
-        // There may not be a prodDeployment if the project is hibernating
-        return (
-          data?.prodDeployment?.status ||
-          V1DeploymentStatus.DEPLOYMENT_STATUS_UNSPECIFIED
-        );
-      },
-      refetchInterval: (data) => {
-        switch (data) {
-          case V1DeploymentStatus.DEPLOYMENT_STATUS_PENDING:
-            return PollTimeWhenProjectDeploymentPending;
+  return createAdminServiceGetProject<V1DeploymentStatus>(
+    orgName,
+    projName,
+    undefined,
+    {
+      query: {
+        select: (data) => {
+          // There may not be a prodDeployment if the project is hibernating
+          return (
+            data?.prodDeployment?.status ||
+            V1DeploymentStatus.DEPLOYMENT_STATUS_UNSPECIFIED
+          );
+        },
+        refetchInterval: (data) => {
+          switch (data) {
+            case V1DeploymentStatus.DEPLOYMENT_STATUS_PENDING:
+              return PollTimeWhenProjectDeploymentPending;
 
-          case V1DeploymentStatus.DEPLOYMENT_STATUS_ERROR:
-          case V1DeploymentStatus.DEPLOYMENT_STATUS_UNSPECIFIED:
-            return PollTimeWhenProjectDeploymentError;
+            case V1DeploymentStatus.DEPLOYMENT_STATUS_ERROR:
+            case V1DeploymentStatus.DEPLOYMENT_STATUS_UNSPECIFIED:
+              return PollTimeWhenProjectDeploymentError;
 
-          case V1DeploymentStatus.DEPLOYMENT_STATUS_OK:
-            return PollTimeWhenProjectDeployed;
+            case V1DeploymentStatus.DEPLOYMENT_STATUS_OK:
+              return PollTimeWhenProjectDeployed;
 
-          default:
-            return PollTimeWhenProjectDeployed;
-        }
+            default:
+              return PollTimeWhenProjectDeployed;
+          }
+        },
       },
     },
-  });
+  );
 }
