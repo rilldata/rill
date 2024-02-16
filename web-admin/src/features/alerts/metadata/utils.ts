5d491bf2130d7cbfee153f646709d16eacca81a5
Aditya Hegde <adityahegderocks@gmail.com>
2024-02-16 16:00:50 +0530

    Add alerts interval in create dialog and alert metadata page (#4056)
    * Add basics of alerts resource
    
    * Add method for alert created by code
    
    * Add alerts page
    
    * Add all parts of alert page
    
    * Add manual ref for UI created alert
    
    * Pass in dashboard name
    
    * Adding history status
    
    * Update no history message
    
    * PR comments
    
    * Add alert interval support
    
    * Fix lint
    
    * Rename methods to be clearer
    
    * Remove TODO

--- web-admin/src/features/alerts/metadata/utils.ts
+++ web-admin/src/features/alerts/metadata/utils.ts
@@ -1,6 +1,17 @@
+import { AlertIntervalOptions } from "@rilldata/web-common/features/alerts/data-tab/intervals";
+import { humaniseISODuration } from "@rilldata/web-common/lib/time/ranges/iso-ranges";
 import { SnoozeOptions } from "@rilldata/web-common/features/alerts/delivery-tab/snooze";
 import type { V1AlertSpec } from "@rilldata/web-common/runtime-client";
 import { Duration } from "luxon";
+
+export function humaniseAlertRunDuration(alert: V1AlertSpec | undefined) {
+  if (!alert?.intervalsIsoDuration) return "None";
+  const preset = AlertIntervalOptions.find(
+    (o) => o.value === alert.intervalsIsoDuration,
+  );
+  if (preset) return preset.label;
+  return humaniseISODuration(alert.intervalsIsoDuration);
+}
 
 export function humaniseAlertSnoozeOption(
   alert: V1AlertSpec | undefined,
