ff2b65b879abf648181aa2fda1bad025afe3312e
egor-ryashin <riashin.egor@gmail.com>
2024-02-20 13:46:15 +0300

    Druid TableRows request

--- runtime/queries/table_columns.go
+++ runtime/queries/table_columns.go
@@ -58,38 +58,70 @@ func (q *TableColumns) Resolve(ctx context.Context, rt *runtime.Runtime, instanc
 	}
 	defer release()
 
-	if olap.Dialect() != drivers.DialectDuckDB {
-		return fmt.Errorf("not available for dialect '%s'", olap.Dialect())
-	}
-
-	return olap.WithConnection(ctx, priority, false, false, func(ctx context.Context, ensuredCtx context.Context, _ *sql.Conn) error {
-		// views return duplicate column names, so we need to create a temporary table
-		temporaryTableName := tempName("profile_columns_")
-		err = olap.Exec(ctx, &drivers.Statement{
-			Query:            fmt.Sprintf(`CREATE TEMPORARY TABLE "%s" AS (SELECT * FROM "%s" LIMIT 1)`, temporaryTableName, q.TableName),
-			Priority:         priority,
-			ExecutionTimeout: defaultExecutionTimeout,
-		})
-		if err != nil {
-			return err
-		}
-		defer func() {
-			// NOTE: Using ensuredCtx
-			_ = olap.Exec(ensuredCtx, &drivers.Statement{
-				Query:            `DROP TABLE "` + temporaryTableName + `"`,
+	if olap.Dialect() == drivers.DialectDuckDB {
+		return olap.WithConnection(ctx, priority, false, false, func(ctx context.Context, ensuredCtx context.Context, _ *sql.Conn) error {
+			// views return duplicate column names, so we need to create a temporary table
+			temporaryTableName := tempName("profile_columns_")
+			err = olap.Exec(ctx, &drivers.Statement{
+				Query:            fmt.Sprintf(`CREATE TEMPORARY TABLE "%s" AS (SELECT * FROM %s LIMIT 1)`, temporaryTableName, safeName(q.TableName)),
 				Priority:         priority,
 				ExecutionTimeout: defaultExecutionTimeout,
 			})
-		}()
+			if err != nil {
+				return err
+			}
+			defer func() {
+				// NOTE: Using ensuredCtx
+				_ = olap.Exec(ensuredCtx, &drivers.Statement{
+					Query:            `DROP TABLE "` + temporaryTableName + `"`,
+					Priority:         priority,
+					ExecutionTimeout: defaultExecutionTimeout,
+				})
+			}()
 
-		rows, err := olap.Execute(ctx, &drivers.Statement{
-			Query: fmt.Sprintf(`
+			rows, err := olap.Execute(ctx, &drivers.Statement{
+				Query: fmt.Sprintf(`
 				SELECT column_name AS name, data_type AS type
 				FROM information_schema.columns
 				WHERE table_catalog = 'temp' AND table_name = '%s'`, temporaryTableName),
+				Priority:         priority,
+				ExecutionTimeout: defaultExecutionTimeout,
+			})
+			if err != nil {
+				return err
+			}
+			defer rows.Close()
+
+			var pcs []*runtimev1.ProfileColumn
+			i := 0
+			for rows.Next() {
+				pc := runtimev1.ProfileColumn{}
+				if err := rows.StructScan(&pc); err != nil {
+					return err
+				}
+				// TODO: Find a better way to handle this, this is ugly
+				if strings.Contains(pc.Type, "ENUM") {
+					pc.Type = "VARCHAR"
+				}
+				pcs = append(pcs, &pc)
+				i++
+			}
+
+			q.Result = pcs[0:i]
+			return nil
+		})
+	}
+
+	if olap.Dialect() == drivers.DialectDruid {
+		rows, err := olap.Execute(ctx, &drivers.Statement{
+			Query: fmt.Sprintf(`
+			SELECT COLUMN_NAME AS name, DATA_TYPE AS type
+			FROM INFORMATION_SCHEMA.COLUMNS
+			WHERE TABLE_NAME = '%s'`, q.TableName),
 			Priority:         priority,
 			ExecutionTimeout: defaultExecutionTimeout,
 		})
+
 		if err != nil {
 			return err
 		}
@@ -102,8 +134,8 @@ func (q *TableColumns) Resolve(ctx context.Context, rt *runtime.Runtime, instanc
 			if err := rows.StructScan(&pc); err != nil {
 				return err
 			}
-			// TODO: Find a better way to handle this, this is ugly
-			if strings.Contains(pc.Type, "ENUM") {
+
+			if strings.Contains(pc.Type, "COMPLEX") {
 				pc.Type = "VARCHAR"
 			}
 			pcs = append(pcs, &pc)
@@ -112,7 +144,9 @@ func (q *TableColumns) Resolve(ctx context.Context, rt *runtime.Runtime, instanc
 
 		q.Result = pcs[0:i]
 		return nil
-	})
+	}
+
+	return fmt.Errorf("not available for dialect '%s'", olap.Dialect())
 }
 
 func (q *TableColumns) Export(ctx context.Context, rt *runtime.Runtime, instanceID string, w io.Writer, opts *runtime.ExportOptions) error {
