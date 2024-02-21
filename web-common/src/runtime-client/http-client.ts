460f9f2d5cc4eeaa7e60c21d88016cfa0ff6d62c
Eric P Green <ericpgreen2@gmail.com>
2024-02-21 09:51:15 -0800

    Fix race condition for expired JWTs (#4114)
    * Ensure fresh JWT before making Runtime requests
    
    * Use new `runtimeStore` data structure
    
    * Comment nits
    
    * More comment nits
    
    * Move constant to `web-common`
    
    * Comment

--- web-common/src/runtime-client/http-client.ts
+++ web-common/src/runtime-client/http-client.ts
@@ -3,8 +3,9 @@ import type {
   HTTPError,
 } from "@rilldata/web-common/runtime-client/fetchWrapper";
 import { get } from "svelte/store";
+import { RUNTIME_ACCESS_TOKEN_DEFAULT_TTL } from "./constants";
 import { HttpRequestQueue } from "./http-request-queue/HttpRequestQueue";
-import { runtime } from "./runtime-store";
+import { JWT, runtime } from "./runtime-store";
 
 /**
  * Runtime base URL
@@ -20,23 +21,48 @@ export const httpRequestQueue = new HttpRequestQueue();
 export const httpClient = async <T>(
   config: FetchWrapperOptions,
 ): Promise<T> => {
-  // naive request interceptors
+  // Naive request interceptors
 
-  // set host
+  // Set host
   const host = get(runtime).host;
   const interceptedConfig = { ...config, baseUrl: host };
 
-  // set jwt
-  const jwt = get(runtime).jwt;
+  // Set JWT
+  let jwt = get(runtime).jwt;
   if (jwt) {
+    jwt = await maybeWaitForFreshJWT(jwt);
     interceptedConfig.headers = {
       ...interceptedConfig.headers,
-      Authorization: `Bearer ${jwt}`,
+      Authorization: `Bearer ${jwt.token}`,
     };
   }
 
   return (await httpRequestQueue.add(interceptedConfig)) as Promise<T>;
 };
+
+const JWT_EXPIRY_WARNING_WINDOW = 2 * 1000; // Extra time to ensure that the JWT is not expired when it ultimately reaches the server
+const CHECK_RUNTIME_STORE_FOR_JWT_INTERVAL = 50; // Interval to recheck JWT freshness in milliseconds
+
+/**
+ * If the JWT has expired, or is close to expiring, wait for a fresh one.
+ */
+async function maybeWaitForFreshJWT(jwt: JWT): Promise<JWT> {
+  // This is the approximate time at which the JWT will expire. We could parse the JWT to get the exact
+  // expiration time, but it's better to treat tokens as opaque.
+  let jwtExpiresAt = jwt.receivedAt + RUNTIME_ACCESS_TOKEN_DEFAULT_TTL;
+
+  while (Date.now() + JWT_EXPIRY_WARNING_WINDOW > jwtExpiresAt) {
+    // Note: Rather than waiting, it could be even better to immediately fetch a new token here. Anyways, in
+    // practice, a request for new token is already in flight. So to start simpler, we just wait.
+    await new Promise((resolve) =>
+      setTimeout(resolve, CHECK_RUNTIME_STORE_FOR_JWT_INTERVAL),
+    );
+    jwt = get(runtime).jwt as JWT;
+    jwtExpiresAt = jwt.receivedAt + RUNTIME_ACCESS_TOKEN_DEFAULT_TTL;
+  }
+
+  return jwt;
+}
 
 export default httpClient;
 
