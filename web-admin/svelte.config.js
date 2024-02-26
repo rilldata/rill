4543d72ed88c62be1f64b1b0693dd79c8b76e61c
Himadri Singh <himadri.singh@gmail.com>
2024-02-26 22:31:26 +0530

    Update svelte.config.js

--- web-admin/svelte.config.js
+++ web-admin/svelte.config.js
@@ -11,9 +11,6 @@ const config = {
     csp: {
       directives: {
         'script-src': ['self']
-      },
-      reportOnly: {
-        'script-src': ['self']
       }
     },
     adapter: adapter({
