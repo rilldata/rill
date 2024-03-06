77fce034213052ce145dab71b41529e3978617af
Anshul Khandelwal <anshulkhandelwal94@gmail.com>
2024-03-06 12:50:12 +0530

    policy in metric resolver

--- runtime/resolvers/metricsview_sql.go
+++ runtime/resolvers/metricsview_sql.go
@@ -5,11 +5,15 @@ import (
 	"errors"
 	"fmt"
 	"regexp"
+	"slices"
 	"strings"
+	"time"
 
 	runtimev1 "github.com/rilldata/rill/proto/gen/rill/runtime/v1"
 	"github.com/rilldata/rill/runtime"
+	compilerv1 "github.com/rilldata/rill/runtime/compilers/rillv1"
 	"github.com/rilldata/rill/runtime/drivers"
+	"golang.org/x/exp/maps"
 )
 
 func init() {
@@ -17,7 +21,7 @@ func init() {
 }
 
 var (
-	aggRegexWithoutMV = regexp.MustCompile(`(?i)AGGREGATE\(([a-zA-z_][a-zA-Z0-9_]*|"(?:[^"]|"")*")\)`)
+	aggWithoutMVRegex = regexp.MustCompile(`(?i)AGGREGATE\(([a-zA-z_][a-zA-Z0-9_]*|"(?:[^"]|"")*")\)`)
 	fromMVRegex       = regexp.MustCompile(`(?i)FROM\s+([a-zA-z_][a-zA-Z0-9_]*|"(?:[^"]|"")*")`)
 )
 
@@ -32,12 +36,7 @@ func newMetricsViewSQL(ctx context.Context, opts *runtime.APIResolverOptions) (r
 		return nil, err
 	}
 
-	resolvedSQL, deps, err := resolveSQLAndDeps(ctx, sql, opts)
-	if err != nil {
-		return nil, err
-	}
-
-	parsedSQL, err := parseMetricsViewSQL(ctx, ctrl, resolvedSQL)
+	parsedSQL, deps, err := expandMetricsViewSQL(ctx, ctrl, opts, sql)
 	if err != nil {
 		return nil, err
 	}
@@ -56,7 +55,7 @@ func newMetricsViewSQL(ctx context.Context, opts *runtime.APIResolverOptions) (r
 	}, nil
 }
 
-// parseMetricsViewSQL parses the metrics SQL where it
+// expandMetricsViewSQL parses the metrics SQL where it
 //
 // 1. expands AGGREGATE(metric) into actual aggregate definition from model_view definition
 // 2. converts the FROM model_view clause to the underlying FROM table
@@ -64,38 +63,53 @@ func newMetricsViewSQL(ctx context.Context, opts *runtime.APIResolverOptions) (r
 // example transformation:
 // input : SELECT dim_col, AGGREGATE(metric) FROM metrics_view GROUP BY dim_col
 // output : SELECT dim_name, sum(col2) FROM metrics_view_model  GROUP BY dim_col
-func parseMetricsViewSQL(ctx context.Context, ctrl *runtime.Controller, sql string) (string, error) {
+func expandMetricsViewSQL(ctx context.Context, ctrl *runtime.Controller, opts *runtime.APIResolverOptions, sql string) (string, []*runtimev1.ResourceName, error) {
+	// 1. get the dependencies
+	meta, err := compilerv1.AnalyzeTemplate(sql)
+	if err != nil {
+		return "", nil, err
+	}
+	var deps []*runtimev1.ResourceName
+	for _, ref := range meta.Refs {
+		deps = append(deps, &runtimev1.ResourceName{Kind: ref.Kind.String(), Name: ref.Name})
+	}
+
+	// 2. Expand metricsview SQL
 	// if there is a match, it will be of the form `from (metrics_view)``
 	// first is full match second is `metrics_view`
 	matches := fromMVRegex.FindAllStringSubmatch(sql, -1)
 
-	seenMV := make(map[string]any)
+	seenMV := make(map[string]*runtimev1.ResourceName)
 	var mvConnector string
 	for _, match := range matches {
 		metricView := unquote(match[1])
 		if _, ok := seenMV[metricView]; ok {
 			continue
 		}
-		seenMV[metricView] = nil
 
 		resource, err := ctrl.Get(ctx, &runtimev1.ResourceName{Kind: runtime.ResourceKindMetricsView, Name: metricView}, false)
 		if err != nil {
 			if errors.Is(err, drivers.ErrResourceNotFound) {
 				continue
 			}
-			return "", fmt.Errorf("error fetching resource %v: %w", metricView, err)
+			return "", nil, fmt.Errorf("error fetching resource %v: %w", metricView, err)
 		}
 		if resource.GetMetricsView() == nil { // resource is not a metrics view
 			continue
 		}
+		seenMV[metricView] = &runtimev1.ResourceName{Kind: runtime.ResourceKindMetricsView, Name: metricView}
 
 		if mvConnector != "" && resource.GetMetricsView().Spec.Connector != mvConnector {
-			return "", fmt.Errorf("all referenced metrics views must use the same connector")
+			return "", nil, fmt.Errorf("all referenced metrics views must use the same connector")
 		}
 		mvConnector = resource.GetMetricsView().Spec.Connector
 
 		// change from metric view to underlying table
-		sql = strings.ReplaceAll(sql, match[0], fmt.Sprintf("FROM %s", safeSQLName(resource.GetMetricsView().Spec.Table)))
+		fromQry, err := underlyingTableQuery(ctrl.Runtime, opts, resource.GetMetricsView(), resource.Meta.StateUpdatedOn.AsTime())
+		if err != nil {
+			return "", nil, err
+		}
+		sql = strings.ReplaceAll(sql, match[0], fromQry)
 
 		measures := resource.GetMetricsView().Spec.Measures
 		nameToExprMap := make(map[string]string, len(measures))
@@ -104,17 +118,17 @@ func parseMetricsViewSQL(ctx context.Context, ctrl *runtime.Controller, sql stri
 		}
 
 		// example query = select dim1, aggregate(mv."my measure") from mv
+		// captures AGGREGATE("mv name"."my measure"), my name, measure
 		aggRegex, err := regexp.Compile(fmt.Sprintf(`(?i)AGGREGATE\((%s|"%s").([a-zA-z_][a-zA-Z0-9_]*|"(?:[^"]|"")*")\)`, metricView, metricView))
 		if err != nil {
-			return "", err
+			return "", nil, err
 		}
 
-		// captures AGGREGATE("mv name"."my measure"), my name, measure
 		aggMatches := aggRegex.FindAllStringSubmatch(sql, -1)
 		for _, aggMatch := range aggMatches {
 			expr, ok := nameToExprMap[unquote(aggMatch[2])]
 			if !ok {
-				return "", fmt.Errorf("MetricsViewSQL: measure %v not found", aggMatch[2])
+				return "", nil, fmt.Errorf("MetricsViewSQL: measure %v not found", aggMatch[2])
 			}
 
 			// TODO handle case when two different tables have same column name in the measure expression
@@ -125,18 +139,80 @@ func parseMetricsViewSQL(ctx context.Context, ctrl *runtime.Controller, sql stri
 		// in which case user can submit query without mv name appended to measure
 		// select dim1, aggregate("my measure") from mv
 		if len(matches) == 1 {
-			aggMatches = aggRegexWithoutMV.FindAllStringSubmatch(sql, -1)
+			aggMatches = aggWithoutMVRegex.FindAllStringSubmatch(sql, -1)
 			for _, aggMatch := range aggMatches {
 				expr, ok := nameToExprMap[unquote(aggMatch[1])]
 				if !ok {
-					return "", fmt.Errorf("MetricsViewSQL: measure %v not found", aggMatch[1])
+					return "", nil, fmt.Errorf("MetricsViewSQL: measure %v not found", aggMatch[1])
 				}
 
 				sql = strings.ReplaceAll(sql, aggMatch[0], expr)
 			}
 		}
 	}
-	return sql, nil
+	deps = append(deps, maps.Values(seenMV)...)
+
+	// 3. resolver all templates
+	sql, err = compilerv1.ResolveTemplate(sql, compilerv1.TemplateData{
+		User:       opts.UserAttributes,
+		ExtraProps: opts.Args,
+		Self: compilerv1.TemplateResource{
+			Meta:  &runtimev1.ResourceMeta{}, // TODO: Fill in with actual metadata
+			Spec:  opts.API.Spec,
+			State: opts.API.State,
+		},
+		Resolve: func(ref compilerv1.ResourceName) (string, error) {
+			return safeSQLName(ref.Name), nil
+		},
+		Lookup: func(name compilerv1.ResourceName) (compilerv1.TemplateResource, error) {
+			// TODO: Implement this, do we need to support this?
+			return compilerv1.TemplateResource{}, nil
+		},
+	})
+	if err != nil {
+		return "", nil, err
+	}
+
+	return sql, deps, nil
+}
+
+func underlyingTableQuery(rt *runtime.Runtime, opts *runtime.APIResolverOptions, mv *runtimev1.MetricsViewV2, lastUpdatedTime time.Time) (string, error) {
+	security, err := rt.ResolveMetricsViewSecurity(opts.UserAttributes, opts.InstanceID, mv.Spec, lastUpdatedTime)
+	if err != nil {
+		return "", err
+	}
+
+	if security == nil {
+		return fmt.Sprintf("FROM %s", safeSQLName(mv.Spec.Table)), nil
+	}
+
+	if !security.Access || security.ExcludeAll {
+		return "", fmt.Errorf("access forbidden")
+	}
+
+	var sql string
+	if len(security.Include) > 0 {
+		var cols []string
+		for _, col := range security.Include {
+			cols = append(cols, safeSQLName(col))
+		}
+		sql = "SELECT " + strings.Join(cols, ", ") + " FROM " + safeSQLName(mv.Spec.Table)
+	} else if len(security.Exclude) > 0 {
+		var cols []string
+		for _, col := range mv.Spec.Dimensions {
+			if !slices.Contains(security.Exclude, col.Column) {
+				cols = append(cols, safeSQLName(col.Column))
+			}
+		}
+		sql = "SELECT " + strings.Join(cols, ", ") + " FROM " + safeSQLName(mv.Spec.Table)
+	} else {
+		sql = "SELECT * FROM " + safeSQLName(mv.Spec.Table)
+	}
+
+	if security.RowFilter != "" {
+		sql += " WHERE " + security.RowFilter
+	}
+	return fmt.Sprintf("FROM (%s)", sql), nil
 }
 
 func unquote(input string) string {
--- runtime/resolvers/metricsview_sql_test.go
+++ runtime/resolvers/metricsview_sql_test.go
@@ -2,12 +2,16 @@ package resolvers
 
 import (
 	"context"
+	"encoding/json"
 	"regexp"
 	"strings"
 	"testing"
 
+	runtimev1 "github.com/rilldata/rill/proto/gen/rill/runtime/v1"
+	"github.com/rilldata/rill/runtime"
 	"github.com/rilldata/rill/runtime/testruntime"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/types/known/structpb"
 )
 
 func Test_parsedSQL(t *testing.T) {
@@ -15,40 +19,62 @@ func Test_parsedSQL(t *testing.T) {
 	ctrl, err := rt.Controller(context.Background(), instanceID)
 	require.NoError(t, err)
 
+	type result struct {
+		sql  string
+		deps []*runtimev1.ResourceName
+	}
 	tests := []struct {
 		name string
 		sql  string
-		want string
+		want result
 	}{
 		{
 			"simple",
 			"select * from ad_bids_metrics",
-			"select * FROM \"ad_bids\"",
+			result{
+				sql:  "select * FROM \"ad_bids\"",
+				deps: []*runtimev1.ResourceName{{Kind: runtime.ResourceKindMetricsView, Name: "ad_bids_metrics"}},
+			},
 		},
 		{
 			"simple quoted",
 			"select * from \"ad_bids_metrics\"",
-			"select * FROM \"ad_bids\"",
+			result{
+				sql:  "select * FROM \"ad_bids\"",
+				deps: []*runtimev1.ResourceName{{Kind: runtime.ResourceKindMetricsView, Name: "ad_bids_metrics"}},
+			},
 		},
 		{
 			"aggregate",
 			"SELECT pub,domain_parts,dom,tld,null_publisher,AGGREGATE(num_bids),AGGREGATE(avg_bid_price) FROM ad_bids_metrics GROUP BY ALL",
-			"SELECT pub,domain_parts,dom,tld,null_publisher,count(*),avg(bid_price) FROM \"ad_bids\" GROUP BY ALL",
+			result{
+				sql:  "SELECT pub,domain_parts,dom,tld,null_publisher,count(*),avg(bid_price) FROM \"ad_bids\" GROUP BY ALL",
+				deps: []*runtimev1.ResourceName{{Kind: runtime.ResourceKindMetricsView, Name: "ad_bids_metrics"}},
+			},
 		},
 		{
 			"aggregate with mv appended",
 			"SELECT pub,domain_parts,dom,tld,null_publisher,AGGREGATE(ad_bids_metrics.num_bids),AGGREGATE(ad_bids_metrics.avg_bid_price) FROM ad_bids_metrics GROUP BY ALL",
-			"SELECT pub,domain_parts,dom,tld,null_publisher,count(*),avg(bid_price) FROM \"ad_bids\" GROUP BY ALL",
+			result{
+				sql:  "SELECT pub,domain_parts,dom,tld,null_publisher,count(*),avg(bid_price) FROM \"ad_bids\" GROUP BY ALL",
+				deps: []*runtimev1.ResourceName{{Kind: runtime.ResourceKindMetricsView, Name: "ad_bids_metrics"}},
+			},
 		},
 		{
 			"aggregate with mv appended and quoted",
 			"SELECT pub,domain_parts,dom,tld,null_publisher,AGGREGATE(\"ad_bids_metrics\".\"num_bids\"),AGGREGATE(ad_bids_metrics.\"avg_bid_price\") FROM ad_bids_metrics GROUP BY ALL",
-			"SELECT pub,domain_parts,dom,tld,null_publisher,count(*),avg(bid_price) FROM \"ad_bids\" GROUP BY ALL",
+			result{
+				sql:  "SELECT pub,domain_parts,dom,tld,null_publisher,count(*),avg(bid_price) FROM \"ad_bids\" GROUP BY ALL",
+				deps: []*runtimev1.ResourceName{{Kind: runtime.ResourceKindMetricsView, Name: "ad_bids_metrics"}},
+			},
 		},
 		{
 			"aggregate and spaces",
 			`SELECT pub,dom,AGGREGATE("bid's number"),AGGREGATE("total volume"),Aggregate("total click""s") From ad_bids_mini_metrics GROUP BY ALL`,
-			"SELECT pub,dom,count(*),sum(volume),sum(clicks) FROM \"ad_bids_mini\" GROUP BY ALL",
+			result{
+				sql:  "SELECT pub,dom,count(*),sum(volume),sum(clicks) FROM \"ad_bids_mini\" GROUP BY ALL",
+				deps: []*runtimev1.ResourceName{{Kind: runtime.ResourceKindMetricsView, Name: "ad_bids_mini_metrics"}},
+			},
 		},
 		{
 			"aggregate and join",
@@ -81,7 +107,8 @@ func Test_parsedSQL(t *testing.T) {
 				join b on
 					a.publisher = b.publisher
 				`,
-			`with a as (
+			result{
+				sql: `with a as (
 					select
 						publisher,
 						sum(volume) as total_volume,
@@ -108,17 +135,109 @@ func Test_parsedSQL(t *testing.T) {
 					join b on
 						a.publisher = b.publisher
 					`,
+				deps: []*runtimev1.ResourceName{{Kind: runtime.ResourceKindMetricsView, Name: "ad_bids_mini_metrics"}, {Kind: runtime.ResourceKindMetricsView, Name: "ad_bids_metrics"}},
+			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := parseMetricsViewSQL(context.Background(), ctrl, tt.sql)
+			props, err := structpb.NewStruct(map[string]any{"sql": tt.sql})
 			require.NoError(t, err)
+
+			got, deps, err := expandMetricsViewSQL(context.Background(), ctrl, &runtime.APIResolverOptions{
+				InstanceID: instanceID,
+				API: &runtimev1.API{
+					Spec: &runtimev1.APISpec{
+						ResolverProperties: props,
+					},
+				},
+			}, tt.sql)
+			require.NoError(t, err)
+
+			require.Subset(t, deps, tt.want.deps)
+			require.Subset(t, tt.want.deps, deps)
+
 			got = regexp.MustCompile(`\s+`).ReplaceAllString(strings.ReplaceAll(strings.ReplaceAll(got, "\n", " "), "\t", " "), " ")
-			tt.want = regexp.MustCompile(`\s+`).ReplaceAllString(strings.ReplaceAll(strings.ReplaceAll(tt.want, "\n", " "), "\t", " "), " ")
-			if got != tt.want {
-				t.Errorf("parsedSQL() = %v, want %v", got, tt.want)
+			tt.want.sql = regexp.MustCompile(`\s+`).ReplaceAllString(strings.ReplaceAll(strings.ReplaceAll(tt.want.sql, "\n", " "), "\t", " "), " ")
+			if got != tt.want.sql {
+				t.Errorf("parsedSQL() = %v, want %v", got, tt.want.sql)
 			}
 		})
 	}
+}
+
+func TestSimpleMVSQLApi(t *testing.T) {
+	rt, instanceID := testruntime.NewInstanceForProject(t, "ad_bids")
+
+	api, err := rt.APIForName(context.Background(), instanceID, "simple_mv_sql_api")
+	require.NoError(t, err)
+
+	res, err := runtime.Resolve(context.Background(), &runtime.APIResolverOptions{
+		Runtime:        rt,
+		InstanceID:     instanceID,
+		API:            api,
+		Args:           nil,
+		UserAttributes: nil,
+		Priority:       0,
+	})
+
+	require.NoError(t, err)
+	require.NotNil(t, res)
+	var rows []map[string]interface{}
+	require.NoError(t, json.Unmarshal(res, &rows))
+	require.Equal(t, 5, len(rows))
+	require.Equal(t, 3, len(rows[0]))
+	require.Equal(t, "msn.com", rows[0]["domain"])
+	require.Equal(t, nil, rows[0]["publisher"])
+	require.Equal(t, "2022-03-05T14:49:50.459Z", rows[0]["timestamp"])
+}
+
+func TestTemplateMVSQLApi(t *testing.T) {
+	rt, instanceID := testruntime.NewInstanceForProject(t, "ad_bids")
+
+	api, err := rt.APIForName(context.Background(), instanceID, "templated_mv_sql_api")
+	require.NoError(t, err)
+
+	res, err := runtime.Resolve(context.Background(), &runtime.APIResolverOptions{
+		Runtime:        rt,
+		InstanceID:     instanceID,
+		API:            api,
+		Args:           map[string]any{"domain": "yahoo.com"},
+		UserAttributes: nil,
+		Priority:       0,
+	})
+
+	require.NoError(t, err)
+	require.NotNil(t, res)
+	var rows []map[string]interface{}
+	require.NoError(t, json.Unmarshal(res, &rows))
+	require.Equal(t, 1, len(rows))
+	require.Equal(t, 3.0, rows[0]["total_imp"])
+	require.Equal(t, "yahoo.com", rows[0]["domain"])
+	require.Equal(t, "Yahoo", rows[0]["publisher"])
+}
+
+func TestPolicyMVSQLApi(t *testing.T) {
+	rt, instanceID := testruntime.NewInstanceForProject(t, "ad_bids")
+
+	api, err := rt.APIForName(context.Background(), instanceID, "policy_mv_sql_api")
+	require.NoError(t, err)
+
+	res, err := runtime.Resolve(context.Background(), &runtime.APIResolverOptions{
+		Runtime:        rt,
+		InstanceID:     instanceID,
+		API:            api,
+		Args:           nil,
+		UserAttributes: map[string]any{"domain": "yahoo.com"},
+		Priority:       0,
+	})
+
+	require.NoError(t, err)
+	require.NotNil(t, res)
+	var rows []map[string]interface{}
+	require.NoError(t, json.Unmarshal(res, &rows))
+	require.Equal(t, 1, len(rows))
+	require.Equal(t, 3.0, rows[0]["total_imp"])
+	require.Equal(t, "yahoo.com", rows[0]["domain"])
+	require.Equal(t, "Yahoo", rows[0]["publisher"])
 }
