8a7dec478e8b6a50262eef7dd9d0f824143c6d3b
Dhiraj Kumar <sumankumaribarnwal@gmail.com>
2024-03-06 19:48:39 +0530

    Remove unused selectors

--- web-common/src/features/charts/selectors.ts
+++ web-common/src/features/charts/selectors.ts
@@ -2,7 +2,6 @@ import {
   ResourceKind,
   useResource,
 } from "@rilldata/web-common/features/entity-management/resource-selectors";
-import type { V1Chart } from "@rilldata/web-common/runtime-client";
 import { useMainEntityFiles } from "../entity-management/file-selectors";
 
 export function useChartFileNames(instanceId: string) {
@@ -11,14 +10,4 @@ export function useChartFileNames(instanceId: string) {
 
 export const useChart = (instanceId: string, chartName: string) => {
   return useResource(instanceId, chartName, ResourceKind.Chart);
-};
-
-export const useChartView = <T = V1Chart>(
-  instanceId: string,
-  chartName: string,
-  selector?: (meta: V1Chart) => T,
-) => {
-  return useResource<T>(instanceId, chartName, ResourceKind.Chart, (data) =>
-    selector ? selector(data.chart) : (data.chart as T),
-  );
 };
