0336fa7a40c0ee7e4fcd551732e34a14ecac75af
Eric P Green <ericpgreen2@gmail.com>
2024-03-13 12:03:08 +0100

    Recompute table names when source & model names change

--- web-common/src/features/tables/selectors.ts
+++ web-common/src/features/tables/selectors.ts
@@ -1,4 +1,12 @@
-import { createRuntimeServiceGetInstance } from "../../runtime-client";
+import { Readable, derived } from "svelte/store";
+import {
+  createConnectorServiceOLAPListTables,
+  createRuntimeServiceGetInstance,
+} from "../../runtime-client";
+import {
+  ResourceKind,
+  useFilteredResourceNames,
+} from "../entity-management/resource-selectors";
 import { OLAP_DRIVERS_WITHOUT_MODELING } from "./config";
 
 export function useIsModelingSupportedForCurrentOlapDriver(instanceId: string) {
@@ -10,4 +18,52 @@ export function useIsModelingSupportedForCurrentOlapDriver(instanceId: string) {
       },
     },
   });
+}
+
+export function useTableNames(
+  runtimeInstanceId: string,
+  connectorInstanceId: string | undefined,
+  olapConnector: string | undefined,
+): Readable<string[]> {
+  return derived(
+    [
+      useFilteredResourceNames(runtimeInstanceId, ResourceKind.Source),
+      useFilteredResourceNames(runtimeInstanceId, ResourceKind.Model),
+      createConnectorServiceOLAPListTables(
+        {
+          instanceId: connectorInstanceId,
+          connector: olapConnector,
+        },
+        {
+          query: {
+            enabled: !!connectorInstanceId && !!olapConnector,
+          },
+        },
+      ),
+    ],
+    ([$sources, $models, $tables]) => {
+      if (
+        !$sources.data ||
+        !$models.data ||
+        !$tables.data ||
+        !$tables.data.tables
+      ) {
+        return [];
+      }
+
+      // Filter out Rill-managed tables (Sources and Models)
+      const sourceNames = $sources.data;
+      const modelNames = $models.data;
+      const filteredTables = $tables.data.tables?.filter(
+        (table) =>
+          !sourceNames.includes(table.name as string) &&
+          !modelNames.includes(table.name as string),
+      );
+
+      // Return the fully qualified table names
+      return (
+        filteredTables?.map((table) => table.database + "." + table.name) || []
+      );
+    },
+  );
 }
