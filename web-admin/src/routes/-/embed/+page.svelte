06554fb8be68a8fb2cecb38013c304b5e4844740
Eric P Green <ericpgreen2@gmail.com>
2024-03-14 12:11:36 +0100

    Refactor `/-/embed` to handle different resource kinds

--- web-admin/src/routes/-/embed/+page.svelte
+++ web-admin/src/routes/-/embed/+page.svelte
@@ -1,53 +1,22 @@
 <script lang="ts">
   import { page } from "$app/stores";
-  import { Dashboard } from "@rilldata/web-common/features/dashboards";
-  import DashboardThemeProvider from "@rilldata/web-common/features/dashboards/DashboardThemeProvider.svelte";
-  import DashboardURLStateProvider from "@rilldata/web-common/features/dashboards/proto-state/DashboardURLStateProvider.svelte";
-  import { useDashboard } from "@rilldata/web-common/features/dashboards/selectors";
-  import StateManagersProvider from "@rilldata/web-common/features/dashboards/state-managers/StateManagersProvider.svelte";
-  import DashboardStateProvider from "@rilldata/web-common/features/dashboards/stores/DashboardStateProvider.svelte";
-  import { errorStore } from "../../../features/errors/error-store";
+  import DashboardEmbed from "@rilldata/web-admin/features/embeds/DashboardEmbed.svelte";
+  import { ResourceKind } from "@rilldata/web-common/features/entity-management/resource-selectors";
 
+  // Get the query parameters from the iframe's URL
   const instanceId = $page.url.searchParams.get("instance_id");
-  const dashboardName = $page.url.searchParams.get("resource");
-  // ignoring state and theme params for now
-
-  $: dashboard = useDashboard(instanceId, dashboardName);
-  $: isDashboardNotFound =
-    $dashboard.isError && $dashboard.error?.response?.status === 404;
-  // We check for metricsView.state.validSpec instead of meta.reconcileError. validSpec persists
-  // from previous valid dashboards, allowing display even when the current dashboard spec is invalid
-  // and a meta.reconcileError exists.
-  $: isDashboardErrored = !$dashboard.data?.metricsView?.state?.validSpec;
-
-  // If no dashboard is found, show a 404 page
-  $: if (isDashboardNotFound) {
-    errorStore.set({
-      statusCode: 404,
-      header: "Dashboard not found",
-      body: `The dashboard you requested could not be found. Please check that you provided the name of a working dashboard.`,
-    });
-  }
+  const resourceName = $page.url.searchParams.get("resource");
+  const resourceKind = $page.url.searchParams.get("kind");
+  const navigation = $page.url.searchParams.get("navigation");
+  // Ignoring state and theme params for now
+  // const state = $page.url.searchParams.get("state");
+  // const theme = $page.url.searchParams.get("theme");
 </script>
 
 <svelte:head>
-  <title>{dashboardName} - Rill</title>
+  <title>{resourceName} - Rill</title>
 </svelte:head>
 
-{#if $dashboard.isSuccess}
-  {#if isDashboardErrored}
-    <br /> Dashboard Error <br />
-  {:else}
-    {#key dashboardName}
-      <StateManagersProvider metricsViewName={dashboardName}>
-        <DashboardStateProvider metricViewName={dashboardName}>
-          <DashboardURLStateProvider metricViewName={dashboardName}>
-            <DashboardThemeProvider>
-              <Dashboard metricViewName={dashboardName} leftMargin={"48px"} />
-            </DashboardThemeProvider>
-          </DashboardURLStateProvider>
-        </DashboardStateProvider>
-      </StateManagersProvider>
-    {/key}
-  {/if}
+{#if resourceKind === ResourceKind.MetricsView.toString()}
+  <DashboardEmbed {instanceId} dashboardName={resourceName} />
 {/if}
