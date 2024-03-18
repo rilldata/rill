01d010156ad9d868f120e1b9640065e919d780e1
Dhiraj Kumar <sumankumaribarnwal@gmail.com>
2024-03-18 15:45:21 +0530

    JSON in yaml parser

--- web-common/src/features/charts/editor/ChartsEditor.svelte
+++ web-common/src/features/charts/editor/ChartsEditor.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { EditorView } from "@codemirror/view";
   import YAMLEditor from "@rilldata/web-common/components/editor/YAMLEditor.svelte";
+  import { customYAMLWithJSON } from "@rilldata/web-common/components/editor/presets/yamlWithJson";
   import ChartsEditorContainer from "@rilldata/web-common/features/charts/editor/ChartsEditorContainer.svelte";
   import {
     getFileAPIPathFromNameAndType,
@@ -63,6 +64,7 @@
     bind:this={editor}
     bind:view
     content={yaml}
+    extensions={[customYAMLWithJSON]}
     on:update={(e) => debounceUpdateChartContent(e.detail.content)}
   />
 </ChartsEditorContainer>
