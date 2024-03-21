dea8fec858ed395d8be8d6438c6fce3f377280e7
Brian Holmes <briangregoryholmes@gmail.com>
2024-03-21 13:55:36 -0400

    remove unrelated changes

--- web-common/src/layout/navigation/Navigation.svelte
+++ web-common/src/layout/navigation/Navigation.svelte
@@ -143,12 +143,13 @@
     @apply h-screen border-r overflow-hidden;
   }
 
-  .scroll-container {
-    @apply overflow-y-auto overflow-x-hidden;
-    @apply transition-colors h-full bg-white pb-8;
-  }
-
   .nav-wrapper {
     @apply flex flex-col h-fit w-full gap-y-3;
+  }
+
+  .scroll-container {
+    @apply grow;
+    @apply overflow-y-scroll overflow-x-hidden;
+    @apply transition-colors h-full bg-white pb-8;
   }
 </style>
