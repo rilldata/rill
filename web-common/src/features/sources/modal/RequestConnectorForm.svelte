7a013b766f1ecedc55bc64d897d76524519f1feb
Alexander Thor <alec.karlsson@gmail.com>
2024-03-24 22:58:41 +0100

    feat: add email to connector form

--- web-common/src/features/sources/modal/RequestConnectorForm.svelte
+++ web-common/src/features/sources/modal/RequestConnectorForm.svelte
@@ -10,15 +10,17 @@
   const FORM_ID = "1P9sP1jxjFcMqDzxsweIrZiU7pFUgRY452S3Nk7cEeao";
   const GOOGLE_FORM_ENDPOINT = `https://docs.google.com/forms/d/${FORM_ID}`;
   const REQUEST_FIELD_ID = "entry.849552298";
+  const EMAIL_FIELD_ID = "entry.516049603";
 
   const { form, errors, handleChange, handleSubmit, isSubmitting } = createForm(
     {
       initialValues: {
         request: "",
+        email: "",
       },
       onSubmit: async (values) => {
         // Following the approach here: https://stackoverflow.com/questions/51995070/post-data-to-a-google-form-with-ajax
-        const submitFormEndpoint = `${GOOGLE_FORM_ENDPOINT}/formResponse?${REQUEST_FIELD_ID}=${values.request}&submit=Submit`;
+        const submitFormEndpoint = `${GOOGLE_FORM_ENDPOINT}/formResponse?${REQUEST_FIELD_ID}=${values.request}&${EMAIL_FIELD_ID}=${values.email}&submit=Submit`;
         try {
           await fetch(submitFormEndpoint, {
             method: "GET",
@@ -53,6 +55,16 @@
         placeholder="Your data source"
         error={$errors["request"]}
         bind:value={$form["request"]}
+        on:change={handleChange}
+      />
+    </div>
+    <div class="pt-4 pb-5 text-slate-800">
+      <Input
+        id="email"
+        label="Optionally, we can let you know when the connector is available."
+        placeholder="Your email address"
+        error={$errors["email"]}
+        bind:value={$form["email"]}
         on:change={handleChange}
       />
     </div>
