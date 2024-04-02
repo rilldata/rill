418ff6763562cce504937752599be480c9ab81a5
Aditya Hegde <adityahegderocks@gmail.com>
2024-04-02 18:26:32 +0530

    Refactor sources to use filePath instead of sourceName (#4425)
    * Unified artifact store
    
    * Cleanup of old stores
    
    * Fix reassignment of FileArtifact
    
    * Refactor source editor to use path instead of name
    
    * Get FileArtifact in components and use selectors on it
    
    * Add parsing of unknown files
    
    * Cleanups
    
    * Rename to FileArtifacts
    
    * Filter errored source and models
    
    * Fix paths around initial source modal
    
    * Fix lint
    
    * Fixes

--- web-common/src/features/sources/saveAndRefresh.ts
+++ web-common/src/features/sources/saveAndRefresh.ts
@@ -1,12 +1,9 @@
 import { get } from "svelte/store";
 import { runtimeServicePutFile } from "../../runtime-client";
 import { runtime } from "../../runtime-client/runtime-store";
-import { getFileAPIPathFromNameAndType } from "../entity-management/entity-mappers";
-import { EntityType } from "../entity-management/types";
 
-export async function saveAndRefresh(tableName: string, yaml: string) {
+export async function saveAndRefresh(filePath: string, yaml: string) {
   const instanceId = get(runtime).instanceId;
-  const filePath = getFileAPIPathFromNameAndType(tableName, EntityType.Table);
 
   return runtimeServicePutFile(instanceId, filePath, {
     blob: yaml,
