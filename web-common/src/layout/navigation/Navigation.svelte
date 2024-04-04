fedee7fa93aef113b8037618183ac36069d8a361
Brian Holmes <briangregoryholmes@gmail.com>
2024-04-04 12:59:23 -0400

    remove console log

--- web-common/src/layout/navigation/Navigation.svelte
+++ web-common/src/layout/navigation/Navigation.svelte
@@ -36,8 +36,6 @@
 
   $: isModelerEnabled = $readOnly === false;
 
-  $: console.log("RUNTIME", $runtime);
-
   $: instance = createRuntimeServiceGetInstance($runtime.instanceId);
   $: olapConnector = $instance.data?.instance?.olapConnector;
   $: isModelingSupportedForCurrentOlapDriver =
