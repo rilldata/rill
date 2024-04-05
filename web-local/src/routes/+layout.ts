baa6d1bdeacd6c473c6578e5991cfba6d08367d8
Brian Holmes <briangregoryholmes@gmail.com>
2024-04-05 16:54:41 -0400

    use sveltekit module

--- web-local/src/routes/+layout.ts
+++ web-local/src/routes/+layout.ts
@@ -1,5 +1,7 @@
+import { dev } from "$app/environment";
+
 // When testing, we need to use the relative path to the server
-const host = import.meta.env.DEV ? "http://localhost:9009" : "";
+const host = dev ? "http://localhost:9009" : "";
 
 export const ssr = false;
 
