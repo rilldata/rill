1a5467697a970550d2a49becd24574fab0cbefa6
Benjamin Egelund-Müller <b@egelund-muller.com>
2024-04-08 18:48:31 +0200

    Runtime: add is_default_database_schema to OLAPListTables

--- web-common/src/runtime-client/gen/index.schemas.ts
+++ web-common/src/runtime-client/gen/index.schemas.ts
@@ -661,6 +661,8 @@ export interface V1TableRowsRequest {
 export interface V1TableInfo {
   database?: string;
   databaseSchema?: string;
+  isDefaultDatabase?: boolean;
+  isDefaultDatabaseSchema?: boolean;
   name?: string;
   hasUnsupportedDataTypes?: boolean;
 }
@@ -2235,7 +2237,7 @@ export interface Runtimev1CharLocation {
  * `NullValue` is a singleton enumeration to represent the null value for the
 `Value` type union.
 
-The JSON representation for `NullValue` is JSON `null`.
+ The JSON representation for `NullValue` is JSON `null`.
 
  - NULL_VALUE: Null value.
  */
