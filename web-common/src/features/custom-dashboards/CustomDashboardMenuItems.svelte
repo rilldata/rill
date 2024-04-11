20380570a995119f9856f16c708e055ff03fcfa9
Brian Holmes <120223836+briangregoryholmes@users.noreply.github.com>
2024-04-11 14:16:13 -0400

    Fix: Navigate after delete (#4586)

--- web-common/src/features/custom-dashboards/CustomDashboardMenuItems.svelte
+++ web-common/src/features/custom-dashboards/CustomDashboardMenuItems.svelte
@@ -5,18 +5,30 @@
   import { EntityType } from "../entity-management/types";
   import { useCustomDashboardRoutes } from "./selectors";
   import * as DropdownMenu from "@rilldata/web-common/components/dropdown-menu/";
+  import { goto } from "$app/navigation";
+  import { getNextRoute } from "../models/utils/navigate-to-next";
 
   export let customDashboardName: string;
+  export let open: boolean;
 
-  $: customDashboardRoutes = useCustomDashboardRoutes($runtime.instanceId);
+  $: customDashboardRoutesQuery = useCustomDashboardRoutes($runtime.instanceId);
+  $: customDashboardRoutes = $customDashboardRoutesQuery.data ?? [];
 
   async function handleDeleteCustomDashboard() {
-    await deleteFileArtifact(
-      $runtime.instanceId,
-      getFileAPIPathFromNameAndType(customDashboardName, EntityType.Dashboard),
-      EntityType.Dashboard,
-      $customDashboardRoutes.data ?? [],
-    );
+    try {
+      await deleteFileArtifact(
+        $runtime.instanceId,
+        getFileAPIPathFromNameAndType(
+          customDashboardName,
+          EntityType.Dashboard,
+        ),
+        EntityType.Dashboard,
+      );
+
+      if (open) await goto(getNextRoute(customDashboardRoutes));
+    } catch (error) {
+      console.error(error);
+    }
   }
 </script>
 
--- web-common/src/features/entity-management/name-utils.ts
+++ web-common/src/features/entity-management/name-utils.ts
@@ -29,18 +29,6 @@ export function getName(name: string, others: string[]): string {
   return result;
 }
 
-export function getNextEntityName(
-  entityNames: Array<string>,
-  entityName: string,
-): string {
-  const idx = entityNames.indexOf(entityName);
-  if (idx <= 0) {
-    return entityNames[idx + 1];
-  } else {
-    return entityNames[idx - 1];
-  }
-}
-
 export function isDuplicateName(
   name: string,
   fromName: string,
