e846b9586aac30528c527964cc4e829b7f1c15c6
Anshul Khandelwal <12948312+k-anshul@users.noreply.github.com>
2024-04-12 21:06:20 +0530

    Runtime : Detach duckdb files with `tx` lock (#4590)
    * detach with tx
    
    * detach with tx
    
    * whole rename should be with tx = true
    
    * cast_to_enum fix
    
    * remove file even if attach failed
    
    * fix duckdb to duckdb transfer

--- runtime/reconcilers/util.go
+++ runtime/reconcilers/util.go
@@ -12,7 +12,6 @@ import (
 	compilerv1 "github.com/rilldata/rill/runtime/compilers/rillv1"
 	"github.com/rilldata/rill/runtime/drivers"
 	"github.com/robfig/cron/v3"
-	"go.uber.org/zap"
 )
 
 // checkRefs checks that all refs exist, are idle, and have no errors.
@@ -169,35 +168,6 @@ func olapForceRenameTable(ctx context.Context, c *runtime.Controller, connector,
 
 	// Do the rename
 	return olap.RenameTable(ctx, fromName, toName, fromIsView)
-}
-
-func logTableNameAndType(ctx context.Context, c *runtime.Controller, connector, name string) {
-	olap, release, err := c.AcquireOLAP(ctx, connector)
-	if err != nil {
-		c.Logger.Warn("LogTableNameAndType: failed to acquire OLAP", zap.Error(err))
-		return
-	}
-	defer release()
-
-	res, err := olap.Execute(context.Background(), &drivers.Statement{Query: "SELECT column_name, data_type FROM information_schema.columns WHERE table_name=? ORDER BY column_name ASC", Args: []any{name}})
-	if err != nil {
-		c.Logger.Warn("LogTableNameAndType: failed information_schema.columns", zap.Error(err))
-		return
-	}
-	defer res.Close()
-
-	colTyp := make([]string, 0)
-	var col, typ string
-	for res.Next() {
-		err = res.Scan(&col, &typ)
-		if err != nil {
-			c.Logger.Warn("LogTableNameAndType: failed scan", zap.Error(err))
-			return
-		}
-		colTyp = append(colTyp, fmt.Sprintf("%s:%s", col, typ))
-	}
-
-	c.Logger.Info("LogTableNameAndType: ", zap.String("name", name), zap.String("schema", strings.Join(colTyp, ", ")))
 }
 
 func resolveTemplatedProps(ctx context.Context, c *runtime.Controller, self compilerv1.TemplateResource, props map[string]any) (map[string]any, error) {
