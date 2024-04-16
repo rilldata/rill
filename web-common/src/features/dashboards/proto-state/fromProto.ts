d297c2c6a7498c8235d454ab8d7f67ebdebc3fbd
Brian Holmes <120223836+briangregoryholmes@users.noreply.github.com>
2024-04-16 03:32:19 -0400

    Fix: Scrub range not resetting on back navigation (#4592)
    * initial commit
    
    * set state to undefined
    
    * revert

--- web-common/src/features/dashboards/proto-state/fromProto.ts
+++ web-common/src/features/dashboards/proto-state/fromProto.ts
@@ -133,6 +133,9 @@ export function getDashboardStateFromProto(
     entity.lastDefinedScrubRange = fromTimeRangeProto(
       dashboard.scrubRange,
     ) as ScrubRange;
+  } else {
+    entity.selectedScrubRange = undefined;
+    entity.lastDefinedScrubRange = undefined;
   }
 
   if (dashboard.leaderboardMeasure) {
