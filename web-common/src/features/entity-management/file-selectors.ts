b344624ae367b18102f5fbf4f0866f058e7d2d46
Aditya Hegde <adityahegderocks@gmail.com>
2024-04-17 11:45:27 +0530

    Fix paths with leading slash

--- web-common/src/features/entity-management/file-selectors.ts
+++ web-common/src/features/entity-management/file-selectors.ts
@@ -151,16 +151,19 @@ export function useFileNamesInDirectory(
   instanceId: string,
   directoryPath: string,
 ) {
-  if (directoryPath === "") directoryPath = "/";
+  directoryPath += "/";
   return createRuntimeServiceListFiles(instanceId, undefined, {
     query: {
       select: (data) => {
         const files = data.files?.filter(
           (file) => !file.isDir && file.path?.startsWith(directoryPath),
         );
-        const fileNames = files?.map((file) => {
-          return file.path?.replace(directoryPath, "") ?? "";
-        });
+        const fileNames = files
+          ?.map((file) => {
+            return file.path?.replace(directoryPath, "") ?? "";
+          })
+          // filter out files in subdirectories
+          .filter((filePath) => !filePath.includes("/"));
         const sortedFileNames = fileNames?.sort((fileNameA, fileNameB) =>
           fileNameA.localeCompare(fileNameB, undefined, {
             sensitivity: "base",
@@ -176,19 +179,23 @@ export function useDirectoryNamesInDirectory(
   instanceId: string,
   directoryPath: string,
 ) {
-  if (directoryPath === "") directoryPath = "/";
+  directoryPath += "/";
   return createRuntimeServiceListFiles(instanceId, undefined, {
     query: {
       select: (data) => {
-        const files = data.files?.filter(
-          (file) =>
-            file.isDir &&
-            file.path?.startsWith(directoryPath) &&
-            file.path !== directoryPath,
-        );
-        const directoryNames = files?.map((file) => {
-          return file.path?.replace(directoryPath, "") ?? "";
-        });
+        const files =
+          data.files?.filter(
+            (file) =>
+              file.isDir &&
+              file.path?.startsWith(directoryPath) &&
+              file.path !== directoryPath,
+          ) ?? [];
+        const directoryNames = files
+          ?.map((file) => {
+            return file.path?.replace(directoryPath, "") ?? "";
+          })
+          // filter out dirs in subdirectories
+          .filter((filePath) => !filePath.includes("/"));
         const sortedDirectoryNames = directoryNames?.sort(
           (dirNameA, dirNameB) =>
             dirNameA.localeCompare(dirNameB, undefined, {
