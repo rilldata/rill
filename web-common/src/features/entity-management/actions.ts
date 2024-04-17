85bd8519dc736059ab1eacf6fd54aa5bfdf72895
Aditya Hegde <adityahegderocks@gmail.com>
2024-04-18 04:35:20 +0530

    Fix flicker during rename (#4629)

--- web-common/src/features/entity-management/actions.ts
+++ web-common/src/features/entity-management/actions.ts
@@ -1,21 +1,37 @@
 import { notifications } from "@rilldata/web-common/components/notifications";
+import { fileArtifacts } from "@rilldata/web-common/features/entity-management/file-artifacts";
+import { fileIsMainEntity } from "@rilldata/web-common/features/entity-management/file-selectors";
 import { extractFileName } from "@rilldata/web-common/features/sources/extract-file-name";
 import {
   runtimeServiceDeleteFile,
   runtimeServiceRenameFile,
 } from "@rilldata/web-common/runtime-client";
 import { httpRequestQueue } from "@rilldata/web-common/runtime-client/http-client";
-import { removeLeadingSlash } from "./entity-mappers";
+import { addLeadingSlash, removeLeadingSlash } from "./entity-mappers";
+import { get } from "svelte/store";
 
 export async function renameFileArtifact(
   instanceId: string,
   fromPath: string,
   toPath: string,
 ) {
-  // fromPath = removeLeadingSlash(fromPath);
   const fromName = extractFileName(fromPath);
-  // toPath = removeLeadingSlash(toPath);
   const toName = extractFileName(toPath);
+
+  if (fileIsMainEntity(fromPath)) {
+    // try and copy over kind+name proactively for main entities (.sql,.yml,.yaml)
+    // this fixes a flicker when renamed
+    const fromFileArtifact = fileArtifacts.getFileArtifact(
+      addLeadingSlash(fromPath),
+    );
+    const toFileArtifact = fileArtifacts.getFileArtifact(
+      addLeadingSlash(toPath),
+    );
+    if (!get(toFileArtifact.name)) {
+      // if there is no name set yet copy over from the source
+      toFileArtifact.name.set(get(fromFileArtifact.name));
+    }
+  }
 
   try {
     await runtimeServiceRenameFile(instanceId, {
