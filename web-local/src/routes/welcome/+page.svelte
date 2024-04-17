8fde8a6b2096558c400e8c242549443468d4454b
Brian Holmes <briangregoryholmes@gmail.com>
2024-04-16 20:10:04 -0400

    not necessary for pr

--- web-local/src/routes/welcome/+page.svelte
+++ web-local/src/routes/welcome/+page.svelte
@@ -1,31 +1,5 @@
 <script lang="ts">
-  import { fly } from "svelte/transition";
-  import ProjectCards from "@rilldata/web-common/features/welcome/ProjectCards.svelte";
-  import TitleContent from "@rilldata/web-common/features/welcome/TitleContent.svelte";
-  import UserTestCta from "@rilldata/web-common/features/welcome/UserTestCTA.svelte";
+  import Welcome from "@rilldata/web-common/features/welcome/Welcome.svelte";
 </script>
 
-<div class="scroll" in:fly={{ duration: 1600, delay: 400, y: 8 }}>
-  <div class="wrapper column p-10 2xl:py-16">
-    <TitleContent />
-    <div class="column" in:fly={{ duration: 1600, delay: 1200, y: 4 }}>
-      <ProjectCards />
-      <UserTestCta />
-    </div>
-  </div>
-</div>
-
-<style lang="postcss">
-  .scroll {
-    @apply size-full overflow-x-hidden overflow-y-auto;
-  }
-
-  .wrapper {
-    @apply w-screen h-fit min-h-screen bg-no-repeat bg-cover;
-    background-image: url("$img/welcome-bg-art.png");
-  }
-
-  .column {
-    @apply flex flex-col items-center gap-y-6;
-  }
-</style>
+<Welcome />
