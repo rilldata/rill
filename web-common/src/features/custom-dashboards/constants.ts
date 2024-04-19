bd18f91e5b0416402ca6ba1f77f9a79778f22437
Brian Holmes <120223836+briangregoryholmes@users.noreply.github.com>
2024-04-19 13:19:19 -0400

    init commit (#4671)

--- web-common/src/features/custom-dashboards/constants.ts
+++ web-common/src/features/custom-dashboards/constants.ts
@@ -1,2 +1,7 @@
-export const DEFAULT_WIDTH = 800;
-export const DEFAULT_RADIUS = 0.08;
+export const DASHBOARD_WIDTH = 800;
+export const COMPONENT_RADIUS = 0.08;
+export const FONT_SIZE = 40;
+export const COMPONENT_WIDTH = 1;
+export const COMPONENT_HEIGHT = 1;
+export const COLUMN_COUNT = 20;
+export const GAP_SIZE = 4;
