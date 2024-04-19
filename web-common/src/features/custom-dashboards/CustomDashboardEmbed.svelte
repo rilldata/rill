b4df84d30a68e44e2fd79c194947c67f2923f3f2
Brian Holmes <briangregoryholmes@gmail.com>
2024-04-19 12:51:39 -0400

    init commit

--- web-common/src/features/custom-dashboards/CustomDashboardEmbed.svelte
+++ web-common/src/features/custom-dashboards/CustomDashboardEmbed.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
   import type { V1DashboardComponent } from "@rilldata/web-common/runtime-client";
   import Chart from "./Chart.svelte";
-  import { DEFAULT_WIDTH, DEFAULT_RADIUS } from "./constants";
+  import * as defaults from "./constants";
   import Wrapper from "./Wrapper.svelte";
   import Component from "./Component.svelte";
+  import Markdown from "./Markdown.svelte";
 
   export let columns = 20;
   export let components: V1DashboardComponent[];
@@ -13,10 +14,10 @@
   let contentRect: DOMRectReadOnly = new DOMRectReadOnly(0, 0, 0, 0);
 
   $: gridWidth = contentRect.width;
-  $: scale = gridWidth / DEFAULT_WIDTH;
-  $: gapSize = DEFAULT_WIDTH * (gap / 1000);
-  $: gridCell = DEFAULT_WIDTH / columns;
-  $: radius = gridCell * DEFAULT_RADIUS;
+  $: scale = gridWidth / defaults.DASHBOARD_WIDTH;
+  $: gapSize = defaults.DASHBOARD_WIDTH * (gap / 1000);
+  $: gridCell = defaults.DASHBOARD_WIDTH / columns;
+  $: radius = gridCell * defaults.COMPONENT_RADIUS;
 
   $: maxBottom = components.reduce((max, el) => {
     const bottom = Number(el.height) + Number(el.y);
@@ -26,27 +27,32 @@
 
 <Wrapper
   {scale}
-  width={DEFAULT_WIDTH}
+  width={defaults.DASHBOARD_WIDTH}
   height={maxBottom * gridCell}
   bind:contentRect
   color="bg-slate-50"
 >
   {#each components as component, i (i)}
-    {#if component.chart && component.width && component.height}
-      <Component
-        {chartView}
-        embed
-        {i}
-        {scale}
-        {radius}
-        padding={gapSize}
-        width={Number(component.width) * gridCell}
-        height={Number(component.height) * gridCell}
-        left={Number(component.x) * gridCell}
-        top={Number(component.y) * gridCell}
-      >
-        <Chart chartName={component.chart} {chartView} />
-      </Component>
-    {/if}
+    <Component
+      {chartView}
+      embed
+      {i}
+      {scale}
+      {radius}
+      padding={gapSize}
+      width={Number(component.width ?? defaults.COMPONENT_WIDTH) * gridCell}
+      height={Number(component.height ?? defaults.COMPONENT_HEIGHT) * gridCell}
+      left={Number(component.x) * gridCell}
+      top={Number(component.y) * gridCell}
+    >
+      {#if component.markdown}
+        <Markdown
+          markdown={component.markdown}
+          fontSize={component.fontSize ?? defaults.FONT_SIZE}
+        />
+      {:else if component.chart}
+        <Chart chartName={component.chart} />
+      {/if}
+    </Component>
   {/each}
 </Wrapper>
