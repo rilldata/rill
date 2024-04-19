6750410beca4645f5e9de6bf37673247afca9b51
Benjamin Egelund-Müller <b@egelund-muller.com>
2024-04-19 17:25:10 +0200

    Runtime: Prevent concurrent repo reads while cloning/pulling

--- runtime/drivers/admin/repo.go
+++ runtime/drivers/admin/repo.go
@@ -21,10 +21,11 @@ func (h *Handle) Root() string {
 }
 
 func (h *Handle) CommitHash(ctx context.Context) (string, error) {
-	err := h.cloneOrPull(ctx, true)
+	err := h.rlockEnsureCloned(ctx)
 	if err != nil {
 		return "", err
 	}
+	defer h.repoMu.RUnlock()
 
 	repo, err := git.PlainOpen(h.repoPath)
 	if err != nil {
@@ -44,10 +45,11 @@ func (h *Handle) CommitHash(ctx context.Context) (string, error) {
 }
 
 func (h *Handle) ListRecursive(ctx context.Context, glob string, skipDirs bool) ([]drivers.DirEntry, error) {
-	err := h.cloneOrPull(ctx, true)
+	err := h.rlockEnsureCloned(ctx)
 	if err != nil {
 		return nil, err
 	}
+	defer h.repoMu.RUnlock()
 
 	fsRoot := os.DirFS(h.projPath)
 	glob = path.Clean(path.Join("./", glob))
@@ -80,10 +82,11 @@ func (h *Handle) ListRecursive(ctx context.Context, glob string, skipDirs bool) 
 }
 
 func (h *Handle) Get(ctx context.Context, filePath string) (string, error) {
-	err := h.cloneOrPull(ctx, true)
+	err := h.rlockEnsureCloned(ctx)
 	if err != nil {
 		return "", err
 	}
+	defer h.repoMu.RUnlock()
 
 	filePath = filepath.Join(h.projPath, filePath)
 
@@ -96,10 +99,11 @@ func (h *Handle) Get(ctx context.Context, filePath string) (string, error) {
 }
 
 func (h *Handle) Stat(ctx context.Context, filePath string) (*drivers.RepoObjectStat, error) {
-	err := h.cloneOrPull(ctx, true)
+	err := h.rlockEnsureCloned(ctx)
 	if err != nil {
 		return nil, err
 	}
+	defer h.repoMu.RUnlock()
 
 	filePath = filepath.Join(h.projPath, filePath)
 
@@ -131,7 +135,13 @@ func (h *Handle) Delete(ctx context.Context, filePath string) error {
 }
 
 func (h *Handle) Sync(ctx context.Context) error {
-	return h.cloneOrPull(ctx, false)
+	err := h.repoMu.Lock(ctx)
+	if err != nil {
+		return err
+	}
+	defer h.repoMu.Unlock()
+
+	return h.cloneOrPull(ctx)
 }
 
 func (h *Handle) Watch(ctx context.Context, callback drivers.WatchCallback) error {
