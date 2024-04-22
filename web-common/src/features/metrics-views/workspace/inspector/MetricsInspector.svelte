f268991e01dad1a61dab6f3d7cffb884e2e5dca7
Eric P Green <ericpgreen2@gmail.com>
2024-04-21 20:36:21 -0700

    Fix lint / cleanup file

--- web-common/src/features/metrics-views/workspace/inspector/MetricsInspector.svelte
+++ web-common/src/features/metrics-views/workspace/inspector/MetricsInspector.svelte
@@ -1,54 +1,41 @@
 <script lang="ts">
-  import {
-    useModel,
-    useModels,
-  } from "@rilldata/web-common/features/models/selectors";
+  import { useModel } from "@rilldata/web-common/features/models/selectors";
+  import WorkspaceInspector from "@rilldata/web-common/features/sources/inspector/WorkspaceInspector.svelte";
   import {
     V1Resource,
     createRuntimeServiceGetFile,
   } from "@rilldata/web-common/runtime-client";
   import { runtime } from "@rilldata/web-common/runtime-client/runtime-store";
+  import type { CreateQueryResult } from "@tanstack/svelte-query";
   import {
     getModelOutOfPossiblyMalformedYAML,
     getTableOutOfPossiblyMalformedYAML,
   } from "../../utils";
-  import WorkspaceInspector from "@rilldata/web-common/features/sources/inspector/WorkspaceInspector.svelte";
 
   export let filePath: string;
 
+  // Get model/table name from YAML
   $: fileQuery = createRuntimeServiceGetFile($runtime.instanceId, filePath);
   $: yaml = $fileQuery.data?.blob || "";
-
-  // get file.
   $: modelName = getModelOutOfPossiblyMalformedYAML(yaml)?.replace(/"/g, "");
   $: tableName = getTableOutOfPossiblyMalformedYAML(yaml)?.replace(/"/g, "");
 
-  // check to see if this model name exists.
-  $: modelQuery = useModel($runtime.instanceId, modelName ?? "");
-
-  $: allModels = useModels($runtime.instanceId);
-
-  let isValidModel = false;
-  $: if ($allModels?.data?.entries) {
-    isValidModel = $allModels?.data.some(
-      (model) => model?.meta?.name?.name === modelName,
-    );
+  let modelQuery: CreateQueryResult<V1Resource>;
+  $: if (modelName) {
+    modelQuery = useModel($runtime.instanceId, modelName);
   }
-
-  let entry: V1Resource;
-  // refresh entry value only if the data has changed
-  $: entry = $modelQuery?.data || entry;
 </script>
 
-{#if modelName && !$modelQuery?.isError && isValidModel && entry}
+{#if modelName && $modelQuery.data && $modelQuery.data?.model?.state?.connector}
   <WorkspaceInspector
+    connector={$modelQuery.data?.model?.state?.connector}
+    tableName={modelName}
+    model={$modelQuery.data?.model}
     hasErrors={false}
     showReferences={false}
     sourceIsReconciling={false}
     hasUnsavedChanges={false}
     showSummaryTitle
-    model={entry?.model}
-    tableName={modelName}
   />
 {:else}
   <div
