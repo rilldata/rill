4bc8596b94c290bbea0843096aac9f00f66053d1
Egor Riashin <egor-ryashin@users.noreply.github.com>
2024-04-28 19:14:42 +0300

    DuckDb HAVING clause with the same alias name (#4696)
    * duckdb HAVING clause with the same alias name
    
    * duckdb HAVING clause with the same alias name
    
    * refactoring
    
    * refactoring
    
    * refactoring
    
    * refactoring
    
    * refactoring
    
    ---------
    
    Co-authored-by: Egor Ryashin <egor.ryashin@rilldata.com>

--- runtime/queries/metricsview_toplist_test.go
+++ runtime/queries/metricsview_toplist_test.go
@@ -106,3 +106,66 @@ func TestMetricsViewsToplist_measure_filters(t *testing.T) {
 	require.Equal(t, "news.google.com", q.Result.Data[1].AsMap()["dom"])
 	require.Equal(t, "instagram.com", q.Result.Data[2].AsMap()["dom"])
 }
+
+func TestMetricsViewsToplist_measure_filters_same_name(t *testing.T) {
+	rt, instanceID := testruntime.NewInstanceForProject(t, "ad_bids")
+
+	ctr := &queries.ColumnTimeRange{
+		TableName:  "ad_bids",
+		ColumnName: "timestamp",
+	}
+	err := ctr.Resolve(context.Background(), rt, instanceID, 0)
+	require.NoError(t, err)
+	diff := ctr.Result.Max.AsTime().Sub(ctr.Result.Min.AsTime())
+	maxTime := ctr.Result.Min.AsTime().Add(diff / 2)
+
+	ctrl, err := rt.Controller(context.Background(), instanceID)
+	require.NoError(t, err)
+	r, err := ctrl.Get(context.Background(), &runtimev1.ResourceName{Kind: runtime.ResourceKindMetricsView, Name: "ad_bids_metrics"}, false)
+	require.NoError(t, err)
+	mv := r.GetMetricsView()
+
+	lmt := int64(250)
+	q := &queries.MetricsViewToplist{
+		MetricsViewName: "ad_bids_metrics",
+		DimensionName:   "dom",
+		MeasureNames:    []string{"bid_price"},
+		MetricsView:     mv.Spec,
+		TimeStart:       ctr.Result.Min,
+		TimeEnd:         timestamppb.New(maxTime),
+		Having: &runtimev1.Expression{
+			Expression: &runtimev1.Expression_Cond{
+				Cond: &runtimev1.Condition{
+					Op: runtimev1.Operation_OPERATION_GT,
+					Exprs: []*runtimev1.Expression{
+						{
+							Expression: &runtimev1.Expression_Ident{
+								Ident: "bid_price",
+							},
+						},
+						{
+							Expression: &runtimev1.Expression_Val{
+								Val: structpb.NewNumberValue(3.25),
+							},
+						},
+					},
+				},
+			},
+		},
+		Sort: []*runtimev1.MetricsViewSort{
+			{
+				Name:      "bid_price",
+				Ascending: false,
+			},
+		},
+		Limit: &lmt,
+	}
+
+	err = q.Resolve(context.Background(), rt, instanceID, 0)
+	require.NoError(t, err)
+	require.NotEmpty(t, q.Result)
+	require.Len(t, q.Result.Data, 3)
+	require.Equal(t, "sports.yahoo.com", q.Result.Data[0].AsMap()["dom"])
+	require.Equal(t, "news.google.com", q.Result.Data[1].AsMap()["dom"])
+	require.Equal(t, "instagram.com", q.Result.Data[2].AsMap()["dom"])
+}
