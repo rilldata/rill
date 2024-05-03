b069d1cdb3e71048fee0b05cea6cddc45aa39f50
Dhiraj Barnwal <sumankumaribarnwal@gmail.com>
2024-05-03 14:06:54 +0530

    Disable stacked charts for no comparison (#4804)
    * Disable stacked charts for no comparison
    
    * Switch to default when not available
    
    * Update disabled color

--- web-common/src/features/dashboards/time-dimension-details/charts/ChartTypeSelector.svelte
+++ web-common/src/features/dashboards/time-dimension-details/charts/ChartTypeSelector.svelte
@@ -9,10 +9,10 @@
 
   export let metricViewName: string;
   export let chartType: TDDChart;
+  export let hasComparison: boolean;
 
-  function handleChartTypeChange(type: TDDChart) {
-    metricsExplorerStore.setTDDChartType(metricViewName, type);
-  }
+  const comparisonCharts = [TDDChart.STACKED_AREA, TDDChart.STACKED_BAR];
+
   const chartTypeTabs = [
     {
       label: "Line",
@@ -35,20 +35,32 @@
       Icon: StackedBar,
     },
   ];
+
+  function handleChartTypeChange(type: TDDChart, isDisabled: boolean) {
+    if (isDisabled) return;
+    metricsExplorerStore.setTDDChartType(metricViewName, type);
+  }
+
+  // switch to default if current selected chart is not available
+  $: if (!hasComparison && comparisonCharts.includes(chartType)) {
+    metricsExplorerStore.setTDDChartType(metricViewName, TDDChart.DEFAULT);
+  }
 </script>
 
 <div class="chart-type-selector">
   {#each chartTypeTabs as { label, id, Icon } (label)}
     {@const active = chartType === id}
+    {@const disabled = !hasComparison && comparisonCharts.includes(id)}
     <div class:bg-primary-100={active} class="chart-icon-wrapper">
       <IconButton
+        {disabled}
         disableHover
         tooltipLocation="top"
-        on:click={() => handleChartTypeChange(id)}
+        on:click={() => handleChartTypeChange(id, disabled)}
       >
         <Icon
-          primaryColor="var(--color-primary-700)"
-          secondaryColor="var(--color-primary-300)"
+          primaryColor={disabled ? "#9CA3AF" : "var(--color-primary-700)"}
+          secondaryColor={disabled ? "#CBD5E1" : "var(--color-primary-300)"}
           size="20px"
         />
         <svelte:fragment slot="tooltip-content">
