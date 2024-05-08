036b81b18e247f57ad0f5debcaa994d2547234f1
Eric P Green <ericpgreen2@gmail.com>
2024-05-08 12:35:31 -0700

    Use env variable

--- web-admin/src/features/help/initPylonChat.ts
+++ web-admin/src/features/help/initPylonChat.ts
@@ -1,10 +1,11 @@
 import type { V1User } from "../../client";
 
+const appId = import.meta.env.RILL_UI_PUBLIC_PYLON_APP_ID as string;
+
 export function initPylonChat(user: V1User) {
   window.pylon = {
     chat_settings: {
-      // TODO: get the APP_ID from an environment variable
-      app_id: "26a0fdd2-3bd3-41e2-82bc-1b35a444729f",
+      app_id: appId,
       email: user.email,
       name: user.displayName,
       avatar_url: user.photoUrl,
