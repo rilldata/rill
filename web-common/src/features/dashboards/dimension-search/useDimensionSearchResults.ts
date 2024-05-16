6f6fd08904eaf07641d64120f83f4943c94757bf
Aditya Hegde <adityahegderocks@gmail.com>
2024-05-16 16:05:41 +0530

    Add caching

--- web-common/src/features/dashboards/dimension-search/useDimensionSearchResults.ts
+++ web-common/src/features/dashboards/dimension-search/useDimensionSearchResults.ts
@@ -1,6 +1,26 @@
 import { createLikeExpression } from "@rilldata/web-common/features/dashboards/stores/filter-utils";
-import { V1MetricsViewSpec } from "@rilldata/web-common/runtime-client";
-import { createQueryServiceStreamingQueryBatch } from "@rilldata/web-common/runtime-client/createQueryServiceStreamingQueryBatch";
+import {
+  createQueryServiceMetricsViewAggregation,
+  MetricsViewSpecDimensionV2,
+  V1MetricsViewSpec,
+} from "@rilldata/web-common/runtime-client";
+import { StreamingQueryBatch } from "@rilldata/web-common/runtime-client/StreamingQueryBatch";
+import { derived } from "svelte/store";
+
+export type DimensionSearchResults = {
+  responses: (
+    | {
+        dimension: string;
+        values: any[];
+      }
+    | undefined
+  )[];
+  errors: Error[];
+  completed: boolean;
+  progress: number;
+};
+
+const batch = new StreamingQueryBatch(100);
 
 export function useDimensionSearchResults(
   instanceId: string,
@@ -9,24 +29,80 @@ export function useDimensionSearchResults(
   searchText: string,
 ) {
   const dimensions = metricsView.dimensions ?? [];
-  return createQueryServiceStreamingQueryBatch(
+  return derived(
+    dimensions.map((dimension) =>
+      getValuesForDimension(instanceId, metricsViewName, dimension, searchText),
+    ),
+    (dimensionsValues) => {
+      const results: DimensionSearchResults = {
+        responses: new Array(dimensionsValues.length),
+        errors: [],
+        completed: false,
+        progress: 0,
+      };
+
+      let completedCount = 0;
+      dimensionsValues.forEach((dimensionValues, index) => {
+        results.responses[index] = dimensionValues.data;
+        if (dimensionValues.error) {
+          results.errors.push(
+            new Error(
+              dimensionValues.error.response?.data?.message ??
+                dimensionValues.error.message ??
+                "Unknown error",
+            ),
+          );
+        }
+        if (!dimensionValues.isFetching) {
+          completedCount++;
+        }
+      });
+      results.completed = completedCount === dimensionsValues.length;
+      results.progress = Math.round(
+        (completedCount * 100) / dimensionsValues.length,
+      );
+
+      return results;
+    },
+  );
+}
+
+function getValuesForDimension(
+  instanceId: string,
+  metricsViewName: string,
+  dimension: MetricsViewSpecDimensionV2,
+  searchText: string,
+) {
+  const dimensionName = dimension.name ?? "";
+  return createQueryServiceMetricsViewAggregation(
     instanceId,
-    dimensions?.map((d) => ({
-      metricsViewAggregationRequest: {
-        instanceId,
-        metricsView: metricsViewName,
-        dimensions: [{ name: d.name }],
-        measures: [],
-        where: createLikeExpression(d.name ?? "", `%${searchText}%`),
-        limit: "100",
+    metricsViewName,
+    {
+      dimensions: [{ name: dimensionName }],
+      measures: [],
+      where: createLikeExpression(dimensionName, `%${searchText}%`),
+      limit: "100",
+    },
+    {
+      query: {
+        queryFn: ({ signal }) =>
+          batch.fetch(
+            "metricsViewAggregation",
+            {
+              instanceId,
+              metricsView: metricsViewName,
+              dimensions: [{ name: dimensionName }],
+              measures: [],
+              where: createLikeExpression(dimensionName, `%${searchText}%`),
+              limit: "100",
+            },
+            signal,
+          ),
+        select: (resp) => ({
+          dimension: dimensionName,
+          values: resp?.data?.map((d) => d[dimensionName]) ?? [],
+        }),
       },
-    })) ?? [],
-    (resp, index) => ({
-      dimension: dimensions[index]?.name ?? "",
-      values:
-        resp.metricsViewAggregationResponse?.data?.map(
-          (d) => d[dimensions[index]?.name ?? ""],
-        ) ?? [],
-    }),
+    },
   );
 }
---
+++ web-common/src/runtime-client/StreamingQueryBatch.ts
@@ -0,0 +1,106 @@
+import { streamingFetchWrapper } from "@rilldata/web-common/runtime-client/fetch-streaming-wrapper";
+import {
+  QueryServiceQueryBatchBody,
+  V1Query,
+  V1QueryBatchResponse,
+  V1QueryResult,
+} from "@rilldata/web-common/runtime-client/gen/index.schemas";
+import { runtime } from "@rilldata/web-common/runtime-client/runtime-store";
+import { get } from "svelte/store";
+
+type BatchQueryEntry<Type extends BatchQueryTypes> = {
+  query: V1Query;
+  signal: AbortSignal | undefined;
+  resolve: (resp: V1QueryResult[`${Type}Response`], index: number) => void;
+  reject: (err: Error) => void;
+  type: BatchQueryTypes;
+};
+
+/**
+ * Extracts the query types supported in BatchQuery.
+ */
+type BatchQueryTypes = keyof V1Query extends `${infer Args}Request`
+  ? Args
+  : never;
+
+export class StreamingQueryBatch {
+  private timer: ReturnType<typeof setTimeout> | undefined;
+  private queryEntries: BatchQueryEntry<any>[] = [];
+
+  public constructor(private readonly batchPeriod: number) {}
+
+  public fetch<Type extends BatchQueryTypes>(
+    type: Type,
+    request: V1Query[`${Type}Request`],
+    signal: AbortSignal | undefined,
+  ): Promise<NonNullable<V1QueryResult[`${Type}Response`]>> {
+    return new Promise((resolve, reject) => {
+      this.queryEntries.push({
+        query: {
+          [`${type}Request`]: request,
+        },
+        signal,
+        resolve,
+        reject,
+        type,
+      });
+
+      if (this.timer) return;
+      this.timer = setTimeout(() => {
+        void this.runBatch();
+        this.timer = undefined;
+      }, this.batchPeriod);
+    });
+  }
+
+  private async runBatch() {
+    const runtimeState = get(runtime);
+    const queries = this.queryEntries;
+    this.queryEntries = [];
+
+    const headers = { "Content-Type": "application/json" };
+    const jwt = runtimeState.jwt;
+    if (jwt) {
+      headers["Authorization"] = `Bearer ${jwt.token}`;
+    }
+
+    const body: QueryServiceQueryBatchBody = {
+      queries: [],
+    };
+    // create a single abort controller that cancels if any signal is cancelled.
+    const controller = new AbortController();
+    queries.forEach(({ query, signal }) => {
+      body.queries?.push(query);
+      signal?.addEventListener("abort", () => {
+        controller.abort();
+      });
+    });
+
+    const stream = streamingFetchWrapper<{ result: V1QueryBatchResponse }>(
+      `${runtimeState.host}/v1/instances/${runtimeState.instanceId}/query/batch`,
+      "POST",
+      body,
+      headers,
+      controller.signal,
+    );
+
+    const seen = new Set<number>();
+    for await (const { result } of stream) {
+      if (result.index === undefined || !queries[result.index]) continue;
+      // index matches the request order. check batch_query.go
+      seen.add(result.index);
+      const entry = queries[result.index];
+      if (result.result) {
+        entry.resolve(result.result[`${entry.type}Response`], result.index);
+      } else {
+        entry.reject(new Error(result.error ?? "no response"));
+      }
+    }
+
+    // after the request if finished reject any missing responses
+    queries.forEach(({ reject }, i) => {
+      if (seen.has(i)) return;
+      reject(new Error("no response"));
+    });
+  }
+}
