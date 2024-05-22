7486f7ac0630ee8d1b992ad09ae79f6b7cf0bb99
Eric P Green <ericpgreen2@gmail.com>
2024-05-22 11:33:28 -0700

    Add more icons

--- web-common/src/features/connectors/connector-icon-mapping.ts
+++ web-common/src/features/connectors/connector-icon-mapping.ts
@@ -1,5 +1,7 @@
 import { SnowflakeIcon } from "lucide-svelte";
 import AmazonS3Icon from "../../components/icons/connectors/AmazonS3Icon.svelte";
+import ApacheDruidIcon from "../../components/icons/connectors/ApacheDruidIcon.svelte";
+import ApachePinotIcon from "../../components/icons/connectors/ApachePinotIcon.svelte";
 import ClickHouseIcon from "../../components/icons/connectors/ClickHouseIcon.svelte";
 import DuckDbIcon from "../../components/icons/connectors/DuckDBIcon.svelte";
 import GoogleBigQueryIcon from "../../components/icons/connectors/GoogleBigQueryIcon.svelte";
@@ -9,12 +11,13 @@ export const connectorIconMapping = {
   // TODO: azure
   bigquery: GoogleBigQueryIcon,
   clickhouse: ClickHouseIcon,
+  druid: ApacheDruidIcon,
   duckdb: DuckDbIcon,
   // TODO: gcs
   // TODO: mysql
-  s3: AmazonS3Icon,
-  snowflake: SnowflakeIcon,
-  // TODO: pinot
+  pinot: ApachePinotIcon,
   // TODO: postgres
   // TODO: redshift
+  s3: AmazonS3Icon,
+  snowflake: SnowflakeIcon,
 };
