ead1284959870c226ff0b8e77ddd3cf74c69c4b8
Aditya Hegde <adityahegderocks@gmail.com>
2024-05-30 11:26:17 +0530

    Add behind a feature flag

--- web-common/src/features/dashboards/dimension-search/GlobalDimensionSearch.svelte
+++ web-common/src/features/dashboards/dimension-search/GlobalDimensionSearch.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
   import { Button } from "@rilldata/web-common/components/button";
-  import CancelCircle from "@rilldata/web-common/components/icons/CancelCircle.svelte";
+  import Cancel from "@rilldata/web-common/components/icons/Cancel.svelte";
   import { Search } from "@rilldata/web-common/components/search";
   import GlobalDimensionSearchResults from "@rilldata/web-common/features/dashboards/dimension-search/GlobalDimensionSearchResults.svelte";
+  import { featureFlags } from "@rilldata/web-common/features/feature-flags";
   import { slideRight } from "@rilldata/web-common/lib/transitions";
   import SearchIcon from "@rilldata/web-common/components/icons/Search.svelte";
 
   export let metricsViewName: string;
+
+  const { dimensionSearch } = featureFlags;
 
   let searchBarOpen = false;
   let searchText = "";
@@ -23,39 +26,41 @@
   }
 </script>
 
-<div class="relative flex flex-row">
-  {#if searchBarOpen}
-    <div
-      transition:slideRight={{}}
-      class="flex items-center gap-x-2 pr-2 w-60 bg-slate-50 border border-primary-300"
-    >
-      <Search
-        bind:value={searchText}
-        on:submit={onSubmit}
-        placeholder="Search dimensions"
-        autofocus
-        border={false}
-        background={false}
-      />
-      <button class="ui-copy-icon" on:click={reset}>
-        <CancelCircle size="16px" />
-      </button>
-    </div>
-  {:else}
-    <Button
-      class="flex items-center gap-x-2 p-1.5 text-gray-700"
-      on:click={() => (searchBarOpen = !searchBarOpen)}
-      type="secondary"
-      compact
-    >
-      <SearchIcon size="16px" />
-    </Button>
-  {/if}
+{#if $dimensionSearch}
+  <div class="relative flex flex-row">
+    {#if searchBarOpen}
+      <div
+        transition:slideRight={{}}
+        class="flex items-center gap-x-2 pr-2 w-60 bg-slate-50 border border-primary-300"
+      >
+        <Search
+          bind:value={searchText}
+          on:submit={onSubmit}
+          placeholder="Search dimensions"
+          autofocus
+          border={false}
+          background={false}
+        />
+        <button class="ui-copy-icon" on:click={reset}>
+          <Cancel size="16px" />
+        </button>
+      </div>
+    {:else}
+      <Button
+        class="flex items-center gap-x-2 p-1.5 text-gray-700"
+        on:click={() => (searchBarOpen = !searchBarOpen)}
+        type="secondary"
+        compact
+      >
+        <SearchIcon size="16px" />
+      </Button>
+    {/if}
 
-  <GlobalDimensionSearchResults
-    {metricsViewName}
-    searchText={submittedSearchText}
-    onSelect={reset}
-    bind:open={searchResultsOpen}
-  />
-</div>
+    <GlobalDimensionSearchResults
+      {metricsViewName}
+      searchText={submittedSearchText}
+      onSelect={reset}
+      bind:open={searchResultsOpen}
+    />
+  </div>
+{/if}
