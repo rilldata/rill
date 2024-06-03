1f15e552a122e6c7c9df576845f31c334c1d933f
e.sevastyanov <eugene.sevastianov@rilldata.com>
2024-06-03 18:03:06 +0400

    Merged main

--- web-common/src/runtime-client/gen/index.schemas.ts
+++ web-common/src/runtime-client/gen/index.schemas.ts
@@ -2061,7 +2061,7 @@ export interface V1API {
  * `NullValue` is a singleton enumeration to represent the null value for the
 `Value` type union.
 
- The JSON representation for `NullValue` is JSON `null`.
+The JSON representation for `NullValue` is JSON `null`.
 
  - NULL_VALUE: Null value.
  */
