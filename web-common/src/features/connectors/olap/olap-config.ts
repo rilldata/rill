ea37f82095ba5ed536b0dc179d670bf8d642cbbd
Eric P Green <ericpgreen2@gmail.com>
2024-06-04 08:51:43 -0700

    Connectors: Handle arbitrary names (#5015)
    * Change routes to include the connector's `name`
    
    * Use the table name (not fully qualified name) in the workspace header
    
    * Include connector name in table identifier
    
    * Error handling
    
    * Fix link
    
    * Bugfix

--- web-common/src/features/connectors/olap/olap-config.ts
+++ web-common/src/features/connectors/olap/olap-config.ts
@@ -1,43 +1,42 @@
 export const OLAP_DRIVERS_WITHOUT_MODELING = ["clickhouse", "druid", "pinot"];
 
 export function makeFullyQualifiedTableName(
-  connector: string,
+  driver: string,
   database: string,
   databaseSchema: string,
   table: string,
 ) {
-  switch (connector) {
+  switch (driver) {
     case "clickhouse":
       return `${databaseSchema}.${table}`;
     case "druid":
       return `${databaseSchema}.${table}`;
     case "duckdb":
-      // return `${database}.${databaseSchema}.${table}`;
-      // For now, only show the table name
-      return table;
+      return `${database}.${databaseSchema}.${table}`;
     case "pinot":
       return table;
     default:
-      throw new Error(`Unsupported OLAP connector: ${connector}`);
+      throw new Error(`Unsupported OLAP connector: ${driver}`);
   }
 }
 
 export function makeTablePreviewHref(
-  connector: string,
+  driver: string,
+  connectorName: string,
   database: string,
   databaseSchema: string,
   table: string,
 ): string {
-  switch (connector) {
+  switch (driver) {
     case "clickhouse":
-      return `/connector/clickhouse/${databaseSchema}/${table}`;
+      return `/connector/clickhouse/${connectorName}/${databaseSchema}/${table}`;
     case "druid":
-      return `/connector/druid/${databaseSchema}/${table}`;
+      return `/connector/druid/${connectorName}/${databaseSchema}/${table}`;
     case "duckdb":
-      return `/connector/duckdb/${database}/${databaseSchema}/${table}`;
+      return `/connector/duckdb/${connectorName}/${database}/${databaseSchema}/${table}`;
     case "pinot":
-      return `/connector/pinot/${table}`;
+      return `/connector/pinot/${connectorName}/${table}`;
     default:
-      throw new Error(`Unsupported connector: ${connector}`);
+      throw new Error(`Unsupported connector: ${driver}`);
   }
 }
