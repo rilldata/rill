41f5bcfa83f64c85ef6820968a6030cda5d0994a
Brian Holmes <briangregoryholmes@gmail.com>
2024-06-04 02:09:44 -0400

    reorg, test fix

--- web-common/src/features/welcome/is-project-initialized.ts
+++ web-common/src/features/welcome/is-project-initialized.ts
@@ -1,9 +1,14 @@
 import {
   V1ListFilesResponse,
   getRuntimeServiceListFilesQueryKey,
+  runtimeServiceGetInstance,
   runtimeServiceListFiles,
+  runtimeServiceUnpackEmpty,
 } from "@rilldata/web-common/runtime-client";
 import { queryClient } from "@rilldata/web-common/lib/svelte-query/globalQueryClient";
+import { runtime } from "@rilldata/web-common/runtime-client/runtime-store";
+import { EMPTY_PROJECT_TITLE } from "./constants";
+import { get } from "svelte/store";
 
 export async function isProjectInitialized(instanceId: string) {
   const files = await queryClient.fetchQuery<V1ListFilesResponse>({
@@ -15,4 +20,27 @@ export async function isProjectInitialized(instanceId: string) {
 
   // Return true if `rill.yaml` exists, else false
   return files.files?.some((file) => file.path === "/rill.yaml");
+}
+
+export async function handleUninitializedProject() {
+  const instanceId = get(runtime).instanceId;
+  // If the project is not initialized, determine what page to route to dependent on the OLAP connector
+  const instance = await runtimeServiceGetInstance(instanceId, {
+    sensitive: true,
+  });
+  const olapConnector = instance.instance?.olapConnector;
+  if (!olapConnector) {
+    throw new Error("OLAP connector is not defined");
+  }
+
+  // DuckDB-backed projects should head to the Welcome page for user-guided initialization
+  if (olapConnector === "duckdb" && window.location.pathname !== "/welcome") {
+    window.location.replace("/welcome");
+  } else {
+    // Clickhouse and Druid-backed projects should be initialized immediately
+    await runtimeServiceUnpackEmpty(instanceId, {
+      title: EMPTY_PROJECT_TITLE,
+      force: true,
+    });
+  }
 }
--- web-common/src/runtime-client/runtime-store.ts
+++ web-common/src/runtime-client/runtime-store.ts
@@ -1,11 +1,8 @@
 import { get, writable } from "svelte/store";
-import { goto } from "$app/navigation";
 import {
-  runtimeServiceGetInstance,
-  runtimeServiceUnpackEmpty,
-} from "./gen/runtime-service/runtime-service";
-import { EMPTY_PROJECT_TITLE } from "../features/welcome/constants";
-import { isProjectInitialized } from "@rilldata/web-common/features/welcome/is-project-initialized";
+  isProjectInitialized,
+  handleUninitializedProject,
+} from "@rilldata/web-common/features/welcome/is-project-initialized";
 
 export interface JWT {
   token: string;
@@ -32,9 +29,8 @@ export const projectInitialized = (() => {
     if (!initialized) {
       await handleUninitializedProject();
     } else if (window.location.pathname === "/welcome") {
-      await goto("/");
+      window.location.replace("/");
     }
-
     set(initialized);
   };
 
@@ -48,26 +44,3 @@ export const projectInitialized = (() => {
     },
   };
 })();
-
-async function handleUninitializedProject() {
-  const instanceId = get(runtime).instanceId;
-  // If the project is not initialized, determine what page to route to dependent on the OLAP connector
-  const instance = await runtimeServiceGetInstance(instanceId, {
-    sensitive: true,
-  });
-  const olapConnector = instance.instance?.olapConnector;
-  if (!olapConnector) {
-    throw new Error("OLAP connector is not defined");
-  }
-
-  // DuckDB-backed projects should head to the Welcome page for user-guided initialization
-  if (olapConnector === "duckdb") {
-    await goto("/welcome");
-  } else {
-    // Clickhouse and Druid-backed projects should be initialized immediately
-    await runtimeServiceUnpackEmpty(instanceId, {
-      title: EMPTY_PROJECT_TITLE,
-      force: true,
-    });
-  }
-}
