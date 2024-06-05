46536d1592519ffe381d0cbeeebfd8dcfbaee430
Brian Holmes <120223836+briangregoryholmes@users.noreply.github.com>
2024-06-05 19:25:51 -0400

    refactor: project initialization check (#5016)
    * init commit
    
    * runtime fix
    
    * cleanup
    
    * reorg, test fix
    
    * fixes
    
    * cleanup

---
+++ web-local/src/hooks.client.ts
@@ -0,0 +1,14 @@
+import { dev } from "$app/environment";
+import { runtime } from "@rilldata/web-common/runtime-client/runtime-store";
+
+/** INITIALIZE RUNTIME STORE **/
+// When testing, we need to use the relative path to the server
+const HOST = dev ? "http://localhost:9009" : "";
+const INSTANCE_ID = "default";
+
+const runtimeInit = {
+  host: HOST,
+  instanceId: INSTANCE_ID,
+};
+
+runtime.set(runtimeInit);
