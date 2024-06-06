0e973f257afff80f39de1bfc15798e3c62145793
Brian Holmes <briangregoryholmes@gmail.com>
2024-06-06 11:42:15 -0400

    cleanup

--- web-common/src/features/custom-dashboards/AddChartMenu.svelte
+++ web-common/src/features/custom-dashboards/AddChartMenu.svelte
@@ -3,16 +3,15 @@
     ResourceKind,
     useClientFilteredResources,
   } from "@rilldata/web-common/features/entity-management/resource-selectors";
-  import { ChevronDown, Plus, WandIcon } from "lucide-svelte";
+  import { ChevronDown, Plus } from "lucide-svelte";
   import * as DropdownMenu from "@rilldata/web-common/components/dropdown-menu";
   import { runtime } from "../../runtime-client/runtime-store";
-
   import Search from "@rilldata/web-common/components/search/Search.svelte";
-  import { featureFlags } from "../feature-flags";
+  // import { featureFlags } from "../feature-flags";
   import Button from "@rilldata/web-common/components/button/Button.svelte";
   import { handleEntityCreate } from "../file-explorer/new-files";
 
-  const { ai } = featureFlags;
+  // const { ai } = featureFlags;
 
   export let addChart: (chartName: string) => void;
 
@@ -50,22 +49,24 @@
   </DropdownMenu.Trigger>
   <DropdownMenu.Content class="flex flex-col gap-y-1 ">
     <DropdownMenu.Group>
-      <DropdownMenu.Item disabled>
+      <!-- <DropdownMenu.Item disabled>
         Generate chart
         {#if $ai}
           with AI
           <WandIcon class="w-3 h-3" />
         {/if}
+      </DropdownMenu.Item> -->
+      <DropdownMenu.Item on:click={handleAddChart}>
+        Create new chart
       </DropdownMenu.Item>
-      <DropdownMenu.Item on:click={handleAddChart}
-        >Create new chart</DropdownMenu.Item
-      >
     </DropdownMenu.Group>
+
     <DropdownMenu.Separator />
     <div class="px-1">
       <Search bind:value />
     </div>
     <DropdownMenu.Separator />
+
     <DropdownMenu.Label class="text-[11px] text-gray-500 py-0">
       EXISTING CHARTS
     </DropdownMenu.Label>
