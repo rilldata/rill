3b2a81690526cf07361c0833b8658578dfffb348
Parag Jain <pjain1@apache.org>
2024-06-11 02:14:39 +0530

    col name

--- admin/database/postgres/migrations/0029.sql
+++ admin/database/postgres/migrations/0029.sql
@@ -2,8 +2,8 @@ ALTER TABLE orgs ADD COLUMN billing_customer_id TEXT DEFAULT NULL;
 
 CREATE UNIQUE INDEX orgs_billing_customer_id_idx ON orgs (billing_customer_id) WHERE billing_customer_id IS NOT NULL;
 
-ALTER TABLE orgs ADD COLUMN quota_users INTEGER NOT NULL DEFAULT -1; -- review: do we need quota on num admins as well
+ALTER TABLE orgs ADD COLUMN quota_num_users INTEGER NOT NULL DEFAULT -1; -- review: do we need quota on num admins as well
 ALTER TABLE orgs ADD COLUMN quota_managed_data_bytes BIGINT NOT NULL DEFAULT -1;
-UPDATE orgs SET quota_users = 10, quota_managed_data_bytes = 5368709120; -- review: does this fit with current limits
+UPDATE orgs SET quota_num_users = 10, quota_managed_data_bytes = 5368709120; -- review: does this fit with current limits
 
 ALTER TABLE projects ADD COLUMN next_usage_reporting_time TIMESTAMP DEFAULT '0001-01-01 00:00:00';
