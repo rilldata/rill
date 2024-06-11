6b7e371ec1edfa1ba1e028d2e5e5819ae895b27a
Dhiraj Kumar <sumankumaribarnwal@gmail.com>
2024-06-11 20:11:27 +0530

    Combo lint fix

--- web-common/src/features/dashboards/pivot/pivot-expansion.ts
+++ web-common/src/features/dashboards/pivot/pivot-expansion.ts
@@ -340,7 +340,7 @@ export function queryExpandedRowMeasureValues(
         },
       );
     }) ?? [],
-    (combos) => {
+    (combos: ExpandedRowMeasureValues[]) => {
       return combos;
     },
   );
