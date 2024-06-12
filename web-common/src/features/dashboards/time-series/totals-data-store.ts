4f21dd36755df45f0105fc677ec207973f2d1294
Aditya Hegde <adityahegderocks@gmail.com>
2024-06-12 18:03:06 +0530

    fix: incorrect totals for advanced measures (#5065)
    * Fix incorrect totals for advanced measures
    
    * Fix CI

--- web-common/src/features/dashboards/time-series/totals-data-store.ts
+++ web-common/src/features/dashboards/time-series/totals-data-store.ts
@@ -9,7 +9,6 @@ import {
 import { useTimeControlStore } from "@rilldata/web-common/features/dashboards/time-controls/time-control-store";
 import {
   createQueryServiceMetricsViewAggregation,
-  MetricsViewSpecDimensionSelector,
   type V1MetricsViewAggregationResponse,
 } from "@rilldata/web-common/runtime-client";
 import type { CreateQueryResult } from "@tanstack/svelte-query";
@@ -18,7 +17,6 @@ import { derived } from "svelte/store";
 export function createTotalsForMeasure(
   ctx: StateManagers,
   measures: string[],
-  dimensions: MetricsViewSpecDimensionSelector[],
   isComparison = false,
 ): CreateQueryResult<V1MetricsViewAggregationResponse> {
   return derived(
@@ -44,17 +42,18 @@ export function createTotalsForMeasure(
         metricsViewName,
         {
           measures: measures.map((measure) => ({ name: measure })),
-          dimensions,
           where: sanitiseExpression(
             dashboard.whereFilter,
             measureFilterResolution.filter,
           ),
-          timeStart: isComparison
-            ? timeControls?.comparisonTimeStart
-            : timeControls.timeStart,
-          timeEnd: isComparison
-            ? timeControls?.comparisonTimeEnd
-            : timeControls.timeEnd,
+          timeRange: {
+            start: isComparison
+              ? timeControls?.comparisonTimeStart
+              : timeControls.timeStart,
+            end: isComparison
+              ? timeControls?.comparisonTimeEnd
+              : timeControls.timeEnd,
+          },
         },
         {
           query: {
