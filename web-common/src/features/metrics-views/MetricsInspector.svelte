7c80b72728352f88f47dbdc651981103f265c458
Eric P Green <ericpgreen2@gmail.com>
2024-06-20 22:14:20 -0700

    Better Inspector logic

--- web-common/src/features/metrics-views/MetricsInspector.svelte
+++ web-common/src/features/metrics-views/MetricsInspector.svelte
@@ -13,6 +13,7 @@
   $: resource = fileArtifacts
     .getFileArtifact(filePath)
     .getResource(queryClient, $runtime.instanceId);
+  $: ({ isLoading: isResourceLoading, error: resourceError } = $resource);
 
   $: connector = $resource.data?.metricsView?.spec?.connector;
   $: database = $resource.data?.metricsView?.spec?.database ?? "";
@@ -33,45 +34,51 @@
       },
     },
   );
-  $: ({ error, isLoading } = $tableQuery);
+  $: ({ error: tableError, isLoading: isTableLoading } = $tableQuery);
 
-  function customErrorHandler(errorMessage: string) {
+  function handleTableError(errorMessage: string) {
     if (errorMessage === "driver: not found") {
-      return "Table not found. Please check the table name and try again.";
+      return "Table not found. Please check the table name.";
     }
     return errorMessage;
   }
 </script>
 
-{#if !connector || !table}
-  <div class="custom-instructions-wrapper" style:text-wrap="balance">
-    <div>
-      <p>Table not defined.</p>
-      <p>
-        Specify a table with <code>table: TABLE_NAME</code>
-      </p>
-    </div>
-  </div>
-{:else if isLoading}
+{#if isResourceLoading}
   <div class="spinner-wrapper">
     <ReconcilingSpinner />
   </div>
-{:else if error}
+{:else if resourceError}
+  <!-- The editor will show actual validation errors -->
   <div class="custom-instructions-wrapper" style:text-wrap="balance">
-    <p>Error: {customErrorHandler(error.response.data.message)}</p>
+    <p>
+      For help building dashboards, see:<br /><a
+        href="https://docs.rilldata.com/build/dashboards"
+        target="_blank"
+        rel="noopener noreferrer">https://docs.rilldata.com/build/dashboards</a
+      >
+    </p>
+  </div>
+{:else if isTableLoading}
+  <div class="spinner-wrapper">
+    <ReconcilingSpinner />
+  </div>
+{:else if tableError}
+  <div class="custom-instructions-wrapper" style:text-wrap="balance">
+    <p>Error: {handleTableError(tableError.response.data.message)}</p>
   </div>
 {:else}
   <TableInspector {connector} {database} {databaseSchema} {table} />
 {/if}
 
 <style lang="postcss">
-  .custom-instructions-wrapper {
-    @apply px-4 py-24 w-full;
-    @apply italic text-gray-500 text-center;
-  }
-
   .spinner-wrapper {
     @apply px-4 py-8 size-full;
     @apply flex items-center justify-center;
+  }
+
+  .custom-instructions-wrapper {
+    @apply px-4 py-24 w-full;
+    @apply italic text-gray-500 text-center;
   }
 </style>
