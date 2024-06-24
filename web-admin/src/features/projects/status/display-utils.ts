8c68f807ae45568b90509c3eb7afd5f6d81242c7
Eric P Green <ericpgreen2@gmail.com>
2024-06-24 01:55:24 -0700

    Cloud UI: refactor project status indicators (#5124)
    * Fix project status indicators
    
    * Fix lint
    
    * Fix Typescript
    
    * Show error icon when there are resource or parse errors

--- web-admin/src/features/projects/status/display-utils.ts
+++ web-admin/src/features/projects/status/display-utils.ts
@@ -1,4 +1,56 @@
+import { V1DeploymentStatus } from "@rilldata/web-admin/client";
+import CancelCircle from "@rilldata/web-common/components/icons/CancelCircle.svelte";
+import CheckCircle from "@rilldata/web-common/components/icons/CheckCircle.svelte";
+import InfoCircleFilled from "@rilldata/web-common/components/icons/InfoCircleFilled.svelte";
+import Spinner from "@rilldata/web-common/features/entity-management/Spinner.svelte";
+import { EntityStatus } from "@rilldata/web-common/features/entity-management/types";
 import { V1ReconcileStatus } from "@rilldata/web-common/runtime-client";
+
+export type StatusDisplay = {
+  icon: any; // SvelteComponent
+  iconProps?: {
+    [key: string]: unknown;
+  };
+  text?: string;
+  textClass?: string;
+  wrapperClass?: string;
+};
+
+export const deploymentChipDisplays: Record<V1DeploymentStatus, StatusDisplay> =
+  {
+    [V1DeploymentStatus.DEPLOYMENT_STATUS_UNSPECIFIED]: {
+      icon: InfoCircleFilled,
+      iconProps: { className: "text-indigo-600 hover:text-indigo-500" },
+      text: "Not deployed",
+      textClass: "text-indigo-600",
+      wrapperClass: "bg-indigo-50 border-indigo-300",
+    },
+    [V1DeploymentStatus.DEPLOYMENT_STATUS_PENDING]: {
+      icon: Spinner,
+      iconProps: {
+        bg: "linear-gradient(90deg, #22D3EE -0.5%, #6366F1 98.5%)",
+        className: "text-purple-600 hover:text-purple-500",
+        status: EntityStatus.Running,
+      },
+      text: "Pending",
+      textClass: "text-purple-600",
+      wrapperClass: "bg-purple-50 border-purple-300",
+    },
+    [V1DeploymentStatus.DEPLOYMENT_STATUS_ERROR]: {
+      icon: CancelCircle,
+      iconProps: { className: "text-red-600 hover:text-red-500" },
+      text: "Error",
+      textClass: "text-red-600",
+      wrapperClass: "bg-red-50 border-red-300",
+    },
+    [V1DeploymentStatus.DEPLOYMENT_STATUS_OK]: {
+      icon: CheckCircle,
+      iconProps: { className: "text-primary-600 hover:text-primary-500" },
+      text: "Ready",
+      textClass: "text-primary-600",
+      wrapperClass: "bg-primary-50 border-primary-300",
+    },
+  };
 
 export function prettyReconcileStatus(status: V1ReconcileStatus) {
   switch (status) {
