9c75a158da7cc98f6dea85e9079c614248af576c
Aditya Hegde <adityahegderocks@gmail.com>
2024-06-24 21:55:06 +0530

    Update No-User experience

--- web-common/src/features/authentication/LocalAvatarButton.svelte
+++ web-common/src/features/authentication/LocalAvatarButton.svelte
@@ -2,6 +2,7 @@
   import { page } from "$app/stores";
   import { Button } from "@rilldata/web-common/components/button";
   import * as DropdownMenu from "@rilldata/web-common/components/dropdown-menu";
+  import NoUser from "@rilldata/web-common/components/icons/NoUser.svelte";
   import {
     createLocalServiceDeployValidation,
     createLocalServiceGetCurrentUser,
@@ -11,13 +12,6 @@
   $: user = createLocalServiceGetCurrentUser();
   $: deployValidation = createLocalServiceDeployValidation();
 
-  // $: if ($user.data?.authenticated) initPylonChat($user.data);
-
-  function handlePylon() {
-    // TODO
-    // window.Pylon("show");
-  }
-
   function handleSignIn() {
     window.location.href = `${$deployValidation.data?.loginUrl}/?redirect=${window.location.origin}${window.location.pathname}`;
   }
@@ -26,48 +20,60 @@
     // Create the logout URL, providing the current URL
     return `${$runtime.host}/auth/logout?redirect=${$page.url.href}`;
   }
+
+  $: loggedIn = $user.isSuccess && $user.data?.user;
 </script>
 
 {#if !$user.isFetching}
-  {#if $user.isSuccess && $user.data?.user}
-    <DropdownMenu.Root>
-      <DropdownMenu.Trigger class="flex-none">
+  <DropdownMenu.Root>
+    <DropdownMenu.Trigger class="flex-none">
+      {#if loggedIn}
         <img
           src={$user.data?.user?.photoUrl}
           alt="avatar"
           class="h-7 inline-flex items-center rounded-full"
           referrerpolicy="no-referrer"
         />
-      </DropdownMenu.Trigger>
-      <DropdownMenu.Content>
-        <DropdownMenu.Item
-          href="https://docs.rilldata.com"
-          target="_blank"
-          rel="noreferrer noopener"
-          class="text-gray-800 font-normal"
-        >
-          Documentation
-        </DropdownMenu.Item>
-        <DropdownMenu.Item
-          href="https://discord.com/invite/ngVV4KzEGv?utm_source=rill&utm_medium=rill-cloud-avatar-menu"
-          target="_blank"
-          rel="noreferrer noopener"
-          class="text-gray-800 font-normal"
-        >
-          Join us on Discord
-        </DropdownMenu.Item>
-        <DropdownMenu.Item on:click={handlePylon}>
-          Contact Rill support
-        </DropdownMenu.Item>
+      {:else}
+        <NoUser />
+      {/if}
+    </DropdownMenu.Trigger>
+    <DropdownMenu.Content>
+      <DropdownMenu.Item
+        href="https://docs.rilldata.com"
+        target="_blank"
+        rel="noreferrer noopener"
+        class="text-gray-800 font-normal"
+      >
+        Documentation
+      </DropdownMenu.Item>
+      <DropdownMenu.Item
+        href="https://discord.com/invite/ngVV4KzEGv?utm_source=rill&utm_medium=rill-cloud-avatar-menu"
+        target="_blank"
+        rel="noreferrer noopener"
+        class="text-gray-800 font-normal"
+      >
+        Join us on Discord
+      </DropdownMenu.Item>
+      <!-- TODO -->
+      <!-- <DropdownMenu.Item on:click={handlePylon}>-->
+      <!--   Contact Rill support-->
+      <!-- </DropdownMenu.Item>-->
+      {#if loggedIn}
         <DropdownMenu.Item
           href={makeLogOutHref()}
           class="text-gray-800 font-normal"
         >
           Logout
         </DropdownMenu.Item>
-      </DropdownMenu.Content>
-    </DropdownMenu.Root>
-  {:else}
-    <Button type="primary" on:click={handleSignIn}>Log In / Sign Up</Button>
-  {/if}
+      {:else}
+        <DropdownMenu.Item
+          on:click={handleSignIn}
+          class="text-gray-800 font-normal"
+        >
+          Log in / Sign up
+        </DropdownMenu.Item>
+      {/if}
+    </DropdownMenu.Content>
+  </DropdownMenu.Root>
 {/if}
