eb648676b1c08ab229e1503246b4f14f058d1e1f
Dhiraj Kumar <sumankumaribarnwal@gmail.com>
2024-07-08 15:08:38 +0530

    Support time comparison

--- web-common/src/features/templates/table/selector.ts
+++ web-common/src/features/templates/table/selector.ts
@@ -1,43 +1,93 @@
+import { canEnablePivotComparison } from "@rilldata/web-common/features/dashboards/pivot/pivot-utils";
 import {
+  COMPARISON_DELTA,
+  COMPARISON_PERCENT,
   PivotDataStoreConfig,
   PivotState,
 } from "@rilldata/web-common/features/dashboards/pivot/types";
-import { useMetricsView } from "@rilldata/web-common/features/dashboards/selectors";
+import {
+  useMetricsView,
+  useMetricsViewTimeRange,
+} from "@rilldata/web-common/features/dashboards/selectors";
 import { createAndExpression } from "@rilldata/web-common/features/dashboards/stores/filter-utils";
 import { useStartEndTime } from "@rilldata/web-common/features/templates/kpi/selector";
 import { TableProperties } from "@rilldata/web-common/features/templates/types";
+import { isoDurationToTimeRange } from "@rilldata/web-common/lib/time/ranges/iso-ranges";
 import { Readable, derived } from "svelte/store";
+
+export function useComparisonStartEndTime(
+  instanceId: string,
+  metricViewName: string,
+  timeRange: string,
+  comparisonRange: string | undefined,
+) {
+  const allTimeRangeQuery = useMetricsViewTimeRange(instanceId, metricViewName);
+  return derived(allTimeRangeQuery, (allTimeRange) => {
+    const maxTime = allTimeRange?.data?.timeRangeSummary?.max;
+    const maxTimeDate = new Date(maxTime ?? 0);
+    const { startTime } = isoDurationToTimeRange(timeRange, maxTimeDate);
+
+    let comparisonStartTime: Date | undefined = undefined;
+    let comparisonEndTime: Date | undefined = undefined;
+
+    if (comparisonRange) {
+      ({ startTime: comparisonStartTime, endTime: comparisonEndTime } =
+        isoDurationToTimeRange(comparisonRange, startTime));
+    }
+    return { start: comparisonStartTime, end: comparisonEndTime };
+  });
+}
 
 export function getTableConfig(
   instanceId: string,
   tableProperties: TableProperties,
   pivotState: PivotState,
 ): Readable<PivotDataStoreConfig> {
+  const { metric_view, time_range, comparison_range } = tableProperties;
   return derived(
     [
       useMetricsView(instanceId, tableProperties.metric_view),
-      useStartEndTime(
+      useStartEndTime(instanceId, metric_view, time_range),
+      useComparisonStartEndTime(
         instanceId,
-        tableProperties.metric_view,
-        tableProperties.time_range,
+        metric_view,
+        time_range,
+        comparison_range,
       ),
     ],
-    ([metricsView, timeRange]) => {
+    ([metricsView, timeRange, comparisonRange]) => {
+      const enableComparison = canEnablePivotComparison(
+        pivotState,
+        comparisonRange.start,
+      );
+
       const config: PivotDataStoreConfig = {
-        measureNames: tableProperties.measures,
+        measureNames: tableProperties.measures.flatMap((name) => {
+          const group = [name];
+          if (enableComparison) {
+            group.push(
+              `${name}${COMPARISON_DELTA}`,
+              `${name}${COMPARISON_PERCENT}`,
+            );
+          }
+          return group;
+        }),
         rowDimensionNames: tableProperties.row_dimensions || [],
         colDimensionNames: tableProperties.col_dimensions || [],
         allMeasures: metricsView.data?.measures || [],
         allDimensions: metricsView.data?.dimensions || [],
         whereFilter: createAndExpression([]),
         pivot: pivotState,
-        enableComparison: false,
-        comparisonTime: undefined,
+        enableComparison,
+        comparisonTime: {
+          start: comparisonRange?.start?.toISOString() || undefined,
+          end: comparisonRange?.end?.toISOString() || undefined,
+        },
         time: {
           timeStart: timeRange?.data?.start?.toISOString() || undefined,
           timeEnd: timeRange?.data?.end?.toISOString() || undefined,
           timeZone: "UTC",
-          timeDimension: "",
+          timeDimension: metricsView?.data?.timeDimension || "",
         },
       };
 
