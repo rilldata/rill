ea7785b6a0443f7a50bdcb9e2afdd7f80f26b7b1
Aditya Hegde <adityahegderocks@gmail.com>
2024-07-10 18:37:35 +0530

    Fix order of measures in dimension table export

--- web-common/src/features/dashboards/dimension-table/dimension-table-export-utils.ts
+++ web-common/src/features/dashboards/dimension-table/dimension-table-export-utils.ts
@@ -1,4 +1,8 @@
-import { getQuerySortType } from "@rilldata/web-common/features/dashboards/leaderboard/leaderboard-utils";
+import { getComparisonRequestMeasures } from "@rilldata/web-common/features/dashboards/dashboard-utils";
+import {
+  ComparisonDeltaAbsoluteSuffix,
+  ComparisonDeltaRelativeSuffix,
+} from "@rilldata/web-common/features/dashboards/filters/measure-filters/measure-filter-entry";
 import { SortDirection } from "@rilldata/web-common/features/dashboards/proto-state/derived-types";
 import { useMetricsView } from "@rilldata/web-common/features/dashboards/selectors/index";
 import type { StateManagers } from "@rilldata/web-common/features/dashboards/state-managers/state-managers";
@@ -8,13 +12,17 @@ import {
   mapComparisonTimeRange,
   mapTimeRange,
 } from "@rilldata/web-common/features/dashboards/time-controls/time-range-mappers";
-import { type V1MetricsViewComparisonRequest } from "@rilldata/web-common/runtime-client";
+import { DashboardState_LeaderboardSortType } from "@rilldata/web-common/proto/gen/rill/ui/v1/dashboard_pb";
+import type {
+  V1MetricsViewAggregationMeasure,
+  V1MetricsViewAggregationRequest,
+} from "@rilldata/web-common/runtime-client";
 import { runtime } from "@rilldata/web-common/runtime-client/runtime-store";
 import { derived, get, Readable } from "svelte/store";
 
 export function getDimensionTableExportArgs(
   ctx: StateManagers,
-): Readable<V1MetricsViewComparisonRequest | undefined> {
+): Readable<V1MetricsViewAggregationRequest | undefined> {
   return derived(
     [
       ctx.metricsViewName,
@@ -40,23 +48,44 @@ export function getDimensionTableExportArgs(
         comparisonMeasures = [dashboardState.leaderboardMeasureName];
       }
 
+      const measures: V1MetricsViewAggregationMeasure[] = [
+        ...dashboardState.visibleMeasureKeys,
+      ].map((name) => ({
+        name: name,
+      }));
+
+      let apiSortName = dashboardState.leaderboardMeasureName;
+      if (comparisonTimeRange && timeControlState.selectedComparisonTimeRange) {
+        // insert beside the correct measure
+        measures.splice(
+          measures.findIndex((m) => m.name === apiSortName),
+          0,
+          ...getComparisonRequestMeasures(apiSortName),
+        );
+        switch (dashboardState.dashboardSortType) {
+          case DashboardState_LeaderboardSortType.DELTA_ABSOLUTE:
+            apiSortName += ComparisonDeltaAbsoluteSuffix;
+            break;
+          case DashboardState_LeaderboardSortType.DELTA_PERCENT:
+            apiSortName += ComparisonDeltaRelativeSuffix;
+            break;
+        }
+      }
+
       return {
         instanceId: get(runtime).instanceId,
-        metricsViewName: metricViewName,
+        metricsView: metricViewName,
         dimension: {
           name: dashboardState.selectedDimensionName,
         },
-        measures: [...dashboardState.visibleMeasureKeys].map((name) => ({
-          name: name,
-        })),
+        measures,
         comparisonMeasures: comparisonMeasures,
         timeRange,
         ...(comparisonTimeRange ? { comparisonTimeRange } : {}),
         sort: [
           {
-            name: dashboardState.leaderboardMeasureName,
+            name: apiSortName,
             desc: dashboardState.sortDirection === SortDirection.DESCENDING,
-            sortType: getQuerySortType(dashboardState.dashboardSortType),
           },
         ],
         where: sanitiseExpression(dashboardState.whereFilter, undefined),
