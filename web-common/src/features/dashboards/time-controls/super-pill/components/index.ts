60ef7764bd4f86ddc7e03248e9c800a7385123e0
Brian Holmes <120223836+briangregoryholmes@users.noreply.github.com>
2024-07-17 15:05:34 -0400

    Feat: Super Pill and Range Picker (#4422)
    * initial commit
    
    * cleanup
    
    * remove litepicker
    
    * update time comparison selector
    
    * font weight and cleanup
    
    * rework calendar picker, add super pill
    
    * prettier
    
    * add custom dropdown item to time range menu
    
    * testing out fixed width for range
    
    * add apply button to calendar picker
    
    * implement comparison dropdown
    
    * file clean up
    
    * move grain selector to time series section
    
    * remove import
    
    * fix tests
    
    * unused import
    
    * move nudge, broader date formats, rough UI parse error
    
    * unused import
    
    * always show time, change tab layout
    
    * comparison toggle adjustments
    
    * add time grain selector to tdd
    
    * ui adjustments
    
    * add range display to zoom button
    
    * capitalize normal label
    
    * border highlighting
    
    * update test
    
    * unused import
    
    * cleanup and apply range fix
    
    * add calendar input to comparison
    
    * unused import
    
    * update comparison toggle
    
    * change month to short display
    
    * fix comment
    
    * move calendar picker into range selector
    
    * hide calendar picker by default, update zone change behavior
    
    * cleanup
    
    * regen package lock
    
    * test fix
    
    * time display logic
    
    * color tweaks
    
    * show/hide behavior
    
    * tooltips
    
    * comparison adjustments
    
    * variable cleanup
    
    * test fix
    
    * revert pivot changes
    
    * chart interaction fix
    
    * wip commit
    
    * wip table commit
    
    * wip commit
    
    * wip commit
    
    * wip commit
    
    * wip test fix
    
    * wip test fix
    
    * wip test fixes
    
    * remove time dimension option from tdd
    
    * test fixes
    
    * consolidate comparison visibility with pivot
    
    * cleanup
    
    * remove unused import
    
    * cleanup
    
    * remove import
    
    * revert unintended changes
    
    * change no comparison label
    
    * bar percentage fix
    
    * fix leaderboard bar extension
    
    * leaderboard fixes
    
    * cleanup
    
    * test fix
    
    * type reversion
    
    * type fixes
    
    * chip styling
    
    * type fix
    
    * actual type fix
    
    * comparisons fix
    
    * hide pills if no time series
    
    * workaround for tab menu closure
    
    * make time and dimension comparison mutually exclusive
    
    * remove import
    
    * test fix
    
    * firefox styling fix

---
+++ web-common/src/features/dashboards/time-controls/super-pill/components/index.ts
@@ -0,0 +1,7 @@
+import Comparison from "./Comparison.svelte";
+import Nudge from "./Nudge.svelte";
+import Zone from "./Zone.svelte";
+import Zoom from "./Zoom.svelte";
+import RangePicker from "./RangePicker.svelte";
+
+export { Comparison, Nudge, Zone, Zoom, RangePicker };
