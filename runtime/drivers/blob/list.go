708bb2f08b9c9a25f04f00a2442057cea0fcc50e
Benjamin Egelund-Müller <b@egelund-muller.com>
2024-07-18 15:18:52 +0200

    Self review

--- runtime/drivers/blob/list.go
+++ runtime/drivers/blob/list.go
@@ -13,11 +13,15 @@ import (
 	"gocloud.dev/blob"
 )
 
+// Bucket wraps a blob.Bucket with functionality for implementing the drivers.ObjectStore interface.
+// NOTE: It currently only supports listing objects, but eventually we should refactor NewIterator to a member function of this struct.
 type Bucket struct {
 	bucket *blob.Bucket
 	logger *zap.Logger
 }
 
+// NewBucket wraps a *blob.Bucket.
+// It takes ownership of the bucket and will close it when Close is called.
 func NewBucket(bucket *blob.Bucket, logger *zap.Logger) (*Bucket, error) {
 	return &Bucket{
 		bucket: bucket,
@@ -25,10 +29,14 @@ func NewBucket(bucket *blob.Bucket, logger *zap.Logger) (*Bucket, error) {
 	}, nil
 }
 
+// Close the underlying bucket.
 func (b *Bucket) Close() error {
 	return b.bucket.Close()
 }
 
+// ListObjects lists objects in the bucket that match the given glob pattern.
+// The glob pattern should be a valid path *without* scheme or bucket name.
+// E.g. to list gs://my-bucket/path/to/files/*, the glob pattern should be "path/to/files/*".
 func (b *Bucket) ListObjects(ctx context.Context, glob string) ([]drivers.ObjectStoreEntry, error) {
 	// If it's not a glob, we're pulling a single file.
 	// TODO: Should we add support for listing out directories without ** at the end?
