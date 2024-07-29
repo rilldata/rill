604c84642de202df3c4b1e984c23ca2519e2b084
royendo <67675319+royendo@users.noreply.github.com>
2024-07-29 20:50:52 +0900

    Adding further backbones of 202 series
    some UI updates coming in 0.48. so paused on some contents,
    
    outlining some 200 series contents, for chronological storytelling

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -89,16 +89,11 @@ const sidebars = {
           description: 'Advanced  description',
           items: [
             'learn/rill_learn_200/201_0',
-            {
-              type: 'category',
-              label: 'Rill Cloud Features ',
-              description: 'Advanced Rill Cloud Features',
-              items: [
-                'learn/rill_learn_200/cloud_components/202_0',
-                'learn/rill_learn_200/cloud_components/202_1',
-                'learn/rill_learn_200/cloud_components/202_2',
-              ]
-            },
+            'learn/rill_learn_200/201_1',
+            'learn/rill_learn_200/cloud_components/202_0',
+            'learn/rill_learn_200/cloud_components/202_1',
+            'learn/rill_learn_200/cloud_components/202_2',
+            'learn/rill_learn_200/203_0',
             'learn/rill_learn_200/204_0',
           ]
         },
