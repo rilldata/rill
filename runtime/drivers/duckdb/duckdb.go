92ad97ea721063b163db0f904372766f2f620692
Eric P Green <ericpgreen2@gmail.com>
2024-07-29 08:47:13 -0400

    Add dedicated Motherduck Source form (#5324)
    * Fix validation of DuckDB connector form
    
    * Replace "Remote Source" with "Add Data" terminology from UI
    
    * Add `name` to backend source properties
    
    * Use `configProperties` not `sourceProperties` for OLAP Connectors; Add `prompt` boolean
    
    * Frontend integration
    
    * Better spacing
    
    * Fix merge
    
    * Move DuckDB to Sources section
    
    * Better placeholder
    
    * Fix typescript
    
    * Add validation to prevent duplicate `https://` prefix
    
    * Fix merge
    
    * Add dedicated MotherDuck Source form
    
    * Update `compileSourceYAML` function to write secrets to `.env`
    
    * `connection.string` -> `connection_string`
    
    * handle new spec
    
    * Use `dsn` not `connection_string`
    
    * Fix yup schema
    
    * change to DSN
    
    * fix allow_host_access for md
    
    * attach fix
    
    * do not switch to db
    
    * Do not explicitly reference secrets in Source YAML
    
    * Ensure deterministic key order
    
    ---------
    
    Co-authored-by: Anshul Khandelwal <anshulkhandelwal94@gmail.com>

--- runtime/drivers/duckdb/duckdb.go
+++ runtime/drivers/duckdb/duckdb.go
@@ -95,6 +95,40 @@ var motherduckSpec = drivers.Spec{
 			Secret: true,
 		},
 	},
+	SourceProperties: []*drivers.PropertySpec{
+		{
+			Key:         "dsn",
+			Type:        drivers.StringPropertyType,
+			Required:    true,
+			DisplayName: "MotherDuck Connection String",
+			Placeholder: "md:motherduck.db",
+		},
+		{
+			Key:         "sql",
+			Type:        drivers.StringPropertyType,
+			Required:    true,
+			DisplayName: "SQL",
+			Description: "Query to extract data from MotherDuck.",
+			Placeholder: "select * from table;",
+		},
+		{
+			Key:         "token",
+			Type:        drivers.StringPropertyType,
+			Required:    true,
+			DisplayName: "Access token",
+			Description: "MotherDuck access token",
+			Placeholder: "your.access_token.here",
+			Secret:      true,
+		},
+		{
+			Key:         "name",
+			Type:        drivers.StringPropertyType,
+			DisplayName: "Source name",
+			Description: "The name of the source",
+			Placeholder: "my_new_source",
+			Required:    true,
+		},
+	},
 }
 
 type Driver struct {
