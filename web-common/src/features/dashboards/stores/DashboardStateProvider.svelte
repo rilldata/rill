ebe2e623e4ad5aba8b815157e74e1d11ca609a63
Cyrus Goh <hello@cyrusgoh.com>
2024-07-31 12:04:17 -0700

    loading with timeout in tdd header and dashboard store ready

--- web-common/src/features/dashboards/stores/DashboardStateProvider.svelte
+++ web-common/src/features/dashboards/stores/DashboardStateProvider.svelte
@@ -4,15 +4,20 @@
   import { initLocalUserPreferenceStore } from "@rilldata/web-common/features/dashboards/user-preferences";
   import Spinner from "../../entity-management/Spinner.svelte";
   import { EntityStatus } from "../../entity-management/types";
+  import { isLoadingWithTimeout } from "../../entity-management/spinner-utils";
 
   const dashboardStoreReady = createDashboardStateSync(getStateManagers());
 
   export let metricViewName: string;
 
   $: initLocalUserPreferenceStore(metricViewName);
+
+  const loadingWithTimeout = isLoadingWithTimeout();
+  $: loadingWithTimeout.setLoading($dashboardStoreReady.isFetching);
+  $: isFetchingWithTimeout = $loadingWithTimeout;
 </script>
 
-{#if $dashboardStoreReady.isFetching}
+{#if isFetchingWithTimeout}
   <div class="grid place-items-center size-full">
     <Spinner status={EntityStatus.Running} size="40px" />
   </div>
