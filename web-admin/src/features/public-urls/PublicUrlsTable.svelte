fea578d0a1022ac7cf89e20dfee60f4aa3594d5a
Cyrus Goh <hello@cyrusgoh.com>
2024-08-05 10:15:06 -0700

    list all magic auth tokens in table

--- web-admin/src/features/public-urls/PublicUrlsTable.svelte
+++ web-admin/src/features/public-urls/PublicUrlsTable.svelte
@@ -1,29 +1,63 @@
 <script lang="ts">
   import * as Table from "@rilldata/web-common/components/table-shadcn";
-  import type { V1Resource } from "@rilldata/web-common/runtime-client";
   import { Render, Subscribe, createTable } from "svelte-headless-table";
   import { readable } from "svelte/store";
+  import { goto } from "$app/navigation";
+  import { type Row } from "@tanstack/svelte-table";
 
-  export let resources: V1Resource[];
+  // TODO: to be added to orval in `web-common/src/runtime-client/gen/index.schemas.ts`
+  interface V1MagicAuthToken {
+    id?: string;
+    secretHash?: string;
+    projectId?: string;
+    createdOn?: string;
+    expiresOn?: string;
+    usedOn?: string;
+    createdByUserId?: string;
+    attributes?: { [key: string]: any };
+    metricsView?: string;
+    metricsViewFilterJSON?: string;
+    metricsViewFields?: string[];
+    state?: string;
+  }
+  export let magicAuthTokens: V1MagicAuthToken[];
+  $: console.log("magicAuthTokens: ", magicAuthTokens);
 
-  const table = createTable(readable(resources));
+  export let organization: string;
+  export let project: string;
+
+  function handleClickRow(row: any) {
+    // TODO: revisit when token secret is available
+    // `/${organization}/${project}/magic-link/${token.id}`
+    goto(`/${organization}/${project}/${row.original.metricsView}`);
+  }
+
+  const table = createTable(readable(magicAuthTokens));
 
   const columns = table.createColumns([
     table.column({
-      accessor: (resource) => resource.meta.name.name,
-      header: "Public URL name",
-    }),
-    table.column({
-      accessor: (resource) => resource.meta.name.name,
+      accessor: (token) => token.metricsView,
       header: "Dashboard name",
     }),
     table.column({
-      accessor: (resource) => resource.meta.createdOn,
+      accessor: (token) => token.attributes.email,
       header: "Created by",
     }),
     table.column({
-      accessor: (resource) => resource.meta.createdOn,
+      accessor: (token) => token.usedOn,
       header: "Last used",
+      cell: ({ value }) => {
+        if (!value) {
+          return "-";
+        }
+        return new Date(value).toLocaleDateString(undefined, {
+          year: "numeric",
+          month: "short",
+          day: "numeric",
+          hour: "numeric",
+          minute: "numeric",
+        });
+      },
     }),
   ]);
 
@@ -51,7 +85,7 @@
     <Table.Body {...$tableBodyAttrs}>
       {#each $pageRows as row (row.id)}
         <Subscribe rowAttrs={row.attrs()} let:rowAttrs>
-          <Table.Row {...rowAttrs}>
+          <Table.Row {...rowAttrs} on:click={() => handleClickRow(row)}>
             {#each row.cells as cell (cell.id)}
               <Subscribe attrs={cell.attrs()} let:attrs>
                 <Table.Cell {...attrs}>
