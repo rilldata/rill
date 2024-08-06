b2e59eef6641ad0778c642c4726893e17d6d5387
Cyrus Goh <hello@cyrusgoh.com>
2024-08-05 18:16:34 -0700

    ui for row deletion

---
+++ web-admin/src/features/public-urls/PublicURLsDeleteRow.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Trash from "@rilldata/web-common/components/icons/Trash.svelte";
+
+  export let url: string;
+
+  function handleClick(e: PointerEvent) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    console.log("deleting row", url);
+  }
+</script>
+
+<div class="flex items-center justify-center">
+  <button on:click={handleClick} class="text-gray-400 hover:text-gray-500">
+    <Trash size="16px " />
+  </button>
+</div>
