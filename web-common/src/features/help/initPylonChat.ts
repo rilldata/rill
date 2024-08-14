24b76b94ce3ace8ee644e4a7f8090459f204fc1a
Aditya Hegde <adityahegderocks@gmail.com>
2024-08-14 19:39:43 +0530

    Add comment

--- web-common/src/features/help/initPylonChat.ts
+++ web-common/src/features/help/initPylonChat.ts
@@ -1,5 +1,8 @@
 const appId = import.meta.env.RILL_UI_PUBLIC_PYLON_APP_ID as string;
 
+// Right now both local and cloud return the same V1User from admin.
+// Until we figure out a good place to put those common types (not the admin clients),
+// this represents the user fields needed by pylon
 export type UserLike = {
   email: string;
   displayName: string;
