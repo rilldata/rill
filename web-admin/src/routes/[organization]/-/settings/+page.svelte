d64fa5690ff19c0c379dbda30c6b21be0825d1db
Aditya Hegde <adityahegderocks@gmail.com>
2024-08-20 20:02:12 +0530

    Add user domain allowlist item

--- web-admin/src/routes/[organization]/-/settings/+page.svelte
+++ web-admin/src/routes/[organization]/-/settings/+page.svelte
@@ -2,11 +2,13 @@
   import { page } from "$app/stores";
   import DeleteOrgItems from "@rilldata/web-admin/features/organizations/settings/DeleteOrgItems.svelte";
   import UpdateOrgItem from "@rilldata/web-admin/features/organizations/settings/UpdateOrgItem.svelte";
+  import UserDomainAllowListItem from "@rilldata/web-admin/features/organizations/settings/UserDomainAllowListItem.svelte";
 
   $: organization = $page.params.organization;
 </script>
 
 <div class="flex flex-col gap-y-6">
   <UpdateOrgItem {organization} />
+  <UserDomainAllowListItem {organization} />
   <DeleteOrgItems {organization} />
 </div>
