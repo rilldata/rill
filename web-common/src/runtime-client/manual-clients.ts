dbe6c8ea5caeaa80c10d7afea1b9a4e8fc5cad1c
Dhiraj Barnwal <sumankumaribarnwal@gmail.com>
2024-08-26 21:51:29 +0530

    refac: deprecate chart data query in favor of resolved components query (#5530)
    * refac: deprecate chart data query in favor of resolved components query
    
    * remove unused props
    
    * invalidate component resource query

--- web-common/src/runtime-client/manual-clients.ts
+++ web-common/src/runtime-client/manual-clients.ts
@@ -1,9 +1,6 @@
 // This files contains clients that are not written through GRPC
 
-import { V1ComponentSpecResolverProperties } from "@rilldata/web-common/runtime-client/gen/index.schemas";
 import httpClient from "@rilldata/web-common/runtime-client/http-client";
-import { QueryClient } from "@tanstack/query-core";
-import { createQuery } from "@tanstack/svelte-query";
 
 export type V1RuntimeGetConfig = {
   instance_id: string;
@@ -37,74 +34,3 @@ export const runtimeServiceFileUpload = async (
     headers: {},
   });
 };
-
-export function runtimeServiceGetChartData(
-  instanceId: string,
-  chartName: string,
-  args: any,
-  signal?: AbortSignal,
-) {
-  return httpClient({
-    url: `/v1/instances/${instanceId}/components/${chartName}/data`,
-    method: "GET",
-    headers: {},
-    params: args,
-    signal,
-  });
-}
-
-export function createRuntimeServiceGetChartData(
-  queryClient: QueryClient,
-  instanceId: string,
-  chartName: string,
-  args: any,
-  // we need this till we figure out why last updated is not changing on charts
-  props: V1ComponentSpecResolverProperties | undefined,
-) {
-  return createQuery<unknown, unknown, Record<string, unknown>[]>(
-    [`/v1/instances/${instanceId}/components/${chartName}/data`, props, args],
-    {
-      queryFn: ({ signal }) =>
-        runtimeServiceGetChartData(instanceId, chartName, args, signal),
-      enabled: !!instanceId && !!chartName,
-      queryClient,
-    },
-  );
-}
-
-export function runtimeServiceGetParsedComponent(
-  instanceId: string,
-  componentName: string,
-  args: any,
-  signal?: AbortSignal,
-) {
-  return httpClient({
-    url: `/v1/instances/${instanceId}/components/${componentName}/parse`,
-    method: "GET",
-    headers: {},
-    params: args,
-    signal,
-  });
-}
-
-export function createRuntimeServiceGetParsedComponent(
-  queryClient: QueryClient,
-  instanceId: string,
-  componentName: string,
-  args: any | undefined,
-) {
-  return createQuery<unknown, unknown, Record<string, unknown>[]>(
-    [`/v1/instances/${instanceId}/components/${componentName}/parse`, args],
-    {
-      queryFn: ({ signal }) =>
-        runtimeServiceGetParsedComponent(
-          instanceId,
-          componentName,
-          args,
-          signal,
-        ),
-      enabled: true,
-      queryClient,
-    },
-  );
-}
