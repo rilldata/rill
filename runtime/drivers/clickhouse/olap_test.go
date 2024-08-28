1bd7671865c9b6beeed88e43d7d6b44e02463ac9
Anshul Khandelwal <12948312+k-anshul@users.noreply.github.com>
2024-08-28 15:53:34 +0530

    feat : cluster and direct SQL support for clickhouse models (#5511)
    * cluster and config support for clickhouse models
    
    * changes for on_cluster property
    
    * small self review
    
    * small self review
    
    * unit tests for clickhouse cluster
    
    * unit tests for clickhouse cluster
    
    * lint fix plus other ut changes
    
    * revert config change

--- runtime/drivers/clickhouse/olap_test.go
+++ runtime/drivers/clickhouse/olap_test.go
@@ -1,4 +1,4 @@
-package clickhouse
+package clickhouse_test
 
 import (
 	"context"
@@ -7,41 +7,43 @@ import (
 
 	"github.com/rilldata/rill/runtime/drivers"
 	"github.com/rilldata/rill/runtime/pkg/activity"
+	"github.com/rilldata/rill/runtime/testruntime"
 	"github.com/stretchr/testify/require"
-	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/modules/clickhouse"
 	"go.uber.org/zap"
 )
 
-func TestRenameTable(t *testing.T) {
+func TestClickhouseCrudOps(t *testing.T) {
 	if testing.Short() {
 		t.Skip("clickhouse: skipping test in short mode")
 	}
 
-	ctx := context.Background()
-	clickHouseContainer, err := clickhouse.RunContainer(ctx,
-		testcontainers.WithImage("clickhouse/clickhouse-server:latest"),
-		clickhouse.WithUsername("clickhouse"),
-		clickhouse.WithPassword("clickhouse"),
-		clickhouse.WithConfigFile("../../testruntime/testdata/clickhouse-config.xml"),
-	)
-	require.NoError(t, err)
-	t.Cleanup(func() {
-		err := clickHouseContainer.Terminate(ctx)
-		require.NoError(t, err)
-	})
+	dsn, cluster := testruntime.ClickhouseCluster(t)
+	t.Run("SingleHost", func(t *testing.T) { testClickhouseSingleHost(t, dsn) })
+	t.Run("Cluster", func(t *testing.T) { testClickhouseCluster(t, dsn, cluster) })
+}
 
-	host, err := clickHouseContainer.Host(ctx)
+func testClickhouseSingleHost(t *testing.T, dsn string) {
+	conn, err := drivers.Open("clickhouse", "default", map[string]any{"dsn": dsn}, activity.NewNoopClient(), zap.NewNop())
 	require.NoError(t, err)
-	port, err := clickHouseContainer.MappedPort(ctx, "9000/tcp")
-	require.NoError(t, err)
-
-	conn, err := driver{}.Open("default", map[string]any{"dsn": fmt.Sprintf("clickhouse://clickhouse:clickhouse@%v:%v", host, port.Port())}, activity.NewNoopClient(), zap.NewNop())
-	require.NoError(t, err)
+	defer conn.Close()
 	prepareConn(t, conn)
 
 	olap, ok := conn.AsOLAP("default")
 	require.True(t, ok)
+
+	t.Run("RenameTable", func(t *testing.T) { testRenameTable(t, olap) })
+	t.Run("CreateTableAsSelect", func(t *testing.T) { testCreateTableAsSelect(t, olap) })
+}
+
+func testClickhouseCluster(t *testing.T, dsn, cluster string) {
+	conn, err := drivers.Open("clickhouse", "default", map[string]any{"dsn": dsn, "cluster": cluster}, activity.NewNoopClient(), zap.NewNop())
+	require.NoError(t, err)
+	defer conn.Close()
+
+	olap, ok := conn.AsOLAP("default")
+	require.True(t, ok)
+
+	prepareClusterConn(t, olap, cluster)
 
 	t.Run("RenameTable", func(t *testing.T) { testRenameTable(t, olap) })
 	t.Run("CreateTableAsSelect", func(t *testing.T) { testCreateTableAsSelect(t, olap) })
@@ -55,16 +57,58 @@ func testRenameTable(t *testing.T, olap drivers.OLAPStore) {
 	err = olap.RenameTable(ctx, "foo1", "bar", false)
 	require.NoError(t, err)
 
+	tableNotExists(t, olap, "foo")
+	tableNotExists(t, olap, "foo1")
+}
+
+func tableNotExists(t *testing.T, olap drivers.OLAPStore, tbl string) {
+	result, err := olap.Execute(context.Background(), &drivers.Statement{
+		Query: "EXISTS " + tbl,
+	})
+	require.NoError(t, err)
+	require.True(t, result.Next())
 	var exist bool
-	require.NoError(t, olap.(*connection).db.QueryRowContext(ctx, "EXISTS foo1").Scan(&exist))
-	require.False(t, exist)
-	require.NoError(t, olap.(*connection).db.QueryRowContext(ctx, "EXISTS foo1").Scan(&exist))
+	require.NoError(t, result.Scan(&exist))
 	require.False(t, exist)
 }
 
 func testCreateTableAsSelect(t *testing.T, olap drivers.OLAPStore) {
 	err := olap.CreateTableAsSelect(context.Background(), "tbl", false, "SELECT 1 AS id, 'Earth' AS planet", map[string]any{
-		"engine": "MergeTree",
+		"engine":                   "MergeTree",
+		"table":                    "tbl",
+		"distributed.sharding_key": "rand()",
+	})
+	require.NoError(t, err)
+}
+
+func prepareClusterConn(t *testing.T, olap drivers.OLAPStore, cluster string) {
+	err := olap.Exec(context.Background(), &drivers.Statement{
+		Query: fmt.Sprintf("CREATE OR REPLACE TABLE foo_local ON CLUSTER %s (bar VARCHAR, baz INTEGER) engine=MergeTree ORDER BY tuple()", cluster),
+	})
+	require.NoError(t, err)
+
+	err = olap.Exec(context.Background(), &drivers.Statement{
+		Query: fmt.Sprintf("CREATE OR REPLACE TABLE foo ON CLUSTER %s AS foo_local engine=Distributed(%s, currentDatabase(), foo_local, rand())", cluster, cluster),
+	})
+	require.NoError(t, err)
+
+	err = olap.Exec(context.Background(), &drivers.Statement{
+		Query: "INSERT INTO foo VALUES ('a', 1), ('a', 2), ('b', 3), ('c', 4)",
+	})
+	require.NoError(t, err)
+
+	err = olap.Exec(context.Background(), &drivers.Statement{
+		Query: fmt.Sprintf("CREATE OR REPLACE TABLE bar_local ON CLUSTER %s (bar VARCHAR, baz INTEGER) engine=MergeTree ORDER BY tuple()", cluster),
+	})
+	require.NoError(t, err)
+
+	err = olap.Exec(context.Background(), &drivers.Statement{
+		Query: fmt.Sprintf("CREATE OR REPLACE TABLE bar ON CLUSTER %s AS foo_local engine=Distributed(%s, currentDatabase(), foo_local, rand())", cluster, cluster),
+	})
+	require.NoError(t, err)
+
+	err = olap.Exec(context.Background(), &drivers.Statement{
+		Query: "INSERT INTO bar VALUES ('a', 1), ('a', 2), ('b', 3), ('c', 4)",
 	})
 	require.NoError(t, err)
 }
