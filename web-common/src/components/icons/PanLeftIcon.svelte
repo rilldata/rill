1aff74f60644835e026de4ea77e63d090f9bf925
Cyrus Goh <hello@cyrusgoh.com>
2024-09-12 18:29:29 -0700

    wip

--- web-common/src/components/icons/PanLeftIcon.svelte
+++ web-common/src/components/icons/PanLeftIcon.svelte
@@ -9,12 +9,3 @@
     d="M9.335 16.795L21.678 5.756C22.129 5.352 22.844 5.672 22.844 6.277L22.844 27.342C22.844 27.948 22.128 28.268 21.677 27.863L9.335 16.795Z"
   />
 </svg>
-
-<style lang="postcss">
-  .pan-button {
-    @apply cursor-pointer fill-slate-400;
-  }
-  .pan-button:hover {
-    @apply fill-slate-300;
-  }
-</style>
