fd3ed60a5bec6568f4b25d9bf6fbd2be67f76be9
Eric P Green <ericpgreen2@gmail.com>
2024-09-17 16:34:15 -0400

    Add "Incremental processing" section to inspector

---
+++ web-common/src/features/models/incremental/IncrementalProcessing.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import CollapsibleSectionTitle from "@rilldata/web-common/layout/CollapsibleSectionTitle.svelte";
+  import { V1Resource } from "../../../runtime-client";
+  import IncrementalStateTable from "./IncrementalStateTable.svelte";
+
+  export let resource: V1Resource;
+
+  let active = true;
+
+  $: hasIncrementalState = !!resource.model?.state?.incrementalState;
+</script>
+
+<CollapsibleSectionTitle tooltipText="incremental state" bind:active>
+  Incremental processing
+</CollapsibleSectionTitle>
+
+{#if active}
+  {#if !hasIncrementalState}
+    <div class="help-text">Each refresh will append new records.</div>
+  {:else if hasIncrementalState}
+    <div class="wrapper">
+      <span class="help-text">
+        Use this state to determine which records to process on each model run.
+      </span>
+      <IncrementalStateTable {resource} />
+    </div>
+  {/if}
+{/if}
+
+<style lang="postcss">
+  .wrapper {
+    @apply flex flex-col gap-y-2 items-start;
+  }
+
+  .help-text {
+    @apply text-xs text-gray-500;
+  }
+</style>
