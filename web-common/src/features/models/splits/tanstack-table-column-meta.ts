78384c8129ebd81bba8755c8bc073cacff9f7f3b
Eric P Green <ericpgreen2@gmail.com>
2024-09-20 11:17:44 -0400

    Suppress lint

--- web-common/src/features/models/splits/tanstack-table-column-meta.ts
+++ web-common/src/features/models/splits/tanstack-table-column-meta.ts
@@ -1,6 +1,7 @@
 import { RowData } from "@tanstack/svelte-table";
 
 declare module "@tanstack/svelte-table" {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface ColumnMeta<TData extends RowData, TValue> {
     widthPercent?: number;
   }
