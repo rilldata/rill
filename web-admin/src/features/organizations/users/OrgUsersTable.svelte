ee6210efbcc74c19c21ade27d2874b50b11e1917
Cyrus Goh <hello@cyrusgoh.com>
2024-09-25 16:42:26 -0700

    org users table

---
+++ web-admin/src/features/organizations/users/OrgUsersTable.svelte
@@ -0,0 +1,184 @@
+<script lang="ts">
+  import Tag from "@rilldata/web-common/components/tag/Tag.svelte";
+  import { prettyResourceKind } from "@rilldata/web-common/features/entity-management/resource-selectors";
+  import type { V1MemberUser } from "@rilldata/web-admin/client";
+
+  import { writable } from "svelte/store";
+  import ArrowDown from "@rilldata/web-common/components/icons/ArrowDown.svelte";
+  import {
+    createSvelteTable,
+    flexRender,
+    getCoreRowModel,
+    getSortedRowModel,
+  } from "@tanstack/svelte-table";
+  import type {
+    ColumnDef,
+    OnChangeFn,
+    SortingState,
+    TableOptions,
+  } from "@tanstack/svelte-table";
+
+  export let data: V1MemberUser[];
+
+  let sorting: SortingState = [];
+
+  function formatDate(value: string) {
+    return new Date(value).toLocaleDateString(undefined, {
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+    });
+  }
+
+  const setSorting: OnChangeFn<SortingState> = (updater) => {
+    if (updater instanceof Function) {
+      sorting = updater(sorting);
+    } else {
+      sorting = updater;
+    }
+
+    options.update((old) => ({
+      ...old,
+      state: {
+        ...old.state,
+        sorting,
+      },
+    }));
+  };
+
+  const columns: ColumnDef<V1MemberUser, any>[] = [
+    {
+      accessorKey: "userName",
+      header: "User",
+    },
+    {
+      accessorKey: "roleName",
+      header: "Role",
+    },
+    // {
+    //   accessorFn: (row) => row.userName,
+    //   header: "Groups",
+    // },
+    // {
+    //   accessorFn: (row) => row.userName,
+    //   header: "Projects",
+    // },
+  ];
+
+  const options = writable<TableOptions<V1MemberUser>>({
+    data: data,
+    columns: columns,
+    state: {
+      sorting,
+    },
+    onSortingChange: setSorting,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+  });
+
+  const table = createSvelteTable(options);
+</script>
+
+<div class="list">
+  <table class="w-full">
+    <thead>
+      {#each $table.getHeaderGroups() as headerGroup}
+        <tr>
+          {#each headerGroup.headers as header}
+            <th
+              colSpan={header.colSpan}
+              class="px-4 py-2 text-left"
+              on:click={header.column.getToggleSortingHandler()}
+            >
+              {#if !header.isPlaceholder}
+                <div
+                  class:cursor-pointer={header.column.getCanSort()}
+                  class:select-none={header.column.getCanSort()}
+                  class="font-semibold text-gray-500 flex flex-row items-center gap-x-1"
+                >
+                  <svelte:component
+                    this={flexRender(
+                      header.column.columnDef.header,
+                      header.getContext(),
+                    )}
+                  />
+                  {#if header.column.getIsSorted().toString() === "asc"}
+                    <span>
+                      <ArrowDown flip size="12px" />
+                    </span>
+                  {:else if header.column.getIsSorted().toString() === "desc"}
+                    <span>
+                      <ArrowDown size="12px" />
+                    </span>
+                  {/if}
+                </div>
+              {/if}
+            </th>
+          {/each}
+        </tr>
+      {/each}
+    </thead>
+    <tbody>
+      {#each $table.getRowModel().rows as row}
+        <tr>
+          {#each row.getVisibleCells() as cell}
+            <td
+              class={`px-4 py-2 ${cell.column.id === "actions" ? "w-1" : ""}`}
+              data-label={cell.column.columnDef.header}
+            >
+              <svelte:component
+                this={flexRender(cell.column.columnDef.cell, cell.getContext())}
+              />
+            </td>
+          {/each}
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+</div>
+
+<style lang="postcss">
+  table {
+    @apply border-separate border-spacing-0;
+  }
+  table th,
+  table td {
+    @apply border-b border-gray-200;
+  }
+
+  thead tr th {
+    @apply border-t border-gray-200;
+  }
+  thead tr th:first-child {
+    @apply border-l rounded-tl-sm;
+  }
+  thead tr th:last-child {
+    @apply border-r rounded-tr-sm;
+  }
+  thead tr:last-child th {
+    @apply border-b;
+  }
+  tbody tr {
+    @apply border-t border-gray-200;
+  }
+  tbody tr:first-child {
+    @apply border-t-0;
+  }
+  tbody td {
+    @apply border-b border-gray-200;
+  }
+  tbody td:first-child {
+    @apply border-l;
+  }
+  tbody td:last-child {
+    @apply border-r;
+  }
+  tbody tr:last-child td:first-child {
+    @apply rounded-bl-sm;
+  }
+  tbody tr:last-child td:last-child {
+    @apply rounded-br-sm;
+  }
+</style>
