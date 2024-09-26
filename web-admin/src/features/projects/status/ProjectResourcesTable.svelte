4a25326d023911af318736be87cd7785a77a8670
Cyrus Goh <hello@cyrusgoh.com>
2024-09-26 09:52:48 -0700

    truncate header too

--- web-admin/src/features/projects/status/ProjectResourcesTable.svelte
+++ web-admin/src/features/projects/status/ProjectResourcesTable.svelte
@@ -133,7 +133,7 @@
                 <div
                   class:cursor-pointer={header.column.getCanSort()}
                   class:select-none={header.column.getCanSort()}
-                  class="font-semibold text-gray-500 flex flex-row items-center gap-x-1"
+                  class="font-semibold text-gray-500 flex flex-row items-center gap-x-1 truncate"
                 >
                   <svelte:component
                     this={flexRender(
