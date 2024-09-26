d810323d7bb50e98e9acee2c582ae324f345f9a5
Cyrus Goh <hello@cyrusgoh.com>
2024-09-26 11:18:38 -0700

    add basic table component

--- web-admin/src/features/organizations/users/OrgGroupsTable.svelte
+++ web-admin/src/features/organizations/users/OrgGroupsTable.svelte
@@ -1,64 +1,13 @@
 <script lang="ts">
-  import Tag from "@rilldata/web-common/components/tag/Tag.svelte";
-  import { prettyResourceKind } from "@rilldata/web-common/features/entity-management/resource-selectors";
   import type { V1MemberUsergroup } from "@rilldata/web-admin/client";
-
-  import { writable } from "svelte/store";
-  import ArrowDown from "@rilldata/web-common/components/icons/ArrowDown.svelte";
-  import {
-    createSvelteTable,
-    flexRender,
-    getCoreRowModel,
-    getSortedRowModel,
-  } from "@tanstack/svelte-table";
-  import type {
-    ColumnDef,
-    OnChangeFn,
-    SortingState,
-    TableOptions,
-  } from "@tanstack/svelte-table";
+  import { flexRender } from "@tanstack/svelte-table";
+  import type { ColumnDef } from "@tanstack/svelte-table";
   import OrgGroupsAction from "./OrgGroupsAction.svelte";
+  import BasicTable from "@rilldata/web-common/components/table/BasicTable.svelte";
+  import { formatDate } from "@rilldata/web-common/components/table/utils";
 
   export let data: V1MemberUsergroup[];
   export let onDelete: (deletedGroupName: string) => void;
-
-  let sorting: SortingState = [];
-
-  function formatDate(value: string) {
-    return new Date(value).toLocaleDateString(undefined, {
-      year: "numeric",
-      month: "short",
-      day: "numeric",
-      hour: "numeric",
-      minute: "numeric",
-    });
-  }
-
-  $: safeData = Array.isArray(data) ? data : [];
-  $: {
-    if (safeData) {
-      options.update((old) => ({
-        ...old,
-        data: safeData,
-      }));
-    }
-  }
-
-  const setSorting: OnChangeFn<SortingState> = (updater) => {
-    if (updater instanceof Function) {
-      sorting = updater(sorting);
-    } else {
-      sorting = updater;
-    }
-
-    options.update((old) => ({
-      ...old,
-      state: {
-        ...old.state,
-        sorting,
-      },
-    }));
-  };
 
   const columns: ColumnDef<V1MemberUsergroup, any>[] = [
     {
@@ -100,119 +49,6 @@
         }),
     },
   ];
-
-  const options = writable<TableOptions<V1MemberUsergroup>>({
-    data: data,
-    columns: columns,
-    state: {
-      sorting,
-    },
-    onSortingChange: setSorting,
-    getCoreRowModel: getCoreRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-  });
-
-  const table = createSvelteTable(options);
 </script>
 
-<div class="overflow-x-auto">
-  <table class="w-full">
-    <thead>
-      {#each $table.getHeaderGroups() as headerGroup}
-        <tr>
-          {#each headerGroup.headers as header}
-            <th
-              colSpan={header.colSpan}
-              class="px-4 py-2 text-left"
-              on:click={header.column.getToggleSortingHandler()}
-            >
-              {#if !header.isPlaceholder}
-                <div
-                  class:cursor-pointer={header.column.getCanSort()}
-                  class:select-none={header.column.getCanSort()}
-                  class="font-semibold text-gray-500 flex flex-row items-center gap-x-1 truncate"
-                >
-                  <svelte:component
-                    this={flexRender(
-                      header.column.columnDef.header,
-                      header.getContext(),
-                    )}
-                  />
-                  {#if header.column.getIsSorted().toString() === "asc"}
-                    <span>
-                      <ArrowDown flip size="12px" />
-                    </span>
-                  {:else if header.column.getIsSorted().toString() === "desc"}
-                    <span>
-                      <ArrowDown size="12px" />
-                    </span>
-                  {/if}
-                </div>
-              {/if}
-            </th>
-          {/each}
-        </tr>
-      {/each}
-    </thead>
-    <tbody>
-      {#each $table.getRowModel().rows as row}
-        <tr>
-          {#each row.getVisibleCells() as cell}
-            <td
-              class={`px-4 py-2 truncate ${cell.column.id === "actions" ? "w-1" : ""}`}
-              data-label={cell.column.columnDef.header}
-            >
-              <svelte:component
-                this={flexRender(cell.column.columnDef.cell, cell.getContext())}
-              />
-            </td>
-          {/each}
-        </tr>
-      {/each}
-    </tbody>
-  </table>
-</div>
-
-<style lang="postcss">
-  table {
-    @apply border-separate border-spacing-0;
-  }
-  table th,
-  table td {
-    @apply border-b border-gray-200;
-  }
-
-  thead tr th {
-    @apply border-t border-gray-200;
-  }
-  thead tr th:first-child {
-    @apply border-l rounded-tl-sm;
-  }
-  thead tr th:last-child {
-    @apply border-r rounded-tr-sm;
-  }
-  thead tr:last-child th {
-    @apply border-b;
-  }
-  tbody tr {
-    @apply border-t border-gray-200;
-  }
-  tbody tr:first-child {
-    @apply border-t-0;
-  }
-  tbody td {
-    @apply border-b border-gray-200;
-  }
-  tbody td:first-child {
-    @apply border-l;
-  }
-  tbody td:last-child {
-    @apply border-r;
-  }
-  tbody tr:last-child td:first-child {
-    @apply rounded-bl-sm;
-  }
-  tbody tr:last-child td:last-child {
-    @apply rounded-br-sm;
-  }
-</style>
+<BasicTable {data} {columns} />
