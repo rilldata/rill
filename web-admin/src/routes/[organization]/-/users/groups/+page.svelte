493b28e52f33b5b9d9096ca5ba869e54c9435878
Cyrus Goh <hello@cyrusgoh.com>
2024-09-30 13:50:22 -0700

    lint

--- web-admin/src/routes/[organization]/-/users/groups/+page.svelte
+++ web-admin/src/routes/[organization]/-/users/groups/+page.svelte
@@ -2,7 +2,6 @@
   import { page } from "$app/stores";
   import {
     createAdminServiceAddOrganizationMemberUsergroup,
-    createAdminServiceAddUsergroupMemberUser,
     createAdminServiceCreateUsergroup,
     createAdminServiceDeleteUsergroup,
     createAdminServiceListOrganizationMemberUsergroups,
