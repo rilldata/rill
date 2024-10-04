5dd15f4dce55b90cf07ca0a429a03734f632aec7
Cyrus Goh <hello@cyrusgoh.com>
2024-10-04 16:24:27 -0700

    initial setup, wip

--- web-admin/src/features/projects/user-invite/UserInviteButton.svelte
+++ web-admin/src/features/projects/user-invite/UserInviteButton.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
   import { page } from "$app/stores";
+  import {
+    createAdminServiceListProjectInvites,
+    createAdminServiceListProjectMemberUsergroups,
+    createAdminServiceListProjectMemberUsers,
+  } from "@rilldata/web-admin/client";
   import CopyInviteLinkButton from "@rilldata/web-admin/features/projects/user-invite/CopyInviteLinkButton.svelte";
   import UserInviteAllowlist from "@rilldata/web-admin/features/projects/user-invite/UserInviteAllowlist.svelte";
   import UserInviteForm from "@rilldata/web-admin/features/projects/user-invite/UserInviteForm.svelte";
@@ -9,12 +14,39 @@
     DropdownMenuTrigger,
     DropdownMenuContent,
   } from "@rilldata/web-common/components/dropdown-menu";
+  import type { V1UserInvite } from "@rilldata/web-admin/client";
 
   export let organization: string;
   export let project: string;
+
   let open = false;
 
   $: copyLink = `${$page.url.protocol}//${$page.url.host}/${organization}/${project}`;
+
+  $: listProjectMemberUsergroups =
+    createAdminServiceListProjectMemberUsergroups(organization, project);
+  $: listProjectMemberUsers = createAdminServiceListProjectMemberUsers(
+    organization,
+    project,
+  );
+  $: listProjectInvites = createAdminServiceListProjectInvites(
+    organization,
+    project,
+  );
+
+  $: userGroupsList = $listProjectMemberUsergroups.data?.members ?? [];
+  $: usersList = $listProjectMemberUsers.data?.members ?? [];
+  $: invitesList = $listProjectInvites.data?.invites ?? [];
+
+  function coerceInvitesToUsers(invites: V1UserInvite[]) {
+    return invites.map((invite) => ({
+      ...invite,
+      userEmail: invite.email,
+      roleName: invite.role,
+    }));
+  }
+
+  $: users = [...usersList, ...coerceInvitesToUsers(invitesList)];
 </script>
 
 <DropdownMenu bind:open>
@@ -24,7 +56,7 @@
   <DropdownMenuContent class="w-[520px] p-4" side="bottom" align="end">
     <div class="flex flex-col gap-y-3">
       <div class="flex flex-row items-center">
-        <div class="text-sm font-medium">Share this project</div>
+        <div class="text-sm font-medium">{project}</div>
         <div class="grow"></div>
         <CopyInviteLinkButton {copyLink} />
       </div>
@@ -34,6 +66,23 @@
         onInvite={() => (open = false)}
       />
       <UserInviteAllowlist {organization} {project} />
+      <!-- <div class="text-xs text-gray-500 font-semibold uppercase">
+        Organization
+      </div> -->
+      <div>
+        <div class="text-xs text-gray-500 font-semibold uppercase">Groups</div>
+        <!-- TODO: create AvatarSquareList -->
+        {#each userGroupsList as group}
+          <div class="text-xs text-gray-500">{group.groupName}</div>
+        {/each}
+      </div>
+      <div>
+        <div class="text-xs text-gray-500 font-semibold uppercase">Users</div>
+        <!-- TODO: use AvatarCircleList -->
+        {#each users as user}
+          <div class="text-xs text-gray-500">{user.userEmail}</div>
+        {/each}
+      </div>
     </div>
   </DropdownMenuContent>
 </DropdownMenu>
