8002338047d4da13a256d945a3c5c9f8834960b5
Cyrus Goh <hello@cyrusgoh.com>
2024-10-07 13:56:21 -0700

    user invite org and group

---
+++ web-admin/src/features/projects/user-invite/UserInviteOrganization.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { createAdminServiceListOrganizationMemberUsers } from "@rilldata/web-admin/client";
+  import AvatarSquareList from "../../organizations/users/AvatarSquareList.svelte";
+
+  export let organization: string;
+
+  $: listOrganizationMemberUsers =
+    createAdminServiceListOrganizationMemberUsers(organization);
+  $: organizationUsersCount =
+    $listOrganizationMemberUsers.data?.members?.length ?? 0;
+</script>
+
+<AvatarSquareList name={organization} count={organizationUsersCount}>
+  {@html `Everyone from <span class="font-bold">${organization}</span>`}
+</AvatarSquareList>
