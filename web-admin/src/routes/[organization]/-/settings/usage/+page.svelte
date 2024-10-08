1e5f1dd800445f8a1fb13f66a2d05cd8be01a53f
Aditya Hegde <adityahegderocks@gmail.com>
2024-10-08 13:20:28 +0530

    Embed Orb customer portal

--- web-admin/src/routes/[organization]/-/settings/usage/+page.svelte
+++ web-admin/src/routes/[organization]/-/settings/usage/+page.svelte
@@ -1,1 +1,17 @@
-Coming soon
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { createAdminServiceGetBillingSubscription } from "@rilldata/web-admin/client";
+
+  $: organization = $page.params.organization;
+
+  $: billingSub = createAdminServiceGetBillingSubscription(organization);
+  $: billingUrl = $billingSub.data?.billingPortalUrl;
+</script>
+
+{#if billingUrl}
+  <iframe
+    src={billingUrl}
+    title="Orb Billing Portal"
+    class="w-full h-[600px]"
+  />
+{/if}
