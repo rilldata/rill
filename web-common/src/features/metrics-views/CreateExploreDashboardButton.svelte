3226b9404e91c7bf1c3620dbc29924eac7fe7274
Eric P Green <ericpgreen2@gmail.com>
2024-10-09 17:43:58 -0400

    "Create Explore Dashboard" should navigate to Explore preview

--- web-common/src/features/metrics-views/CreateExploreDashboardButton.svelte
+++ web-common/src/features/metrics-views/CreateExploreDashboardButton.svelte
@@ -1,39 +1,43 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
+  import { useQueryClient } from "@tanstack/svelte-query";
+  import { get } from "svelte/store";
   import { Button } from "../../components/button";
-  import { type V1Resource, runtimeServicePutFile } from "../../runtime-client";
+  import { waitUntil } from "../../lib/waitUtils";
+  import { type V1Resource } from "../../runtime-client";
   import { runtime } from "../../runtime-client/runtime-store";
+  import { fileArtifacts } from "../entity-management/file-artifacts";
   import { ResourceKind } from "../entity-management/resource-selectors";
-  import {
-    generateBlobForNewResourceFile,
-    getPathForNewResourceFile,
-  } from "../file-explorer/new-files";
+  import { handleEntityCreate } from "../file-explorer/new-files";
+
+  const queryClient = useQueryClient();
 
   export let metricsViewResource: V1Resource | undefined;
 
   $: ({ instanceId } = $runtime);
 
-  async function createExploreFromMetricsView(baseResource: V1Resource) {
-    try {
-      const newFilePath = getPathForNewResourceFile(
-        ResourceKind.Explore,
-        baseResource,
-      );
+  async function handleCreateDashboard() {
+    // Create the Explore file
+    const newExploreFilePath = await handleEntityCreate(
+      ResourceKind.Explore,
+      metricsViewResource,
+    );
 
-      await runtimeServicePutFile(instanceId, {
-        path: newFilePath,
-        blob: generateBlobForNewResourceFile(
-          ResourceKind.Explore,
-          baseResource,
-        ),
-        create: true,
-        createOnly: true,
-      });
-
-      await goto(`/files/${newFilePath}`);
-    } catch (error) {
-      console.error(error);
+    // Wait until the Explore resource is ready
+    const exploreFileArtifact =
+      fileArtifacts.getFileArtifact(newExploreFilePath);
+    const exploreResource = exploreFileArtifact.getResource(
+      queryClient,
+      instanceId,
+    );
+    await waitUntil(() => get(exploreResource).data !== undefined);
+    const newExploreName = get(exploreResource).data?.meta?.name?.name;
+    if (!newExploreName) {
+      throw new Error("Failed to create an Explore resource");
     }
+
+    // Navigate to the Explore Preview
+    await goto(`/explore/${newExploreName}`);
   }
 </script>
 
@@ -41,9 +45,7 @@
   type="primary"
   disabled={!metricsViewResource}
   on:click={() => {
-    if (metricsViewResource) {
-      void createExploreFromMetricsView(metricsViewResource);
-    }
+    if (metricsViewResource) void handleCreateDashboard();
   }}
 >
   Create Explore dashboard
