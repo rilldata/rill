949f8e90873a0295569c4479d3eb39b4efac504b
Cyrus Goh <hello@cyrusgoh.com>
2024-10-09 09:11:43 -0700

    wip tooltip

---
+++ web-admin/src/features/projects/user-invite/UserInviteMultipleAccessTooltip.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  // import Tooltip from "@rilldata/web-common/components/tooltip/Tooltip.svelte";
+  // import TooltipContent from "@rilldata/web-common/components/tooltip/TooltipContent.svelte";
+  // import InfoCircle from "@rilldata/web-common/components/icons/InfoCircle.svelte";
+</script>
+
+<div class="flex flex-row items-center gap-x-1">
+  <!-- <Tooltip location="bottom" alignment="middle" distance={8}>
+    <div class="text-yellow-600">
+      <InfoCircle size="16px" />
+    </div>
+    <TooltipContent maxWidth="400px" slot="tooltip-content">
+      This person can still edit because they are part of the group
+    </TooltipContent>
+  </Tooltip> -->
+  <slot />
+</div>
