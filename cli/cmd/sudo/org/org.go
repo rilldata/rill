b997f2cbf66a4800b1e1907fed0fa9e49da4489c
Parag Jain <pjain1@apache.org>
2024-10-21 16:25:00 +0530

    review comments

--- cli/cmd/sudo/org/org.go
+++ cli/cmd/sudo/org/org.go
@@ -12,11 +12,11 @@ func OrgCmd(ch *cmdutil.Helper) *cobra.Command {
 		PersistentPreRunE: cmdutil.CheckAuth(ch),
 	}
 
-	cmd.AddCommand(CreateCmd(ch))
 	cmd.AddCommand(ShowCmd(ch))
 	cmd.AddCommand(JoinCmd(ch))
 	cmd.AddCommand(ListAdminsCmd(ch))
 	cmd.AddCommand(SetCustomDomainCmd(ch))
+	cmd.AddCommand(SetInternalPlanCmd(ch))
 
 	return cmd
 }
