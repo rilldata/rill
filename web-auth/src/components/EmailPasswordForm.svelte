4c6565caeee4ab158859e331118709f164dfab74
Cyrus Goh <hello@cyrusgoh.com>
2024-10-24 15:42:08 -0700

    reorg

---
+++ web-auth/src/components/EmailPasswordForm.svelte
@@ -0,0 +1,223 @@
+<script lang="ts">
+  import CtaButton from "@rilldata/web-common/components/calls-to-action/CTAButton.svelte";
+  import Eye from "@rilldata/web-common/components/icons/Eye.svelte";
+  import EyeInvisible from "@rilldata/web-common/components/icons/EyeInvisible.svelte";
+  import { createEventDispatcher } from "svelte";
+  import { ArrowLeftIcon } from "lucide-svelte";
+  import { WebAuth } from "auth0-js";
+  import { DATABASE_CONNECTION } from "../constants";
+  import { AuthStep } from "../types";
+  import type { Auth0Error } from "auth0-js";
+
+  const dispatch = createEventDispatcher();
+
+  export let disabled = false;
+  export let email = "";
+  export let showForgetPassword = false;
+  export let isDomainDisabled = false;
+  export let isEmailDisabled = false;
+  export let webAuth: WebAuth;
+  export let step: AuthStep;
+
+  let password = "";
+  let showForm = false;
+  let showPassword = false;
+  let errorText = "";
+
+  let inputClasses =
+    "h-10 px-4 py-2 border border-slate-300 rounded-sm text-base";
+  let focusClasses =
+    "ring-offset-2 focus:ring-2 focus:ring-primary-ry-300 focus:outline-none";
+
+  $: type = showPassword ? "text" : "password";
+
+  function onPassInput(e: any) {
+    password = e.target.value;
+  }
+
+  function handleSubmit() {
+    if (!showForm) {
+      showForm = true;
+      return;
+    }
+
+    if (!password) {
+      errorText = "Please enter your password";
+      return;
+    }
+
+    errorText = "";
+
+    authenticateUser;
+  }
+
+  function displayError(err: any) {
+    errorText = err.message;
+  }
+
+  function handleResetPassword() {
+    errorText = "";
+    if (!email) return displayError({ message: "Please enter an email" });
+
+    if (isDomainDisabled) {
+      return displayError({
+        message: "Password reset is not available. Please contact your admin.",
+      });
+    }
+
+    webAuth.changePassword(
+      {
+        connection: DATABASE_CONNECTION,
+        email: email,
+      },
+      (err, resp) => {
+        if (err) displayError({ message: err?.description });
+        else alert(resp);
+      },
+    );
+  }
+
+  function handleAuthError(err: Auth0Error) {
+    // NOTE: Auth0 is not consistent in the naming of the error description field
+    const errorText =
+      typeof err?.description === "string"
+        ? err.description
+        : typeof err?.policy === "string"
+          ? err.policy
+          : typeof err?.error_description === "string"
+            ? err.error_description
+            : err?.errorDescription;
+
+    displayError({ message: errorText });
+    isEmailDisabled = false;
+  }
+
+  function authenticateUser(password: string) {
+    isEmailDisabled = true;
+    errorText = "";
+
+    try {
+      if (step === AuthStep.SignUp) {
+        // Directly attempt to sign up and log in the user
+        webAuth.redirect.signupAndLogin(
+          {
+            connection: DATABASE_CONNECTION,
+            email: email,
+            password: password,
+          },
+          (signupErr: any) => {
+            if (signupErr) handleAuthError(signupErr);
+            else isEmailDisabled = false;
+          },
+        );
+      } else {
+        webAuth.login(
+          {
+            realm: DATABASE_CONNECTION,
+            username: email,
+            password: password,
+          },
+          (err) => {
+            if (err) {
+              // Check if the error indicates the user does not exist
+              // e.g. Wrong email or password
+              // {
+              //   "message": "Wrong email or password.",
+              //   "name": "ValidationError",
+              //   "code": "invalid_user_password",
+              //   "description": "Wrong email or password.",
+              //   "statusCode": 400
+              // }
+              if (
+                err.statusCode === 400 &&
+                err.code === "invalid_user_password"
+              ) {
+                // Attempt to sign up the user
+                webAuth.redirect.signupAndLogin(
+                  {
+                    connection: DATABASE_CONNECTION,
+                    email: email,
+                    password: password,
+                  },
+                  (signupErr: any) => {
+                    if (signupErr) handleAuthError(signupErr);
+                    else isEmailDisabled = false;
+                  },
+                );
+              } else {
+                handleAuthError(err);
+              }
+            } else {
+              isEmailDisabled = false;
+            }
+          },
+        );
+      }
+    } catch (err) {
+      handleAuthError(err);
+    }
+  }
+
+  $: disabled = !(password.length > 0);
+</script>
+
+<form on:submit={handleSubmit} class="flex flex-col gap-y-4">
+  <div class="relative">
+    <!-- TODO: look into using <Input /> component -->
+    <input
+      class="{inputClasses} {focusClasses}"
+      style:width="400px"
+      {type}
+      on:input={onPassInput}
+      id="password"
+      placeholder="Password"
+    />
+
+    {#if errorText}
+      <div style:max-width="400px" class="text-red-500 text-sm mt-3">
+        {errorText}
+      </div>
+    {/if}
+
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <span
+      role="button"
+      tabindex="0"
+      style:right="10px"
+      class="absolute top-1/2 transform -translate-y-1/2 cursor-pointer"
+      on:click={() => (showPassword = !showPassword)}
+    >
+      {#if !showPassword}
+        <Eye />
+      {:else}
+        <EyeInvisible />
+      {/if}
+    </span>
+  </div>
+
+  {#if showForgetPassword}
+    <div class="text-left">
+      <button
+        type="button"
+        on:click={handleResetPassword}
+        class="text-sm text-slate-500 pl-1 font-medium">Forgot password?</button
+      >
+    </div>
+  {/if}
+
+  <CtaButton {disabled} variant="primary" submitForm>
+    <div class="flex justify-center font-medium">Continue</div>
+  </CtaButton>
+  <CtaButton
+    variant="secondary"
+    gray
+    on:click={() => {
+      dispatch("back");
+    }}
+  >
+    <div class="flex justify-center items-center font-medium">
+      <ArrowLeftIcon class="mr-1" size={14} />
+      <span>Back</span>
+    </div>
+  </CtaButton>
+</form>
