9086d04d680850470316da93d488169dcc3ce680
Dhiraj Kumar <sumankumaribarnwal@gmail.com>
2024-10-28 20:23:06 -0700

    add value type for formatter component

--- web-common/src/components/data-types/FormattedDataType.svelte
+++ web-common/src/components/data-types/FormattedDataType.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   /** provides the formatting for data types */
+  import type { PERC_DIFF } from "@rilldata/web-common/components/data-types/type-utils";
   import {
     INTERVALS,
     NUMERICS,
     TIMESTAMPS,
   } from "@rilldata/web-common/lib/duckdb-data-types";
+  import type { NumberParts } from "@rilldata/web-common/lib/number-formatting/humanizer-types";
   import Interval from "./Interval.svelte";
   import MeasureChange from "./MeasureChange.svelte";
   import Number from "./Number.svelte";
@@ -16,7 +18,14 @@
   export let isNull = false;
   export let inTable = false;
   export let dark = false;
-  export let value = undefined;
+  export let value:
+    | string
+    | boolean
+    | number
+    | null
+    | undefined
+    | NumberParts
+    | PERC_DIFF = undefined;
   export let customStyle = "";
   export let truncate = false;
 
@@ -41,7 +50,7 @@ PercentageChange  and MeasureChange don't take a `type` prop,
 so instantiating these directly clears a ton of warnings
 about unknown props.
 -->
-{#if type === "RILL_PERCENTAGE_CHANGE"}
+{#if type === "RILL_PERCENTAGE_CHANGE" && typeof value !== "boolean"}
   <PercentageChange {value} {isNull} {inTable} {customStyle} {dark} />
 {:else if type === "RILL_CHANGE"}
   <MeasureChange {value} {inTable} {customStyle} {dark} />
