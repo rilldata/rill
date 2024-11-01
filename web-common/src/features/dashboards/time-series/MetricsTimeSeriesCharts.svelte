fe0716256dd336ce4111ce4a133a93ee53e4f79b
Dhiraj Barnwal <sumankumaribarnwal@gmail.com>
2024-11-01 15:28:46 -0700

    Feat: Time comparison for dimensions charts (#5419)
    * feat: add time comparison lines for dimension
    
    * Hide dimension time comparison line on default
    
    * Dash comparison line
    
    * Add comparison bars to stacked bar chart
    
    * Update hover label for line time comparison
    
    * Fix nits
    
    * Update main line color for comparison
    
    * Modify opacity and line width
    
    * Fix dimension hover bug
    
    * Add comparison bars to grouped bar
    
    * Order comparison bar at end
    
    * Fix line width bug
    
    * Fix hanging point bug
    
    * Cleanup
    
    * Add scrub to comparison charts

--- web-common/src/features/dashboards/time-series/MetricsTimeSeriesCharts.svelte
+++ web-common/src/features/dashboards/time-series/MetricsTimeSeriesCharts.svelte
@@ -3,6 +3,7 @@
   import SimpleDataGraphic from "@rilldata/web-common/components/data-graphic/elements/SimpleDataGraphic.svelte";
   import { Axis } from "@rilldata/web-common/components/data-graphic/guides";
   import { bisectData } from "@rilldata/web-common/components/data-graphic/utils";
+  import DashboardVisibilityDropdown from "@rilldata/web-common/components/menu/shadcn/DashboardVisibilityDropdown.svelte";
   import { LeaderboardContextColumn } from "@rilldata/web-common/features/dashboards/leaderboard-context-column";
   import ReplacePivotDialog from "@rilldata/web-common/features/dashboards/pivot/ReplacePivotDialog.svelte";
   import {
@@ -44,7 +45,6 @@
     getOrderedStartEnd,
     updateChartInteractionStore,
   } from "./utils";
-  import DashboardVisibilityDropdown from "@rilldata/web-common/components/menu/shadcn/DashboardVisibilityDropdown.svelte";
 
   export let exploreName: string;
   export let workspaceWidth: number;
@@ -82,11 +82,11 @@
 
   $: expandedMeasureName = $exploreStore?.tdd?.expandedMeasureName;
   $: isInTimeDimensionView = Boolean(expandedMeasureName);
+
   $: comparisonDimension = $exploreStore?.selectedComparisonDimension;
-  $: showComparison = Boolean(
-    !comparisonDimension && $timeControlsStore.showTimeComparison,
-  );
+  $: showComparison = Boolean($timeControlsStore.showTimeComparison);
   $: tddChartType = $exploreStore?.tdd?.chartType;
+
   $: interval =
     $timeControlsStore.selectedTimeRange?.interval ??
     $timeControlsStore.minTimeGrain;
