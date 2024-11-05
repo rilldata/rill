6b81911c540c833836daa270ceb9371059b547bb
Aditya Hegde <adityahegderocks@gmail.com>
2024-11-05 12:28:25 +0530

    Add resolvers

--- runtime/pkg/rilltime/rilltime.go
+++ runtime/pkg/rilltime/rilltime.go
@@ -2,87 +2,193 @@ package rilltime
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/alecthomas/participle/v2"
+	"github.com/alecthomas/participle/v2/lexer"
+	"github.com/rilldata/rill/runtime/pkg/timeutil"
 )
 
-// RillTime => Time (, Time)? (: Modifiers)?
+var (
+	rillTimeLexer = lexer.MustSimple([]lexer.SimpleRule{
+		{"Now", `now`},
+		{"Latest", `latest`},
+		{"Earliest", `earliest`},
+		// this needs to be after Now and Latest to match to them
+		{"Grain", `[smhdDWQMY]`},
+		// this has to be at the end
+		{"String", `[a-zA-Z]\w*`},
+		{"Number", `[-+]?\d+`},
+		// needed for random chars used
+		{"Punct", `[-[!@#$%^&*()+_={}\|:;"'<,>.?/]|]`},
+		{"Whitespace", `[ \t\n\r]+`},
+	})
+	rillTimeParser = participle.MustBuild[RillTime](
+		participle.Lexer(rillTimeLexer),
+		participle.Elide("Whitespace"),
+	)
+	daxOffsetRangeNotations = map[string]timeutil.TimeGrain{
+		"s": timeutil.TimeGrainSecond,
+		"m": timeutil.TimeGrainMinute,
+		"h": timeutil.TimeGrainHour,
+		"d": timeutil.TimeGrainDay,
+		"D": timeutil.TimeGrainDay,
+		"W": timeutil.TimeGrainWeek,
+		"Q": timeutil.TimeGrainQuarter,
+		"M": timeutil.TimeGrainMonth,
+		"Y": timeutil.TimeGrainYear,
+	}
+)
+
 type RillTime struct {
-	Start     *Time      `parser:"  @@"`
-	End       *Time      `parser:"(',' @@)?"`
-	Modifiers *Modifiers `parser:"(':' @@)?"`
+	Start      *TimeModifier `parser:"  @@"`
+	End        *TimeModifier `parser:"(',' @@)?"`
+	Modifiers  *Modifiers    `parser:"(':' @@)?"`
+	Grain      timeutil.TimeGrain
+	IsComplete bool
+	TimeZone   *time.Location
 }
 
-// Time => -?[1-9][0-9]* (/\W+)?
-//       | now (/\W+)?
-type Time struct {
-	Neg   bool    `parser:"( @'-'?"`
-	Num   *int    `parser:"  @Int"`
-	Grain *string `parser:"  @Ident"`
-	Now   bool    `parser:"| @'now')"`
-	Trunc *string `parser:"  ('/' @Ident)?"`
+type TimeModifier struct {
+	Num      *int    `parser:"( @Number?"`
+	Grain    *string `parser:"  @Grain"`
+	Now      bool    `parser:"| @Now"`
+	Latest   bool    `parser:"| @Latest"`
+	Earliest bool    `parser:"| @Earliest)"`
+	Trunc    *string `parser:"  ('/' @Grain)?"`
 }
 
-// Modifiers => \W+ | |\W+|
 type Modifiers struct {
-	Grain  *string `parser:"(@Ident | '|' @Ident '|')?"`
-	Offset *Time   `parser:"('@' @@)?"`
+	Grain         *GrainModifier `parser:"( @@"`
+	CompleteGrain *GrainModifier `parser:"| '|' @@ '|')?"`
+	At            *AtModifiers   `parser:"( '@' @@)?"`
 }
 
-func (t *RillTime) String() string {
-	time := ""
-	if t.Start != nil {
-		time = t.Start.String()
-	}
-	if t.End != nil {
-		time += ", " + t.End.String()
-	}
-	if t.Modifiers != nil {
-		time += " :" + t.Modifiers.String()
-	}
-	return time
+type GrainModifier struct {
+	Num   *int   `parser:"(@Number)?"`
+	Grain string `parser:"@Grain"`
 }
 
-func (t *Time) String() string {
-	time := ""
-	if t.Now {
-		time = "now"
-	} else {
-		if t.Neg {
-			time += "-"
-		}
-		if t.Num != nil {
-			time += fmt.Sprintf("%d", *t.Num)
-		}
-		if t.Grain != nil {
-			time += *t.Grain
-		}
-	}
-	if t.Trunc != nil {
-		time += "/" + *t.Trunc
-	}
-	return time
+type AtModifiers struct {
+	Offset   *TimeModifier `parser:"@@?"`
+	TimeZone *string       `parser:"('{' @String '}')?"`
 }
 
-func (m *Modifiers) String() string {
-	time := ""
-	if m.Grain != nil {
-		time += " |" + *m.Grain + "|"
-	}
-	if m.Offset != nil {
-		time += " @" + m.Offset.String()
-	}
-	return time
+type ResolverContext struct {
+	Now        time.Time
+	MinTime    time.Time
+	MaxTime    time.Time
+	FirstDay   int
+	FirstMonth int
 }
 
 func Parse(timeRange string) (*RillTime, error) {
-	parser, err := participle.Build[RillTime]()
+	ast, err := rillTimeParser.ParseString("", timeRange)
 	if err != nil {
 		return nil, err
 	}
-	ast, err := parser.ParseString("", timeRange)
-	if err != nil {
-		return nil, err
+
+	ast.TimeZone = time.UTC
+	if ast.Modifiers != nil {
+		if ast.Modifiers.Grain != nil {
+			ast.Grain = daxOffsetRangeNotations[ast.Modifiers.Grain.Grain]
+			// TODO: non-1 grains
+		} else if ast.Modifiers.CompleteGrain != nil {
+			ast.Grain = daxOffsetRangeNotations[ast.Modifiers.CompleteGrain.Grain]
+			// TODO: non-1 grains
+			ast.IsComplete = true
+		}
+
+		if ast.Modifiers.At != nil {
+			if ast.Modifiers.At.TimeZone != nil {
+				var err error
+				ast.TimeZone, err = time.LoadLocation(*ast.Modifiers.At.TimeZone)
+				if err != nil {
+					return nil, fmt.Errorf("invalid time zone %q: %w", *ast.Modifiers.At.TimeZone, err)
+				}
+			}
+		}
 	}
+
+	if ast.End == nil {
+		ast.End = &TimeModifier{
+			Now: true,
+		}
+	}
+
 	return ast, nil
+}
+
+func (r *RillTime) Resolve(resolverCtx ResolverContext) (time.Time, time.Time, error) {
+	if r.Modifiers != nil && r.Modifiers.At != nil && r.Modifiers.At.Offset != nil {
+		resolverCtx.Now = r.ModifyTime(resolverCtx, resolverCtx.Now, r.Modifiers.At.Offset)
+		resolverCtx.MinTime = r.ModifyTime(resolverCtx, resolverCtx.MinTime, r.Modifiers.At.Offset)
+		resolverCtx.MaxTime = r.ModifyTime(resolverCtx, resolverCtx.MaxTime, r.Modifiers.At.Offset)
+	}
+
+	start := resolverCtx.MaxTime
+	if r.Start != nil {
+		start = r.ModifyTime(resolverCtx, start, r.Start)
+	}
+
+	end := resolverCtx.MaxTime
+	if r.End != nil {
+		end = r.ModifyTime(resolverCtx, end, r.End)
+	}
+
+	return start, end, nil
+}
+
+func (r *RillTime) ModifyTime(resolverCtx ResolverContext, t time.Time, tm *TimeModifier) time.Time {
+	isTruncate := true
+	grain := r.Grain
+
+	if tm.Now {
+		t = resolverCtx.Now.In(r.TimeZone)
+		isTruncate = r.IsComplete
+	} else if tm.Earliest {
+		t = resolverCtx.MinTime.In(r.TimeZone)
+	} else if tm.Latest {
+		t = resolverCtx.MaxTime.In(r.TimeZone)
+		isTruncate = r.IsComplete
+	} else {
+		n := 0
+		if tm.Num != nil {
+			n = *tm.Num
+		}
+		// TODO: what should the defaults here be?
+		g := "s"
+		if tm.Grain != nil {
+			g = *tm.Grain
+		}
+
+		t = t.In(r.TimeZone)
+		switch g {
+		case "s":
+			t = t.Add(time.Duration(n) * time.Second)
+		case "m":
+			t = t.Add(time.Duration(n) * time.Minute)
+		case "h":
+			t = t.Add(time.Duration(n) * time.Hour)
+		case "d":
+			t = t.AddDate(0, 0, n)
+		case "D":
+			t = t.AddDate(0, 0, n)
+		case "W":
+			t = t.AddDate(0, 0, n*7)
+		case "M":
+			t = t.AddDate(0, n, 0)
+		case "Q":
+			t = t.AddDate(0, n*3, 0)
+		case "Y":
+			t = t.AddDate(n, 0, 0)
+		}
+
+		grain = daxOffsetRangeNotations[g]
+	}
+
+	if isTruncate {
+		return timeutil.TruncateTime(t, grain, r.TimeZone, resolverCtx.FirstDay, resolverCtx.FirstMonth)
+	}
+	return timeutil.CeilTime(t, grain, r.TimeZone, resolverCtx.FirstDay, resolverCtx.FirstMonth)
 }
--- runtime/pkg/rilltime/rilltime_test.go
+++ runtime/pkg/rilltime/rilltime_test.go
@@ -1,30 +1,49 @@
 package rilltime
 
 import (
-	"fmt"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
-func Test_Parse(t *testing.T) {
-	rt, err := Parse("-1M/h")
-	require.NoError(t, err)
-	fmt.Println(rt.String())
+func Test_Resolve(t *testing.T) {
+	now := parseTestTime(t, "2024-08-09T10:32:36Z")
+	testCases := []struct {
+		timeRange string
+		start     string
+		end       string
+	}{
+		{`m : |s|`, "2024-08-09T10:32:00Z", "2024-08-09T10:32:36Z"},
+		{`-5m : |m|`, "2024-08-09T10:27:00Z", "2024-08-09T10:33:00Z"},
+		{`-5m, 0m : |m|`, "2024-08-09T10:27:00Z", "2024-08-09T10:32:00Z"},
+		{`h : m`, "2024-08-09T10:00:00Z", "2024-08-09T10:33:00Z"},
+		{`-7d, 0d : |h|`, "2024-08-02T00:00:00Z", "2024-08-09T00:00:00Z"},
+		{`-6d, now : |h|`, "2024-08-03T00:00:00Z", "2024-08-10T00:00:00Z"},
+		{`-6d, now : h`, "2024-08-03T00:00:00Z", "2024-08-11T00:00:00Z"},
+	}
 
-	rt, err = Parse("-1M/h : @-3M")
-	require.NoError(t, err)
-	fmt.Println(rt.String())
+	for _, tc := range testCases {
+		t.Run(tc.timeRange, func(t *testing.T) {
+			rt, err := Parse(tc.timeRange)
+			require.NoError(t, err)
 
-	rt, err = Parse("-1M/h, now")
-	require.NoError(t, err)
-	fmt.Println(rt.String())
+			start, end, err := rt.Resolve(ResolverContext{
+				Now:        now,
+				MinTime:    now.AddDate(-1, 0, 0),
+				MaxTime:    now,
+				FirstDay:   1,
+				FirstMonth: 1,
+			})
+			require.NoError(t, err)
+			require.Equal(t, parseTestTime(t, tc.start), start)
+			require.Equal(t, parseTestTime(t, tc.end), end)
+		})
+	}
+}
 
-	rt, err = Parse("-1M/h, now : @-3M")
-	require.NoError(t, err)
-	fmt.Println(rt.String())
-
-	rt, err = Parse("-1M/h, now : |h| @-3M")
-	require.NoError(t, err)
-	fmt.Println(rt.String())
+func parseTestTime(tst *testing.T, t string) time.Time {
+	ts, err := time.Parse(time.RFC3339, t)
+	require.NoError(tst, err)
+	return ts
 }
