7ea3cb073adac0430800774a0b0b4c6a13b94ac4
Brian Holmes <briangregoryholmes@gmail.com>
2024-11-05 10:50:42 -0500

    feat: create ZoneDisplay component

---
+++ web-common/src/features/dashboards/time-controls/super-pill/components/ZoneDisplay.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { getAbbreviationForIANA } from "@rilldata/web-common/lib/time/timezone";
+  import { IANAZone } from "luxon";
+
+  export let watermark = new Date();
+  export let iana: string;
+
+  $: zone = new IANAZone(iana);
+
+  $: watermarkTs = watermark.getTime();
+</script>
+
+<div class="flex items-center gap-x-1 text-xs cursor-pointer">
+  <b class="min-w-9 max-w-9">
+    {getAbbreviationForIANA(watermark, iana)}
+  </b>
+
+  <p class="inline-block italic min-w-20 max-w-20">
+    GMT {zone.formatOffset(watermarkTs, "short")}
+  </p>
+
+  <p class="truncate">
+    {iana}
+  </p>
+</div>
