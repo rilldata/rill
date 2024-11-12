689c342235fc289003f5953dc9af44c106ddf603
Cyrus Goh <hello@cyrusgoh.com>
2024-11-11 18:15:36 -0800

    actually triggers all sources and models

--- web-admin/src/features/projects/status/ProjectResources.svelte
+++ web-admin/src/features/projects/status/ProjectResources.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
   import DelayedSpinner from "@rilldata/web-common/features/entity-management/DelayedSpinner.svelte";
-  import { createRuntimeServiceListResources } from "@rilldata/web-common/runtime-client";
+  import {
+    createRuntimeServiceCreateTrigger,
+    createRuntimeServiceListResources,
+    getRuntimeServiceListResourcesQueryKey,
+    V1ReconcileStatus,
+  } from "@rilldata/web-common/runtime-client";
   import { runtime } from "@rilldata/web-common/runtime-client/runtime-store";
   import ProjectResourcesTable from "./ProjectResourcesTable.svelte";
   import Button from "web-common/src/components/button/Button.svelte";
+  import { useQueryClient } from "@tanstack/svelte-query";
+
+  const queryClient = useQueryClient();
+  const triggerMutation = createRuntimeServiceCreateTrigger();
+
+  let isReconciling = false;
 
   $: resources = createRuntimeServiceListResources(
     $runtime.instanceId,
@@ -18,31 +29,61 @@
               resource.meta.name.kind !== "rill.runtime.v1.ProjectParser",
           );
         },
+        enabled: true,
         refetchOnMount: true,
+        refetchInterval: isReconciling ? 500 : false,
       },
     },
   );
 
-  function refreshResources() {
-    $resources.refetch();
+  $: isAnySourceOrModelReconciling = $resources?.data?.some(
+    (resource) =>
+      resource.meta.reconcileStatus ===
+        V1ReconcileStatus.RECONCILE_STATUS_PENDING ||
+      resource.meta.reconcileStatus ===
+        V1ReconcileStatus.RECONCILE_STATUS_RUNNING,
+  );
+
+  function refreshAllSourcesAndModels() {
+    isReconciling = true;
+
+    void $triggerMutation.mutateAsync({
+      instanceId: $runtime.instanceId,
+      data: {
+        allSourcesModelsFull: true,
+      },
+    });
+
+    void queryClient.invalidateQueries(
+      getRuntimeServiceListResourcesQueryKey(
+        $runtime.instanceId,
+        // All resource "kinds"
+        undefined,
+      ),
+    );
   }
 
-  $: isLoadingOrRefetching = $resources.isLoading || $resources.isRefetching;
+  $: if (!isAnySourceOrModelReconciling) {
+    isReconciling = false;
+  }
 </script>
 
 <section class="flex flex-col gap-y-4">
   <div class="flex items-center justify-between">
     <h2 class="text-lg font-medium">Resources</h2>
-    <Button type="secondary" on:click={refreshResources}>
-      {#if $resources.isRefetching}
+    <Button type="secondary" on:click={refreshAllSourcesAndModels}>
+      {#if isReconciling}
         Refreshing...
       {:else}
         Refresh all sources and models
       {/if}
     </Button>
   </div>
-  {#if isLoadingOrRefetching}
-    <DelayedSpinner isLoading={isLoadingOrRefetching} size="16px" />
+  {#if $resources.isLoading || $resources.isRefetching}
+    <DelayedSpinner
+      isLoading={$resources.isLoading || $resources.isRefetching}
+      size="16px"
+    />
   {:else if $resources.isError}
     <div class="text-red-500">
       Error loading resources: {$resources.error?.message}
