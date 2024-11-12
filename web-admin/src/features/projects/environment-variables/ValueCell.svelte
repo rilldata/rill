36762e3c1b0ae3fcb4ae0af0035f08e41a8a3d50
Cyrus Goh <hello@cyrusgoh.com>
2024-11-12 13:20:52 -0800

    copy treatment for value cell

--- web-admin/src/features/projects/environment-variables/ValueCell.svelte
+++ web-admin/src/features/projects/environment-variables/ValueCell.svelte
@@ -1,19 +1,24 @@
 <script lang="ts">
   import EyeIcon from "@rilldata/web-common/components/icons/EyeIcon.svelte";
   import EyeOffIcon from "@rilldata/web-common/components/icons/EyeOffIcon.svelte";
+  import Tooltip from "@rilldata/web-common/components/tooltip/Tooltip.svelte";
+  import TooltipContent from "@rilldata/web-common/components/tooltip/TooltipContent.svelte";
 
   export let value: string;
 
   let showValue = false;
+  let copied = false;
+  let displayValue = value;
 
   $: inputType = showValue ? "text" : "password";
-
   $: isEmpty = value.length === 0;
+  $: isValueHidden = !showValue;
 
-  let displayValue = value;
+  // 16 characters
+  const REDACTED_VALUE = "****************";
+
   $: if (!showValue) {
-    // 16 characters
-    displayValue = "****************";
+    displayValue = REDACTED_VALUE;
   } else {
     if (isEmpty) {
       displayValue = "Empty";
@@ -25,6 +30,23 @@
   function toggleShowValue() {
     showValue = !showValue;
   }
+
+  function copyToClipboard(text: string) {
+    navigator.clipboard.writeText(text).catch(console.error);
+  }
+
+  function onCopy() {
+    if (isValueHidden) {
+      return;
+    }
+
+    copyToClipboard(value);
+    copied = true;
+
+    setTimeout(() => {
+      copied = false;
+    }, 2_000);
+  }
 </script>
 
 <div class="flex flex-row gap-[10px] items-center">
@@ -35,10 +57,20 @@
       <EyeOffIcon color="#94A3B8" size="16" />
     {/if}
   </button>
-  <input
-    readonly
-    type={inputType}
-    class="text-sm text-gray-800 font-medium {isEmpty ? 'italic' : ''}"
-    value={displayValue}
-  />
+  <Tooltip distance={8} location="top" suppress={isValueHidden || isEmpty}>
+    <input
+      readonly
+      type={inputType}
+      class="text-sm text-gray-800 font-medium {isEmpty
+        ? 'italic'
+        : ''} outline-none"
+      class:cursor-pointer={showValue}
+      value={displayValue}
+      title={showValue ? displayValue : ""}
+      on:click={onCopy}
+    />
+    <TooltipContent maxWidth="600px" slot="tooltip-content">
+      {copied ? "Copied!" : "Click to copy"}
+    </TooltipContent>
+  </Tooltip>
 </div>
