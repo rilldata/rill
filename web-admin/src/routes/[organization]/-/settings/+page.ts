2b56313f4ee4a718bc7255a6d8574caf99f606f8
Aditya Hegde <adityahegderocks@gmail.com>
2024-11-15 11:33:03 +0530

    fix: deploy errors based on subscription status (#6081)
    * Improve deploy errors based on subscription status
    
    * Add local api for org metadata
    
    * Add redirect back to deploy after upgrading
    
    * Fix edge case
    
    * PR comments
    
    * Fix tsc
    
    * Tweaks
    
    * More tweaks

--- web-admin/src/routes/[organization]/-/settings/+page.ts
+++ web-admin/src/routes/[organization]/-/settings/+page.ts
@@ -1,14 +1,13 @@
 import { getSingleUseUrlParam } from "@rilldata/web-admin/features/navigation/getSingleUseUrlParam";
 import type { PageLoad } from "./$types";
 
-export const load: PageLoad = async ({ params: { organization }, url }) => {
+export const load: PageLoad = async ({ url }) => {
   const showUpgradeDialog = !!getSingleUseUrlParam(
     url,
     "upgrade",
     "rill:app:showUpgrade",
   );
   return {
-    organization,
     showUpgradeDialog,
   };
 };
---
+++ web-common/src/features/billing/PricingDetails.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  export let extraText = "";
+</script>
+
+<div>
+  {extraText} Pricing is based on amount of data ingested (and compressed) into Rill.
+  <a
+    href="https://www.rilldata.com/pricing"
+    target="_blank"
+    rel="noreferrer noopener"
+  >
+    See pricing details ->
+  </a>
+
+  <ul class="mt-5 ml-5 list-disc">
+    <li>Starts at $250/month with 10 GB included, $25/GB thereafter</li>
+    <li>Unlimited projects, limited to 50 GB each</li>
+  </ul>
+</div>
