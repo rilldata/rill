61bb85fcddd3e0a377f3e87841959ae70f00cb3e
Cyrus Goh <hello@cyrusgoh.com>
2024-11-21 09:13:18 -0800

    fetch next page in org users table

--- web-admin/src/routes/[organization]/-/users/+page.svelte
+++ web-admin/src/routes/[organization]/-/users/+page.svelte
@@ -101,6 +101,7 @@
       </div>
       <OrgUsersTable
         data={filteredUsers}
+        query={$orgInvitesInfiniteQuery}
         currentUserEmail={$currentUser.data?.user.email}
       />
     </div>
