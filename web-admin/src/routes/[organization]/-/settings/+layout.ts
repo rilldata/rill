38d721a28d7626e9605ee819fe31878b7cbc7b81
Eric P Green <ericpgreen2@gmail.com>
2024-11-20 22:54:43 -0500

    Billing UI: Hide modules from Orgs on Enterprise plans (#6115)
    * Enterprise Plan: Hide usage tab
    
    * Enterprise Plan: prohibit navigating directly to usage tab
    
    * Enterprise Plan: hide payment module
    
    * Cleanup: use data from loader function
    
    * Whitelist `tsc` error

--- web-admin/src/routes/[organization]/-/settings/+layout.ts
+++ web-admin/src/routes/[organization]/-/settings/+layout.ts
@@ -1,10 +1,33 @@
+import {
+  adminServiceGetBillingSubscription,
+  getAdminServiceGetBillingSubscriptionQueryKey,
+} from "@rilldata/web-admin/client";
 import { getNeverSubscribedIssue } from "@rilldata/web-admin/features/billing/issues/getMessageForCancelledIssue";
+import { queryClient } from "@rilldata/web-common/lib/svelte-query/globalQueryClient.js";
+import { error } from "@sveltejs/kit";
 import type { PageLoad } from "./$types";
 
-export const load: PageLoad = async ({ parent }) => {
+export const load: PageLoad = async ({ parent, params }) => {
   const { issues } = await parent();
   const neverSubscribed = !!getNeverSubscribedIssue(issues);
-  return {
-    neverSubscribed,
-  };
+
+  const queryKey = getAdminServiceGetBillingSubscriptionQueryKey(
+    params.organization,
+  );
+  const queryFn = () => adminServiceGetBillingSubscription(params.organization);
+
+  try {
+    const billingSubscription = await queryClient.fetchQuery({
+      queryKey,
+      queryFn,
+    });
+    return {
+      subscription: billingSubscription.subscription,
+      billingPortalUrl: billingSubscription.billingPortalUrl,
+      neverSubscribed,
+    };
+  } catch (err) {
+    const statusCode = err?.response?.status || 500;
+    throw error(statusCode, "Failed to fetch billing subscription");
+  }
 };
