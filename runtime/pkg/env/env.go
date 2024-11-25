844e34d2c4625bec015fe7def9e292b10e19bc31
Benjamin Egelund-Müller <b@egelund-muller.com>
2024-11-25 17:43:30 +0000

    Update runtime/pkg/env/env.go

--- runtime/pkg/env/env.go
+++ runtime/pkg/env/env.go
@@ -9,7 +9,7 @@ var re = regexp.MustCompile(`^[a-zA-Z_][a-zA-Z0-9_.]*$`)
 
 func ValidateName(name string) error {
 	if !re.MatchString(name) {
-		return fmt.Errorf("invalid variable name: %q. Must start with a letter or underscore, can contain letters, digits, underscores and dots", name)
+		return fmt.Errorf("invalid variable name %q: must start with a letter or underscore and contain only letters, digits, underscores and dots", name)
 	}
 	return nil
 }
