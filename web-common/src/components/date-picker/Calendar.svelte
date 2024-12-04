9508cfc5bf954fce1c8e739686f51e4d224545f5
Brian Holmes <120223836+briangregoryholmes@users.noreply.github.com>
2024-12-04 13:15:55 -0500

    fix: comparison calendar not showing (#6195)
    * fix: comparison calender not showing
    
    * remove comment

--- web-common/src/components/date-picker/Calendar.svelte
+++ web-common/src/components/date-picker/Calendar.svelte
@@ -3,16 +3,16 @@
   import { DateTime } from "luxon";
   import { Interval } from "luxon";
 
-  type MaybeDate = DateTime<true> | DateTime<false> | undefined;
+  type MaybeDate = DateTime | undefined;
 
-  export let selection: MaybeDate | Interval<true> = undefined;
+  export let selection: MaybeDate | Interval = undefined;
   export let minDate: MaybeDate = undefined;
   export let visibleMonths = 1;
   export let selectingStart = true;
   export let firstVisibleMonth: MaybeDate = isValidDateTime(selection)
     ? selection
     : isValidInterval(selection)
-      ? selection.start
+      ? (selection.start ?? DateTime.now())
       : DateTime.now();
   export let singleDaySelection = isValidDateTime(selection);
   export let onSelectDay: (date: DateTime<true>) => void;
@@ -44,7 +44,7 @@
   }
 
   function isValidInterval(
-    value: MaybeDate | Interval<true>,
+    value: MaybeDate | Interval,
   ): value is Interval<true> {
     return Boolean(value && value instanceof Interval && value?.isValid);
   }
