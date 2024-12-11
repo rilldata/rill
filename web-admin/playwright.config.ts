f33225824fddd7e7bc73a11c0ea3d2a390626a64
Eric P Green <ericpgreen2@gmail.com>
2024-12-10 21:59:25 -0500

    Add `baseURL`

--- web-admin/playwright.config.ts
+++ web-admin/playwright.config.ts
@@ -6,6 +6,9 @@ const config: PlaywrightTestConfig = {
     port: 4173,
     timeout: 120_000,
   },
+  use: {
+    baseURL: "http://localhost:4173",
+  },
 };
 
 export default config;
--- web-admin/tests/test.spec.ts
+++ web-admin/tests/test.spec.ts
@@ -2,8 +2,6 @@ import { expect } from "@playwright/test";
 import { test } from "./setup/test";
 
 test("Unauthenticated user can see the login page", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
-  await expect(page.getByText("Log in to Rill")).toBeVisible({
-    timeout: 30_000, // TODO: It's slow because it's the vite dev server, not the built version
-  });
+  await page.goto("/");
+  await expect(page.getByText("Log in to Rill")).toBeVisible();
 });
