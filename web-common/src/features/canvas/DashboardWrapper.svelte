7d3bbbc8d1ac693cce2d166dcc888396bdbaea46
Cyrus Goh <hello@cyrusgoh.com>
2024-12-12 23:33:06 +0800

    remove unused grid lines

--- web-common/src/features/canvas/DashboardWrapper.svelte
+++ web-common/src/features/canvas/DashboardWrapper.svelte
@@ -1,27 +1,9 @@
 <script lang="ts">
-  import { type ComponentType, onMount } from "svelte";
-  import type GridLines from "./GridLines.svelte";
-
   export let width: number;
   export let height: number;
   export let scale: number;
-  export let embed = false;
-  export let showGrid = false;
-  export let changing = false;
-  export let scrollOffset = 0;
-  export let gapSize = 0;
-  export let gridCell = 0;
-  export let radius = 0;
   export let contentRect = new DOMRectReadOnly(0, 0, 0, 0);
   export let color = "bg-slate-200";
-
-  let GridLinesComponent: ComponentType<GridLines>;
-
-  onMount(async () => {
-    if (!embed) {
-      GridLinesComponent = (await import("./GridLines.svelte")).default;
-    }
-  });
 </script>
 
 <div
@@ -33,16 +15,6 @@
     class="wrapper {color} max-w-[1440px] min-h-full"
     style:height="{height}px"
   >
-    {#if GridLinesComponent && (showGrid || changing)}
-      <svelte:component
-        this={GridLinesComponent}
-        {gridCell}
-        {scrollOffset}
-        {gapSize}
-        {radius}
-        {scale}
-      />
-    {/if}
     <div role="presentation" class="size-full relative" on:click|self>
       <div
         class="dash pointer-events-none"
