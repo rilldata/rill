0b593046acd8a139e1bf04530e15377109fbaf41
Eric P Green <ericpgreen2@gmail.com>
2024-12-13 15:48:33 -0500

    Add CLI fixture

--- web-admin/tests/setup/base.ts
+++ web-admin/tests/setup/base.ts
@@ -1,11 +1,12 @@
 import { test as base, type Page } from "@playwright/test";
+import { cliLogin, cliLogout } from "./cli";
 
 type MyFixtures = {
   anonPage: Page;
+  cli: void;
 };
 
 export const test = base.extend<MyFixtures>({
-  // Fixtures
   anonPage: async ({ browser }, use) => {
     const context = await browser.newContext({
       storageState: { cookies: [], origins: [] },
@@ -13,5 +14,11 @@ export const test = base.extend<MyFixtures>({
     const anonPage = await context.newPage();
     await use(anonPage);
     await context.close();
+  },
+
+  cli: async ({ page }, use) => {
+    await cliLogin(page);
+    await use();
+    await cliLogout();
   },
 });
