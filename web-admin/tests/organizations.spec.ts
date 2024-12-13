2414a313c13e1e5f3e2ce4c0eb428e526be618fb
Eric P Green <ericpgreen2@gmail.com>
2024-12-13 15:56:52 -0500

    Add test for the org page

---
+++ web-admin/tests/organizations.spec.ts
@@ -0,0 +1,21 @@
+import { expect } from "@playwright/test";
+import { exec } from "child_process";
+import { promisify } from "util";
+import { test } from "./setup/base";
+
+const execAsync = promisify(exec);
+
+test.describe("Organizations", () => {
+  test("should create an organization", async ({ cli, page }) => {
+    // Create an organization
+    const { stdout: orgCreateStdout } = await execAsync("rill org create e2e");
+    expect(orgCreateStdout).toContain("Created organization");
+
+    // Go to the organization's page
+    await page.goto("/e2e");
+    await expect(page.getByRole("heading", { name: "e2e" })).toBeVisible();
+
+    // Clean up
+    await execAsync("rill org delete e2e --force");
+  });
+});
