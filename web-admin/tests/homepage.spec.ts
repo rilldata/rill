19594dbc413f4583baa33c53dd8e428e2b539467
Eric P Green <ericpgreen2@gmail.com>
2024-12-13 14:26:16 -0500

    Use fixture for anon users

--- web-admin/tests/homepage.spec.ts
+++ web-admin/tests/homepage.spec.ts
@@ -1,16 +1,12 @@
 import { expect } from "@playwright/test";
-import { test } from "./setup/test";
+import { test } from "./setup/base";
 
 test("Authenticated user sees the homepage", async ({ page }) => {
   await page.goto("/");
   await expect(page.getByText("Hi qa@rilldata.com!")).toBeVisible();
 });
 
-test("Unauthenticated user gets redirected to login", async ({ browser }) => {
-  const anonContext = await browser.newContext({
-    storageState: { cookies: [], origins: [] },
-  });
-  const page = await anonContext.newPage();
-  await page.goto("/");
-  await expect(page.getByText("Log in to Rill")).toBeVisible();
+test("Unauthenticated user gets redirected to login", async ({ anonPage }) => {
+  await anonPage.goto("/");
+  await expect(anonPage.getByText("Log in to Rill")).toBeVisible();
 });
--- web-admin/tests/setup/base.ts
+++ web-admin/tests/setup/base.ts
@@ -1,3 +1,17 @@
-import { test as base } from "@playwright/test";
+import { test as base, type Page } from "@playwright/test";
 
-export const test = base;
+type MyFixtures = {
+  anonPage: Page;
+};
+
+export const test = base.extend<MyFixtures>({
+  // Fixtures
+  anonPage: async ({ browser }, use) => {
+    const context = await browser.newContext({
+      storageState: { cookies: [], origins: [] },
+    });
+    const anonPage = await context.newPage();
+    await use(anonPage);
+    await context.close();
+  },
+});
