5ab68a61b5feeb9a8415be71dfe8145c970ffeb3
Eric P Green <ericpgreen2@gmail.com>
2024-12-13 15:57:37 -0500

    Organize the Homepage tests into a suite

--- web-admin/tests/homepage.spec.ts
+++ web-admin/tests/homepage.spec.ts
@@ -1,12 +1,16 @@
 import { expect } from "@playwright/test";
 import { test } from "./setup/base";
 
-test("Authenticated user sees the homepage", async ({ page }) => {
-  await page.goto("/");
-  await expect(page.getByText("Hi qa@rilldata.com!")).toBeVisible();
-});
+test.describe("Homepage", () => {
+  test("Authenticated user should see the homepage", async ({ page }) => {
+    await page.goto("/");
+    await expect(page.getByText("Hi qa@rilldata.com!")).toBeVisible();
+  });
 
-test("Unauthenticated user gets redirected to login", async ({ anonPage }) => {
-  await anonPage.goto("/");
-  await expect(anonPage.getByText("Log in to Rill")).toBeVisible();
+  test("Unauthenticated user should be redirected to login", async ({
+    anonPage,
+  }) => {
+    await anonPage.goto("/");
+    await expect(anonPage.getByText("Log in to Rill")).toBeVisible();
+  });
 });
