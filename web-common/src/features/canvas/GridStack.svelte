9480b033dc2365e8cf7e42d5af6a5135ace5c980
Cyrus Goh <hello@cyrusgoh.com>
2024-12-19 15:37:57 +0800

    encapsulate grid stack item for clarity

---
+++ web-common/src/features/canvas/GridStack.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { onDestroy, onMount } from "svelte";
+  import { GridStackManager } from "./grid-stack-manager";
+  import "gridstack/dist/gridstack.min.css";
+
+  export let readonly = false;
+
+  let gridStackElement: HTMLElement;
+  const gridStackManager = new GridStackManager(readonly);
+
+  onMount(() => {
+    if (gridStackElement) {
+      gridStackManager.initialize();
+    }
+  });
+
+  onDestroy(() => {
+    gridStackManager.destroy();
+  });
+</script>
+
+<div bind:this={gridStackElement} class="grid-stack">
+  <slot />
+</div>
+
+<style lang="postcss">
+</style>
