42a2e560c1f7dfb3d36b6d733617fc9e6706f01a
Aditya Hegde <adityahegderocks@gmail.com>
2024-12-19 17:31:15 +0530

    Add project breakdown page

---
+++ web-admin/src/features/organizations/project-breakdown/ProjectBreakdownDashboard.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import ExploreEmbed from "@rilldata/web-admin/features/embeds/ExploreEmbed.svelte";
+  import { useValidExplores } from "@rilldata/web-common/features/dashboards/selectors";
+  import { runtime } from "@rilldata/web-common/runtime-client/runtime-store";
+
+  $: instanceId = $runtime.instanceId;
+  $: explores = useValidExplores(instanceId);
+  $: exploreName = $explores.data?.[0]?.meta?.name?.name;
+</script>
+
+{#if exploreName}
+  <ExploreEmbed {instanceId} {exploreName} />
+{/if}
