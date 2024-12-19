aa13beb9ced0ae299020e29178a38480db1f8966
Cyrus Goh <hello@cyrusgoh.com>
2024-12-19 14:11:27 +0800

    centralize grid stack manager

---
+++ web-common/src/features/canvas/grid-stack-manager.ts
@@ -0,0 +1,32 @@
+import { GridStack, type GridStackOptions } from "gridstack";
+
+export class GridStackManager {
+  private instance: GridStack | null = null;
+  private options: GridStackOptions;
+
+  constructor(readonly: boolean) {
+    this.options = {
+      column: 12,
+      resizable: {
+        handles: "e,se,s,sw,w",
+      },
+      animate: true,
+      float: true,
+      staticGrid: readonly,
+    };
+  }
+
+  initialize() {
+    if (!this.instance) {
+      this.instance = GridStack.init(this.options);
+    }
+    return this.instance;
+  }
+
+  destroy() {
+    if (this.instance) {
+      this.instance.destroy();
+      this.instance = null;
+    }
+  }
+}
