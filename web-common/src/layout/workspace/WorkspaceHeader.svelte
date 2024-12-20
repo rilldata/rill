9b1bd5f8e4ad121e5b0ecb36cd9c83b12b1cc599
Brian Holmes <120223836+briangregoryholmes@users.noreply.github.com>
2024-12-20 10:53:42 -0500

    add accessible labels (#6308)

--- web-common/src/layout/workspace/WorkspaceHeader.svelte
+++ web-common/src/layout/workspace/WorkspaceHeader.svelte
@@ -84,6 +84,7 @@
             type="secondary"
             square
             selected={$tableVisible}
+            label="Toggle table visibility"
             on:click={workspaceLayout.table.toggle}
           >
             <HideBottomPane size="18px" open={$tableVisible} />
@@ -102,6 +103,7 @@
             type="secondary"
             square
             selected={$inspectorVisible}
+            label="Toggle inspector visibility"
             on:click={workspaceLayout.inspector.toggle}
           >
             <HideSidebar open={$inspectorVisible} size="18px" />
