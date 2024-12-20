df1006aeaecbd095eb83aa6a3c20440bfa4f89e6
Anshul Khandelwal <12948312+k-anshul@users.noreply.github.com>
2024-12-20 15:36:59 +0530

    also allow inline variables

--- runtime/drivers/duckdb/model_executor_sqlstore_self.go
+++ runtime/drivers/duckdb/model_executor_sqlstore_self.go
@@ -11,6 +11,18 @@ import (
 	"github.com/rilldata/rill/runtime/drivers/postgres"
 )
 
+type sqlStoreToSelfInputProps struct {
+	SQL string `mapstructure:"sql"`
+	DSN string `mapstructure:"dsn"`
+}
+
+func (p *sqlStoreToSelfInputProps) Validate() error {
+	if p.SQL == "" {
+		return fmt.Errorf("missing property 'sql'")
+	}
+	return nil
+}
+
 type sqlStoreToSelfExecutor struct {
 	c *connection
 }
@@ -25,7 +37,7 @@ func (e *sqlStoreToSelfExecutor) Concurrency(desired int) (int, bool) {
 }
 
 func (e *sqlStoreToSelfExecutor) Execute(ctx context.Context, opts *drivers.ModelExecuteOptions) (*drivers.ModelResult, error) {
-	inputProps := &ModelInputProperties{}
+	inputProps := &sqlStoreToSelfInputProps{}
 	if err := mapstructure.WeakDecode(opts.InputProperties, inputProps); err != nil {
 		return nil, fmt.Errorf("failed to parse input properties: %w", err)
 	}
@@ -47,26 +59,45 @@ func (e *sqlStoreToSelfExecutor) Execute(ctx context.Context, opts *drivers.Mode
 	return executor.Execute(ctx, newOpts)
 }
 
-func (e *sqlStoreToSelfExecutor) modelInputProperties(modelName, inputConnector string, inputHandle drivers.Handle, inputProps *ModelInputProperties) (map[string]any, error) {
+func (e *sqlStoreToSelfExecutor) modelInputProperties(modelName, inputConnector string, inputHandle drivers.Handle, inputProps *sqlStoreToSelfInputProps) (map[string]any, error) {
 	m := &ModelInputProperties{}
 	dbName := fmt.Sprintf("%s__%s", modelName, inputConnector)
 	safeDBName := safeName(dbName)
 	userQuery, _ := strings.CutSuffix(inputProps.SQL, ";") // trim trailing semi colon
 	switch inputHandle.Driver() {
 	case "mysql":
-		var config *mysql.ConfigProperties
-		if err := mapstructure.Decode(inputHandle.Config(), &config); err != nil {
-			return nil, err
+		var dsn string
+		if inputProps.DSN != "" {
+			dsn = inputProps.DSN
+		} else {
+			// may be configured via a connector
+			var config *mysql.ConfigProperties
+			if err := mapstructure.Decode(inputHandle.Config(), &config); err != nil {
+				return nil, err
+			}
+			dsn = rewriteMySQLDSN(config.DSN)
 		}
-		dsn := rewriteMySQLDSN(config.DSN)
+		if dsn == "" {
+			return nil, fmt.Errorf("must set `dsn` for models that transfer data from `mysql` to `duckdb`")
+		}
 		m.PreExec = fmt.Sprintf("INSTALL 'MYSQL'; LOAD 'MYSQL'; ATTACH %s AS %s (TYPE mysql, READ_ONLY)", safeSQLString(dsn), safeDBName)
 		m.SQL = fmt.Sprintf("SELECT * FROM mysql_query(%s, %s)", safeSQLString(dbName), safeSQLString(userQuery))
 	case "postgres":
-		var config *postgres.ConfigProperties
-		if err := mapstructure.Decode(inputHandle.Config(), &config); err != nil {
-			return nil, err
+		var dsn string
+		if inputProps.DSN != "" {
+			dsn = inputProps.DSN
+		} else {
+			// may be configured via a connector
+			var config *postgres.ConfigProperties
+			if err := mapstructure.Decode(inputHandle.Config(), &config); err != nil {
+				return nil, err
+			}
+			dsn = config.DatabaseURL
 		}
-		m.PreExec = fmt.Sprintf("INSTALL 'POSTGRES'; LOAD 'POSTGRES'; ATTACH %s AS %s (TYPE postgres, READ_ONLY)", safeSQLString(config.DatabaseURL), safeDBName)
+		if dsn == "" {
+			return nil, fmt.Errorf("must set `dsn` for models that transfer data from `postgres` to `duckdb`")
+		}
+		m.PreExec = fmt.Sprintf("INSTALL 'POSTGRES'; LOAD 'POSTGRES'; ATTACH %s AS %s (TYPE postgres, READ_ONLY)", safeSQLString(dsn), safeDBName)
 		m.SQL = fmt.Sprintf("SELECT * FROM postgres_query(%s, %s)", safeSQLString(dbName), safeSQLString(userQuery))
 	default:
 		return nil, fmt.Errorf("internal error: unsupported external database: %s", inputHandle.Driver())
