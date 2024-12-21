50b86c5a349a373b7cee880f8e2090abc52e64ae
royendo <67675319+royendo@users.noreply.github.com>
2024-12-20 17:55:51 -0700

    pretty again

--- web-local/tests/UI/check-inspector-source-model.spec.ts
+++ web-local/tests/UI/check-inspector-source-model.spec.ts
@@ -1,72 +1,71 @@
-import { test, expect } from '@playwright/test';
-import { test as RillTest } from '../utils/test';
-import { cloud, waitForTable } from '../utils/sourceHelpers';
-import { checkInspectorSource, checkInspectorModel } from '../utils/inspectorHelpers';
-import { createModel } from '../utils/modelHelpers';
+import { test } from "@playwright/test";
+import { test as RillTest } from "../utils/test";
+import { cloud, waitForTable } from "../utils/sourceHelpers";
+import {
+  checkInspectorSource,
+  checkInspectorModel,
+} from "../utils/inspectorHelpers";
+import { createModel } from "../utils/modelHelpers";
 
 // Testing the contents of the Inspector Panel
-// Does the correct rows and columns appear, and does each column have a visible graph? 
-test.describe('Checking the Inspector Panel for Source and Model. Check if values are correct as well as if the UI populates graph.', () => {
-  RillTest('Reading Source into Rill from GCS', async ({ page }) => {
-    console.log('Testing cloud sales data ingestion...');
+// Does the correct rows and columns appear, and does each column have a visible graph?
+test.describe("Checking the Inspector Panel for Source and Model. Check if values are correct as well as if the UI populates graph.", () => {
+  RillTest("Reading Source into Rill from GCS", async ({ page }) => {
+    console.log("Testing cloud sales data ingestion...");
     await Promise.all([
-      waitForTable(page, '/sources/sales.yaml', [
-        'sale_date',
-        'sale_id',
-        'duration_ms',
-        'customer_id',
-        'sales_amount_usd',
-        'products',
-        'discounts',
-        'region',
-        'is_online',
+      waitForTable(page, "/sources/sales.yaml", [
+        "sale_date",
+        "sale_id",
+        "duration_ms",
+        "customer_id",
+        "sales_amount_usd",
+        "products",
+        "discounts",
+        "region",
+        "is_online",
       ]),
-      cloud(page, 'sales.csv', 'gcs'),
+      cloud(page, "sales.csv", "gcs"),
     ]);
-    console.log('Sales table validated.');
+    console.log("Sales table validated.");
 
-    await checkInspectorSource(page, '100,000', '9',
-      [
-        'sale_date',
-        'sale_id',
-        'duration_ms',
-        'customer_id',
-        'sales_amount_usd',
-        'products',
-        'discounts',
-        'region',
-        'is_online'
-      ]
-    )
-    console.log('Testing cloud customer data ingestion...');
-    await Promise.all([
-      waitForTable(page, '/sources/customer_data.yaml', [
-        'customer_id',
-        'name',
-        'email',
-        'signup_date',
-        'preferences',
-        'total_spent_usd',
-        'loyalty_tier',
-        'is_active',
-      ]),
-      cloud(page, 'customer_data.csv', 'gcs'),
+    await checkInspectorSource(page, "100,000", "9", [
+      "sale_date",
+      "sale_id",
+      "duration_ms",
+      "customer_id",
+      "sales_amount_usd",
+      "products",
+      "discounts",
+      "region",
+      "is_online",
     ]);
-    console.log('Customer data table validated.');
-    await checkInspectorSource(page, '10,000', '8',
-      [
-        'signup_date',
-        'customer_id',
-        'name',
-        'email',
-        'preferences',
-        'total_spent_usd',
-        'loyalty_tier',
-        'is_active',
-      ]
-    ),
-      console.log("Creating model to join sources.")
-    await createModel(page, 'joined_model.sql');
+    console.log("Testing cloud customer data ingestion...");
+    await Promise.all([
+      waitForTable(page, "/sources/customer_data.yaml", [
+        "customer_id",
+        "name",
+        "email",
+        "signup_date",
+        "preferences",
+        "total_spent_usd",
+        "loyalty_tier",
+        "is_active",
+      ]),
+      cloud(page, "customer_data.csv", "gcs"),
+    ]);
+    console.log("Customer data table validated.");
+    await checkInspectorSource(page, "10,000", "8", [
+      "signup_date",
+      "customer_id",
+      "name",
+      "email",
+      "preferences",
+      "total_spent_usd",
+      "loyalty_tier",
+      "is_active",
+    ]),
+      console.log("Creating model to join sources.");
+    await createModel(page, "joined_model.sql");
     // wait for textbox to appear for model
     await page.waitForSelector('div[role="textbox"]');
 
@@ -81,12 +80,14 @@ test.describe('Checking the Inspector Panel for Source and Model. Check if value
     });
 
     // Mimic typing in the child contenteditable div
-    const childTextbox = await page.locator('div[role="textbox"] div.cm-content');
+    const childTextbox = await page.locator(
+      'div[role="textbox"] div.cm-content',
+    );
     await childTextbox.click(); // Ensure it's focused for typing
 
     // Clear existing contents
-    await childTextbox.press('Meta+A'); // need to check this
-    await childTextbox.press('Backspace'); // Delete selected text
+    await childTextbox.press("Meta+A"); // need to check this
+    await childTextbox.press("Backspace"); // Delete selected text
 
     const lines = [
       "-- Model SQL",
@@ -96,37 +97,35 @@ test.describe('Checking the Inspector Panel for Source and Model. Check if value
       "FROM sales AS a",
       "LEFT JOIN customer_data AS b",
       "ON a.customer_id = b.customer_id",
-      "", ""
+      "",
+      "",
     ];
 
     // Type each line with a newline after
     for (const line of lines) {
       await childTextbox.type(line); // Type the line
-      await childTextbox.press('Enter'); // Press Enter for a new line
+      await childTextbox.press("Enter"); // Press Enter for a new line
     }
 
     console.log("Content typed successfully.");
-    await checkInspectorModel(page, '100,000', '16',
-      [
-        'sale_date',
-        'sale_id',
-        'duration_ms',
-        'customer_id',
-        'sales_amount_usd',
-        'products',
-        'discounts',
-        'region',
-        'is_online',
-        'signup_date',
-        'customer_id',
-        'name',
-        'email',
-        'preferences',
-        'total_spent_usd',
-        'loyalty_tier',
-        'is_active',
-      ]
-    );
+    await checkInspectorModel(page, "100,000", "16", [
+      "sale_date",
+      "sale_id",
+      "duration_ms",
+      "customer_id",
+      "sales_amount_usd",
+      "products",
+      "discounts",
+      "region",
+      "is_online",
+      "signup_date",
+      "customer_id",
+      "name",
+      "email",
+      "preferences",
+      "total_spent_usd",
+      "loyalty_tier",
+      "is_active",
+    ]);
   });
-
 });
--- web-local/tests/UI/check-source-ui-buttons.spec.ts
+++ web-local/tests/UI/check-source-ui-buttons.spec.ts
@@ -1,116 +1,147 @@
-import { test, expect } from '@playwright/test';
-import { test as RillTest } from '../utils/test';
-import { cloud, waitForTable } from '../utils/sourceHelpers';
-import { actionUsingMenu, renameFileUsingMenu, checkExistInConnector } from "../utils/commonHelpers";
+import { test, expect } from "@playwright/test";
+import { test as RillTest } from "../utils/test";
+import { cloud, waitForTable } from "../utils/sourceHelpers";
+import {
+  actionUsingMenu,
+  renameFileUsingMenu,
+  checkExistInConnector,
+} from "../utils/commonHelpers";
 
-// Testing functionality of all the buttons in the source page. 
-// Refresh, "Create Model", "Go to "(after creating model), panels 
+// Testing functionality of all the buttons in the source page.
+// Refresh, "Create Model", "Go to "(after creating model), panels
 
+test.describe("Check Source UI buttons.", () => {
+  RillTest("Reading Source into Rill from GCS", async ({ page }) => {
+    console.log("Testing cloud sales data ingestion...");
+    await Promise.all([
+      waitForTable(page, "/sources/sales.yaml", [
+        "sale_date",
+        "sale_id",
+        "duration_ms",
+        "customer_id",
+        "sales_amount_usd",
+        "products",
+        "discounts",
+        "region",
+        "is_online",
+      ]),
+      cloud(page, "sales.csv", "gcs"),
+    ]);
+    console.log("Sales table validated...");
 
-test.describe('Check Source UI buttons.', () => {
-    RillTest('Reading Source into Rill from GCS', async ({ page }) => {
-        console.log('Testing cloud sales data ingestion...');
-        await Promise.all([
-            waitForTable(page, '/sources/sales.yaml', [
-                'sale_date',
-                'sale_id',
-                'duration_ms',
-                'customer_id',
-                'sales_amount_usd',
-                'products',
-                'discounts',
-                'region',
-                'is_online',
-            ]),
-            cloud(page, 'sales.csv', 'gcs'),
-        ]);
-        console.log('Sales table validated...');
+    // CHECK CONNECTORS for SOURCE (table name dynamic so wildcard)
+    await checkExistInConnector(page, "duckdb", "main_db", "sales");
 
-        // CHECK CONNECTORS for SOURCE (table name dynamic so wildcard)
-        await checkExistInConnector(page, 'duckdb', 'main_db', 'sales')
+    // CHECKING BUTTONS
 
-        // CHECKING BUTTONS
+    //Close File Explore Sidebar
+    await page.locator('span[aria-label="Close sidebar"]').click();
+    // Assert that the class changes
+    const sidebarClose = page.locator(".sidebar.svelte-5nrsv4");
+    await expect(sidebarClose).toHaveClass("sidebar svelte-5nrsv4 hide");
 
-        //Close File Explore Sidebar
-        await page.locator('span[aria-label="Close sidebar"]').click();
-        // Assert that the class changes
-        const sidebarClose = page.locator('.sidebar.svelte-5nrsv4');
-        await expect(sidebarClose).toHaveClass('sidebar svelte-5nrsv4 hide');
+    await page.locator('span[aria-label="Show sidebar"]').click();
+    // Assert that the class changes
+    const sidebarOpen = page.locator(".sidebar.svelte-5nrsv4");
+    await expect(sidebarOpen).toHaveClass("sidebar svelte-5nrsv4");
 
+    // checking the refresh button
+    await Promise.all([
+      page.locator('button[aria-label="Refresh"]').click(),
+      expect(page.getByText("Ingesting source sales").first()).toBeVisible(), // Test will fail if the text is not visible
+    ]);
+    // checking the panels ,
+    await page.getByRole("button", { name: "Toggle table visibility" }).click(); // #6308
+    const resultsPreviewTableClose = await page.locator(
+      '[aria-label="Results Preview Table"]',
+    ); // #6316
+    await expect(resultsPreviewTableClose).toBeHidden();
 
-        await page.locator('span[aria-label="Show sidebar"]').click();
-        // Assert that the class changes
-        const sidebarOpen = page.locator('.sidebar.svelte-5nrsv4');
-        await expect(sidebarOpen).toHaveClass('sidebar svelte-5nrsv4');
+    await page.getByRole("button", { name: "Toggle table visibility" }).click(); // #6308
+    const resultsPreviewTableOpen = await page.locator(
+      '[aria-label="Results Preview Table"]',
+    ); // #6316
+    await expect(resultsPreviewTableOpen).toBeVisible();
 
+    await page
+      .getByRole("button", { name: "Toggle inspector visibility" })
+      .click(); // #6308
+    const inspectorPanelClose = await page.locator(
+      '[aria-label="Inspector Panel"]',
+    ); // #6316
+    await expect(inspectorPanelClose).toBeHidden();
 
-        // checking the refresh button
-        await Promise.all([
-            page.locator('button[aria-label="Refresh"]').click(),
-            expect(page.getByText('Ingesting source sales').first()).toBeVisible() // Test will fail if the text is not visible
-        ]);
-        // checking the panels , 
-        await page.getByRole('button', { name: 'Toggle table visibility' }).click(); // #6308
-        const resultsPreviewTableClose = await page.locator('[aria-label="Results Preview Table"]'); // #6316
-        await expect(resultsPreviewTableClose).toBeHidden();
+    await page
+      .getByRole("button", { name: "Toggle inspector visibility" })
+      .click(); // #6308
+    const inspectorPanelOpen = await page.locator(
+      '[aria-label="Inspector Panel"]',
+    ); // #6316
+    await expect(inspectorPanelOpen).toBeVisible();
 
-        await page.getByRole('button', { name: 'Toggle table visibility' }).click(); // #6308
-        const resultsPreviewTableOpen = await page.locator('[aria-label="Results Preview Table"]'); // #6316
-        await expect(resultsPreviewTableOpen).toBeVisible();
+    // Create Model!
+    console.log("Creating Create Model Button...");
+    await Promise.all([
+      //  waitForFileNavEntry(page, "/files/models/sales_model.sql", false), //set true?
+      page.getByRole("button", { name: "Create model" }).click(),
+    ]);
 
-        await page.getByRole('button', { name: 'Toggle inspector visibility' }).click();  // #6308
-        const inspectorPanelClose = await page.locator('[aria-label="Inspector Panel"]'); // #6316
-        await expect(inspectorPanelClose).toBeHidden();
+    // Return to Source, Select Go to. -> create model, check that its suffixed "_1"
+    await page.locator('span:has-text("sales.yaml")').click();
 
-        await page.getByRole('button', { name: 'Toggle inspector visibility' }).click();  // #6308
-        const inspectorPanelOpen = await page.locator('[aria-label="Inspector Panel"]'); // #6316
-        await expect(inspectorPanelOpen).toBeVisible();
+    //   waitForFileNavEntry(page, "/models/sales_model_1.sql", false), //set true?
+    await page.getByRole("button", { name: "Go to" }).click();
+    await page
+      .locator('div[role="menuitem"]:has-text("Create model")')
+      .waitFor();
+    await page.locator('div[role="menuitem"]:has-text("Create model")').click();
 
-        // Create Model! 
-        console.log("Creating Create Model Button...")
-        await Promise.all([
-            //  waitForFileNavEntry(page, "/files/models/sales_model.sql", false), //set true?
-            page.getByRole('button', { name: 'Create model' }).click()
-        ]);
+    // Return to source and check Go to for both models.
+    await page.locator('span:has-text("sales.yaml")').click();
+    await page.getByRole("button", { name: "Go to" }).click();
+    await page
+      .locator('div[role="menuitem"]:has-text("Create model")')
+      .waitFor();
+    await expect(
+      page.locator('a[role="menuitem"][href="/files/models/sales_model.sql"]'),
+    ).toBeVisible();
+    await expect(
+      page.locator(
+        'a[role="menuitem"][href="/files/models/sales_model_1.sql"]',
+      ),
+    ).toBeVisible();
 
+    // Delete and rename models and confirm back to Create model
+    await page.locator('span:has-text("sales_model.sql")').hover();
+    await actionUsingMenu(page, "/sales_model.sql", "Delete");
 
-        // Return to Source, Select Go to. -> create model, check that its suffixed "_1"
-        await page.locator('span:has-text("sales.yaml")').click();
+    await renameFileUsingMenu(
+      page,
+      "/models/sales_model_1.sql",
+      "random_model.sql",
+    );
 
-        //   waitForFileNavEntry(page, "/models/sales_model_1.sql", false), //set true?
-        await page.getByRole('button', { name: 'Go to' }).click();
-        await page.locator('div[role="menuitem"]:has-text("Create model")').waitFor();
-        await page.locator('div[role="menuitem"]:has-text("Create model")').click();
+    // Check the source and model are still linked
+    await page.locator('span:has-text("sales.yaml")').click();
+    await page.getByRole("button", { name: "Go to" }).click();
+    await page
+      .locator('div[role="menuitem"]:has-text("Create model")')
+      .waitFor();
+    await expect(
+      page.locator('a[role="menuitem"][href="/files/models/random_model.sql"]'),
+    ).toBeVisible();
+    await page
+      .locator('a[role="menuitem"][href="/files/models/random_model.sql"]')
+      .click();
 
+    // Delete new model
+    await page.locator('span:has-text("random_model.sql")').hover();
+    await actionUsingMenu(page, "/random_model.sql", "Delete");
 
-        // Return to source and check Go to for both models.
-        await page.locator('span:has-text("sales.yaml")').click();
-        await page.getByRole('button', { name: 'Go to' }).click();
-        await page.locator('div[role="menuitem"]:has-text("Create model")').waitFor();
-        await expect(page.locator('a[role="menuitem"][href="/files/models/sales_model.sql"]')).toBeVisible();
-        await expect(page.locator('a[role="menuitem"][href="/files/models/sales_model_1.sql"]')).toBeVisible();
-
-        // Delete and rename models and confirm back to Create model
-        await page.locator('span:has-text("sales_model.sql")').hover();
-        await actionUsingMenu(page, "/sales_model.sql", "Delete")
-
-        await renameFileUsingMenu(page, '/models/sales_model_1.sql', 'random_model.sql')
-
-        // Check the source and model are still linked
-        await page.locator('span:has-text("sales.yaml")').click();
-        await page.getByRole('button', { name: 'Go to' }).click();
-        await page.locator('div[role="menuitem"]:has-text("Create model")').waitFor();
-        await expect(page.locator('a[role="menuitem"][href="/files/models/random_model.sql"]')).toBeVisible();
-        await page.locator('a[role="menuitem"][href="/files/models/random_model.sql"]').click();
-
-
-        // Delete new model 
-        await page.locator('span:has-text("random_model.sql")').hover();
-        await actionUsingMenu(page, "/random_model.sql", "Delete")
-
-        // Check the UI has returned to Create Model
-        await page.locator('span:has-text("sales.yaml")').click();
-        await expect(page.getByRole('button', { name: 'Create model' })).toBeVisible();
-
-    });
+    // Check the UI has returned to Create Model
+    await page.locator('span:has-text("sales.yaml")').click();
+    await expect(
+      page.getByRole("button", { name: "Create model" }),
+    ).toBeVisible();
+  });
 });
