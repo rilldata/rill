6e02fab8e9be4c3a52379bd7f724540445c9dcba
Benjamin Egelund-Müller <b@egelund-muller.com>
2024-12-23 17:51:27 +0100

    Add the deprecated field `rill_version` to `rill.yaml` validation

--- runtime/compilers/rillv1/parse_rillyaml.go
+++ runtime/compilers/rillv1/parse_rillyaml.go
@@ -42,7 +42,10 @@ type VariableDef struct {
 
 // rillYAML is the raw YAML structure of rill.yaml
 type rillYAML struct {
+	// Compiler is the parser version to use. It is not consumed here because at this point a parser has already been chosen.
 	Compiler string `yaml:"compiler"`
+	// RillVersion is deprecated and not used anymore.
+	RillVersion string `yaml:"rill_version"`
 	// Title of the project
 	DisplayName string `yaml:"display_name"`
 	// Title of the project
