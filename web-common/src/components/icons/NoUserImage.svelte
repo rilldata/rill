cf37a72367b78b8b0ee35b4b78f0409ce211b47f
Aditya Hegde <adityahegderocks@gmail.com>
2024-12-30 10:29:19 +0530

    Fallback when user avatar image is broken

--- web-common/src/components/icons/NoUserImage.svelte
+++ web-common/src/components/icons/NoUserImage.svelte
@@ -6,18 +6,10 @@
   xmlns="http://www.w3.org/2000/svg"
 >
   <g clip-path="url(#clip0_11031_1337198)">
-    <circle cx="14" cy="11" r="5" fill="#4632F5" />
-    <ellipse cx="14" cy="28.9565" rx="12" ry="10.9565" fill="#4632F5" />
+    <circle cx="14" cy="11" r="5" fill="#4474e0" />
+    <ellipse cx="14" cy="28.9565" rx="12" ry="10.9565" fill="#4474e0" />
   </g>
-  <rect
-    x="0.5"
-    y="0.5"
-    width="27"
-    height="27"
-    rx="13.5"
-    stroke="#4632F5"
-    stroke-dasharray="2 2"
-  />
+  <rect x="0.5" y="0.5" width="27" height="27" rx="13.5" stroke="#4474e0" />
   <defs>
     <clipPath id="clip0_11031_1337198">
       <rect width="28" height="28" rx="14" fill="white" />
