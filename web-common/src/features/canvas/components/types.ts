0cc9eb471853a2ea0962ef5a699cf449439b636e
Dhiraj Kumar <sumankumaribarnwal@gmail.com>
2025-01-08 00:18:49 +0530

    Keep single copy of base component

--- web-common/src/features/canvas/components/types.ts
+++ web-common/src/features/canvas/components/types.ts
@@ -1,5 +1,3 @@
-import type { Writable } from "svelte/store";
-import type { InputParams } from "../inspector/types";
 import type { ChartConfig, ChartType } from "./charts/types";
 import type { ImageSpec } from "./image";
 import type { KPISpec } from "./kpi";
@@ -13,58 +11,6 @@ export type ComponentSpec =
   | ImageSpec
   | KPISpec
   | MarkdownSpec;
-
-// The CanvasComponent interface is generic over the spec type.
-export interface CanvasComponent<T> {
-  /**
-   * Svelte writable for the spec
-   */
-  specStore: Writable<T>;
-
-  /**
-   * Path in the YAML where the component is stored
-   */
-  pathInYAML: (string | number)[];
-
-  /**
-   * Minimum allowed size for the component
-   * container on the canvas
-   */
-  minSize: ComponentSize;
-
-  /**
-   * The default size of the container when the component
-   * is added to the canvas
-   */
-  defaultSize: ComponentSize;
-
-  /**
-   * The minimum condition needed for the spec to be valid
-   * for the given component and to be rendered on the canvas
-   */
-  isValid(spec: T): boolean;
-
-  /**
-   * A map of input params which will be used in the visual
-   * UI builder
-   */
-  inputParams(): InputParams<T>;
-
-  /**
-   * Get the spec when the component is added to the canvas
-   */
-  newComponentSpec(metrics_view: string, measure: string, dimension: string): T;
-
-  /**
-   * Update the spec store with the new values
-   */
-  updateProperty(key: string, value: unknown): Promise<void>;
-
-  /**
-   * Set the spec store with the new values
-   */
-  setSpec(spec: T): Promise<void>;
-}
 
 export interface ComponentCommonProperties {
   title?: string;
