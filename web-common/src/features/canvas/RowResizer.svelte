e99b6fb09997da24b53239e15f0c70efe0677849
Cyrus Goh <hello@cyrusgoh.com>
2025-01-07 20:41:12 +0800

    row resizer

---
+++ web-common/src/features/canvas/RowResizer.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+</script>
+
+<button
+  type="button"
+  aria-label="Resize row"
+  class="row-resize-handle w-full h-[3px] cursor-row-resize bg-transparent hover:bg-primary-300 z-[50] opacity-0 hover:opacity-100 pointer-events-auto"
+  on:mousedown|stopPropagation={(e) => dispatch("resize", e)}
+/>
+
+<style>
+  .row-resize-handle {
+    position: relative;
+    transition: opacity 0.2s;
+  }
+</style>
