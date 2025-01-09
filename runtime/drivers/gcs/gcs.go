902e0a18957bcdf28bc4834abfae4bd8b4491794
Anshul Khandelwal <12948312+k-anshul@users.noreply.github.com>
2025-01-09 10:46:53 +0530

    add a log for gcs usage

--- runtime/drivers/gcs/gcs.go
+++ runtime/drivers/gcs/gcs.go
@@ -80,7 +80,7 @@ func (d driver) Open(instanceID string, config map[string]any, st *storage.Clien
 		return nil, errors.New("gcs driver can't be shared")
 	}
 
-	logger.Info("Opening GCS connection", zap.String("instanceID", instanceID))
+	logger.Info("opening GCS connection", zap.String("instanceID", instanceID))
 	conf := &configProperties{}
 	err := mapstructure.WeakDecode(config, conf)
 	if err != nil {
