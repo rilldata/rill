88dd9a978eb08910e19c0fde82cc2da8c7fb6734
Graham Plata <graham.plata@rilldata.com>
2025-01-10 10:09:38 -0500

    fixup

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -154,16 +154,14 @@ func runCmd(ctx context.Context, ver cmdutil.Version) error {
 		whoami.WhoamiCmd(ch),
 	)
 
-	if ch.IsDev() {
-		// Internal commands - hidden from help menu
-		cmdutil.AddGroup(rootCmd, "Internal",
-			admin.AdminCmd(ch),
-			runtime.RuntimeCmd(ch),
-			devtool.DevtoolCmd(ch),
-			sudo.SudoCmd(ch),
-			verifyInstallCmd(ch),
-		)
-	}
+	// Internal commands - hidden from help menu
+	cmdutil.AddGroup(rootCmd, "Internal",
+		admin.AdminCmd(ch),
+		runtime.RuntimeCmd(ch),
+		devtool.DevtoolCmd(ch),
+		sudo.SudoCmd(ch),
+		verifyInstallCmd(ch),
+	)
 
 	// Additional sub-commands
 	rootCmd.AddCommand(
