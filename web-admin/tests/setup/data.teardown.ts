fb63eddf39eb4f42bb31e77b79a183d3274838ed
Eric P Green <ericpgreen2@gmail.com>
2025-01-12 15:25:52 -0500

    Teardown: delete users from the database

--- web-admin/tests/setup/data.teardown.ts
+++ web-admin/tests/setup/data.teardown.ts
@@ -1,9 +1,14 @@
 import { execAsync } from "../utils/spawn";
 import { test as teardown } from "./base";
 
-teardown("should remove the test data", async ({ cli: _cli }) => {
+teardown("should remove the test data", async ({ cli: _ }) => {
   // Delete the organization and its projects
   await execAsync("rill org delete e2e --force");
+
+  // Delete the user
+  await execAsync(
+    "PGPASSWORD=postgres psql -h localhost -p 6432 -U postgres -d postgres -c 'DELETE FROM users;'",
+  );
 
   // Delete the project's Rill Cloud metadata
   await execAsync(
