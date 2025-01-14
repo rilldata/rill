73173e033aeb360c87c19d2be1800ca051fb904f
Himadri Singh <himadri.singh@gmail.com>
2025-01-14 10:50:27 +0530

    Revert "Update connection_cache.go"
    This reverts commit 648c2ef307c483f3df9fe5100863d3aa3e7f1559.

--- runtime/connection_cache.go
+++ runtime/connection_cache.go
@@ -106,7 +106,7 @@ func (r *Runtime) openAndMigrate(ctx context.Context, cfg cachedConnectionConfig
 		}
 	}
 
-	r.Logger.Debug("opening connection", zap.String("instance_id", cfg.instanceID), zap.String("driver", cfg.driver), zap.String("name", cfg.name))
+	r.Logger.Debug("opening connection", zap.String("instance_id", cfg.instanceID), zap.String("driver", cfg.driver), zap.String("name", cfg.name), zap.Bool("provision", cfg.provision))
 	handle, err := drivers.Open(cfg.driver, cfg.instanceID, cfg.config, r.storage.WithPrefix(cfg.instanceID, cfg.name), activityClient, logger)
 	if err == nil && ctx.Err() != nil {
 		err = fmt.Errorf("timed out while opening driver %q", cfg.driver)
