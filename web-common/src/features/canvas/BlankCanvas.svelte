0a756afc805de9f1664e2ef55f40f65447ae1f00
Cyrus Goh <hello@cyrusgoh.com>
2025-01-18 12:13:05 +0800

    other menu canva icons

--- web-common/src/features/canvas/BlankCanvas.svelte
+++ web-common/src/features/canvas/BlankCanvas.svelte
@@ -7,19 +7,21 @@
     ContextMenuTrigger,
   } from "@rilldata/web-common/components/context-menu";
   import type { CanvasComponentType } from "./components/types";
-  import ChartIcon from "./ChartIcon.svelte";
   import type { ComponentType, SvelteComponent } from "svelte";
-
+  import ChartIcon from "./icons/ChartIcon.svelte";
+  import TableIcon from "./icons/TableIcon.svelte";
+  import TextIcon from "./icons/TextIcon.svelte";
+  import BigNumberIcon from "./icons/BigNumberIcon.svelte";
   const menuItems: {
     id: CanvasComponentType;
     label: string;
     icon: ComponentType<SvelteComponent>;
   }[] = [
-    { id: "markdown", label: "Text", icon: ChartIcon },
-    { id: "kpi", label: "KPI", icon: ChartIcon },
-    { id: "image", label: "Image", icon: ChartIcon },
     { id: "bar_chart", label: "Chart", icon: ChartIcon },
-    { id: "table", label: "Table", icon: ChartIcon },
+    { id: "table", label: "Table", icon: TableIcon },
+    { id: "markdown", label: "Text", icon: TextIcon },
+    { id: "kpi", label: "KPI", icon: BigNumberIcon },
+    { id: "image", label: "Image", icon: ChartIcon },
   ];
 </script>
 
@@ -41,11 +43,7 @@
           class="text-gray-700 text-xs"
         >
           <div class="flex flex-row gap-x-2">
-            <svelte:component
-              this={item.icon}
-              size="16px"
-              class="text-gray-700"
-            />
+            <svelte:component this={item.icon} />
             <span class="text-gray-700 text-xs font-normal">{item.label}</span>
           </div>
         </ContextMenuItem>
