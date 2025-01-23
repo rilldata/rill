078d3ec1559126417ac5f49297799dfc44a32d5b
Dhiraj Kumar <sumankumaribarnwal@gmail.com>
2025-01-23 20:30:09 +0530

    Use component filter for KPI and charts

--- web-common/src/features/canvas/stores/canvas-entity.ts
+++ web-common/src/features/canvas/stores/canvas-entity.ts
@@ -1,12 +1,18 @@
 import { useCanvas } from "@rilldata/web-common/features/canvas/selector";
 import type { CanvasSpecResponseStore } from "@rilldata/web-common/features/canvas/types";
+import { mergeFilters } from "@rilldata/web-common/features/dashboards/pivot/pivot-merge-filters";
 import {
   buildValidMetricsViewFilter,
   createAndExpression,
 } from "@rilldata/web-common/features/dashboards/stores/filter-utils";
+import { convertFilterParamToExpression } from "@rilldata/web-common/features/dashboards/url-state/filters/converters";
 import { queryClient } from "@rilldata/web-common/lib/svelte-query/globalQueryClient";
 import type { DashboardTimeControls } from "@rilldata/web-common/lib/time/types";
-import type { V1TimeRange } from "@rilldata/web-common/runtime-client";
+import {
+  V1Operation,
+  type V1Expression,
+  type V1TimeRange,
+} from "@rilldata/web-common/runtime-client";
 import { runtime } from "@rilldata/web-common/runtime-client/runtime-store";
 import { derived, writable, type Writable } from "svelte/store";
 import { CanvasComponentState } from "./canvas-component";
@@ -76,11 +82,13 @@ export class CanvasEntity {
   createTimeAndFilterStore = (
     metricsViewName: string,
     {
-      timeRangeStore,
-      overrideTimeRange,
+      timeRangeStore = this.timeControls.selectedTimeRange,
+      componentTimeRange,
+      componentFilter,
     }: {
-      timeRangeStore: Writable<DashboardTimeControls | undefined>;
-      overrideTimeRange?: string;
+      timeRangeStore?: Writable<DashboardTimeControls | undefined>;
+      componentTimeRange?: string;
+      componentFilter?: string;
     },
   ) => {
     const { timeControls, filters, spec } = this;
@@ -103,15 +111,30 @@ export class CanvasEntity {
           end: timeRangeVal?.end?.toISOString(),
           timeZone,
         };
-        if (overrideTimeRange) {
-          timeRange = { isoDuration: overrideTimeRange, timeZone };
+        if (componentTimeRange) {
+          timeRange = { isoDuration: componentTimeRange, timeZone };
         }
 
-        const where =
+        const timeGrain = timeRangeVal?.interval;
+
+        const globalWhere =
           buildValidMetricsViewFilter(whereFilter, dtf, dimensions, measures) ??
           createAndExpression([]);
 
-        return { timeRange, where };
+        let where: V1Expression | undefined = globalWhere;
+
+        if (componentFilter) {
+          let expr = convertFilterParamToExpression(componentFilter);
+          if (
+            expr?.cond?.op !== V1Operation.OPERATION_AND &&
+            expr?.cond?.op !== V1Operation.OPERATION_OR
+          ) {
+            expr = createAndExpression([expr]);
+          }
+          where = mergeFilters(globalWhere, expr);
+        }
+
+        return { timeRange, where, timeGrain };
       },
     );
   };
