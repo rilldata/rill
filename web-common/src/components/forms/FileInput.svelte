215f0106643dfbc6141ef4af6b5dfaf8f21f90c7
Aditya Hegde <adityahegderocks@gmail.com>
2025-01-23 18:48:45 +0530

    Add drag-drop support

--- web-common/src/components/forms/FileInput.svelte
+++ web-common/src/components/forms/FileInput.svelte
@@ -7,6 +7,7 @@
   export let error: string | Record<string | number, string[]> | undefined =
     undefined;
   export let multiple: boolean = false;
+  export let accept: string | undefined = undefined;
   export let onInput: (files: File[]) => void;
 
   $: values = value ? (multiple ? value : [value]) : [];
@@ -16,12 +17,26 @@
   function handleInput() {
     onInput(Array.from(fileInput.files));
   }
+
+  function handleFileDrop(event: DragEvent) {
+    console.log(event);
+
+    if (!event.dataTransfer?.files?.length) return;
+    onInput(Array.from(event.dataTransfer.files));
+  }
 </script>
 
-<div class="flex flex-col gap-y-2">
+<div
+  class="container grid"
+  on:dragenter|preventDefault|stopPropagation
+  on:dragleave|preventDefault|stopPropagation
+  on:dragover|preventDefault|stopPropagation
+  on:drop|preventDefault={handleFileDrop}
+  role="presentation"
+>
   <button class="upload-button" on:click={() => fileInput.click()}>
     <Viz size="28px" class="text-gray-400" />
-    <div class="flex flex-col">
+    <div class="container-flex-col">
       <span class="upload-title">Upload an image</span>
       {#if multiple}
         <span class="upload-description">
@@ -33,8 +48,8 @@
   {#if values}
     {#each values as val, i (i)}
       {@const hasError = !!errors?.[i]}
-      <div class="flex flex-col">
-        <div class="flex flex-row items-center gap-x-1.5">
+      <div class="container-flex-col">
+        <div class="file-entry">
           <Attachment size="14px" />
           <span
             class:text-primary-500={!hasError}
@@ -44,10 +59,7 @@
           </span>
         </div>
         {#if hasError}
-          <div
-            in:slide={{ duration: 200 }}
-            class="text-red-600 text-xs py-px mt-0.5"
-          >
+          <div in:slide={{ duration: 200 }} class="error">
             <div>{errors[i]}</div>
           </div>
         {/if}
@@ -56,15 +68,23 @@
   {/if}
   <input
     type="file"
-    accept="image/*"
+    {accept}
     hidden
-    multiple
+    {multiple}
     bind:this={fileInput}
     on:input={handleInput}
   />
 </div>
 
 <style lang="postcss">
+  .container {
+    @apply flex flex-col gap-y-2;
+  }
+
+  .container-flex-col {
+    @apply flex flex-col;
+  }
+
   .upload-button {
     @apply flex flex-row gap-x-2.5 items-center justify-center py-5 min-h-10 w-80;
     @apply border border-neutral-400 bg-neutral-100;
@@ -76,5 +96,13 @@
 
   .upload-description {
     @apply text-xs font-normal text-gray-400;
+  }
+
+  .file-entry {
+    @apply flex flex-row items-center gap-x-1.5;
+  }
+
+  .error {
+    @apply text-red-600 text-xs py-px mt-0.5;
   }
 </style>
