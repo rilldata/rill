8dba5cb2b50b4334dccd400ab6caf697dc16c818
Eric P Green <ericpgreen2@gmail.com>
2025-01-28 14:01:49 +0100

    Add first test cases for different onboarding flows

---
+++ web-local/tests/onboarding.spec.ts
@@ -0,0 +1,102 @@
+import { expect, test } from "@playwright/test";
+import path from "node:path";
+import { TestDataPath } from "./utils/sourceHelpers";
+import { startRuntimeForEachTest } from "./utils/startRuntimeForEachTest";
+
+test.describe("Onboarding", () => {
+  startRuntimeForEachTest({ includeRillYaml: false });
+
+  test.skip("Example project", async ({ page }) => {
+    await page.goto("/");
+
+    // Click on "Example projects"
+    await page.getByRole("link", { name: "Cost monitoring" }).click();
+
+    // TODO: wait for the project to be unpacked
+  });
+
+  test("Rill-managed OLAP - local file", async ({ page }) => {
+    test.setTimeout(20_000);
+    await page.goto("/");
+
+    // Should be redirected to the onboarding page
+    await expect(page).toHaveURL("/welcome");
+    await expect(page.getByText("Welcome to Rill")).toBeVisible();
+
+    // Click on "Connect your data"
+    await page.getByRole("button", { name: "Connect your data" }).click();
+
+    // Should get redirected to the select connectors page
+    await expect(page).toHaveURL("/welcome/select-connectors");
+
+    // Click on button with aria-label "Local file"
+    await page.getByLabel("local_file").click();
+
+    // Click on "Continue"
+    await page.getByRole("button", { name: "Continue" }).click();
+
+    // Should get redirected to the add credentials page
+    await expect(page).toHaveURL("/welcome/add-credentials");
+
+    // Upload a file (Note: this is copied from `sourceHelpers.ts` and should be cleaned-up.)
+    const [fileChooser] = await Promise.all([
+      page.waitForEvent("filechooser"),
+      page.getByText("Upload a CSV, JSON or Parquet file").click(),
+    ]);
+    const fileUploadPromise = fileChooser.setFiles([
+      path.join(TestDataPath, "Adbids.csv"),
+    ]);
+    const fileRespWaitPromise = page.waitForResponse(/files\/entry/);
+    await Promise.all([fileUploadPromise, fileRespWaitPromise]);
+
+    // Expect to be navigated to the new source page
+    await expect(page).toHaveURL("/files/sources/Adbids.yaml");
+
+    // Click "view this source"
+    await page.getByRole("button", { name: "View this source" }).click();
+
+    // Expect to see data
+    await expect(page.getByLabel("Results Preview Table")).toBeVisible();
+    await expect(
+      page.getByLabel("Results Preview Table").getByText("timestamp"), // timestamp is the name of the second column
+    ).toBeVisible();
+    await expect(
+      page.getByLabel("Results Preview Table").getByText("4000", {
+        exact: true,
+      }), // 4000 is the first row's ID
+    ).toBeVisible();
+    await expect(
+      page.getByLabel("Results Preview Table").getByText("Facebook").first(), // Facebook is one of the publishers
+    ).toBeVisible();
+  });
+
+  // TODO: Spin-up a local ClickHouse instance, so we can test this actually working.
+  test("Self-managed OLAP - ClickHouse", async ({ page }) => {
+    await page.goto("/");
+
+    // Should be redirected to the onboarding page
+    await expect(page).toHaveURL("/welcome");
+    await expect(page.getByText("Welcome to Rill")).toBeVisible();
+
+    // Pick a self-managed ClickHouse OLAP
+    await page.getByRole("button", { name: "Connect your data" }).click();
+    await page.getByRole("button", { name: "Self-managed" }).click();
+    // "ClickHouse" is the default, so no need to pick it explicitly.
+    await page.getByRole("button", { name: "Continue" }).click();
+
+    // Add credentials
+    await page.getByRole("textbox", { name: "Host" }).click();
+    await page.getByRole("textbox", { name: "Host" }).fill("localhost");
+    await page.getByRole("textbox", { name: "Host" }).press("Tab");
+    await page.getByRole("textbox", { name: "Port (optional)" }).fill("9000");
+
+    // Submit form and expect to see an error connecting to the dummy ClickHouse instance
+    await page.getByRole("button", { name: "Connect" }).click();
+    await expect(
+      page
+        .locator("#add-data-form div")
+        .filter({ hasText: "dial tcp [::1]:9000: connect" })
+        .nth(1),
+    ).toBeVisible();
+  });
+});
--- web-local/tests/utils/startRuntimeForEachTest.ts
+++ web-local/tests/utils/startRuntimeForEachTest.ts
@@ -1,17 +1,23 @@
 import { test } from "@playwright/test";
-import { rmSync, writeFileSync, existsSync, mkdirSync } from "fs";
-import { spawn } from "node:child_process";
-import type { ChildProcess } from "node:child_process";
-import treeKill from "tree-kill";
-import { isPortOpen } from "@rilldata/web-local/lib/util/isPortOpen";
 import { asyncWaitUntil, waitUntil } from "@rilldata/web-common/lib/waitUtils";
+import { isPortOpen } from "@rilldata/web-local/lib/util/isPortOpen";
 import axios from "axios";
+import { existsSync, mkdirSync, rmSync, writeFileSync } from "fs";
+import type { ChildProcess } from "node:child_process";
+import { spawn } from "node:child_process";
+import treeKill from "tree-kill";
 
 const TEST_PROJECT_DIRECTORY = "temp/test-project";
 const TEST_PORT = 8083;
 const TEST_PORT_GRPC = 9083;
 
-export function startRuntimeForEachTest() {
+interface StartupOptions {
+  includeRillYaml: boolean;
+}
+
+export function startRuntimeForEachTest(
+  options: StartupOptions = { includeRillYaml: true },
+) {
   let childProcess: ChildProcess;
   let rillShutdown = false;
 
@@ -23,13 +29,15 @@ export function startRuntimeForEachTest() {
     if (!existsSync(TEST_PROJECT_DIRECTORY)) {
       mkdirSync(TEST_PROJECT_DIRECTORY, { recursive: true });
     }
-    // Add `rill.yaml` file to the project repo
-    writeFileSync(
-      `${TEST_PROJECT_DIRECTORY}/rill.yaml`,
-      'compiler: rill-beta\ntitle: "Test Project"',
-    );
+    if (options.includeRillYaml) {
+      // Add `rill.yaml` file to the project repo
+      writeFileSync(
+        `${TEST_PROJECT_DIRECTORY}/rill.yaml`,
+        'compiler: rill-beta\ntitle: "Test Project"',
+      );
+    }
 
-    const cmd = `start --no-open --port ${TEST_PORT} --port-grpc ${TEST_PORT_GRPC} --db ${TEST_PROJECT_DIRECTORY}/stage.db?rill_pool_size=4 ${TEST_PROJECT_DIRECTORY} --env connector.duckdb.external_table_storage=false`;
+    const cmd = `start --no-open --port ${TEST_PORT} --port-grpc ${TEST_PORT_GRPC} ${TEST_PROJECT_DIRECTORY}`;
 
     childProcess = spawn("../rill", cmd.split(" "), {
       stdio: "pipe",
