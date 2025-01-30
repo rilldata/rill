f660ac363e757cf2f811e256bc5a18666e5dcd41
Cyrus Goh <hello@cyrusgoh.com>
2025-01-30 09:45:38 -0800

    lint

--- web-admin/src/features/projects/status/selectors.ts
+++ web-admin/src/features/projects/status/selectors.ts
@@ -2,13 +2,6 @@ import {
   createAdminServiceGetProject,
   type V1Deployment,
 } from "@rilldata/web-admin/client";
-import { createRuntimeServiceListResources } from "@rilldata/web-common/runtime-client";
-import type { CreateQueryOptions } from "@tanstack/svelte-query";
-import {
-  type V1ListResourcesResponse,
-  type RpcStatus,
-} from "@rilldata/web-common/runtime-client";
-import type { ErrorType } from "@rilldata/web-common/runtime-client/http-client";
 
 export function useProjectDeployment(orgName: string, projName: string) {
   return createAdminServiceGetProject<V1Deployment | undefined>(
