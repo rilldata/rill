4becef7cca34a11612e2932696154ae272820c1f
Aditya Hegde <adityahegderocks@gmail.com>
2025-01-30 20:34:27 +0530

    fix: public urls with computed measures and dependant measures are hidden (#6520)
    * Add requiredMeasures to explore fields
    
    * Do not send referenced measures for non-window measures
    
    * Fix windowed measures
    
    * Fix lint+tests
    
    * Refactor filter method
    
    * Change function name

--- web-common/src/features/dashboards/state-managers/selectors/dashboard-queries.ts
+++ web-common/src/features/dashboards/state-managers/selectors/dashboard-queries.ts
@@ -1,17 +1,14 @@
-import { getIndependentMeasures } from "@rilldata/web-common/features/dashboards/state-managers/selectors/measures";
-import type { V1MetricsViewSpec } from "@rilldata/web-common/runtime-client";
 import { additionalMeasures } from "../../selectors";
 import type { DimensionThresholdFilter } from "../../stores/metrics-explorer-entity";
 
 export function getMeasuresForDimensionTable(
   activeMeasureName: string,
   dimensionThresholdFilters: DimensionThresholdFilter[],
-  metricsView: V1MetricsViewSpec | undefined,
   visibleMeasureNames: string[],
 ) {
   const allMeasures = new Set([
     ...visibleMeasureNames,
     ...additionalMeasures(activeMeasureName, dimensionThresholdFilters),
   ]);
-  return getIndependentMeasures(metricsView ?? {}, [...allMeasures]);
+  return [...allMeasures];
 }
