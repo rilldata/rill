4d3ea0f819c501a596db1aabe2d1db9ff6f2fc1b
Aditya Hegde <adityahegderocks@gmail.com>
2025-02-03 10:47:01 +0530

    Add preview

--- web-common/src/components/forms/FileInput.svelte
+++ web-common/src/components/forms/FileInput.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
   import LoadingSpinner from "@rilldata/web-common/components/icons/LoadingSpinner.svelte";
   import Viz from "@rilldata/web-common/components/icons/Viz.svelte";
-  import Attachment from "@rilldata/web-common/components/icons/Attachment.svelte";
-  import { extractFileName } from "@rilldata/web-common/features/entity-management/file-path-utils";
-  import { slide } from "svelte/transition";
 
   export let value: string | string[] | undefined = undefined;
   export let error: string | Record<string | number, string[]> | undefined =
@@ -21,35 +18,47 @@
   $: uploadErrors = {};
   let fileInput: HTMLInputElement;
 
+  // maintain a list of filenames to show in error messages.
+  // since the final uploaded url set in `value` is usually not the same this is needed.
+  let fileNames: string[] = value
+    ? multiple
+      ? (value as string[])
+      : [value as string]
+    : [];
+  $: hasValue = values.length > 0 || Object.values(uploading).some((u) => u);
+
   function uploadFiles(files: FileList) {
     uploading = {};
     uploadErrors = {};
+    if (multiple) {
+      value = new Array(files.length).fill("");
+      fileNames = new Array<string>(files.length).fill("");
+    } else {
+      value = "";
+      fileNames = [""];
+    }
     for (let i = 0; i < files.length; i++) {
       void uploadFileWrapper(files[i], i);
     }
   }
 
   async function uploadFileWrapper(file: File, i: number) {
-    setFileUrl(file.name, i);
     uploading[i] = true;
     try {
+      fileNames[i] = file.name;
       const url = await uploadFile(file);
-      setFileUrl(url, i);
+      if (multiple) {
+        if (value === undefined) {
+          value = [];
+        }
+        (value as string[])[i] = url;
+      } else {
+        value = url;
+      }
     } catch (err) {
       uploadErrors[i] = err.message;
     }
     uploading[i] = false;
-  }
-
-  function setFileUrl(fileUrl: string, i: number) {
-    if (multiple) {
-      if (value === undefined) {
-        value = [];
-      }
-      (value as string[])[i] = fileUrl;
-    } else {
-      value = fileUrl;
-    }
   }
 
   function handleInput() {
@@ -65,6 +74,12 @@
   }
 
   let dragOver = false;
+  $: errorMessages = Object.values({
+    ...(errors as Record<string, any>),
+    ...uploadErrors,
+  })
+    .map((e, i) => (fileNames[i] && e ? `${fileNames[i]}:${e}` : ""))
+    .filter(Boolean);
 </script>
 
 <div class="container grid">
@@ -78,41 +93,42 @@
     class:bg-neutral-100={!dragOver}
     class:bg-primary-100={dragOver}
   >
-    <Viz size="28px" class="text-gray-400 pointer-events-none" />
-    <div class="container-flex-col pointer-events-none">
-      <span class="upload-title"> Upload an image </span>
-      {#if multiple}
-        <span class="upload-description">
-          Support for a single or bulk upload.
-        </span>
-      {/if}
-    </div>
-  </button>
-  {#if values}
-    {#each values as val, i (i)}
-      {@const isUploading = !!uploading[i]}
-      {@const hasError = (!!uploadErrors[i] || !!errors?.[i]) && !isUploading}
-      <div class="container-flex-col">
-        <div class="file-entry">
-          {#if isUploading}
-            <LoadingSpinner size="14px" />
-          {:else}
-            <Attachment size="14px" />
+    {#if hasValue}
+      <div class="upload-preview">
+        {#each fileNames as _, i (i)}
+          {@const isUploading = !!uploading[i]}
+          {@const hasError =
+            (!!uploadErrors[i] || !!errors?.[i]) && !isUploading}
+          {@const val = values[i]}
+          {#if (val || isUploading) && !hasError}
+            <div class="border border-neutral-400 p-1">
+              {#if isUploading}
+                <LoadingSpinner size="36px" />
+              {:else}
+                <img src={val} alt="upload" class="h-10 w-fit" />
+              {/if}
+            </div>
           {/if}
-          <span
-            class:text-primary-500={!hasError}
-            class:text-red-600={hasError}
-          >
-            {extractFileName(val)}
+        {/each}
+      </div>
+    {:else}
+      <Viz size="28px" class="text-gray-400 pointer-events-none" />
+      <div class="container-flex-col pointer-events-none">
+        <span class="upload-title"> Upload an image </span>
+        {#if multiple}
+          <span class="upload-description">
+            Support for a single or bulk upload.
           </span>
-        </div>
-        {#if hasError}
-          <div in:slide={{ duration: 200 }} class="error">
-            <div>{uploadErrors[i] ?? errors[i]}</div>
-          </div>
         {/if}
       </div>
-    {/each}
+    {/if}
+  </button>
+  {#if errorMessages.length > 0}
+    <div class="error">
+      {#each errorMessages as errorMessage, i (i)}
+        <div>{errorMessage}</div>
+      {/each}
+    </div>
   {/if}
   <input
     type="file"
@@ -146,8 +162,8 @@
     @apply text-xs font-normal text-gray-400;
   }
 
-  .file-entry {
-    @apply flex flex-row items-center gap-x-1.5;
+  .upload-preview {
+    @apply flex flex-wrap w-full gap-x-1 items-center justify-center pointer-events-none px-4;
   }
 
   .error {
