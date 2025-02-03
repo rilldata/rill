971a14d7826468f3369412c4c31dc5c4b5d3a912
Eric P Green <ericpgreen2@gmail.com>
2025-02-03 20:32:58 +0100

    Provide custom `attributes` in `IssueDevJWT` request

--- web-common/src/features/dashboards/granular-access-policies/useMockUsers.ts
+++ web-common/src/features/dashboards/granular-access-policies/useMockUsers.ts
@@ -2,10 +2,11 @@ import { parseDocument } from "yaml";
 import { createRuntimeServiceGetFile } from "../../../runtime-client";
 
 export interface MockUser {
-  name?: string;
   email?: string;
-  groups?: string[];
+  name?: string;
   admin?: boolean;
+  groups?: string[];
+  attributes?: { [key: string]: any };
 }
 
 export function useMockUsers(instanceId: string) {
