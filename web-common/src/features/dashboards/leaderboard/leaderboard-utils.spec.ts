ec6ec890192d922cdb13c7ec6ba064cc39a3885d
Aditya Hegde <adityahegderocks@gmail.com>
2025-02-05 09:01:17 +0530

    Use objects for tests

--- web-common/src/features/dashboards/leaderboard/leaderboard-utils.spec.ts
+++ web-common/src/features/dashboards/leaderboard/leaderboard-utils.spec.ts
@@ -2,17 +2,33 @@ import { makeHref } from "@rilldata/web-common/features/dashboards/leaderboard/l
 import { describe, it, expect } from "vitest";
 
 describe("makeHref", () => {
-  const TestCases: [
-    string | number | boolean | null,
-    string,
-    string | undefined,
-  ][] = [
-    [null, "http://localhost/dim", undefined],
-    ["http://localhost", "http://localhost/dim", "http://localhost"],
-    [true, "http://localhost/dim", "http://localhost/dim"],
-    [1, "http://localhost/dim", "http://localhost/dim"],
+  const TestCases: {
+    uri: string | number | boolean | null;
+    dimValue: string;
+    expected: string | undefined;
+  }[] = [
+    {
+      uri: null,
+      dimValue: "http://localhost/dim",
+      expected: undefined,
+    },
+    {
+      uri: "http://localhost",
+      dimValue: "http://localhost/dim",
+      expected: "http://localhost",
+    },
+    {
+      uri: true,
+      dimValue: "http://localhost/dim",
+      expected: "http://localhost/dim",
+    },
+    {
+      uri: 1,
+      dimValue: "http://localhost/dim",
+      expected: "http://localhost/dim",
+    },
   ];
-  for (const [uri, dimValue, expected] of TestCases) {
+  for (const { uri, dimValue, expected } of TestCases) {
     it(`makeHref(${uri}, ${dimValue})=${expected}`, () => {
       expect(makeHref(uri as string | boolean | null, dimValue)).toEqual(
         expected,
