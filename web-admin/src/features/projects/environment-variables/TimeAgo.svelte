3092a3595647ba538214d122cc86e0cea11f89a0
Cyrus Goh <hello@cyrusgoh.com>
2025-02-07 10:20:10 -0800

    lint

--- web-admin/src/features/projects/environment-variables/TimeAgo.svelte
+++ web-admin/src/features/projects/environment-variables/TimeAgo.svelte
@@ -1,25 +1,26 @@
 <script lang="ts">
   import { onMount, onDestroy } from "svelte";
   import { timeAgo } from "../../dashboards/listing/utils";
+  import { Duration } from "luxon";
 
   export let datetime: Date | string;
 
   let formattedTime: string;
-  let interval: NodeJS.Timeout | null = null;
+  let interval: ReturnType<typeof setInterval> | null = null;
   let lastInterval: number | null = null;
 
   const INTERVALS = {
-    SECOND: 1000,
-    MINUTE: 60 * 1000,
-    FIVE_MINUTES: 5 * 60 * 1000,
-    HOUR: 60 * 60 * 1000,
+    SECOND: Duration.fromObject({ seconds: 1 }).toMillis(),
+    MINUTE: Duration.fromObject({ minutes: 1 }).toMillis(),
+    FIVE_MINUTES: Duration.fromObject({ minutes: 5 }).toMillis(),
+    HOUR: Duration.fromObject({ hours: 1 }).toMillis(),
   };
 
   function getInterval(date: Date): number {
-    const diff = Date.now() - date.getTime();
-    if (diff < 60 * 1000) return INTERVALS.SECOND;
-    if (diff < 60 * 60 * 1000) return INTERVALS.MINUTE;
-    if (diff < 24 * 60 * 60 * 1000) return INTERVALS.FIVE_MINUTES;
+    const diff = Duration.fromMillis(Date.now() - date.getTime());
+    if (diff.as("minutes") < 1) return INTERVALS.SECOND;
+    if (diff.as("hours") < 1) return INTERVALS.MINUTE;
+    if (diff.as("days") < 1) return INTERVALS.FIVE_MINUTES;
     return INTERVALS.HOUR;
   }
 
@@ -39,7 +40,8 @@
     interval = setInterval(updateTimeAgo, newInterval);
   }
 
-  $: updateTimeAgo(); // Reactively update whenever `datetime` changes
+  // Reactively update whenever `datetime` changes
+  $: updateTimeAgo();
 
   onMount(updateTimeAgo);
 
