bc767b6511ccefe4cdb3b8f5b30c173863f5fa2a
Alexander Thor <alec.karlsson@gmail.com>
2025-02-11 19:43:14 +0100

    Revert "add emit"
    This reverts commit 81d65838eaf58dcc9c1b087a640bf798b81f1012.

--- web-common/src/lib/rpc.ts
+++ web-common/src/lib/rpc.ts
@@ -51,9 +51,3 @@ export function initRPC() {
 export function registerMethod<T>(name: string, func: (params: T) => Promise<unknown> | unknown) {
     methods[name] = func;
 }
-
-export function emit(method: string, params?: unknown) {
-    if (window.parent !== window) {
-        window.parent.postMessage({ method, params } as RPCRequest, "*");
-    }
-}
