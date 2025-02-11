1862713a3c58836e1db72de9882d54d15eb660e6
Alexander Thor <alec.karlsson@gmail.com>
2025-02-11 23:11:02 +0100

    fix: format

--- web-local/tests/explores/banner.spec.ts
+++ web-local/tests/explores/banner.spec.ts
@@ -4,21 +4,20 @@ import { ResourceWatcher } from "web-local/tests/utils/ResourceWatcher";
 import { useDashboardFlowTestSetup } from "web-local/tests/explores/dashboard-flow-test-setup";
 
 test.describe("banner in explore preview", () => {
-    useDashboardFlowTestSetup();
+  useDashboardFlowTestSetup();
 
-    test("Banner should not be visible in explore preview", async ({ page }) => {
+  test("Banner should not be visible in explore preview", async ({ page }) => {
+    await page.getByRole("button", { name: "Preview" }).click();
+    await page.waitForTimeout(1000);
 
-        await page.getByRole("button", { name: "Preview" }).click();
-        await page.waitForTimeout(1000);
+    const banner = page.locator(".app-banner");
+    await expect(banner).toBeHidden();
+  });
 
-        const banner = page.locator('.app-banner');
-        await expect(banner).toBeHidden();
-    });
+  test("Banner should be visible in explore preview", async ({ page }) => {
+    const watcher = new ResourceWatcher(page);
 
-    test("Banner should be visible in explore preview", async ({ page }) => {
-        const watcher = new ResourceWatcher(page);
-
-        const exploreWithBanner = `
+    const exploreWithBanner = `
   type: explore
   metrics_view: "AdBids_model_metrics"
   banner: "This is a banner text"
@@ -26,14 +25,13 @@ test.describe("banner in explore preview", () => {
   dimensions: "*"
       `;
 
-        await page.getByLabel("code").click();
-        await watcher.updateAndWaitForDashboard(exploreWithBanner);
-        await page.getByRole("button", { name: "Preview" }).click();
-        await page.waitForTimeout(500);
+    await page.getByLabel("code").click();
+    await watcher.updateAndWaitForDashboard(exploreWithBanner);
+    await page.getByRole("button", { name: "Preview" }).click();
+    await page.waitForTimeout(500);
 
-        const banner = page.locator('.app-banner');
-        await expect(banner).toBeVisible();
-        await expect(banner).toHaveText(/This is a banner text/);
-    });
-
+    const banner = page.locator(".app-banner");
+    await expect(banner).toBeVisible();
+    await expect(banner).toHaveText(/This is a banner text/);
+  });
 });
