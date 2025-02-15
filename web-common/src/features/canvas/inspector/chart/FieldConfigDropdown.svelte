bdd38eb8233280f8c1118e7334047c64a6f64dfb
Dhiraj Kumar <sumankumaribarnwal@gmail.com>
2025-02-15 15:19:41 +0530

    Fix padding

--- web-common/src/features/canvas/inspector/chart/FieldConfigDropdown.svelte
+++ web-common/src/features/canvas/inspector/chart/FieldConfigDropdown.svelte
@@ -32,46 +32,48 @@
       <ThreeDot size="16px" />
     </IconButton>
   </DropdownMenu.Trigger>
-  <DropdownMenu.Content align="start" class="w-[280px]">
-    <div class="px-2 py-2 border-b border-gray-200">
+  <DropdownMenu.Content align="start" class="w-[280px] p-0">
+    <div class="px-3.5 py-2 border-b border-gray-200">
       <span class="text-xs font-medium"
         >{isDimension ? "X-axis" : "Y-axis"} Configuration</span
       >
     </div>
-    <div class="px-2 py-1.5 flex items-center justify-between">
-      <span class="text-xs">Show axis title</span>
-      <Switch
-        small
-        checked={fieldConfig?.showAxisTitle}
-        on:click={() => {
-          onChange("showAxisTitle", !fieldConfig?.showAxisTitle);
-        }}
-      />
-    </div>
-    {#if isDimension && !isTemporal}
-      <div class="px-2 py-1.5 flex items-center justify-between">
-        <span class="text-xs">Sort</span>
-        <Select
-          size="sm"
-          id="sort-select"
-          width={180}
-          options={sortOptions}
-          value={fieldConfig?.sort || "x"}
-          on:change={(e) => onChange("sort", e.detail)}
-        />
-      </div>
-    {/if}
-    {#if !isDimension}
-      <div class="px-2 py-1.5 flex items-center justify-between">
-        <span class="text-xs">Zero based origin</span>
+    <div class="px-3.5 pb-1.5">
+      <div class="py-1.5 flex items-center justify-between">
+        <span class="text-xs">Show axis title</span>
         <Switch
           small
-          checked={fieldConfig?.zeroBasedOrigin}
+          checked={fieldConfig?.showAxisTitle}
           on:click={() => {
-            onChange("zeroBasedOrigin", !fieldConfig?.zeroBasedOrigin);
+            onChange("showAxisTitle", !fieldConfig?.showAxisTitle);
           }}
         />
       </div>
-    {/if}
+      {#if isDimension && !isTemporal}
+        <div class="py-1.5 flex items-center justify-between">
+          <span class="text-xs">Sort</span>
+          <Select
+            size="sm"
+            id="sort-select"
+            width={180}
+            options={sortOptions}
+            value={fieldConfig?.sort || "x"}
+            on:change={(e) => onChange("sort", e.detail)}
+          />
+        </div>
+      {/if}
+      {#if !isDimension}
+        <div class="py-1.5 flex items-center justify-between">
+          <span class="text-xs">Zero based origin</span>
+          <Switch
+            small
+            checked={fieldConfig?.zeroBasedOrigin}
+            on:click={() => {
+              onChange("zeroBasedOrigin", !fieldConfig?.zeroBasedOrigin);
+            }}
+          />
+        </div>
+      {/if}
+    </div>
   </DropdownMenu.Content>
 </DropdownMenu.Root>
