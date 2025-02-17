dfcc4c842edfe2c724cde39a9406693d4419e76e
Aditya Hegde <adityahegderocks@gmail.com>
2025-02-17 17:00:22 +0530

    fix: billing banner clearing explore banner (#6690)
    * Fix billing banner clearing explore banner
    
    * Fix lint and add explanatory var

--- web-admin/src/features/billing/banner/BillingBannerManager.svelte
+++ web-admin/src/features/billing/banner/BillingBannerManager.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
+  import { onNavigate } from "$app/navigation";
   import type { V1OrganizationPermissions } from "@rilldata/web-admin/client";
   import BillingBannerManagerForAdmins from "@rilldata/web-admin/features/billing/banner/BillingBannerManagerForAdmins.svelte";
   import BillingBannerManagerForViewers from "@rilldata/web-admin/features/billing/banner/BillingBannerManagerForViewers.svelte";
+  import { eventBus } from "@rilldata/web-common/lib/event-bus/event-bus";
 
   export let organization: string;
   export let organizationPermissions: V1OrganizationPermissions;
+
+  onNavigate(({ from, to }) => {
+    const changedOrganization =
+      !from || !to || from.params.organization !== to.params.organization;
+    if (changedOrganization) {
+      eventBus.emit("banner", null);
+    }
+  });
 </script>
 
 {#if organizationPermissions.manageOrg}
