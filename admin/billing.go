d9f7b57398e226467e45db947b19f2760193970a
Kasper Sjørslev <kaspersjo@gmail.com>
2025-02-19 14:28:05 +0100

    Add check for nil pointer

--- admin/billing.go
+++ admin/billing.go
@@ -277,16 +277,20 @@ func (s *Service) StartTrial(ctx context.Context, org *database.Organization) (*
 		return org, sub, nil
 	}
 
-	user, err := s.DB.FindUser(ctx, *org.CreatedByUserID)
-	if err != nil {
-		return nil, nil, fmt.Errorf("failed to get user info: %w", err)
+	var email string
+	if org.CreatedByUserID != nil {
+		user, err := s.DB.FindUser(ctx, *org.CreatedByUserID)
+		if err != nil {
+			return nil, nil, fmt.Errorf("failed to get user info: %w", err)
+		}
+		email = user.Email
 	}
 
 	s.Logger.Named("billing").Info("started trial for organization",
 		zap.String("org_name", org.Name),
 		zap.String("org_id", org.ID),
 		zap.String("trial_end_date", sub.TrialEndDate.String()),
-		zap.String("email", user.Email),
+		zap.String("email", email),
 	)
 
 	org, err = s.DB.UpdateOrganization(ctx, org.ID, &database.UpdateOrganizationOptions{
