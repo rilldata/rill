bb0957cb773567a47a39e2beeee1f73b03677932
Alexander Thor <alec.karlsson@gmail.com>
2025-02-20 21:51:39 +0100

    fix: more lint (in my pocket)

--- web-common/src/features/dashboards/workspace/Dashboard.svelte
+++ web-common/src/features/dashboards/workspace/Dashboard.svelte
@@ -104,7 +104,7 @@
     if (isEmbedded) {
       initEmbedPublicAPI = (
         await import(
-          "@rilldata/web-admin/features/embeds/init-embed-public-api.ts"
+          "@rilldata/web-admin/features/embeds/init-embed-public-api"
         )
       ).default;
     }
