41b02800606c89fbc9c45ce244f577d75951eaf7
Alexander Thor <alec.karlsson@gmail.com>
2025-02-21 15:00:40 +0100

    feat: public api for embeds (#6680)
    * feat: first rpc stub
    
    * add emit
    
    * feat: embed public api
    
    * fix: better name for state stream
    
    * fix: error handling and better spec conformatiy
    
    * fix: cleanup
    
    * fix: format
    
    * fix: make id optional
    
    * fix: first stub of tests
    
    * Revert "fix: first stub of tests"
    
    This reverts commit 2e3308232c5d628da04b862303b04e11337f568f.
    
    * fix: clear method names
    
    * fix: check input
    
    * fix: gitignore
    
    * fix: ignore
    
    * Revert "fix: ignore"
    
    This reverts commit 46ee2b35b1f9f24ea1db10358e43cf3fef9269b3.
    
    * Revert "fix: gitignore"
    
    This reverts commit ec97c8e983bdf8eb47a7023d65de7df2d9594786.
    
    * fix: add tests
    
    * Merge remote-tracking branch 'origin/main' into feat/emit-state-embed
    
    * fix: comments
    
    * fix: moving org and project to const
    
    * fix: moving embed to test hook
    
    * fix: format
    
    * fix: read token
    
    * fix: more fixes
    
    * fix: comments
    
    * fix: format
    
    * .gitignore
    
    * delete test file
    
    * fix: pr comments
    
    * fix: cleaner init
    
    * fix: format
    
    * fix: lint
    
    * fix: more lint (in my pocket)
    
    * fix: wait for bids
    
    * fix: format
    
    * fix: nits
    
    * fix: format

--- web-admin/tests/setup/constants.ts
+++ web-admin/tests/setup/constants.ts
@@ -4,3 +4,8 @@ export const VIEWER_STORAGE_STATE = "playwright/.auth/viewer.json";
 
 export const RILL_DEVTOOL_BACKGROUND_PROCESS_PID_FILE =
   "playwright/rill-devtool-background-process-pid.txt";
+export const RILL_EMBED_SERVICE_TOKEN_FILE =
+  "playwright/rill-service-token.txt";
+export const RILL_ORG_NAME = "e2e";
+export const RILL_PROJECT_NAME = "openrtb";
+export const RILL_SERVICE_NAME = "e2e";
