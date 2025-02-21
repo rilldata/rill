df84ad6920f5453cbaca67d2e122749c3b6efc60
Cyrus Goh <hello@cyrusgoh.com>
2025-02-21 15:07:51 -0800

    remaining leaderboardMeasureName references

--- web-common/src/features/dashboards/state-managers/actions/dimension-table.ts
+++ web-common/src/features/dashboards/state-managers/actions/dimension-table.ts
@@ -9,18 +9,18 @@ export const handleMeasureColumnHeaderClick = (
   generalArgs: DashboardMutables,
   measureName: string,
 ) => {
-  const { leaderboardMeasureName: name } = generalArgs.dashboard;
+  const { leaderboardMeasureNames: names } = generalArgs.dashboard;
 
-  if (measureName === name + "_delta") {
+  if (measureName === names[0] + "_delta") {
     toggleSort(generalArgs, SortType.DELTA_ABSOLUTE);
     setContextColumn(generalArgs, LeaderboardContextColumn.DELTA_ABSOLUTE);
-  } else if (measureName === name + "_delta_perc") {
+  } else if (measureName === names[0] + "_delta_perc") {
     toggleSort(generalArgs, SortType.DELTA_PERCENT);
     setContextColumn(generalArgs, LeaderboardContextColumn.DELTA_PERCENT);
-  } else if (measureName === name + "_percent_of_total") {
+  } else if (measureName === names[0] + "_percent_of_total") {
     toggleSort(generalArgs, SortType.PERCENT);
     setContextColumn(generalArgs, LeaderboardContextColumn.PERCENT);
-  } else if (measureName === name) {
+  } else if (measureName === names[0]) {
     toggleSort(generalArgs, SortType.VALUE);
   } else {
     setLeaderboardMeasureName(generalArgs, measureName);
