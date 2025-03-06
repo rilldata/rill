14b288d0f0c6249a5537fea5701d62dd80d8b67d
Graham Plata <graham.plata@gmail.com>
2025-03-06 09:38:30 -0500

    Fix: Jobs to clean Dangling Resources (#6785)
    * add jobs
    
    * add deletion job
    
    * add project hibernation
    
    * fix query
    
    * swap port
    
    * refactor inactive organization handling to hibernate projects and update job definitions
    
    * bump lint action
    
    * rename LogInactiveOrgs to HibernateInactiveOrgs and update related logging messages
    
    * update HibernateInactiveOrgs job schedule to run Mondays at 7:00am UTC

--- admin/jobs/river/river.go
+++ admin/jobs/river/river.go
@@ -96,6 +96,7 @@ func New(ctx context.Context, dsn string, adm *admin.Service) (jobs.Client, erro
 	river.AddWorker(workers, &RepairOrgBillingWorker{admin: adm, logger: billingLogger})
 	river.AddWorker(workers, &StartTrialWorker{admin: adm, logger: billingLogger})
 	river.AddWorker(workers, &DeleteOrgWorker{admin: adm, logger: billingLogger})
+	river.AddWorker(workers, &HibernateInactiveOrgsWorker{admin: adm, logger: billingLogger})
 
 	periodicJobs := []*river.PeriodicJob{
 		// NOTE: Add new periodic jobs here
@@ -105,6 +106,7 @@ func New(ctx context.Context, dsn string, adm *admin.Service) (jobs.Client, erro
 		newPeriodicJob(&TrialEndCheckArgs{}, "10 1 * * *", true),                 // daily at 1:10am UTC
 		newPeriodicJob(&TrialGracePeriodCheckArgs{}, "15 1 * * *", true),         // daily at 1:15am UTC
 		newPeriodicJob(&SubscriptionCancellationCheckArgs{}, "20 1 * * *", true), // daily at 1:20am UTC
+		newPeriodicJob(&HibernateInactiveOrgsArgs{}, "0 7 * * 1", true),          // Monday at 7:00am UTC
 	}
 
 	// Wire our zap logger to a slog logger for the river client
@@ -382,6 +384,17 @@ func (c *Client) PlanChanged(ctx context.Context, billingCustomerID string) (*jo
 		return nil, err
 	}
 
+	return &jobs.InsertResult{
+		ID:        res.Job.ID,
+		Duplicate: res.UniqueSkippedAsDuplicate,
+	}, nil
+}
+
+func (c *Client) HibernateInactiveOrgs(ctx context.Context) (*jobs.InsertResult, error) {
+	res, err := c.riverClient.Insert(ctx, HibernateInactiveOrgsArgs{}, nil)
+	if err != nil {
+		return nil, err
+	}
 	return &jobs.InsertResult{
 		ID:        res.Job.ID,
 		Duplicate: res.UniqueSkippedAsDuplicate,
