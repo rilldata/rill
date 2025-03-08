9a93ea3cb25ce48b409a4756d426e0834569833e
Cyrus Goh <hello@cyrusgoh.com>
2025-03-07 18:54:58 -0800

    hmm

--- web-common/src/features/dashboards/url-state/convertExploreStateToURLSearchParams.ts
+++ web-common/src/features/dashboards/url-state/convertExploreStateToURLSearchParams.ts
@@ -278,9 +278,7 @@ function toExploreUrl(
     );
   }
 
-  // sort_by is only set when the leaderboard_measure_count is set
   if (
-    exploreState.leaderboardMeasureCount !== undefined &&
     shouldSetParam(preset.exploreSortBy, exploreState.leaderboardMeasureName)
   ) {
     searchParams.set(
