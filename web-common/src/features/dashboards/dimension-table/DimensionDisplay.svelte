5450328be44e4e76656bec917082127942b2692f
Cyrus Goh <hello@cyrusgoh.com>
2025-03-11 10:32:38 -0700

    nit

--- web-common/src/features/dashboards/dimension-table/DimensionDisplay.svelte
+++ web-common/src/features/dashboards/dimension-table/DimensionDisplay.svelte
@@ -39,7 +39,7 @@
   export let timeControlsReady: boolean;
   export let dimension: MetricsViewSpecDimensionV2;
   export let hideStartPivotButton = false;
-  export let canShowForAllMeasures: boolean;
+  export let canShowAllMeasures: boolean;
 
   const {
     selectors: {
@@ -226,7 +226,7 @@
         bind:searchText={$dimensionSearchText}
         onToggleSearchItems={toggleAllSearchItems}
         {comparisonTimeRange}
-        {canShowForAllMeasures}
+        {canShowAllMeasures}
       />
     </div>
 
