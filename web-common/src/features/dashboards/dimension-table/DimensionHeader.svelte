fcfc71606929c128a11190655155499863329257
Cyrus Goh <hello@cyrusgoh.com>
2025-03-11 10:30:13 -0700

    one more

--- web-common/src/features/dashboards/dimension-table/DimensionHeader.svelte
+++ web-common/src/features/dashboards/dimension-table/DimensionHeader.svelte
@@ -47,7 +47,7 @@
     },
     actions: {
       sorting: { toggleSort },
-      dimensions: { setPrimaryDimension, toggledimensionShowAllMeasures },
+      dimensions: { setPrimaryDimension, toggleDimensionShowAllMeasures },
       dimensionsFilter: { toggleDimensionFilterMode },
       contextColumn: { setContextColumns },
     },
@@ -164,7 +164,7 @@
       selectedFilters={$contextColumns}
       dimensionShowAllMeasures={$dimensionShowAllMeasures}
       onContextColumnChange={setContextColumns}
-      onShowForAllMeasures={toggledimensionShowAllMeasures}
+      onShowForAllMeasures={toggleDimensionShowAllMeasures}
       {canShowForAllMeasures}
     />
     {#if !isRowsEmpty}
