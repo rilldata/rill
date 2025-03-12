15ac25e9c044bcdec1b5cc383c5877f69a144f01
Benjamin Egelund-Müller <b@egelund-muller.com>
2025-03-12 17:07:00 +0000

    Fix web checks

--- web-admin/src/features/organizations/users/OrgUsersTable.svelte
+++ web-admin/src/features/organizations/users/OrgUsersTable.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { writable } from "svelte/store";
-  import type { V1MemberUser, V1UserInvite } from "@rilldata/web-admin/client";
+  import type { V1OrganizationMemberUser, V1UserInvite } from "@rilldata/web-admin/client";
   import OrgUsersTableUserCompositeCell from "./OrgUsersTableUserCompositeCell.svelte";
   import OrgUsersTableActionsCell from "./OrgUsersTableActionsCell.svelte";
   import OrgUsersTableRoleCell from "./OrgUsersTableRoleCell.svelte";
@@ -20,7 +20,7 @@
   import ArrowDown from "@rilldata/web-common/components/icons/ArrowDown.svelte";
   import type { InfiniteQueryObserverResult } from "@tanstack/svelte-query";
 
-  interface OrgUser extends V1MemberUser, V1UserInvite {
+  interface OrgUser extends V1OrganizationMemberUser, V1UserInvite {
     invitedBy?: string;
   }
 
