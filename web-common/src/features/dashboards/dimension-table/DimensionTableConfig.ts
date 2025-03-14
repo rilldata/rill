9882f2d048562851a546c9d19a67e0e4004c9a69
Cyrus Goh <hello@cyrusgoh.com>
2025-03-14 12:12:31 -0700

    revert leaderboard and dimension's default col and comparison widths

--- web-common/src/features/dashboards/dimension-table/DimensionTableConfig.ts
+++ web-common/src/features/dashboards/dimension-table/DimensionTableConfig.ts
@@ -9,7 +9,7 @@ export const DIMENSION_TABLE_CONFIG: DimensionTableConfig = {
   maxColumnWidth: 320,
   minColumnWidth: 104,
   minHeaderWidthWhenColumsAreSmall: 160,
-  comparisonColumnWidth: 64,
+  comparisonColumnWidth: 95,
   rowHeight: 24,
   columnHeaderHeight: 28,
   indexWidth: 24,
