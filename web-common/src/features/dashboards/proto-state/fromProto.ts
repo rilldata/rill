4c9654228a87aa414f233a10841fa7c0bae7857d
Aditya Hegde <adityahegderocks@gmail.com>
2025-03-19 11:27:31 +0530

    feat: URL params and bookmarks cloud E2E tests (#6825)
    * Bookmarks e2e tests
    
    * Add bookmark E2E support
    
    * Filter-only bookmark test
    
    * Add a complete bookmark test
    
    * Fix test interference
    
    * PR comments
    
    * Add separate dashboard for bookmarks

--- web-common/src/features/dashboards/proto-state/fromProto.ts
+++ web-common/src/features/dashboards/proto-state/fromProto.ts
@@ -5,8 +5,8 @@ import {
 } from "@rilldata/web-common/features/dashboards/filters/measure-filters/measure-filter-entry";
 import { LeaderboardContextColumn } from "@rilldata/web-common/features/dashboards/leaderboard-context-column";
 import {
-  PivotChipType,
   type PivotChipData,
+  PivotChipType,
   type PivotState,
 } from "@rilldata/web-common/features/dashboards/pivot/types";
 import {
@@ -176,7 +176,10 @@ export function getDashboardStateFromProto(
       chartType: chartTypeMap(dashboard.chartType),
       expandedMeasureName: dashboard.expandedMeasure,
     };
-  } else if (dashboard.activePage !== undefined) {
+  } else if (
+    dashboard.activePage !== undefined &&
+    dashboard.activePage !== DashboardState_ActivePage.UNSPECIFIED
+  ) {
     entity.tdd = {
       pinIndex: -1,
       chartType: TDDChart.DEFAULT,
