cf1048f542b967fd0ee69270804276d55dc9451a
Cyrus Goh <hello@cyrusgoh.com>
2025-03-21 16:52:01 -0700

    attempt

--- web-common/src/features/dashboards/url-state/url-state-variations.spec.ts
+++ web-common/src/features/dashboards/url-state/url-state-variations.spec.ts
@@ -586,6 +586,7 @@ export function getCleanMetricsExploreForAssertion() {
   // TODO
   delete cleanedState.selectedScrubRange;
   delete cleanedState.leaderboardContextColumn;
+  delete cleanedState.leaderboardMeasureCount;
 
   return cleanedState;
 }
