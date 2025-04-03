d575b32f393fb3713770ff4718879a025049679c
Graham Plata <graham.plata@gmail.com>
2025-04-03 09:53:25 -0400

    test: add error handling checks for Excel file reading in metrics export tests

--- runtime/server/server_test.go
+++ runtime/server/server_test.go
@@ -2,24 +2,9 @@ package server_test
 
 import (
 	"context"
-	"testing"
 
-	"github.com/rilldata/rill/runtime/pkg/activity"
-	"github.com/rilldata/rill/runtime/pkg/ratelimit"
-	"github.com/rilldata/rill/runtime/server"
 	"github.com/rilldata/rill/runtime/server/auth"
-	"github.com/rilldata/rill/runtime/testruntime"
-	"github.com/stretchr/testify/require"
 )
-
-func getTestServer(t *testing.T) (*server.Server, string) {
-	rt, instanceID := testruntime.NewInstance(t)
-
-	server, err := server.NewServer(context.Background(), &server.Options{}, rt, nil, ratelimit.NewNoop(), activity.NewNoopClient())
-	require.NoError(t, err)
-
-	return server, instanceID
-}
 
 func testCtx() context.Context {
 	return auth.WithOpen(context.Background())
