b633d4bc9366b22ab59254e7c6b3fa5f0cba9188
Benjamin Egelund-Müller <b@egelund-muller.com>
2025-04-03 17:35:32 +0100

    Rename `runtime/compilers/rillv1` to `runtime/parser` (#7064)

--- runtime/reconcilers/model.go
+++ runtime/reconcilers/model.go
@@ -17,8 +17,8 @@ import (
 	"github.com/google/uuid"
 	runtimev1 "github.com/rilldata/rill/proto/gen/rill/runtime/v1"
 	"github.com/rilldata/rill/runtime"
-	compilerv1 "github.com/rilldata/rill/runtime/compilers/rillv1"
 	"github.com/rilldata/rill/runtime/drivers"
+	"github.com/rilldata/rill/runtime/parser"
 	"github.com/rilldata/rill/runtime/pkg/observability"
 	"github.com/rilldata/rill/runtime/pkg/pbutil"
 	"go.uber.org/zap"
@@ -1436,18 +1436,18 @@ func (r *ModelReconciler) resolveTemplatedProps(ctx context.Context, self *runti
 		}
 	}
 
-	td := compilerv1.TemplateData{
+	td := parser.TemplateData{
 		Environment: inst.Environment,
 		User:        map[string]any{},
 		Variables:   inst.ResolveVariables(false),
 		State:       incrementalState,
 		ExtraProps:  extraProps,
-		Self: compilerv1.TemplateResource{
+		Self: parser.TemplateResource{
 			Meta:  self.Meta,
 			Spec:  self.GetModel().Spec,
 			State: self.GetModel().State,
 		},
-		Resolve: func(ref compilerv1.ResourceName) (string, error) {
+		Resolve: func(ref parser.ResourceName) (string, error) {
 			if dialect == drivers.DialectUnspecified {
 				return ref.Name, nil
 			}
@@ -1455,7 +1455,7 @@ func (r *ModelReconciler) resolveTemplatedProps(ctx context.Context, self *runti
 		},
 	}
 
-	val, err := compilerv1.ResolveTemplateRecursively(props, td, false)
+	val, err := parser.ResolveTemplateRecursively(props, td, false)
 	if err != nil {
 		return nil, fmt.Errorf("failed to resolve template: %w", err)
 	}
@@ -1466,7 +1466,7 @@ func (r *ModelReconciler) resolveTemplatedProps(ctx context.Context, self *runti
 // It returns a map of variable names referenced in the props mapped to their current value (if known).
 func (r *ModelReconciler) analyzeTemplatedVariables(ctx context.Context, props map[string]any) (map[string]string, error) {
 	res := make(map[string]string)
-	err := compilerv1.AnalyzeTemplateRecursively(props, res)
+	err := parser.AnalyzeTemplateRecursively(props, res)
 	if err != nil {
 		return nil, err
 	}
