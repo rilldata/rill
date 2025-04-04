8c2e0ffaaf5cd59991356ba4ad53a9b4bb4bd7f1
Cyrus Goh <hello@cyrusgoh.com>
2025-04-04 15:41:45 -0700

    LeaderboardMeasures url state

--- web-common/src/features/dashboards/url-state/url-params.ts
+++ web-common/src/features/dashboards/url-state/url-params.ts
@@ -22,7 +22,7 @@ export enum ExploreStateURLParams {
   SortDirection = "sort_dir",
 
   LeaderboardMeasureCount = "leaderboard_measure_count",
-
+  LeaderboardMeasures = "leaderboard_measures",
   ExpandedMeasure = "measure",
   ChartType = "chart_type",
   Pin = "pin",
