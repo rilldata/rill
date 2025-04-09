5d1c68e4699279a2b0420dac0626817ce3a920be
Cyrus Goh <hello@cyrusgoh.com>
2025-04-09 15:55:28 -0700

    generic url to wait

--- web-admin/tests/setup/setup.ts
+++ web-admin/tests/setup/setup.ts
@@ -135,7 +135,7 @@ setup.describe("global setup", () => {
       .click();
 
     // Wait for navigation to complete and password field to be visible
-    await page.waitForURL("https://auth.rilldata.io/u/login/password*");
+    await page.waitForURL("**/u/login/password*");
 
     // Fill in the password
     const passwordInput = page.locator('input[name="password"]');
