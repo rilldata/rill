71a86042de90e9172b3caf08010632657e24a502
Cyrus Goh <hello@cyrusgoh.com>
2025-04-10 13:04:01 -0400

    e2e

---
+++ web-local/tests/explores/leaderboard-measure-names.spec.ts
@@ -0,0 +1,44 @@
+import { expect } from "@playwright/test";
+import { interactWithTimeRangeMenu } from "@rilldata/web-common/tests/utils/explore-interactions";
+import { ResourceWatcher } from "../utils/ResourceWatcher";
+import { gotoNavEntry } from "../utils/waitHelpers";
+import { test } from "../setup/base";
+
+test.describe("leaderboard measure names", () => {
+  test.use({ project: "AdBids" });
+
+  test.beforeEach(async ({ page }) => {
+    await page.getByLabel("/dashboards").click();
+    await gotoNavEntry(page, "/dashboards/AdBids_metrics_explore.yaml");
+    await page.getByRole("button", { name: "Preview" }).click();
+  });
+
+  test("single selection", async ({ page }) => {
+    await page.getByTestId("leaderboard-measure-names-dropdown").click();
+
+    await page.getByRole("menuitem", { name: "Sum of Bid Price" }).click();
+
+    // Check if the item is checked using aria-checked
+    await expect(
+      page.getByRole("menuitem", { name: "Sum of Bid Price" }),
+    ).toHaveAttribute("aria-checked", "true");
+  });
+
+  test.skip("multiple selection", async ({ page }) => {
+    await page.getByTestId("leaderboard-measure-names-dropdown").click();
+
+    await page.getByTestId("multi-measure-select-switch").click();
+    await expect(page.getByTestId("multi-measure-select-switch")).toBeChecked();
+
+    await page.getByRole("menuitem", { name: "Sum of Bid Price" }).click();
+    await page.getByRole("menuitem", { name: "Total records" }).click();
+
+    // Check if the items are checked using aria-checked
+    await expect(
+      page.getByRole("menuitem", { name: "Sum of Bid Price" }),
+    ).toHaveAttribute("aria-checked", "true");
+    await expect(
+      page.getByRole("menuitem", { name: "Total records" }),
+    ).toHaveAttribute("aria-checked", "true");
+  });
+});
