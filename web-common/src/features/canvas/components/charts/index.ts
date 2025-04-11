4d3875530f553fb89a84581b8fad3a1c95d59882
Dhiraj Kumar <sumankumaribarnwal@gmail.com>
2025-04-11 16:12:07 +0530

    Move to index file

--- web-common/src/features/canvas/components/charts/index.ts
+++ web-common/src/features/canvas/components/charts/index.ts
@@ -1,1 +1,20 @@
+import { CartesianChartComponent } from "./CartesianChart";
+import type { ChartType } from "./types";
+
 export { default as Chart } from "./Chart.svelte";
+
+type ChartComponent = typeof CartesianChartComponent;
+
+export function createChart(type: ChartType): ChartComponent {
+  switch (type) {
+    case "bar_chart":
+    case "line_chart":
+    case "area_chart":
+    case "stacked_bar":
+    case "stacked_bar_normalized":
+      return CartesianChartComponent;
+
+    default:
+      throw new Error(`Unsupported chart type: ${type}`);
+  }
+}
