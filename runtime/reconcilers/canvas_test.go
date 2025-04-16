bee0f3939032091e0fc518a9533a78767ff9ff8e
Benjamin Egelund-Müller <b@egelund-muller.com>
2025-04-16 15:53:58 +0200

    Check components also

--- runtime/reconcilers/canvas_test.go
+++ runtime/reconcilers/canvas_test.go
@@ -47,8 +47,8 @@ rows:
 	})
 	testruntime.ReconcileParserAndWait(t, rt, id)
 	testruntime.RequireReconcileState(t, rt, id, 6, 0, 0)
-	e1 := testruntime.GetResource(t, rt, id, runtime.ResourceKindCanvas, "c1")
-	require.NotNil(t, e1.GetCanvas().State.ValidSpec)
+	c1 := testruntime.GetResource(t, rt, id, runtime.ResourceKindCanvas, "c1")
+	require.NotNil(t, c1.GetCanvas().State.ValidSpec)
 
 	// Change the model so it breaks the metrics view and canvas, check valid spec is preserved
 	testruntime.PutFiles(t, rt, id, map[string]string{"m1.sql": `SELECT 'bar' as bar, 2 as y`})
@@ -56,8 +56,13 @@ rows:
 	testruntime.RequireReconcileState(t, rt, id, 6, 4, 0)
 	mv1 := testruntime.GetResource(t, rt, id, runtime.ResourceKindMetricsView, "mv1")
 	require.NotNil(t, mv1.GetMetricsView().State.ValidSpec)
-	e1 = testruntime.GetResource(t, rt, id, runtime.ResourceKindCanvas, "c1")
-	require.NotNil(t, e1.GetCanvas().State.ValidSpec)
+	c1 = testruntime.GetResource(t, rt, id, runtime.ResourceKindCanvas, "c1")
+	require.NotNil(t, c1.GetCanvas().State.ValidSpec)
+	require.Len(t, c1.Meta.Refs, 2)
+	for _, componentName := range c1.Meta.Refs {
+		r := testruntime.GetResource(t, rt, id, runtime.ResourceKindComponent, componentName.Name)
+		require.NotNil(t, r.GetComponent().State.ValidSpec)
+	}
 
 	// Fix everything
 	testruntime.PutFiles(t, rt, id, map[string]string{"m1.sql": `SELECT 'foo' as foo, 1 as x`})
@@ -82,7 +87,19 @@ rows:
 	testruntime.RequireReconcileState(t, rt, id, 6, 2, 0)
 	mv1 = testruntime.GetResource(t, rt, id, runtime.ResourceKindMetricsView, "mv1")
 	require.NotNil(t, mv1.GetMetricsView().State.ValidSpec)
-	e1 = testruntime.GetResource(t, rt, id, runtime.ResourceKindCanvas, "c1")
-	require.NotNil(t, e1.GetCanvas().State.ValidSpec)
-	require.NotEmpty(t, e1.Meta.ReconcileError)
+	c1 = testruntime.GetResource(t, rt, id, runtime.ResourceKindCanvas, "c1")
+	require.NotNil(t, c1.GetCanvas().State.ValidSpec)
+	require.NotEmpty(t, c1.Meta.ReconcileError)
+	require.Len(t, c1.Meta.Refs, 2)
+	var valid, invalid int
+	for _, componentName := range c1.Meta.Refs {
+		r := testruntime.GetResource(t, rt, id, runtime.ResourceKindComponent, componentName.Name)
+		if r.GetComponent().State.ValidSpec == nil {
+			invalid++
+		} else {
+			valid++
+		}
+	}
+	require.Equal(t, 1, valid)
+	require.Equal(t, 1, invalid)
 }
