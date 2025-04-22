41bbbc4cfe32618a6ca8a9618b7330e1e97d8201
Eric P Green <ericpgreen2@gmail.com>
2025-04-22 20:33:01 +0200

    Restore previous format of `index.ts`

--- web-common/src/components/dialog/index.ts
+++ web-common/src/components/dialog/index.ts
@@ -1,37 +1,37 @@
 import { Dialog as DialogPrimitive } from "bits-ui";
 
-import Content from "web-common/src/components/dialog/dialog-content.svelte";
-import Description from "web-common/src/components/dialog/dialog-description.svelte";
-import Footer from "web-common/src/components/dialog/dialog-footer.svelte";
-import Header from "web-common/src/components/dialog/dialog-header.svelte";
-import Overlay from "web-common/src/components/dialog/dialog-overlay.svelte";
-import Portal from "web-common/src/components/dialog/dialog-portal.svelte";
-import Title from "web-common/src/components/dialog/dialog-title.svelte";
+import Title from "./dialog-title.svelte";
+import Portal from "./dialog-portal.svelte";
+import Footer from "./dialog-footer.svelte";
+import Header from "./dialog-header.svelte";
+import Overlay from "./dialog-overlay.svelte";
+import Content from "./dialog-content.svelte";
+import Description from "./dialog-description.svelte";
 
 const Root = DialogPrimitive.Root;
 const Trigger = DialogPrimitive.Trigger;
 const Close = DialogPrimitive.Close;
 
 export {
-  Close,
-  Content,
-  Description,
-  //
-  Root as Dialog,
-  Close as DialogClose,
-  Content as DialogContent,
-  Description as DialogDescription,
-  Footer as DialogFooter,
-  Header as DialogHeader,
-  Overlay as DialogOverlay,
-  Portal as DialogPortal,
-  Title as DialogTitle,
-  Trigger as DialogTrigger,
-  Footer,
-  Header,
-  Overlay,
-  Portal,
   Root,
   Title,
+  Portal,
+  Footer,
+  Header,
   Trigger,
+  Overlay,
+  Content,
+  Description,
+  Close,
+  //
+  Root as Dialog,
+  Title as DialogTitle,
+  Portal as DialogPortal,
+  Footer as DialogFooter,
+  Header as DialogHeader,
+  Trigger as DialogTrigger,
+  Overlay as DialogOverlay,
+  Content as DialogContent,
+  Description as DialogDescription,
+  Close as DialogClose,
 };
