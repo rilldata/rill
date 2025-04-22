d3529f400e1be3b76dca2f869d72110bd7b40808
Eric P Green <ericpgreen2@gmail.com>
2025-04-22 21:08:28 +0200

    Add ShadCN `Command` component set

---
+++ web-common/src/components/command/command-separator.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { Command as CommandPrimitive } from "cmdk-sv";
+  import { cn } from "@rilldata/web-common/lib/shadcn";
+
+  type $$Props = CommandPrimitive.SeparatorProps;
+  let className: string | undefined | null = undefined;
+  export { className as class };
+</script>
+
+<CommandPrimitive.Separator
+  class={cn("bg-border -mx-1 h-px", className)}
+  {...$$restProps}
+/>
