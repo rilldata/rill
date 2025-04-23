bef6648cba93fe3e2144f93d3940b652b2dc2ac9
Cyrus Goh <hello@cyrusgoh.com>
2025-04-23 10:27:33 -0700

    include desc to org user invite

--- web-admin/src/features/projects/user-management/UserRoleSelect.svelte
+++ web-admin/src/features/projects/user-management/UserRoleSelect.svelte
@@ -54,9 +54,17 @@
     {/if}
   </DropdownMenuTrigger>
   <DropdownMenuContent side="bottom" align="end">
-    {#each Options as { value, label } (value)}
-      <DropdownMenuItem on:click={() => onSelect(value)} class="text-xs">
-        {label}
+    {#each Options as { value, label, description } (value)}
+      <DropdownMenuItem
+        on:click={() => onSelect(value)}
+        class="text-xs hover:bg-slate-100 {selected?.value === value
+          ? 'bg-slate-50'
+          : ''}"
+      >
+        <div class="flex flex-col">
+          <div class="font-medium">{label}</div>
+          <div class="text-slate-500 text-[10px]">{description}</div>
+        </div>
       </DropdownMenuItem>
     {/each}
   </DropdownMenuContent>
