971a38a4faca574e0857024db8b00a01321ea76a
Alexander Thor <alec.karlsson@gmail.com>
2025-04-24 21:13:25 +0200

    fix: time sorting for export in flat table

--- web-common/src/features/dashboards/pivot/pivot-export.ts
+++ web-common/src/features/dashboards/pivot/pivot-export.ts
@@ -105,7 +105,7 @@ function getPivotAggregationRequest(
           name: timeDimension,
           timeGrain: d.id as V1TimeGrain,
           timeZone: dashboardState.selectedTimezone,
-          alias: `Time ${d.title}`,
+          alias: `${timeDimension}_rill_${d.id}`,
         }
       : {
           name: d.id,
