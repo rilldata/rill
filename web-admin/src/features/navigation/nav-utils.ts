ce4d76d7bf0812bb7100f62a97fc633b6063792f
Cyrus Goh <hello@cyrusgoh.com>
2025-04-24 14:23:26 -0700

    refetch bg when on dashboard page

--- web-admin/src/features/navigation/nav-utils.ts
+++ web-admin/src/features/navigation/nav-utils.ts
@@ -13,6 +13,10 @@ export function withinOrganization(page: Page): boolean {
   return !!page.route?.id?.startsWith("/[organization]");
 }
 
+export function isDashboardPage(page: Page): boolean {
+  return !!(page.route.id === "/[organization]/[project]");
+}
+
 export function isProjectPage(page: Page): boolean {
   return (
     page.route.id === "/[organization]/[project]" ||
