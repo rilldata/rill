75b8053f07fa22b75a12930d62cd1d1d0bf0545d
Cyrus Goh <hello@cyrusgoh.com>
2025-04-29 10:49:26 -0700

    fix project editor granular perms

--- web-admin/src/features/projects/user-management/permissions.ts
+++ web-admin/src/features/projects/user-management/permissions.ts
@@ -21,10 +21,8 @@ export function isProjectEditor(permissions?: V1ProjectPermissions): boolean {
     permissions?.readProdStatus &&
     permissions?.readProjectMembers &&
     permissions?.manageProjectMembers &&
-    // FIXME: when user is editor, isProjectEditor returns false
-    // https://www.notion.so/User-Management-Role-Based-Access-Control-RBAC-Enhancements-8d331b29d9b64d87bca066e06ef87f54?d=1dfba33c8f57807f93d0001cb3b0f23d&pvs=4#1dfba33c8f578065bd26cd44df09e830
-    // permissions?.createMagicAuthTokens &&
-    // permissions?.manageMagicAuthTokens &&
+    permissions?.createMagicAuthTokens &&
+    permissions?.manageMagicAuthTokens &&
     permissions?.createReports &&
     permissions?.createAlerts &&
     permissions?.createBookmarks
