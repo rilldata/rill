839edf3c4cc7698b84d0fe6dbd9c7f1c1656eec6
Graham Plata <graham.plata@gmail.com>
2025-04-29 12:25:08 -0400

    fix: remove error handling from WatchResourcesHandler goroutine

--- runtime/server/controller.go
+++ runtime/server/controller.go
@@ -119,14 +119,11 @@ func (s *Server) WatchResourcesHandler(w http.ResponseWriter, r *http.Request) {
 
 	// Use the existing WatchResources implementation in a goroutine
 	go func() {
-		err := s.WatchResources(&runtimev1.WatchResourcesRequest{
+		s.WatchResources(&runtimev1.WatchResourcesRequest{
 			InstanceId: instanceID,
 			Kind:       kind,
 			Replay:     replay,
 		}, shim)
-		if err != nil {
-			s.logger.Error("watch resources ended with error", zap.Error(err))
-		}
 	}()
 
 	eventServer.ServeHTTP(w, r)
