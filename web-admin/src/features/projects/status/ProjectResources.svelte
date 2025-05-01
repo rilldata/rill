dc594bfcdad3515e01a01100d4462aefc14f9fca
Cyrus Goh <hello@cyrusgoh.com>
2025-05-01 13:00:32 -0700

    reset

--- web-admin/src/features/projects/status/ProjectResources.svelte
+++ web-admin/src/features/projects/status/ProjectResources.svelte
@@ -1,26 +1,85 @@
 <script lang="ts">
   import DelayedSpinner from "@rilldata/web-common/features/entity-management/DelayedSpinner.svelte";
+  import { ResourceKind } from "@rilldata/web-common/features/entity-management/resource-selectors";
   import {
     createRuntimeServiceCreateTrigger,
+    createRuntimeServiceListResources,
     getRuntimeServiceListResourcesQueryKey,
+    V1ReconcileStatus,
+    type V1ListResourcesResponse,
+    type V1Resource,
   } from "@rilldata/web-common/runtime-client";
   import { runtime } from "@rilldata/web-common/runtime-client/runtime-store";
-  import { useQueryClient } from "@tanstack/svelte-query";
+  import { useQueryClient, type Query } from "@tanstack/svelte-query";
   import Button from "web-common/src/components/button/Button.svelte";
   import ProjectResourcesTable from "./ProjectResourcesTable.svelte";
   import RefreshAllSourcesAndModelsConfirmDialog from "./RefreshAllSourcesAndModelsConfirmDialog.svelte";
-  import { useResources } from "./selectors";
-  import { isResourceReconciling } from "../../shared/refetch-interval";
+  import type { HTTPError } from "@rilldata/web-common/runtime-client/fetchWrapper";
+  import {
+    INITIAL_REFETCH_INTERVAL,
+    MAX_REFETCH_INTERVAL,
+    BACKOFF_FACTOR,
+    isResourceErrored,
+    isResourceReconciling,
+  } from "../../shared/refetch-interval";
 
   const queryClient = useQueryClient();
   const createTrigger = createRuntimeServiceCreateTrigger();
 
   let isConfirmDialogOpen = false;
+  let currentRefetchInterval = INITIAL_REFETCH_INTERVAL;
 
   $: ({ instanceId } = $runtime);
 
-  // FIXME: pass `isProjectAdmin` to be self-contained
-  $: resources = useResources(instanceId, true);
+  function calculateRefetchInterval(
+    currentInterval: number,
+    data: V1ListResourcesResponse,
+    query: Query<V1ListResourcesResponse, HTTPError>,
+  ): number | false {
+    if (query.state.error) return false;
+    if (!data) return INITIAL_REFETCH_INTERVAL;
+
+    const hasErrors = data.resources.some(isResourceErrored);
+    const hasReconcilingResources = data.resources.some(isResourceReconciling);
+
+    if (hasErrors || !hasReconcilingResources) {
+      currentRefetchInterval = INITIAL_REFETCH_INTERVAL;
+      return false;
+    }
+
+    currentRefetchInterval = Math.min(
+      currentInterval * BACKOFF_FACTOR,
+      MAX_REFETCH_INTERVAL,
+    );
+    return currentRefetchInterval;
+  }
+
+  $: resources = createRuntimeServiceListResources(
+    instanceId,
+    {
+      // Ensure admins can see all resources, regardless of the security policy
+      skipSecurityChecks: true,
+    },
+    {
+      query: {
+        select: (data: V1ListResourcesResponse) => ({
+          ...data,
+          // Filter out project parser and refresh triggers
+          resources: data?.resources?.filter(
+            (resource: V1Resource) =>
+              resource.meta.name.kind !== ResourceKind.ProjectParser &&
+              resource.meta.name.kind !== ResourceKind.RefreshTrigger,
+          ),
+        }),
+        refetchInterval: (query) =>
+          calculateRefetchInterval(
+            currentRefetchInterval,
+            query.state.data,
+            query,
+          ),
+      },
+    },
+  );
 
   $: hasReconcilingResources = $resources.data?.resources?.some(
     isResourceReconciling,
@@ -35,6 +94,7 @@
         data: { all: true },
       })
       .then(() => {
+        currentRefetchInterval = INITIAL_REFETCH_INTERVAL;
         void queryClient.invalidateQueries({
           queryKey: getRuntimeServiceListResourcesQueryKey(
             instanceId,
