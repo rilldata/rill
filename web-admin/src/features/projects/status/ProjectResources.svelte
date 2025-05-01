c6685ec1955aeca90d241cdc59891ef32bafc34a
Cyrus Goh <hello@cyrusgoh.com>
2025-05-01 12:51:12 -0700

    skipSecurityChecks with admin check

--- web-admin/src/features/projects/status/ProjectResources.svelte
+++ web-admin/src/features/projects/status/ProjectResources.svelte
@@ -20,7 +20,8 @@
 
   $: ({ instanceId } = $runtime);
 
-  $: resources = useResources(instanceId);
+  // FIXME: pass `isProjectAdmin` to be self-contained
+  $: resources = useResources(instanceId, true);
 
   $: hasReconcilingResources = $resources.data?.resources?.some(
     isResourceReconciling,
