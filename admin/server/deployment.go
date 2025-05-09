81d64f10fcc2dc5bf15602aa5a4f2231045e971b
Benjamin Egelund-Müller <b@egelund-muller.com>
2025-05-09 12:26:26 +0100

    Review

--- admin/server/deployment.go
+++ admin/server/deployment.go
@@ -123,7 +123,6 @@ func (s *Server) GetDeploymentCredentials(ctx context.Context, req *adminv1.GetD
 	claims := auth.GetClaims(ctx)
 	permissions := claims.ProjectPermissions(ctx, proj.OrganizationID, proj.ID)
 
-	// If the user is not a superuser, they must have ManageProd permissions
 	if !permissions.ManageProd {
 		return nil, status.Error(codes.PermissionDenied, "does not have permission to manage deployment")
 	}
@@ -218,7 +217,6 @@ func (s *Server) GetIFrame(ctx context.Context, req *adminv1.GetIFrameRequest) (
 	claims := auth.GetClaims(ctx)
 	permissions := claims.ProjectPermissions(ctx, proj.OrganizationID, proj.ID)
 
-	// If the user is not a superuser, they must have ManageProd permissions
 	if !permissions.ManageProd {
 		return nil, status.Error(codes.PermissionDenied, "does not have permission to manage deployment")
 	}
