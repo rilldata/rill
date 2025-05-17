ca43e8ac1fb304906cd8e17957eda3c145d0b1c2
Cyrus Goh <hello@cyrusgoh.com>
2025-05-16 17:33:59 -0700

    functional hover intent

--- web-common/src/components/tooltip/Tooltip.svelte
+++ web-common/src/components/tooltip/Tooltip.svelte
@@ -8,6 +8,15 @@ You can set the following parameters:
 - alignment: start, middle, end. This will align using flexbox-like language.
   - start on left or right means that the top of the tooltip aligns with the top of the target.
   - start on top or  bottom means that the left side of the tooltip aligns with the left side of the target.
+- distance: the distance in pixels between the tooltip and the target.
+- pad: the padding in pixels between the tooltip and the target.
+- suppress: a boolean to suppress the tooltip.
+- activeDelay: the delay in miliseconds before rendering the tooltip once mouse has entered.
+- nonActiveDelay: the delay in miliseconds before unrendering the tooltip once mouse has left.
+- hoverIntentThreshold: the threshold in pixels for hover intent detection.
+- hoverIntentTimeout: the time in milliseconds to wait for hover intent.
+- active: a boolean to control the active state of the tooltip.
+
 
 The suppress prop gives programmatic access to suppressing the tooltip, e.g. if a menu is open or you just need
 to stop all tooltips.
@@ -25,25 +34,48 @@ FIXME: In the future, we should also be listening to focus events from the child
     Alignment,
     Location,
   } from "@rilldata/web-common/lib/place-element";
+  import { onDestroy } from "svelte";
 
   export let location: Location = "bottom";
   export let alignment: Alignment = "middle";
   export let distance = 0;
   export let pad = 8;
-  // provide a programmatic guard to suppressing the tooltip.
   export let suppress = false;
-  /** the delay in miliseconds before rendering the tooltip once mouse has entered */
   export let activeDelay = 200;
-  /** the delay in miliseconds before unrendering the tooltip once mouse has left */
   export let nonActiveDelay = 0;
+  export let hoverIntentThreshold = 5;
+  export let hoverIntentTimeout = 100;
   export let active = false;
 
   let parent: HTMLDivElement;
-  let waitUntilTimer: ReturnType<typeof setTimeout>;
+  let waitUntilTimer: ReturnType<typeof setTimeout> | undefined;
+  let hoverIntentTimer: ReturnType<typeof setTimeout> | undefined;
+  let resetMoveTimer: ReturnType<typeof setTimeout> | undefined;
+  let lastMouseX = 0;
+  let lastMouseY = 0;
+  let mouseMoved = false;
+  let isHovering = false;
+
+  function clearAllTimers() {
+    if (waitUntilTimer) {
+      clearTimeout(waitUntilTimer);
+      waitUntilTimer = undefined;
+    }
+    if (hoverIntentTimer) {
+      clearTimeout(hoverIntentTimer);
+      hoverIntentTimer = undefined;
+    }
+    if (resetMoveTimer) {
+      clearTimeout(resetMoveTimer);
+      resetMoveTimer = undefined;
+    }
+  }
 
   function waitUntil(callback, time = activeDelay) {
-    if (waitUntilTimer) clearTimeout(waitUntilTimer);
-    waitUntilTimer = setTimeout(callback, time);
+    clearAllTimers();
+    waitUntilTimer = setTimeout(() => {
+      callback();
+    }, time);
   }
 
   /** create child-supported suppression.
@@ -58,17 +90,67 @@ FIXME: In the future, we should also be listening to focus events from the child
     childRequestedTooltipSuppression,
   );
 
-  function handlePointerEnter() {
-    waitUntil(() => {
-      active = true;
-    });
+  function handlePointerEnter(event: PointerEvent) {
+    isHovering = true;
+    lastMouseX = event.clientX;
+    lastMouseY = event.clientY;
+    mouseMoved = false;
+
+    clearAllTimers();
+
+    hoverIntentTimer = setTimeout(() => {
+      if (!mouseMoved && isHovering) {
+        waitUntil(() => {
+          active = true;
+        });
+      }
+    }, hoverIntentTimeout);
+  }
+
+  function resetMoveState() {
+    mouseMoved = false;
+    // Start a new hover intent check
+    if (isHovering) {
+      hoverIntentTimer = setTimeout(() => {
+        if (!mouseMoved && isHovering) {
+          waitUntil(() => {
+            active = true;
+          });
+        }
+      }, hoverIntentTimeout);
+    }
+  }
+
+  function handlePointerMove(event: PointerEvent) {
+    if (!isHovering) return;
+
+    const deltaX = Math.abs(event.clientX - lastMouseX);
+    const deltaY = Math.abs(event.clientY - lastMouseY);
+
+    if (deltaX > hoverIntentThreshold || deltaY > hoverIntentThreshold) {
+      mouseMoved = true;
+      clearAllTimers();
+
+      // Set a timer to reset the move state after movement stops
+      resetMoveTimer = setTimeout(resetMoveState, hoverIntentTimeout);
+    }
+
+    lastMouseX = event.clientX;
+    lastMouseY = event.clientY;
   }
 
   function handlePointerLeave() {
+    isHovering = false;
+    mouseMoved = false;
+    clearAllTimers();
     waitUntil(() => {
       active = false;
     }, nonActiveDelay);
   }
+
+  onDestroy(() => {
+    clearAllTimers();
+  });
 </script>
 
 <div
@@ -76,6 +158,7 @@ FIXME: In the future, we should also be listening to focus events from the child
   class="contents"
   bind:this={parent}
   on:pointerenter={handlePointerEnter}
+  on:pointermove={handlePointerMove}
   on:pointerleave={handlePointerLeave}
 >
   <slot />
