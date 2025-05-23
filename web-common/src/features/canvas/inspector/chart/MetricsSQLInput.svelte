e3e80cff6bf3c94a1e1c3b36546215a9553cd1e2
Dhiraj Kumar <sumankumaribarnwal@gmail.com>
2025-05-23 16:19:42 +0530

    style metrics sql input

--- web-common/src/features/canvas/inspector/chart/MetricsSQLInput.svelte
+++ web-common/src/features/canvas/inspector/chart/MetricsSQLInput.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+  import { IconButton } from "@rilldata/web-common/components/button";
   import Button from "@rilldata/web-common/components/button/Button.svelte";
   import InputLabel from "@rilldata/web-common/components/forms/InputLabel.svelte";
+  import MinusIcon from "@rilldata/web-common/components/icons/MinusIcon.svelte";
 
   export let key: string;
   export let label: string | undefined;
@@ -29,36 +31,43 @@
   }
 </script>
 
-<div class="flex flex-col gap-y-4">
+<div class="flex flex-col gap-y-2">
   {#each value as sql, idx (idx)}
     <div class="flex flex-col gap-y-2 relative group">
-      <InputLabel
-        hint={description}
-        small
-        label={(label ?? key) + (value.length > 1 ? ` (Query ${idx + 1})` : "")}
-        id={key + "-" + idx}
-      />
+      <div class="flex items-center justify-between">
+        <InputLabel
+          hint={description}
+          small
+          label={(label ?? key) +
+            (value.length > 1 ? ` (Query ${idx + 1})` : "")}
+          id={key + "-" + idx}
+        />
+        {#if value.length > 1}
+          <IconButton
+            rounded
+            on:click={() => removeQuery(idx)}
+            ariaLabel="Remove this query"
+          >
+            <MinusIcon />
+          </IconButton>
+        {/if}
+      </div>
       <textarea
-        class="w-full p-2 border border-gray-300 rounded-sm font-mono"
-        rows="3"
+        class="w-full p-2 border border-gray-300 rounded-sm source-code"
+        rows="2"
         bind:value={value[idx]}
-        on:blur={(e) => updateSQL(idx, e.target.value)}
+        on:blur={(e) => updateSQL(idx, value[idx])}
         placeholder="SELECT * FROM metrics"
       />
-      {#if value.length > 1}
-        <Button
-          type="plain"
-          class="absolute top-0 right-0 mt-1 mr-1 opacity-70 group-hover:opacity-100 z-10"
-          small
-          on:click={() => removeQuery(idx)}
-          label="Remove this query"
-        >
-          ✕
-        </Button>
-      {/if}
     </div>
   {/each}
-  <Button type="dashed" wide on:click={addQuery} class="mt-2">
+  <Button type="dashed" small on:click={addQuery} class="mt-2">
     + Add another query
   </Button>
 </div>
+
+<style>
+  .source-code {
+    font-family: "Source Code Variable", monospace;
+  }
+</style>
