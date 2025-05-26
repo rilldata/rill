e4663545944d91677ed4eaf6902634da7776f65f
Parag Jain <pjain1@apache.org>
2025-05-26 22:06:02 +0530

    fix for legacy metrics resolver

--- runtime/queries/proto.go
+++ runtime/queries/proto.go
@@ -38,6 +38,7 @@ func ProtoToQuery(q *runtimev1.Query, claims *runtime.SecurityClaims) (runtime.Q
 			Offset:              req.Offset,
 			PivotOn:             req.PivotOn,
 			SecurityClaims:      claims,
+			TimeColumn:          req.TimeColumn,
 		}, nil
 	case *runtimev1.Query_MetricsViewComparisonRequest:
 		req := r.MetricsViewComparisonRequest
