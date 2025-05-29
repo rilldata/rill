ca1c9236ad2846ea268e122ade61c33ae4a86faf
Cyrus Goh <hello@cyrusgoh.com>
2025-05-28 17:27:49 -0700

    wip

--- web-common/src/features/dashboards/state-managers/actions/leaderboard.ts
+++ web-common/src/features/dashboards/state-managers/actions/leaderboard.ts
@@ -18,23 +18,11 @@ export const setLeaderboardMeasureNames = (
   dashboard.leaderboardMeasureNames = names;
 };
 
-const ensureTimeComparisonEnabled = (
-  dashboard: DashboardMutables["dashboard"],
-) => {
-  if (
-    dashboard.leaderboardShowContextForAllMeasures &&
-    !dashboard.showTimeComparison
-  ) {
-    dashboard.showTimeComparison = true;
-  }
-};
-
 export const toggleLeaderboardShowContextForAllMeasures = ({
   dashboard,
 }: DashboardMutables) => {
   dashboard.leaderboardShowContextForAllMeasures =
     !dashboard.leaderboardShowContextForAllMeasures;
-  ensureTimeComparisonEnabled(dashboard);
 };
 
 export const leaderboardActions = {
