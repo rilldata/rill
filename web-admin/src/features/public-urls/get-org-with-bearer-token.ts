e3f858fc89f2426d37254c8952c8659ca02bbd34
Alexander Thor <alec.karlsson@gmail.com>
2025-06-03 21:29:39 +0200

    feat: fetch logos for public url (#7085)
    * feat: fetch logos for public url
    
    * fix: format
    
    * fix: comments

---
+++ web-admin/src/features/public-urls/get-org-with-bearer-token.ts
@@ -0,0 +1,18 @@
+import type { V1GetOrganizationResponse } from "@rilldata/web-admin/client";
+import httpClient from "@rilldata/web-admin/client/http-client";
+
+export const getOrgWithBearerToken = (
+  organizationName: string,
+  token: string,
+) => {
+  return httpClient<V1GetOrganizationResponse>({
+    url: `/v1/organizations/${organizationName}`,
+    method: "get",
+    // We use the bearer token to authenticate the request
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    // To be explicit, we don't need to send credentials (cookies) with the request
+    withCredentials: false,
+  });
+};
