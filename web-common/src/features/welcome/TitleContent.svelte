d2ee7275f7c312259ae75b8570c780420b1439cc
ericokuma <eric.okuma@rilldata.com>
2025-06-18 10:04:45 -0700

    Remove gift card message and 'Let's get started' text from welcome page

--- web-common/src/features/welcome/TitleContent.svelte
+++ web-common/src/features/welcome/TitleContent.svelte
@@ -34,9 +34,6 @@
     <Subheading twColor="text-slate-600">
       Build fast operational dashboards that your team will actually use.
     </Subheading>
-    <div class="hidden">
-      <Subheading twColor="text-slate-600">Let's get started.</Subheading>
-    </div>
   </div>
   <button
     class="pl-2 pr-4 py-2 rounded-sm bg-gradient-to-b from-primary-400 to-primary-500 hover:from-primary-500 hover:to-primary-500"
