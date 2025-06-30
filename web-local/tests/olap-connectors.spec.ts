aa4fa3a0d9cb38036a09fb453d349fd838860d74
Cyrus Goh <hello@cyrusgoh.com>
2025-06-30 14:10:35 -0700

    fix olap connector e2e

--- web-local/tests/olap-connectors.spec.ts
+++ web-local/tests/olap-connectors.spec.ts
@@ -29,8 +29,8 @@ test.describe("ClickHouse connector", () => {
 
   test("Create connector using individual fields", async ({ page }) => {
     // Open the Add Data modal
-    await page.getByRole("button", { name: "Add Asset" }).click();
-    await page.getByRole("menuitem", { name: "Add Data" }).click();
+    await page.getByRole("button", { name: "Add" }).click();
+    await page.getByRole("menuitem", { name: "Data" }).click();
 
     // Select ClickHouse
     await page.locator("#clickhouse").click();
