e6477483ac56ec942985083f0d7b889eff922753
NamanMahor <naman.mahor@rilldata.com>
2025-07-03 00:02:18 +0530

    Implementing ListDatabaseSchemas

--- runtime/drivers/mysql/information_schema.go
+++ runtime/drivers/mysql/information_schema.go
@@ -7,7 +7,41 @@ import (
 )
 
 func (c *connection) ListDatabaseSchemas(ctx context.Context) ([]*drivers.DatabaseSchemaInfo, error) {
-	return nil, nil
+	q := `
+	SELECT schema_name FROM information_schema.schemata
+	WHERE schema_name not in ('information_schema', 'performance_schema', 'sys')
+	ORDER BY schema_name
+	`
+
+	db, err := c.getDB()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	rows, err := db.QueryxContext(ctx, q)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var res []*drivers.DatabaseSchemaInfo
+	for rows.Next() {
+		var schema string
+		if err := rows.Scan(&schema); err != nil {
+			return nil, err
+		}
+		res = append(res, &drivers.DatabaseSchemaInfo{
+			Database:       "",
+			DatabaseSchema: schema,
+		})
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return res, nil
 }
 
 func (c *connection) ListTables(ctx context.Context, database, schema string) ([]*drivers.TableInfo, error) {
