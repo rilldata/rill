cd95f3fa7f98753482ee1cd549b445ebee4795a0
Eric P Green <ericpgreen2@gmail.com>
2025-07-02 16:59:33 -0400

    Turn off feature flag by default

--- web-common/src/features/feature-flags.ts
+++ web-common/src/features/feature-flags.ts
@@ -52,7 +52,7 @@ class FeatureFlags {
   alerts = new FeatureFlag("user", true);
   reports = new FeatureFlag("user", true);
   darkMode = new FeatureFlag("user", false);
-  chat = new FeatureFlag("user", true); // TODO: set this to false before merging
+  chat = new FeatureFlag("user", false);
 
   constructor() {
     this.ready = new Promise<void>((resolve) => {
