cac4a71817517fc9b7543fcc06ead24cfd741d7f
NamanMahor <naman.mahor@rilldata.com>
2025-07-03 13:19:16 +0530

    Implementing ListTables

--- runtime/drivers/redshift/information_schema.go
+++ runtime/drivers/redshift/information_schema.go
@@ -11,9 +11,11 @@ import (
 )
 
 func (c *Connection) ListDatabaseSchemas(ctx context.Context) ([]*drivers.DatabaseSchemaInfo, error) {
-	q := `SELECT database_name,schema_name 
-	FROM svv_all_tables where schema_name not in ('information_schema','pg_catalog') 
-	group by database_name,schema_name`
+	q := `
+	SELECT database_name,schema_name 
+	FROM svv_all_tables 
+	WHERE schema_name NOT IN ('information_schema','pg_catalog') 
+	GROUP BY database_name,schema_name`
 
 	awsConfig, err := c.awsConfig(ctx, c.config.AWSRegion)
 	if err != nil {
@@ -44,8 +46,46 @@ func (c *Connection) ListDatabaseSchemas(ctx context.Context) ([]*drivers.Databa
 	return res, nil
 }
 
-func (c *Connection) ListTables(ctx context.Context, database, schema string) ([]*drivers.TableInfo, error) {
-	return nil, nil
+func (c *Connection) ListTables(ctx context.Context, database, databaseSchema string) ([]*drivers.TableInfo, error) {
+	q := fmt.Sprintf(`
+	SELECT
+	table_name,
+	CASE
+		WHEN table_type = 'VIEW' THEN true
+		ELSE false
+	END AS view
+	FROM svv_all_tables
+	WHERE database_name = '%s'
+	AND schema_name = '%s';
+	`, database, databaseSchema)
+
+	awsConfig, err := c.awsConfig(ctx, c.config.AWSRegion)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get AWS config: %w", err)
+	}
+
+	client := redshiftdata.NewFromConfig(awsConfig)
+
+	queryExecutionID, err := c.executeQuery(ctx, client, q, c.config.Database, c.config.Workgroup, c.config.ClusterIdentifier)
+	if err != nil {
+		return nil, fmt.Errorf("failed to list schemas: %w", err)
+	}
+
+	result, err := client.GetStatementResult(ctx, &redshiftdata.GetStatementResultInput{
+		Id: aws.String(queryExecutionID),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to get query results: %w", err)
+	}
+
+	var res []*drivers.TableInfo
+	for _, record := range result.Records {
+		res = append(res, &drivers.TableInfo{
+			Name: record[0].(*types.FieldMemberStringValue).Value,
+			View: record[1].(*types.FieldMemberBooleanValue).Value,
+		})
+	}
+	return res, nil
 }
 
 func (c *Connection) GetTable(ctx context.Context, database, schema, table string) (*drivers.TableMetadata, error) {
