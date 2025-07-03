fdcc24b1d0f07cd5b20eb47145a8b6d84f345a28
NamanMahor <naman.mahor@rilldata.com>
2025-07-03 13:06:47 +0530

    Implementing ListDatabaseSchemas

--- runtime/drivers/redshift/information_schema.go
+++ runtime/drivers/redshift/information_schema.go
@@ -2,12 +2,46 @@ package redshift
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/redshiftdata"
+	"github.com/aws/aws-sdk-go-v2/service/redshiftdata/types"
 	"github.com/rilldata/rill/runtime/drivers"
 )
 
 func (c *Connection) ListDatabaseSchemas(ctx context.Context) ([]*drivers.DatabaseSchemaInfo, error) {
-	return nil, nil
+	q := `SELECT database_name,schema_name 
+	FROM svv_all_tables where schema_name not in ('information_schema','pg_catalog') 
+	group by database_name,schema_name`
+
+	awsConfig, err := c.awsConfig(ctx, c.config.AWSRegion)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get AWS config: %w", err)
+	}
+
+	client := redshiftdata.NewFromConfig(awsConfig)
+
+	queryExecutionID, err := c.executeQuery(ctx, client, q, c.config.Database, c.config.Workgroup, c.config.ClusterIdentifier)
+	if err != nil {
+		return nil, fmt.Errorf("failed to list schemas: %w", err)
+	}
+
+	result, err := client.GetStatementResult(ctx, &redshiftdata.GetStatementResultInput{
+		Id: aws.String(queryExecutionID),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to get query results: %w", err)
+	}
+
+	var res []*drivers.DatabaseSchemaInfo
+	for _, record := range result.Records {
+		res = append(res, &drivers.DatabaseSchemaInfo{
+			Database:       record[0].(*types.FieldMemberStringValue).Value,
+			DatabaseSchema: record[1].(*types.FieldMemberStringValue).Value,
+		})
+	}
+	return res, nil
 }
 
 func (c *Connection) ListTables(ctx context.Context, database, schema string) ([]*drivers.TableInfo, error) {
