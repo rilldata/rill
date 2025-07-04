7ed2dd87ad51a84b34326a7ccfd920c5cae0b91d
Parag Jain <pjain1@apache.org>
2025-07-04 10:56:15 +0530

    add service name as args to remove cmd

--- cli/cmd/service/remove.go
+++ cli/cmd/service/remove.go
@@ -7,17 +7,13 @@ import (
 )
 
 func RemoveCmd(ch *cmdutil.Helper) *cobra.Command {
-	var service, projectName string
+	var projectName string
 
 	removeCmd := &cobra.Command{
-		Use:   "remove",
+		Use:   "remove <service-name>",
+		Args:  cobra.ExactArgs(1),
 		Short: "remove service from org or project",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			err := cmdutil.StringPromptIfEmpty(&service, "Enter service name")
-			if err != nil {
-				return err
-			}
-
 			client, err := ch.Client()
 			if err != nil {
 				return err
@@ -26,32 +22,31 @@ func RemoveCmd(ch *cmdutil.Helper) *cobra.Command {
 			if projectName != "" {
 				// If projectName is provided, delete the service from the specified project
 				_, err = client.RemoveProjectMemberService(cmd.Context(), &adminv1.RemoveProjectMemberServiceRequest{
-					Name:             service,
+					Name:             args[0],
 					OrganizationName: ch.Org,
 					ProjectName:      projectName,
 				})
 				if err != nil {
 					return err
 				}
-				ch.PrintfSuccess("Removed service %q from project %q\n", service, projectName)
+				ch.PrintfSuccess("Removed service %q from project %q\n", args[0], projectName)
 				return nil
 			}
 
 			_, err = client.RemoveOrganizationMemberService(cmd.Context(), &adminv1.RemoveOrganizationMemberServiceRequest{
-				Name:             service,
+				Name:             args[0],
 				OrganizationName: ch.Org,
 			})
 			if err != nil {
 				return err
 			}
 
-			ch.PrintfSuccess("Removed service %q from org %q\n", service, ch.Org)
+			ch.PrintfSuccess("Removed service %q from org %q\n", args[0], ch.Org)
 
 			return nil
 		},
 	}
 
-	removeCmd.Flags().StringVar(&service, "service", "", "Service name to remove")
 	removeCmd.Flags().StringVar(&projectName, "project", "", "Project to remove service from")
 	return removeCmd
 }
