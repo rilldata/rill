bde20704edc052491327ad8c79f88e631beaf0b4
NamanMahor <naman.mahor@rilldata.com>
2025-07-05 02:21:23 +0530

    implementing snowflake information schema apis

--- runtime/drivers/snowflake/information_schema.go
+++ runtime/drivers/snowflake/information_schema.go
@@ -3,64 +3,157 @@ package snowflake
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/rilldata/rill/runtime/drivers"
 )
 
 func (c *connection) ListDatabaseSchemas(ctx context.Context) ([]*drivers.DatabaseSchemaInfo, error) {
-	// in snowflake it show all the schemas in all the databases
-	q := "SHOW SCHEMAS"
 	db, err := c.getDB()
 	if err != nil {
 		return nil, err
 	}
 	defer db.Close()
 
-	rows, err := db.QueryxContext(ctx, q)
+	// Get all databases (TERSE for faster results)
+	dbRows, err := db.QueryxContext(ctx, "SHOW TERSE DATABASES")
+	if err != nil {
+		return nil, err
+	}
+	defer dbRows.Close()
+
+	var results []*drivers.DatabaseSchemaInfo
+
+	for dbRows.Next() {
+		cols := make([]interface{}, 5)
+		for i := range cols {
+			var v interface{}
+			cols[i] = &v
+		}
+		if err := dbRows.Scan(cols...); err != nil {
+			return nil, err
+		}
+		dbName := fmt.Sprintf("%v", *(cols[1].(*interface{}))) // column 1 = database name
+
+		schemaQuery := fmt.Sprintf("SHOW TERSE SCHEMAS IN DATABASE %s", sqlSafeName(dbName))
+		schemaRows, err := db.QueryxContext(ctx, schemaQuery)
+		if err != nil {
+			return nil, err
+		}
+
+		for schemaRows.Next() {
+			schemaCols := make([]interface{}, 5)
+			for i := range schemaCols {
+				var v interface{}
+				schemaCols[i] = &v
+			}
+			if err := schemaRows.Scan(schemaCols...); err != nil {
+				schemaRows.Close()
+				return nil, err
+			}
+			schemaName := fmt.Sprintf("%v", *(schemaCols[1].(*interface{}))) // column 1 = schema name
+
+			// Skip INFORMATION_SCHEMA and other system schemas
+			if strings.EqualFold(schemaName, "INFORMATION_SCHEMA") {
+				continue
+			}
+
+			results = append(results, &drivers.DatabaseSchemaInfo{
+				Database:       dbName,
+				DatabaseSchema: schemaName,
+			})
+		}
+		schemaRows.Close()
+	}
+
+	if err := dbRows.Err(); err != nil {
+		return nil, err
+	}
+
+	return results, nil
+}
+
+func (c *connection) ListTables(ctx context.Context, database, databaseSchema string) ([]*drivers.TableInfo, error) {
+	q := fmt.Sprintf(`
+		SELECT
+			table_name,
+			CASE WHEN table_type = 'VIEW' THEN true ELSE false END AS view
+		FROM %s.INFORMATION_SCHEMA.TABLES
+		WHERE table_schema = ?
+		ORDER BY table_name
+	`, sqlSafeName(database))
+
+	db, err := c.getDB()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	rows, err := db.QueryxContext(ctx, q, databaseSchema)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
-	var names []*drivers.DatabaseSchemaInfo
+	var res []*drivers.TableInfo
 	for rows.Next() {
-		cols, _ := rows.Columns()
-		vals := make([]interface{}, len(cols))
-		for i := range vals {
-			var v interface{}
-			vals[i] = &v
-		}
-
-		if err := rows.Scan(vals...); err != nil {
+		var name string
+		var view bool
+		if err := rows.Scan(&name, &view); err != nil {
 			return nil, err
 		}
-		nameVal := *(vals[1].(*interface{})) // column at index 1 is "name"
-		nameStr, ok := nameVal.(string)
-		if !ok {
-			return nil, fmt.Errorf("expected schema name as string got %T", nameVal)
-		}
-
-		databaseNameVal := *(vals[4].(*interface{})) // column at index 4 is "name"
-		databaseNameStr, ok := databaseNameVal.(string)
-		if !ok {
-			return nil, fmt.Errorf("expected database name as string got %T", nameVal)
-		}
-		names = append(names, &drivers.DatabaseSchemaInfo{
-			Database:       databaseNameStr,
-			DatabaseSchema: nameStr,
+		res = append(res, &drivers.TableInfo{
+			Name: name,
+			View: view,
 		})
 	}
 	if err := rows.Err(); err != nil {
 		return nil, err
 	}
 
-	return names, nil
+	return res, nil
 }
 
-func (c *connection) ListTables(ctx context.Context, database, schema string) ([]*drivers.TableInfo, error) {
-	return nil, nil
+func (c *connection) GetTable(ctx context.Context, database, databaseSchema, table string) (*drivers.TableMetadata, error) {
+	q := fmt.Sprintf(`
+		SELECT
+			column_name,
+			data_type
+		FROM %s.INFORMATION_SCHEMA.COLUMNS
+		WHERE table_schema = ? AND table_name = ?
+		ORDER BY ordinal_position
+	`, sqlSafeName(database))
+
+	db, err := c.getDB()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	rows, err := db.QueryxContext(ctx, q, databaseSchema, table)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	schemaMap := make(map[string]string)
+	for rows.Next() {
+		var colName, colType string
+		if err := rows.Scan(&colName, &colType); err != nil {
+			return nil, err
+		}
+		schemaMap[colName] = colType
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return &drivers.TableMetadata{
+		Schema: schemaMap,
+	}, nil
 }
 
-func (c *connection) GetTable(ctx context.Context, database, schema, table string) (*drivers.TableMetadata, error) {
-	return nil, nil
+func sqlSafeName(name string) string {
+	escaped := strings.ReplaceAll(name, `"`, `""`)
+	return fmt.Sprintf("%q", escaped)
 }
