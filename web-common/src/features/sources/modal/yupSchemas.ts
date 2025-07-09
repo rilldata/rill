bc039ab4900a5e2ddf57e90fcb91b8f21105682a
Cyrus Goh <hello@cyrusgoh.com>
2025-07-08 18:00:38 -0700

    remove salesforce yup

--- web-common/src/features/sources/modal/yupSchemas.ts
+++ web-common/src/features/sources/modal/yupSchemas.ts
@@ -203,15 +203,6 @@ export const getYupSchema = {
     //   .matches(VALID_NAME_PATTERN, INVALID_NAME_MESSAGE)
     //   .required("Connector name is required"),
   }),
-
-  salesforce: yup.object().shape({
-    soql: yup.string().required("SOQL is required"),
-    sobject: yup.string().required("SObject is required"),
-    name: yup
-      .string()
-      .matches(VALID_NAME_PATTERN, INVALID_NAME_MESSAGE)
-      .required("Source name is required"),
-  }),
 };
 
 export const dsnSchema = yup.object().shape({
