b202290c129c1185f779cd890713996c0f69dcc7
Eric P Green <ericpgreen2@gmail.com>
2025-07-09 14:25:11 -0400

    feat: AI chatbot v0 (#7444)
    * First commit
    
    * Some cleanup
    
    * Scope conversations by `owner_id`
    
    Bugfix
    
    * Use a sequence number to order messages, rather than microsecond timestamps
    
    * Add specific runtime permission for using AI
    
    * Add simple truncation strategy for long conversations
    
    * Use "Find" and "Insert" terminology for db layer
    
    * Provide `useAI` permission
    
    * Frontend cleanup
    
    * Turn off feature flag by default
    
    * Fix lint
    
    * Fix test
    
    * Simplify `mcp_test.go`
    
    * Address some feedback
    
    * Declare common AI types in new AI proto file
    
    * Address more feedback
    
    * Fix test builds
    
    * Do a better job converting to OpenAI message format
    
    * Remove unused `parentMessageID`
    
    * Add index on `conversations` table: `instance_id` + `owner_id`
    
    * Nit: use `nil` not empty slice
    
    Co-authored-by: Anshul Khandelwal <12948312+k-anshul@users.noreply.github.com>
    
    * Remove unnecessary index
    
    ---------
    
    Co-authored-by: Nishant Bangarwa <nishant.monu51@gmail.com>
    Co-authored-by: Anshul Khandelwal <12948312+k-anshul@users.noreply.github.com>

--- runtime/drivers/ai.go
+++ runtime/drivers/ai.go
@@ -2,13 +2,10 @@ package drivers
 
 import (
 	"context"
+
+	aiv1 "github.com/rilldata/rill/proto/gen/rill/ai/v1"
 )
 
 type AIService interface {
-	Complete(ctx context.Context, msgs []*CompletionMessage) (*CompletionMessage, error)
-}
-
-type CompletionMessage struct {
-	Role string
-	Data string
+	Complete(ctx context.Context, msgs []*aiv1.CompletionMessage, tools []*aiv1.Tool) (*aiv1.CompletionMessage, error)
 }
