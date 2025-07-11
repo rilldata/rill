472dd1b1f0af7ef4e195dc50ae9f7eade82cb234
Cyrus Goh <hello@cyrusgoh.com>
2025-07-11 08:49:24 -0700

    lint

--- web-common/src/features/sources/modal/utils.ts
+++ web-common/src/features/sources/modal/utils.ts
@@ -6,3 +6,13 @@ export function isEmpty(val: any) {
     (typeof val === "string" && val.trim() === "")
   );
 }
+
+export function normalizeErrors(
+  err: any,
+): string | string[] | null | undefined {
+  if (!err) return undefined;
+  if (Array.isArray(err)) return err;
+  if (typeof err === "string") return err;
+  if (err._errors && Array.isArray(err._errors)) return err._errors;
+  return undefined;
+}
