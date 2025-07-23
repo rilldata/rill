323456da350fe2ff5924302400e49a2f5aa41bad
Aditya Hegde <adityahegderocks@gmail.com>
2025-07-23 09:45:17 +0530

    feat: editing report rows and columns (#7632)
    * feat: editing report rows and columns
    
    * Fix time column from TDD
    
    * Identify flat mode correctly
    
    * Add editing of rows and columns E2E
    
    * UX tweaks
    
    * Fix issue with sorting
    
    * Update title
    
    * Highlight column error only after user submits
    
    * Move to using builder pattern for aggregation request
    
    * Improve readability

--- web-common/src/features/scheduled-reports/BaseScheduledReportForm.svelte
+++ web-common/src/features/scheduled-reports/BaseScheduledReportForm.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+  import InputLabel from "@rilldata/web-common/components/forms/InputLabel.svelte";
   import MultiInput from "@rilldata/web-common/components/forms/MultiInput.svelte";
   import FormSection from "@rilldata/web-common/components/forms/FormSection.svelte";
   import { getHasSlackConnection } from "@rilldata/web-common/features/alerts/delivery-tab/notifiers-utils";
   import type { Filters } from "@rilldata/web-common/features/dashboards/stores/Filters.ts";
   import type { TimeControls } from "@rilldata/web-common/features/dashboards/stores/TimeControls.ts";
   import FiltersForm from "@rilldata/web-common/features/scheduled-reports/FiltersForm.svelte";
+  import RowsAndColumnsForm from "@rilldata/web-common/features/scheduled-reports/fields/RowsAndColumnsForm.svelte";
   import ScheduleForm from "@rilldata/web-common/features/scheduled-reports/ScheduleForm.svelte";
   import type { ReportValues } from "@rilldata/web-common/features/scheduled-reports/utils";
   import { V1ExportFormat } from "@rilldata/web-common/runtime-client";
@@ -34,115 +36,127 @@
 
 <form
   autocomplete="off"
-  class="flex flex-col gap-y-3"
+  class="flex flex-col gap-y-3 w-full"
   id={formId}
   on:submit|preventDefault={submit}
   use:enhance
 >
   <span>Email recurring exports to recipients.</span>
-  <Input
-    bind:value={$data["title"]}
-    errors={$errors["title"]}
-    id="title"
-    label="Report title"
-    placeholder="My report"
-  />
-  <ScheduleForm {data} {exploreName} />
-  <Select
-    bind:value={$data["exportFormat"]}
-    id="exportFormat"
-    label="Format"
-    options={[
-      { value: V1ExportFormat.EXPORT_FORMAT_CSV, label: "CSV" },
-      { value: V1ExportFormat.EXPORT_FORMAT_PARQUET, label: "Parquet" },
-      { value: V1ExportFormat.EXPORT_FORMAT_XLSX, label: "XLSX" },
-    ]}
-  />
-  <Input
-    bind:value={$data["exportLimit"]}
-    errors={$errors["exportLimit"]}
-    id="exportLimit"
-    label="Row limit"
-    optional
-    placeholder="1000"
-  />
-  <div class="flex items-center gap-x-1">
-    <Checkbox
-      bind:checked={$data["exportIncludeHeader"]}
-      id="exportIncludeHeader"
-      onCheckedChange={(checked) => {
-        $data["exportIncludeHeader"] = Boolean(checked);
-      }}
-      inverse
-      disabled={$data["exportFormat"] === V1ExportFormat.EXPORT_FORMAT_PARQUET}
-      label="Include metadata"
+  <div class="flex flex-col gap-y-3 w-full h-[600px] overflow-y-scroll">
+    <Input
+      bind:value={$data["title"]}
+      errors={$errors["title"]}
+      id="title"
+      label="Report title"
+      placeholder="My report"
     />
-    <Tooltip location="right" alignment="middle" distance={8}>
-      <div class="text-gray-500" style="transform:translateY(-.5px)">
-        <InfoCircle size="13px" />
-      </div>
-      <TooltipContent maxWidth="400px" slot="tooltip-content">
-        Adds a header to the file that includes filters, time range, and other
-        metadata.
-      </TooltipContent>
-    </Tooltip>
-  </div>
+    <ScheduleForm {data} {exploreName} />
+    <Select
+      bind:value={$data["exportFormat"]}
+      id="exportFormat"
+      label="Format"
+      options={[
+        { value: V1ExportFormat.EXPORT_FORMAT_CSV, label: "CSV" },
+        { value: V1ExportFormat.EXPORT_FORMAT_PARQUET, label: "Parquet" },
+        { value: V1ExportFormat.EXPORT_FORMAT_XLSX, label: "XLSX" },
+      ]}
+    />
+    <Input
+      bind:value={$data["exportLimit"]}
+      errors={$errors["exportLimit"]}
+      id="exportLimit"
+      label="Row limit"
+      optional
+      placeholder="1000"
+    />
+    <div class="flex items-center gap-x-1">
+      <Checkbox
+        bind:checked={$data["exportIncludeHeader"]}
+        id="exportIncludeHeader"
+        onCheckedChange={(checked) => {
+          $data["exportIncludeHeader"] = Boolean(checked);
+        }}
+        inverse
+        disabled={$data["exportFormat"] ===
+          V1ExportFormat.EXPORT_FORMAT_PARQUET}
+        label="Include metadata"
+      />
+      <Tooltip location="right" alignment="middle" distance={8}>
+        <div class="text-gray-500" style="transform:translateY(-.5px)">
+          <InfoCircle size="13px" />
+        </div>
+        <TooltipContent maxWidth="400px" slot="tooltip-content">
+          Adds a header to the file that includes filters, time range, and other
+          metadata.
+        </TooltipContent>
+      </Tooltip>
+    </div>
 
-  <FormSection title="Filters" padding="">
-    <FiltersForm {filters} {timeControls} maxWidth={750} side="top" />
-  </FormSection>
+    <div class="flex flex-col gap-y-3">
+      <InputLabel label="Filters" id="filters" capitalize={false} />
+      <FiltersForm {filters} {timeControls} maxWidth={750} side="top" />
+    </div>
 
-  <MultiInput
-    id="emailRecipients"
-    label="Email Recipients"
-    hint="Recipients will receive different views based on their security policy.
+    <RowsAndColumnsForm
+      bind:rows={$data["rows"]}
+      bind:columns={$data["columns"]}
+      columnErrors={$errors["columns"]}
+      {instanceId}
+      {exploreName}
+    />
+
+    <MultiInput
+      id="emailRecipients"
+      label="Email Recipients"
+      hint="Recipients will receive different views based on their security policy.
         Recipients without project access can only download the report."
-    bind:values={$data["emailRecipients"]}
-    errors={$errors["emailRecipients"]}
-    singular="email"
-    plural="emails"
-    placeholder="Enter an email address"
-  />
-  {#if $hasSlackNotifier.data}
-    <FormSection
-      bind:enabled={$data["enableSlackNotification"]}
-      showSectionToggle
-      title="Slack notifications"
-      padding=""
-    >
-      <MultiInput
-        id="slackChannels"
-        label="Channels"
-        hint="We’ll send alerts directly to these channels."
-        bind:values={$data["slackChannels"]}
-        errors={$errors["slackChannels"]}
-        singular="channel"
-        plural="channels"
-        placeholder="# Enter a Slack channel name"
-      />
-      <MultiInput
-        id="slackUsers"
-        label="Users"
-        hint="We’ll alert them with direct messages in Slack."
-        bind:values={$data["slackUsers"]}
-        errors={$errors["slackUsers"]}
-        singular="user"
-        plural="users"
-        placeholder="Enter an email address"
-      />
-    </FormSection>
-  {:else}
-    <FormSection title="Slack notifications" padding="">
-      <svelte:fragment slot="description">
-        <span class="text-sm text-slate-600">
-          Slack has not been configured for this project. Read the <a
-            href="https://docs.rilldata.com/explore/alerts/slack"
-            target="_blank"
-          >
-            docs
-          </a> to learn more.
-        </span>
-      </svelte:fragment>
-    </FormSection>
-  {/if}
+      bind:values={$data["emailRecipients"]}
+      errors={$errors["emailRecipients"]}
+      singular="email"
+      plural="emails"
+      placeholder="Enter an email address"
+    />
+    {#if $hasSlackNotifier.data}
+      <FormSection
+        bind:enabled={$data["enableSlackNotification"]}
+        showSectionToggle
+        title="Slack notifications"
+        padding=""
+      >
+        <MultiInput
+          id="slackChannels"
+          label="Channels"
+          hint="We’ll send alerts directly to these channels."
+          bind:values={$data["slackChannels"]}
+          errors={$errors["slackChannels"]}
+          singular="channel"
+          plural="channels"
+          placeholder="# Enter a Slack channel name"
+        />
+        <MultiInput
+          id="slackUsers"
+          label="Users"
+          hint="We’ll alert them with direct messages in Slack."
+          bind:values={$data["slackUsers"]}
+          errors={$errors["slackUsers"]}
+          singular="user"
+          plural="users"
+          placeholder="Enter an email address"
+        />
+      </FormSection>
+    {:else}
+      <FormSection title="Slack notifications" padding="">
+        <svelte:fragment slot="description">
+          <span class="text-sm text-slate-600">
+            Slack has not been configured for this project. Read the <a
+              href="https://docs.rilldata.com/explore/alerts/slack"
+              target="_blank"
+            >
+              docs
+            </a> to learn more.
+          </span>
+        </svelte:fragment>
+      </FormSection>
+    {/if}
+  </div>
 </form>
