104075478774257ffc2703ddad093818848de598
Eric P Green <ericpgreen2@gmail.com>
2025-07-29 10:26:05 -0400

    Fix lint; improve comment

--- web-admin/src/routes/[organization]/[project]/+page.ts
+++ web-admin/src/routes/[organization]/[project]/+page.ts
@@ -2,12 +2,11 @@ import { featureFlags } from "@rilldata/web-common/features/feature-flags.js";
 import { redirect } from "@sveltejs/kit";
 import { get } from "svelte/store";
 
-export const load = async ({ params: { organization, project }, parent }) => {
+export const load = async ({ params: { organization, project } }) => {
+  // NOTE: in the future, we'll use user-level `ai` permissions to redirect to the chat page.
   if (get(featureFlags.chat)) {
     throw redirect(307, `/${organization}/${project}/-/chat`);
   }
 
-  // TODO: if the user has `ai` permissions, redirect to `/${organization}/${project}/-/chat`
-  // otherwise, redirect to `/${organization}/${project}/-/dashboards`
   throw redirect(307, `/${organization}/${project}/-/dashboards`);
 };
