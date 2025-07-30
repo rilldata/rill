b2b0f979761f583896e24a67606525743fd7d50e
Cyrus Goh <hello@cyrusgoh.com>
2025-07-30 13:28:28 -0700

    try

--- web-integration/tests/deploy.spec.ts
+++ web-integration/tests/deploy.spec.ts
@@ -202,14 +202,8 @@ async function login(deployPage: Page) {
 
   // Login 1st to start deploy.
 
-  // // If not logged in, we append screen_hint=signup to the login URL.
-  // // Default to signup page, click "Login" to switch to login mode
-  // const loginLink = deployPage.locator("a", { hasText: "Log in" });
-  // await loginLink.waitFor({ state: "visible", timeout: 10000 });
-  // await loginLink.click();
-
   // Fill in the email
-  const emailInput = deployPage.locator('input[name="username"]');
+  const emailInput = deployPage.locator('input[name="email"]');
   await emailInput.waitFor({ state: "visible" });
   await emailInput.click();
   await emailInput.fill(process.env.RILL_DEVTOOL_E2E_ADMIN_ACCOUNT_EMAIL);
