2f496af5a2a50a349123ebf933cbb399160c1c72
Parag Jain <pjain1@apache.org>
2025-08-01 00:43:35 +0530

    wrap every computed time dim

--- runtime/metricsview/executor_wrap_clickhouse_compare_time_dim.go
+++ runtime/metricsview/executor_wrap_clickhouse_compare_time_dim.go
@@ -16,58 +16,65 @@ func (e *Executor) wrapClickhouseCompareTimeDim(ast *AST) {
 		return
 	}
 
-	e.wrapClickhouseCompareTimeDimWalk(ast, ast.Root)
-}
+	timeDim := e.metricsView.TimeDimension
+	if ast.query.TimeRange != nil && ast.query.TimeRange.TimeDimension != "" {
+		timeDim = ast.query.TimeRange.TimeDimension
+	}
+	computedDims := make(map[string]string)
+	for _, qd := range ast.query.Dimensions {
+		if qd.Compute == nil || qd.Compute.TimeFloor == nil || !strings.EqualFold(qd.Compute.TimeFloor.Dimension, timeDim) {
+			continue
+		}
+		dim, err := ast.lookupDimension(timeDim, false)
+		if err != nil { // this should never happen
+			panic(fmt.Errorf("failed to lookup time dimension %q: %w", timeDim, err))
+		}
+		if !strings.EqualFold(dim.Name, dim.Column) {
+			continue
+		}
+		hash := crc32.ChecksumIEEE([]byte(dim.Name))
+		uniqName := fmt.Sprintf("%s_uniq_%x", dim.Name, hash)
+		computedDims[dim.Name] = uniqName
+	}
 
-func (e *Executor) wrapClickhouseCompareTimeDimWalk(a *AST, n *SelectNode) {
-	// find node with alias "comparison"
-	if n == nil {
+	if len(computedDims) == 0 {
+		// no computed time dimensions found, nothing to wrap
 		return
 	}
-	if n.Alias == "comparison" {
-		timeDim := e.metricsView.TimeDimension
-		if a.query.TimeRange != nil && a.query.TimeRange.TimeDimension != "" {
-			timeDim = a.query.TimeRange.TimeDimension
+
+	e.wrapClickhouseCompareTimeDimWalk(ast, ast.Root, computedDims)
+}
+
+func (e *Executor) wrapClickhouseCompareTimeDimWalk(a *AST, n *SelectNode, computedDims map[string]string) {
+	wrapNeeded := false
+	for _, f := range n.DimFields {
+		if _, ok := computedDims[f.Name]; ok {
+			wrapNeeded = true
+			break
 		}
-		for _, qd := range a.query.Dimensions {
-			if qd.Compute == nil || qd.Compute.TimeFloor == nil || !strings.EqualFold(qd.Compute.TimeFloor.Dimension, timeDim) {
-				continue
+	}
+
+	if wrapNeeded {
+		a.wrapSelect(n, a.generateIdentifier())
+		for i, f := range n.DimFields {
+			if uniqName, ok := computedDims[f.Name]; ok {
+				// change the name of the inner query dimension
+				n.FromSelect.DimFields[i].Name = uniqName
 			}
-			dim, err := a.lookupDimension(timeDim, false)
-			if err != nil { // this should never happen
-				panic(fmt.Errorf("failed to lookup time dimension %q: %w", timeDim, err))
-			}
-			if !strings.EqualFold(dim.Name, dim.Column) {
-				// if the underlying column name is already different from dim name that will be used in the query, we don't need to wrap
-				return
-			}
-			// append hash of time dim to the compare name to have diff alias as the column name otherwise clickhouse bypasses the date calculations
-			// calculate non crypto hash of the name which should be fast
-			hash := crc32.ChecksumIEEE([]byte(dim.Name))
-			uniqName := fmt.Sprintf("%s_comp_%x", dim.Name, hash)
-			// warp the select node in an outer select, but change the name of inner time dimension
-			a.wrapSelect(n, a.generateIdentifier())
-			for i, f := range n.FromSelect.DimFields {
-				if f.Name == dim.Name {
-					// change the name of the inner query dimension
-					n.FromSelect.DimFields[i].Name = uniqName
-					break
-				}
-			}
-			for i, f := range n.DimFields {
-				if f.Name == dim.Name {
-					// select inner dim in the outer query using uniqName but keep alias as actual query dimension name so change the expression and keep f.Name as is
-					n.DimFields[i].Expr = a.sqlForMember(n.FromSelect.Alias, uniqName)
-					break
-				}
-			}
-			return
 		}
+		for i, f := range n.DimFields {
+			if uniqName, ok := computedDims[f.Name]; ok {
+				// select inner dim in the outer query using uniqName but keep alias as actual query dimension name so change the expression and keep f.Name as is
+				n.DimFields[i].Expr = a.sqlForMember(n.FromSelect.Alias, uniqName)
+
+			}
+		}
+	}
+
+	if n.FromSelect != nil {
+		e.wrapClickhouseCompareTimeDimWalk(a, n.FromSelect, computedDims)
 	}
 	if n.JoinComparisonSelect != nil {
-		e.wrapClickhouseCompareTimeDimWalk(a, n.JoinComparisonSelect)
-	}
-	if n.FromSelect != nil {
-		e.wrapClickhouseCompareTimeDimWalk(a, n.FromSelect)
+		e.wrapClickhouseCompareTimeDimWalk(a, n.JoinComparisonSelect, computedDims)
 	}
 }
