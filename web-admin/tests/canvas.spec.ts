3898636c770513d6e131abba8a6b1232e381ec69
Dhiraj Kumar <sumankumaribarnwal@gmail.com>
2025-08-04 18:48:33 +0530

    fix nit

--- web-admin/tests/canvas.spec.ts
+++ web-admin/tests/canvas.spec.ts
@@ -5,7 +5,7 @@ test.describe("Canvases", () => {
   test("should have data", async ({ page }) => {
     await page.goto("/e2e/openrtb");
 
-    // Navigate to the explore
+    // Navigate to canvas
     await page
       .getByRole("link", { name: "Bids Canvas Dashboard" })
       .first()
