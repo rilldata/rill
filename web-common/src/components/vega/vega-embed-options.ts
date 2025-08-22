1f589bbd90a001949c7b63843551e756e58c0df7
Dhiraj Barnwal <sumankumaribarnwal@gmail.com>
2025-08-22 16:52:58 +0530

    APP-142 feat: multi metric charts for canvas, better tooltips (#7766)
    * add multi metric chart
    
    * color adjustments
    
    * multi value hover
    
    * place rule behind bars
    
    * clean up
    
    * better hover for bar charts
    
    * improve tooltip styling
    
    * Abstract multivalue tooltip channel
    
    * tooltip style updates
    
    * add multi metric chart icon
    
    * lint fix
    
    * add color sort order
    
    * use domain values for sort directly
    
    * use common tooltip for heatmap
    
    * preserve domain order in rule layer
    
    * add color symbol to hover tooltips
    
    * hide multi metric chart

--- web-common/src/components/vega/vega-embed-options.ts
+++ web-common/src/components/vega/vega-embed-options.ts
@@ -12,8 +12,10 @@ export interface CreateEmbedOptionsParams {
   height: number;
   config?: Config;
   renderer?: "canvas" | "svg";
+  theme?: "light" | "dark";
   expressionFunctions?: ExpressionFunction;
   useExpressionInterpreter?: boolean;
+  colorMapping: { value: string; color: string }[];
 }
 
 export function createEmbedOptions({
@@ -22,14 +24,22 @@ export function createEmbedOptions({
   height,
   config,
   renderer = "canvas",
+  theme = "light",
   expressionFunctions = {},
   useExpressionInterpreter = true,
+  colorMapping,
 }: CreateEmbedOptionsParams): EmbedOptions {
   const jwt = get(runtime).jwt;
 
   return {
     config: config || getRillTheme(canvasDashboard),
     renderer,
+    tooltip: {
+      theme: theme,
+      ...(colorMapping.length
+        ? { formatTooltip: getTooltipFormatter(colorMapping) }
+        : {}),
+    },
     actions: false,
     logLevel: 0, // only show errors
     width: canvasDashboard ? width : undefined,
@@ -51,5 +61,27 @@ export function createEmbedOptions({
           },
         }),
     },
+  };
+}
+
+export function getTooltipFormatter(
+  colorMapping: { value: string; color: string }[],
+) {
+  return (items: any, sanitize: (value: any) => string) => {
+    const rows = Object.entries(items)
+      .map(([key, val]) => {
+        if (val === undefined) return "";
+        const colorEntry = colorMapping.find(
+          (mapping) => mapping.value === key,
+        );
+        const keyColor = colorEntry
+          ? `<svg  class="key-color">
+            <circle cx="6" cy="6" r="6" style="fill:${colorEntry.color};"/>
+          </svg>`
+          : "";
+        return `<tr><td class="key">${keyColor}<span>${sanitize(key)}</span></td><td class="value">${sanitize(val)}</td></tr>`;
+      })
+      .join("");
+    return `<table><tbody>${rows}</tbody></table>`;
   };
 }
--- web-common/src/features/canvas/components/util.ts
+++ web-common/src/features/canvas/components/util.ts
@@ -71,6 +71,7 @@ const CHART_TYPES = [
   "pie_chart",
   "heatmap",
   "funnel_chart",
+  "multi_metric_chart",
 ] as const;
 const NON_CHART_TYPES = [
   "markdown",
