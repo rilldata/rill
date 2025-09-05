51d52716ce70a4dce895485951c349c48b7c7091
Graham Plata <graham.plata@gmail.com>
2025-09-05 17:11:20 -0400

    remove conditional connector file creation for DuckDB OLAP engine

--- runtime/parser/init.go
+++ runtime/parser/init.go
@@ -57,19 +57,6 @@ mock_users:
 		return err
 	}
 
-	// Only create connector files for non-DuckDB OLAP engines or when explicitly requested
-	// This allows DuckDB-based projects to go through the welcome page for user-guided initialization
-	if olap != "duckdb" {
-		connector := "type: connector\ndriver: duckdb"
-		if olap == "clickhouse" {
-			connector = "type: connector\ndriver: clickhouse\nmanaged: true"
-		}
-
-		err = repo.Put(ctx, fmt.Sprintf("connectors/%s.yaml", olap), strings.NewReader(connector))
-		if err != nil {
-			return err
-		}
-	}
 	return nil
 }
 
