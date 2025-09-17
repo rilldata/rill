8612cf6a08903e1fb5958daa0baeb4beef379038
Brian Holmes <briangregoryholmes@gmail.com>
2025-09-17 18:47:23 -0400

    attempt to fix test

--- web-common/src/features/dashboards/time-controls/super-pill/components/RangePicker.svelte
+++ web-common/src/features/dashboards/time-controls/super-pill/components/RangePicker.svelte
@@ -32,6 +32,7 @@
 </script>
 
 <DropdownMenu.Root
+  portal="#rill-portal"
   bind:open
   onOpenChange={(open) => {
     if (open) {
@@ -60,7 +61,11 @@
       </span>
     </button>
   </DropdownMenu.Trigger>
-  <DropdownMenu.Content align="start" {side} class="p-0 overflow-hidden">
+  <DropdownMenu.Content
+    align="start"
+    {side}
+    class="p-0 z-[100] overflow-hidden"
+  >
     <div class="flex">
       <div class="flex flex-col w-48 p-1">
         <TimeRangeMenu
