ad4c6971ce1988d8bd2a0e90e63d770bc250bc2c
Brian Holmes <120223836+briangregoryholmes@users.noreply.github.com>
2025-09-18 09:24:23 -0400

    [APP-331] fix: canvas theme loading issue (#7903)

--- web-common/src/features/canvas/CanvasDashboardWrapper.svelte
+++ web-common/src/features/canvas/CanvasDashboardWrapper.svelte
@@ -4,6 +4,8 @@
   import { getCanvasStore } from "./state-managers/state-managers";
   import { runtime } from "@rilldata/web-common/runtime-client/runtime-store";
   import { page } from "$app/stores";
+  import { updateThemeVariables } from "../themes/actions";
+
   export let maxWidth: number;
   export let clientWidth = 0;
   export let showGrabCursor = false;
@@ -11,19 +13,21 @@
   export let canvasName: string;
   export let onClick: () => void = () => {};
 
-  $: ({ instanceId } = $runtime);
-
-  $: ({
-    canvasEntity: { saveSnapshot, restoreSnapshot },
-  } = getCanvasStore(canvasName, instanceId));
-
-  let contentRect = new DOMRectReadOnly(0, 0, 0, 0);
-
-  $: ({ width: clientWidth } = contentRect);
-
   onMount(async () => {
     await restoreSnapshot();
   });
+
+  let contentRect = new DOMRectReadOnly(0, 0, 0, 0);
+
+  $: ({ instanceId } = $runtime);
+
+  $: ({
+    canvasEntity: { saveSnapshot, restoreSnapshot, themeSpec },
+  } = getCanvasStore(canvasName, instanceId));
+
+  $: ({ width: clientWidth } = contentRect);
+
+  $: updateThemeVariables($themeSpec);
 
   onDestroy(() => {
     saveSnapshot($page.url.searchParams.toString());
