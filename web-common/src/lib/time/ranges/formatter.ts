acff9d0d766b5a65302cbfb1f880bef8376b2ce4
Brian Holmes <briangregoryholmes@gmail.com>
2025-09-18 09:40:52 -0400

    fix: display ranges as inclusive

--- web-common/src/lib/time/ranges/formatter.ts
+++ web-common/src/lib/time/ranges/formatter.ts
@@ -2,13 +2,9 @@ import { V1TimeGrainToOrder } from "@rilldata/web-common/lib/time/new-grains.ts"
 import { V1TimeGrain } from "@rilldata/web-common/runtime-client";
 import { DateTime, type DateTimeFormatOptions, Interval } from "luxon";
 
-/**
- * Formats a start and end for usage in the application.
- * NOTE: this is primarily used for the time range picker. We might want to
- * colocate the code w/ the component.
- */
+// Formats a Luxon interval for human readable display throughout the application.
 export function prettyFormatTimeRange(interval: Interval, grain: V1TimeGrain) {
-  if (!interval.isValid || !interval.start || !interval.end)
+  if (!interval?.isValid || !interval.start || !interval.end)
     return "Invalid interval";
 
   const datePart = formatDatePartOfTimeRange(interval, grain);
@@ -22,6 +18,8 @@ export function prettyFormatTimeRange(interval: Interval, grain: V1TimeGrain) {
 
 const yearGrainOrder = V1TimeGrainToOrder[V1TimeGrain.TIME_GRAIN_YEAR];
 const monthGrainOrder = V1TimeGrainToOrder[V1TimeGrain.TIME_GRAIN_MONTH];
+const dayGrainOrder = V1TimeGrainToOrder[V1TimeGrain.TIME_GRAIN_DAY];
+
 function formatDatePartOfTimeRange(interval: Interval, grain: V1TimeGrain) {
   if (!interval.start?.isValid || !interval.end?.isValid) return ""; // type safety
 
@@ -41,7 +39,14 @@ function formatDatePartOfTimeRange(interval: Interval, grain: V1TimeGrain) {
 
   if (showDay) format.day = "numeric";
 
-  return interval.toLocaleString(format);
+  const displayAsInclusiveEnd = grainOrder >= dayGrainOrder;
+
+  return displayAsInclusiveEnd
+    ? Interval.fromDateTimes(
+        interval.start,
+        interval.end.minus({ millisecond: 1 }),
+      ).toLocaleString(format)
+    : interval.toLocaleString(format);
 }
 
 const hourGrainOrder = V1TimeGrainToOrder[V1TimeGrain.TIME_GRAIN_HOUR];
