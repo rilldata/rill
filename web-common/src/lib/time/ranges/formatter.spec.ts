d6e9b09bf7a73288d1394368c528c3df9ccfdb27
Brian Holmes <120223836+briangregoryholmes@users.noreply.github.com>
2025-09-19 00:48:12 -0400

    fix: add timezone specific format tests (#7986)

--- web-common/src/lib/time/ranges/formatter.spec.ts
+++ web-common/src/lib/time/ranges/formatter.spec.ts
@@ -274,4 +274,99 @@ describe("prettyFormatTimeRange", () => {
       });
     });
   });
+
+  describe("Handles time zones correctly", () => {
+    const America_New_York = [
+      {
+        test: "Non-zero minute, minute grain",
+        time: "2025-09-04T16:30:30.000Z",
+        grain: V1TimeGrain.TIME_GRAIN_MINUTE,
+        formattedTime: "Sep 4, 2025 (12:30:30PM)",
+      },
+      {
+        test: "Same month, days difference, same time at midnight",
+        start: "2025-09-02T04:00:00.000Z",
+        end: "2025-09-06T04:00:00.000Z",
+        grain: V1TimeGrain.TIME_GRAIN_DAY,
+        formattedTime: "Sep 2 – 5, 2025",
+      },
+      {
+        test: "Full 2024, year grain",
+        start: "2024-01-01T05:00:00.000Z",
+        end: "2025-01-01T05:00:00.000Z",
+        grain: V1TimeGrain.TIME_GRAIN_YEAR,
+        formattedTime: "2024",
+      },
+      {
+        test: "Two non-zero dates in America/New_York, second grain",
+        start: "2025-09-18T11:12:03.605-04:00",
+        end: "2025-09-20T14:24:04.485-04:00",
+        grain: V1TimeGrain.TIME_GRAIN_SECOND,
+        formattedTime: "Sep 18 – 20, 2025 (11:12:03AM-2:24:04PM)",
+      },
+      {
+        test: "Two non-zero dates, second grain",
+        start: "2025-09-18T15:12:03.605Z",
+        end: "2025-09-20T18:24:04.485Z",
+        grain: V1TimeGrain.TIME_GRAIN_SECOND,
+        formattedTime: "Sep 18 – 20, 2025 (11:12:03AM-2:24:04PM)",
+      },
+    ];
+
+    const Asia_Kathmandu = [
+      {
+        test: "Point in time at midnight, minute grain",
+        time: "2025-09-04T18:15:00.000Z",
+        grain: V1TimeGrain.TIME_GRAIN_MINUTE,
+        formattedTime: "Sep 5, 2025 (12:00AM)",
+      },
+      {
+        test: "Same month, days difference, same time not at midnight",
+        start: "2025-09-01T10:15:00.000Z",
+        end: "2025-09-04T10:15:00.000Z",
+        grain: V1TimeGrain.TIME_GRAIN_HOUR,
+        formattedTime: "Sep 1 – 4, 2025 (4PM)",
+      },
+      {
+        test: "Full 2023, year grain",
+        start: "2022-12-31T18:15:00.000Z",
+        end: "2023-12-31T18:15:00.000Z",
+        grain: V1TimeGrain.TIME_GRAIN_YEAR,
+        formattedTime: "2023",
+      },
+      {
+        test: "Two non-zero dates in Asia/Kathmandu, minute grain",
+        start: "2021-08-27T02:30:00.000+05:45",
+        end: "2023-08-29T13:30:00.000+05:45",
+        grain: V1TimeGrain.TIME_GRAIN_MINUTE,
+        formattedTime: "Aug 27, 2021 – Aug 29, 2023 (2:30AM-1:30PM)",
+      },
+    ];
+
+    America_New_York.forEach(
+      ({ test, start, end, time, grain, formattedTime }) => {
+        it(test + " in America/New_York", () => {
+          const interval = Interval.fromDateTimes(
+            DateTime.fromISO(start ?? time).setZone("America/New_York"),
+            DateTime.fromISO(end ?? time).setZone("America/New_York"),
+          );
+          const actualFormattedTime = prettyFormatTimeRange(interval, grain);
+          expect(actualFormattedTime).toEqual(formattedTime);
+        });
+      },
+    );
+
+    Asia_Kathmandu.forEach(
+      ({ test, time, start, end, grain, formattedTime }) => {
+        it(test + " in Asia/Kathmandu", () => {
+          const interval = Interval.fromDateTimes(
+            DateTime.fromISO(start ?? time).setZone("Asia/Kathmandu"),
+            DateTime.fromISO(end ?? time).setZone("Asia/Kathmandu"),
+          );
+          const actualFormattedTime = prettyFormatTimeRange(interval, grain);
+          expect(actualFormattedTime).toEqual(formattedTime);
+        });
+      },
+    );
+  });
 });
