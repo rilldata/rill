939b64360a72451944b22eb483d831cf958b37c7
Graham Plata <graham.plata@gmail.com>
2025-09-30 14:59:58 -0400

    client

--- web-admin/src/client/index.ts
+++ web-admin/src/client/index.ts
@@ -2,4 +2,3 @@ export * from "./gen/admin-service/admin-service";
 export * from "./gen/index.schemas";
 export * from "./gen/aiservice/aiservice";
 export * from "./gen/telemetry-service/telemetry-service";
-export * from "./gen/public/public";
