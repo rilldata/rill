0e31a648610a7903d3e4ee726ad169ac4b31fd58
Cyrus Goh <hello@cyrusgoh.com>
2025-10-01 11:48:27 -0700

    3 cases of default olap connector

--- web-local/tests/rill-yaml.spec.ts
+++ web-local/tests/rill-yaml.spec.ts
@@ -1,21 +1,28 @@
-import { expect } from "@playwright/test";
+import { expect, type Page } from "@playwright/test";
 import { uploadFile } from "./utils/sourceHelpers";
 import { test } from "./setup/base";
 
+async function expectRillYAMLToContain(page: Page, text: string) {
+  const rillYamlEditor = page
+    .getByLabel("codemirror editor")
+    .getByRole("textbox");
+  await expect(rillYamlEditor).toContainText(`olap_connector: ${text}`);
+}
+
 test.describe("Default olap_connector behavior", () => {
-  test("Start with an empty project", async ({ page }) => {
+  test("Should set default olap_connector to duckdb for empty project", async ({
+    page,
+  }) => {
     await page.getByRole("link", { name: "Empty Project" }).click();
     await expect(page.getByText("Getting started")).toBeVisible();
 
     await page.getByRole("link", { name: "rill.yaml" }).click();
-    const rillYamlEditor = page
-      .getByLabel("codemirror editor")
-      .getByRole("textbox");
-
-    await expect(rillYamlEditor).toContainText("olap_connector: duckdb");
+    await expectRillYAMLToContain(page, "duckdb");
   });
 
-  test("Start with a new project with a local file", async ({ page }) => {
+  test("Should set default olap_connector to duckdb for local file upload", async ({
+    page,
+  }) => {
     await page.getByRole("link", { name: "Empty Project" }).click();
     await expect(page.getByText("Getting started")).toBeVisible();
 
@@ -24,10 +31,30 @@ test.describe("Default olap_connector behavior", () => {
     await page.getByText("View this source").click();
 
     await page.getByRole("link", { name: "rill.yaml" }).click();
-    const rillYamlEditor = page
-      .getByLabel("codemirror editor")
-      .getByRole("textbox");
+    await expectRillYAMLToContain(page, "duckdb");
+  });
 
-    await expect(rillYamlEditor).toContainText("olap_connector: duckdb");
+  test("Should set default olap_connector to clickhouse for Rill-managed ClickHouse", async ({
+    page,
+  }) => {
+    await page.getByRole("link", { name: "Empty Project" }).click();
+    await expect(page.getByText("Getting started")).toBeVisible();
+
+    await page.getByRole("button", { name: "Add Data" }).click();
+    await page.locator("#clickhouse").click();
+    await page.locator("#managed").selectOption("rill-managed");
+    await page
+      .getByRole("dialog", { name: "ClickHouse" })
+      .getByRole("button", {
+        name: "Connect",
+        exact: true,
+      })
+      .click();
+
+    // Wait for navigation to the connector file
+    await page.waitForURL(`**/files/connectors/clickhouse.yaml`);
+
+    await page.getByRole("link", { name: "rill.yaml" }).click();
+    await expectRillYAMLToContain(page, "clickhouse");
   });
 });
