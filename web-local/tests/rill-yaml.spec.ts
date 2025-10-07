e32672c0845b710987ef1759f6f7c5b87f597574
Cyrus Goh <hello@cyrusgoh.com>
2025-10-07 16:41:45 -0700

    remove skip, add wait for file nav entry

--- web-local/tests/rill-yaml.spec.ts
+++ web-local/tests/rill-yaml.spec.ts
@@ -22,7 +22,7 @@ async function expectRillYAMLToContainOlapConnector(page: Page, text: string) {
 }
 
 test.describe("Default olap_connector behavior", () => {
-  test.skip("Should set default olap_connector to duckdb for empty project", async ({
+  test("Should set default olap_connector to duckdb for empty project", async ({
     page,
   }) => {
     await page.getByRole("link", { name: "Empty Project" }).click();
@@ -34,13 +34,16 @@ test.describe("Default olap_connector behavior", () => {
     await expectRillYAMLToContainOlapConnector(page, "duckdb");
   });
 
-  test.skip("Should set default olap_connector to duckdb for local file upload", async ({
+  test("Should set default olap_connector to duckdb for local file upload", async ({
     page,
   }) => {
     await page.getByRole("link", { name: "Empty Project" }).click();
     await expect(page.getByText("Getting started")).toBeVisible();
 
     await uploadFile(page, "AdBids.csv");
+
+    // Wait for the source file to be created in the file nav
+    await waitForFileNavEntry(page, "/sources/AdBids.yaml", false);
 
     await page.getByText("View this source").click();
 
