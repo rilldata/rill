a9ca1fd82119d2e40d7e7014772bb00599c6db71
Aditya Hegde <adityahegderocks@gmail.com>
2025-10-24 19:00:49 +0530

    Remove schema from new code

--- web-admin/src/features/bookmarks/explore-bookmark-legacy-data-transformer.ts
+++ web-admin/src/features/bookmarks/explore-bookmark-legacy-data-transformer.ts
@@ -1,15 +1,11 @@
 import { getDashboardStateFromUrl } from "@rilldata/web-common/features/dashboards/proto-state/fromProto.ts";
-import {
-  getMetricsViewSchemaOptions,
-  getMetricsViewTimeRangeFromExploreQueryOptions,
-} from "@rilldata/web-common/features/dashboards/selectors.ts";
+import { getMetricsViewTimeRangeFromExploreQueryOptions } from "@rilldata/web-common/features/dashboards/selectors.ts";
 import { getTimeControlState } from "@rilldata/web-common/features/dashboards/time-controls/time-control-store.ts";
 import { convertPartialExploreStateToUrlParams } from "@rilldata/web-common/features/dashboards/url-state/convert-partial-explore-state-to-url-params.ts";
 import { getExploreValidSpecQueryOptions } from "@rilldata/web-common/features/explores/selectors.ts";
 import type {
   V1ExploreSpec,
   V1MetricsViewSpec,
-  V1StructType,
   V1TimeRangeSummary,
 } from "@rilldata/web-common/runtime-client";
 import { createQuery } from "@tanstack/svelte-query";
@@ -24,17 +20,13 @@ export function createExploreBookmarkLegacyDataTransformer(
   const timeRangeQuery = createQuery(
     getMetricsViewTimeRangeFromExploreQueryOptions(exploreNameStore),
   );
-  const schemaQuery = createQuery(
-    getMetricsViewSchemaOptions(exploreNameStore),
-  );
 
   return derived(
-    [validSpecQuery, timeRangeQuery, schemaQuery],
-    ([validSpecResp, timeRangeResp, schemaResp]) => {
+    [validSpecQuery, timeRangeQuery],
+    ([validSpecResp, timeRangeResp]) => {
       const metricsViewSpec = validSpecResp.data?.metricsViewSpec ?? {};
       const exploreSpec = validSpecResp.data?.exploreSpec ?? {};
       const timeRangeSummary = timeRangeResp.data?.timeRangeSummary;
-      const schema = schemaResp.data?.schema ?? {};
 
       return (data: string) =>
         exploreBookmarkDataTransformer({
@@ -42,7 +34,6 @@ export function createExploreBookmarkLegacyDataTransformer(
           metricsViewSpec,
           exploreSpec,
           timeRangeSummary,
-          schema,
         });
     },
   );
@@ -53,19 +44,16 @@ export function exploreBookmarkDataTransformer({
   metricsViewSpec,
   exploreSpec,
   timeRangeSummary,
-  schema,
 }: {
   data: string;
   metricsViewSpec: V1MetricsViewSpec;
   exploreSpec: V1ExploreSpec;
   timeRangeSummary: V1TimeRangeSummary | undefined;
-  schema: V1StructType;
 }) {
   const exploreStateFromBookmark = getDashboardStateFromUrl(
     data,
     metricsViewSpec,
     exploreSpec,
-    schema,
   );
 
   // We need to check if the bookmark's url is equal to current url or not to show an "active" state.
