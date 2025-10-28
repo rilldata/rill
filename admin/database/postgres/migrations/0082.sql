e4fcb3f7440d285f2fc47933080b9c7a7ada8c10
Kasper Sjørslev <kaspersjo@gmail.com>
2025-10-28 21:09:39 +0100

    Fix typo in migration

--- admin/database/postgres/migrations/0082.sql
+++ admin/database/postgres/migrations/0082.sql
@@ -3,4 +3,4 @@ DROP CONSTRAINT deployments_project_id_fkey;
 
 ALTER TABLE deployments
 ALTER COLUMN project_id DROP NOT NULL,
-ADD CONSTRAINT deployments_project_id_fkey FOREIGN KEY (project_id) REFERENCES orgs(id) ON DELETE SET NULL;
+ADD CONSTRAINT deployments_project_id_fkey FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE SET NULL;
