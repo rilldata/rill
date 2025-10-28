aa9662924b6a9488abf5603147694502f89439f9
Kasper Sjørslev <kaspersjo@gmail.com>
2025-10-28 20:58:12 +0100

    Remove not null constraint on deployment project id

---
+++ admin/database/postgres/migrations/0082.sql
@@ -0,0 +1,6 @@
+ALTER TABLE deployments
+DROP CONSTRAINT deployments_project_id_fkey;
+
+ALTER TABLE deployments
+ALTER COLUMN project_id DROP NOT NULL,
+ADD CONSTRAINT deployments_project_id_fkey FOREIGN KEY (project_id) REFERENCES orgs(id) ON DELETE SET NULL;
