93e5179078292e5a493a814208889084cd6fe7f8
Eric P Green <ericpgreen2@gmail.com>
2025-10-27 21:09:35 -0700

    design (Cloud UI): Improve resource listings (#8160)
    * feat (Cloud UI): Create new project homepage
    
    * Second iteration
    
    * Fix tests and lint
    
    * "Welcome back" -> "Weclome"
    
    * Account for disabled `chat` feature flag on homepage
    
    * Make the chat input element taller on the project homepage
    
    * Tighten spacing
    
    * design (Cloud UI): Update resource listing tables
    
    * Update report & alert history tables
    
    * Code cleanup
    
    * Fix lint
    
    * Fix lint x2
    
    * Remove divider
    
    * Remove count of resources
    
    * Better empty states
    
    * Self-review
    
    * Bump spacing

---
+++ web-admin/src/features/resources/ResourceListEmptyState.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import type { ComponentType } from "svelte";
+
+  export let icon: ComponentType;
+  export let iconColor: string;
+  export let message: string;
+  export let action: string = "";
+</script>
+
+<div class="flex flex-col items-center gap-y-4">
+  <svelte:component this={icon} size="32px" color={iconColor} />
+  <div class="flex flex-col gap-y-2 items-center text-sm">
+    <div class="text-gray-600 font-semibold">
+      {message}
+    </div>
+    {#if $$slots.action}
+      <div class="text-gray-500">
+        <slot name="action" />
+      </div>
+    {:else if action}
+      <div class="text-gray-500">
+        {action}
+      </div>
+    {/if}
+  </div>
+</div>
