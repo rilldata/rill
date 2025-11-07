d76aa9593e6705f5825bc8bc0dd7b1623f66b994
Brian Holmes <briangregoryholmes@gmail.com>
2025-11-06 21:34:43 -0500

    test fix

--- web-admin/src/routes/[organization]/[project]/canvas/[dashboard]/+page.ts
+++ web-admin/src/routes/[organization]/[project]/canvas/[dashboard]/+page.ts
@@ -1,11 +1,12 @@
 import { lastVisitedState } from "@rilldata/web-common/features/canvas/stores/canvas-entity";
 import { redirect } from "@sveltejs/kit";
 
-export const load = async ({ url, params }) => {
+export const load = async ({ url: { searchParams }, params }) => {
+  const urlSearchParams = searchParams.toString();
   const canvasName = params.dashboard;
   const snapshotSearchParams = lastVisitedState.get(canvasName);
 
-  if (snapshotSearchParams && !url.search.toString()) {
+  if (snapshotSearchParams && !urlSearchParams) {
     throw redirect(307, `?${snapshotSearchParams}`);
   }
 };
