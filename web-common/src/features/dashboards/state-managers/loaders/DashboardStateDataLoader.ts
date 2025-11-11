2456173c16b6232f6ec5758ab6f0fcb63e1e330a
Brian Holmes <briangregoryholmes@gmail.com>
2025-11-11 11:35:54 -0500

    remove logs

--- web-common/src/features/dashboards/state-managers/loaders/DashboardStateDataLoader.ts
+++ web-common/src/features/dashboards/state-managers/loaders/DashboardStateDataLoader.ts
@@ -325,15 +325,6 @@ export class DashboardStateDataLoader {
       exploreSpec,
     );
 
-    console.log({
-      exploreStateFromSessionStorage,
-      partialExploreStateFromUrl,
-      mostRecentPartialExploreState,
-      bookmarkOrTokenExploreState,
-      exploreStateFromYAMLConfig,
-      rillDefaultExploreState,
-    });
-
     const shouldSkipOtherSources =
       // If the url has some params that do not map to session storage then we need to only use state from url back-filled with rill defaults.
       (urlSearchParams.size > 0 && !exploreStateFromSessionStorage) ||
@@ -366,8 +357,6 @@ export class DashboardStateDataLoader {
     const finalExploreState = cascadingExploreStateMerge(
       nonEmptyExploreStateOrder,
     ) as ExploreState;
-
-    console.log({ finalExploreState });
 
     return finalExploreState;
   }
