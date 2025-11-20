f14e59efa36d7414a288023e0eab8d0c296ff5f5
Parag Jain <pjain1@apache.org>
2025-11-20 18:56:08 +0530

    add pie_chart to canvas component

--- runtime/reconcilers/canvas.go
+++ runtime/reconcilers/canvas.go
@@ -481,7 +481,7 @@ func populateRendererRefs(res *rendererRefs, renderer string, rendererProps map[
 				return err
 			}
 		}
-	case "donut_chart":
+	case "donut_chart", "pie_chart":
 		if colorField, ok := pathutil.GetPath(rendererProps, "color.field"); ok {
 			err = res.metricsViewField(mv, colorField)
 			if err != nil {
