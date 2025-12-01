16e86ab30ccd11b3a06e279bc566e25a8291b26e
Anshul Khandelwal <12948312+k-anshul@users.noreply.github.com>
2025-12-01 16:13:06 +0530

    feat: Custom branch and editable flags for dev deployments

--- admin/projects.go
+++ admin/projects.go
@@ -325,6 +325,14 @@ func (s *Service) UpdateOrgDeploymentAnnotations(ctx context.Context, org *datab
 // RedeployProject de-provisions and re-provisions a project's deployment.
 // If prevDepl is nil, it provisions a new prod deployment based on prodBranch.
 func (s *Service) RedeployProject(ctx context.Context, proj *database.Project, prevDepl *database.Deployment) (*database.Project, error) {
+	// Delete old prod deployment if exists
+	if prevDepl != nil {
+		err := s.TeardownDeployment(ctx, prevDepl)
+		if err != nil {
+			s.Logger.Error("trigger redeploy: could not teardown old deployment", zap.String("deployment_id", prevDepl.ID), zap.Error(err), observability.ZapCtx(ctx))
+		}
+	}
+
 	// Provision new deployment
 	var branch, environment string
 	var editable bool
@@ -384,6 +392,7 @@ func (s *Service) RedeployProject(ctx context.Context, proj *database.Project, p
 		err2 := s.TeardownDeployment(ctx, newDepl)
 		return nil, multierr.Combine(err, err2)
 	}
+<<<<<<< HEAD
 	// Delete old prod deployment if exists
 	if prevDepl != nil {
 		err := s.TeardownDeployment(ctx, prevDepl)
@@ -391,6 +400,8 @@ func (s *Service) RedeployProject(ctx context.Context, proj *database.Project, p
 			s.Logger.Error("trigger redeploy: could not teardown old deployment", zap.String("deployment_id", prevDepl.ID), zap.Error(err), observability.ZapCtx(ctx))
 		}
 	}
+=======
+>>>>>>> cf3774736 (feat: Custom branch and editable flags for dev deployments)
 
 	return proj, nil
 }
