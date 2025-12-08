e80104452de35155ccdbafe5853f2eb0a955ce25
NamanMahor <naman.mahor@rilldata.com>
2025-12-08 10:45:33 +0530

    partial changes

--- runtime/pkg/blob/bucket.go
+++ runtime/pkg/blob/bucket.go
@@ -45,7 +45,14 @@ func (b *Bucket) Underlying() *blob.Bucket {
 // ListObjectsForGlob lists objects in the bucket that match the given glob pattern.
 // The glob pattern should be a valid path *without* scheme or bucket name.
 // E.g. to list gs://my-bucket/path/to/files/*, the glob pattern should be "path/to/files/*".
-func (b *Bucket) ListObjectsForGlob(ctx context.Context, glob string) ([]drivers.ObjectStoreEntry, error) {
+func (b *Bucket) ListObjectsForGlob(ctx context.Context, glob string, pageSize uint32, pageToken string) ([]drivers.ObjectStoreEntry, string, error) {
+	validPageSize := pagination.ValidPageSize(pageSize, drivers.DefaultPageSize)
+	var startAfter string
+	if pageToken != "" {
+		if err := pagination.UnmarshalPageToken(pageToken, &startAfter); err != nil {
+			return nil, "", fmt.Errorf("invalid page token: %w", err)
+		}
+	}
 	// If it's not a glob, we're pulling a single file.
 	// TODO: Should we add support for listing out directories without ** at the end?
 	if !fileutil.IsGlob(glob) {
