2069efa579ccf17d5419d160bf76fd041ea46d82
Brian Holmes <briangregoryholmes@gmail.com>
2025-12-12 22:29:23 -0500

    fix types and test

--- web-common/src/features/canvas/StaticCanvasRow.svelte
+++ web-common/src/features/canvas/StaticCanvasRow.svelte
@@ -4,14 +4,14 @@
   import ItemWrapper from "./ItemWrapper.svelte";
   import RowWrapper from "./RowWrapper.svelte";
   import { normalizeSizeArray } from "./layout-util";
-  import type { CanvasEntity } from "./stores/canvas-entity";
   import type { Row } from "./stores/row";
+  import type { BaseCanvasComponent } from "./components/BaseCanvasComponent";
 
   export let row: Row;
   export let zIndex = 1;
   export let maxWidth: number;
   export let rowIndex: number;
-  export let components: CanvasEntity["componentsStore"];
+  export let components: Map<string, BaseCanvasComponent>;
   export let heightUnit: string = "px";
   export let navigationEnabled: boolean = true;
 
@@ -33,7 +33,7 @@
   gridTemplate={widths.map((w) => `${w}fr`).join(" ")}
 >
   {#each itemIds as id, columnIndex (columnIndex)}
-    {@const component = $components.get(id)}
+    {@const component = components.get(id)}
     <ItemWrapper type={component?.type} zIndex={4 - columnIndex}>
       {#if component}
         <CanvasComponent {component} {navigationEnabled} />
