5988ae05a672e9be7d82d8ecfc7208fb23a5240c
Brian Holmes <briangregoryholmes@gmail.com>
2025-12-15 10:31:12 -0500

    remove log

--- web-common/src/features/canvas/stores/canvas-entity.ts
+++ web-common/src/features/canvas/stores/canvas-entity.ts
@@ -108,7 +108,6 @@ export class CanvasEntity {
     public instanceId: string,
     private spec: CanvasResponse,
   ) {
-    console.log({ spec });
     this.specStore = useCanvas(instanceId, name, {}, queryClient);
 
     // This will be deprecated soon - bgh
