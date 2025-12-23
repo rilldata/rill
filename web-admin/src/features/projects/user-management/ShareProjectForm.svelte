0ec93e3dabb293802aaf9b953a2989c21834e51c
Cyrus Goh <hello@cyrusgoh.com>
2025-12-23 09:58:56 +0800

    Revert "test page size"
    This reverts commit f83bd4f99459468f9557706c663b7c91924f2864.

--- web-admin/src/features/projects/user-management/ShareProjectForm.svelte
+++ web-admin/src/features/projects/user-management/ShareProjectForm.svelte
@@ -46,8 +46,8 @@
   let isHovered = false;
 
   const currentUser = createAdminServiceGetCurrentUser();
-  const PAGE_SIZE = 10;
 
+  const PAGE_SIZE = 50;
   $: orgInvitesInfiniteQuery =
     createAdminServiceListOrganizationInvitesInfinite(
       organization,
