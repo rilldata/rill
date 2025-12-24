d8e011578d1e25a6ff86fe43f41e4a9a649209ce
Brian Holmes <briangregoryholmes@gmail.com>
2025-12-23 22:40:10 -0500

    type fix

--- web-local/src/routes/(viz)/-/open-query/+page.ts
+++ web-local/src/routes/(viz)/-/open-query/+page.ts
@@ -1,6 +1,10 @@
 import { openQuery } from "@rilldata/web-common/features/explore-mappers/open-query";
+import { runtime } from "@rilldata/web-common/runtime-client/runtime-store";
+import { get } from "svelte/store";
 
 export async function load({ url }) {
+  const instanceId = get(runtime).instanceId;
+
   // Open the query (this'll redirect to the relevant Explore page)
-  await openQuery({ url });
+  await openQuery({ url, instanceId });
 }
