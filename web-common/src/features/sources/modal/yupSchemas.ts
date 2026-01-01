eeccc055acbf23a5aea64b522da822a47dd4d442
Cyrus Goh <hello@cyrusgoh.com>
2025-12-31 17:32:55 -0800

    clean up

--- web-common/src/features/sources/modal/yupSchemas.ts
+++ web-common/src/features/sources/modal/yupSchemas.ts
@@ -1,20 +1,6 @@
 import * as yup from "yup";
 
-export const getYupSchema = {
-  clickhouse: yup.object().shape({
-    dsn: yup.string().optional(),
-    managed: yup.boolean(),
-    host: yup.string(),
-    port: yup
-      .string() // Purposefully using a string input, not a numeric input
-      .matches(/^\d+$/, "Port must be a number"),
-    username: yup.string(),
-    password: yup.string(),
-    cluster: yup.string(),
-    ssl: yup.boolean(),
-    name: yup.string(), // Required for typing
-  }),
-};
+export const getYupSchema: Record<string, yup.AnySchema> = {};
 
 export const dsnSchema = yup.object().shape({
   dsn: yup.string().required("DSN is required"),
