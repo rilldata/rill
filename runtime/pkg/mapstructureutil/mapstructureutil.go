0022455507583fd55900eda43a72cce6f754f3a4
Parag Jain <pjain1@apache.org>
2026-01-08 18:23:51 +0530

    use custom decode hook for time range

--- runtime/pkg/mapstructureutil/mapstructureutil.go
+++ runtime/pkg/mapstructureutil/mapstructureutil.go
@@ -1,15 +1,17 @@
 package mapstructureutil
 
 import (
+	"reflect"
 	"time"
 
 	"github.com/mitchellh/mapstructure"
+	"github.com/rilldata/rill/runtime/metricsview"
 )
 
 // WeakDecode is similar to mapstructure.WeakDecode, but it also supports decoding RFC3339Nano-formatted timestamp strings to time.Time.
 func WeakDecode(input, output any) error {
 	config := &mapstructure.DecoderConfig{
-		DecodeHook:       mapstructure.StringToTimeHookFunc(time.RFC3339Nano),
+		DecodeHook:       mapstructure.ComposeDecodeHookFunc(mapstructure.StringToTimeHookFunc(time.RFC3339Nano), timeRangeDecodeHook),
 		Metadata:         nil,
 		Result:           output,
 		WeaklyTypedInput: true,
@@ -21,4 +23,23 @@ func WeakDecode(input, output any) error {
 	}
 
 	return decoder.Decode(input)
+}
+
+func timeRangeDecodeHook(from, to reflect.Type, data any) (any, error) {
+	if from == reflect.TypeOf(&metricsview.TimeRange{}) {
+		tr := data.(*metricsview.TimeRange)
+
+		// first decode to map normally, this will not handle time fields correctly
+		trMap := map[string]any{}
+		err := mapstructure.Decode(tr, &trMap)
+		if err != nil {
+			return nil, err
+		}
+
+		// now set the time fields correctly
+		trMap["start"] = tr.Start.Format(time.RFC3339Nano)
+		trMap["end"] = tr.End.Format(time.RFC3339Nano)
+		return trMap, nil
+	}
+	return data, nil
 }
