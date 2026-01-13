071ef30c3f8d78e49799980b6453e61815b3205b
Cyrus Goh <hello@cyrusgoh.com>
2026-01-13 11:24:59 -0800

    add connector schemas, duh

--- web-common/src/features/sources/modal/connector-schemas.ts
+++ web-common/src/features/sources/modal/connector-schemas.ts
@@ -1,9 +1,39 @@
 import type { MultiStepFormSchema } from "../../templates/schemas/types";
+import { athenaSchema } from "../../templates/schemas/athena";
 import { azureSchema } from "../../templates/schemas/azure";
+import { bigquerySchema } from "../../templates/schemas/bigquery";
+import { clickhouseSchema } from "../../templates/schemas/clickhouse";
 import { gcsSchema } from "../../templates/schemas/gcs";
+import { mysqlSchema } from "../../templates/schemas/mysql";
+import { postgresSchema } from "../../templates/schemas/postgres";
+import { redshiftSchema } from "../../templates/schemas/redshift";
+import { salesforceSchema } from "../../templates/schemas/salesforce";
+import { snowflakeSchema } from "../../templates/schemas/snowflake";
+import { sqliteSchema } from "../../templates/schemas/sqlite";
+import { localFileSchema } from "../../templates/schemas/local_file";
+import { duckdbSchema } from "../../templates/schemas/duckdb";
+import { httpsSchema } from "../../templates/schemas/https";
+import { motherduckSchema } from "../../templates/schemas/motherduck";
+import { druidSchema } from "../../templates/schemas/druid";
+import { pinotSchema } from "../../templates/schemas/pinot";
 import { s3Schema } from "../../templates/schemas/s3";
 
 export const multiStepFormSchemas: Record<string, MultiStepFormSchema> = {
+  athena: athenaSchema,
+  bigquery: bigquerySchema,
+  clickhouse: clickhouseSchema,
+  mysql: mysqlSchema,
+  postgres: postgresSchema,
+  redshift: redshiftSchema,
+  salesforce: salesforceSchema,
+  snowflake: snowflakeSchema,
+  sqlite: sqliteSchema,
+  motherduck: motherduckSchema,
+  duckdb: duckdbSchema,
+  druid: druidSchema,
+  pinot: pinotSchema,
+  local_file: localFileSchema,
+  https: httpsSchema,
   s3: s3Schema,
   gcs: gcsSchema,
   azure: azureSchema,
