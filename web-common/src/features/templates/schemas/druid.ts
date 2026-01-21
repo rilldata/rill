a1b5cd436cb74487ab445f44e924072b417d66bf
Cyrus Goh <hello@cyrusgoh.com>
2026-01-20 16:12:27 -0800

    remove olap connectors from explorer step

--- web-common/src/features/templates/schemas/druid.ts
+++ web-common/src/features/templates/schemas/druid.ts
@@ -59,24 +59,8 @@ export const druidSchema: MultiStepFormSchema = {
       description: "Use SSL for the connection",
       default: true,
     },
-    sql: {
-      type: "string",
-      title: "SQL",
-      description: "SQL query to run against your database",
-      "x-display": "textarea",
-      "x-placeholder": "SELECT * FROM my_table",
-      "x-step": "explorer",
-    },
-    name: {
-      type: "string",
-      title: "Model name",
-      description: "Name for the source model",
-      pattern: "^[a-zA-Z0-9_]+$",
-      "x-placeholder": "my_model",
-      "x-step": "explorer",
-    },
   },
-  required: ["sql", "name"],
+  required: [],
   oneOf: [
     {
       title: "Use connection string",
