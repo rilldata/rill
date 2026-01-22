7d3ebd5a87d620772f026b1c85c0f8204cb224ad
Brian Holmes <briangregoryholmes@gmail.com>
2026-01-22 17:46:26 -0500

    update syntax element

--- web-common/src/features/dashboards/time-controls/super-pill/components/SyntaxElement.svelte
+++ web-common/src/features/dashboards/time-controls/super-pill/components/SyntaxElement.svelte
@@ -15,12 +15,12 @@
 
 <style lang="postcss">
   .element {
-    @apply bg-surface-muted text-fg-secondary rounded-[2px] px-1 line-clamp-1 truncate flex-none h-5 flex items-center select-none cursor-default;
+    @apply bg-surface-elevated text-fg-secondary rounded-[2px] px-1 line-clamp-1 truncate flex-none h-5 flex items-center select-none cursor-default;
     font-family: "Source Code Variable", monospace;
     @apply font-medium w-fit;
   }
 
   .element:hover {
-    @apply bg-gray-700;
+    @apply bg-surface-container-hover;
   }
 </style>
