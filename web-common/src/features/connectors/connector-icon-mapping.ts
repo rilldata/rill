a868dcc56a2d8f1f930f8fc33267baa3dcde4780
Cyrus Goh <hello@cyrusgoh.com>
2026-01-29 13:51:11 -0800

    salesforce icon

--- web-common/src/features/connectors/connector-icon-mapping.ts
+++ web-common/src/features/connectors/connector-icon-mapping.ts
@@ -11,6 +11,7 @@ import MySqlIcon from "../../components/icons/connectors/MySqlIcon.svelte";
 import MotherDuckIcon from "../../components/icons/connectors/MotherDuckIcon.svelte";
 import RedshiftIcon from "../../components/icons/connectors/RedshiftIcon.svelte";
 import SnowflakeIcon from "../../components/icons/connectors/SnowflakeIcon.svelte";
+import SalesforceIcon from "../../components/icons/connectors/SalesforceIcon.svelte";
 import StarRocksIcon from "../../components/icons/connectors/StarRocksIcon.svelte";
 
 export const connectorIconMapping = {
@@ -26,6 +27,7 @@ export const connectorIconMapping = {
   postgres: PostgresIcon,
   redshift: RedshiftIcon,
   s3: AmazonS3Icon,
+  salesforce: SalesforceIcon,
   snowflake: SnowflakeIcon,
   starrocks: StarRocksIcon,
 };
