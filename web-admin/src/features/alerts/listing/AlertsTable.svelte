ea75cc71729590581b9c3fbb23155eab2cfa53ee
Brian Holmes <120223836+briangregoryholmes@users.noreply.github.com>
2026-01-29 13:58:13 -0500

    chore: timeseries e2e test coverage (#8675)
    * chore: timeseries e2e test
    
    * change comment
    
    * remove unused code
    
    * add value validation
    
    * remove unused code
    
    * prettier

--- web-admin/src/features/alerts/listing/AlertsTable.svelte
+++ web-admin/src/features/alerts/listing/AlertsTable.svelte
@@ -2,7 +2,7 @@
   import ResourceList from "@rilldata/web-admin/features/resources/ResourceList.svelte";
   import ResourceListEmptyState from "@rilldata/web-admin/features/resources/ResourceListEmptyState.svelte";
   import AlertIcon from "@rilldata/web-common/components/icons/AlertIcon.svelte";
-  import type { V1Resource } from "@rilldata/web-common/runtime-client";
+  import type { V1Resource } from "@rilldata/web-common/runtime-client/gen/index.schemas";
   import { flexRender, type ColumnDef } from "@tanstack/svelte-table";
   import AlertsTableCompositeCell from "./AlertsTableCompositeCell.svelte";
 
