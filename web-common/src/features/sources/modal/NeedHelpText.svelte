316bf6ec11f4b0d0d4dae4547e25f23d11417d24
royendo <67675319+royendo@users.noreply.github.com>
2026-02-04 13:50:16 -0500

    update Modal Help for pbulic

--- web-common/src/features/sources/modal/NeedHelpText.svelte
+++ web-common/src/features/sources/modal/NeedHelpText.svelte
@@ -6,47 +6,46 @@
 </script>
 
 <div>
-  <div class="text-sm leading-none font-medium text-fg-secondary mb-4">
-    Help
-  </div>
-  <div class="text-sm leading-normal font-medium text-fg-muted mb-2">
-    Need help connecting to {connector.displayName}? Check out our documentation
-    for detailed instructions.
-  </div>
-  <span class="flex flex-row items-center gap-2 group">
-    <a
-      href={connector.docsUrl || "https://docs.rilldata.com/build/connectors/"}
-      rel="noreferrer noopener"
-      target="_blank"
-      class="text-sm leading-normal text-primary-500 hover:text-primary-600 font-medium group-hover:underline break-all"
-    >
-      How to connect to {connector.displayName}
-    </a>
-    <ExternalLinkIcon size="16px" color="#6366F1" />
-  </span>
-  {#if connector.displayName === "DuckDB" || connector.displayName === "SQLite"}
-    <div class="mt-8">
-      <div class="text-sm leading-none font-medium text-fg-secondary mb-4">
-        Additional Information
-      </div>
-
-      <div class="text-sm leading-normal font-medium text-fg-muted mb-2">
-        External {connector.displayName} files are meant for local development only.
-        They may run fine on your machine, but aren't reliably supported in production
-        deployments—especially if the file is large (100MB) or outside the data directory.
-      </div>
-    </div>
-  {/if}
   {#if connector.displayName === "Public URL"}
-    <div class="mt-8">
-      <div class="text-sm leading-none font-medium text-fg-secondary mb-4">
-        Supported Sources
-      </div>
-
-      <div class="text-sm leading-normal font-medium text-fg-muted mb-2">
-        Connect to any publicly accessible dataset via HTTP/HTTPS URLs, including
-        public files from GCS, S3, and Azure blob storage.
-      </div>
+    <div class="text-sm leading-none font-medium text-fg-secondary mb-4">
+      Supported Sources
     </div>
+
+    <div class="text-sm leading-normal font-medium text-fg-muted mb-2">
+      Connect to any publicly accessible dataset via HTTP/HTTPS URLs, including
+      public files from GCS, S3, and Azure blob storage.
+    </div>
+  {:else}
+    <div class="text-sm leading-none font-medium text-fg-secondary mb-4">
+      Help
+    </div>
+    <div class="text-sm leading-normal font-medium text-fg-muted mb-2">
+      Need help connecting to {connector.displayName}? Check out our documentation
+      for detailed instructions.
+    </div>
+    <span class="flex flex-row items-center gap-2 group">
+      <a
+        href={connector.docsUrl || "https://docs.rilldata.com/build/connectors/"}
+        rel="noreferrer noopener"
+        target="_blank"
+        class="text-sm leading-normal text-primary-500 hover:text-primary-600 font-medium group-hover:underline break-all"
+      >
+        How to connect to {connector.displayName}
+      </a>
+      <ExternalLinkIcon size="16px" color="#6366F1" />
+    </span>
+    {#if connector.displayName === "DuckDB" || connector.displayName === "SQLite"}
+      <div class="mt-8">
+        <div class="text-sm leading-none font-medium text-fg-secondary mb-4">
+          Additional Information
+        </div>
+
+        <div class="text-sm leading-normal font-medium text-fg-muted mb-2">
+          External {connector.displayName} files are meant for local development only.
+          They may run fine on your machine, but aren't reliably supported in production
+          deployments—especially if the file is large (100MB) or outside the data directory.
+        </div>
+      </div>
+    {/if}
   {/if}
 </div>
