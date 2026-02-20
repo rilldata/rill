91282b6d4bb4d6168935744dbdf4a1791ed20df0
NamanMahor <naman.mahor@rilldata.com>
2026-02-20 16:42:28 +0530

    fix glob resolver

--- runtime/resolvers/glob.go
+++ runtime/resolvers/glob.go
@@ -198,6 +198,10 @@ func (r *globResolver) ResolveInteractive(ctx context.Context) (runtime.Resolver
 		return nil, err
 	}
 
+	if len(entries) > 0 && entries[0].IsDir && r.props.Partition == globPartitionTypeUnspecified {
+		r.props.Partition = globPartitionTypeDirectory
+	}
+
 	var rows []map[string]any
 	switch r.props.Partition {
 	case globPartitionTypeUnspecified, globPartitionTypeFile:
@@ -262,9 +266,6 @@ func (r *globResolver) buildPartitionedResult(entries []drivers.ObjectStoreEntry
 	// Group the entries by directory
 	rows := make(map[string]map[string]any)
 	for _, entry := range entries {
-		if entry.IsDir {
-			continue
-		}
 
 		dir := path.Dir(entry.Path)
 
