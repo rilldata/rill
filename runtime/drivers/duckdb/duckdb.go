f817928e317bf77a5222aa4a969f5e17e34e1403
royendo <67675319+royendo@users.noreply.github.com>
2026-02-21 10:07:34 -0500

    install msql at startup

--- runtime/drivers/duckdb/duckdb.go
+++ runtime/drivers/duckdb/duckdb.go
@@ -529,12 +529,14 @@ func (c *connection) reopenDB(ctx context.Context) error {
 		"INSTALL 'parquet'",
 		"INSTALL 'httpfs'",
 		"INSTALL 'spatial'",
+		"INSTALL 'msql'",
 		"LOAD 'json'",
 		"LOAD 'sqlite'",
 		"LOAD 'icu'",
 		"LOAD 'parquet'",
 		"LOAD 'httpfs'",
 		"LOAD 'spatial'",
+		"LOAD 'msql'",
 		"SET GLOBAL timezone='UTC'",
 		"SET GLOBAL old_implicit_casting = true", // Implicit Cast to VARCHAR
 	)
