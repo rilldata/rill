ef9b8af8b705f6f6329f7c10411828fd4437e3f4
NamanMahor <naman.mahor@rilldata.com>
2026-02-23 18:43:21 +0530

    lint

--- runtime/pkg/blob/bucket.go
+++ runtime/pkg/blob/bucket.go
@@ -120,7 +120,7 @@ func (b *Bucket) ListObjectsForGlob(ctx context.Context, glob string, pageSize u
 		for i, obj := range retval {
 			// Skip entries until we're past startAfter
 			if startAfter != "" {
-				//error out here because we already have  StartOffset/StartAfter/StartFrom pass in api
+				// error out here because we already have StartOffset/StartAfter/StartFrom pass in api
 				if obj.Key < startAfter {
 					return nil, "", fmt.Errorf("blob: entry with key < startAfter (%q)", startAfter)
 				}
