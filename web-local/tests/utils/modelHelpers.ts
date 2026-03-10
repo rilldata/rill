8f54d88097dda746cc6afaacdf93fa7d306e1f81
royendo <67675319+royendo@users.noreply.github.com>
2026-03-10 13:24:34 -0400

    Update modelHelpers.ts

--- web-local/tests/utils/modelHelpers.ts
+++ web-local/tests/utils/modelHelpers.ts
@@ -18,8 +18,7 @@ export async function createModel(page: Page, modelFileName: string) {
 }
 
 export async function modelHasError(page: Page, hasError: boolean, error = "") {
-  const errorLocator = page.getByLabel("Model errors");
-  try {
+  const errorLocator = page.locator(".editor-pane .border-destructive");  try {
     await errorLocator.waitFor({
       timeout: 100,
     });
