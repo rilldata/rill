6284f6089340b1a3845452b667d80b4f287aef0c
royendo <67675319+royendo@users.noreply.github.com>
2026-03-18 13:34:17 -0400

    Update DeploymentSection.svelte

--- web-admin/src/features/projects/status/overview/DeploymentSection.svelte
+++ web-admin/src/features/projects/status/overview/DeploymentSection.svelte
@@ -166,7 +166,7 @@
       </span>
     </div>
 
-    {#if !olapConnector || olapConnector.type === "duckdb" || olapConnector.provision}
+    {#if !olapConnector || olapConnector.provision}
       <div class="info-row">
         <span class="info-label">Data usage</span>
         <span class="info-value">
