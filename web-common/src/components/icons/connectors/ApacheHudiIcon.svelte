3a0be90a5007e0d92aed1843eece9df3a9e1952f
royendo <67675319+royendo@users.noreply.github.com>
2026-03-23 11:46:50 -0400

    chc support table engines

---
+++ web-common/src/components/icons/connectors/ApacheHudiIcon.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  export let size = "1em";
+</script>
+
+<svg
+  width={size}
+  height={size}
+  viewBox="0 0 24 24"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <rect x="2" y="2" width="20" height="20" rx="4" fill="#FF6B35" />
+  <text
+    x="12"
+    y="17"
+    font-family="Arial, Helvetica, sans-serif"
+    font-size="14"
+    font-weight="bold"
+    fill="white"
+    text-anchor="middle"
+  >H</text>
+</svg>
