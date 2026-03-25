8de5b13981f325931fc8d1af2f8788f83155e853
royendo <67675319+royendo@users.noreply.github.com>
2026-03-25 16:44:15 -0400

    Update ContentContainer.svelte

--- web-common/src/components/layout/ContentContainer.svelte
+++ web-common/src/components/layout/ContentContainer.svelte
@@ -2,13 +2,19 @@
   export let title = "";
   export let maxWidth = 800;
   export let showTitle = true;
+  export let flush = false;
 </script>
 
 <main
-  class="bg-surface-base size-full pt-8 pb-16 lg:pt-12 flex flex-col items-center px-8 sm:px-16 lg:px-32 2xl:px-40 overflow-y-auto"
+  class="bg-surface-base size-full flex flex-col items-center overflow-y-auto {flush
+    ? ''
+    : 'pt-8 pb-16 lg:pt-12 px-8 sm:px-16 lg:px-32 2xl:px-40'}"
   style="scrollbar-gutter: stable;"
 >
-  <section class="w-full flex flex-col gap-y-3" style:max-width="{maxWidth}px">
+  <section
+    class="w-full flex flex-col gap-y-3"
+    style:max-width={flush ? 'none' : `${maxWidth}px`}
+  >
     {#if title && showTitle}
       <h1
         class="text-2xl text-fg-primary font-semibold"
