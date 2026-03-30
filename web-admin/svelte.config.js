c6fae342fd0461a06bf550beab0d646a22f4b2eb
Himadri Singh <himadri.singh@gmail.com>
2026-03-30 18:16:03 +0530

    Update svelte.config.js

--- web-admin/svelte.config.js
+++ web-admin/svelte.config.js
@@ -5,11 +5,11 @@ import { resolve, dirname } from "path";
 import { fileURLToPath } from "url";
 
 // svelte.config.js runs before Vite loads .env files, so we load manually.
-// envDir in vite.config.ts points to the repo root ("../").
+// envDir in vite.config.ts points to the repo root ("../"). This if for local testing.
 const __dirname = dirname(fileURLToPath(import.meta.url));
 dotenv({ path: resolve(__dirname, "../.env"), override: false });
 
-const dev = process.env.RILL_ADMIN_FRONTEND_URL?.includes("localhost")
+const dev = process.env.RILL_ADMIN_FRONTEND_URL?.includes("localhost");
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
