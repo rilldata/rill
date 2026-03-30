aa810ddcef145d9e2dad94e5eb416c89de44b764
Himadri Singh <himadri.singh@gmail.com>
2026-03-30 18:16:03 +0530

    Update svelte.config.js

--- web-admin/svelte.config.js
+++ web-admin/svelte.config.js
@@ -16,7 +16,7 @@ const dev = process.env.RILL_ADMIN_FRONTEND_URL?.includes("localhost");
 const __dirname = dirname(fileURLToPath(import.meta.url));
 dotenv({ path: resolve(__dirname, "../.env"), override: false });
 
-const dev = process.env.RILL_ADMIN_FRONTEND_URL?.includes("localhost")
+const dev = process.env.RILL_ADMIN_FRONTEND_URL?.includes("localhost");
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
