acf5888294320cc0375bb4d5b625f4e741909ba3
royendo <67675319+royendo@users.noreply.github.com>
2026-03-31 13:59:09 -0400

    view as canvas

--- web-common/src/features/canvas/CanvasPreviewCTAs.svelte
+++ web-common/src/features/canvas/CanvasPreviewCTAs.svelte
@@ -4,6 +4,15 @@
   import { useRuntimeClient } from "../../runtime-client/v2";
   import { featureFlags } from "../feature-flags";
   import ChatToggle from "@rilldata/web-common/features/chat/layouts/sidebar/ChatToggle.svelte";
+  import ViewAsButton from "../dashboards/granular-access-policies/ViewAsButton.svelte";
+  import {
+    useDashboardPolicyCheck,
+    useRillYamlPolicyCheck,
+  } from "../dashboards/granular-access-policies/useSecurityPolicyCheck";
+  import {
+    ResourceKind,
+    useFilteredResources,
+  } from "../entity-management/resource-selectors";
 
   const client = useRuntimeClient();
 
@@ -12,14 +21,39 @@
   $: canvasQuery = useCanvas(client, canvasName);
   $: canvasFilePath = $canvasQuery.data?.filePath ?? "";
 
+  $: canvasPolicyCheck = useDashboardPolicyCheck(client, canvasFilePath);
+  $: rillYamlPolicyCheck = useRillYamlPolicyCheck(client);
+
+  // Check if any metrics view in the project has security rules
+  $: metricsViewResources = useFilteredResources(
+    client,
+    ResourceKind.MetricsView,
+    (data) => {
+      return (
+        data?.resources?.some(
+          (res) =>
+            (res.metricsView?.state?.validSpec?.securityRules?.length ?? 0) > 0,
+        ) ?? false
+      );
+    },
+  );
+
+  $: hasSecurityPolicy =
+    $canvasPolicyCheck.data ||
+    $rillYamlPolicyCheck.data ||
+    $metricsViewResources.data;
+
   const { dashboardChat, readOnly } = featureFlags;
 </script>
 
-{#if $dashboardChat}
-  <ChatToggle />
-{/if}
-{#if !$readOnly}
-  <div class="flex gap-2 flex-shrink-0 ml-auto">
+<div class="flex gap-2 flex-shrink-0 ml-auto">
+  {#if hasSecurityPolicy}
+    <ViewAsButton />
+  {/if}
+  {#if $dashboardChat}
+    <ChatToggle />
+  {/if}
+  {#if !$readOnly}
     <Button type="secondary" href={`/files${canvasFilePath}`}>Edit</Button>
-  </div>
-{/if}
+  {/if}
+</div>
--- web-local/src/routes/(viz)/canvas/[name]/+page.svelte
+++ web-local/src/routes/(viz)/canvas/[name]/+page.svelte
@@ -4,6 +4,15 @@
   import CanvasProvider from "@rilldata/web-common/features/canvas/CanvasProvider.svelte";
   import DashboardChat from "@rilldata/web-common/features/chat/DashboardChat.svelte";
   import { ResourceKind } from "@rilldata/web-common/features/entity-management/resource-selectors.ts";
+  import ErrorPage from "@rilldata/web-common/components/ErrorPage.svelte";
+  import { resetSelectedMockUserAfterNavigate } from "@rilldata/web-common/features/dashboards/granular-access-policies/resetSelectedMockUserAfterNavigate";
+  import { selectedMockUserStore } from "@rilldata/web-common/features/dashboards/granular-access-policies/stores";
+  import { useCanvas } from "@rilldata/web-common/features/canvas/selector";
+  import {
+    isNotFoundError,
+    extractErrorStatusCode,
+  } from "@rilldata/web-common/lib/errors";
+  import { queryClient } from "@rilldata/web-common/lib/svelte-query/globalQueryClient";
   import type { PageData } from "./$types";
 
   const runtimeClient = useRuntimeClient();
@@ -11,19 +20,34 @@
   export let data: PageData;
 
   $: ({ canvasName } = data);
+
+  resetSelectedMockUserAfterNavigate(queryClient, runtimeClient);
+
+  $: canvasQuery = useCanvas(runtimeClient, canvasName);
+
+  $: mockUserHasNoAccess =
+    $selectedMockUserStore && isNotFoundError($canvasQuery.error);
 </script>
 
 {#key runtimeClient.instanceId}
-  <div class="flex h-full overflow-hidden">
-    <div class="flex-1 overflow-hidden">
-      <CanvasProvider
-        {canvasName}
-        instanceId={runtimeClient.instanceId}
-        showBanner
-      >
-        <CanvasDashboardEmbed {canvasName} />
-      </CanvasProvider>
+  {#if mockUserHasNoAccess}
+    <ErrorPage
+      statusCode={extractErrorStatusCode($canvasQuery.error)}
+      header="This user can't access this dashboard"
+      body="The security policy for this dashboard may make contents invisible to you. If you deploy this dashboard, {$selectedMockUserStore?.email} will see a 404."
+    />
+  {:else}
+    <div class="flex h-full overflow-hidden">
+      <div class="flex-1 overflow-hidden">
+        <CanvasProvider
+          {canvasName}
+          instanceId={runtimeClient.instanceId}
+          showBanner
+        >
+          <CanvasDashboardEmbed {canvasName} />
+        </CanvasProvider>
+      </div>
+      <DashboardChat kind={ResourceKind.Canvas} />
     </div>
-    <DashboardChat kind={ResourceKind.Canvas} />
-  </div>
+  {/if}
 {/key}
