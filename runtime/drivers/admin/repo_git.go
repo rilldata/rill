e020bb700641eeb2b97440f99ba7084421f5f154
Anshul Khandelwal <12948312+k-anshul@users.noreply.github.com>
2026-04-10 14:15:32 +0530

    use ctx

--- runtime/drivers/admin/repo_git.go
+++ runtime/drivers/admin/repo_git.go
@@ -162,7 +162,7 @@ func (r *gitRepo) pullInner(ctx context.Context, force bool) error {
 	}
 
 	// Hard reset to remote branch
-	err = resetToRemoteTrackingBranch(r.repoDir, r.defaultBranch)
+	err = resetToRemoteTrackingBranch(ctx, r.repoDir, r.defaultBranch)
 	if err != nil {
 		if !(errors.Is(err, errRefNotFound) && r.editable()) { // In editable mode, the default branch may not exist yet on remote.
 			return fmt.Errorf("failed to reset to remote tracking branch %q: %w", r.defaultBranch, err)
@@ -291,7 +291,7 @@ func (r *gitRepo) commitAndPushToPrimaryBranch(ctx context.Context, message stri
 	if err != nil {
 		return fmt.Errorf("failed to checkout primary branch %q: %w", r.primaryBranch, err)
 	}
-	err = resetToRemoteTrackingBranch(r.repoDir, r.primaryBranch)
+	err = resetToRemoteTrackingBranch(ctx, r.repoDir, r.primaryBranch)
 	if err != nil {
 		return fmt.Errorf("failed to reset to remote tracking branch %q: %w", r.primaryBranch, err)
 	}
@@ -418,8 +418,8 @@ var errRefNotFound = errors.New("reference not found")
 // resetToRemoteTrackingBranch resets to the commit pointed by the remote tracking branch.
 // This is used to reset the local branch to the state of the remote branch so it is expected that the latest changes have been fetched.
 // go-git wipes out git-ignored changes so must use the git command.
-func resetToRemoteTrackingBranch(repoDir, branch string) error {
-	cmd := exec.Command("git", "-C", repoDir, "reset", "--hard", "origin/"+branch)
+func resetToRemoteTrackingBranch(ctx context.Context, repoDir, branch string) error {
+	cmd := exec.CommandContext(ctx, "git", "-C", repoDir, "reset", "--hard", "origin/"+branch)
 	_, err := cmd.Output()
 	if err != nil {
 		var execErr *exec.ExitError
