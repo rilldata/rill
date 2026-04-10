dfe60d80124c9d22932d18bce926e40e936884da
royendo <67675319+royendo@users.noreply.github.com>
2026-04-10 13:54:17 -0400

    Revert "fix: do not apply layout classes in ThemeProvider when applyLayout is false"
    This reverts commit 566731cbeae1137071b07e4af85b3f16c6b9a1f0.

--- web-common/src/features/dashboards/ThemeProvider.svelte
+++ web-common/src/features/dashboards/ThemeProvider.svelte
@@ -36,10 +36,7 @@
 </script>
 
 <div
-  class="dashboard-theme-boundary"
-  class:flex={applyLayout}
-  class:flex-col={applyLayout}
-  class:overflow-hidden={applyLayout}
+  class="dashboard-theme-boundary flex flex-col overflow-hidden"
   class:w-full={applyLayout && $dynamicHeight}
   class:size-full={applyLayout && !$dynamicHeight}
   bind:this={themeBoundary}
