66d95d14e0c5fa1fb154ad08fa784c4edf7fb4aa
Benjamin Egelund-Müller <b@egelund-muller.com>
2026-04-15 15:06:57 +0200

    Revert TimestampsResult refactor

--- runtime/metricsview/timestamps.go
+++ runtime/metricsview/timestamps.go
@@ -3,11 +3,6 @@ package metricsview
 import "time"
 
 type TimestampsResult struct {
-	BaseTable TimestampsResultEntry
-	Rollups   map[string]TimestampsResultEntry
-}
-
-type TimestampsResultEntry struct {
 	Min       time.Time
 	Max       time.Time
 	Watermark time.Time
