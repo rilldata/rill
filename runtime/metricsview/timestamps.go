c46f0ef9b9db591fc1aedb13bde443e595141cba
Benjamin Egelund-Müller <b@egelund-muller.com>
2026-04-15 15:04:33 +0200

    Clean up project schema, refactor TimestampsResult, fix SQL example

--- runtime/metricsview/timestamps.go
+++ runtime/metricsview/timestamps.go
@@ -3,6 +3,11 @@ package metricsview
 import "time"
 
 type TimestampsResult struct {
+	BaseTable TimestampsResultEntry
+	Rollups   map[string]TimestampsResultEntry
+}
+
+type TimestampsResultEntry struct {
 	Min       time.Time
 	Max       time.Time
 	Watermark time.Time
