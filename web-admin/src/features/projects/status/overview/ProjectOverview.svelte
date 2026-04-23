d796210cb8430ca6f530b9fd736341aa8b41482f
royendo <67675319+royendo@users.noreply.github.com>
2026-04-23 12:59:03 -0400

    prettier

--- web-admin/src/features/projects/status/overview/ProjectOverview.svelte
+++ web-admin/src/features/projects/status/overview/ProjectOverview.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { createAdminServiceGetProject } from "@rilldata/web-admin/client";
+  import { getGitUrlFromRemote } from "@rilldata/web-common/features/project/deploy/github-utils";
   import { useRuntimeClient } from "@rilldata/web-common/runtime-client/v2";
   import { useParserReconcileError } from "../selectors";
   import DeploymentSection from "./DeploymentSection.svelte";
@@ -12,11 +14,24 @@
   const runtimeClient = useRuntimeClient();
   $: parserErrorQuery = useParserReconcileError(runtimeClient);
   $: hasProjectError = !!($parserErrorQuery.data ?? "");
+
+  // Only hide the sections below Deployment when the parser error is actually
+  // surfaced to the user (i.e. on GitHub-connected projects). Otherwise a
+  // silent parser error on a Rill-managed project leaves the page looking
+  // empty with no explanation.
+  $: proj = createAdminServiceGetProject(organization, project);
+  $: projectData = $proj.data?.project;
+  $: githubUrl = projectData?.gitRemote
+    ? getGitUrlFromRemote(projectData.gitRemote)
+    : "";
+  $: isGithubConnected =
+    !!projectData?.gitRemote && !projectData?.managedGitId && !!githubUrl;
+  $: hideBelow = hasProjectError && isGithubConnected;
 </script>
 
 <div class="flex flex-col gap-6">
   <DeploymentSection {organization} {project} />
-  {#if !hasProjectError}
+  {#if !hideBelow}
     <ResourcesSection />
     <TablesSection />
     <ErrorsSection />
