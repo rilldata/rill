39d3706275c1e13ff5853d5243a2fb9287425bea
Benjamin Egelund-Müller <b@egelund-muller.com>
2026-04-24 14:28:21 +0100

    Server error codes audit (#9277)
    * Server error codes audit
    
    * Review
    
    * Handle controller closed
    
    * Review

--- runtime/server/chat.go
+++ runtime/server/chat.go
@@ -133,10 +133,10 @@ func (s *Server) ShareConversation(ctx context.Context, req *runtimev1.ShareConv
 	}
 	msg, ok := session.LatestMessage(preds...)
 	if !ok {
-		return nil, status.Errorf(codes.InvalidArgument, "message with id %q not found in conversation %q", req.UntilMessageId, req.ConversationId)
+		return nil, status.Errorf(codes.NotFound, "message with id %q not found in conversation %q", req.UntilMessageId, req.ConversationId)
 	}
 	if req.UntilMessageId != "" && !(msg.Tool == ai.RouterAgentName && msg.Type == ai.MessageTypeResult) {
-		return nil, status.Errorf(codes.InvalidArgument, "cannot share incomplete conversation as message with id %q is not a router agent result message", req.UntilMessageId)
+		return nil, status.Errorf(codes.FailedPrecondition, "cannot share incomplete conversation as message with id %q is not a router agent result message", req.UntilMessageId)
 	}
 
 	// now save the session with the shared until message id and flush immediately
@@ -545,12 +545,12 @@ func (s *Server) GetAIMessage(ctx context.Context, req *runtimev1.GetAIMessageRe
 		Claims:     claims,
 	})
 	if err != nil {
-		return nil, status.Errorf(codes.NotFound, "failed to find the conversation: %q", req.ConversationId)
+		return nil, err
 	}
 
 	msg, ok := session.Message(ai.FilterByID(req.MessageId))
 	if !ok {
-		return nil, status.Errorf(codes.NotFound, "failed to find the call: %q", req.MessageId)
+		return nil, status.Errorf(codes.NotFound, "message %q not found in conversation %q", req.MessageId, req.ConversationId)
 	}
 
 	pbMsg, err := messageToPB(session, msg)
