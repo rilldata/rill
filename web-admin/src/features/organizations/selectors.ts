1dbe1d1c27510368ae0c2a2bce81ab7234ebe688
Aditya Hegde <adityahegderocks@gmail.com>
2026-05-02 11:55:55 +0530

    fix: hibernated message for orgs with only unpublished project

--- web-admin/src/features/organizations/selectors.ts
+++ web-admin/src/features/organizations/selectors.ts
@@ -1,29 +1,94 @@
 import {
   adminServiceGetOrganization,
+  adminServiceListDeployments,
   adminServiceListProjectsForOrganization,
+  createAdminServiceListDeployments,
   createAdminServiceListProjectsForOrganization,
   getAdminServiceGetOrganizationQueryKey,
+  getAdminServiceListDeploymentsQueryKey,
   getAdminServiceListProjectsForOrganizationQueryKey,
   type V1GetOrganizationResponse,
   type V1Organization,
+  type V1Project,
 } from "@rilldata/web-admin/client";
 import { queryClient } from "@rilldata/web-common/lib/svelte-query/globalQueryClient";
 import type { FetchQueryOptions } from "@tanstack/query-core";
+import { derived } from "svelte/store";
+import { isActiveDeployment } from "@rilldata/web-admin/features/branches/deployment-utils.ts";
 
 export function areAllProjectsHibernating(organization: string) {
-  return createAdminServiceListProjectsForOrganization(
+  const projectsQuery = createAdminServiceListProjectsForOrganization(
     organization,
     undefined,
     {
       query: {
         enabled: !!organization,
-        select: (data) =>
-          data.projects?.length &&
-          data.projects.every((p) => !p.primaryDeploymentId),
       },
     },
+    queryClient,
   );
+  return derived(projectsQuery, (projectsResp, set) => {
+    const isPending = projectsResp.isPending;
+    const error = projectsResp.error;
+    if (isPending || error) {
+      set({
+        isPending,
+        error,
+        data: undefined,
+      });
+      return;
+    }
+
+    const projects = projectsResp.data?.projects ?? [];
+    const allHibernating = allProjectsHibernating(projects);
+    if (!allHibernating) {
+      set({
+        isPending: false,
+        error: undefined,
+        data: false,
+      });
+      return;
+    }
+
+    projects.sort((a, b) => (a.updatedOn > b.updatedOn ? -1 : 1));
+    const deploymentQueries = projects
+      .slice(0, TopProjectsCount)
+      .map((project) =>
+        createAdminServiceListDeployments(
+          organization,
+          project.name,
+          {},
+          {},
+          queryClient,
+        ),
+      );
+
+    return derived(deploymentQueries, (deploymentResps) => {
+      const isPending = deploymentResps.some((dr) => dr.isPending);
+      const error = deploymentResps.find((dr) => dr.error)?.error;
+      if (isPending || error) {
+        return {
+          isPending,
+          error,
+          data: undefined,
+        };
+      }
+
+      const hasSomeActiveDeployment = deploymentResps.some((deploymentsResp) =>
+        deploymentsResp.data?.deployments?.some(
+          (d) => d && isActiveDeployment(d),
+        ),
+      );
+      return {
+        isPending: false,
+        error: error,
+        data: !hasSomeActiveDeployment,
+      };
+    }).subscribe(set);
+  });
 }
+
+const TopProjectsCount = 5;
 
 export async function fetchAllProjectsHibernating(organization: string) {
   const projectsResp = await queryClient.fetchQuery({
@@ -31,7 +96,43 @@ export async function fetchAllProjectsHibernating(organization: string) {
     queryFn: () => adminServiceListProjectsForOrganization(organization),
     staleTime: Infinity,
   });
-  return projectsResp.projects?.every((p) => !p.primaryDeploymentId) ?? false;
+  const projects = projectsResp.projects ?? [];
+  const allHibernating = allProjectsHibernating(projects);
+  if (!allHibernating) return false;
+
+  projects.sort((a, b) => (a.updatedOn > b.updatedOn ? -1 : 1));
+  const deploymentsQueryPromises = projects
+    .slice(0, TopProjectsCount)
+    .map(async (project) => {
+      const deploymentsResp = await queryClient.fetchQuery({
+        queryKey: getAdminServiceListDeploymentsQueryKey(
+          organization,
+          project.name,
+          {},
+        ),
+        queryFn: () =>
+          adminServiceListDeployments(organization, project.name, {}),
+        staleTime: Infinity,
+      });
+      return deploymentsResp.deployments?.some(
+        (d) => d && isActiveDeployment(d),
+      );
+    });
+  const hasSomeActiveDeployment = (
+    await Promise.all(deploymentsQueryPromises)
+  ).some(Boolean);
+  return !hasSomeActiveDeployment;
+}
+
+export function getFetchOrganizationQueryOptions(
+  organization: string | V1Organization | undefined,
+) {
+  const orgName = normalizeOrganization(organization);
+  return <FetchQueryOptions<V1GetOrganizationResponse>>{
+    queryKey: getAdminServiceGetOrganizationQueryKey(orgName),
+    queryFn: () => adminServiceGetOrganization(orgName),
+    staleTime: Infinity,
+  };
 }
 
 function normalizeOrganization(
@@ -53,13 +154,6 @@ function normalizeOrganization(
   );
 }
 
-export function getFetchOrganizationQueryOptions(
-  organization: string | V1Organization | undefined,
-) {
-  const orgName = normalizeOrganization(organization);
-  return <FetchQueryOptions<V1GetOrganizationResponse>>{
-    queryKey: getAdminServiceGetOrganizationQueryKey(orgName),
-    queryFn: () => adminServiceGetOrganization(orgName),
-    staleTime: Infinity,
-  };
+function allProjectsHibernating(projects: V1Project[] | undefined) {
+  return projects?.length && projects.every((p) => !p.primaryDeploymentId);
 }
