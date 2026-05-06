ae0e626f6a01179f50de7fd6a7a807db3b4f0d49
Aditya Hegde <adityahegderocks@gmail.com>
2026-05-06 21:26:12 +0530

    chore: remove legacy org hibernating message (#9396)

--- web-admin/src/features/billing/issues/useBillingIssueMessage.ts
+++ web-admin/src/features/billing/issues/useBillingIssueMessage.ts
@@ -8,7 +8,6 @@ import {
   isTeamPlan,
 } from "@rilldata/web-admin/features/billing/plans/utils";
 import { useCategorisedOrganizationBillingIssues } from "@rilldata/web-admin/features/billing/selectors";
-import { areAllProjectsHibernating } from "@rilldata/web-admin/features/organizations/selectors";
 import type { BannerMessage } from "@rilldata/web-common/lib/event-bus/events";
 import { derived } from "svelte/store";
 
@@ -32,32 +31,20 @@ export function useBillingIssueMessage(organization: string) {
     [
       createAdminServiceGetOrganization(organization),
       useCategorisedOrganizationBillingIssues(organization),
-      areAllProjectsHibernating(organization),
     ],
-    ([orgResp, categorisedIssuesResp, allProjectsHibernatingResp]) => {
-      if (
-        orgResp.isLoading ||
-        categorisedIssuesResp.isLoading ||
-        allProjectsHibernatingResp.isLoading
-      ) {
+    ([orgResp, categorisedIssuesResp]) => {
+      if (orgResp.isLoading || categorisedIssuesResp.isLoading) {
         return {
           isFetching: true,
           isLoading: true,
           error: undefined,
         };
       }
-      if (
-        orgResp.error ||
-        categorisedIssuesResp.error ||
-        allProjectsHibernatingResp.error
-      ) {
+      if (orgResp.error || categorisedIssuesResp.error) {
         return {
           isFetching: false,
           isLoading: false,
-          error:
-            orgResp.error ??
-            categorisedIssuesResp.error ??
-            allProjectsHibernatingResp.error,
+          error: orgResp.error ?? categorisedIssuesResp.error,
         };
       }
 
@@ -98,24 +85,6 @@ export function useBillingIssueMessage(organization: string) {
             error: undefined,
             data: paymentIssue,
           };
-      }
-
-      if (allProjectsHibernatingResp.data) {
-        return {
-          isFetching: false,
-          isLoading: false,
-          error: undefined,
-          data: <BillingIssueMessage>{
-            type: "default",
-            title: "Your org’s projects are hibernating.",
-            description: "",
-            iconType: "sleep",
-            cta: {
-              type: "wake-projects",
-              text: "Wake projects",
-            },
-          },
-        };
       }
 
       return {
