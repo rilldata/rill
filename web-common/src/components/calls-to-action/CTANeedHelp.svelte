55917ed9a6efb5db159a90e169325288374d3cd6
Eric P Green <ericpgreen2@gmail.com>
2026-05-06 17:46:14 -0400

    Prettier

--- web-common/src/components/calls-to-action/CTANeedHelp.svelte
+++ web-common/src/components/calls-to-action/CTANeedHelp.svelte
@@ -8,9 +8,7 @@
 
 {#if !isEmbedded}
   <p class="text-sm text-fg-secondary text-center">
-    {#if leading}{leading}{" "}{/if}Need help? Reach out to us on <a
-      href="https://discord.gg/2ubRfjC7Rh"
-      target="_blank">Discord</a
-    >
+    {#if leading}{leading}{" "}{/if}Need help? Reach out to us on
+    <a href="https://discord.gg/2ubRfjC7Rh" target="_blank">Discord</a>
   </p>
 {/if}
