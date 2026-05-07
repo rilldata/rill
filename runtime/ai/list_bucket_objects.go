fa8176bec5e33a5fd4dc8f022d28a5c1b785f0d7
Benjamin Egelund-Müller <b@egelund-muller.com>
2026-05-07 15:05:25 +0100

    Fix func signature

--- runtime/ai/list_bucket_objects.go
+++ runtime/ai/list_bucket_objects.go
@@ -94,7 +94,7 @@ func (t *ListBucketObjects) Handler(ctx context.Context, args *ListBucketObjects
 	var objects []drivers.ObjectStoreEntry
 	var nextToken string
 	if fileutil.IsGlob(args.Path) {
-		objects, nextToken, err = os.ListObjectsForGlob(ctx, args.Bucket, args.Path, uint32(pageSize), args.PageToken)
+		objects, nextToken, err = os.ListObjectsForGlob(ctx, args.Bucket, args.Path, uint32(pageSize), args.PageToken, "", "")
 	} else {
 		objects, nextToken, err = os.ListObjects(ctx, args.Bucket, args.Path, "/", uint32(pageSize), args.PageToken)
 	}
