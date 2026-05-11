8e8a12ce892c47ee7a2d9bb14c4c0ff86691222b
Aditya Hegde <adityahegderocks@gmail.com>
2026-05-11 15:18:56 +0530

    fix: file upload error not surfacing in the form (#9415)
    * fix: file upload error not surfacing in the form
    
    * fix lint

--- web-common/src/features/add-data/form/SourceForm.svelte
+++ web-common/src/features/add-data/form/SourceForm.svelte
@@ -33,8 +33,12 @@
     getImportStepsForSource,
   } from "@rilldata/web-common/features/add-data/manager/steps/utils.ts";
   import { maybeInitProject } from "@rilldata/web-common/features/add-data/manager/steps/connector.ts";
+  import { setSubmitError } from "@rilldata/web-common/features/add-data/form/errors.ts";
+  import type { AddDataStateManager } from "@rilldata/web-common/features/add-data/manager/AddDataStateManager.svelte.ts";
+  import { setError, type SuperValidated } from "sveltekit-superforms";
 
   export let config: AddDataConfig;
+  export let stateManager: AddDataStateManager;
   export let step: CreateModelStep;
   export let onSubmit: (importConfig: ImportStepConfig) => void;
   export let onBack: () => void;
@@ -85,7 +89,12 @@
     formType: "source",
     onUpdate: async ({ form }) => {
       if (!form.valid) return;
-      return submitImportConfig(form.data);
+      try {
+        await submitImportConfig(form);
+      } catch (e) {
+        stateManager.fireErrorEvent(e.message);
+        setSubmitError(form, e);
+      }
     },
     additionalDefaults: step.connectorFormValues,
   });
@@ -106,10 +115,13 @@
 
   $: sourceFormLabels = getLabelsForSource(importSteps);
 
-  async function submitImportConfig(formValues: any) {
+  async function submitImportConfig(
+    form: SuperValidated<Record<string, unknown>>,
+  ) {
     if (!connectorDriver) {
       throw new Error("Connector driver not found for: " + step.connector);
     }
+    const formValues = form.data;
 
     await maybeInitProject(runtimeClient);
 
@@ -137,14 +149,17 @@
     if (formValues.file) {
       // TODO: support multiple files upload
       const firstFile = formValues.file[0];
-      const filePath = await uploadFile(runtimeClient, firstFile);
-      if (filePath) {
+      try {
+        const filePath = await uploadFile(runtimeClient, firstFile);
         formValues.path = filePath;
         const [, fileName] = splitFolderFileNameAndExtension(filePath);
         formValues.name = getName(
           fileName,
           fileArtifacts.getNamesForKind(ResourceKind.Model),
         );
+      } catch (e) {
+        setError(form, "file", e.message); // set error on the file field
+        throw e; // rethrow so that global error handler is triggered
       }
     }
     const yaml = getSourceYamlPreview({
@@ -165,7 +180,10 @@
       importSteps,
       connector: rewrittenConnector.name!,
       importFrom,
-      importTo: generateImportToConfig(importFrom, formValues.name),
+      importTo: generateImportToConfig(
+        importFrom,
+        formValues.name as string | undefined,
+      ),
       envBlob: newBlob,
     } satisfies ImportStepConfig;
 
