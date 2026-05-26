e5ac544a68fbf56cbf059c09b9491ab6a15794bf
Parag Jain <pjain1@apache.org>
2026-05-26 17:59:23 +0530

    review comments

--- runtime/drivers/sqlite/migrations/0041.sql
+++ runtime/drivers/sqlite/migrations/0041.sql
@@ -1,1 +1,1 @@
-CREATE INDEX IF NOT EXISTS ai_messages_session_id_created_on_idx ON ai_messages (session_id, created_on);
+CREATE INDEX IF NOT EXISTS ai_messages_session_id_idx ON ai_messages (session_id);
