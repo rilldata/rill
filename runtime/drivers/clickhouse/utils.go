2aa8489ea2122312311dacd6718595c89546420d
NamanMahor <naman.mahor@rilldata.com>
2026-06-03 16:56:54 +0530

    lint

--- runtime/drivers/clickhouse/utils.go
+++ runtime/drivers/clickhouse/utils.go
@@ -1,17 +1,9 @@
 package clickhouse
 
-import (
-	"strings"
-)
-
 func safeSQLName(name string) string {
 	return DialectClickhouse.EscapeIdentifier(name)
 }
 
 func localTableName(name string) string {
 	return name + "_local"
-}
-
-func localToActualTableName(name string) string {
-	return strings.TrimSuffix(name, "_local")
 }
