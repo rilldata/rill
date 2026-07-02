d335ed0b811effae726e76110d7f08490979fdbe
dfliess <dfliess@gmail.com>
2026-07-02 16:42:45 +0200

    Add webhook notifier driver with Standard Webhooks signing

--- cli/cmd/runtime/start.go
+++ cli/cmd/runtime/start.go
@@ -54,6 +54,7 @@ import (
 	_ "github.com/rilldata/rill/runtime/drivers/snowflake"
 	_ "github.com/rilldata/rill/runtime/drivers/sqlite"
 	_ "github.com/rilldata/rill/runtime/drivers/starrocks"
+	_ "github.com/rilldata/rill/runtime/drivers/webhook"
 	_ "github.com/rilldata/rill/runtime/reconcilers"
 	_ "github.com/rilldata/rill/runtime/resolvers"
 )
